50dc7cd5b23d7e823611a42388e92c96b8b38c67
Marcus Klein <marcus.klein@open-xchange.com>
2006-06-26 16:06:21 +0000

    Fixed to be able to insert a task.

--- openexchange-test/src/com/openexchange/ajax/TasksTest.java
+++ openexchange-test/src/com/openexchange/ajax/TasksTest.java
@@ -1,9 +1,9 @@
 package com.openexchange.ajax;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Date;
 
 import org.json.JSONObject;
 
@@ -24,35 +24,50 @@ public class TasksTest extends AbstractAJAXTest {
      * Test method for 'com.openexchange.ajax.Tasks.doPut(HttpServletRequest, HttpServletResponse)'
      */
     public void testInsertTask() throws Throwable {
-        Task task = new Task();
+        final Task task = new Task();
         task.setTitle("Test task");
-        insertTask(getWebConversation(), hostName, getSessionId(), task);
+        task.setParentFolderID(62);
+        assertTrue(insertTask(getWebConversation(), hostName, getSessionId(),
+            task) > 0);
     }
 
+    /**
+     * This method implements storing of a task through the AJAX interface.
+     * @param wc WebConversation.
+     * @param hostname Host name of the server.
+     * @param sessionId Session identifier of the user.
+     * @param task Task to store.
+     * @return the unique identifer of the task.
+     * @throws Exception if an error occurs while storing the task.
+     */
     public static int insertTask(final WebConversation wc,
         final String hostname, final String sessionId, final Task task)
         throws Exception {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        TaskWriter tw = new TaskWriter(pw);
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw);
+        final TaskWriter tw = new TaskWriter(pw);
         tw.writeTask(task);
         pw.flush();
-        ByteArrayInputStream bais = new ByteArrayInputStream(sw.toString()
+        String object = sw.toString();
+        System.out.println(object);
+        final ByteArrayInputStream bais = new ByteArrayInputStream(object
             .getBytes("UTF-8"));
-        URLParameter parameter = new URLParameter();
+        final URLParameter parameter = new URLParameter();
         parameter.setParameter(AJAXServlet.PARAMETER_SESSION, sessionId);
         parameter.setParameter(AJAXServlet.PARAMETER_ACTION, AJAXServlet.ACTION_NEW);
-        WebRequest req = new PutMethodWebRequest(PROTOCOL + hostname +
+        final WebRequest req = new PutMethodWebRequest(PROTOCOL + hostname +
             TASKS_URL + parameter.getURLParameters(), bais, AJAXServlet
             .CONTENTTYPE_JAVASCRIPT);
-        WebResponse resp = wc.getResponse(req);
-        assertEquals(200, resp.getResponseCode());
-        JSONObject jsonId = new JSONObject(resp.getText());
+        final WebResponse resp = wc.getResponse(req);
+        assertEquals("Response code is not okay.", 200, resp.getResponseCode());
+        String text = resp.getText();
+        System.out.println("!" + text + "!");
+        final JSONObject jsonId = new JSONObject(text);
         if (jsonId.has("error")) {
             fail(jsonId.getString("error"));
         }
-        final int id = jsonId.getInt(OXObject.OBJECT_ID);
-        assertTrue(id > 0);
-        return id;
+        final int identifier = jsonId.getInt(OXObject.OBJECT_ID);
+        assertTrue("Unique identifier of task is zero.", identifier > 0);
+        return identifier;
     }
 }
