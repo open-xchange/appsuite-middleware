2ee258b54e967a67b8b2ab82ae06e786cfc544f1
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-07-27 15:58:53 +0000

    add AppointmentTest for webdav

---
+++ openexchange-test/src/com/openexchange/webdav/xml/AppointmentTest.java
@@ -0,0 +1,41 @@
+package com.openexchange.webdav.xml;
+
+import junit.framework.TestCase;
+
+public class AppointmentTest extends TestCase {
+	
+	private static String url = "/servlet/calendar";
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testNewAppointment() throws Exception {
+		fail("not supported yet!");
+	}
+	
+	public void testNewAppointmentWithParticipants() throws Exception {
+		fail("not supported yet!");
+	}
+	
+	public void testUpdateAppointment() throws Exception {
+		fail("not supported yet!");
+	}
+	
+	public void testUpdateAppointmentWithParticipants() throws Exception {
+		fail("not supported yet!");
+	}
+
+	public void testDelete() throws Exception {
+		fail("not supported yet!");
+	}
+	
+	public void testPropFind() throws Exception {
+		fail("not supported yet!");
+	}
+	
+	public void testConfirm() throws Exception {
+		fail("not supported yet!");
+	}
+}
+
