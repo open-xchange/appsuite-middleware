a43a8fcbbbb184f23a2d8f390079b91b026787d0
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-08-10 13:57:14 +0000

    add ResourceTest

---
+++ openexchange-test/src/com/openexchange/ajax/ResourceTest.java
@@ -0,0 +1,99 @@
+package com.openexchange.ajax;
+
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.PutMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.openexchange.ajax.container.Response;
+import com.openexchange.ajax.fields.ParticipantsFields;
+import com.openexchange.tools.URLParameter;
+import java.io.ByteArrayInputStream;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ResourceTest extends AbstractAJAXTest {
+	
+	private static final String GROUP_URL = "/ajax/resource";
+	
+	public void testSearch() throws Exception {
+		com.openexchange.groupware.ldap.Resource resources[] = searchResource(getWebConversation(), "*", PROTOCOL + getHostName(), getSessionId());
+		assertTrue("resource array size is not > 0", resources.length > 0);
+	}
+	
+	public void testGet() throws Exception {
+		com.openexchange.groupware.ldap.Resource resources[] = searchResource(getWebConversation(), "*", PROTOCOL + getHostName(), getSessionId());
+		assertTrue("resource array size is not > 0", resources.length > 0);
+		com.openexchange.groupware.ldap.Resource r = loadResource(getWebConversation(), resources[0].getIdentifier(), PROTOCOL + getHostName(), getSessionId());
+	}
+	
+	public static com.openexchange.groupware.ldap.Resource[] searchResource(WebConversation webCon, String searchpattern, String host, String session) throws Exception {
+		final URLParameter parameter = new URLParameter();
+		parameter.setParameter(AJAXServlet.PARAMETER_SESSION, session);
+		parameter.setParameter(AJAXServlet.PARAMETER_ACTION, AJAXServlet.ACTION_SEARCH);
+		
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("pattern", searchpattern);
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(jsonObj.toString().getBytes());
+		WebRequest req = new PutMethodWebRequest(host + GROUP_URL + parameter.getURLParameters(), bais, "text/javascript");
+		WebResponse resp = webCon.getResponse(req);
+		
+		assertEquals(200, resp.getResponseCode());
+		
+		final Response response = Response.parse(resp.getText());
+		
+		if (response.hasError()) {
+			fail("json error: " + response.getErrorMessage());
+		}
+		
+		assertNotNull("timestamp is null", response.getTimestamp());
+		
+		JSONArray jsonArray = (JSONArray)response.getData();
+		com.openexchange.groupware.ldap.Resource[] r = new com.openexchange.groupware.ldap.Resource[jsonArray.length()];
+		for (int a = 0; a < r.length; a++) {
+			JSONObject jObj = jsonArray.getJSONObject(a);
+			r[a] = new com.openexchange.groupware.ldap.Resource();
+			r[a].setIdentifier(jObj.getInt(ParticipantsFields.ID));
+			if (jObj.has(ParticipantsFields.DISPLAY_NAME)) {
+				r[a].setDisplayName(jObj.getString(ParticipantsFields.DISPLAY_NAME));
+			}
+		}
+		
+		return r;
+	}
+	
+	public static com.openexchange.groupware.ldap.Resource loadResource(WebConversation webCon, int groupId, String host, String session) throws Exception {
+		final URLParameter parameter = new URLParameter();
+		parameter.setParameter(AJAXServlet.PARAMETER_SESSION, session);
+		parameter.setParameter(AJAXServlet.PARAMETER_ACTION, AJAXServlet.ACTION_GET);
+		parameter.setParameter(AJAXServlet.PARAMETER_ID, String.valueOf(groupId));
+		
+		WebRequest req = new GetMethodWebRequest(host + GROUP_URL + parameter.getURLParameters());
+		WebResponse resp = webCon.getResponse(req);
+		
+		assertEquals(200, resp.getResponseCode());
+		
+		final Response response = Response.parse(resp.getText());
+		
+		if (response.hasError()) {
+			fail("json error: " + response.getErrorMessage());
+		}
+		
+		assertNotNull("timestamp is null", response.getTimestamp());
+		
+		JSONObject jsonObj = (JSONObject)response.getData();
+		
+		com.openexchange.groupware.ldap.Resource r = new com.openexchange.groupware.ldap.Resource();
+		assertTrue("id is not in json object", jsonObj.has(ParticipantsFields.ID));
+		r.setIdentifier(jsonObj.getInt(ParticipantsFields.ID));
+		
+		if (jsonObj.has(ParticipantsFields.DISPLAY_NAME)) {
+			r.setDisplayName(jsonObj.getString(ParticipantsFields.DISPLAY_NAME));
+		}
+		
+		return r;
+	}
+}
+
