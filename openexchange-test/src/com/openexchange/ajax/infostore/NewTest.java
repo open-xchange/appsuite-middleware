4422d9e04e7c654f5f66120e1e0fc4b855d99143
Francisco Laguna <francisco.laguna@open-xchange.com>
2006-08-15 08:56:06 +0000

    disable upload test

--- openexchange-test/src/com/openexchange/ajax/infostore/NewTest.java
+++ openexchange-test/src/com/openexchange/ajax/infostore/NewTest.java
@@ -1,6 +1,16 @@
 package com.openexchange.ajax.infostore;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.json.JSONObject;
+
 import com.openexchange.ajax.InfostoreAJAXTest;
+import com.openexchange.ajax.container.Response;
+import com.openexchange.groupware.Init;
 
 
 public class NewTest extends InfostoreAJAXTest {
@@ -9,8 +19,68 @@ public class NewTest extends InfostoreAJAXTest {
 		super();
 	}
 	
-	public void testNothingInParticular(){
-		assertTrue(true); // FIXME
+	public void notestUpload() throws Exception{
+		File upload = new File(Init.getTestProperty("ajaxPropertiesFile"));
+		int id = createNew(
+				sessionId,
+				m(
+						"folder_id" 		,	((Integer)folderId).toString(),
+						"title"  		,  	"test upload",
+						"description" 	, 	"test upload description"
+				),
+				upload,
+				"text/plain"
+		);
+		clean.add(id);
+		
+		Response res = get(sessionId,id);
+		JSONObject obj = (JSONObject) res.getData();
+		
+		assertEquals("test upload",obj.getString("title"));
+		assertEquals("test upload description",obj.getString("description"));
+		assertEquals(1,obj.getInt("version"));
+		assertEquals("text/plain",obj.getString("file_mimetype"));
+		assertEquals(upload.getName(),obj.getString("filename"));
+
+		
+		InputStream is = null;
+		InputStream is2 = null;
+		try {
+			//is = new FileInputStream(upload);
+			is2 = document(sessionId,id,1);
+			
+			//BufferedReader r = new BufferedReader(new InputStreamReader(is2));
+			//String line = null;
+			//while((line=r.readLine())!=null){
+			//	System.out.println(line);
+			//}
+			
+			//assertSameContent(is,is2);
+		} finally {
+			if(is!=null)
+				is.close();
+			if(is2!=null)
+				is2.close();
+		}
+		
+		id = createNew(
+			sessionId,
+			m(
+					"folder_id" 		,	((Integer)folderId).toString(),
+					"title"  		,  	"test no upload",
+					"description" 	, 	"test no upload description"
+			),
+			null,
+			""
+		);
+		clean.add(id);
+		
+		res = get(sessionId,id);
+		obj = (JSONObject) res.getData();
+		
+		assertEquals("test no upload",obj.getString("title"));
+		assertEquals("test no upload description",obj.getString("description"));
+		
 	}
 
 }
--- openexchange-test/src/com/openexchange/ajax/infostore/UpdateTest.java
+++ openexchange-test/src/com/openexchange/ajax/infostore/UpdateTest.java
@@ -1,9 +1,14 @@
 package com.openexchange.ajax.infostore;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import org.json.JSONObject;
 
 import com.openexchange.ajax.InfostoreAJAXTest;
 import com.openexchange.ajax.container.Response;
+import com.openexchange.groupware.Init;
 
 
 public class UpdateTest extends InfostoreAJAXTest {
@@ -27,6 +32,67 @@ public class UpdateTest extends InfostoreAJAXTest {
 		assertEquals("test knowledge description", object.getString("description"));
 		assertEquals(0, object.getInt("version"));
 		
+	}
+	
+	public void testConflict() throws Exception{
+		Response res = this.get(sessionId,clean.get(0));
+		Response res2 = this.update(sessionId, clean.get(0),res.getTimestamp().getTime()-2000,m(
+					"title" , "test knowledge updated"
+			));
+		assertNotNull(res2.getErrorMessage());
+		assertFalse("".equals(res2.getErrorMessage()));
+		
+	}
+	
+	public void notestUpload() throws Exception{
+		File upload = new File(Init.getTestProperty("ajaxPropertiesFile"));
+		
+		int id = clean.get(0);
+		
+		Response res = update(sessionId,id,System.currentTimeMillis(),m(),upload,"text/plain");
+		assertNoError(res);
+		
+		res = get(sessionId,id);
+		JSONObject obj = (JSONObject) res.getData();
+		
+		assertEquals(1,obj.getInt("version"));
+		
+		assertEquals("text/plain",obj.getString("file_mimetype"));
+		assertEquals(upload.getName(),obj.getString("filename"));
+
+		InputStream is = null;
+		InputStream is2 = null;
+		try {
+			is = new FileInputStream(upload);
+			is2 = document(sessionId,id,1);
+			assertSameContent(is,is2);
+		} finally {
+			if(is!=null)
+				is.close();
+			if(is2!=null)
+				is2.close();
+		}
+	}
+	
+	public void testSwitchVersion() throws Exception{
+		File upload = new File(Init.getTestProperty("ajaxPropertiesFile"));
+		
+		int id = clean.get(0);
+		
+		Response res = update(sessionId,id,System.currentTimeMillis(),m(),upload,"text/plain");
+		assertNoError(res);
+		
+		res = update(sessionId,id,System.currentTimeMillis(),m(),upload,"text/plain");
+		assertNoError(res);
+		
+		res = update(sessionId,id,System.currentTimeMillis(),m(),upload,"text/plain");
+		assertNoError(res);
+		
+		update(sessionId,id,System.currentTimeMillis(),m("version" , "2"));
+		
+		res = get(sessionId,id);
+		JSONObject obj = (JSONObject) res.getData();
+		assertEquals(2,obj.get("version"));
 	}
 
 }
