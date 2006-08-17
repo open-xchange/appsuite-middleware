67ba871425ba65bb9052d2733de2feb414dac61e
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-08-17 10:13:54 +0000

    add test to request a list of groups

--- openexchange-test/src/com/openexchange/ajax/GroupTest.java
+++ openexchange-test/src/com/openexchange/ajax/GroupTest.java
@@ -22,6 +22,19 @@ public class GroupTest extends AbstractAJAXTest {
 		assertTrue("group array size > 0", groups.length > 0);
 	}
 
+	public void testList() throws Exception {
+		com.openexchange.groupware.ldap.Group groups[] = searchGroup(getWebConversation(), "*", PROTOCOL + getHostName(), getSessionId());
+		assertTrue("group array size > 0", groups.length > 0);
+		
+		int[] id = new int[groups.length];
+		for (int a = 0; a < id.length; a++) {
+			id[a] = groups[a].getIdentifier();
+		}
+		
+		groups = listGroup(getWebConversation(), id, PROTOCOL + getHostName(), getSessionId());
+		assertTrue("group array size > 0", groups.length > 0);
+	}
+	
 	public void testSearchGroupUsers() throws Exception {
 		com.openexchange.groupware.ldap.Group groups[] = searchGroup(getWebConversation(), "users", PROTOCOL + getHostName(), getSessionId());
 		assertTrue("group array size > 0", groups.length > 0);
@@ -42,6 +55,44 @@ public class GroupTest extends AbstractAJAXTest {
 		jsonObj.put("pattern", searchpattern);
 		
 		ByteArrayInputStream bais = new ByteArrayInputStream(jsonObj.toString().getBytes());
+		WebRequest req = new PutMethodWebRequest(host + GROUP_URL + parameter.getURLParameters(), bais, "text/javascript");
+		WebResponse resp = webCon.getResponse(req);
+		
+		assertEquals(200, resp.getResponseCode());
+		
+		final Response response = Response.parse(resp.getText());
+		
+		if (response.hasError()) {
+			fail("json error: " + response.getErrorMessage());
+		}
+		
+		assertNotNull("timestamp", response.getTimestamp());
+		
+		JSONArray jsonArray = (JSONArray)response.getData();
+		com.openexchange.groupware.ldap.Group[] g = new com.openexchange.groupware.ldap.Group[jsonArray.length()];
+		for (int a = 0; a < g.length; a++) {
+			JSONObject jObj = jsonArray.getJSONObject(a);
+			g[a] = new com.openexchange.groupware.ldap.Group();
+			g[a].setIdentifier(jObj.getInt(ParticipantsFields.ID));
+			if (jObj.has(ParticipantsFields.DISPLAY_NAME)) {
+				g[a].setDisplayName(jObj.getString(ParticipantsFields.DISPLAY_NAME));
+			} 
+		}
+		
+		return g;
+	}
+	
+	public static com.openexchange.groupware.ldap.Group[] listGroup(WebConversation webCon, int[] id, String host, String session) throws Exception {
+        final URLParameter parameter = new URLParameter();
+		parameter.setParameter(AJAXServlet.PARAMETER_SESSION, session);
+		parameter.setParameter(AJAXServlet.PARAMETER_ACTION, AJAXServlet.ACTION_LIST);
+		
+		JSONArray requestArray = new JSONArray();
+		for (int a = 0; a < id.length; a++) {
+			requestArray.put(id[a]);
+		}
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(requestArray.toString().getBytes());
 		WebRequest req = new PutMethodWebRequest(host + GROUP_URL + parameter.getURLParameters(), bais, "text/javascript");
 		WebResponse resp = webCon.getResponse(req);
 		
