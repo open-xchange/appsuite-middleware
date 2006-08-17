5020f4ec0a925c3fc101ffe332245febd32325e3
Francisco Laguna <francisco.laguna@open-xchange.com>
2006-08-17 17:13:10 +0000

    added meaningful versions test

--- openexchange-test/src/com/openexchange/ajax/infostore/VersionsTest.java
+++ openexchange-test/src/com/openexchange/ajax/infostore/VersionsTest.java
@@ -1,6 +1,17 @@
 package com.openexchange.ajax.infostore;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.json.JSONArray;
+
 import com.openexchange.ajax.InfostoreAJAXTest;
+import com.openexchange.ajax.container.Response;
+import com.openexchange.groupware.Init;
+import com.openexchange.groupware.infostore.utils.Metadata;
 
 
 public class VersionsTest extends InfostoreAJAXTest {
@@ -10,7 +21,25 @@ public class VersionsTest extends InfostoreAJAXTest {
 		super();
 	}
 	
-	public void testNothingInParticular(){
-		assertTrue(true); //FIXME
+	public void testVersions() throws Exception{
+		File upload = new File(Init.getTestProperty("ajaxPropertiesFile"));
+		Response res = update(sessionId,clean.get(0),System.currentTimeMillis(),m(),upload,"text/plain");
+		assertNoError(res);
+		res = update(sessionId,clean.get(0),System.currentTimeMillis(),m(),upload,"text/plain");
+		assertNoError(res);
+		res = update(sessionId,clean.get(0),System.currentTimeMillis(),m(),upload,"text/plain");
+		assertNoError(res);
+		
+		res = versions(sessionId,clean.get(0), new int[]{Metadata.VERSION});
+		assertNoError(res);
+
+		Set<Integer> versions = new HashSet<Integer>(Arrays.asList(new Integer[]{0,1,2,3}));
+		JSONArray arrayOfarrays = (JSONArray) res.getData();
+		
+		for(int i = 0; i < arrayOfarrays.length(); i++) {
+			JSONArray comp = arrayOfarrays.getJSONArray(i);
+			assertTrue(versions.remove(comp.getInt(0)));
+		}
+		assertTrue(versions.isEmpty());
 	}
 }
