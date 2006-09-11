0e58d4b2db449887e74b8afa7b13e98a648c3b59
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-09-11 13:02:12 +0000

    change folder test to check multiple permissions

--- openexchange-test/src/com/openexchange/webdav/xml/FolderTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/FolderTest.java
@@ -143,7 +143,7 @@ public class FolderTest extends AbstractWebdavTest {
 		
 		OCLPermission oclp[] = new OCLPermission[3];
 		oclp[0] = createPermission( userId, false, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
-		oclp[1] = createPermission( userParticipantId, false, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
+		oclp[1] = createPermission( userParticipantId, false, OCLPermission.CREATE_OBJECTS_IN_FOLDER, OCLPermission.READ_OWN_OBJECTS, OCLPermission.WRITE_ALL_OBJECTS, OCLPermission.NO_PERMISSIONS);
 		oclp[2] = createPermission( groupParticipantId, true, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
 		
 		folderObj.setPermissionsAsArray( oclp );
