0b04233668b5fb16309397564a08aae09865fed1
Martin Kauss <martin.kauss@open-xchange.com>
2006-09-21 09:03:29 +0000

    speed up test time

--- openexchange-test/src/com/openexchange/server/PoolRunner.java
+++ openexchange-test/src/com/openexchange/server/PoolRunner.java
@@ -24,7 +24,7 @@ public class PoolRunner implements Runnable {
     private int current_run = 0;
     
     public static final int TEST_RUNS = 20;
-    public static final int WAIT_TIME = 200;
+    public static final int WAIT_TIME = 20;
     public static final String TEST_QUERY = "SELECT 1 as test";
     
     private boolean isrunning = true;
