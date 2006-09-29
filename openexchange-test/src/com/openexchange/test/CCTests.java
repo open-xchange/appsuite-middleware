bbc08f1b76a4b9c13055be81c7972858f22520b0
Francisco Laguna <francisco.laguna@open-xchange.com>
2006-09-29 09:28:40 +0000

    Run all tests

---
+++ openexchange-test/src/com/openexchange/test/CCTests.java
@@ -0,0 +1,56 @@
+package com.openexchange.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class CCTests {
+	public static Test suite(){
+			
+		TestSuite tests = new TestSuite();
+		
+		tests.addTestSuite(com.openexchange.ajax.LoginTest.class);
+		tests.addTestSuite(com.openexchange.ajax.FolderTest.class);
+		tests.addTestSuite(com.openexchange.ajax.TasksTest.class);
+		tests.addTestSuite(com.openexchange.ajax.ReminderTest.class);
+		tests.addTest(com.openexchange.ajax.infostore.InfostoreAJAXSuite.suite());
+		tests.addTestSuite(com.openexchange.ajax.ConfigMenuTest.class);
+		tests.addTestSuite(com.openexchange.ajax.ContactTest.class);
+		tests.addTestSuite(com.openexchange.ajax.AppointmentTest.class);
+		tests.addTestSuite(com.openexchange.ajax.GroupTest.class);
+		tests.addTestSuite(com.openexchange.ajax.ResourceTest.class);
+		tests.addTestSuite(com.openexchange.ajax.LinkTest.class);
+		tests.addTestSuite(com.openexchange.ajax.MultipleTest.class);
+
+		tests.addTestSuite(com.openexchange.ajax.attach.SimpleAttachmentTest.class);
+		tests.addTestSuite(com.openexchange.ajax.attach.TaskAttachmentTest.class);
+
+
+		tests.addTestSuite(com.openexchange.webdav.xml.AppointmentTest.class);
+		tests.addTestSuite(com.openexchange.webdav.xml.GroupUserTest.class);
+		tests.addTestSuite(com.openexchange.webdav.xml.ContactTest.class);	
+		tests.addTestSuite(com.openexchange.webdav.xml.FolderTest.class);
+		
+
+		tests.addTestSuite(com.openexchange.ajax.infostore.InfostoreParserTest.class);
+		tests.addTestSuite(com.openexchange.ajax.infostore.InfostoreWriterTest.class);
+		tests.addTestSuite(com.openexchange.ajax.infostore.JSONSimpleRequestTest.class);
+		tests.addTestSuite(com.openexchange.ajax.attach.AttachmentParserTest.class);
+		tests.addTestSuite(com.openexchange.ajax.attach.AttachmentWriterTest.class);
+		tests.addTest(com.openexchange.webdav.protocol.ProtocolTestSuite.suite());
+		tests.addTest(com.openexchange.webdav.action.ActionTestSuite.suite());
+		tests.addTest(com.openexchange.webdav.xml.writer.WriterSuite.suite());
+		
+						
+		tests.addTestSuite(com.openexchange.groupware.IDGeneratorTest.class);
+		tests.addTestSuite(com.openexchange.sessiond.SessiondTest.class);
+		tests.addTestSuite(com.openexchange.groupware.CalendarTest.class);
+	    tests.addTestSuite(com.openexchange.tools.file.FileStorageTest.class);
+		tests.addTestSuite(com.openexchange.tools.file.QuotaFileStorageTest.class);
+		tests.addTestSuite(com.openexchange.groupware.attach.AttachmentBaseTest.class);
+		tests.addTestSuite(com.openexchange.i18n.CompiledLineParserTemplateTest.class);
+		tests.addTestSuite(com.openexchange.groupware.notify.ParticipantNotifyTest.class);
+	
+		
+		return tests;
+	}
+}
