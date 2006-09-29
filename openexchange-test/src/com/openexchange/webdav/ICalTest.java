d69c9e3255e5268fb8d2816d9184dad0dd985ddc
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-09-29 13:15:07 +0000

    add ical test

---
+++ openexchange-test/src/com/openexchange/webdav/ICalTest.java
@@ -0,0 +1,87 @@
+package com.openexchange.webdav;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.PutMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.openexchange.groupware.container.AppointmentObject;
+import com.openexchange.groupware.tasks.Task;
+import com.openexchange.tools.versit.Versit;
+import com.openexchange.tools.versit.VersitDefinition;
+import com.openexchange.tools.versit.VersitObject;
+import com.openexchange.tools.versit.converter.OXContainerConverter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class ICalTest extends AbstractWebdavTest {
+	
+	protected Date startTime = null;
+	
+	protected Date endTime = null;
+	
+	private static final String ICAL_URL = "/servlet/webdav.ical";
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testUpload() throws Exception {
+		WebRequest initRequest = new GetMethodWebRequest(PROTOCOL + hostName + ICAL_URL);
+		initRequest.setHeaderField(AUTHORIZATION, "Basic " + getAuthData(login, password));
+		WebResponse initresponse = webCon.getResponse(initRequest);
+		
+		assertEquals(200, initresponse.getResponseCode());
+		
+		AppointmentObject appointmentObj = new AppointmentObject();
+		appointmentObj.setTitle("testUpload");
+		appointmentObj.setStartDate(new Date());
+		appointmentObj.setEndDate(new Date());
+		
+		Task taskObj = new Task();
+		taskObj.setTitle("testUpload");
+		taskObj.setStartDate(new Date());
+		taskObj.setEndDate(new Date());
+		
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		
+		final VersitDefinition def = Versit.getDefinition("text/calendar");
+		final VersitDefinition.Writer w = def.getWriter(byteArrayOutputStream, "UTF-8");
+		final VersitObject ical = OXContainerConverter.newCalendar("2.0");
+		def.writeProperties(w, ical);
+		final VersitDefinition eventDef = def.getChildDef("VEVENT");
+		final VersitDefinition taskDef = def.getChildDef("VTODO");
+		
+		OXContainerConverter oxContainerConverter = new OXContainerConverter(TimeZone.getDefault(), "t@t.de");
+		
+		VersitObject versitAppointment = oxContainerConverter.convertAppointment(appointmentObj);
+							
+		eventDef.write(w, versitAppointment);
+		
+		VersitObject versitTask = oxContainerConverter.convertTask(taskObj);
+							
+		eventDef.write(w, versitTask);
+		
+		def.writeEnd(w, ical);
+		
+		w.flush();
+		byteArrayOutputStream.flush();
+		
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+		WebRequest req = new PutMethodWebRequest(PROTOCOL + hostName + ICAL_URL, byteArrayInputStream, "text/calendar");
+		req.setHeaderField(AUTHORIZATION, "Basic " + getAuthData(login, password));
+		WebResponse resp = webCon.getResponse(req);
+		
+		assertEquals(200, resp.getResponseCode());
+	}
+	
+	public void testDownload() throws Exception {
+		WebRequest req = new GetMethodWebRequest(PROTOCOL + hostName + ICAL_URL);
+		req.setHeaderField(AUTHORIZATION, "Basic " + getAuthData(login, password));
+		WebResponse resp = webCon.getResponse(req);
+		
+		assertEquals(200, resp.getResponseCode());
+	}	
+}
+
