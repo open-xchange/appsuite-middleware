6a183d15bb26517994325a661e799264a2a47e71
Marcus Klein <marcus.klein@open-xchange.com>
2006-10-04 15:33:00 +0000

    Implemented private flag for nearly all selects.

--- openexchange-test/src/com/openexchange/ajax/TasksTest.java
+++ openexchange-test/src/com/openexchange/ajax/TasksTest.java
@@ -173,6 +173,8 @@ public class TasksTest extends AbstractAJAXTest {
         final Response response = getTask(getWebConversation(), getHostName(),
             getSessionId(), folderId, taskId);
         assertFalse(response.getErrorMessage(), response.hasError());
+        final Task reload = (Task) response.getData();
+        compareAttributes(task, reload);
 
         deleteTask(getWebConversation(), getHostName(), getSessionId(),
             lastModified, folderId, taskId);
@@ -980,5 +982,51 @@ public class TasksTest extends AbstractAJAXTest {
         final int retval = myTasks.getCreatedBy();
         LOG.trace("My identifier is " + retval);
         return retval;
+    }
+
+    public void compareAttributes(final Task task, final Task reload) {
+        assertEquals("Title differs", task.containsTitle(),
+            reload.containsTitle());
+        assertEquals("Title differs", task.getTitle(), reload.getTitle());
+        assertEquals("Private Flag differs", task.containsPrivateFlag(),
+            reload.containsPrivateFlag());
+        /* Not implemented in parser
+        assertEquals("Creation date differs", task.containsCreationDate(),
+            reload.containsCreationDate());
+        assertEquals("Creation date differs", task.getCreationDate(),
+            reload.getCreationDate());
+        assertEquals("Last modified differs", task.containsLastModified(),
+            reload.containsLastModified());
+        assertEquals("Last modified differs", task.getLastModified(),
+            reload.getLastModified());
+        */
+        assertEquals("Start date differs", task.containsStartDate(),
+            reload.containsStartDate());
+        assertEquals("Start date differs", task.getStartDate(),
+            reload.getStartDate());
+        assertEquals("End date differs", task.containsEndDate(),
+            reload.containsEndDate());
+        assertEquals("End date differs", task.getEndDate(),
+            reload.getEndDate());
+        /*
+        assertEquals("After complete differs", task.containsAfterComplete(),
+            reload.containsAfterComplete());
+        assertEquals("After complete differs", task.getAfterComplete(),
+            reload.getAfterComplete());
+        */
+        /*
+        task.setNote("Description");
+        task.setStatus(Task.NOT_STARTED); //FIXME!
+        task.setPriority(Task.NORMAL);
+        task.setCategories("Categories");
+        task.setTargetDuration(1440);
+        task.setActualDuration(1440);
+        task.setTargetCosts(1.0f);
+        task.setActualCosts(1.0f);
+        task.setCurrency("\u20ac");
+        task.setTripMeter("trip meter");
+        task.setBillingInformation("billing information");
+        task.setCompanies("companies");
+        */
     }
 }
