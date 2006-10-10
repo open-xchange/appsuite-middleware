12882bd618266fe1f43d8f3e50bb143eefba8fcd
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-10 15:44:55 +0000

    add AppointmentAJAXSuite for tests

---
+++ openexchange-test/src/com/openexchange/ajax/appointment/AppointmentAJAXSuite.java
@@ -0,0 +1,26 @@
+package com.openexchange.ajax.appointment;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AppointmentAJAXSuite extends TestSuite{
+	
+	public static Test suite(){
+		TestSuite tests = new TestSuite();
+		tests.addTestSuite( AllTest.class );
+		tests.addTestSuite( ConfirmTest.class );
+		tests.addTestSuite( CopyTest.class );
+		tests.addTestSuite( DeleteTest.class );
+		tests.addTestSuite( FreeBusyTest.class );
+		tests.addTestSuite( HasTest.class );
+		tests.addTestSuite( ListTest.class );
+		tests.addTestSuite( MoveTest.class );
+		tests.addTestSuite( NewTest.class );
+		tests.addTestSuite( SearchTest.class );
+		tests.addTestSuite( UpdateTest.class );
+		tests.addTestSuite( UpdatesTest.class );
+		
+		return tests;
+	}
+}
