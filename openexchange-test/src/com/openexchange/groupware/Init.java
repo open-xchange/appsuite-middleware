c8d3ce6324425479701dbb496226426ae05f7b85
henning <henning>
2006-10-11 12:54:28 +0000

    Initialisierung der ajaxProperties veraendert: Wenn in den Testproperties ein ajaxPropertiesDir angegeben wurde, wird zufaellig eine properties-Datei aus diesem Verzeichnis gewaehlt.

--- openexchange-test/src/com/openexchange/groupware/Init.java
+++ openexchange-test/src/com/openexchange/groupware/Init.java
@@ -1,8 +1,13 @@
 package com.openexchange.groupware;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import org.apache.commons.logging.Log;
 
 import com.openexchange.server.ComfireConfig;
 import com.openexchange.server.DBPool;
@@ -13,120 +18,187 @@ import com.openexchange.tools.conf.GlobalConfig;
 
 /**
  * This class contains methods for initialising tests.
+ * 
  * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
  */
 public final class Init {
-	
+
 	private static boolean testPropertiesLoaded = false;
-	
+
 	private static boolean ajaxPropertiesLoaded = false;
 
 	private static boolean webdavPropertiesLoaded = false;
 
 	private static boolean infostorePropertiesLoaded = false;
-	
+
 	private static boolean systemPropertiesLoaded = false;
-	
+
 	private static boolean serverConfLoaded = false;
-	
+
 	private static boolean dbInitialized = false;
-	
+
 	private static boolean sessiondInit = false;
-	
+
 	private static Properties testProps = null;
-	
+
 	private static Properties ajaxProps = null;
-	
+
 	private static Properties webdavProps = null;
-	
+
 	private static Properties infostoreProps = null;
-	
+
+	private static boolean isAjaxDirInitialized = false;
+
+	private static String[] ajaxPropFiles;
+
 	public static void loadTestProperties() {
 		if (!testPropertiesLoaded) {
 			testProps = new Properties();
 			try {
-                final String propfile = System.getProperty("test.propfile");
-                if (null == propfile) {
-                    throw new RuntimeException("Test properties file "
-                        + "test.propfile is not defined as a JVM "
-                        + "system property.");
-                }
+				final String propfile = System.getProperty("test.propfile");
+				if (null == propfile) {
+					throw new RuntimeException("Test properties file "
+							+ "test.propfile is not defined as a JVM "
+							+ "system property.");
+				}
 				testProps.load(new FileInputStream(propfile));
-			} catch (IOException e) {
+			}
+			catch (IOException e) {
 				throw new RuntimeException(e);
 			}
 			testPropertiesLoaded = true;
 		}
 	}
-	
+
 	private static void loadAJAXProperties() {
 		loadTestProperties();
-		ajaxProps  = new Properties();
+		ajaxProps = new Properties();
 		try {
-			ajaxProps.load(new FileInputStream(testProps.getProperty("ajaxPropertiesFile")));
-		} catch (IOException e) {
+			ajaxProps.load(new FileInputStream(getFileName()));
+		}
+		catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 		ajaxPropertiesLoaded = true;
 	}
-	
+
+	private static String getFileName() {
+
+		String retval = null;
+		if (testProps.getProperty("ajaxPropertiesDir") != null) {
+			StringBuilder filename = new StringBuilder();
+			if (!isAjaxDirInitialized) {
+				synchronized (Init.class) {
+					if (!isAjaxDirInitialized) {
+						initAjaxProperties();
+					}
+				}
+			}
+
+			retval = ajaxPropFiles[(int) Math.random() * ajaxPropFiles.length];
+		}
+		else {
+
+			retval = testProps.getProperty("webdavPropertiesFile");
+		}
+
+		return retval;
+	}
+
+	private static void initAjaxProperties() {
+		// Welches Verzeichnis soll gelesen werden?
+		String ajaxPropertiesDir = testProps.getProperty("ajaxPropertiesDir");
+		// ist der Pfad mit abschliessendem "/" ? Wenn nicht, packe den dazu:
+		if (!ajaxPropertiesDir.endsWith(System.getProperty("file.separator"))) {
+			ajaxPropertiesDir = new StringBuilder().append(ajaxPropertiesDir)
+					.append(System.getProperty("file.separator")).toString();
+		}
+		File dir = new File(ajaxPropertiesDir);
+		File myFile;
+		// Lese das Verzeichnis und packe alle Files in das Array ajaxPropFiles
+		// Aber nur dann, wenn es ein File ist (kein Directory) und es auch
+		// gelesen werden kann.
+		if (dir.isDirectory()) {
+			// Hilfsliste:
+			List<String> fileList = new ArrayList<String>();
+			// Pruefe jeden im Verzeichnis vorhandenen Namen:
+			for (String fileName : dir.list()) {
+				myFile = new File(new StringBuilder().append(ajaxPropertiesDir)
+						.append(fileName).toString());
+				if (!myFile.isDirectory() && myFile.canRead()) {
+					fileList.add(myFile.getAbsolutePath());
+				}
+			}
+			// Umladen:
+			ajaxPropFiles = new String[fileList.size()];
+			System.arraycopy(fileList.toArray(), 0, ajaxPropFiles, 0, fileList
+					.size());
+		}
+
+		isAjaxDirInitialized = true;
+	}
+
 	public static Properties getAJAXProperties() {
 		if (!ajaxPropertiesLoaded) {
 			loadAJAXProperties();
 		}
 		return ajaxProps;
 	}
-	
+
 	private static void loadWebdavProperties() {
 		loadTestProperties();
-		webdavProps  = new Properties();
+		webdavProps = new Properties();
 		try {
-			webdavProps.load(new FileInputStream(testProps.getProperty("webdavPropertiesFile")));
-		} catch (IOException e) {
+			webdavProps.load(new FileInputStream(testProps
+					.getProperty("webdavPropertiesFile")));
+		}
+		catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 		webdavPropertiesLoaded = true;
 	}
-	
+
 	public static Properties getWebdavProperties() {
 		if (!webdavPropertiesLoaded) {
 			loadWebdavProperties();
 		}
 		return webdavProps;
 	}
-	
+
 	public static String getAJAXProperty(final String key) {
-        return getAJAXProperties().getProperty(key);
-    }
-	
+		return getAJAXProperties().getProperty(key);
+	}
+
 	public static Properties getTestProperties() {
 		if (!testPropertiesLoaded) {
 			loadTestProperties();
 		}
-		return testProps;		
+		return testProps;
 	}
-	
+
 	public static String getTestProperty(final String key) {
 		return getTestProperties().getProperty(key);
 	}
-	
+
 	public static void loadSystemProperties() {
 		if (!systemPropertiesLoaded) {
 			loadTestProperties();
-			GlobalConfig.loadConf(testProps.getProperty("openexchange.propfile"));
+			GlobalConfig.loadConf(testProps
+					.getProperty("openexchange.propfile"));
 			systemPropertiesLoaded = true;
 		}
 	}
-	
+
 	public static void loadServerConf() {
 		if (!serverConfLoaded) {
 			loadSystemProperties();
 			ComfireConfig cf = new ComfireConfig();
-			cf.loadServerConf((String) ComfireConfig.properties.get("SERVERCONF"));
+			cf.loadServerConf((String) ComfireConfig.properties
+					.get("SERVERCONF"));
 			serverConfLoaded = true;
 		}
 	}
-	
+
 	public static void initDB() {
 		if (!dbInitialized) {
 			loadServerConf();
@@ -134,27 +206,30 @@ public final class Init {
 			dbInitialized = true;
 		}
 	}
-	
+
 	public static void initSessiond() throws Exception {
 		if (!sessiondInit) {
-			String propfile = ComfireConfig.properties.getProperty("SESSIONDPROPERTIES");
-			
+			String propfile = ComfireConfig.properties
+					.getProperty("SESSIONDPROPERTIES");
+
 			SessiondConfigWrapper config = new SessiondConfigWrapper(propfile);
 			SessiondConnector.setConfig(config);
 			new Sessiond(config);
-			
+
 			sessiondInit = true;
 		}
 	}
-	
+
 	private static void loadInfostoreProperties() {
-		if(infostorePropertiesLoaded)
+		if (infostorePropertiesLoaded)
 			return;
 		loadTestProperties();
-		infostoreProps  = new Properties();
+		infostoreProps = new Properties();
 		try {
-			infostoreProps.load(new FileInputStream(testProps.getProperty("infostorePropertiesFile")));
-		} catch (IOException e) {
+			infostoreProps.load(new FileInputStream(testProps
+					.getProperty("infostorePropertiesFile")));
+		}
+		catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 		infostorePropertiesLoaded = true;
