c85ac29be9e40f822873e858c1c8d6f1b7c9be74
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-11 10:02:57 +0000

    add GetTest for appointments

---
+++ openexchange-test/src/com/openexchange/ajax/appointment/GetTest.java
@@ -0,0 +1,176 @@
+package com.openexchange.ajax.appointment;
+
+import com.openexchange.ajax.AppointmentTest;
+import com.openexchange.ajax.ContactTest;
+import com.openexchange.ajax.GroupTest;
+import com.openexchange.ajax.ResourceTest;
+import com.openexchange.groupware.container.AppointmentObject;
+import com.openexchange.groupware.container.ContactObject;
+import com.openexchange.groupware.container.FolderObject;
+import com.openexchange.groupware.container.GroupParticipant;
+import com.openexchange.groupware.container.ResourceParticipant;
+import com.openexchange.groupware.container.UserParticipant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class GetTest extends AppointmentTest {
+	
+	private static final Log LOG = LogFactory.getLog(AllTest.class);
+	
+	public GetTest(String name) {
+		super(name);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testGet() throws Exception {
+		AppointmentObject appointmentObj = createAppointmentObject("testGet");
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		appointmentObj.setObjectID(objectId);
+		compareObject(appointmentObj, loadAppointment, appointmentObj.getStartDate().getTime(), appointmentObj.getEndDate().getTime());
+		
+		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
+	}
+	
+	public void testGetWithParticipants() throws Exception {
+		AppointmentObject appointmentObj = createAppointmentObject("testGetWithParticipants");
+		
+		int userParticipantId = ContactTest.searchContact(getWebConversation(), userParticipant3, FolderObject.SYSTEM_LDAP_FOLDER_ID, new int[] { ContactObject.INTERNAL_USERID }, PROTOCOL + getHostName(), getSessionId())[0].getInternalUserId();
+		int groupParticipantId = GroupTest.searchGroup(getWebConversation(), groupParticipant, PROTOCOL + getHostName(), getSessionId())[0].getIdentifier();
+		int resourceParticipantId = ResourceTest.searchResource(getWebConversation(), resourceParticipant, PROTOCOL + getHostName(), getSessionId())[0].getIdentifier();
+		
+		com.openexchange.groupware.container.Participant[] participants = new com.openexchange.groupware.container.Participant[4];
+		participants[0] = new UserParticipant();
+		participants[0].setIdentifier(userId);
+		participants[1] = new UserParticipant();
+		participants[1].setIdentifier(userParticipantId);
+		participants[2] = new GroupParticipant();
+		participants[2].setIdentifier(groupParticipantId);
+		participants[3] = new ResourceParticipant();
+		participants[3].setIdentifier(resourceParticipantId);
+		
+		appointmentObj.setParticipants(participants);
+		
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, timeZone, PROTOCOL + getHostName(), getSessionId());
+		appointmentObj.setObjectID(objectId);
+		compareObject(appointmentObj, loadAppointment, appointmentObj.getStartDate().getTime(), appointmentObj.getEndDate().getTime());
+		
+		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
+	}
+	
+	public void testGetWithAllFields() throws Exception {
+		AppointmentObject appointmentObj = new AppointmentObject();
+		appointmentObj.setTitle("testGetWithAllFields");
+		appointmentObj.setStartDate(new Date(startTime));
+		appointmentObj.setEndDate(new Date(endTime));
+		appointmentObj.setLocation("Location");
+		appointmentObj.setShownAs(AppointmentObject.FREE);
+		appointmentObj.setParentFolderID(appointmentFolderId);
+		appointmentObj.setPrivateFlag(true);
+		appointmentObj.setFullTime(true);
+		appointmentObj.setLabel(2);
+		appointmentObj.setNote("note");
+		appointmentObj.setCategories("testcat1,testcat2,testcat3");
+		
+		int userParticipantId = ContactTest.searchContact(getWebConversation(), userParticipant3, FolderObject.SYSTEM_LDAP_FOLDER_ID, new int[] { ContactObject.INTERNAL_USERID }, PROTOCOL + getHostName(), getSessionId())[0].getInternalUserId();
+		int groupParticipantId = GroupTest.searchGroup(getWebConversation(), groupParticipant, PROTOCOL + getHostName(), getSessionId())[0].getIdentifier();
+		int resourceParticipantId = ResourceTest.searchResource(getWebConversation(), resourceParticipant, PROTOCOL + getHostName(), getSessionId())[0].getIdentifier();
+		
+		com.openexchange.groupware.container.Participant[] participants = new com.openexchange.groupware.container.Participant[4];
+		participants[0] = new UserParticipant();
+		participants[0].setIdentifier(userId);
+		participants[1] = new UserParticipant();
+		participants[1].setIdentifier(userParticipantId);
+		participants[2] = new GroupParticipant();
+		participants[2].setIdentifier(groupParticipantId);
+		participants[3] = new ResourceParticipant();
+		participants[3].setIdentifier(resourceParticipantId);
+		
+		appointmentObj.setParticipants(participants);
+		
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		Calendar c = Calendar.getInstance();
+		c.setTimeZone(TimeZone.getTimeZone("UTC"));
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+		
+		long newStartTime = c.getTimeInMillis();
+		long newEndTime = newStartTime + dayInMillis;
+		
+		appointmentObj.setObjectID(objectId);
+		compareObject(appointmentObj, loadAppointment, newStartTime, newEndTime);
+		
+		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
+	}
+	
+	public void testGetWithAllFieldsOnUpdate() throws Exception {
+		AppointmentObject appointmentObj = new AppointmentObject();
+		appointmentObj.setTitle("testGetWithAllFieldsOnUpdate");
+		appointmentObj.setStartDate(new Date(startTime));
+		appointmentObj.setEndDate(new Date(endTime));
+		appointmentObj.setParentFolderID(appointmentFolderId);
+		
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		appointmentObj.setLocation("Location");
+		appointmentObj.setShownAs(AppointmentObject.FREE);
+		appointmentObj.setPrivateFlag(true);
+		appointmentObj.setFullTime(true);
+		appointmentObj.setLabel(2);
+		appointmentObj.setNote("note");
+		appointmentObj.setCategories("testcat1,testcat2,testcat3");
+		
+		int userParticipantId = ContactTest.searchContact(getWebConversation(), userParticipant3, FolderObject.SYSTEM_LDAP_FOLDER_ID, new int[] { ContactObject.INTERNAL_USERID }, PROTOCOL + getHostName(), getSessionId())[0].getInternalUserId();
+		int groupParticipantId = GroupTest.searchGroup(getWebConversation(), groupParticipant, PROTOCOL + getHostName(), getSessionId())[0].getIdentifier();
+		int resourceParticipantId = ResourceTest.searchResource(getWebConversation(), resourceParticipant, PROTOCOL + getHostName(), getSessionId())[0].getIdentifier();
+		
+		com.openexchange.groupware.container.Participant[] participants = new com.openexchange.groupware.container.Participant[4];
+		participants[0] = new UserParticipant();
+		participants[0].setIdentifier(userId);
+		participants[1] = new UserParticipant();
+		participants[1].setIdentifier(userParticipantId);
+		participants[2] = new GroupParticipant();
+		participants[2].setIdentifier(groupParticipantId);
+		participants[3] = new ResourceParticipant();
+		participants[3].setIdentifier(resourceParticipantId);
+		
+		appointmentObj.setParticipants(participants);
+		
+		appointmentObj.removeParentFolderID();
+		
+		updateAppointment(getWebConversation(), appointmentObj, objectId, appointmentFolderId, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		Calendar c = Calendar.getInstance();
+		c.setTimeZone(TimeZone.getTimeZone("UTC"));
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+		
+		long newStartTime = c.getTimeInMillis();
+		long newEndTime = newStartTime + 86400000;
+		
+		appointmentObj.setObjectID(objectId);
+		appointmentObj.setParentFolderID(appointmentFolderId);
+		compareObject(appointmentObj, loadAppointment, newStartTime, newEndTime);
+		
+		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
+	}
+}
