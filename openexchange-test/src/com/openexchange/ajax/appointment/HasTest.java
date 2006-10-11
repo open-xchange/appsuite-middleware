cbc88731b9563f57040be042990bbd20d25fb8d8
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-11 12:54:13 +0000

    test action has with normal appointment and will full time appointment

--- openexchange-test/src/com/openexchange/ajax/appointment/HasTest.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/HasTest.java
@@ -1,6 +1,7 @@
 package com.openexchange.ajax.appointment;
 
 import com.openexchange.ajax.AppointmentTest;
+import com.openexchange.groupware.container.AppointmentObject;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
@@ -30,9 +31,79 @@ public class HasTest extends AppointmentTest {
         int numberOfDays = 7;
         
         Date start = c.getTime();
-        Date end = new Date(start.getTime() + (24*60*60*1000*numberOfDays));
+        Date end = new Date(start.getTime() + (dayInMillis*numberOfDays));
+		
+		boolean conflict = true;
+		
+		int posInArray = -1;
+		boolean[] hasAppointments = hasAppointments(getWebConversation(), start, end, PROTOCOL + getHostName(), getSessionId());
+		for (int a = 0; a < hasAppointments.length; a++) {
+			if (!hasAppointments[a]) {
+				posInArray = a;
+				conflict = false;
+				break;
+			}
+		}
+		
+		assertEquals("all days have appointments", conflict, false);
+		
+		AppointmentObject appointmentObj = new AppointmentObject();
+		appointmentObj.setTitle("testHasAppointmentFullTime");
+		appointmentObj.setStartDate(new Date(start.getTime()+(dayInMillis*posInArray)+(60*60*1000)));
+		appointmentObj.setEndDate(new Date(start.getTime()+(dayInMillis*posInArray)+(60*60*1000)));
+		appointmentObj.setShownAs(AppointmentObject.ABSENT);
+		appointmentObj.setParentFolderID(appointmentFolderId);
+		
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
         
-        boolean[] hasAppointments = hasAppointments(getWebConversation(), start, end, PROTOCOL + getHostName(), getSessionId());
+        hasAppointments = hasAppointments(getWebConversation(), start, end, PROTOCOL + getHostName(), getSessionId());
         assertEquals("has array length is wrong", numberOfDays, hasAppointments.length);
+		assertEquals("boolean on pos " + posInArray + " is not true", hasAppointments[posInArray], true);
+		
+		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
+    }
+	
+    public void testHasAppointmentFullTime() throws Exception {
+        Calendar c = Calendar.getInstance();
+        c.setTimeZone(TimeZone.getTimeZone("UTC"));
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        
+        int numberOfDays = 7;
+        
+        Date start = c.getTime();
+        Date end = new Date(start.getTime() + (dayInMillis*numberOfDays));
+		
+		boolean conflict = true;
+		
+		int posInArray = -1;
+		boolean[] hasAppointments = hasAppointments(getWebConversation(), start, end, PROTOCOL + getHostName(), getSessionId());
+		for (int a = 0; a < hasAppointments.length; a++) {
+			if (!hasAppointments[a]) {
+				posInArray = a;
+				conflict = false;
+				break;
+			}
+		}
+		
+		assertEquals("all days have appointments", conflict, false);
+		
+		AppointmentObject appointmentObj = new AppointmentObject();
+		appointmentObj.setTitle("testHasAppointmentFullTime");
+		appointmentObj.setStartDate(new Date(start.getTime()+(dayInMillis*posInArray)));
+		appointmentObj.setEndDate(new Date(start.getTime()+(dayInMillis*(posInArray+1))));
+		appointmentObj.setShownAs(AppointmentObject.ABSENT);
+		appointmentObj.setFullTime(true);
+		appointmentObj.setParentFolderID(appointmentFolderId);
+		
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+        
+        hasAppointments = hasAppointments(getWebConversation(), start, end, PROTOCOL + getHostName(), getSessionId());
+        assertEquals("has array length is wrong", numberOfDays, hasAppointments.length);
+		assertEquals("boolean on pos " + posInArray + " is not true", hasAppointments[posInArray], true);
+		
+		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
     }
 }
