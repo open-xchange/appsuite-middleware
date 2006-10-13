7317aa2cce803df2d0aa731f03b28ec1bf6b36cd
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-13 09:44:31 +0000

    add simple updates test

--- openexchange-test/src/com/openexchange/ajax/contact/UpdatesTest.java
+++ openexchange-test/src/com/openexchange/ajax/contact/UpdatesTest.java
@@ -1,19 +1,11 @@
 package com.openexchange.ajax.contact;
 
-import com.openexchange.ajax.AppointmentTest;
 import com.openexchange.ajax.ContactTest;
-import com.openexchange.ajax.GroupTest;
-import com.openexchange.ajax.ResourceTest;
-import com.openexchange.groupware.container.AppointmentObject;
-import com.openexchange.groupware.container.ContactObject;
-import com.openexchange.groupware.container.FolderObject;
-import com.openexchange.groupware.container.GroupParticipant;
-import com.openexchange.groupware.container.ResourceParticipant;
-import com.openexchange.groupware.container.UserParticipant;
+import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class UpdatesTest extends AppointmentTest {
+public class UpdatesTest extends ContactTest {
 
 	private static final Log LOG = LogFactory.getLog(UpdateTest.class);
 	
@@ -26,7 +18,7 @@ public class UpdatesTest extends AppointmentTest {
 	}
 	
 	public void testUpdates() throws Exception {
-		
+		listModifiedAppointment(getWebConversation(), contactFolderId, new Date(0), PROTOCOL + getHostName(), getSessionId());
 	}
 }
 
