1d9777cb0fc9cec8325ad98c3d14e689336b3b89
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-13 12:08:14 +0000

    use new method from GroupUserTest to find user id

--- openexchange-test/src/com/openexchange/webdav/xml/FolderTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/FolderTest.java
@@ -396,8 +396,8 @@ public class FolderTest extends AbstractWebdavXMLTest {
 		
 		FolderObject[] folderArray = listFolder(webCon, new Date(0), true, false, host, login, password);
 		
-		ContactObject[] contactArray = GroupUserTest.searchUser(webCon, login, new Date(0), host, login, password);
-		int userId = contactArray[0].getInternalUserId();
+		final int userId = GroupUserTest.getUserId(webCon, host, login, password);
+		assertTrue("user not found", userId != -1);
 		
 		for (int a = 0; a < folderArray.length; a++) {
 			FolderObject folderObj = folderArray[a];
@@ -432,8 +432,8 @@ public class FolderTest extends AbstractWebdavXMLTest {
 		
 		FolderObject[] folderArray = listFolder(webCon, new Date(0), true, false, host, login, password);
 		
-		ContactObject[] contactArray = GroupUserTest.searchUser(webCon, login, new Date(0), host, login, password);
-		int userId = contactArray[0].getInternalUserId();
+		final int userId = GroupUserTest.getUserId(webCon, host, login, password);
+		assertTrue("user not found", userId != -1);
 		
 		for (int a = 0; a < folderArray.length; a++) {
 			FolderObject folderObj = folderArray[a];
