8a8bd9968fa9a587758b83db41e139f791afd0f1
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-16 14:54:25 +0000

    add simple search method to test search with start letter

--- openexchange-test/src/com/openexchange/ajax/contact/SearchTest.java
+++ openexchange-test/src/com/openexchange/ajax/contact/SearchTest.java
@@ -23,4 +23,19 @@ public class SearchTest extends ContactTest {
 		assertTrue("contact array size is 0", contactArray.length > 0);
 		assertEquals("user id is not equals", userId, contactArray[0].getInternalUserId());
 	}
+	
+	public void testSearchStartCharacter() throws Exception {
+		ContactObject contactObj = new ContactObject();
+		contactObj.setSurName("Meier");
+		contactObj.setParentFolderID(contactFolderId);
+		int objectId1 = insertContact(getWebConversation(), contactObj, PROTOCOL + getHostName(), getSessionId());
+		int objectId2 = insertContact(getWebConversation(), contactObj, PROTOCOL + getHostName(), getSessionId());
+		
+		ContactObject[] contactArray = searchContact(getWebConversation(), "M", contactFolderId, new int[] { ContactObject.INTERNAL_USERID }, true, PROTOCOL + getHostName(), getSessionId());
+		assertTrue("contact array size < 2", contactArray.length >= 2);
+	}
+	
+	public void _notestExtendedSearch() throws Exception {
+		fail("not implemented yet!");
+	}
 }
