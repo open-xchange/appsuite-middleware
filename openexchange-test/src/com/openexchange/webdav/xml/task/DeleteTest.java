eb8cdb935a234582abc7c2d466493e2adf035ea2
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-17 10:38:16 +0000

    add tests to test concurent delete exception

--- openexchange-test/src/com/openexchange/webdav/xml/task/DeleteTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/task/DeleteTest.java
@@ -1,7 +1,10 @@
 package com.openexchange.webdav.xml.task;
 
+import com.openexchange.api2.OXException;
 import com.openexchange.groupware.tasks.Task;
 import com.openexchange.webdav.xml.TaskTest;
+import com.openexchange.webdav.xml.XmlServlet;
+import java.util.Date;
 
 public class DeleteTest extends TaskTest {
 	
@@ -13,6 +16,34 @@ public class DeleteTest extends TaskTest {
 		int[][] objectIdAndFolderId = { { objectId1, taskFolderId }, { objectId2, taskFolderId } };
 		
 		deleteTask(webCon, objectIdAndFolderId, PROTOCOL + hostName, login, password);
+	}
+	
+	public void _notestDeleteConcurentConflict() throws Exception {
+		Task appointmentObj = createTask("testUpdateTaskConcurentConflict");
+		int objectId = insertTask(webCon, appointmentObj, PROTOCOL + hostName, login, password);
+		
+		try {
+			deleteTask(webCon, objectId, taskFolderId, new Date(0), PROTOCOL + hostName, login, password );
+			fail("expected concurent modification exception!");
+		} catch (OXException exc) {
+			assertExceptionMessage(exc.getMessage(), XmlServlet.MODIFICATION_STATUS);
+		}
+		
+		deleteTask(webCon, objectId, taskFolderId, PROTOCOL + hostName, login, password );
+	}
+	
+	public void testDeleteNotFound() throws Exception {
+		Task appointmentObj = createTask("testUpdateTaskNotFound");
+		int objectId = insertTask(webCon, appointmentObj, PROTOCOL + hostName, login, password);
+		
+		try {
+			deleteTask(webCon, (objectId + 1000), taskFolderId, PROTOCOL + hostName, login, password );
+			fail("expected object not found exception!");
+		} catch (OXException exc) {
+			assertExceptionMessage(exc.getMessage(), XmlServlet.OBJECT_NOT_FOUND_STATUS);
+		}
+		
+		deleteTask(webCon, objectId, taskFolderId, PROTOCOL + hostName, login, password );
 	}
 	
 }
