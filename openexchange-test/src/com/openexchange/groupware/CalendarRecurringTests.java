350a39fb610f09c006ebe514e368ac0f2608130e
Martin Kauss <martin.kauss@open-xchange.com>
2006-10-17 09:21:46 +0000

    new test class

---
+++ openexchange-test/src/com/openexchange/groupware/CalendarRecurringTests.java
@@ -0,0 +1,242 @@
+
+package com.openexchange.groupware;
+
+
+import com.openexchange.event.EventConfigImpl;
+import com.openexchange.groupware.calendar.CalendarDataObject;
+import com.openexchange.groupware.calendar.CalendarOperation;
+import com.openexchange.groupware.calendar.CalendarRecurringCollection;
+import com.openexchange.groupware.calendar.CalendarSql;
+import com.openexchange.groupware.calendar.CalendarSqlImp;
+import com.openexchange.groupware.calendar.ConflictHandler;
+import com.openexchange.groupware.calendar.FreeBusyResults;
+import com.openexchange.groupware.configuration.AbstractConfigWrapper;
+import com.openexchange.groupware.container.AppointmentObject;
+import com.openexchange.groupware.container.FolderObject;
+import com.openexchange.groupware.container.Participant;
+import com.openexchange.groupware.container.Participants;
+import com.openexchange.groupware.container.UserParticipant;
+import com.openexchange.groupware.contexts.Context;
+import com.openexchange.groupware.contexts.ContextImpl;
+import com.openexchange.groupware.contexts.ContextStorage;
+import com.openexchange.groupware.ldap.UserStorage;
+import com.openexchange.server.DBPool;
+import com.openexchange.groupware.calendar.RecurringResults;
+import com.openexchange.groupware.calendar.RecurringResult;
+import com.openexchange.groupware.container.CalendarObject;
+import com.openexchange.server.OCLPermission;
+import com.openexchange.sessiond.SessionObject;
+import com.openexchange.sessiond.SessionObjectWrapper;
+import com.openexchange.tools.OXFolderTools;
+import com.openexchange.tools.iterator.SearchIterator;
+import com.openexchange.tools.oxfolder.OXFolderAction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Properties;
+import junit.framework.TestCase;
+
+public class CalendarRecurringTests extends TestCase {
+    
+    private final static int TEST_PASS = 1;
+    private final static int TEST_PASS_HOT_SPOT = 1;    
+    public static final long SUPER_END = 253402210800000L; // 31.12.9999 00:00:00 (GMT)
+    
+    private static int userid = 11; // bishoph
+    public final static int contextid = 1;
+    
+    private static boolean init = false;
+    
+    protected void setUp() throws Exception {        
+        super.setUp();
+        EventConfigImpl event = new EventConfigImpl();
+        event.setEventQueueEnabled(false);
+        userid = getUserId();
+        
+    }
+    
+    protected void tearDown() throws Exception {
+        if (init) {
+            init = false;
+            Init.stopDB();
+        }
+        super.tearDown();
+    }
+    
+    private static Properties getAJAXProperties() {
+        Properties properties = Init.getAJAXProperties();
+        return properties;
+    }        
+    
+    private static int resolveUser(String user) throws Exception {
+        UserStorage uStorage = UserStorage.getInstance(getContext());
+        userid = uStorage.getUserId(user);
+        return userid;
+    }
+    
+    public static int getUserId() throws Exception {
+        if (!init) {
+            Init.initDB();
+            init = true;
+        }
+        String user = AbstractConfigWrapper.parseProperty(getAJAXProperties(), "user_participant2", "");        
+        return resolveUser(user);
+    }
+    
+    public static Context getContext() {
+        return new ContextImpl(contextid);
+    }
+    
+    public static int getPrivateFolder() throws Exception {
+        int privatefolder = 0;
+        Context context = getContext();
+        Connection readcon = DBPool.pickup(context);
+        privatefolder = new Integer(OXFolderTools.getCalendarStandardFolder(userid, context, readcon)).intValue();
+        DBPool.push(context, readcon);
+        return privatefolder;        
+    }
+    
+    
+   public void testBasicRecurring() throws Throwable {
+        CalendarDataObject cdao = new CalendarDataObject();      
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setStartDate(new Date(0));
+        cdao.setEndDate(new Date(0));
+        cdao.setUntil(new Date(0));        
+        cdao.setTitle("Basic Recurring Test");
+        cdao.setRecurrenceID(1);
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setRecurrenceType(CalendarObject.DAILY);        
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setRecurrenceCalculator(1);        
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setInterval(1);
+        assertTrue(cdao.calculateRecurrence());
+    }
+    
+    public void testBasicRecurringWithoutUntil() throws Throwable {
+        CalendarDataObject cdao = new CalendarDataObject();      
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setStartDate(new Date(0));
+        cdao.setEndDate(new Date(0));        
+        cdao.setTitle("Basic Recurring Test");
+        cdao.setRecurrenceID(1);
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setRecurrenceType(CalendarObject.DAILY);        
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setRecurrenceCalculator(1);        
+        assertFalse(cdao.calculateRecurrence());
+        cdao.setInterval(1);
+        assertTrue(cdao.calculateRecurrence());
+    }    
+    
+    
+    public void testDailyRecurring() throws Throwable {
+        long s = 1149768000000L; // 08.06.2006 12:00 (GMT)
+        long e = 1149771600000L; // 08.06.2006 13:00 (GMT)
+        long u = 1150156800000L; // 13.06.2006 00:00 (GMT)
+        long u_test = 1150106400000L; // 12.06.2006 10:00 (GMT)
+        String testrecurrence = "t|1|i|1|s|"+s+"|e|"+u+"|";
+        CalendarDataObject cdao = new CalendarDataObject();
+        cdao.setStartDate(new Date(s));
+        cdao.setEndDate(new Date(e));
+        cdao.setUntil(new Date(u_test));
+        cdao.setTitle("Daily Appointment Test");
+        cdao.setRecurrence(testrecurrence);
+        cdao.setRecurrenceCalculator(1);
+        cdao.setRecurrenceID(1);
+        long pass_one_start = System.currentTimeMillis();
+        for (int a = 0; a < TEST_PASS; a++) {
+            CalendarRecurringCollection.fillDAO(cdao);
+        }
+        long pass_one_end = System.currentTimeMillis();
+        long pass_one_time = pass_one_end - pass_one_start;
+        
+        long pass_two_start = System.currentTimeMillis();
+        for (int a = 0; a < TEST_PASS_HOT_SPOT; a++) {
+            CalendarRecurringCollection.fillDAO(cdao);
+        }
+        long pass_two_end = System.currentTimeMillis();
+        long pass_two_time = pass_two_end - pass_two_start;
+        
+        String check = CalendarRecurringCollection.createDSString(cdao);        
+        assertTrue("Checking daily sequence", check.equals(testrecurrence));
+        RecurringResults m = CalendarRecurringCollection.calculateRecurring(cdao, 0, 0, 0);
+        assertEquals("Check calculation", 6, m.size());
+        m = CalendarRecurringCollection.calculateRecurring(cdao, 0, 0, 1);
+        assertEquals("Check calculation", 1, m.size());
+        
+        double percent = pass_two_time/10;
+        percent = pass_one_time * 100 / percent;
+        
+    }    
+    
+    public void testDailyRecurringWithDAO() throws Throwable {
+        long s = 1149768000000L; // 08.06.2006 12:00 (GMT)
+        long e = 1149771600000L; // 08.06.2006 13:00 (GMT)
+        long u = 1150156800000L; // 13.06.2006 00:00 (GMT)
+        long u_test = 1150106400000L; // 12.06.2006 10:00 (GMT)
+        String testrecurrence = "t|1|i|1|s|"+s+"|e|"+u+"|";
+        CalendarDataObject cdao = new CalendarDataObject();
+        cdao.setStartDate(new Date(s));
+        cdao.setEndDate(new Date(e));
+        cdao.setUntil(new Date(u_test));
+        cdao.setTitle("Daily Appointment Test only with DAO");
+        cdao.setRecurrenceCalculator(1);
+        cdao.setRecurrenceID(1);
+        cdao.setRecurrenceType(CalendarObject.DAILY);
+        cdao.setInterval(1);
+        for (int a = 0; a < TEST_PASS; a++) {
+            CalendarRecurringCollection.fillDAO(cdao);
+        }
+        String check = CalendarRecurringCollection.createDSString(cdao);        
+        assertTrue("Checking daily sequence", check.equals(testrecurrence));
+        RecurringResults m = CalendarRecurringCollection.calculateRecurring(cdao, 0, 0, 0);
+        assertEquals("Check calculation", 6, m.size()); 
+        
+        m = CalendarRecurringCollection.calculateRecurring(cdao, 0, 0, 1);
+        assertEquals("Check calculation", 1, m.size());
+    }       
+    
+    public void testWeeklyRecurrence() throws Throwable {
+        
+/*
+      Juni 2006
+So Mo Di Mi Do Fr Sa 
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 
+*/       
+        long s = 1149768000000L; // 08.06.2006 12:00 (GMT)
+        long e = 1149771600000L; // 08.06.2006 13:00 (GMT)
+        long u = 1151100000000L; // 24.06.2006 00:00 (GMT)
+        
+        CalendarDataObject cdao = new CalendarDataObject();
+        cdao.setStartDate(new Date(s));
+        cdao.setEndDate(new Date(e));
+        cdao.setUntil(new Date(u));
+        cdao.setTitle("Weekly Appointment Test");
+        cdao.setRecurrenceCalculator(1);
+        cdao.setRecurrenceID(1);
+        cdao.setRecurrenceType(CalendarObject.WEEKLY);
+        cdao.setInterval(1);
+        cdao.setDays(AppointmentObject.MONDAY + AppointmentObject.THURSDAY);
+        CalendarRecurringCollection.fillDAO(cdao);
+        RecurringResults m = CalendarRecurringCollection.calculateRecurring(cdao, 0, 0, 0);
+        assertEquals("Check calculation", 5, m.size()); 
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(new Date(m.getRecurringResult(0).getStart()).getTime());
+        assertEquals("First day check (THURSDAY)", c.get(Calendar.DAY_OF_WEEK), Calendar.THURSDAY);
+        c.setTimeInMillis(new Date(m.getRecurringResult(1).getStart()).getTime());
+        assertEquals("First day check (THURSDAY)", c.get(Calendar.DAY_OF_WEEK), Calendar.MONDAY);
+        
+    }       
+    
+    
+    
+}
