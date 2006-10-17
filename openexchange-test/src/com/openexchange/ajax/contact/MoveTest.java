0dcedc48cbfe00332f82e6b6a881df382cce5f78
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-17 10:06:51 +0000

    Fixed compile errors

--- openexchange-test/src/com/openexchange/ajax/contact/MoveTest.java
+++ openexchange-test/src/com/openexchange/ajax/contact/MoveTest.java
@@ -1,14 +1,15 @@
 package com.openexchange.ajax.contact;
 
-import com.openexchange.ajax.AppointmentTest;
+import com.openexchange.ajax.ContactTest;
 import com.openexchange.groupware.configuration.AbstractConfigWrapper;
 import com.openexchange.groupware.container.AppointmentObject;
+import com.openexchange.groupware.container.ContactObject;
 import com.openexchange.groupware.container.FolderObject;
 import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class MoveTest extends AppointmentTest {
+public class MoveTest extends ContactTest {
 
 	private static final Log LOG = LogFactory.getLog(MoveTest.class);
 	
@@ -21,52 +22,46 @@ public class MoveTest extends AppointmentTest {
 	}
 	
 	public void testMove2PrivateFolder() throws Exception {
-		AppointmentObject appointmentObj = new AppointmentObject();
-		String date = String.valueOf(System.currentTimeMillis());
-		appointmentObj.setTitle("testMove2PrivateFolder" + date);
-		appointmentObj.setStartDate(new Date(startTime));
-		appointmentObj.setEndDate(new Date(endTime));
-		appointmentObj.setParentFolderID(appointmentFolderId);
-		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+		ContactObject contactObj = new ContactObject();
+		contactObj.setSurName("testMove2PrivateFolder");
+		contactObj.setParentFolderID(contactFolderId);
+		int objectId = insertContact(getWebConversation(), contactObj, PROTOCOL + getHostName(), getSessionId());
 		
 		String login = AbstractConfigWrapper.parseProperty(getAJAXProperties(), "login", "");
 		String password = AbstractConfigWrapper.parseProperty(getAJAXProperties(), "password", "");
 		
-		FolderObject folderObj = com.openexchange.webdav.xml.FolderTest.createFolderObject(userId, "testMove2PrivateFolder" + System.currentTimeMillis(), FolderObject.CALENDAR, false);
+		FolderObject folderObj = com.openexchange.webdav.xml.FolderTest.createFolderObject(userId, "testMove2PrivateFolder" + System.currentTimeMillis(), FolderObject.CONTACT, false);
 		int targetFolder = com.openexchange.webdav.xml.FolderTest.insertFolder(getWebConversation(), folderObj, PROTOCOL + getHostName(), login, password);
 
-		appointmentObj.setParentFolderID(targetFolder);
-		updateAppointment(getWebConversation(), appointmentObj, objectId, appointmentFolderId, timeZone, PROTOCOL + getHostName(), getSessionId());
-		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, targetFolder, timeZone, PROTOCOL + getHostName(), getSessionId());
-		appointmentObj.setObjectID(objectId);
-		compareObject(appointmentObj, loadAppointment, appointmentObj.getStartDate().getTime(), appointmentObj.getEndDate().getTime());
+		contactObj.setParentFolderID(targetFolder);
+		updateContact(getWebConversation(), contactObj, objectId, contactFolderId, PROTOCOL + getHostName(), getSessionId());
+		ContactObject loadContact = loadContact(getWebConversation(), objectId, targetFolder, PROTOCOL + getHostName(), getSessionId());
+		contactObj.setObjectID(objectId);
+		compareObject(contactObj, loadContact);
 		
-		deleteAppointment(getWebConversation(), objectId, targetFolder, PROTOCOL + getHostName(), getSessionId());
+		deleteContact(getWebConversation(), objectId, targetFolder, PROTOCOL + getHostName(), getSessionId());
 		com.openexchange.webdav.xml.FolderTest.deleteFolder(getWebConversation(), new int[] { targetFolder }, PROTOCOL + getHostName(), login, password);
 	}	
 	
 	public void testMove2PublicFolder() throws Exception {
-		AppointmentObject appointmentObj = new AppointmentObject();
-		String date = String.valueOf(System.currentTimeMillis());
-		appointmentObj.setTitle("testMove2PublicFolder" + date);
-		appointmentObj.setStartDate(new Date(startTime));
-		appointmentObj.setEndDate(new Date(endTime));
-		appointmentObj.setParentFolderID(appointmentFolderId);
-		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+		ContactObject contactObj = new ContactObject();
+		contactObj.setSurName("testMove2PublicFolder");
+		contactObj.setParentFolderID(contactFolderId);
+		int objectId = insertContact(getWebConversation(), contactObj, PROTOCOL + getHostName(), getSessionId());
 		
 		String login = AbstractConfigWrapper.parseProperty(getAJAXProperties(), "login", "");
 		String password = AbstractConfigWrapper.parseProperty(getAJAXProperties(), "password", "");
 		
-		FolderObject folderObj = com.openexchange.webdav.xml.FolderTest.createFolderObject(userId, "testMove2PublicFolder" + System.currentTimeMillis(), FolderObject.CALENDAR, true);
+		FolderObject folderObj = com.openexchange.webdav.xml.FolderTest.createFolderObject(userId, "testMove2PublicFolder" + System.currentTimeMillis(), FolderObject.CONTACT, true);
 		int targetFolder = com.openexchange.webdav.xml.FolderTest.insertFolder(getWebConversation(), folderObj, PROTOCOL + getHostName(), login, password);
 
-		appointmentObj.setParentFolderID(targetFolder);
-		updateAppointment(getWebConversation(), appointmentObj, objectId, appointmentFolderId, timeZone, PROTOCOL + getHostName(), getSessionId());
-		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, targetFolder, timeZone, PROTOCOL + getHostName(), getSessionId());
-		appointmentObj.setObjectID(objectId);
-		compareObject(appointmentObj, loadAppointment, appointmentObj.getStartDate().getTime(), appointmentObj.getEndDate().getTime());
+		contactObj.setParentFolderID(targetFolder);
+		updateContact(getWebConversation(), contactObj, objectId, contactFolderId, PROTOCOL + getHostName(), getSessionId());
+		ContactObject loadContact = loadContact(getWebConversation(), objectId, targetFolder, PROTOCOL + getHostName(), getSessionId());
+		contactObj.setObjectID(objectId);
+		compareObject(contactObj, loadContact);
 		
-		deleteAppointment(getWebConversation(), objectId, targetFolder, PROTOCOL + getHostName(), getSessionId());
+		deleteContact(getWebConversation(), objectId, targetFolder, PROTOCOL + getHostName(), getSessionId());
 		com.openexchange.webdav.xml.FolderTest.deleteFolder(getWebConversation(), new int[] { targetFolder }, PROTOCOL + getHostName(), login, password);
 	}
 }
