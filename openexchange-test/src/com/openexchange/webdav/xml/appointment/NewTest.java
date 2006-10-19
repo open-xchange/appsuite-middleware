21dc5c9aa8728958079e5bec07441029defad497
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-10-19 10:42:16 +0000

    Fixed participant test and add test with external participants

--- openexchange-test/src/com/openexchange/webdav/xml/appointment/NewTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/appointment/NewTest.java
@@ -2,8 +2,10 @@ package com.openexchange.webdav.xml.appointment;
 
 import com.openexchange.groupware.container.AppointmentObject;
 import com.openexchange.groupware.container.CalendarObject;
-import com.openexchange.groupware.container.ContactObject;
+import com.openexchange.groupware.container.ExternalGroupParticipant;
+import com.openexchange.groupware.container.ExternalUserParticipant;
 import com.openexchange.groupware.container.GroupParticipant;
+import com.openexchange.groupware.container.Participant;
 import com.openexchange.groupware.container.UserParticipant;
 import com.openexchange.groupware.ldap.Group;
 import com.openexchange.webdav.xml.AppointmentTest;
@@ -24,23 +26,22 @@ public class NewTest extends AppointmentTest {
 		AppointmentObject appointmentObj = createAppointmentObject("testNewAppointmentWithParticipants");
 		appointmentObj.setIgnoreConflicts(true);
 		
-		int userParticipantId = GroupUserTest.getUserId(getWebConversation(), PROTOCOL + getHostName(), getLogin(), getPassword());
-		assertTrue("user participant not found", userParticipantId != -1);
 		Group[] groupArray = GroupUserTest.searchGroup(getWebConversation(), groupParticipant, new Date(0), PROTOCOL + getHostName(), getLogin(), getPassword());
 		assertTrue("group array size is not > 0", groupArray.length > 0);
 		int groupParticipantId = groupArray[0].getIdentifier();
 		
-		com.openexchange.groupware.container.Participant[] participants = new com.openexchange.groupware.container.Participant[3];
+		com.openexchange.groupware.container.Participant[] participants = new com.openexchange.groupware.container.Participant[2];
 		participants[0] = new UserParticipant();
 		participants[0].setIdentifier(userId);
-		participants[1] = new UserParticipant();
-		participants[1].setIdentifier(userParticipantId);
-		participants[2] = new GroupParticipant();
-		participants[2].setIdentifier(groupParticipantId);
+		participants[1] = new GroupParticipant();
+		participants[1].setIdentifier(groupParticipantId);
 		
 		appointmentObj.setParticipants(participants);
 		
 		int objectId = insertAppointment(webCon, appointmentObj, PROTOCOL + hostName, login, password);
+		appointmentObj.setObjectID(objectId);
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getLogin(), getPassword());
+		compareObject(appointmentObj, loadAppointment);
 		int[][] objectIdAndFolderId = { {objectId, appointmentFolderId } };
 		deleteAppointment(webCon, objectIdAndFolderId, PROTOCOL + hostName, login, password );
 	}
@@ -49,7 +50,7 @@ public class NewTest extends AppointmentTest {
 		AppointmentObject appointmentObj = createAppointmentObject("testNewAppointmentWithUsers");
 		appointmentObj.setIgnoreConflicts(true);
 		
-		int userParticipantId = GroupUserTest.getUserId(getWebConversation(), PROTOCOL + getHostName(), getLogin(), getPassword());
+		int userParticipantId = GroupUserTest.getUserId(getSecondWebConversation(), PROTOCOL + getHostName(), getSecondLogin(), getPassword());
 		assertTrue("user participant not found", userParticipantId != -1);
 		
 		UserParticipant[] users = new UserParticipant[2];
@@ -61,11 +62,39 @@ public class NewTest extends AppointmentTest {
 		users[1].setConfirm(CalendarObject.DECLINE);
 		
 		appointmentObj.setUsers(users);
+		appointmentObj.setParticipants(users);
 		
 		int objectId = insertAppointment(webCon, appointmentObj, PROTOCOL + hostName, login, password);
+		appointmentObj.setObjectID(objectId);
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getLogin(), getPassword());
+		compareObject(appointmentObj, loadAppointment);
 		int[][] objectIdAndFolderId = { {objectId, appointmentFolderId } };
 		deleteAppointment(webCon, objectIdAndFolderId, PROTOCOL + hostName, login, password );
-
+	}
+	
+	public void _notestNewAppointmentWithExternalParticipants() throws Exception {
+		AppointmentObject appointmentObj = createAppointmentObject("testNewAppointmentWithExternalParticipants");
+		appointmentObj.setIgnoreConflicts(true);
+		
+		int userParticipantId = GroupUserTest.getUserId(getWebConversation(), PROTOCOL + getHostName(), getLogin(), getPassword());
+		assertTrue("user participant not found", userParticipantId != -1);
+		
+		Participant[] participant = new Participant[3];
+		participant[0] = new UserParticipant();
+		participant[0].setIdentifier(userId);
+		participant[1] = new ExternalUserParticipant();
+		participant[1].setEmailAddress("externaluser@example.org");
+		participant[2] = new ExternalGroupParticipant();
+		participant[2].setEmailAddress("externalgroup@example.org");
+		
+		appointmentObj.setParticipants(participant);
+		
+		int objectId = insertAppointment(webCon, appointmentObj, PROTOCOL + hostName, login, password);
+		appointmentObj.setObjectID(objectId);
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getLogin(), getPassword());
+		compareObject(appointmentObj, loadAppointment);
+		int[][] objectIdAndFolderId = { {objectId, appointmentFolderId } };
+		deleteAppointment(webCon, objectIdAndFolderId, PROTOCOL + hostName, login, password );
 	}
 }
 
