b3c07869876eecfe91e8757dadfd035fce4231cf
Thorben Betten <thorben.betten@open-xchange.com>
2006-11-09 10:30:58 +0000

    *** empty log message ***

--- openexchange-test/src/com/openexchange/folder/FolderTest.java
+++ openexchange-test/src/com/openexchange/folder/FolderTest.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 
 import junit.framework.TestCase;
 
+import com.openexchange.api.OXPermissionException;
+import com.openexchange.api2.FolderSQLInterface;
+import com.openexchange.api2.RdbFolderSQLInterface;
 import com.openexchange.cache.FolderCacheManager;
 import com.openexchange.groupware.Init;
 import com.openexchange.groupware.UserConfiguration;
@@ -17,14 +20,17 @@ import com.openexchange.server.EffectivePermission;
 import com.openexchange.server.OCLPermission;
 import com.openexchange.sessiond.SessionObject;
 import com.openexchange.sessiond.SessionObjectWrapper;
-import com.openexchange.tools.oxfolder.OXFolderTools;
+import com.openexchange.tools.iterator.SearchIterator;
 import com.openexchange.tools.oxfolder.OXFolderAction;
+import com.openexchange.tools.oxfolder.OXFolderTools;
 
 /**
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  *
  */
 public class FolderTest extends TestCase {
+	
+	private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(FolderTest.class);
 	
 	public final static int CONTEXT_ID = 1;
 	
@@ -68,9 +74,11 @@ public class FolderTest extends TestCase {
 	}
 	
 	public void testFolderInsertSuccess() {
+		final int userId = session.getUserObject().getId();
+		final int[] groups = session.getUserObject().getGroups();
+		final OXFolderAction oxfa = new OXFolderAction(session);
+		int fuid = -1;
 		try {
-			final int userId = session.getUserObject().getId();
-			final int[] groups = session.getUserObject().getGroups();
 			final FolderObject fo = new FolderObject();
 			fo.setFolderName("NewCalendarTestFolder");
 			fo.setParentFolderID(FolderObject.SYSTEM_PRIVATE_FOLDER_ID);
@@ -82,8 +90,10 @@ public class FolderTest extends TestCase {
 			ocl.setGroupPermission(false);
 			ocl.setFolderAdmin(true);
 			fo.setPermissionsAsArray(new OCLPermission[] { ocl });
-			final OXFolderAction oxfa = new OXFolderAction(session);
-			final int fuid = oxfa.createFolder(fo, userId, groups, session.getUserConfiguration(), true, true, session.getContext(), null, null, true, true);
+			/*
+			 * Create folder
+			 */
+			fuid = oxfa.createFolder(fo, userId, groups, session.getUserConfiguration(), true, true, session.getContext(), null, null, true, true);
 			/*
 			 * Check folder object
 			 */
@@ -102,6 +112,15 @@ public class FolderTest extends TestCase {
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail(e.getMessage());
+		} finally {
+			try {
+				/*
+				 * Delete Test Folder...
+				 */
+				oxfa.deleteFolder(fuid, userId, groups, session.getUserConfiguration(), true, session.getContext(), null, System.currentTimeMillis());
+			} catch (Exception e) {
+				LOG.error(e.getMessage(), e);
+			}
 		}
 	}
 	
@@ -152,12 +171,14 @@ public class FolderTest extends TestCase {
 			fo.setType(FolderObject.PRIVATE);
 			final ArrayList<OCLPermission> perms = new ArrayList<OCLPermission>();
 			OCLPermission ocl = new OCLPermission();
-			// Wrong user id in permission!
 			ocl.setEntity(userId);
 			ocl.setAllPermission(OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
 			ocl.setGroupPermission(false);
 			ocl.setFolderAdmin(true);
 			perms.add(ocl);
+			/*
+			 * ERROR: Define a second admin
+			 */
 			ocl = new OCLPermission();
 			ocl.setEntity(resolveUser(Init.getAJAXProperty("seconduser")));
 			ocl.setAllPermission(OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
@@ -193,6 +214,10 @@ public class FolderTest extends TestCase {
 			fo.setFolderName("NewCalendarTestFolder");
 			fo.setParentFolderID(FolderObject.SYSTEM_PRIVATE_FOLDER_ID);
 			fo.setModule(FolderObject.CALENDAR);
+			/*
+			 * ERROR: Define a wrong type. Enforced type should be private since
+			 * we are going to insert beneath system private folder
+			 */
 			fo.setType(FolderObject.PUBLIC);
 			final ArrayList<OCLPermission> perms = new ArrayList<OCLPermission>();
 			final OCLPermission ocl = new OCLPermission();
@@ -229,6 +254,9 @@ public class FolderTest extends TestCase {
 			final FolderObject fo = new FolderObject();
 			fo.setFolderName("NewCalendarTestFolder");
 			fo.setParentFolderID(FolderObject.SYSTEM_PRIVATE_FOLDER_ID);
+			/*
+			 * ERROR: Define a prohibited module beneath system private folder
+			 */
 			fo.setModule(FolderObject.INFOSTORE);
 			fo.setType(FolderObject.PRIVATE);
 			final ArrayList<OCLPermission> perms = new ArrayList<OCLPermission>();
@@ -621,6 +649,186 @@ public class FolderTest extends TestCase {
 				final FolderObject tmp = FolderCacheManager.getInstance().getFolderObject(fuid, session.getContext());
 				if (tmp != null) {
 					oxfa.deleteFolder(fuid, userId, groups, session.getUserConfiguration(), true, session.getContext(), null, System.currentTimeMillis());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testWithModifiedUserConfig001() {
+		try {
+			final int userId = session.getUserObject().getId();
+			final int[] groups = session.getUserObject().getGroups();
+			int fuid = -1;
+			OXFolderAction oxfa = null;
+			try {
+				/*
+				 * Create a public folder
+				 */
+				FolderObject fo = new FolderObject();
+				fo.setFolderName("NewCalendarTestFolder");
+				fo.setParentFolderID(FolderObject.SYSTEM_PUBLIC_FOLDER_ID);
+				fo.setModule(FolderObject.CALENDAR);
+				fo.setType(FolderObject.PUBLIC);
+				OCLPermission ocl = new OCLPermission();
+				ocl.setEntity(userId);
+				ocl.setAllPermission(OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
+				ocl.setGroupPermission(false);
+				ocl.setFolderAdmin(true);
+				fo.setPermissionsAsArray(new OCLPermission[] { ocl });
+				oxfa = new OXFolderAction(session);
+				try {
+					fuid = oxfa.createFolder(fo, userId, groups, session.getUserConfiguration(), true, true, session.getContext(), null, null, true, true);
+				} catch (Exception e) {
+					oxfa.deleteFolder(2498, session, true, System.currentTimeMillis());
+					fuid = oxfa.createFolder(fo, userId, groups, session.getUserConfiguration(), true, true, session.getContext(), null, null, true, true);
+				}
+				/*
+				 * Deny creation or modifications of public folders
+				 */
+				session.getUserConfiguration().setFullPublicFolderAccess(false);
+				/*
+				 * Try to edit a public folder
+				 */
+				fo.setFolderName("NewCalendarTestFolder_Changed");
+				Exception exc=null;
+				try {
+					fo = oxfa.updateMoveRenameFolder(fo, session, true, System.currentTimeMillis(), null, null);
+				} catch (Exception e) {
+					System.out.println("\n\n\n" + e.getMessage());
+					exc = e;
+				}
+				assertTrue(exc != null && exc instanceof OXPermissionException);
+			} finally {
+				session.getUserConfiguration().setFullPublicFolderAccess(true);
+				/*
+				 * Delete Test Folder...
+				 */
+				final FolderObject tmp = FolderCacheManager.getInstance().getFolderObject(fuid, session.getContext());
+				if (tmp != null) {
+					oxfa.deleteFolder(fuid, userId, groups, session.getUserConfiguration(), true, session.getContext(), null, System.currentTimeMillis());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testWithModifiedUserConfig002() {
+		try {
+			final int userId = session.getUserObject().getId();
+			final int[] groups = session.getUserObject().getGroups();
+			int fuid = -1;
+			OXFolderAction oxfa = null;
+			try {
+				/*
+				 * Create a public folder
+				 */
+				FolderObject fo = new FolderObject();
+				fo.setFolderName("NewCalendarTestFolder");
+				fo.setParentFolderID(FolderObject.SYSTEM_PUBLIC_FOLDER_ID);
+				fo.setModule(FolderObject.CALENDAR);
+				fo.setType(FolderObject.PUBLIC);
+				OCLPermission ocl = new OCLPermission();
+				ocl.setEntity(userId);
+				ocl.setAllPermission(OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
+				ocl.setGroupPermission(false);
+				ocl.setFolderAdmin(true);
+				fo.setPermissionsAsArray(new OCLPermission[] { ocl });
+				oxfa = new OXFolderAction(session);
+				fuid = oxfa.createFolder(fo, userId, groups, session.getUserConfiguration(), true, true, session.getContext(), null, null, true, true);
+				/*
+				 * Deny calendar module access
+				 */
+				session.getUserConfiguration().setCalendar(false);
+				/*
+				 * Try to edit a public folder
+				 */
+				fo.setFolderName("NewCalendarTestFolder_Changed");
+				Exception exc=null;
+				try {
+					fo = oxfa.updateMoveRenameFolder(fo, session, true, System.currentTimeMillis(), null, null);
+				} catch (Exception e) {
+					System.out.println("\n\n\n" + e.getMessage());
+					exc = e;
+				}
+				assertTrue(exc != null && exc instanceof OXPermissionException);
+			} finally {
+				session.getUserConfiguration().setCalendar(true);
+				/*
+				 * Delete Test Folder...
+				 */
+				final FolderObject tmp = FolderCacheManager.getInstance().getFolderObject(fuid, session.getContext());
+				if (tmp != null) {
+					oxfa.deleteFolder(fuid, userId, groups, session.getUserConfiguration(), true, session.getContext(), null, System.currentTimeMillis());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testGetSubfolders() {
+		try {
+			FolderSQLInterface folderSQLInterface = new RdbFolderSQLInterface(session);
+			SearchIterator it = null;
+			try {
+				it = folderSQLInterface.getSubfolders(FolderObject.SYSTEM_PRIVATE_FOLDER_ID, null);
+				if (it.hasSize()) {
+					final int size = it.size();
+					assertTrue(size >= 3);
+					for (int i = 0; i < size; i++) {
+						FolderObject fo = (FolderObject) it.next();
+						assertTrue(fo != null);
+					}
+				} else {
+					assertTrue(it.hasNext());
+					while (it.hasNext()) {
+						FolderObject fo = (FolderObject) it.next();
+						assertTrue(fo != null);
+					}
+				}
+			} finally {
+				if (it != null) {
+					it.close();
+					it = null;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	public void testGetSubfoldersWithRestrictedAccess() {
+		try {
+			session.getUserConfiguration().setCalendar(false);
+			FolderSQLInterface folderSQLInterface = new RdbFolderSQLInterface(session);
+			SearchIterator it = null;
+			try {
+				it = folderSQLInterface.getSubfolders(FolderObject.SYSTEM_PRIVATE_FOLDER_ID, null);
+				if (it.hasSize()) {
+					final int size = it.size();
+					assertTrue(size >= 1);
+					for (int i = 0; i < size; i++) {
+						FolderObject fo = (FolderObject) it.next();
+						assertTrue(fo.getModule() != FolderObject.CALENDAR);
+					}
+				} else {
+					assertTrue(it.hasNext());
+					while (it.hasNext()) {
+						FolderObject fo = (FolderObject) it.next();
+						assertTrue(fo.getModule() != FolderObject.CALENDAR);
+					}
+				}
+			} finally {
+				if (it != null) {
+					it.close();
+					it = null;
 				}
 			}
 		} catch (Exception e) {
