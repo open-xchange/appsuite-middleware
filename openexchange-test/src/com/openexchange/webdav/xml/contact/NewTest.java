29fffb5b79a3b5d648c305a51020d451f51210f4
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-12-11 11:10:42 +0000

    add disabled method to check private contacts in public folder

--- openexchange-test/src/com/openexchange/webdav/xml/contact/NewTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/contact/NewTest.java
@@ -1,7 +1,12 @@
 package com.openexchange.webdav.xml.contact;
 
 import com.openexchange.groupware.container.ContactObject;
+import com.openexchange.groupware.container.FolderObject;
+import com.openexchange.server.OCLPermission;
+import com.openexchange.test.TestException;
 import com.openexchange.webdav.xml.ContactTest;
+import com.openexchange.webdav.xml.FolderTest;
+import com.openexchange.webdav.xml.XmlServlet;
 
 public class NewTest extends ContactTest {
 	
@@ -10,5 +15,33 @@ public class NewTest extends ContactTest {
 		insertContact(webCon, contactObj, PROTOCOL + hostName, login, password);
 	}
 	
+	public void _notestContactInPrivateFlagInPublicFolder() throws Exception {
+		FolderObject folderObj = new FolderObject();
+		folderObj.setFolderName("testContactInPrivateFlagInPublicFolder" + System.currentTimeMillis());
+		folderObj.setModule(FolderObject.CONTACT);
+		folderObj.setType(FolderObject.PUBLIC);
+		folderObj.setParentFolderID(2);
+		
+		OCLPermission[] permission = new OCLPermission[] { 
+			FolderTest.createPermission( userId, false, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION)
+		};
+		
+		folderObj.setPermissionsAsArray( permission );
+		
+		final int parentFolderId = FolderTest.insertFolder(getWebConversation(), folderObj, PROTOCOL + getHostName(), getLogin(), getPassword());
+		
+		ContactObject contactObj = new ContactObject();
+		contactObj.setSurName("testContactInPrivateFlagInPublicFolder");
+		contactObj.setPrivateFlag(true);
+		contactObj.setParentFolderID(parentFolderId);
+
+		try {
+			int objectId = insertContact(getWebConversation(), contactObj, PROTOCOL + getHostName(), getLogin(), getPassword());
+			deleteContact(getWebConversation(), objectId, parentFolderId, PROTOCOL + getHostName(), getLogin(), getPassword());
+			fail("conflict exception expected!");
+		} catch (TestException exc) {
+			assertExceptionMessage(exc.getMessage(), XmlServlet.CONFLICT_STATUS);
+		}
+	}	
 }
 
