5b9c2babcfb571c5093f0c6ef20ea04316bcf937
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-12-11 10:59:34 +0000

    add testModifiedWithFolderId to check if response if correct if the request doesn't contains a folder id

--- openexchange-test/src/com/openexchange/ajax/appointment/UpdatesTest.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/UpdatesTest.java
@@ -1,6 +1,7 @@
 package com.openexchange.ajax.appointment;
 
 import com.openexchange.ajax.AppointmentTest;
+import com.openexchange.groupware.container.AppointmentObject;
 import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -27,6 +28,29 @@ public class UpdatesTest extends AppointmentTest {
 	
 	public void testDeleted() throws Exception {
 		AppointmentTest.listDeleteAppointment(getWebConversation(), appointmentFolderId, new Date(), new Date(), new Date(System.currentTimeMillis()-(dayInMillis*7)), timeZone, PROTOCOL + getHostName(), getSessionId());
+	}
+	
+	public void testModifiedWithoutFolderId() throws Exception {
+		Date start = new Date(System.currentTimeMillis() - (7 * dayInMillis));
+		Date end = new Date(System.currentTimeMillis() + (7 * dayInMillis));
+		Date since = new Date();
+		
+		AppointmentObject appointmentObj = createAppointmentObject("testModifiedWithoutFolderId");
+		appointmentObj.setIgnoreConflicts(true);
+		int objectId = AppointmentTest.insertAppointment(getWebConversation(), appointmentObj, timeZone, getHostName(), getSessionId());
+		
+		AppointmentObject[] appointmentArray = AppointmentTest.listModifiedAppointment(getWebConversation(), start, end, since, timeZone, PROTOCOL + getHostName(), getSessionId());
+		
+		assertTrue("no appointment object in response", appointmentArray.length > 0);
+		boolean found = false;
+		
+		for (int a = 0; a < appointmentArray.length; a++) {
+			if (appointmentArray[a].getObjectID() == objectId) {
+				found = true;
+			}
+		}
+		
+		assertTrue("created object not found in response", found);
 	}
 }
 
