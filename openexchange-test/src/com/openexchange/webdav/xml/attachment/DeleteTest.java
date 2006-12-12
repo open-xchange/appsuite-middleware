55bdaa51167ab3ccdb5446ebd095b0f04441a8fe
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-12-12 10:48:48 +0000

    redesign of attachment test methods

--- openexchange-test/src/com/openexchange/webdav/xml/attachment/DeleteTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/attachment/DeleteTest.java
@@ -1,11 +1,14 @@
 package com.openexchange.webdav.xml.attachment;
 
 import com.openexchange.groupware.Types;
+import com.openexchange.groupware.attach.AttachmentMetadata;
+import com.openexchange.groupware.attach.impl.AttachmentImpl;
 import com.openexchange.groupware.container.ContactObject;
 import com.openexchange.groupware.container.FolderObject;
 import com.openexchange.webdav.xml.AttachmentTest;
 import com.openexchange.webdav.xml.ContactTest;
 import com.openexchange.webdav.xml.FolderTest;
+import java.io.ByteArrayInputStream;
 
 public class DeleteTest extends AttachmentTest {
 	
@@ -15,10 +18,25 @@ public class DeleteTest extends AttachmentTest {
 		ContactObject contactObj = new ContactObject();
 		contactObj.setSurName("testDeleteAttachment");
 		contactObj.setParentFolderID(contactFolderId);
+		
 		int objectId = ContactTest.insertContact(webCon, contactObj, PROTOCOL + hostName, login, password);
-		int attachmentId = insertAttachment(System.currentTimeMillis() + "test.txt", Types.CONTACT, objectId, contactFolderId, false);
-		deleteAttachment(attachmentId, Types.TASK, objectId, contactFolderId);
-	}
-	
+		
+		AttachmentMetadata attachmentObj = new AttachmentImpl();
+		attachmentObj.setFilename(System.currentTimeMillis() + "test.txt");
+		attachmentObj.setModuleId(Types.CONTACT);
+		attachmentObj.setAttachedId(objectId);
+		attachmentObj.setFolderId(contactFolderId);
+		attachmentObj.setRtfFlag(false);
+		attachmentObj.setFileMIMEType(CONTENT_TYPE);
+		
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+		
+		int attachmentId = insertAttachment(webCon, attachmentObj, byteArrayInputStream, getHostName(), getLogin(), getPassword());
+		assertTrue("attachment is 0", attachmentId > 0);
+		
+		attachmentObj.setId(attachmentId);
+		
+		deleteAttachment(webCon, attachmentObj, getHostName(), getLogin(), getPassword());
+	}	
 }
 
--- openexchange-test/src/com/openexchange/webdav/xml/attachment/ListTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/attachment/ListTest.java
@@ -1,11 +1,16 @@
 package com.openexchange.webdav.xml.attachment;
 
 import com.openexchange.groupware.Types;
+import com.openexchange.groupware.attach.AttachmentMetadata;
+import com.openexchange.groupware.attach.impl.AttachmentImpl;
 import com.openexchange.groupware.container.ContactObject;
 import com.openexchange.groupware.container.FolderObject;
 import com.openexchange.webdav.xml.AttachmentTest;
 import com.openexchange.webdav.xml.ContactTest;
 import com.openexchange.webdav.xml.FolderTest;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 public class ListTest extends AttachmentTest {
 
@@ -15,9 +20,31 @@ public class ListTest extends AttachmentTest {
 		ContactObject contactObj = new ContactObject();
 		contactObj.setSurName("testLoadAttachment");
 		contactObj.setParentFolderID(contactFolderId);
+		
 		int objectId = ContactTest.insertContact(webCon, contactObj, PROTOCOL + hostName, login, password);
-		int attachmentId = insertAttachment(System.currentTimeMillis() + "test.txt", Types.CONTACT, objectId, contactFolderId, false);
-		loadAttachment(attachmentId, Types.CONTACT, objectId, contactFolderId, false);
+		
+		AttachmentMetadata attachmentObj = new AttachmentImpl();
+		attachmentObj.setFilename(System.currentTimeMillis() + "test.txt");
+		attachmentObj.setModuleId(Types.CONTACT);
+		attachmentObj.setAttachedId(objectId);
+		attachmentObj.setFolderId(contactFolderId);
+		attachmentObj.setRtfFlag(false);
+		attachmentObj.setFileMIMEType(CONTENT_TYPE);
+		
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+		
+		int attachmentId = insertAttachment(webCon, attachmentObj, byteArrayInputStream, getHostName(), getLogin(), getPassword());
+		assertTrue("attachment is 0", attachmentId > 0);
+		
+		attachmentObj.setId(attachmentId);
+		InputStream is = loadAttachment(webCon, attachmentObj, getHostName(), getLogin(), getPassword());
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		byte b[] = new byte[512];
+		int len = 0;
+		while ((len = is.read(b)) != -1) {
+			byteArrayOutputStream.write(b, 0, len);
+		}
+		assertEqualsAndNotNull("byte[] are not equals", data, byteArrayOutputStream.toByteArray());
 	}
 	
 	public void testLoadAttachmentWithRtf() throws Exception {
@@ -26,9 +53,31 @@ public class ListTest extends AttachmentTest {
 		ContactObject contactObj = new ContactObject();
 		contactObj.setSurName("testLoadAttachmentWithRtf");
 		contactObj.setParentFolderID(contactFolderId);
+		
 		int objectId = ContactTest.insertContact(webCon, contactObj, PROTOCOL + hostName, login, password);
-		int attachmentId = insertAttachment(System.currentTimeMillis() + "test.txt", Types.CONTACT, objectId, contactFolderId, true);
-		loadAttachment(attachmentId, Types.CONTACT, objectId, contactFolderId, true);
+		
+		AttachmentMetadata attachmentObj = new AttachmentImpl();
+		attachmentObj.setFilename(System.currentTimeMillis() + "test.txt");
+		attachmentObj.setModuleId(Types.CONTACT);
+		attachmentObj.setAttachedId(objectId);
+		attachmentObj.setFolderId(contactFolderId);
+		attachmentObj.setRtfFlag(true);
+		attachmentObj.setFileMIMEType(CONTENT_TYPE);
+		
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+		
+		int attachmentId = insertAttachment(webCon, attachmentObj, byteArrayInputStream, getHostName(), getLogin(), getPassword());
+		assertTrue("attachment is 0", attachmentId > 0);
+		
+		attachmentObj.setId(attachmentId);
+		InputStream is = loadAttachment(webCon, attachmentObj, getHostName(), getLogin(), getPassword());
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		byte b[] = new byte[512];
+		int len = 0;
+		while ((len = is.read(b)) != -1) {
+			byteArrayOutputStream.write(b, 0, len);
+		}
+		assertEqualsAndNotNull("byte[] are not equals", data, byteArrayOutputStream.toByteArray());
 	}
 	
 }
