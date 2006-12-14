88c207270249ddfe4df2aba55b7538ccf34ebeba
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-12-14 13:53:27 +0000

    add appointment bug test suite

---
+++ openexchange-test/src/com/openexchange/ajax/appointment/AppointmentBugTestSuite.java
@@ -0,0 +1,15 @@
+package com.openexchange.ajax.appointment;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AppointmentBugTestSuite extends TestSuite{
+	
+	public static Test suite(){
+		TestSuite tests = new TestSuite();
+		tests.addTestSuite( Bug4392Test.class );
+		
+		return tests;
+	}
+}
