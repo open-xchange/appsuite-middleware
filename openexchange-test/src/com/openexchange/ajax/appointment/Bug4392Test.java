5c544901dc5c0f83ede25032ca169daa3b7854df
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-12-14 13:53:14 +0000

    add test case for bug 4392

---
+++ openexchange-test/src/com/openexchange/ajax/appointment/Bug4392Test.java
@@ -0,0 +1,80 @@
+package com.openexchange.ajax.appointment;
+
+import com.openexchange.ajax.AppointmentTest;
+import com.openexchange.groupware.calendar.CalendarDataObject;
+import com.openexchange.groupware.container.AppointmentObject;
+import com.openexchange.groupware.container.CalendarObject;
+import com.openexchange.groupware.container.CommonObject;
+import com.openexchange.groupware.container.DataObject;
+import com.openexchange.groupware.container.FolderChildObject;
+import com.openexchange.test.TestException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Bug4392Test extends AppointmentTest {
+	
+	private final static int[] _appointmentFields = {
+		DataObject.OBJECT_ID,
+		DataObject.CREATED_BY,
+		DataObject.CREATION_DATE,
+		DataObject.LAST_MODIFIED,
+		DataObject.MODIFIED_BY,
+		FolderChildObject.FOLDER_ID,
+		CommonObject.PRIVATE_FLAG,
+		CommonObject.CATEGORIES,
+		CalendarObject.TITLE,
+		AppointmentObject.LOCATION,
+		CalendarObject.START_DATE,
+		CalendarObject.END_DATE,
+		CalendarObject.NOTE,
+		CalendarObject.RECURRENCE_TYPE,
+		CalendarObject.PARTICIPANTS,
+		CalendarObject.USERS,
+		AppointmentObject.SHOWN_AS,
+		AppointmentObject.FULL_TIME,
+		AppointmentObject.COLOR_LABEL,
+		CalendarDataObject.TIMEZONE
+	};
+
+	private static final Log LOG = LogFactory.getLog(AllTest.class);
+	
+	public Bug4392Test(String name) {
+		super(name);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testBug4392() throws Exception {
+		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		
+		Date start = new Date(calendar.getTimeInMillis() + dayInMillis);
+		Date end = new Date(start.getTime()+dayInMillis);
+		
+		int occurrences = 4;
+		
+		AppointmentObject appointmentObj = createAppointmentObject("testBug4392");
+		appointmentObj.setRecurrenceType(AppointmentObject.DAILY);
+		appointmentObj.setInterval(1);
+		appointmentObj.setOccurrence(4);
+		appointmentObj.setIgnoreConflicts(true);
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, getHostName(), getSessionId());
+		appointmentObj.setObjectID(objectId);
+		appointmentObj.setUntil(new Date(calendar.getTimeInMillis() + ((occurrences-1) * dayInMillis)));
+		
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, timeZone, getHostName(), getSessionId());
+		try {
+			compareObject(appointmentObj, loadAppointment, appointmentObj.getStartDate().getTime(), appointmentObj.getEndDate().getTime());
+		} catch (TestException exc) {
+			fail("exception: " + exc.toString());
+		}
+	}
+}
