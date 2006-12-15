4b534b4b7a8372d7aa8eb0e919f019ff64fa9345
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-12-15 14:58:53 +0000

    add constructor with test name and disabled test for attachments

--- openexchange-test/src/com/openexchange/ajax/contact/NewTest.java
+++ openexchange-test/src/com/openexchange/ajax/contact/NewTest.java
@@ -1,7 +1,11 @@
 package com.openexchange.ajax.contact;
 
 import com.openexchange.ajax.ContactTest;
+import com.openexchange.groupware.Types;
+import com.openexchange.groupware.attach.AttachmentMetadata;
+import com.openexchange.groupware.attach.impl.AttachmentImpl;
 import com.openexchange.groupware.container.ContactObject;
+import java.io.ByteArrayInputStream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -40,6 +44,31 @@ public class NewTest extends ContactTest {
 		link2.setObjectID(linkId2);
 		
 		int objectId = createContactWithLinks("testNewWithLinks", link1, link2);
+	}
+	
+	public void _notestNewContactWithAttachment() throws Exception {
+		ContactObject contactObj = createContactObject("testNewContactWithAttachment");
+		int objectId = insertContact(getWebConversation(), contactObj, getHostName(), getSessionId());
+		contactObj.setNumberOfAttachments(2);
+		contactObj.setObjectID(objectId);
+		
+		AttachmentMetadata attachmentObj = new AttachmentImpl();
+		attachmentObj.setFilename(System.currentTimeMillis() + "test1.txt");
+		attachmentObj.setModuleId(Types.CONTACT);
+		attachmentObj.setAttachedId(objectId);
+		attachmentObj.setFolderId(contactFolderId);
+		attachmentObj.setRtfFlag(false);
+		attachmentObj.setFileMIMEType("plain/text");
+		
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("t1".getBytes());
+		
+		int attachmentId1 = com.openexchange.webdav.xml.AttachmentTest.insertAttachment(getWebConversation(), attachmentObj, byteArrayInputStream, getHostName(), getLogin(), getPassword());
+		
+		byteArrayInputStream = new ByteArrayInputStream("t2".getBytes());
+		int attachmentId2 = com.openexchange.webdav.xml.AttachmentTest.insertAttachment(getWebConversation(), attachmentObj, byteArrayInputStream, getHostName(), getLogin(), getPassword());
+		
+		ContactObject loadContact = ContactTest.loadContact(getWebConversation(), objectId, contactFolderId, getHostName(), getSessionId());
+		compareObject(contactObj, loadContact);	
 	}
 }
 
