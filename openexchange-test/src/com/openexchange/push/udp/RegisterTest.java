767520165e08506e591816368ae497d25f961d21
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2006-12-18 16:55:29 +0000

    add RegisterTest

---
+++ openexchange-test/src/com/openexchange/push/udp/RegisterTest.java
@@ -0,0 +1,70 @@
+package com.openexchange.push.udp;
+
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import junit.framework.TestCase;
+
+public class RegisterTest extends TestCase {
+	
+	private static String host = "localhost";
+	
+	private static int port = 44335;
+	
+	private DatagramSocket datagramSocket = null;
+	
+	private int userId = 0;
+	
+	private int contextId = 0;
+	
+	public RegisterTest(String name) {
+		super(name);
+	}
+	
+	public RegisterTest(String name, DatagramSocket datagramSocket, int userId, int contextId) {
+		super(name);
+		this.datagramSocket = datagramSocket;
+		this.userId = userId;
+		this.contextId = contextId;
+	}
+	
+	public void testRegister() throws Exception {
+		if (datagramSocket == null) {
+			throw new Exception("DatagramSocket is null");
+		}
+
+		register(datagramSocket, userId, contextId, host, port);
+	}
+	
+	public static void register(DatagramSocket datagramSocket, int userId, int contextId, String host, int port) throws Exception {
+		StringBuffer body = new StringBuffer();
+		body.append(PushRequest.REGISTER);
+		body.append('\1');
+		body.append(userId);
+		body.append('\1');
+		body.append(contextId);
+		
+		StringBuffer requestData = new StringBuffer();
+		requestData.append("1337");
+		requestData.append('\1');
+		requestData.append(body.length());
+		requestData.append('\1');
+		requestData.append(body);
+		
+		byte b[] = requestData.toString().getBytes();
+		
+		DatagramPacket datagramPacket = new DatagramPacket(b, b.length, InetAddress.getByName(host), port);
+		
+		datagramSocket.send(datagramPacket);
+		
+		byte[] responseByte = new byte[1024];
+		datagramPacket = new DatagramPacket(responseByte, responseByte.length);
+		datagramSocket.receive(datagramPacket);
+		responseByte = datagramPacket.getData();
+		byte[] responseData = new byte[datagramPacket.getLength()];
+		System.arraycopy(responseByte, 0, responseData, 0, responseData.length);
+		
+		assertEquals("unexpected response", "OK\1", new String(responseData));
+	}
+}
