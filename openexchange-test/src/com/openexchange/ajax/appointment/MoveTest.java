ae0582ff06ab1e7097a8ef18056c41ad4558d6db
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-01-24 13:29:20 +0000

    uncomment setting attribute shown_as

--- openexchange-test/src/com/openexchange/ajax/appointment/MoveTest.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/MoveTest.java
@@ -28,7 +28,7 @@ public class MoveTest extends AppointmentTest {
 		appointmentObj.setEndDate(new Date(endTime));
 		appointmentObj.setParentFolderID(appointmentFolderId);
 		appointmentObj.setIgnoreConflicts(true);
-		// appointmentObj.setShownAs(AppointmentObject.RESERVED);
+		appointmentObj.setShownAs(AppointmentObject.RESERVED);
 		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
 		
 		String login = AbstractConfigWrapper.parseProperty(getAJAXProperties(), "login", "");
@@ -55,7 +55,7 @@ public class MoveTest extends AppointmentTest {
 		appointmentObj.setEndDate(new Date(endTime));
 		appointmentObj.setParentFolderID(appointmentFolderId);
 		appointmentObj.setIgnoreConflicts(true);
-		// appointmentObj.setShownAs(AppointmentObject.RESERVED);
+		appointmentObj.setShownAs(AppointmentObject.RESERVED);
 		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
 		
 		String login = AbstractConfigWrapper.parseProperty(getAJAXProperties(), "login", "");
