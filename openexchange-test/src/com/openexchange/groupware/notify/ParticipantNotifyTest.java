c60c7f1494bf5486a85b4dc906ea173adb8c42e3
Francisco Laguna <francisco.laguna@open-xchange.com>
2007-01-29 18:33:14 +0000

    Bug 5557 - Resolve Object IDs for copied Appointments/Tasks http://www.open-xchange.org/bugzilla/show_bug.cgi?id=5557

--- openexchange-test/src/com/openexchange/groupware/notify/ParticipantNotifyTest.java
+++ openexchange-test/src/com/openexchange/groupware/notify/ParticipantNotifyTest.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
-import java.util.TimeZone;
 
 import junit.framework.TestCase;
 
@@ -61,6 +61,7 @@ public class ParticipantNotifyTest extends TestCase{
 		assertNames( msg.addresses, "user1@test.invalid" );
 		assertLanguage( EN , msg );
 		assertNames( participantNames,"User 1" );
+		assertEquals(200, msg.folderId);
 		
 		notify.clearMessages();
 		
@@ -75,6 +76,7 @@ public class ParticipantNotifyTest extends TestCase{
 		assertNames( msg.addresses, "user3@test.invalid" );
 		assertLanguage( DE , msg );
 		assertNames( participantNames,"User 3" );
+		assertEquals(400, msg.folderId);
 		
 	}
 	
@@ -114,48 +116,44 @@ public class ParticipantNotifyTest extends TestCase{
 		
 		notify.taskCreated(t,session);
 		
-		assertEquals(3,notify.getMessages().size());
+		
+		List<String> deAddresses = new ArrayList<String>();
+		List<String> enAddresses = new ArrayList<String>();
+		
 		
 		for(Message message : notify.getMessages()){
 			assertNames(parseParticipants(message), "The Mailadmin", "User 1", "User 2", "User 3", "User 4", "User 5", "User 6", "User 7","User 8","User 9");
-			
-			if(message.addresses.size() == 1) {
-				// DE and Samoa
-				assertNames(message.addresses, "user3@test.invalid");
-				assertLanguage(DE, message);
-			} else {
-				int lang = guessLanguage(message);
-				switch(lang) {
-				case DE:
-					if (Locale.getDefault().getLanguage().equalsIgnoreCase("de")) {
-						assertNames(message.addresses, "user2@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid","user9@test.invalid");
-					} else {
-						assertNames(message.addresses, "user2@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid");	
-					}	
-					break;
-				case EN:
-					if (Locale.getDefault().getLanguage().equalsIgnoreCase("en")) {
-						assertNames(message.addresses, "mailadmin@test.invalid", "user1@test.invalid", "user5@test.invalid", "user7@test.invalid","user9@test.invalid");
-					} else {
-						assertNames(message.addresses, "mailadmin@test.invalid", "user1@test.invalid", "user5@test.invalid", "user7@test.invalid");	
-					}
-					break;
-				}
-			} 
+			int lang = guessLanguage(message);
+			switch(lang) {
+			case DE:
+				deAddresses.addAll(message.addresses);
+				break;
+			case EN:
+				enAddresses.addAll(message.addresses);
+				break;
+			}
+		}
+		
+		if (Locale.getDefault().getLanguage().equalsIgnoreCase("de")) {
+			assertNames(deAddresses, "user2@test.invalid","user3@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid","user9@test.invalid");
+			assertNames(enAddresses, "mailadmin@test.invalid", "user1@test.invalid", "user7@test.invalid");	
+		} else {
+			assertNames(enAddresses, "user2@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid","user9@test.invalid");
+			assertNames(deAddresses, "mailadmin@test.invalid", "user1@test.invalid","user7@test.invalid", "user3@test.invalid");	
 		}
 	}
 	
 	public void testResolveGroup() throws Exception{
 		Participant[] participants = getParticipants(U(),G(2),S());
 		Task t = getTask(participants);
-		
+		t.setUsers((UserParticipant[])null); // If the user participants are not set, fall back to resolving groups in ParticipantNotify
 		notify.taskCreated(t,session);
 		
 		Message msg = notify.getMessages().get(0);
 		
 		String[] participantNames = parseParticipants( msg );
 		
-		assertNames( msg.addresses, "user2@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid");
+		assertAddresses( notify.getMessages(), "user2@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid");
 		assertLanguage( DE , msg );
 		assertNames( participantNames, "User 2", "User 4", "User 6", "User 8" );
 	}
@@ -169,8 +167,7 @@ public class ParticipantNotifyTest extends TestCase{
 		Message msg = notify.getMessages().get(0);
 		
 		String[] participantNames = parseParticipants( msg );
-		
-		assertNames( msg.addresses, "user2@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid");
+		assertAddresses( notify.getMessages(), "user2@test.invalid", "user4@test.invalid", "user6@test.invalid", "user8@test.invalid");
 		assertLanguage( DE , msg );
 		assertNames( participantNames, "User 2", "User 4", "User 6", "User 8" );
 	}
@@ -187,12 +184,20 @@ public class ParticipantNotifyTest extends TestCase{
 		Set<String> expectSet = new HashSet<String>(Arrays.asList(expected));
 		
 		for(String name : names) {
-			assertTrue(expectSet.remove(name));
+			assertTrue(names.toString(), expectSet.remove(name));
 		}
 	}
 	
+	public static final void assertAddresses(Collection<Message> messages, String...addresses) {
+		List<String> collected = new ArrayList<String>();
+		for(Message msg : messages) {
+			collected.addAll(msg.addresses);
+		}
+		assertNames(collected,addresses);
+	}
 	
-	public Task getTask(Participant[] participants) {
+	
+	public Task getTask(Participant[] participants) throws LdapException {
 		Task task = new Task();
 		task.setStartDate(start);
 		task.setEndDate(end);
@@ -203,6 +208,26 @@ public class ParticipantNotifyTest extends TestCase{
 		
 		
 		task.setParticipants(participants);
+		
+		List<UserParticipant> userParticipants = new ArrayList<UserParticipant>();
+		for(Participant p : participants) {
+			switch(p.getType()){
+			case Participant.USER :
+				userParticipants.add((UserParticipant)p);
+				break;
+			case Participant.GROUP :
+				int[] memberIds = G(p.getIdentifier())[0].getMember();
+				User[] asUsers = U(memberIds);
+				Participant[] userParticipantsFromGroup = getParticipants(asUsers, G(), S());
+				for(Participant up : userParticipantsFromGroup) {
+					userParticipants.add((UserParticipant)up);
+				}
+				break;
+			}
+		}
+		
+		task.setUsers(userParticipants);
+		
 		return task;
 	}
 	
@@ -234,10 +259,11 @@ public class ParticipantNotifyTest extends TestCase{
 		int i = 0;
 		
 		for(User user : users) {
-			Participant p = new UserParticipant();
+			UserParticipant p = new UserParticipant();
 			p.setDisplayName(user.getDisplayName());
 			p.setEmailAddress(user.getMail());
 			p.setIdentifier(user.getId());
+			p.setPersonalFolderId(user.getId()*100); // Imaginary
 			participants[i++] = p;
 		}
 		
@@ -320,11 +346,13 @@ public class ParticipantNotifyTest extends TestCase{
 		public String messageTitle;
 		public String message;
 		public List<String> addresses;
+		public int folderId;
 		
-		public Message(String messageTitle, String message, List<String>addresses) {
+		public Message(String messageTitle, String message, List<String>addresses, int folderId) {
 			this.messageTitle = messageTitle;
 			this.message = message;
 			this.addresses = addresses;
+			this.folderId = folderId;
 		}
 	}
 	
@@ -351,8 +379,8 @@ public class ParticipantNotifyTest extends TestCase{
 		}
 		
 		@Override
-		protected void sendMessage(String messageTitle, String message, List<String> name, SessionObject session, CalendarObject obj, State state) {
-			messageCollector.add(new Message(messageTitle,message,name));
+		protected void sendMessage(String messageTitle, String message, List<String> name, SessionObject session, CalendarObject obj, int folderId, State state) {
+			messageCollector.add(new Message(messageTitle,message,name, folderId));
 		}
 		
 		@Override
