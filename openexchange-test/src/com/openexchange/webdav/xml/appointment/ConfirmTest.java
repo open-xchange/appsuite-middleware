95d6c52cd6c12b9db98aea4bf4371cdfc078a6e8
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-01-30 11:17:46 +0000

    enbable confirm test

--- openexchange-test/src/com/openexchange/webdav/xml/appointment/ConfirmTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/appointment/ConfirmTest.java
@@ -2,6 +2,7 @@ package com.openexchange.webdav.xml.appointment;
 
 import com.openexchange.groupware.container.AppointmentObject;
 import com.openexchange.groupware.container.FolderObject;
+import com.openexchange.groupware.container.Participants;
 import com.openexchange.groupware.container.UserParticipant;
 import com.openexchange.webdav.xml.AppointmentTest;
 import com.openexchange.webdav.xml.FolderTest;
@@ -20,33 +21,33 @@ public class ConfirmTest extends AppointmentTest {
 		
 	}
 	
-    public void _notestConfirm() throws Exception {
+    public void testConfirm() throws Exception {
 		final FolderObject sharedFolderObject = FolderTest.getAppointmentDefaultFolder(getSecondWebConversation(), PROTOCOL + getHostName(), getSecondLogin(), getPassword());
 		final int secondUserId = sharedFolderObject.getCreatedBy();
 		
         AppointmentObject appointmentObj = createAppointmentObject("testConfirm");
 		appointmentObj.setIgnoreConflicts(true);
-		UserParticipant[] users = new UserParticipant[2];
-		users[0] = new UserParticipant();
-		users[0].setIdentifier(userId);
-		users[1] = new UserParticipant();
-		users[1].setIdentifier(secondUserId);
+		UserParticipant[] participants = new UserParticipant[2];
+		participants[0] = new UserParticipant();
+		participants[0].setIdentifier(userId);
+		participants[1] = new UserParticipant();
+		participants[1].setIdentifier(secondUserId);
 		
-		appointmentObj.setUsers(users);
+		appointmentObj.setParticipants(participants);
 
         int objectId = insertAppointment(getWebConversation(), appointmentObj, PROTOCOL + getHostName(), getLogin(), getPassword());
-        
+
         confirmAppointment(getSecondWebConversation(), objectId, AppointmentObject.ACCEPT, null, PROTOCOL + getHostName(), getSecondLogin(), getPassword());
 		
 		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getLogin(), getPassword());
 		
 		boolean found = false;
 		
-		users = loadAppointment.getUsers();
-		for (int a = 0; a < users.length; a++) {
-			if (users[a].getIdentifier() == secondUserId) {
+		participants = loadAppointment.getUsers();
+		for (int a = 0; a < participants.length; a++) {
+			if (participants[a].getIdentifier() == secondUserId) {
 				found = true;
-				assertEquals("wrong confirm status", AppointmentObject.ACCEPT, users[a].getConfirm());
+				assertEquals("wrong confirm status", AppointmentObject.ACCEPT, participants[a].getConfirm());
 			}
 		}
 		
