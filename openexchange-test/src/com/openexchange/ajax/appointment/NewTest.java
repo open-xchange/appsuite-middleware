1abca598b6034633f1550dbc13fa32b1afe5376c
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-01-30 10:29:52 +0000

    enable test with special characters

--- openexchange-test/src/com/openexchange/ajax/appointment/NewTest.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/NewTest.java
@@ -205,7 +205,7 @@ public class NewTest extends AppointmentTest {
 		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
 	}
 	
-	public void _notestSpecialCharacters() throws Exception {
+	public void testSpecialCharacters() throws Exception {
 		AppointmentObject appointmentObj = new AppointmentObject();
 		appointmentObj.setTitle("testSpecialCharacters - öäü-:,;.#?!§$%&/()=\"<>");
 		appointmentObj.setStartDate(new Date(startTime));
