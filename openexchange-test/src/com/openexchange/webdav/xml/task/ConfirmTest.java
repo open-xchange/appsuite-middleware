37af711db74d19fd5f61133473333f7e1ffd87bf
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-01-30 11:51:05 +0000

    enable the confirmTest

--- openexchange-test/src/com/openexchange/webdav/xml/task/ConfirmTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/task/ConfirmTest.java
@@ -1,7 +1,9 @@
 package com.openexchange.webdav.xml.task;
 
-import com.openexchange.groupware.container.CalendarObject;
+import com.openexchange.groupware.container.FolderObject;
+import com.openexchange.groupware.container.UserParticipant;
 import com.openexchange.groupware.tasks.Task;
+import com.openexchange.webdav.xml.FolderTest;
 import com.openexchange.webdav.xml.TaskTest;
 
 public class ConfirmTest extends TaskTest {
@@ -14,10 +16,36 @@ public class ConfirmTest extends TaskTest {
 		
 	}
 	
-	public void _notestConfirm() throws Exception {
-		Task taskObj = createTask("testConfirm");
-		int objectId = insertTask(webCon, taskObj, PROTOCOL + hostName, login, password);
-		confirmTask(webCon, objectId, CalendarObject.DECLINE, null, PROTOCOL + hostName, login, password);
+	public void testConfirm() throws Exception {
+		final FolderObject sharedFolderObject = FolderTest.getTaskDefaultFolder(getSecondWebConversation(), PROTOCOL + getHostName(), getSecondLogin(), getPassword());
+		final int secondUserId = sharedFolderObject.getCreatedBy();
+		
+        Task TaskObj = createTask("testConfirm");
+		UserParticipant[] participants = new UserParticipant[1];
+		participants[0] = new UserParticipant();
+		participants[0].setIdentifier(secondUserId);
+		
+		TaskObj.setParticipants(participants);
+
+        int objectId = insertTask(getWebConversation(), TaskObj, PROTOCOL + getHostName(), getLogin(), getPassword());
+
+        confirmTask(getSecondWebConversation(), objectId, Task.ACCEPT, null, PROTOCOL + getHostName(), getSecondLogin(), getPassword());
+		
+		Task loadTask = loadTask(getWebConversation(), objectId, taskFolderId, PROTOCOL + getHostName(), getLogin(), getPassword());
+		
+		boolean found = false;
+		
+		participants = loadTask.getUsers();
+		for (int a = 0; a < participants.length; a++) {
+			if (participants[a].getIdentifier() == secondUserId) {
+				found = true;
+				assertEquals("wrong confirm status", Task.ACCEPT, participants[a].getConfirm());
+			}
+		}
+		
+		assertTrue("user participant with id " + secondUserId + " not found", found);
+		
+        deleteTask(getWebConversation(), new int[][] { { objectId, taskFolderId } }, PROTOCOL + getHostName(), getLogin(), getPassword());
 	}
 	
 }
