015fc3733a6484fe21a8f76daf5034d0b26db147
tierlieb <tierlieb>
2007-02-15 12:58:32 +0000

    changed interfaces importer, exporter and their users. added license text.

--- src/com/openexchange/groupware/importexport/ImporterExporter.java
+++ src/com/openexchange/groupware/importexport/ImporterExporter.java
@@ -1,9 +1,59 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
 package com.openexchange.groupware.importexport;
 
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.openexchange.groupware.OXThrowsMultiple;
@@ -13,15 +63,17 @@ import com.openexchange.groupware.importexport.exceptions.ImportExportExceptionF
 import com.openexchange.sessiond.SessionObject;
 
 @OXThrowsMultiple(
-		category = { 
+	category = { 
 		Category.SUBSYSTEM_OR_SERVICE_DOWN,
 		Category.SUBSYSTEM_OR_SERVICE_DOWN
-		}, 
-		desc = { "" , ""}, 
-		exceptionId = { 0 , 1 }, 
-		msg = { 
+	}, 
+	desc = { "" , ""}, 
+	exceptionId = { 0 , 1 }, 
+	msg = { 
 		"Cannot find an importer for format %s into folders %s",
-		"Cannot find an exporter for folder %s to format %s" })
+		"Cannot find an exporter for folder %s to format %s" 
+	}
+)
 
 
 /** 
@@ -80,13 +132,13 @@ public class ImporterExporter {
 	 * @param is: InputStream containing the data to be imported
 	 * @param folders: One or more folders the data is to be imported to (usually one, but iCal may contain both tasks and appointments and future formats might contain even more)
 	 */
-	public List<ImportResult> importData(final SessionObject sessObj, final Format format, final InputStream is, final String... folders) throws ImportExportException{
+	public List<ImportResult> importData(final SessionObject sessObj, final Format format, final InputStream is, final Map<String, Integer> folderMapping) throws ImportExportException{
 		for(Importer imp : importers){
-			if(imp.canImport(sessObj, format, folders)){
-				return imp.importData(sessObj, format, is, folders);
+			if(imp.canImport(sessObj, format, folderMapping)){
+				return imp.importData(sessObj, format, is, folderMapping);
 			}
 		}
-		throw EXCEPTIONS.create(0, format, folders);
+		throw EXCEPTIONS.create(0, format, folderMapping.keySet());
 	}
 
 	/**
@@ -99,24 +151,30 @@ public class ImporterExporter {
 	 * @return InputStream containing the exported data in given format
 	 * @throws ImportExportException in case of a missing exporter for that kind of data 
 	 */
-	public InputStream exportData(final SessionObject sessObj, final Format format, final String folder, final String...fieldsToBeExported) throws ImportExportException{
+	public InputStream exportData(final SessionObject sessObj, final Format format, final String folder, final int type, final String...fieldsToBeExported) throws ImportExportException{
 		for(Exporter exp: exporters){
-			if(exp.canExport(sessObj, format, folder)){
-				return exp.exportData(sessObj, format, folder, fieldsToBeExported);
+			if(exp.canExport(sessObj, format, folder, type)){
+				return exp.exportData(sessObj, format, folder, type, fieldsToBeExported);
 			}
 		}
 		throw EXCEPTIONS.create(1, folder, format);
 	}
 
 	/**
-	 * Exports data of one object in one folder
+	 * Exports the data of one object in one folder
 	 * 
-	 * @param sessObj: Session object used to determine access rights
-	 * @param objectId: Identifier for one object in a folder
+	 * @param sessObj
+	 * @param format
+	 * @param folder
+	 * @param type
+	 * @param objectId
+	 * @param fieldsToBeExported
+	 * @return
+	 * @throws ImportExportException
 	 */
-	public InputStream exportData(final SessionObject sessObj, final Format format, final String folder, final int objectId, final String...fieldsToBeExported) throws ImportExportException{
+	public InputStream exportData(final SessionObject sessObj, final Format format, final String folder, final int type, final int objectId, final String...fieldsToBeExported) throws ImportExportException{
 		for(Exporter exp: exporters){
-			if(exp.canExport(sessObj, format, folder)){
+			if(exp.canExport(sessObj, format, folder, type)){
 				return exp.exportData(sessObj, format, folder, objectId, fieldsToBeExported);
 			}
 		}
@@ -129,14 +187,15 @@ public class ImporterExporter {
 	 * 
 	 * @param sessObj: Session object used to determine access rights
 	 * @param folder: Identifier of a certain folder within the OX
+	 * @param type: Type of the folder as defined in class Types
 	 * @return A set of possible formats this folder can be exported to
 	 */
-	public Set<Format> getPossibleExportFormats(final SessionObject sessObj, final String folder){
+	public Set<Format> getPossibleExportFormats(final SessionObject sessObj, final String folder, final int type){
 		Set<Format> res = new HashSet<Format>();
 		
 		for(Format format: Format.values()){
 			for(Exporter exp: exporters){
-				if(exp.canExport(sessObj, format, folder)){
+				if(exp.canExport(sessObj, format, folder, type)){
 					res.add(format);
 					break;
 				}
@@ -149,15 +208,15 @@ public class ImporterExporter {
 	 * Lists all formats a folder can import data from
 	 *
 	 * @param sessObj: Session object used to determine access rights
-	 * @param folder: Identifier of a certain folder within the OX
+	 * @param folderMapping: Identifier of a certain folder (plus its type as defined in class Type) within the OX
 	 * @return A set of possible formats this folder can import
 	 */
-	public Set<Format> getPossibleImportFormats(final SessionObject sessObj, final String folder){
+	public Set<Format> getPossibleImportFormats(final SessionObject sessObj, final Map<String, Integer> folderMapping){
 		Set<Format> res = new HashSet<Format>();
 		
 		for(Format format: Format.values()){
 			for(Importer imp: importers){
-				if(imp.canImport(sessObj, format, folder)){
+				if(imp.canImport(sessObj, format, folderMapping)){
 					res.add(format);
 					break;
 				}
