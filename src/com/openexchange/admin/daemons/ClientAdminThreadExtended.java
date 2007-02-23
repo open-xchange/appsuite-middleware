4af8bb7b547892c6f8f693ca3ac5b3243e8733ba
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-02-23 15:59:52 +0000

    - made this class extend from clientadminthread instead of thread - add new cache variable which overrides cache from old class

--- src/com/openexchange/admin/daemons/ClientAdminThreadExtended.java
+++ src/com/openexchange/admin/daemons/ClientAdminThreadExtended.java
@@ -1,7 +1,9 @@
 package com.openexchange.admin.daemons;
 
 import com.openexchange.admin.AdminJobExecutorGlobalInterface;
+import com.openexchange.admin.tools.AdminCacheExtended;
 
-public class ClientAdminThreadExtended extends Thread {
+public class ClientAdminThreadExtended extends ClientAdminThread {
+    public static AdminCacheExtended      cache       = null;
     public static AdminJobExecutorGlobalInterface ajx = null;
 }
