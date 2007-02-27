88b190653fb8ac4f659ea470701de18885351c36
tierlieb <tierlieb>
2007-02-27 13:17:58 +0000

    Helpers to parse and handle CSV

---
+++ src/com/openexchange/groupware/importexport/csv/CSVLibrary.java
@@ -0,0 +1,164 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.groupware.importexport.csv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import com.openexchange.api2.OXException;
+import com.openexchange.cache.FolderCacheManager;
+import com.openexchange.groupware.Component;
+import com.openexchange.groupware.OXExceptionSource;
+import com.openexchange.groupware.OXThrowsMultiple;
+import com.openexchange.groupware.AbstractOXException.Category;
+import com.openexchange.groupware.contact.Contacts;
+import com.openexchange.groupware.container.FolderObject;
+import com.openexchange.groupware.importexport.exceptions.ImportExportException;
+import com.openexchange.groupware.importexport.exceptions.ImportExportExceptionClasses;
+import com.openexchange.groupware.importexport.exceptions.ImportExportExceptionFactory;
+import com.openexchange.sessiond.SessionObject;
+
+@OXExceptionSource(
+	classId=ImportExportExceptionClasses.CSVLIBRARY, 
+	component=Component.IMPORT_EXPORT)
+@OXThrowsMultiple(
+	category={
+		Category.SUBSYSTEM_OR_SERVICE_DOWN,
+		Category.PROGRAMMING_ERROR,
+		Category.PROGRAMMING_ERROR}, 
+	desc={"","",""}, 
+	exceptionId={0,1,2}, 
+	msg={
+		"Could not load folder %s",
+		"Could not create folderId from String %s",
+		"Could not read InputStream as String"})
+public class CSVLibrary {
+	protected static ImportExportExceptionFactory EXCEPTIONS = new ImportExportExceptionFactory(CSVLibrary.class);
+	public static final char CELL_DELIMITER = ',';
+	public static final char ROW_DELIMITER = '\n';
+
+	public static FolderObject getFolderObject(SessionObject sessObj, String folder) throws ImportExportException {
+		final int folderId = getFolderId(folder);
+		FolderObject fo = null;
+		try {
+			if(FolderCacheManager.isEnabled()){
+				fo = FolderCacheManager.getInstance().getFolderObject(folderId, sessObj.getContext());
+			} else {
+				fo = FolderObject.loadFolderObjectFromDB(folderId, sessObj.getContext());
+			}
+		} catch (OXException e) {
+			throw EXCEPTIONS.create(0, folder);
+		}
+		return fo;
+	}
+
+	public static int getFolderId(String folderString) throws ImportExportException {
+		try{
+			return new Integer(folderString).intValue();
+		} catch (NumberFormatException e) {
+			throw EXCEPTIONS.create(1, folderString);
+		}
+	}
+
+	public static List<String> convertToList(int[] cols) {
+		List<String> l = new LinkedList<String>();
+		for(int col : cols){
+			l.add( Contacts.mapping[col].getReadableTitle() );
+		}
+		return l;
+	}
+
+	/**
+	 * ...because Java5, basic data types and Arrays don't mix
+	 */
+	public static Set<Integer> transformIntArrayToSet(int[] arr) {
+		LinkedHashSet<Integer> s = new LinkedHashSet<Integer>();
+		for(int val : arr){
+			s.add(val);
+		}
+		return s;
+	}
+
+	/**
+	 * ...because Java5, basic data types and Arrays don't mix
+	 */
+	public static int[] transformSetToIntArray(Set<Integer> s) {
+		int[] ret = new int[s.size()];
+		int i = 0;
+		for(Integer val : s){
+			ret[i++] = val;
+		}
+		return ret;
+	}
+	
+	public static String transformInputStreamToString(InputStream is) throws ImportExportException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		StringBuilder bob = new StringBuilder();
+		String buffer;
+		try {
+			while( (buffer = br.readLine()) != null){
+				bob.append(buffer);
+				bob.append('\n');
+			}
+		} catch (IOException e) {
+			throw EXCEPTIONS.create(2);
+		} finally {
+			try { br.close(); } catch (IOException e) { /*already closed*/}
+		}
+		return bob.toString();
+	}
+
+}
---
+++ src/com/openexchange/groupware/importexport/csv/CSVParser.java
@@ -0,0 +1,173 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.groupware.importexport.csv;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.openexchange.groupware.Component;
+import com.openexchange.groupware.OXExceptionSource;
+import com.openexchange.groupware.OXThrowsMultiple;
+import com.openexchange.groupware.AbstractOXException.Category;
+import com.openexchange.groupware.importexport.exceptions.ImportExportException;
+import com.openexchange.groupware.importexport.exceptions.ImportExportExceptionClasses;
+import com.openexchange.groupware.importexport.exceptions.ImportExportExceptionFactory;
+
+/**
+ * This class represents a combined parser and lexer for CSV files.
+ * It is designed rather simple with speed in mind. 
+ * 
+ * Note: It does not do any guessing, so make sure it gets a proper 
+ * CSV file with the dimensions M x N.
+ * 
+ * Note: See also RFC 4180
+ * 
+ * @author <a href="mailto:tobias.prinz@open-xchange.com">Tobias 'Tierlieb' Prinz</a>
+ *
+ */
+
+@OXExceptionSource(
+	classId=ImportExportExceptionClasses.CSVPARSER, 
+	component=Component.IMPORT_EXPORT)
+@OXThrowsMultiple(
+	category={Category.USER_INPUT, Category.PROGRAMMING_ERROR}, 
+	desc={"",""}, 
+	exceptionId={0,1}, 
+	msg={
+		"Broken CSV file: Lines have different number of cells, line #1 has %d, line #%d has %d",
+		"Illegal state: Found data after presumed last line."})
+public class CSVParser {
+	private static final ImportExportExceptionFactory EXCEPTIONS = new ImportExportExceptionFactory(CSVParser.class);
+	private static boolean isEscaping;
+	public static final char LINE_DELIMITER = '\n';
+	public static final char CELL_DELIMITER = ',';
+	public static final char ESCAPER = '"';
+	
+	public static List< List<String> > parse(String file) throws ImportExportException{
+		//changing all possible formats (Mac, DOS) to Unix
+		file = file.replace("\r\n", "\n").replace("\r", "\n");
+		//adding ending to create well-formed file
+		if(! file.endsWith("\n")){
+			file = file + "\n";
+		}
+		//converting to char array to make it iterable
+		char[] arr = file.toCharArray();
+		//preparations
+		int numberOfCells = -1, currentLineNumber = 0;
+		StringBuilder currentCell = new StringBuilder();
+		List<String> currentLine = new LinkedList<String>();
+		List< List<String> > structure = new LinkedList<List <String> >();
+		
+		for(int i = 0; i < arr.length; i++){
+			switch(arr[i]){
+				case LINE_DELIMITER: 
+					if(isEscaping){
+						currentCell.append(LINE_DELIMITER);
+					} else {
+						currentLineNumber++;
+						currentLine.add( currentCell.toString().trim() );
+						currentCell = new StringBuilder();
+						if(numberOfCells == -1 ){
+							numberOfCells = currentLine.size();
+						} else if(numberOfCells != currentLine.size() ){
+							throw EXCEPTIONS.create(0, numberOfCells, currentLineNumber, currentLine.size());
+						}
+						structure.add(currentLine);
+						currentLine = new LinkedList<String>();
+					}
+				break;
+				
+				case ESCAPER: 
+					if(isEscaping){
+						if( (i+1) < arr.length && arr[i+1] == ESCAPER){
+							currentCell.append(ESCAPER);
+							i++;
+						} else {
+							isEscaping = false;
+						}
+					} else {
+						isEscaping = true;
+					}
+				break;
+				
+				case CELL_DELIMITER:
+					if(isEscaping){
+						currentCell.append(CELL_DELIMITER);
+					} else {
+						currentLine.add( currentCell.toString().trim() );
+						currentCell = new StringBuilder();
+					}
+				break;
+				
+				default:
+					currentCell.append(arr[i]);
+				break;
+			}
+		}
+		
+		if( ! (currentCell.length() == 0 && currentLine.size() == 0) ){
+			throw EXCEPTIONS.create(1);
+		}
+		/*if(currentCell.length() != 0){
+			currentLine.add( currentCell.toString().trim() );
+		}
+		if(currentLine.size() != 0){
+			currentLineNumber++;
+			if(numberOfCells == -1 ){
+				numberOfCells = currentLine.size();
+			} else if(numberOfCells != currentLine.size() ){
+				throw EXCEPTIONS.create(0, numberOfCells, currentLineNumber, currentLine.size());
+			}
+			structure.add( currentLine );
+		}*/
+		
+		return structure;
+	}
+}
