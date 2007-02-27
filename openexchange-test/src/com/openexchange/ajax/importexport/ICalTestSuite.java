7be540238e6244093dc871bd21ba8e9bcb7c9ed6
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-02-27 15:05:23 +0000

    add ICalTestSuite

---
+++ openexchange-test/src/com/openexchange/ajax/importexport/ICalTestSuite.java
@@ -0,0 +1,16 @@
+package com.openexchange.ajax.importexport;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class ICalTestSuite extends TestSuite{
+	
+	public static Test suite(){
+		TestSuite tests = new TestSuite();
+		tests.addTestSuite( ICalImportTest.class );
+		tests.addTestSuite( ICalExportTest.class );
+
+		return tests;
+	}
+}
