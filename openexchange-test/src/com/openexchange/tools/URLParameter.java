823e7c0229619436efbc43caf95da61d664cd573
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-02-27 12:32:43 +0000

    add new implementation to handle multi values

--- openexchange-test/src/com/openexchange/tools/URLParameter.java
+++ openexchange-test/src/com/openexchange/tools/URLParameter.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.openexchange.tools;
 
@@ -9,65 +9,118 @@ import java.util.Iterator;
 import java.util.Map;
 
 import com.openexchange.tools.encoding.URLCoder;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * This class provides methods to generate URL parameters.
  * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
  */
 public class URLParameter {
-
-    /**
-     * Container for the parameters.
-     */
-    private final transient Map parameters;
-    
-    /**
-     * Default constructor.
-     */
-    public URLParameter() {
-        super();
-        parameters = new HashMap();
-    }
-
-    public void setParameter(final String name, final String value) {
-        parameters.put(name, value);
-    }
 	
-    public void setParameter(final String name, final int value) {
-        parameters.put(name, String.valueOf(value));
-    }
+	/**
+	 * enables multivalues for the url parameter
+	 **/
+	private boolean multivalue = false;
 	
-    public void setParameter(final String name, final long value) {
-        parameters.put(name, String.valueOf(value));
-    }
+	/**
+	 * Container for the parameters.
+	 */
+	private final transient Map parameters;
 	
-    public void setParameter(final String name, final boolean value) {
-        parameters.put(name, String.valueOf(value));
-    }
+	/**
+	 * Container for the parameters with multi values
+	 */
+	private final transient Map<String, List<String>> multivalueParameters;
 	
-    public void setParameter(final String name, final Date value) {
-        parameters.put(name, String.valueOf(value.getTime()));
-    }
-
-
-    public String getURLParameters() throws UnsupportedEncodingException {
-        final StringBuffer sb = new StringBuffer();
-        if (parameters.size() > 0) {
-            sb.append('?');
-        }
-        final Iterator iter = parameters.keySet().iterator();
-        while (iter.hasNext()) {
-            final String name = (String) iter.next();
-            sb.append(URLCoder.encode(name, "UTF-8"));
-            sb.append('=');
-            sb.append(URLCoder.encode((String) parameters.get(name), "UTF-8"));
-            if (iter.hasNext()) {
-                sb.append('&');
-            }
-        }
-        return sb.toString();
-    }
+	/**
+	 * Default constructor.
+	 */
+	public URLParameter() {
+		super();
+		parameters = new HashMap();
+		multivalueParameters = new HashMap<String, List<String>>();
+	}
+	
+	public URLParameter(boolean multivalue) {
+		super();
+		this.multivalue = multivalue;
+		parameters = new HashMap();
+		multivalueParameters = new HashMap<String, List<String>>();
+	}
+	
+	public void setParameter(final String name, final String value) {
+		setParameter2Map(name, value);
+	}
+	
+	public void setParameter(final String name, final int value) {
+		setParameter2Map(name, String.valueOf(value));
+	}
+	
+	public void setParameter(final String name, final long value) {
+		setParameter2Map(name, String.valueOf(value));
+	}
+	
+	public void setParameter(final String name, final boolean value) {
+		setParameter2Map(name, String.valueOf(value));
+	}
+	
+	public void setParameter(final String name, final Date value) {
+		setParameter2Map(name, String.valueOf(value.getTime()));
+	}
+	
+	private void setParameter2Map(final String name, final String value) {
+		if (multivalue) {
+			if (multivalueParameters.containsKey(name)) {
+				List l = multivalueParameters.get(name);
+				l.add(value);
+			} else {
+				List<String> l = new ArrayList<String>();
+				l.add(value);
+				
+				multivalueParameters.put(name, l);
+			}
+		} else {
+			parameters.put(name, value);
+		}
+	}
+	
+	public String getURLParameters() throws UnsupportedEncodingException {
+		final StringBuffer sb = new StringBuffer();
+		if (parameters.size() > 0) {
+			sb.append('?');
+		}
+		
+		if (multivalue) {
+			final Iterator iter = parameters.keySet().iterator();
+			while (iter.hasNext()) {
+				final String name = (String) iter.next();
+				sb.append(URLCoder.encode(name, "UTF-8"));
+				sb.append('=');
+				sb.append(URLCoder.encode((String) parameters.get(name), "UTF-8"));
+				if (iter.hasNext()) {
+					sb.append('&');
+				}
+			}
+		} else {
+			final Iterator iter = multivalueParameters.keySet().iterator();
+			while (iter.hasNext()) {
+				final String name = (String) iter.next();
+				final List l = multivalueParameters.get(name);
+				
+				for (int a = 0; a < l.size(); a++) {
+					sb.append(URLCoder.encode(name, "UTF-8"));
+					sb.append('=');
+					sb.append(URLCoder.encode((String) l.get(a), "UTF-8"));
+					if (iter.hasNext()) {
+						sb.append('&');
+					}
+				}
+			}
+		}
+		return sb.toString();
+	}
 	
 	public static String colsArray2String(int cols[]) {
 		StringBuffer sb = new StringBuffer();
@@ -76,7 +129,8 @@ public class URLParameter {
 				sb.append(cols[a]);
 			} else {
 				// sb.append("%2C" + cols[a]);
-				sb.append("," + cols[a]);
+				sb.append(',');
+				sb.append(cols[a]);
 			}
 		}
 		
