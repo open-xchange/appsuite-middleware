b1a3a6a90d77c51f9ac4006d08449cbe6d710d5d
Tobias Prinz <tobias.prinz@open-xchange.com>
2007-02-28 12:11:03 +0000

    test for a full roundtrip of an CSV file

---
+++ openexchange-test/src/com/openexchange/ajax/ImportExportServletTest.java
@@ -0,0 +1,207 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.xml.sax.SAXException;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.PutMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import com.openexchange.ajax.container.Response;
+import com.openexchange.api2.OXException;
+import com.openexchange.groupware.Init;
+import com.openexchange.groupware.Types;
+import com.openexchange.groupware.contact.ContactConfig;
+import com.openexchange.groupware.contact.helpers.ContactField;
+import com.openexchange.groupware.container.FolderObject;
+import com.openexchange.groupware.contexts.ContextImpl;
+import com.openexchange.groupware.importexport.AbstractCSVContactTest;
+import com.openexchange.groupware.importexport.CSVContactImportTest;
+import com.openexchange.groupware.importexport.Format;
+import com.openexchange.groupware.importexport.ModuleTypeTranslator;
+import com.openexchange.groupware.importexport.csv.CSVParser;
+import com.openexchange.groupware.ldap.UserStorage;
+import com.openexchange.server.OCLPermission;
+import com.openexchange.sessiond.SessionObject;
+import com.openexchange.sessiond.SessionObjectWrapper;
+import com.openexchange.tools.oxfolder.OXFolderException;
+import com.openexchange.webdav.xml.FolderTest;
+
+
+/**
+ * Test of the ImporterExporter servlet. 
+ * 
+ * @author <a href="mailto:tobias.prinz@open-xchange.com">Tobias 'Tierlieb' Prinz</a>
+ *
+ */
+public class ImportExportServletTest extends AbstractAJAXTest {
+	private SessionObject sessObj;
+	public static final String FOLDER_NAME = "csv-contact-roundtrip-ajax-test";
+	public static final String IMPORTED_CSV = CSVContactImportTest.IMPORT_MULTIPLE;
+	
+	public ImportExportServletTest(String name){
+		super(name);
+	}
+	
+	public void setUp() throws Exception{
+		super.setUp();
+		Init.initDB();
+		ContactConfig.init();
+		final UserStorage uStorage = UserStorage.getInstance(new ContextImpl(1));
+	    final int userId = uStorage.getUserId( Init.getAJAXProperty("login") );
+		sessObj = SessionObjectWrapper.createSessionObject(userId, 1, "csv-roundtrip-test");
+	}
+	
+	public void tearDown() throws Exception{
+		super.tearDown();
+	}
+	
+	public void testCSVRoundtrip() throws Exception{
+		//preparations
+		final int type = Types.CONTACT;
+		final String insertedCSV = IMPORTED_CSV;
+		final Format format = Format.CSV;
+		final int folderId = createFolder("csv-contact-roundtrip-" + System.currentTimeMillis(),FolderObject.CONTACT);
+		
+		
+		//test: import
+		InputStream is = new ByteArrayInputStream(insertedCSV.getBytes());
+		WebConversation webconv = getWebConversation();
+		WebRequest req = new PutMethodWebRequest(
+				getUrl(folderId, type, format), 
+				is, 
+				Format.CSV.getMimeType());
+		WebResponse webRes = webconv.sendRequest(req);
+		//Response res = Response.parse(webRes.getText());
+		
+		//test: export
+		webconv =  getWebConversation();
+		req = new GetMethodWebRequest( getUrl(folderId, type, format) );
+		webRes = webconv.sendRequest(req);
+		is = webRes.getInputStream();
+		String resultingCSV = AbstractCSVContactTest.readStreamAsString(is);
+		//finally: checking
+		assertEquals("input == output ?" , CSVParser.parse(insertedCSV) , CSVParser.parse(resultingCSV));
+		
+		//clean up
+		removeFolder(folderId);
+	}
+	
+	private int getUserId_FIXME() throws MalformedURLException, OXException, IOException, SAXException, JSONException {
+		final FolderObject folderObj = com.openexchange.ajax.FolderTest
+		.getStandardCalendarFolder(getWebConversation(),
+		getHostName(), getSessionId());
+
+		return folderObj.getCreatedBy();
+	}
+
+	public String getUrl(int folderId, int type, Format format) throws IOException, SAXException, JSONException{
+		StringBuilder bob = new StringBuilder("http://");
+		bob.append(getHostName());
+		bob.append("/ajax/importexport?session=");
+		bob.append(getSessionId());
+		addParam(bob, ImportExport.PARAMETER_FOLDERID, folderId ) ;
+		addParam(bob, ImportExport.AJAX_TYPE, type);
+		addParam(bob, ImportExport.PARAMETER_CONTENT_TYPE, format.getMimeType());
+		addParam(bob, ImportExport.PARAMETER_COLUMNS, ContactField.GIVEN_NAME.getNumber());
+		addParam(bob, ImportExport.PARAMETER_COLUMNS, ContactField.EMAIL1.getNumber());
+		return bob.toString();
+	}
+	
+	private void addParam(StringBuilder bob, String param, String value){
+		bob.append("&");
+		bob.append(param);
+		bob.append("=");
+		bob.append(value);
+	}
+	
+	private void addParam(StringBuilder bob, String param, int value){
+		addParam(bob, param, Integer.toString(value));
+	}
+	
+	private int createFolder(String title, int folderObjectModuleID) throws Exception{
+		FolderObject folderObj = new FolderObject();
+		folderObj.setFolderName(title);
+		folderObj.setParentFolderID(FolderObject.PRIVATE);
+		folderObj.setModule(folderObjectModuleID);
+		folderObj.setType(FolderObject.PRIVATE);
+		
+		OCLPermission[] permission = new OCLPermission[] {
+			FolderTest.createPermission( getUserId_FIXME(), false, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION),
+		};
+		
+		folderObj.setPermissionsAsArray( permission );
+		try{
+			return FolderTest.insertFolder(getWebConversation(), folderObj, getHostName(), getLogin(), getPassword());
+		} catch(OXFolderException e){
+			return -1;
+		}
+	}
+	
+	private void removeFolder( int folderId) throws OXException, Exception{
+		if(folderId == -1){
+			return;
+		}
+		FolderTest.deleteFolder(getWebConversation(), new int[] { folderId }, getHostName(), getLogin(), getPassword());
+	}
+	
+
+	
+	
+	
+}
