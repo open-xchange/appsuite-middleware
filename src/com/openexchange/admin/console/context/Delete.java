7f8fc0bed8762648df7dc07826a9d4ebbeab5b32
Manuel Kraft <manuel.kraft@open-xchange.com>
2007-02-28 14:06:44 +0000

    implemented

--- src/com/openexchange/admin/console/context/Delete.java
+++ src/com/openexchange/admin/console/context/Delete.java
@@ -28,12 +28,9 @@ import com.openexchange.admin.rmi.exceptions.InvalidDataException;
 import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
 import com.openexchange.admin.rmi.exceptions.StorageException;
 
-public class Create extends UserAbstraction {
+public class Delete extends UserAbstraction {     
     
-    private final static String OPT_QUOTA_SHORT = "q";
-    private final static String OPT_QUOTA_LONG = "quota";
-    
- public Create(String[] args2) {
+ public Delete(String[] args2) {
         
         CommandLineParser parser = new PosixParser();
 
@@ -42,33 +39,16 @@ public class Create extends UserAbstraction {
         try {
             
             CommandLine cmd = parser.parse(options, args2);            
-            Context ctx = new Context(DEFAULT_CONTEXT);
-            
-            if(cmd.hasOption(OPT_NAME_CONTEXT_SHORT)){
-                ctx.setID(Integer.parseInt(cmd.getOptionValue(OPT_NAME_CONTEXT_SHORT)));
-            }
+            Context ctx = new Context();
+                      
+            ctx.setID(Integer.parseInt(cmd.getOptionValue(OPT_NAME_CONTEXT_SHORT)));            
             
             Credentials auth = new Credentials(cmd.getOptionValue(OPT_NAME_ADMINUSER_SHORT),cmd.getOptionValue(OPT_NAME_ADMINPASS_SHORT));
-            
-            
+                        
             // get rmi ref
             OXContextInterface oxres = (OXContextInterface)Naming.lookup(OXContextInterface.RMI_NAME);
-            
-            // create user obj
-            User usr = new User();
-            
-            // fill user obj with mandatory values from console
-            usr.setUsername(cmd.getOptionValue(OPT_USERNAME_SHORT));
-            usr.setDisplay_name(cmd.getOptionValue(OPT_DISPLAYNAME_SHORT));
-            usr.setGiven_name(cmd.getOptionValue(OPT_GIVENNAME_SHORT));
-            usr.setSur_name(cmd.getOptionValue(OPT_SURNAME_SHORT));
-            usr.setPassword(cmd.getOptionValue(OPT_PASSWORD_SHORT));
-            usr.setPrimaryEmail(cmd.getOptionValue(OPT_PRIMARY_EMAIL_SHORT));
-            usr.setEmail1(cmd.getOptionValue(OPT_PRIMARY_EMAIL_SHORT));
-            
-            long quota = Long.parseLong(cmd.getOptionValue(OPT_QUOTA_SHORT));            
                        
-            oxres.create(ctx, usr, quota, auth);
+            oxres.delete(ctx,auth);
             
         }catch(java.rmi.ConnectException neti){
             printError(neti.getMessage());            
@@ -76,13 +56,13 @@ public class Create extends UserAbstraction {
             printInvalidInputMsg("Ids must be numbers!");
         }catch(org.apache.commons.cli.MissingArgumentException as){
             printError("Missing arguments on the command line: " + as.getMessage());;
-            printHelpText("create", options);
+            printHelpText("delete", options);
         }catch(org.apache.commons.cli.UnrecognizedOptionException ux){
             printError("Unrecognized options on the command line: " + ux.getMessage());;
-            printHelpText("create", options);
+            printHelpText("delete", options);
         } catch (org.apache.commons.cli.MissingOptionException mis) {
             printError("Missing options on the command line: " + mis.getMessage());;
-            printHelpText("create", options);
+            printHelpText("delete", options);
         } catch (ParseException e) {
             e.printStackTrace();
         } catch (MalformedURLException e) {            
@@ -94,15 +74,15 @@ public class Create extends UserAbstraction {
         } catch (StorageException e) {            
             printServerResponse(e.getMessage());
         } catch (InvalidCredentialsException e) {
-            printServerResponse(e.getMessage());        
-        } catch (InvalidDataException e) {            
+            printServerResponse(e.getMessage());
+        } catch (NoSuchContextException e) {
             printServerResponse(e.getMessage());
         }
 
     }
 
     public static void main(String args[]){
-        new Create(args);
+        new Delete(args);
     }
     
     private Options getOptions() {
@@ -111,18 +91,7 @@ public class Create extends UserAbstraction {
         // this time context id is mandatory
         Option tmp = retval.getOption(OPT_NAME_CONTEXT_SHORT);
         tmp.setRequired(true);
-        retval.addOption(tmp);
-        
-        // add mandattory  options
-        retval.addOption(getUsernameOption());
-        retval.addOption(getDisplayNameOption());
-        retval.addOption(getGivenNameOption());
-        retval.addOption(getSurNameOption());
-        retval.addOption(getPasswordOption());
-        retval.addOption(getPrimaryMailOption());
-        
-        Option quota = getShortLongOpt(OPT_QUOTA_SHORT, OPT_QUOTA_LONG, "How much quota the context can use for filestore", true, true);
-        retval.addOption(quota);
+        retval.addOption(tmp);                
         
         return retval;
     }
