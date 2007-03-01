d7b53ad30fe9380e22942e14f0fcb4f3750c77c9
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-03-01 13:45:45 +0000

    - first and hopefully finished version of unregisterFilestore

--- src/com/openexchange/admin/console/util/UnregisterFilestore.java
+++ src/com/openexchange/admin/console/util/UnregisterFilestore.java
@@ -1,5 +1,89 @@
 package com.openexchange.admin.console.util;
 
-public class UnregisterFilestore {
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+import com.openexchange.admin.rmi.OXUtilInterface;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+
+
+public class UnregisterFilestore extends UtilAbstraction {
+
+    private final static String GENERAL_UTILITY_NAME = "unregisterFilestore";
+
+    // Setting names for options
+    private final static String OPT_NAME_STORE_ID_SHORT = "i";
+    private final static String OPT_NAME_STORE_ID_LONG = "id";
+
+    public UnregisterFilestore(final String[] args2) {
+
+        final CommandLineParser parser = new PosixParser();
+
+        final Options options = getOptions();
+
+        try {
+            final CommandLine cmd = parser.parse(options, args2);
+
+            final Credentials auth = new Credentials(cmd.getOptionValue(OPT_NAME_ADMINUSER_SHORT), cmd.getOptionValue(OPT_NAME_ADMINPASS_SHORT));
+
+            // get rmi ref
+            final OXUtilInterface oxutil = (OXUtilInterface) Naming.lookup(OXUtilInterface.RMI_NAME);
+
+            final String store_id = cmd.getOptionValue(OPT_NAME_STORE_ID_SHORT);
+
+            oxutil.unregisterFilestore(Integer.parseInt(store_id), auth);
+        } catch (final java.rmi.ConnectException neti) {
+            printError(neti.getMessage());
+        } catch (final java.lang.NumberFormatException num) {
+            printInvalidInputMsg("Ids must be numbers!");
+        } catch (final org.apache.commons.cli.MissingArgumentException as) {
+            printError("Missing arguments on the command line: " + as.getMessage());
+            printHelpText(GENERAL_UTILITY_NAME, options);
+        } catch (final org.apache.commons.cli.UnrecognizedOptionException ux) {
+            printError("Unrecognized options on the command line: " + ux.getMessage());
+            printHelpText(GENERAL_UTILITY_NAME, options);
+        } catch (final org.apache.commons.cli.MissingOptionException mis) {
+            printError("Missing options on the command line: " + mis.getMessage());
+            printHelpText(GENERAL_UTILITY_NAME, options);
+        } catch (final ParseException e) {
+            e.printStackTrace();
+        } catch (final MalformedURLException e) {
+            printServerResponse(e.getMessage());
+        } catch (final RemoteException e) {
+            printServerResponse(e.getMessage());
+        } catch (final NotBoundException e) {
+            printServerResponse(e.getMessage());
+        } catch (final StorageException e) {
+            printServerResponse(e.getMessage());
+        } catch (final InvalidCredentialsException e) {
+            printServerResponse(e.getMessage());
+        } catch (final InvalidDataException e) {
+            printServerResponse(e.getMessage());
+        }
+
+    }
+
+    public static void main(final String args[]) {
+        new UnregisterFilestore(args);
+    }
+
+    private Options getOptions() {
+        final Options retval = getDefaultCommandLineOptions();
+
+        retval.addOption(addDefaultArgName(getShortLongOpt(OPT_NAME_STORE_ID_SHORT, OPT_NAME_STORE_ID_LONG, "the id of the filestore which should be deleted", true, true)));
+        return retval;
+    }
+
 
 }
