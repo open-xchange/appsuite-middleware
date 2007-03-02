ffc35cd97cffd4b4731cdf55edbb75caae7a0e7e
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-03-02 11:09:25 +0000

    - some code refactoring

--- com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/util/UtilAbstraction.java
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/util/UtilAbstraction.java
@@ -1,5 +1,6 @@
 package com.openexchange.admin.console.util;
 
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
@@ -7,9 +8,41 @@ import com.openexchange.admin.console.BasicCommandlineOptions;
 
 public class UtilAbstraction extends BasicCommandlineOptions {
     
+    //  Setting default values for some options
+    protected final static String HOSTNAME_DEFAULT = "localhost";
+    protected final static String USER_DEFAULT = "openexchange";
+    protected final static int MAXUNITS_DEFAULT = 1000;
+    protected final static int CLUSTER_WEIGHT_DEFAULT = 100;
+    
+    //  Setting names for options
     protected final static String OPT_NAME_SEARCH_PATTERN_SHORT = "s";
     protected final static String OPT_NAME_SEARCH_PATTERN_LONG = "searchpattern";
+    protected final static String OPT_NAME_DBNAME_SHORT="n";
+    protected final static String OPT_NAME_DBNAME_LONG="name";
+    protected final static String OPT_NAME_HOSTNAME_SHORT="h";
+    protected final static String OPT_NAME_HOSTNAME_LONG="hostname";
+    protected final static String OPT_NAME_DB_USERNAME_SHORT="u";
+    protected final static String OPT_NAME_DB_USERNAME_LONG="dbuser";
+    protected final static String OPT_NAME_DB_PASSWD_SHORT="p";
+    protected final static String OPT_NAME_DB_PASSWD_LONG="dbpasswd";
+    protected final static String OPT_NAME_IS_MASTER_SHORT="m";
+    protected final static String OPT_NAME_IS_MASTER_LONG="master";
+    protected final static String OPT_NAME_MASTER_ID_SHORT="i";
+    protected final static String OPT_NAME_MASTER_ID_LONG="masterid";
+    protected final static String OPT_NAME_WEIGHT_SHORT="w";
+    protected final static String OPT_NAME_WEIGHT_LONG="dbweight";
+    protected final static String OPT_NAME_MAX_UNITS_SHORT="x";
+    protected final static String OPT_NAME_MAX_UNITS_LONG="maxunit";
+    protected final static String OPT_NAME_DBPARAM_SHORT="p";
+    protected final static String OPT_NAME_DBPARAM_LONG="dbparam";
+    protected final static String OPT_NAME_POOL_HARDLIMIT_SHORT="l";
+    protected final static String OPT_NAME_POOL_HARDLIMIT_LONG="poolhardlimit";
+    protected final static String OPT_NAME_POOL_INITIAL_SHORT="i";
+    protected final static String OPT_NAME_POOL_INITIAL_LONG="poolinitial";
+    protected final static String OPT_NAME_POOL_MAX_SHORT="a";
+    protected final static String OPT_NAME_POOL_MAX_LONG="poolmax";
 
+    
     /**
      * 
      * @return Options containing context,adminuser,adminpass Option objects.
@@ -44,6 +77,19 @@ public class UtilAbstraction extends BasicCommandlineOptions {
         return retval;
     }
 
+    protected String verifySetAndGetOption(final CommandLine cmd, final String optionname) {
+        if (cmd.hasOption(optionname)) {
+            return cmd.getOptionValue(optionname);
+        } else {
+            return null;
+        }
+    }
 
-
+    protected int testStringAndGetIntOrDefault(final String test, final int defaultvalue) throws NumberFormatException {
+        if (null != test) {
+            return Integer.parseInt(test);
+        } else {
+            return defaultvalue;
+        }
+    }
 }
