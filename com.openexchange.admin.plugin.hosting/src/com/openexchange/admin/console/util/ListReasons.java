18527229fc786b8c848c25972aab2317cd68d084
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-03-05 11:14:50 +0000

    - first and hopefully finished version of listReasons

---
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/util/ListReasons.java
@@ -0,0 +1,89 @@
+package com.openexchange.admin.console.util;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+import com.openexchange.admin.rmi.OXUtilInterface;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.dataobjects.MaintenanceReason;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+
+public class ListReasons extends UtilAbstraction {
+    
+    private final static String GENERAL_UTILITY_NAME = "listReasons";
+
+    public ListReasons(final String[] args2) {
+
+        final CommandLineParser parser = new PosixParser();
+
+        final Options options = getOptions();
+
+        try {
+            final CommandLine cmd = parser.parse(options, args2);
+
+            final Credentials auth = new Credentials(cmd.getOptionValue(OPT_NAME_ADMINUSER_SHORT), cmd.getOptionValue(OPT_NAME_ADMINPASS_SHORT));
+
+            // get rmi ref
+            final OXUtilInterface oxutil = (OXUtilInterface) Naming.lookup(OXUtilInterface.RMI_NAME);
+
+            final String searchpattern = cmd.getOptionValue(OPT_NAME_SEARCH_PATTERN_SHORT);
+
+            final MaintenanceReason[] mrs = oxutil.getAllMaintenanceReasons(auth);
+            
+            // FIXME: Format output right
+            for (final MaintenanceReason mr : mrs) {
+                if (searchpattern.equals('*')) {
+                    System.out.println(mr);
+                } else if (mr.getText().equals(searchpattern)) {
+                    System.out.println(mr);                    
+                } 
+            }
+        } catch (final java.rmi.ConnectException neti) {
+            printError(neti.getMessage());
+        } catch (final java.lang.NumberFormatException num) {
+            printInvalidInputMsg("Ids must be numbers!");
+        } catch (final org.apache.commons.cli.MissingArgumentException as) {
+            printError("Missing arguments on the command line: " + as.getMessage());
+            printHelpText(GENERAL_UTILITY_NAME, options);
+        } catch (final org.apache.commons.cli.UnrecognizedOptionException ux) {
+            printError("Unrecognized options on the command line: " + ux.getMessage());
+            printHelpText(GENERAL_UTILITY_NAME, options);
+        } catch (final org.apache.commons.cli.MissingOptionException mis) {
+            printError("Missing options on the command line: " + mis.getMessage());
+            printHelpText(GENERAL_UTILITY_NAME, options);
+        } catch (final ParseException e) {
+            e.printStackTrace();
+        } catch (final MalformedURLException e) {
+            printServerResponse(e.getMessage());
+        } catch (final RemoteException e) {
+            printServerResponse(e.getMessage());
+        } catch (final NotBoundException e) {
+            printServerResponse(e.getMessage());
+        } catch (final StorageException e) {
+            printServerResponse(e.getMessage());
+        } catch (final InvalidCredentialsException e) {
+            printServerResponse(e.getMessage());
+        }
+    }
+
+    public static void main(final String args[]) {
+        new ListReasons(args);
+    }
+
+    private Options getOptions() {
+        final Options retval = getDefaultCommandLineOptions();
+
+        retval.addOption(addDefaultArgName(getShortLongOpt(OPT_NAME_SEARCH_PATTERN_SHORT, OPT_NAME_SEARCH_PATTERN_LONG, "a search pattern for the databases to list", true, true)));
+        return retval;
+    }
+
+}
