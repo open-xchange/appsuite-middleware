e7248a2dad13387402565cb4125c6ed938946ee4
dennis <dennis>
2007-03-28 16:52:09 +0000

    - added new table for storing data of swupdate plugin

---
+++ com.openexchange.admin/etc/mysql/optimize/swupdate.sql
@@ -0,0 +1,15 @@
+#@(#) swupdate.sql optimizations
+
+CREATE TABLE swupdate (
+    cid INT4 UNSIGNED,
+    id INT4 UNSIGNED,
+    monday TIME NULL,
+    tuesday TIME NULL,
+    wednesday TIME NULL,
+    thursday TIME NULL,
+    friday TIME NULL,
+    saturday TIME NULL,
+    sunday TIME NULL,
+    `interval` INT2 UNSIGNED NULL,
+    PRIMARY KEY (cid, id)
+);
