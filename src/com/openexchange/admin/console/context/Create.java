b319b7cafe8d36c6f0cd26b5b38d3e7f34f69f14
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-04-03 10:03:05 +0000

    - changes for output (now it should be clear where the admin is used)

--- src/com/openexchange/admin/console/context/Create.java
+++ src/com/openexchange/admin/console/context/Create.java
@@ -14,7 +14,6 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
-import com.openexchange.admin.console.user.UserAbstraction;
 import com.openexchange.admin.rmi.OXContextInterface;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
@@ -24,33 +23,33 @@ import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
 import com.openexchange.admin.rmi.exceptions.InvalidDataException;
 import com.openexchange.admin.rmi.exceptions.StorageException;
 
-public class Create extends UserAbstraction {
+public class Create extends ContextAbtraction {
     
     private final static String OPT_QUOTA_SHORT = "q";
     private final static String OPT_QUOTA_LONG = "quota";
     
- public Create(String[] args2) {
+ public Create(final String[] args2) {
         
-        CommandLineParser parser = new PosixParser();
+        final CommandLineParser parser = new PosixParser();
 
-        Options options = getOptions();
+        final Options options = getOptions();
 
         try {
             
-            CommandLine cmd = parser.parse(options, args2);        
+            final CommandLine cmd = parser.parse(options, args2);        
             
-            Context ctx = new Context();
+            final Context ctx = new Context();
             ctx.setID(Integer.parseInt(cmd.getOptionValue(OPT_NAME_CONTEXT_SHORT)));
             
             
-            Credentials auth = new Credentials(cmd.getOptionValue(OPT_NAME_ADMINUSER_SHORT),cmd.getOptionValue(OPT_NAME_ADMINPASS_SHORT));
+            final Credentials auth = new Credentials(cmd.getOptionValue(OPT_NAME_ADMINUSER_SHORT),cmd.getOptionValue(OPT_NAME_ADMINPASS_SHORT));
             
             
             // get rmi ref
-            OXContextInterface oxctx = (OXContextInterface)Naming.lookup(OXContextInterface.RMI_NAME);
+            final OXContextInterface oxctx = (OXContextInterface)Naming.lookup(OXContextInterface.RMI_NAME);
             
             // create user obj
-            User usr = new User();
+            final User usr = new User();
             
             // fill user obj with mandatory values from console
             usr.setUsername(cmd.getOptionValue(OPT_USERNAME_SHORT));
@@ -61,52 +60,52 @@ public class Create extends UserAbstraction {
             usr.setPrimaryEmail(cmd.getOptionValue(OPT_PRIMARY_EMAIL_SHORT));
             usr.setEmail1(cmd.getOptionValue(OPT_PRIMARY_EMAIL_SHORT));
             
-            long quota = Long.parseLong(cmd.getOptionValue(OPT_QUOTA_SHORT));            
+            final long quota = Long.parseLong(cmd.getOptionValue(OPT_QUOTA_SHORT));            
                        
             System.out.println(oxctx.create(ctx, usr, quota, auth));
             
-        }catch(java.rmi.ConnectException neti){
+        }catch(final java.rmi.ConnectException neti){
             printError(neti.getMessage());            
-        }catch(java.lang.NumberFormatException num){
+        }catch(final java.lang.NumberFormatException num){
             printInvalidInputMsg("Ids must be numbers!");
-        }catch(org.apache.commons.cli.MissingArgumentException as){
+        }catch(final org.apache.commons.cli.MissingArgumentException as){
             printError("Missing arguments on the command line: " + as.getMessage());;
             printHelpText("create", options);
-        }catch(org.apache.commons.cli.UnrecognizedOptionException ux){
+        }catch(final org.apache.commons.cli.UnrecognizedOptionException ux){
             printError("Unrecognized options on the command line: " + ux.getMessage());;
             printHelpText("create", options);
-        } catch (org.apache.commons.cli.MissingOptionException mis) {
+        } catch (final org.apache.commons.cli.MissingOptionException mis) {
             printError("Missing options on the command line: " + mis.getMessage());;
             printHelpText("create", options);
-        } catch (ParseException e) {
+        } catch (final ParseException e) {
             e.printStackTrace();
-        } catch (MalformedURLException e) {            
+        } catch (final MalformedURLException e) {            
             printServerResponse(e.getMessage());
-        } catch (RemoteException e) {            
+        } catch (final RemoteException e) {            
             printServerResponse(e.getMessage());
-        } catch (NotBoundException e) {
+        } catch (final NotBoundException e) {
             printNotBoundResponse(e);
-        } catch (StorageException e) {            
+        } catch (final StorageException e) {            
             printServerResponse(e.getMessage());
-        } catch (InvalidCredentialsException e) {
+        } catch (final InvalidCredentialsException e) {
             printServerResponse(e.getMessage());        
-        } catch (InvalidDataException e) {            
+        } catch (final InvalidDataException e) {            
             printServerResponse(e.getMessage());
-        } catch (ContextExistsException e) {
+        } catch (final ContextExistsException e) {
             printServerResponse(e.getMessage());
         }
 
     }
 
-    public static void main(String args[]){
+    public static void main(final String args[]){
         new Create(args);
     }
     
     private Options getOptions() {
-        Options retval = getDefaultCommandLineOptions();
+        final Options retval = getDefaultCommandLineOptions();
         
         // this time context id is mandatory
-        Option tmp = retval.getOption(OPT_NAME_CONTEXT_SHORT);
+        final Option tmp = retval.getOption(OPT_NAME_CONTEXT_SHORT);
         tmp.setRequired(true);
         retval.addOption(tmp);
         
@@ -118,7 +117,7 @@ public class Create extends UserAbstraction {
         retval.addOption(getPasswordOption());
         retval.addOption(getPrimaryMailOption());
         
-        Option quota = getShortLongOpt(OPT_QUOTA_SHORT, OPT_QUOTA_LONG, "How much quota the context can use for filestore", true, true);
+        final Option quota = getShortLongOpt(OPT_QUOTA_SHORT, OPT_QUOTA_LONG, "How much quota the context can use for filestore", true, true);
         retval.addOption(quota);
         
         return retval;
