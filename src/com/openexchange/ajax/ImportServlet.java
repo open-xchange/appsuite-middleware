7bed2b4a82d706123ee3da6af96082b5a35e23bd
suphi <suphi>
2007-04-13 14:11:12 +0000

    @tobias.prinz: callback replacement

--- src/com/openexchange/ajax/ImportServlet.java
+++ src/com/openexchange/ajax/ImportServlet.java
@@ -139,7 +139,7 @@ public class ImportServlet extends ImportExport {
 				PrintWriter w = null;
 				try {
 					w = resp.getWriter();
-					w.write(substitute(JS_FRAGMENT,"json",resObj.getJSON().toString(),"import",JSON_CALLBACK));
+					w.write(substitute(JS_FRAGMENT,"json",resObj.getJSON().toString(),"action",JSON_CALLBACK));
 					close(w);
 				} catch (IOException e) {
 					LOG.warn(e);
