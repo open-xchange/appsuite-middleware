67b53bbff53887021850f6e3840b274c538f78dc
Manuel Kraft <manuel.kraft@open-xchange.com>
2007-04-25 08:50:27 +0000

    refactored console tools to new jargs and csv output added

--- com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/context/ContextAbtraction.java
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/context/ContextAbtraction.java
@@ -13,8 +13,8 @@ public abstract class ContextAbtraction extends UserAbstraction {
     protected final static char OPT_NAME_COMMON_ID_SHORT = 'i';
     protected final static String OPT_NAME_COMMON_ID_LONG  = "id";
     
-    protected void setSearchOption(AdminParser parser){
-        searchOption = setShortLongOpt(parser, OPT_NAME_SEARCH_PATTERN_SHORT,OPT_NAME_SEARCH_PATTERN_LONG,"Search/List pattern!",true, false);
+    protected void setSearchOption(AdminParser parser,boolean required){
+        searchOption = setShortLongOpt(parser, OPT_NAME_SEARCH_PATTERN_SHORT,OPT_NAME_SEARCH_PATTERN_LONG,"Search/List pattern!",true, required);
     }
     
     protected void setCommonIDOption(AdminParser parser,boolean required ){
