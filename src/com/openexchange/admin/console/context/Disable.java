65ba4bc82cdca16a36d148b997bbf1c6284bf0c4
Manuel Kraft <manuel.kraft@open-xchange.com>
2007-04-26 09:47:12 +0000

    refactored console tools to new jargs and csv output added

--- src/com/openexchange/admin/console/context/Disable.java
+++ src/com/openexchange/admin/console/context/Disable.java
@@ -1,105 +1,105 @@
-//package com.openexchange.admin.console.context;
-//
-//
-//
-//import java.net.MalformedURLException;
-//import java.rmi.Naming;
-//import java.rmi.NotBoundException;
-//import java.rmi.RemoteException;
-//
-//import org.apache.commons.cli.CommandLine;
-//import org.apache.commons.cli.CommandLineParser;
-//import org.apache.commons.cli.Option;
-//import org.apache.commons.cli.Options;
-//import org.apache.commons.cli.ParseException;
-//import org.apache.commons.cli.PosixParser;
-//
-//import com.openexchange.admin.rmi.OXContextInterface;
-//import com.openexchange.admin.rmi.dataobjects.Context;
-//import com.openexchange.admin.rmi.dataobjects.Credentials;
-//import com.openexchange.admin.rmi.dataobjects.MaintenanceReason;
-//import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
-//import com.openexchange.admin.rmi.exceptions.InvalidDataException;
-//import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
-//import com.openexchange.admin.rmi.exceptions.StorageException;
-//
-//public class Disable extends ContextAbtraction {    
-//   
-//    private final static String OPT_REASON_SHORT = "r";
-//    private final static String OPT_REASON_LONG= "reason";
-//    
-// public Disable(final String[] args2) {
-//        
-//        final CommandLineParser parser = new PosixParser();
-//
-//        final Options options = getOptions();
-//
-//        try {
-//            
-//            final CommandLine cmd = parser.parse(options, args2);            
-//            final Context ctx = new Context();            
-//            
-//            ctx.setID(Integer.parseInt(cmd.getOptionValue(OPT_NAME_CONTEXT_SHORT)));            
-//            
-//            final Credentials auth = new Credentials(cmd.getOptionValue(OPT_NAME_ADMINUSER_SHORT),cmd.getOptionValue(OPT_NAME_ADMINPASS_SHORT));
-//                        
-//            // get rmi ref
-//            final OXContextInterface oxres = (OXContextInterface)Naming.lookup(OXContextInterface.RMI_NAME);
-//            
-//            final MaintenanceReason mr = new MaintenanceReason(Integer.parseInt(cmd.getOptionValue(OPT_REASON_SHORT))); 
-//            oxres.disable(ctx, mr, auth);
-//            
-//        }catch(final java.rmi.ConnectException neti){
-//            printError(neti.getMessage());            
-//        }catch(final java.lang.NumberFormatException num){
-//            printInvalidInputMsg("Ids must be numbers!");
-//        }catch(final org.apache.commons.cli.MissingArgumentException as){
-//            printError("Missing arguments on the command line: " + as.getMessage());;
-//            printHelpText("disable", options);
-//        }catch(final org.apache.commons.cli.UnrecognizedOptionException ux){
-//            printError("Unrecognized options on the command line: " + ux.getMessage());;
-//            printHelpText("disable", options);
-//        } catch (final org.apache.commons.cli.MissingOptionException mis) {
-//            printError("Missing options on the command line: " + mis.getMessage());;
-//            printHelpText("disable", options);
-//        } catch (final ParseException e) {
-//            e.printStackTrace();
-//        } catch (final MalformedURLException e) {            
-//            printServerResponse(e.getMessage());
-//        } catch (final RemoteException e) {            
-//            printServerResponse(e.getMessage());
-//        } catch (final NotBoundException e) {
-//            printNotBoundResponse(e);
-//        } catch (final StorageException e) {            
-//            printServerResponse(e.getMessage());
-//        } catch (final InvalidCredentialsException e) {
-//            printServerResponse(e.getMessage());        
-//        } catch (final InvalidDataException e) {            
-//            printServerResponse(e.getMessage());
-//        } catch (final NoSuchContextException e) {
-//            printServerResponse(e.getMessage());
-//        }
-//
-//    }
-//
-//    public static void main(final String args[]){
-//        new Disable(args);
-//    }
-//    
-//    private Options getOptions() {
-//        final Options retval = getDefaultCommandLineOptions();
-//        
-//        // this time context id is mandatory
-//        final Option tmp = retval.getOption(OPT_NAME_CONTEXT_SHORT);
-//        tmp.setRequired(true);
-//        retval.addOption(tmp);      
-//        
-//        final Option reason = getShortLongOpt(OPT_REASON_SHORT, OPT_REASON_LONG, "Maintenance reason id", true, true);
-//        retval.addOption(reason);
-//        
-//        return retval;
-//    }
-//    
-//    
-//
-//}
+package com.openexchange.admin.console.context;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import com.openexchange.admin.console.AdminParser;
+import com.openexchange.admin.console.AdminParser.MissingOptionException;
+import com.openexchange.admin.console.CmdLineParser.IllegalOptionValueException;
+import com.openexchange.admin.console.CmdLineParser.UnknownOptionException;
+import com.openexchange.admin.rmi.OXContextInterface;
+import com.openexchange.admin.rmi.dataobjects.Context;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.dataobjects.MaintenanceReason;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+
+public class Disable extends ContextAbtraction {    
+   
+    
+    
+ public Disable(final String[] args2) {
+        
+     AdminParser parser = new AdminParser("disableall");
+
+     setOptions(parser);
+
+     try {
+         
+            parser.ownparse(args2);    
+            final Context ctx = new Context();            
+            
+            ctx.setID(Integer.parseInt((String)parser.getOptionValue(contextIDOption)));            
+            
+            final Credentials auth = new Credentials((String)parser.getOptionValue(adminUserOption),(String)parser.getOptionValue(adminPassOption));
+                       
+            // get rmi ref
+            final OXContextInterface oxres = (OXContextInterface)Naming.lookup(OXContextInterface.RMI_NAME);
+            
+            final MaintenanceReason mr = new MaintenanceReason(Integer.parseInt((String)parser.getOptionValue(maintenanceReasonIDOption))); 
+            oxres.disable(ctx, mr, auth);
+            
+        }catch(final java.rmi.ConnectException neti){
+            printError(neti.getMessage());       
+            System.exit(1);
+        }catch(final java.lang.NumberFormatException num){
+            printInvalidInputMsg("Ids must be numbers!");       
+            System.exit(1);
+        } catch (final MalformedURLException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final RemoteException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final NotBoundException e) {
+            printNotBoundResponse(e);
+            System.exit(1);
+        } catch (final StorageException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final InvalidCredentialsException e) {
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final InvalidDataException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final NoSuchContextException e) {
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (IllegalOptionValueException e) {            
+            printError("Illegal option value : " + e.getMessage());
+            parser.printUsage();
+            System.exit(1);
+        } catch (UnknownOptionException e) {
+            printError("Unrecognized options on the command line: " + e.getMessage());
+            parser.printUsage();
+            System.exit(1);
+        } catch (MissingOptionException e) {
+            printError(e.getMessage());
+            parser.printUsage();
+            System.exit(1);
+        }
+
+    }
+
+    public static void main(final String args[]){
+        new Disable(args);
+    }
+    
+    private void setOptions(AdminParser parser) {
+        setDefaultCommandLineOptions(parser);
+        
+       setContextIDOption(parser, true);
+        
+       setMaintenanceReasodIDOption(parser, true);
+        
+        
+    }
+    
+    
+
+}
--- src/com/openexchange/admin/console/context/Search.java
+++ src/com/openexchange/admin/console/context/Search.java
@@ -1,95 +1,151 @@
-//package com.openexchange.admin.console.context;
-//
-//
-//
-//import java.net.MalformedURLException;
-//import java.rmi.Naming;
-//import java.rmi.NotBoundException;
-//import java.rmi.RemoteException;
-//
-//import org.apache.commons.cli.CommandLine;
-//import org.apache.commons.cli.CommandLineParser;
-//import org.apache.commons.cli.Option;
-//import org.apache.commons.cli.Options;
-//import org.apache.commons.cli.ParseException;
-//import org.apache.commons.cli.PosixParser;
-//
-//import com.openexchange.admin.rmi.OXContextInterface;
-//import com.openexchange.admin.rmi.dataobjects.Context;
-//import com.openexchange.admin.rmi.dataobjects.Credentials;
-//import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
-//import com.openexchange.admin.rmi.exceptions.InvalidDataException;
-//import com.openexchange.admin.rmi.exceptions.StorageException;
-//
-//public class Search extends ContextAbtraction {    
-//       
-// public Search(final String[] args2) {
-//        
-//        final CommandLineParser parser = new PosixParser();
-//
-//        final Options options = getOptions();
-//
-//        try {
-//            
-//            final CommandLine cmd = parser.parse(options, args2); 
-//            
-//            final Credentials auth = new Credentials(cmd.getOptionValue(OPT_NAME_ADMINUSER_SHORT),cmd.getOptionValue(OPT_NAME_ADMINPASS_SHORT));
-//                        
-//            // get rmi ref
-//            final OXContextInterface oxres = (OXContextInterface)Naming.lookup(OXContextInterface.RMI_NAME);
-//                        
-//            final Context[] ctxs = oxres.search(cmd.getOptionValue(OPT_NAME_SEARCHPATTERN), auth);
-//            for (final Context ctx_tmp : ctxs) {
-//                System.out.println(""+ctx_tmp.getIdAsInt());
-//            }
-//        }catch(final java.rmi.ConnectException neti){
-//            printError(neti.getMessage());            
-//        }catch(final java.lang.NumberFormatException num){
-//            printInvalidInputMsg("Ids must be numbers!");
-//        }catch(final org.apache.commons.cli.MissingArgumentException as){
-//            printError("Missing arguments on the command line: " + as.getMessage());;
-//            printHelpText("search", options);
-//        }catch(final org.apache.commons.cli.UnrecognizedOptionException ux){
-//            printError("Unrecognized options on the command line: " + ux.getMessage());;
-//            printHelpText("search", options);
-//        } catch (final org.apache.commons.cli.MissingOptionException mis) {
-//            printError("Missing options on the command line: " + mis.getMessage());;
-//            printHelpText("search", options);
-//        } catch (final ParseException e) {
-//            e.printStackTrace();
-//        } catch (final MalformedURLException e) {            
-//            printServerResponse(e.getMessage());
-//        } catch (final RemoteException e) {            
-//            printServerResponse(e.getMessage());
-//        } catch (final NotBoundException e) {
-//            printNotBoundResponse(e);
-//        } catch (final StorageException e) {            
-//            printServerResponse(e.getMessage());
-//        } catch (final InvalidCredentialsException e) {
-//            printServerResponse(e.getMessage());        
-//        } catch (final InvalidDataException e) {            
-//            printServerResponse(e.getMessage());        
-//        }
-//
-//    }
-//
-//    public static void main(final String args[]){
-//        new Search(args);
-//    }
-//    
-//    private Options getOptions() {
-//        final Options retval = new Options();
-//        
-//        retval.addOption(getAdminUserOption());
-//        retval.addOption(getAdminPassOption());        
-//        
-//        final Option search = getSearchPatternOption();
-//        search.setRequired(true);
-//        retval.addOption(search);
-//        
-//        return retval;
-//    }
-//    
-//    
-//
-//}
+package com.openexchange.admin.console.context;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import com.openexchange.admin.console.AdminParser;
+import com.openexchange.admin.console.AdminParser.MissingOptionException;
+import com.openexchange.admin.console.CmdLineParser.IllegalOptionValueException;
+import com.openexchange.admin.console.CmdLineParser.UnknownOptionException;
+import com.openexchange.admin.rmi.OXContextInterface;
+import com.openexchange.admin.rmi.dataobjects.Context;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+
+public class Search extends ContextAbtraction {    
+       
+ public Search(final String[] args2) {
+        
+        AdminParser parser = new AdminParser("listcontexts");
+
+        setOptions(parser);
+        setCSVOutputOption(parser);
+        
+        try {
+            
+             parser.ownparse( args2); 
+            
+             final Credentials auth = new Credentials((String)parser.getOptionValue(adminUserOption),(String)parser.getOptionValue(adminPassOption));
+                        
+            // get rmi ref
+            final OXContextInterface oxres = (OXContextInterface)Naming.lookup(OXContextInterface.RMI_NAME);
+                        
+            String pattern = "*";
+            if(parser.getOptionValue(searchOption)!=null){
+                pattern = (String)parser.getOptionValue(searchOption);
+            }
+            
+            final Context[] ctxs = oxres.search(pattern, auth);
+            
+//          needed for csv output, KEEP AN EYE ON ORDER!!!
+            ArrayList<String> columns = new ArrayList<String>();
+            columns.add("id");
+            columns.add("name");
+            columns.add("enabeld");
+            columns.add("filestore_id");
+            columns.add("filestore_name");
+            
+            ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+            
+            
+            for (final Context ctx_tmp : ctxs) {
+                if (parser.getOptionValue(csvOutputOption) != null) { 
+                    data.add(makeCSVData(ctx_tmp));
+                }else{
+                    System.out.println(ctx_tmp.toString());
+                }
+            }
+            
+            if (parser.getOptionValue(csvOutputOption) != null) {
+                doCSVOutput(columns, data);
+            }
+            
+        }catch(final java.rmi.ConnectException neti){
+            printError(neti.getMessage()); 
+            System.exit(1);
+        }catch(final java.lang.NumberFormatException num){
+            printInvalidInputMsg("Ids must be numbers!");
+            System.exit(1);
+        } catch (final MalformedURLException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final RemoteException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final NotBoundException e) {
+            printNotBoundResponse(e);
+            System.exit(1);
+        } catch (final StorageException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final InvalidCredentialsException e) {
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (final InvalidDataException e) {            
+            printServerResponse(e.getMessage());
+            System.exit(1);
+        } catch (IllegalOptionValueException e) {            
+            printError("Illegal option value : " + e.getMessage());
+            parser.printUsage();
+            System.exit(1);
+        } catch (UnknownOptionException e) {
+            printError("Unrecognized options on the command line: " + e.getMessage());
+            parser.printUsage();
+            System.exit(1);
+        } catch (MissingOptionException e) {
+            printError(e.getMessage());
+            parser.printUsage();
+            System.exit(1);
+        }
+
+    }
+
+    public static void main(final String args[]){
+        new Search(args);
+    }
+    
+    private void setOptions(AdminParser parser) {
+        setDefaultCommandLineOptions(parser);
+        setSearchOption(parser,false);
+    }
+    
+    private ArrayList<String> makeCSVData(Context ctx){
+        ArrayList<String> srv_data = new ArrayList<String>();
+        srv_data.add(String.valueOf(ctx.getIdAsInt()));
+        if(ctx.getName()!=null){
+            srv_data.add(ctx.getName());
+        }else{
+            srv_data.add(null);
+        }
+        if(ctx.isEnabled()!=null){
+            srv_data.add(String.valueOf(ctx.isEnabled()));
+        }else{
+            srv_data.add(null);
+        }
+        
+        if(ctx.getFilestore()!=null){
+            if(ctx.getFilestore().getId()!=null){
+                srv_data.add(String.valueOf(ctx.getFilestore().getId()));
+            }else{
+                srv_data.add(null);
+            }
+            if(ctx.getFilestore().getName()!=null){
+                srv_data.add(ctx.getFilestore().getName());
+            }else{
+                srv_data.add(null);
+            }
+        }else{
+            srv_data.add(null);
+            srv_data.add(null);
+        }
+        
+        return srv_data;
+    }
+    
+
+}
