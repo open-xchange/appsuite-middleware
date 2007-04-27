65c0477f9c9cde6354150eca16b0537c5b2b78cb
choeger <choeger>
2007-04-27 08:49:07 +0000

    added GPL Header

--- com.openexchange.admin/src/com/openexchange/admin/rmi/exceptions/InvalidCredentialsException.java
+++ com.openexchange.admin/src/com/openexchange/admin/rmi/exceptions/InvalidCredentialsException.java
@@ -46,9 +46,7 @@
  *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
-/**
- * 
- */
+
 package com.openexchange.admin.rmi.exceptions;
 
 import java.io.Serializable;
--- com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXToolMySQLStorage.java
+++ com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXToolMySQLStorage.java
@@ -46,9 +46,7 @@
  *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
-/**
- *
- */
+
 package com.openexchange.admin.storage.mysqlStorage;
 
 import java.sql.Connection;
