7fb765389c83cc2731cb3df8db0edf21570ce7d9
bishoph <bishoph>
2007-05-04 08:42:12 +0000

    fixed bug #7062

--- com.openexchange.admin/etc/mysql/consistency/calendar.sql
+++ com.openexchange.admin/etc/mysql/consistency/calendar.sql
@@ -9,7 +9,3 @@ alter table del_date_rights ENGINE=InnoDB;
 alter table prg_dates_members ENGINE=InnoDB; 
 alter table del_dates_members ENGINE=InnoDB; 
 
-drop table prg_date_resources;
-drop table sys_holiday;
-drop table usr_holiday;
-drop table prg_date_notification;
