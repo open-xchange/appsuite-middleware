0f9ce0e3128121a221caf4fdda31c50db0810a97
choeger <choeger>
2007-05-07 16:06:37 +0000

    mail.sql cleaned up

--- com.openexchange.admin/etc/mysql/mail.sql
+++ com.openexchange.admin/etc/mysql/mail.sql
@@ -1,82 +1,51 @@
 #@(#) mail.sql optimizations
 
-
 CREATE TABLE mail_domains (
-    cid INT4 UNSIGNED,
-    id INT4 UNSIGNED,
-	domainName VARCHAR(128),
-	smtpSenderRule VARCHAR(128),
-	restriction VARCHAR(64),
+    cid INT4 UNSIGNED NOT NULL,
+    id INT4 UNSIGNED NOT NULL,
+	domainName VARCHAR(128) NOT NULL,
+	smtpSenderRule VARCHAR(128) NOT NULL,
+	restriction VARCHAR(64) NOT NULL,
     PRIMARY KEY (cid, id),
-    INDEX (domainName)
-);
+    INDEX (domainName),
+    CONSTRAINT mail_domains_domainName_unique UNIQUE (domainName)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE user_mail_restrictions (
-	cid INT4 UNSIGNED,
-	id INT4 UNSIGNED,
-	className VARCHAR(64),
-	address VARCHAR(255),
-	INDEX(className, address)
-);
+	cid INT4 UNSIGNED NOT NULL,
+	id INT4 UNSIGNED NOT NULL,
+	className VARCHAR(64) NOT NULL,
+	address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (cid, id),
+	INDEX(className),
+	INDEX(address),
+    CONSTRAINT user_mail_restrictions_address_unique UNIQUE (address)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE group_address_mappings (
-	cid INT4 UNSIGNED,
-	id INT4 UNSIGNED,
-	address VARCHAR(255),
-	INDEX(cid,address)
-);
+	cid INT4 UNSIGNED NOT NULL,
+	id INT4 UNSIGNED NOT NULL,
+	address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (cid, id),
+	INDEX(address),
+    CONSTRAINT group_address_mappings_address_unique UNIQUE (address),
+    FOREIGN KEY (cid, id) REFERENCES groups(cid, id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE resource_address_mappings (
-	cid INT4 UNSIGNED,
-	id INT4 UNSIGNED,
-	destination VARCHAR(255),
-	INDEX(cid)
-);
+	cid INT4 UNSIGNED NOT NULL,
+	id INT4 UNSIGNED NOT NULL,
+	destination VARCHAR(255) NOT NULL,
+    PRIMARY KEY (cid, id),
+    FOREIGN KEY (cid, id) REFERENCES resource(cid, id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE address_mappings (
-	cid INT4 UNSIGNED,
-	id INT4 UNSIGNED,
-	address VARCHAR(255),
-	destination VARCHAR(255),
+	cid INT4 UNSIGNED NOT NULL,
+	id INT4 UNSIGNED NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	destination VARCHAR(255) NOT NULL,
     PRIMARY KEY (cid, id),
-	INDEX(address)
-);
-#@(#) mail.sql consistency
-
-
-ALTER TABLE mail_domains
-    MODIFY cid INT4 UNSIGNED NOT NULL,
-    MODIFY id INT4 UNSIGNED NOT NULL,
-	MODIFY domainName VARCHAR(128) UNIQUE NOT NULL,
-	MODIFY smtpSenderRule VARCHAR(128) NOT NULL,
-	MODIFY restriction VARCHAR(64) NOT NULL,
-    ENGINE=InnoDB;
-
-ALTER TABLE user_mail_restrictions
-	MODIFY cid INT4 UNSIGNED NOT NULL,
-	MODIFY id INT4 UNSIGNED NOT NULL,
-	MODIFY className VARCHAR(64) NOT NULL,
-	MODIFY address VARCHAR(255) UNIQUE NOT NULL,
-    ADD FOREIGN KEY (cid, id) REFERENCES user(cid, id),
-	ENGINE=InnoDB;
-
-ALTER TABLE group_address_mappings
-	MODIFY cid INT4 UNSIGNED NOT NULL,
-	MODIFY id INT4 UNSIGNED NOT NULL,
-	MODIFY address VARCHAR(255) UNIQUE NOT NULL,
-    ADD FOREIGN KEY (cid, id) REFERENCES groups(cid, id),
-	ENGINE=InnoDB;
-
-ALTER TABLE resource_address_mappings
-	MODIFY cid INT4 UNSIGNED NOT NULL,
-	MODIFY id INT4 UNSIGNED NOT NULL,
-	MODIFY destination VARCHAR(255) NOT NULL,
-    ADD FOREIGN KEY (cid, id) REFERENCES resource(cid, id),
-	ENGINE=InnoDB;
-
-ALTER TABLE address_mappings
-	MODIFY cid INT4 UNSIGNED NOT NULL,
-	MODIFY id INT4 UNSIGNED NOT NULL,
-	MODIFY address VARCHAR(255) UNIQUE NOT NULL,
-	MODIFY destination VARCHAR(255) NOT NULL,
-	ENGINE=InnoDB;
+	INDEX(address),
+	CONSTRAINT address_mappings_address_unique UNIQUE (address)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
