2429ad1ccc28081b80227e031f6f538612f6d7b0
Carsten Hoeger <carsten.hoeger@open-xchange.com>
2007-05-08 06:53:06 +0000

    swupdate.sql cleaned up

--- com.openexchange.admin/etc/mysql/swupdate.sql
+++ com.openexchange.admin/etc/mysql/swupdate.sql
@@ -1,27 +1,14 @@
-#@(#) swupdate.sql optimizations
+#@(#) swupdate.sql
 
 CREATE TABLE swupdate (
-    cid INT4 UNSIGNED,
-    monday TIME NULL,
-    tuesday TIME NULL,
-    wednesday TIME NULL,
-    thursday TIME NULL,
-    friday TIME NULL,
-    saturday TIME NULL,
-    sunday TIME NULL,
-    `interval` INT2 UNSIGNED NULL,
+    cid INT4 UNSIGNED NOT NULL,
+    monday TIME default NULL,
+    tuesday TIME default NULL,
+    wednesday TIME default NULL,
+    thursday TIME default NULL,
+    friday TIME default NULL,
+    saturday TIME default NULL,
+    sunday TIME default NULL,
+    `interval` INT2 UNSIGNED default NULL,
     PRIMARY KEY (cid)
-);
-#@(#) swupdate.sql consistency
-
-ALTER TABLE swupdate
-    MODIFY cid INT4 UNSIGNED NOT NULL,
-    MODIFY monday TIME default NULL,
-    MODIFY tuesday TIME default NULL,
-    MODIFY wednesday TIME default NULL,
-    MODIFY thursday TIME default NULL,
-    MODIFY friday TIME default NULL,
-    MODIFY saturday TIME default NULL,
-    MODIFY sunday TIME default NULL,
-    MODIFY `interval` INT2 UNSIGNED default NULL,
-    ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
