f8d97b5f2939e1860b72fc57c9ea0f329f704607
Carsten Hoeger <carsten.hoeger@open-xchange.com>
2007-05-08 08:58:09 +0000

    added version table

--- com.openexchange.admin/etc/mysql/database-changes/changes-initial-EE.sql
+++ com.openexchange.admin/etc/mysql/database-changes/changes-initial-EE.sql
@@ -1,4 +1,12 @@
-# What about the version table?
+
+# 2007-05-08, Carsten Hoeger, added version table
+CREATE TABLE version (
+	version INT4 UNSIGNED NOT NULL,
+	locked BOOLEAN NOT NULL,
+	gw_compatible BOOLEAN NOT NULL,
+	admin_compatible BOOLEAN NOT NULL,
+	server VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE = InnoDB;
 
 # 2007-02-05, Carsten Hoeger, initial entries
 ALTER TABLE user_setting_mail ADD COLUMN confirmed_ham VARCHAR(128) NOT NULL;
