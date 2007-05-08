07272aa0df1fb732d40853d22fc0338a2f7f1e1d
Carsten Hoeger <carsten.hoeger@open-xchange.com>
2007-05-08 11:30:37 +0000

    don't create index of unique keys

--- com.openexchange.admin/etc/mysql/mail.sql
+++ com.openexchange.admin/etc/mysql/mail.sql
@@ -7,7 +7,6 @@ CREATE TABLE mail_domains (
 	smtpSenderRule VARCHAR(128) NOT NULL,
 	restriction VARCHAR(64) NOT NULL,
     PRIMARY KEY (cid, id),
-    INDEX (domainName),
     CONSTRAINT mail_domains_domainName_unique UNIQUE (domainName)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -18,7 +17,6 @@ CREATE TABLE user_mail_restrictions (
 	address VARCHAR(255) NOT NULL,
     PRIMARY KEY (cid, id),
 	INDEX(className),
-	INDEX(address),
     CONSTRAINT user_mail_restrictions_address_unique UNIQUE (address)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -27,7 +25,6 @@ CREATE TABLE group_address_mappings (
 	id INT4 UNSIGNED NOT NULL,
 	address VARCHAR(255) NOT NULL,
     PRIMARY KEY (cid, id),
-	INDEX(address),
     CONSTRAINT group_address_mappings_address_unique UNIQUE (address),
     FOREIGN KEY (cid, id) REFERENCES groups(cid, id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -46,6 +43,5 @@ CREATE TABLE address_mappings (
 	address VARCHAR(255) NOT NULL,
 	destination VARCHAR(255) NOT NULL,
     PRIMARY KEY (cid, id),
-	INDEX(address),
 	CONSTRAINT address_mappings_address_unique UNIQUE (address)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
