561b875918554f2a2d1a86775b469857b9b94ba7
marcus <marcus>
2007-05-09 15:08:09 +0000

    Fixed bad sql script.

--- etc/mysql/attachment.sql
+++ etc/mysql/attachment.sql
@@ -1,28 +1,28 @@
 CREATE TABLE `prg_attachment` (
-  `cid` int(10) unsigned NOT NULL,
-  `id` int(10) unsigned NOT NULL,
-  `created_by` int(10) unsigned NOT NULL,
-  `creation_date` bigint(20) NOT NULL,
+  `cid` int4 unsigned NOT NULL,
+  `id` int4 unsigned NOT NULL,
+  `created_by` int4 unsigned NOT NULL,
+  `creation_date` int8 NOT NULL,
   `file_mimetype` varchar(255) NOT NULL,
-  `file_size` int(10) unsigned NOT NULL,
+  `file_size` int4 unsigned NOT NULL,
   `filename` varchar(255) NOT NULL,
-  `attached` int(10) unsigned NOT NULL,
-  `module` int(10) unsigned NOT NULL,
-  `rtf_flag` tinyint(1),
+  `attached` int4 unsigned NOT NULL,
+  `module` int4 unsigned NOT NULL,
+  `rtf_flag` boolean,
   `comment` varchar(255),
   `file_id` varchar(255) NOT NULL,
   PRIMARY KEY  (`cid`,`id`),
   KEY `cid` (`cid`,`attached`,`module`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 CREATE TABLE `del_attachment` (
-  `cid` int(10) unsigned NOT NULL,
-  `id` int(10) unsigned NOT NULL,
-  `attached` int(10) unsigned NOT NULL,
-  `module` int(10) unsigned NOT NULL,
-  `del_date` bigint(20) NOT NULL,
+  `cid` int4 unsigned NOT NULL,
+  `id` int4 unsigned NOT NULL,
+  `attached` int4 unsigned NOT NULL,
+  `module` int4 unsigned NOT NULL,
+  `del_date` int8 NOT NULL,
   PRIMARY KEY  (`cid`,`id`),
   KEY `cid` (`cid`,`attached`,`module`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
