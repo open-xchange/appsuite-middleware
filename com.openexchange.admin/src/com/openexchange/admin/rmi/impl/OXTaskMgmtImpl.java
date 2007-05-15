2963c4ad50a2e810b03e34a5e74cb2518b5f9365
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-05-15 13:14:24 +0000

    - extended task handling so that the results of a task can now be fetched by one central method in the admin daemon core - added RMI Interface for all task mgmt specific tasks

---
+++ com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXTaskMgmtImpl.java
@@ -0,0 +1,35 @@
+package com.openexchange.admin.rmi.impl;
+
+import java.rmi.RemoteException;
+import java.util.concurrent.ExecutionException;
+
+import com.openexchange.admin.rmi.BasicAuthenticator;
+import com.openexchange.admin.rmi.OXTaskMgmtInterface;
+import com.openexchange.admin.rmi.dataobjects.Context;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+import com.openexchange.admin.taskmanagement.ExtendedFutureTask;
+import com.openexchange.admin.taskmanagement.TaskManager;
+
+public class OXTaskMgmtImpl extends BasicAuthenticator implements OXTaskMgmtInterface {
+    
+    public Object getTaskResults(final Context ctx, final Credentials cred, final int id) throws RemoteException, InvalidCredentialsException, StorageException, InterruptedException, ExecutionException, InvalidDataException {
+        contextcheck(ctx);
+        doAuthentication(cred, ctx);
+        final ExtendedFutureTask<?> task = TaskManager.getInstance().getTask(id);
+        if (null != task && task.isDone()) {
+            return task.get();
+        } else {
+            return null;
+        }
+    }
+
+    public String getJobList(final Context ctx, final Credentials cred) throws RemoteException, InvalidDataException, InvalidCredentialsException, StorageException {
+        contextcheck(ctx);
+        doAuthentication(cred, ctx);
+        return TaskManager.getInstance().getJobList();
+    }
+
+}
