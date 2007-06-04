ce7ee0015411a155bf92b6f257a8dec218afbdb8
thorben <thorben>
2007-06-04 14:45:47 +0000

    *** empty log message ***

--- src/com/openexchange/ajax/AJAXServlet.java
+++ src/com/openexchange/ajax/AJAXServlet.java
@@ -338,7 +338,7 @@ public abstract class AJAXServlet extends HttpServlet implements UploadRegistry 
 
 	// Javascript
 
-	protected static final String JS_FRAGMENT = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\""
+	public static final String JS_FRAGMENT = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\""
 			+ "\"http://www.w3.org/TR/html4/strict.dtd\"> " + "<html> " + "<head>"
 			+ "<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"> "
 			+ "<script type=\"text/javascript\"> " + "callback (**json**);" + "function callback(arg) " + "{ "
