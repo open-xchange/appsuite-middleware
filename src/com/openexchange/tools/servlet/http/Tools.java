7ccc7caec7eeccb8953e510a968c2a11dc3d6579
thorben <thorben>
2007-06-05 07:19:42 +0000

    Added comment

--- src/com/openexchange/tools/servlet/http/Tools.java
+++ src/com/openexchange/tools/servlet/http/Tools.java
@@ -64,82 +64,84 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Convenience methods for servlets.
+ * 
  * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
  */
 public final class Tools {
 
-    /**
-     * Pattern for the HTTP header date format.
-     */
-    private static final String DATE_PATTERN =
-        "EEE',' dd MMMM yyyy HH:mm:ss z";
+	/**
+	 * Pattern for the HTTP header date format.
+	 */
+	private static final String DATE_PATTERN = "EEE',' dd MMMM yyyy HH:mm:ss z";
 
-    /**
-     * DateFormat for HTTP header.
-     */
-    public static final DateFormat HEADER_DATEFORMAT;
+	/**
+	 * DateFormat for HTTP header.
+	 */
+	public static final DateFormat HEADER_DATEFORMAT;
 
-    /**
-     * Lock for the HTTP header date format.
-     */
-    private static final Lock DATEFORMAT_LOCK = new ReentrantLock(true);
+	/**
+	 * Lock for the HTTP header date format.
+	 */
+	private static final Lock DATEFORMAT_LOCK = new ReentrantLock(true);
 
-    /**
-     * Cache-Control HTTP header name.
-     */
-    private static final String CACHE_CONTROL_KEY = "Cache-Control";
+	/**
+	 * Cache-Control HTTP header name.
+	 */
+	private static final String CACHE_CONTROL_KEY = "Cache-Control";
 
-    /**
-     * First Cache-Control value.
-     */
-    private static final String CACHE_VALUE1 =
-        "no-store, no-cache, must-revalidate";
+	/**
+	 * First Cache-Control value.
+	 */
+	private static final String CACHE_VALUE1 = "no-store, no-cache, must-revalidate";
 
-    /**
-     * Second Cache-Control value.
-     */
-    private static final String CACHE_VALUE2 =
-        "post-check=0, pre-check=0";
+	/**
+	 * Second Cache-Control value.
+	 */
+	private static final String CACHE_VALUE2 = "post-check=0, pre-check=0";
 
-    /**
-     * Expires HTTP header name.
-     */
-    private static final String EXPIRES_KEY = "Expires";
+	/**
+	 * Expires HTTP header name.
+	 */
+	private static final String EXPIRES_KEY = "Expires";
 
-    /**
-     * Expires HTTP header value.
-     */
-    private static final String EXPIRES_DATE;
+	/**
+	 * Expires HTTP header value.
+	 */
+	private static final String EXPIRES_DATE;
 
-    /**
-     * Pragma HTTP header key.
-     */
-    private static final String PRAGMA_KEY = "Pragma";
+	/**
+	 * Pragma HTTP header key.
+	 */
+	private static final String PRAGMA_KEY = "Pragma";
 
-    /**
-     * Pragma HTTP header value.
-     */
-    private static final String PRAGMA_VALUE = "no-cache";
+	/**
+	 * Pragma HTTP header value.
+	 */
+	private static final String PRAGMA_VALUE = "no-cache";
 
-    /**
-     * Prevent instanciation.
-     */
-    private Tools() {
-        super();
-    }
+	/**
+	 * Prevent instanciation.
+	 */
+	private Tools() {
+		super();
+	}
 
-    /**
-     * The magic spell to disable caching.
-     * @param resp the servlet response.
-     */
-    public static void disableCaching(final HttpServletResponse resp) {
-        resp.addHeader(EXPIRES_KEY, EXPIRES_DATE);
-        resp.addHeader(CACHE_CONTROL_KEY, CACHE_VALUE1);
-        resp.addHeader(CACHE_CONTROL_KEY, CACHE_VALUE2);
-        resp.addHeader(PRAGMA_KEY, PRAGMA_VALUE);
-    }
-    
-    /**
+	/**
+	 * The magic spell to disable caching. Do not use these headers if response
+	 * is directly written into servlet's output stream to initiate a download
+	 * 
+	 * @param resp
+	 *            the servlet response.
+	 * @see #removeCachingHeader(HttpServletResponse)
+	 */
+	public static void disableCaching(final HttpServletResponse resp) {
+		resp.addHeader(EXPIRES_KEY, EXPIRES_DATE);
+		resp.addHeader(CACHE_CONTROL_KEY, CACHE_VALUE1);
+		resp.addHeader(CACHE_CONTROL_KEY, CACHE_VALUE2);
+		resp.addHeader(PRAGMA_KEY, PRAGMA_VALUE);
+	}
+
+	/**
 	 * Remove <tt>Pragma</tt> response header value if we are going to write
 	 * directly into servlet's output stream cause then some browsers do not
 	 * allow this header
@@ -153,23 +155,23 @@ public final class Tools {
 		resp.setHeader(EXPIRES_KEY, null);
 	}
 
-    /**
+	/**
 	 * Formats a date for http headers.
 	 * 
 	 * @param date
 	 *            date to format.
 	 * @return the string with the formated date.
 	 */
-    public static String formatHeaderDate(final Date date) {
-        DATEFORMAT_LOCK.lock();
-        try {
-            return HEADER_DATEFORMAT.format(date);
-        } finally {
-            DATEFORMAT_LOCK.unlock();
-        }
-    }
-    
-    /**
+	public static String formatHeaderDate(final Date date) {
+		DATEFORMAT_LOCK.lock();
+		try {
+			return HEADER_DATEFORMAT.format(date);
+		} finally {
+			DATEFORMAT_LOCK.unlock();
+		}
+	}
+
+	/**
 	 * Parses a date from a http date header
 	 * 
 	 * @param str -
@@ -186,58 +188,59 @@ public final class Tools {
 		}
 	}
 
-    /**
-     * HTTP header name containing the user agent.
-     */
-    public static final String HEADER_AGENT = "User-Agent";
+	/**
+	 * HTTP header name containing the user agent.
+	 */
+	public static final String HEADER_AGENT = "User-Agent";
 
-    /**
-     * HTTP header name containing the content type of the body.
-     */
-    public static final String HEADER_TYPE = "Content-Type";
+	/**
+	 * HTTP header name containing the content type of the body.
+	 */
+	public static final String HEADER_TYPE = "Content-Type";
 
-    /**
-     * HTTP header name containing the site that caused the request.
-     */
-    public static final String HEADER_REFERER = "Referer";
+	/**
+	 * HTTP header name containing the site that caused the request.
+	 */
+	public static final String HEADER_REFERER = "Referer";
 
-    /**
-     * HTTP header name containing the length of the body.
-     */
-    public static final String HEADER_LENGTH = "Content-Length";
+	/**
+	 * HTTP header name containing the length of the body.
+	 */
+	public static final String HEADER_LENGTH = "Content-Length";
 
-    /**
-     * This method integrates interesting HTTP header values into a string for
-     * logging purposes. This is usefull if a client sent an illegal request for
-     * discovering the cause of the illegal request.
-     * @param req the servlet request.
-     * @return a string containing interesting HTTP headers.
-     */
-    public static String logHeaderForError(final HttpServletRequest req) {
-        final StringBuilder message = new StringBuilder();
-        message.append("|\n");
-        message.append(HEADER_AGENT);
-        message.append(": ");
-        message.append(req.getHeader(HEADER_AGENT));
-        message.append('\n');
-        message.append(HEADER_TYPE);
-        message.append(": ");
-        message.append(req.getHeader(HEADER_TYPE));
-        message.append('\n');
-        message.append(HEADER_REFERER);
-        message.append(": ");
-        message.append(req.getHeader(HEADER_REFERER));
-        message.append('\n');
-        message.append(HEADER_LENGTH);
-        message.append(": ");
-        message.append(req.getHeader(HEADER_LENGTH));
-        return message.toString();
-    }
+	/**
+	 * This method integrates interesting HTTP header values into a string for
+	 * logging purposes. This is usefull if a client sent an illegal request for
+	 * discovering the cause of the illegal request.
+	 * 
+	 * @param req
+	 *            the servlet request.
+	 * @return a string containing interesting HTTP headers.
+	 */
+	public static String logHeaderForError(final HttpServletRequest req) {
+		final StringBuilder message = new StringBuilder();
+		message.append("|\n");
+		message.append(HEADER_AGENT);
+		message.append(": ");
+		message.append(req.getHeader(HEADER_AGENT));
+		message.append('\n');
+		message.append(HEADER_TYPE);
+		message.append(": ");
+		message.append(req.getHeader(HEADER_TYPE));
+		message.append('\n');
+		message.append(HEADER_REFERER);
+		message.append(": ");
+		message.append(req.getHeader(HEADER_REFERER));
+		message.append('\n');
+		message.append(HEADER_LENGTH);
+		message.append(": ");
+		message.append(req.getHeader(HEADER_LENGTH));
+		return message.toString();
+	}
 
-    static {
-        HEADER_DATEFORMAT = new SimpleDateFormat(DATE_PATTERN, Locale
-            .ENGLISH);
-        HEADER_DATEFORMAT.setTimeZone(TimeZone.getTimeZone("GMT"));
-        EXPIRES_DATE = HEADER_DATEFORMAT.format(new Date(799761600000L));
-    }
+	static {
+		HEADER_DATEFORMAT = new SimpleDateFormat(DATE_PATTERN, Locale.ENGLISH);
+		HEADER_DATEFORMAT.setTimeZone(TimeZone.getTimeZone("GMT"));
+		EXPIRES_DATE = HEADER_DATEFORMAT.format(new Date(799761600000L));
+	}
 }
