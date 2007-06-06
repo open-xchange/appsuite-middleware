d4f13462e0a26ef2da8c2320cb08c035e2f5e3a2
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-06-06 11:35:00 +0000

    - moved PoolException

--- src/com/openexchange/admin/storage/sqlStorage/OXAdminPoolDBPoolExtension.java
+++ src/com/openexchange/admin/storage/sqlStorage/OXAdminPoolDBPoolExtension.java
@@ -48,7 +48,7 @@
  */
 package com.openexchange.admin.storage.sqlStorage;
 
-import com.openexchange.admin.exceptions.PoolException;
+import com.openexchange.admin.rmi.exceptions.PoolException;
 import com.openexchange.admin.storage.sqlStorage.OXAdminPoolInterfaceExtension;
 import com.openexchange.admin.tools.PropertyHandler;
 import com.openexchange.database.Database;
