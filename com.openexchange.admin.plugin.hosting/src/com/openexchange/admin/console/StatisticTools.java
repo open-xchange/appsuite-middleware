16654c7b39906176767b8578ac62a450442eff83
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-06-27 16:57:01 +0000

    - removed old runtimestats and check in of new version

---
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/StatisticTools.java
@@ -0,0 +1,317 @@
+package com.openexchange.admin.console;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.IntrospectionException;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanException;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import javax.management.RuntimeMBeanException;
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+import com.openexchange.admin.console.AdminParser.MissingOptionException;
+import com.openexchange.admin.console.CmdLineParser.IllegalOptionValueException;
+import com.openexchange.admin.console.CmdLineParser.Option;
+import com.openexchange.admin.console.CmdLineParser.UnknownOptionException;
+
+public class StatisticTools extends BasicCommandlineOptions {
+
+    private static final String JMX_SERVER_PORT = "9999";
+
+    private static final String JMX_ADMIN_PORT = "9998";
+
+    private static final char OPT_HOST_SHORT = 'H';
+
+    private static final String OPT_HOST_LONG = "host";
+
+    private static final char OPT_STATS_SHORT = 'x';
+
+    private static final String OPT_STATS_LONG = "xchangestats";
+
+    private static final char OPT_RUNTIME_STATS_SHORT = 'r';
+
+    private static final String OPT_RUNTIME_STATS_LONG = "runtimestats";
+
+    private static final char OPT_OS_STATS_SHORT = 'o';
+
+    private static final String OPT_OS_STATS_LONG = "osstats";
+
+    private static final char OPT_THREADING_STATS_SHORT = 't';
+
+    private static final String OPT_THREADING_STATS_LONG = "threadingstats";
+
+    private static final char OPT_ALL_STATS_SHORT = 'a';
+
+    private static final String OPT_ALL_STATS_LONG = "allstats";
+
+    private static final char OPT_ADMINDAEMON_STATS_SHORT = 'A';
+
+    private static final String OPT_ADMINDAEMON_STATS_LONG = "admindaemonstats";
+
+    private String JMX_HOST = "localhost";
+
+    private String ox_jmx_url = null;
+
+//    MBeanServerConnection mbc = null;
+
+    JMXConnector c = null;
+
+    private Option host = null;
+
+    private Option xchangestats = null;
+
+    private Option runtimestats = null;
+
+    private Option osstats = null;
+
+    private Option threadingstats = null;
+
+    private Option allstats = null;
+
+    private Option admindaemonstats;
+
+    /**
+     * This method is called after a hostname change and input parsing, because
+     * the url depends on both steps
+     */
+    private void updatejmxurl(final boolean showAdminStats) {
+        final String jmxPort = showAdminStats ? JMX_ADMIN_PORT : JMX_SERVER_PORT;
+        this.ox_jmx_url = "service:jmx:rmi:///jndi/rmi://" + JMX_HOST + ':' + jmxPort + "/server";
+    }
+
+    public static void main(final String args[]) {
+        final StatisticTools st = new StatisticTools();
+        st.start(args);
+    }
+
+    public void start(final String args[]) {
+        final AdminParser parser = new AdminParser("statistictools");
+
+        setOptions(parser);
+
+        try {
+            parser.ownparse(args);
+
+            boolean admin = false;
+            final String host = (String) parser.getOptionValue(this.host);
+            if (null != host) {
+                JMX_HOST = host;
+            }
+            if (null != parser.getOptionValue(this.admindaemonstats)) {
+                admin = true;
+            }
+            int count = 0;
+            if (null != parser.getOptionValue(this.xchangestats)) {
+                final MBeanServerConnection initConnection = initConnection(admin);
+                showOXData(initConnection, admin);
+                count++;
+            }
+            if (null != parser.getOptionValue(this.runtimestats)) {
+                if (0 == count) {
+                    final MBeanServerConnection initConnection = initConnection(admin);
+                    showStats(initConnection, "sun.management.RuntimeImpl");
+                    showMemoryPoolData(initConnection);
+                }
+                count++;
+            }
+            if (null != parser.getOptionValue(this.osstats)) {
+                if (0 == count) {
+                    final MBeanServerConnection initConnection = initConnection(admin);
+                    showStats(initConnection, "com.sun.management.UnixOperatingSystem");
+                }
+                count++;
+
+            }
+            if (null != parser.getOptionValue(this.threadingstats)) {
+                if (0 == count) {
+                    final MBeanServerConnection initConnection = initConnection(admin);
+                    showSysThreadingData(initConnection);
+                }
+                count++;
+
+            }
+            if (null != parser.getOptionValue(this.allstats)) {
+                if (0 == count) {
+                    final MBeanServerConnection initConnection = initConnection(admin);
+                    showOXData(initConnection, admin);
+                    showStats(initConnection, "com.sun.management.UnixOperatingSystem");
+                    showStats(initConnection, "sun.management.RuntimeImpl");
+                    showMemoryPoolData(initConnection);
+                    showSysThreadingData(initConnection);
+                }
+                count++;
+
+            }
+            if (0 == count) {
+                System.err.println(new StringBuilder("No option selected (").append(OPT_STATS_LONG).append(", ").append(OPT_RUNTIME_STATS_LONG).append(", ").append(OPT_OS_STATS_LONG).append(", ").append(OPT_THREADING_STATS_LONG).append(", ").append(OPT_ALL_STATS_LONG).append(", ").append(OPT_ADMINDAEMON_STATS_LONG).append(")"));
+                parser.printUsage();
+            } else if (count > 1) {
+                System.err.println("More than one of the stat options given. Using the first one only");
+            }
+        } catch (final IllegalOptionValueException e) {
+            printError("Illegal option value : " + e.getMessage());
+            parser.printUsage();
+            sysexit(SYSEXIT_ILLEGAL_OPTION_VALUE);
+        } catch (final UnknownOptionException e) {
+            printError("Unrecognized options on the command line: " + e.getMessage());
+            parser.printUsage();
+            sysexit(SYSEXIT_UNKNOWN_OPTION);
+        } catch (final MissingOptionException e) {
+            printError(e.getMessage());
+            parser.printUsage();
+            sysexit(SYSEXIT_MISSING_OPTION);
+        } catch (final IOException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final InstanceNotFoundException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final AttributeNotFoundException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final IntrospectionException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final MBeanException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final ReflectionException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final InterruptedException e) {
+            printServerException(e);
+            sysexit(1);
+        } finally {
+            closeConnection();
+        }
+    }
+
+    private void setOptions(AdminParser parser) {
+        this.host = setShortLongOpt(parser, OPT_HOST_SHORT, OPT_HOST_LONG, "host", "specifies the host", false);
+        this.xchangestats = setShortLongOpt(parser, OPT_STATS_SHORT, OPT_STATS_LONG, "shows Open-Xchange stats", false, false);
+        this.runtimestats = setShortLongOpt(parser, OPT_RUNTIME_STATS_SHORT, OPT_RUNTIME_STATS_LONG, "shows Java runtime stats", false, false);
+        this.osstats = setShortLongOpt(parser, OPT_OS_STATS_SHORT, OPT_OS_STATS_LONG, "shows operating system stats", false, false);
+        this.threadingstats = setShortLongOpt(parser, OPT_THREADING_STATS_SHORT, OPT_THREADING_STATS_LONG, "shows threading stats", false, false);
+        this.allstats = setShortLongOpt(parser, OPT_ALL_STATS_SHORT, OPT_ALL_STATS_LONG, "shows all stats", false, false);
+        this.admindaemonstats = setShortLongOpt(parser, OPT_ADMINDAEMON_STATS_SHORT, OPT_ADMINDAEMON_STATS_LONG, "shows stats for the admin instead of the groupware", false, false);
+    }
+
+    private MBeanServerConnection initConnection(final boolean adminstats) throws InterruptedException, IOException {
+        updatejmxurl(adminstats);
+        // Set timeout here, it is given in ms
+        final long timeout = 1000;
+        final JMXServiceURL serviceurl = new JMXServiceURL(ox_jmx_url);
+        // If server needs auth
+        // Hashtable h = new Hashtable();
+        // String[] credentials = new String[] {"username" ,"password" };
+        // h.put("jmx.remote.credentials", credentials);
+        // c = JMXConnectorFactory.connect(u,h);
+        final IOException[] exc = new IOException[1];
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    c = JMXConnectorFactory.connect(serviceurl);
+                } catch (IOException e) {
+                    exc[0] = e;
+                }
+            }
+        };
+        t.start();
+        t.join(timeout);
+        if (t.isAlive()) {
+            t.interrupt();
+            throw new InterruptedIOException("Connection timed out");
+        }
+        if (exc[0] != null) {
+            throw exc[0];
+        }
+        return c.getMBeanServerConnection();
+    }
+
+    private void closeConnection() {
+
+        if (c != null) {
+            try {
+                c.close();
+            } catch (final IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+    private void showStats(final MBeanServerConnection mbc, final String class_name) throws IOException, InstanceNotFoundException, MBeanException, AttributeNotFoundException, ReflectionException, IntrospectionException {
+        final Set data_set = mbc.queryMBeans(null, null);
+        final Iterator itr = data_set.iterator();
+        while (itr.hasNext()) {
+            final ObjectInstance oin = (ObjectInstance) itr.next();
+
+            final ObjectName obj = oin.getObjectName();
+            final MBeanInfo info = mbc.getMBeanInfo(obj);
+            if (info.getClassName().equals(class_name)) {
+                final String ocname = obj.getCanonicalName();
+                final MBeanAttributeInfo[] attrs = info.getAttributes();
+                if (attrs.length > 0) {
+                    for (final MBeanAttributeInfo element : attrs) {
+                        try {
+                            final Object o = mbc.getAttribute(obj, element.getName());
+                            if (o != null) {
+                                final StringBuilder sb = new StringBuilder(ocname).append(",").append(element.getName()).append(" = ");
+                                if (o instanceof CompositeDataSupport) {
+                                    final CompositeDataSupport c = (CompositeDataSupport) o;
+                                    sb.append("[init=").append(c.get("init")).append(",max=").append(c.get("max")).append(",committed=").append(c.get("committed")).append(",used=").append(c.get("used")).append("]");
+                                    System.out.println(sb.toString());
+                                } else {
+                                    if (o instanceof String[]) {
+                                        final String[] c = (String[]) o;
+                                        System.out.println(sb.append(Arrays.toString(c)).toString());
+                                    } else if (o instanceof long[]) {
+                                        final long[] l = (long[]) o;
+                                        System.out.println(sb.append(Arrays.toString(l)).toString());
+                                    } else {
+                                        System.out.println(sb.append(o.toString()).toString());
+                                    }
+                                }
+                            }
+                        } catch (final RuntimeMBeanException e) {
+                            // eat it
+                        }                        
+                    }
+                }
+            }
+        }
+    }
+
+    private void showMemoryPoolData(final MBeanServerConnection mbc) throws InstanceNotFoundException, AttributeNotFoundException, IntrospectionException, MBeanException, ReflectionException, IOException {
+        showStats(mbc, "sun.management.MemoryPoolImpl");
+    }
+
+    private void showSysThreadingData(final MBeanServerConnection mbc) throws InstanceNotFoundException, AttributeNotFoundException, IntrospectionException, MBeanException, ReflectionException, IOException {
+        showStats(mbc, "sun.management.ThreadImpl");
+    }
+
+    private void showOXData(final MBeanServerConnection mbc, final boolean admin) throws InstanceNotFoundException, AttributeNotFoundException, IntrospectionException, MBeanException, ReflectionException, IOException {
+        if (admin) {
+            showStats(mbc, "com.openexchange.admin.tools.monitoring.Monitor");
+        } else {
+            showStats(mbc, "com.openexchange.tools.ajp13.monitoring.AJPv13ServerThreadsMonitor");
+            showStats(mbc, "com.openexchange.tools.ajp13.monitoring.AJPv13ListenerMonitor");
+            showStats(mbc, "com.openexchange.monitoring.GeneralMonitor");
+            showStats(mbc, "com.openexchange.api2.MailInterfaceMonitor");
+            showStats(mbc, "com.openexchange.database.ConnectionPool");
+        }
+    }
+}
