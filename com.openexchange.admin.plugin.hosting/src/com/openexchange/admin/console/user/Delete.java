5b0ab802325c9e356ceccf25cb44eefa8f6f80b5
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-07-06 09:59:21 +0000

    - refactoring of user command line tools

--- com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/user/Delete.java
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/user/Delete.java
@@ -1,15 +1,9 @@
 
 package com.openexchange.admin.console.user;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import com.openexchange.admin.console.AdminParser;
-import com.openexchange.admin.console.AdminParser.MissingOptionException;
-import com.openexchange.admin.console.CmdLineParser.IllegalOptionValueException;
-import com.openexchange.admin.console.CmdLineParser.UnknownOptionException;
 import com.openexchange.admin.rmi.OXUserInterface;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
@@ -21,7 +15,7 @@ import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
 import com.openexchange.admin.rmi.exceptions.NoSuchUserException;
 import com.openexchange.admin.rmi.exceptions.StorageException;
 
-public class Delete extends UserAbstraction {
+public class Delete extends DeleteCore {
 
     public static void main(final String[] args) {
         new Delete(args);
@@ -31,74 +25,15 @@ public class Delete extends UserAbstraction {
 
         final AdminParser parser = new AdminParser("delete");
 
-        // set all needed options in our parser
-        setDefaultCommandLineOptions(parser);
-        setIdOption(parser);
-        try {
-            parser.ownparse(args2);
+        commonfunctions(parser, args2);
+    }
 
-            final Context ctx = new Context(DEFAULT_CONTEXT);
+    @Override
+    protected void maincall(final AdminParser parser, final OXUserInterface oxusr, final Context ctx, final User usr, final Credentials auth) throws RemoteException, StorageException, InvalidCredentialsException, NoSuchContextException, InvalidDataException, DatabaseUpdateException, NoSuchUserException {
+        oxusr.delete(ctx, usr, auth);
+    }
 
-            if (parser.getOptionValue(this.contextOption) != null) {
-                ctx.setID(Integer.parseInt((String) parser.getOptionValue(this.contextOption)));
-            }
-
-            final Credentials auth = new Credentials((String) parser.getOptionValue(this.adminUserOption), (String) parser.getOptionValue(this.adminPassOption));
-
-            final OXUserInterface oxres = (OXUserInterface) Naming.lookup(RMI_HOSTNAME + OXUserInterface.RMI_NAME);
-
-            final int id = Integer.valueOf((String) parser.getOptionValue(this.idOption));
-
-            final User usr = new User(id);
-
-            // WITHOUT extensions
-            oxres.delete(ctx, usr, auth);
-            sysexit(0);
-        } catch (final java.rmi.ConnectException neti) {
-            printError(neti.getMessage());
-            sysexit(SYSEXIT_COMMUNICATION_ERROR);
-        } catch (final java.lang.NumberFormatException num) {
-            printInvalidInputMsg("Ids must be numbers!");
-            sysexit(1);
-        } catch (final MalformedURLException e) {
-            printServerException(e);
-            sysexit(1);
-        } catch (final RemoteException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_REMOTE_ERROR);
-        } catch (final NotBoundException e) {
-            printServerException(e);
-            sysexit(1);
-        } catch (final StorageException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_SERVERSTORAGE_ERROR);
-        } catch (final InvalidCredentialsException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_INVALID_CREDENTIALS);
-        } catch (final NoSuchContextException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_NO_SUCH_CONTEXT);
-        } catch (final InvalidDataException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_INVALID_DATA);
-        } catch (final IllegalOptionValueException e) {
-            printError("Illegal option value : " + e.getMessage());
-            parser.printUsage();
-            sysexit(SYSEXIT_ILLEGAL_OPTION_VALUE);
-        } catch (final UnknownOptionException e) {
-            printError("Unrecognized options on the command line: " + e.getMessage());
-            parser.printUsage();
-            sysexit(SYSEXIT_UNKNOWN_OPTION);
-        } catch (final MissingOptionException e) {
-            printError(e.getMessage());
-            parser.printUsage();
-            sysexit(SYSEXIT_MISSING_OPTION);
-        } catch (final DatabaseUpdateException e) {
-            printServerException(e);
-            sysexit(1);
-        } catch (final NoSuchUserException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_NO_SUCH_USER);
-        }
+    @Override
+    protected void setFurtherOptions(AdminParser parser) {
     }
 }
