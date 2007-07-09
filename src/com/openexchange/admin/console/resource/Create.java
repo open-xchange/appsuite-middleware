00bcc0d42d1898c15c8795bf4f4eaa5b134d9d3e
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-07-09 11:44:08 +0000

    - refactoring of resource command line tools

--- src/com/openexchange/admin/console/resource/Create.java
+++ src/com/openexchange/admin/console/resource/Create.java
@@ -1,26 +1,15 @@
 
 package com.openexchange.admin.console.resource;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import com.openexchange.admin.console.AdminParser;
-import com.openexchange.admin.console.AdminParser.MissingOptionException;
-import com.openexchange.admin.console.CmdLineParser.IllegalOptionValueException;
-import com.openexchange.admin.console.CmdLineParser.UnknownOptionException;
 import com.openexchange.admin.rmi.OXResourceInterface;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
 import com.openexchange.admin.rmi.dataobjects.Resource;
-import com.openexchange.admin.rmi.exceptions.DatabaseUpdateException;
-import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
-import com.openexchange.admin.rmi.exceptions.InvalidDataException;
-import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
-import com.openexchange.admin.rmi.exceptions.StorageException;
 
-public class Create extends ResourceAbstraction {
+public class Create extends CreateCore {
 
     public static void main(final String[] args) {
         new Create(args);
@@ -29,87 +18,18 @@ public class Create extends ResourceAbstraction {
     public Create(final String[] args2) {
 
         final AdminParser parser = new AdminParser("create");
-
-        setOptions(parser);
-
-        try {
-            parser.ownparse(args2);
-
-            final Context ctx = new Context(DEFAULT_CONTEXT);
-
-            if (parser.getOptionValue(this.contextOption) != null) {
-                ctx.setID(Integer.parseInt((String) parser.getOptionValue(this.contextOption)));
-            }
-
-            final Credentials auth = new Credentials((String) parser.getOptionValue(this.adminUserOption), (String) parser.getOptionValue(this.adminPassOption));
-
-            final OXResourceInterface oxres = (OXResourceInterface) Naming.lookup(RMI_HOSTNAME + OXResourceInterface.RMI_NAME);
-            final Resource res = new Resource();
-
-            res.setAvailable(Boolean.parseBoolean((String) parser.getOptionValue(this.resourceAvailableOption)));
-
-            if (parser.getOptionValue(this.resourceDescriptionOption) != null) {
-                res.setDescription((String) parser.getOptionValue(this.resourceDescriptionOption));
-            }
-            res.setDisplayname((String) parser.getOptionValue(this.resourceDisplayNameOption));
-            res.setEmail((String) parser.getOptionValue(this.resourceEmailOption));
-            res.setName((String) parser.getOptionValue(this.resourceNameOption));
-            System.out.println(oxres.create(ctx, res, auth));
-
-            sysexit(0);
-        } catch (final java.rmi.ConnectException neti) {
-            printError(neti.getMessage());
-            sysexit(SYSEXIT_COMMUNICATION_ERROR);
-        } catch (final java.lang.NumberFormatException num) {
-            printInvalidInputMsg("Ids must be numbers!");
-            sysexit(1);
-        } catch (final MalformedURLException e) {
-            printServerException(e);
-            sysexit(1);
-        } catch (final RemoteException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_REMOTE_ERROR);
-        } catch (final NotBoundException e) {
-            printServerException(e);
-            sysexit(1);
-        } catch (final StorageException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_SERVERSTORAGE_ERROR);
-        } catch (final InvalidCredentialsException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_INVALID_CREDENTIALS);
-        } catch (final NoSuchContextException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_NO_SUCH_CONTEXT);
-        } catch (final InvalidDataException e) {
-            printServerException(e);
-            sysexit(SYSEXIT_INVALID_DATA);
-        } catch (final IllegalOptionValueException e) {
-            printError("Illegal option value : " + e.getMessage());
-            parser.printUsage();
-            sysexit(SYSEXIT_ILLEGAL_OPTION_VALUE);
-        } catch (final UnknownOptionException e) {
-            printError("Unrecognized options on the command line: " + e.getMessage());
-            parser.printUsage();
-            sysexit(SYSEXIT_UNKNOWN_OPTION);
-        } catch (final MissingOptionException e) {
-            printError(e.getMessage());
-            parser.printUsage();
-            sysexit(SYSEXIT_MISSING_OPTION);
-        } catch (final DatabaseUpdateException e) {
-            printServerException(e);
-            sysexit(1);
-        }
+        
+        commonfunctions(parser, args2);
     }
 
-    private void setOptions(final AdminParser parser) {
-        setDefaultCommandLineOptions(parser);
-
-        setNameOption(parser, true);
-        setDisplayNameOption(parser, true);
-        setAvailableOption(parser, false);
-        setDescriptionOption(parser, false);
-        setEmailOption(parser, true);
-
+    @Override
+    protected void maincall(AdminParser parser, OXResourceInterface oxres, Context ctx, Resource res, Credentials auth) throws RemoteException {
+        // Nothing to do here
     }
+
+    @Override
+    protected void setFurtherOptions(AdminParser parser) {
+        // Nothing to do here
+    }
+
 }
