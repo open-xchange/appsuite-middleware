ca8afdc45019b9dff9b8ed13815d17a4c18fade7
Manuel Kraft <manuel.kraft@open-xchange.com>
2007-07-09 09:28:41 +0000

    bugfix Bugzilla Bug 7302 changecontext is missing

--- com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/context/ContextAbtraction.java
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/context/ContextAbtraction.java
@@ -19,9 +19,17 @@ public abstract class ContextAbtraction extends UserAbstraction {
     private final static char OPT_QUOTA_SHORT = 'q';
     private final static String OPT_QUOTA_LONG = "quota";
     
+    private final static char OPT_CONTEXT_ADD_LOGIN_MAPPINGS_SHORT = 'L';
+    private final static String OPT_CONTEXT_ADD_LOGIN_MAPPINGS_LONG = "addmapping";
+    
+    private final static char OPT_CONTEXT_REMOVE_LOGIN_MAPPINGS_SHORT = 'R';
+    private final static String OPT_CONTEXT_REMOVE_LOGIN_MAPPINGS_LONG = "removemapping";
+    
     protected Option searchOption = null;
     protected Option commonIDOption = null;
     protected Option contextNameOption = null;
+    protected Option addLoginMappingOption = null;
+    protected Option removeLoginMappingOption = null;
     protected Option maintenanceReasonIDOption = null;
     protected Option filestoreContextQuotaOption = null;
 
@@ -50,5 +58,13 @@ public abstract class ContextAbtraction extends UserAbstraction {
     
     protected void setContextNameOption(final AdminParser parser,final boolean required ){
         this.contextNameOption = setShortLongOpt(parser, OPT_NAME_CONTEXT_NAME_SHORT,OPT_NAME_CONTEXT_NAME_LONG,OPT_NAME_CONTEXT_NAME_DESCRIPTION,true, required);
+    }
+    
+    protected void setAddMappingOption(final AdminParser parser,final boolean required ){
+        this.addLoginMappingOption = setShortLongOpt(parser, OPT_CONTEXT_ADD_LOGIN_MAPPINGS_SHORT,OPT_CONTEXT_ADD_LOGIN_MAPPINGS_LONG,"Add login mappings.Seperated by \",\"",true, required);
+    }
+    
+    protected void setRemoveMappingOption(final AdminParser parser,final boolean required ){
+        this.removeLoginMappingOption = setShortLongOpt(parser, OPT_CONTEXT_REMOVE_LOGIN_MAPPINGS_SHORT,OPT_CONTEXT_REMOVE_LOGIN_MAPPINGS_LONG,"Remove login mappings.Seperated by \",\"",true, required);
     }
 }
