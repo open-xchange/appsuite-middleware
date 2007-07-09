20fcfe2031752d6399a53d04a71738adab0bb30a
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-07-09 09:57:29 +0000

    - refactoring of group command line tools

---
+++ com.openexchange.admin/src/com/openexchange/admin/console/group/DeleteCore.java
@@ -0,0 +1,105 @@
+package com.openexchange.admin.console.group;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import com.openexchange.admin.console.AdminParser;
+import com.openexchange.admin.console.AdminParser.MissingOptionException;
+import com.openexchange.admin.console.CmdLineParser.IllegalOptionValueException;
+import com.openexchange.admin.console.CmdLineParser.UnknownOptionException;
+import com.openexchange.admin.rmi.OXGroupInterface;
+import com.openexchange.admin.rmi.dataobjects.Context;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.dataobjects.Group;
+import com.openexchange.admin.rmi.exceptions.DatabaseUpdateException;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
+import com.openexchange.admin.rmi.exceptions.NoSuchGroupException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+
+public abstract class DeleteCore extends GroupAbstraction {
+    
+    protected final void setOptions(final AdminParser parser) {
+        setDefaultCommandLineOptions(parser);
+
+        // create id option for this command line tool
+        setGroupIdOption(parser, true);
+    }
+
+    protected final void commonfunctions(final AdminParser parser, final String[] args) {
+        setOptions(parser);
+
+        try {
+            parser.ownparse(args);
+
+            final Context ctx = new Context(DEFAULT_CONTEXT);
+
+            if (parser.getOptionValue(this.contextOption) != null) {
+                ctx.setID(Integer.parseInt((String) parser.getOptionValue(this.contextOption)));
+            }
+
+            final Credentials auth = new Credentials((String) parser.getOptionValue(this.adminUserOption), (String) parser.getOptionValue(this.adminPassOption));
+
+            final OXGroupInterface oxgrp = (OXGroupInterface) Naming.lookup(RMI_HOSTNAME + OXGroupInterface.RMI_NAME);
+
+            final int groupid = Integer.valueOf((String) parser.getOptionValue(this.IdOption));
+
+            final Group grp = new Group(groupid);
+            
+            maincall(parser, oxgrp, ctx, grp, auth);
+
+            oxgrp.delete(ctx, new Group[] { grp }, auth);
+            sysexit(0);
+        } catch (final java.rmi.ConnectException neti) {
+            printError(neti.getMessage());
+            sysexit(SYSEXIT_COMMUNICATION_ERROR);
+        } catch (final NumberFormatException e) {
+            printInvalidInputMsg("The Option for the id of the group contains no parseable integer number");
+            sysexit(1);
+        } catch (final MalformedURLException e) {
+            printServerResponse("Error conntecting to server: " + e.getMessage());
+            sysexit(1);
+        } catch (final RemoteException e) {
+            printServerException(e);
+            sysexit(SYSEXIT_REMOTE_ERROR);
+        } catch (final NotBoundException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final InvalidCredentialsException e) {
+            printServerException(e);
+            sysexit(SYSEXIT_INVALID_CREDENTIALS);
+        } catch (final NoSuchContextException e) {
+            printServerException(e);
+            sysexit(SYSEXIT_NO_SUCH_CONTEXT);
+        } catch (final StorageException e) {
+            printServerException(e);
+            sysexit(SYSEXIT_SERVERSTORAGE_ERROR);
+        } catch (final InvalidDataException e) {
+            printServerException(e);
+            sysexit(SYSEXIT_INVALID_DATA);
+        } catch (final IllegalOptionValueException e) {
+            printError("Illegal option value : " + e.getMessage());
+            parser.printUsage();
+            sysexit(SYSEXIT_ILLEGAL_OPTION_VALUE);
+        } catch (final UnknownOptionException e) {
+            printError("Unrecognized options on the command line: " + e.getMessage());
+            parser.printUsage();
+            sysexit(SYSEXIT_UNKNOWN_OPTION);
+        } catch (final MissingOptionException e) {
+            printError(e.getMessage());
+            parser.printUsage();
+            sysexit(SYSEXIT_MISSING_OPTION);
+        } catch (final DatabaseUpdateException e) {
+            printServerException(e);
+            sysexit(1);
+        } catch (final NoSuchGroupException e) {
+            printServerException(e);
+            sysexit(SYSEXIT_NO_SUCH_GROUP);
+        }
+    }
+    
+    protected abstract void maincall(final AdminParser parser, final OXGroupInterface oxgrp, final Context ctx, final Group grp, final Credentials auth) throws RemoteException;
+}
