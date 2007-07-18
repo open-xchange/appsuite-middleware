9fa9d5ebd529f8600ef1e4831a0e588e3eec0faf
thorben <thorben>
2007-07-18 14:55:53 +0000

    Bugfix #8498: Additional check when reading an infostore document's MIME type setting

--- src/com/openexchange/ajax/Mail.java
+++ src/com/openexchange/ajax/Mail.java
@@ -2522,8 +2522,9 @@ public class Mail extends PermissionServlet implements UploadListener {
 				mao.setSize(docMeta.getFileSize());
 				mao.setContent(null);
 				mao.setContentID(JSONMessageAttachmentObject.CONTENT_NONE);
-				mao.setContentType(docMeta.getFileMIMEType() == null ? MIME_APPLICATION_OCTET_STREAM : docMeta
-						.getFileMIMEType());
+				final String docMIMEType = docMeta.getFileMIMEType();
+				mao.setContentType(docMIMEType == null || docMIMEType.length() == 0 ? MIME_APPLICATION_OCTET_STREAM
+						: docMeta.getFileMIMEType());
 				try {
 					mao.setFileName(MimeUtility.encodeText(docMeta.getFileName(), IMAPProperties
 							.getDefaultMimeCharset(), ENC_Q));
