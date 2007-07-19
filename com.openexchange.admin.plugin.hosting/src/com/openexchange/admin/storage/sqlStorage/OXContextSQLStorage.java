634ebd4a02d9623f609cc8a37317c32b6cb46c34
Carsten Hoeger <carsten.hoeger@open-xchange.com>
2007-07-19 17:26:47 +0000

    removed unneeded abstract defs

--- com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/storage/sqlStorage/OXContextSQLStorage.java
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/storage/sqlStorage/OXContextSQLStorage.java
@@ -1,14 +1,6 @@
 
 package com.openexchange.admin.storage.sqlStorage;
 
-import com.openexchange.admin.rmi.dataobjects.Context;
-import com.openexchange.admin.rmi.dataobjects.Database;
-import com.openexchange.admin.rmi.dataobjects.Filestore;
-import com.openexchange.admin.rmi.dataobjects.MaintenanceReason;
-import com.openexchange.admin.rmi.dataobjects.User;
-import com.openexchange.admin.rmi.exceptions.InvalidDataException;
-
-import com.openexchange.admin.rmi.exceptions.StorageException;
 import com.openexchange.admin.storage.interfaces.OXContextStorageInterface;
 
 /**
@@ -20,29 +12,5 @@ import com.openexchange.admin.storage.interfaces.OXContextStorageInterface;
  * 
  */
 public abstract class OXContextSQLStorage extends OXContextStorageInterface {
-
-    abstract public void delete(final Context ctx) throws StorageException;
-
-    abstract public void disableAll(final MaintenanceReason reason) throws StorageException;
-
-    abstract public void disable(final Context ctx, final MaintenanceReason reason) throws StorageException;
-
-    abstract public void enableAll() throws StorageException;
-
-    abstract public void enable(final Context ctx) throws StorageException;
-
-    abstract public Context create( final Context ctx, final User admin_user) throws StorageException,InvalidDataException;
-    
-    abstract public Context getData(final Context ctx) throws StorageException;
-
-    abstract public void moveDatabaseContext(final Context ctx, final Database target_database_id, final MaintenanceReason reason) throws StorageException;
-
-    abstract public String moveContextFilestore(final Context ctx, final Filestore dst_filestore_id, final MaintenanceReason reason) throws StorageException;;
-
-    abstract public Context[] searchContext(final String search_pattern) throws StorageException;
-
-    abstract public Context[] searchContextByDatabase(final Database db_host) throws StorageException;
-
-    abstract public Context[] searchContextByFilestore(final Filestore filestore) throws StorageException;
 
 }
