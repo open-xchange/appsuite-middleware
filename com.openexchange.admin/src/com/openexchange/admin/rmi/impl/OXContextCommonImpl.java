dcd6cd02d0ff3fdc7eeba30c0bf62c9e8f0515cd
dennis <dennis>
2007-07-23 11:55:50 +0000

    - moved BasicAuthenticator to impl

--- com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXContextCommonImpl.java
+++ com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXContextCommonImpl.java
@@ -3,7 +3,6 @@ package com.openexchange.admin.rmi.impl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.openexchange.admin.rmi.BasicAuthenticator;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
 import com.openexchange.admin.rmi.dataobjects.Database;
--- com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXResource.java
+++ com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXResource.java
@@ -47,7 +47,7 @@
  *
  */
 /*
- * $Id: OXResource.java,v 1.38 2007/07/20 15:23:43 dennis Exp $
+ * $Id: OXResource.java,v 1.39 2007/07/23 11:55:50 dennis Exp $
  */
 package com.openexchange.admin.rmi.impl;
 
@@ -66,7 +66,6 @@ import com.openexchange.admin.daemons.ClientAdminThread;
 import com.openexchange.admin.plugins.OXResourcePluginInterface;
 import com.openexchange.admin.plugins.PluginException;
 import com.openexchange.admin.properties.AdminProperties;
-import com.openexchange.admin.rmi.BasicAuthenticator;
 import com.openexchange.admin.rmi.OXResourceInterface;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
