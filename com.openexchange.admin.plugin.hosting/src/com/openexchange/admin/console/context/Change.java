3ee3cf966c3d8c6eb09ec6dec5ce7abd174cdc33
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-07-27 12:46:58 +0000

    - added a common core for changecontext

--- com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/context/Change.java
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/context/Change.java
@@ -1,107 +1,54 @@
 package com.openexchange.admin.console.context;
 
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 import com.openexchange.admin.console.AdminParser;
 import com.openexchange.admin.rmi.OXContextInterface;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
 
-public class Change extends ContextHostingAbstraction {
+public class Change extends ChangeCore {
 
+    private final ContextHostingAbstraction ctxabs = new ContextHostingAbstraction();
     
     public Change(final String[] args2) {
         final AdminParser parser = new AdminParser("changecontext");
 
-        setOptions(parser);
-        
-        try {
-            
-            int doChange = 0;
-            
-            String[] remove_mappings = null;
-            String[] add_mappings = null;
-            
-            parser.ownparse(args2);
-            final Context ctx = contextparsing(parser);
-            
-            // context filestore quota
-            if(parser.getOptionValue(this.contextQuotaOption)!=null){                
-                ctx.setMaxQuota(Long.parseLong((String) parser.getOptionValue(this.contextQuotaOption)));
-                doChange++;
-            }
-            
-            // context name 
-            if (parser.getOptionValue(this.contextNameOption) != null) {
-                ctx.setName((String) parser.getOptionValue(this.contextNameOption));
-                doChange++;
-            }
-            
-            // add login mappings
-            if (parser.getOptionValue(this.addLoginMappingOption) != null) {
-                add_mappings = ((String) parser.getOptionValue(this.addLoginMappingOption)).split(",");
-                doChange++;
-            }
-            
-            // remove login mappings
-            if (parser.getOptionValue(this.removeLoginMappingOption) != null) {
-                remove_mappings = ((String) parser.getOptionValue(this.removeLoginMappingOption)).split(",");
-                doChange++;
-            }
-            
-            final Credentials auth = credentialsparsing(parser);
-                        
-            // only if he supplied args, make the  call
-            if(doChange>0){
-                
-                // get rmi ref
-                final OXContextInterface oxres = (OXContextInterface) Naming.lookup(RMI_HOSTNAME +OXContextInterface.RMI_NAME);
-                
-                // check if wants to change login mappings, then first load current mappings from server
-                if(add_mappings!=null || remove_mappings!=null){
-                    Context server_ctx = oxres.getData(ctx, auth);
-                    ctx.setLoginMappings(server_ctx.getLoginMappings());
-                    
-                    
-                    // add new mappings
-                    if(add_mappings!=null){
-                        for (String login_mapping : add_mappings) {
-                            ctx.addLoginMapping(login_mapping);
-                        }
-                    }
-                    
-                    // remove mappings
-                    if(remove_mappings!=null){
-                        for (String login_mapping : remove_mappings) {
-                            ctx.removeLoginMapping(login_mapping);
-                        }
-                    }
-                }
-                
-                // do the change
-                oxres.change(ctx, auth);
-                
-                displayChangedMessage(null, null);
-                sysexit(0);
-            }
-        } catch (final Exception e) {
-            printErrors(ctxid, null, e, parser);
-        }
+        commonfunctions(parser, args2);
     }
 
     public static void main(final String args[]) {
         new Change(args);
     }
 
-    private void setOptions(final AdminParser parser) {
-        setDefaultCommandLineOptions(parser);
+    @Override
+    protected void maincall(final AdminParser parser, final Context ctx, final Credentials auth) throws MalformedURLException, RemoteException, NotBoundException, InvalidCredentialsException, NoSuchContextException, StorageException, InvalidDataException {
+        // get rmi ref
+        final OXContextInterface oxctx = (OXContextInterface) Naming.lookup(RMI_HOSTNAME +OXContextInterface.RMI_NAME);
+
+        // add login mappings
+        ctxabs.parseAndSetAddLoginMapping(parser);
         
-        setContextNameOption(parser,false);
+        // remove login mappings
+        ctxabs.parseAndSetRemoveLoginMapping(parser);
         
-        setAddMappingOption(parser, false);
+        ctxabs.changeMappingSetting(oxctx, ctx, auth, true);
+                    
+        // do the change
+        oxctx.change(ctx, auth);
+    }
+
+    @Override
+    protected void setFurtherOptions(final AdminParser parser) {
+        ctxabs.setAddMappingOption(parser, false);
         
-        setRemoveMappingOption(parser, false);
-        
-        setContextQuotaOption(parser, false);
+        ctxabs.setRemoveMappingOption(parser, false);
     }
 }
