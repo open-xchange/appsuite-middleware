7ac59e1494663b084f2184919874a24a8d1135ee
dennis <dennis>
2007-07-27 12:46:58 +0000

    - added a common core for changecontext

---
+++ com.openexchange.admin/src/com/openexchange/admin/console/context/ChangeCore.java
@@ -0,0 +1,54 @@
+package com.openexchange.admin.console.context;
+
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import com.openexchange.admin.console.AdminParser;
+import com.openexchange.admin.rmi.dataobjects.Context;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+
+public abstract class ChangeCore extends ContextAbstraction {
+    
+    protected void setOptions(final AdminParser parser) {
+        setDefaultCommandLineOptions(parser);
+        
+        setContextNameOption(parser);
+
+        setContextQuotaOption(parser, false);
+        
+        setFurtherOptions(parser);
+    }
+    
+    protected final void commonfunctions(final AdminParser parser, final String[] args) {
+        setOptions(parser);
+        
+        try {
+            parser.ownparse(args);
+            final Context ctx = contextparsing(parser);
+            
+            // context filestore quota
+            parseAndSetContextQuota(parser, ctx);
+            
+            // context name
+            parseAndSetContextName(parser, ctx);
+            
+            final Credentials auth = credentialsparsing(parser);
+
+            maincall(parser, ctx, auth);
+
+            displayChangedMessage(ctxid, null);
+            sysexit(0);
+        } catch (final Exception e) {
+            printErrors(ctxid, null, e, parser);
+        }
+    }
+
+    protected abstract void maincall(final AdminParser parser, final Context ctx, final Credentials auth) throws MalformedURLException, RemoteException, NotBoundException, InvalidCredentialsException, NoSuchContextException, StorageException, InvalidDataException;
+        
+    protected abstract void setFurtherOptions(final AdminParser parser);
+}
