c1b7e470fb9ed3fc58dba49882db831ed8582316
thorben <thorben>
2007-08-06 15:39:30 +0000

    Reformatted code

--- src/com/openexchange/groupware/delete/ContextDelete.java
+++ src/com/openexchange/groupware/delete/ContextDelete.java
@@ -1,8 +1,9 @@
 package com.openexchange.groupware.delete;
 
 public abstract class ContextDelete implements DeleteListener {
-//	 I'll go to hell for this.
-	protected boolean isContextDelete(DeleteEvent sqlDelEvent) {
-		return sqlDelEvent.getType() == DeleteEvent.TYPE_USER && sqlDelEvent.getId() == sqlDelEvent.getContext().getMailadmin();
+	// I'll go to hell for this.
+	protected final boolean isContextDelete(final DeleteEvent deleteEvent) {
+		return deleteEvent.getType() == DeleteEvent.TYPE_USER
+				&& deleteEvent.getId() == deleteEvent.getContext().getMailadmin();
 	}
 }
