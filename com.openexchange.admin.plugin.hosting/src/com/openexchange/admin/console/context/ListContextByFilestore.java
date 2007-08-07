2016268f3f609de1915f3663b1a8abdabb58f44f
Dennis Sieben <dennis.sieben@open-xchange.com>
2007-08-07 09:45:04 +0000

    - added ListContextByFilestore command line tool

---
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/context/ListContextByFilestore.java
@@ -0,0 +1,60 @@
+package com.openexchange.admin.console.context;
+
+import java.rmi.Naming;
+
+import com.openexchange.admin.console.AdminParser;
+import com.openexchange.admin.rmi.OXContextInterface;
+import com.openexchange.admin.rmi.dataobjects.Context;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.dataobjects.Filestore;
+
+public class ListContextByFilestore extends ContextHostingAbstraction {
+
+    protected void setOptions(final AdminParser parser) {
+        setDefaultCommandLineOptionsWithoutContextID(parser);
+        setFilestoreIdOption(parser);
+        
+        setCSVOutputOption(parser);
+    }
+
+    public ListContextByFilestore(final String[] args2) {
+
+        final AdminParser parser = new AdminParser("listcontext");
+
+        setOptions(parser);
+        
+        try {
+            parser.ownparse(args2);
+
+            final Filestore fs = parseAndSetFilestoreId(parser);
+
+            final Credentials auth = credentialsparsing(parser);
+
+            final OXContextInterface oxctx = (OXContextInterface) Naming.lookup(RMI_HOSTNAME + OXContextInterface.RMI_NAME);
+            final Context[] ctxs = oxctx.listByFilestore(fs, auth);
+
+            if (null != parser.getOptionValue(this.csvOutputOption)) {
+                precsvinfos(ctxs);
+            } else {
+                sysoutOutput(ctxs);
+            }
+
+            sysexit(0);
+        } catch (final Exception e) {
+            if (null == filestoreid) {
+                printErrors(null, null, e, parser);
+            } else {
+                printErrors(String.valueOf(filestoreid), null, e, parser);
+            }
+        }
+    }
+
+    public static void main(final String args[]) {
+        new ListContextByFilestore(args);
+    }
+    
+    @Override
+    protected final String getObjectName() {
+        return "contexts for filestore";
+    }
+}
