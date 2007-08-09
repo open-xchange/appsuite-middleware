d5b9dd3af17f6995ce1ce22c47f4100ad744a850
Carsten Hoeger <carsten.hoeger@open-xchange.com>
2007-08-09 09:53:32 +0000

    cleanup exceptions, remove serializable and generate unique ID

--- src/com/openexchange/admin/exceptions/DatabaseContextMappingException.java
+++ src/com/openexchange/admin/exceptions/DatabaseContextMappingException.java
@@ -6,14 +6,10 @@ package com.openexchange.admin.exceptions;
  * @author cutmasta
  */
 public class DatabaseContextMappingException extends Exception{
-    
-    
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	public DatabaseContextMappingException(String s) {
+    private static final long serialVersionUID = -1030830753506095471L;
+
+    public DatabaseContextMappingException(String s) {
         super( s );
     }
     
