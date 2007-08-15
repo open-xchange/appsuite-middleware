edb43d8696c94db10f2711ae77ded21e3699ab62
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-08-15 13:19:11 +0000

    add test case for bug 8836

---
+++ openexchange-test/src/com/openexchange/ajax/appointment/Bug8836Test.java
@@ -0,0 +1,49 @@
+package com.openexchange.ajax.appointment;
+
+import com.openexchange.ajax.AppointmentTest;
+import com.openexchange.groupware.calendar.CalendarDataObject;
+import com.openexchange.groupware.container.AppointmentObject;
+import com.openexchange.groupware.container.CalendarObject;
+import com.openexchange.groupware.container.CommonObject;
+import com.openexchange.groupware.container.DataObject;
+import com.openexchange.groupware.container.FolderChildObject;
+import com.openexchange.test.TestException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Bug8836Test extends AppointmentTest {
+
+	private static final Log LOG = LogFactory.getLog(Bug8836Test.class);
+	
+	public Bug8836Test(String name) {
+		super(name);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testBug8836() throws Exception {
+		AppointmentObject appointmentObj = createAppointmentObject("testBug8836");
+		appointmentObj.setIgnoreConflicts(true);
+		int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, getHostName(), getSessionId());
+		
+		appointmentObj.setObjectID(objectId);
+		appointmentObj.setPrivateFlag(true);
+		
+		AppointmentObject loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, timeZone, getHostName(), getSessionId());
+		Date modified = new Date(loadAppointment.getLastModified().getTime()+1);
+		
+		updateAppointment(getWebConversation(), appointmentObj, objectId, appointmentFolderId, modified, timeZone, getHostName(), getSessionId());
+
+		loadAppointment = loadAppointment(getWebConversation(), objectId, appointmentFolderId, timeZone, getHostName(), getSessionId());
+		modified = new Date(loadAppointment.getLastModified().getTime()+1);
+		
+		compareObject(appointmentObj, loadAppointment);
+
+		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, modified, getHostName(), getSessionId());
+	}
+}
