412c85c9738f494c81e4217eb332d32a54701676
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2007-08-20 13:42:32 +0000

    check response appointment object

--- openexchange-test/src/com/openexchange/ajax/appointment/PortalSearchTest.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/PortalSearchTest.java
@@ -6,6 +6,10 @@ import com.openexchange.ajax.appointment.action.NewAppointmentSearchResponse;
 import com.openexchange.ajax.framework.AJAXSession;
 import com.openexchange.ajax.framework.Executor;
 import com.openexchange.groupware.container.AppointmentObject;
+import com.openexchange.groupware.container.CalendarObject;
+import com.openexchange.groupware.container.CommonObject;
+import com.openexchange.groupware.container.DataObject;
+import com.openexchange.groupware.container.FolderChildObject;
 import java.util.Calendar;
 import java.util.Date;
 import org.apache.commons.logging.Log;
@@ -14,6 +18,22 @@ import org.apache.commons.logging.LogFactory;
 public class PortalSearchTest extends AppointmentTest {
 
 	private static final Log LOG = LogFactory.getLog(PortalSearchTest.class);
+	
+	private int[] columns = {
+		DataObject.OBJECT_ID,
+		FolderChildObject.FOLDER_ID,
+		CommonObject.PRIVATE_FLAG,
+		CommonObject.CATEGORIES,
+		CalendarObject.TITLE,
+		AppointmentObject.LOCATION,
+		CalendarObject.START_DATE,
+		CalendarObject.END_DATE,
+		CalendarObject.NOTE,
+		CalendarObject.RECURRENCE_TYPE,
+		AppointmentObject.SHOWN_AS,
+		AppointmentObject.FULL_TIME,
+		AppointmentObject.COLOR_LABEL
+	};
 	
 	public PortalSearchTest(String name) {
 		super(name);
@@ -38,11 +58,13 @@ public class PortalSearchTest extends AppointmentTest {
 		final Date end = calendar.getTime();
 		
 		final AppointmentObject appointmentObj = createAppointmentObject("testNewAppointmentsSearch");
-
+		appointmentObj.setIgnoreConflicts(true);
+		
 		final int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, getHostName(), getSessionId());
+		appointmentObj.setObjectID(objectId);
 		
 		final AJAXSession ajaxSession = new AJAXSession(getWebConversation(), getSessionId());
-		final NewAppointmentSearchRequest request = new NewAppointmentSearchRequest(start, end, 5, timeZone);
+		final NewAppointmentSearchRequest request = new NewAppointmentSearchRequest(start, end, 10000, timeZone, columns);
 		final NewAppointmentSearchResponse response = (NewAppointmentSearchResponse)Executor.execute(ajaxSession, request);
 		
 		if (response.hasError()) {
@@ -54,6 +76,7 @@ public class PortalSearchTest extends AppointmentTest {
 		boolean found = false;
 		for (int a = 0; a < appointmentArray.length; a++) {
 			if (appointmentArray[a].getObjectID() == objectId) {
+				compareObject(appointmentObj, appointmentArray[a]);
 				found = true;
 			}
 		}
