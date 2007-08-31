9bef7a7492810eca33439ac00f92482cdf36f1e0
thorben <thorben>
2007-08-31 12:21:58 +0000

    Bugfix #8699: JMX connector and its rmi data socket get now bound to the same address (if configured through server.properties' attribute "MonitorJMXBindAddress")

--- src/com/openexchange/monitoring/AbstractAgent.java
+++ src/com/openexchange/monitoring/AbstractAgent.java
@@ -47,8 +47,6 @@
  *
  */
 
-
-
 package com.openexchange.monitoring;
 
 import java.io.IOException;
@@ -65,6 +63,8 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
@@ -72,18 +72,20 @@ import javax.management.monitor.GaugeMonitor;
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
+import javax.management.remote.rmi.RMIConnectorServer;
 
 import com.sun.jmx.snmp.daemon.SnmpAdaptorServer;
 
 /**
  * 
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
- *
+ * 
  */
 public abstract class AbstractAgent {
-	
-	private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(AbstractAgent.class);
-	
+
+	private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory
+			.getLog(AbstractAgent.class);
+
 	private static final class AbstractAgentSocketFactory extends RMISocketFactory implements Serializable {
 
 		/**
@@ -112,10 +114,19 @@ public abstract class AbstractAgent {
 
 	}
 
+	private final Lock lockSocketFactory;
+
+	private boolean initialized;
+
+	protected RMISocketFactory rmiSocketFactory;
+
 	protected MBeanServer mbs;
+
+	protected Map<String, Object> environment;
 
 	public AbstractAgent() {
 		mbs = ManagementFactory.getPlatformMBeanServer();
+		lockSocketFactory = new ReentrantLock();
 	}
 
 	/**
@@ -128,13 +139,16 @@ public abstract class AbstractAgent {
 	}
 
 	/**
-     * Unregister an mbean.
-     * @param name name of the mbean.
-	 * @throws Exception if an error occurs.
+	 * Unregister an mbean.
+	 * 
+	 * @param name
+	 *            name of the mbean.
+	 * @throws Exception
+	 *             if an error occurs.
 	 */
-    public void unregisterMBean(final String name) throws Exception {
-        unregisterMBean(new ObjectName(name));
-    }
+	public void unregisterMBean(final String name) throws Exception {
+		unregisterMBean(new ObjectName(name));
+	}
 
 	/**
 	 * Uniquely identify the MBeans and register them with the MBeanServer
@@ -161,7 +175,7 @@ public abstract class AbstractAgent {
 	 * @param attributeName
 	 * @throws Exception
 	 */
-	public void addObservedMBean(final ObjectName objectName, final String attributeName) throws Exception {
+	public final void addObservedMBean(final ObjectName objectName, final String attributeName) throws Exception {
 		if (gm == null) {
 			gm = new GaugeMonitor();
 			gm.setGranularityPeriod(5000);
@@ -183,26 +197,29 @@ public abstract class AbstractAgent {
 	 * @param objectName
 	 * @throws Exception
 	 */
-	public void removeObservedMBean(final ObjectName objectName) throws Exception {
+	public final void removeObservedMBean(final ObjectName objectName) throws Exception {
 		if (gm != null) {
 			gm.removeObservedObject(objectName);
 		}
 	}
 
-	private final Map<Integer,Registry> registries = new HashMap<Integer,Registry>();
+	private final Map<Integer, Registry> registries = new HashMap<Integer, Registry>();
 
 	/**
 	 * add a RMI registry
 	 * 
 	 * @param port
+	 *            The port
 	 * @param bindAddr
+	 *            The bind address or <code>"*"</code> to accept connections
+	 *            on any/all local addresses
 	 */
-	protected void addRMIRegistry(final int port, final String bindAddr) {
+	protected final void addRMIRegistry(final int port, final String bindAddr) {
 		Registry registry = null;
 		try {
-			final AbstractAgentSocketFactory socketFactory = new AbstractAgentSocketFactory(0, bindAddr);
-			registry = LocateRegistry.createRegistry(port, socketFactory, socketFactory);
-		} catch (Exception e) {
+			initializeRMIServerSocketFactory(bindAddr);
+			registry = LocateRegistry.createRegistry(port, rmiSocketFactory, rmiSocketFactory);
+		} catch (final Exception e) {
 			printTrace(new StringBuilder(200).append("Can not create a RMI registry on port ").append(port).append(
 					" and bind address ").append(bindAddr).append(": ").append(e.getMessage()).toString());
 			return;
@@ -212,17 +229,34 @@ public abstract class AbstractAgent {
 				" and bind address ").append(bindAddr).toString());
 	}
 
+	private final void initializeRMIServerSocketFactory(final String bindAddr) throws UnknownHostException {
+		if (!initialized) {
+			lockSocketFactory.lock();
+			try {
+				if (null == rmiSocketFactory) {
+					rmiSocketFactory = new AbstractAgentSocketFactory(0, bindAddr);
+					environment = new HashMap<String, Object>(2);
+					environment.put(RMIConnectorServer.RMI_SERVER_SOCKET_FACTORY_ATTRIBUTE, rmiSocketFactory);
+					environment.put(RMIConnectorServer.RMI_CLIENT_SOCKET_FACTORY_ATTRIBUTE, rmiSocketFactory);
+					initialized = true;
+				}
+			} finally {
+				lockSocketFactory.unlock();
+			}
+		}
+	}
+
 	/**
 	 * remove a RMI registry
 	 * 
 	 * @param port
 	 */
-	protected void removeRMIRegistry(final int port) {
+	protected final void removeRMIRegistry(final int port) {
 		final Registry registry = registries.get(Integer.valueOf(port));
 		try {
 			UnicastRemoteObject.unexportObject(registry, true);
 			printTrace("Unexport RMI registry on port " + port);
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			printTrace("Can not unexport RMI registry on port " + port);
 		}
 	}
@@ -232,24 +266,24 @@ public abstract class AbstractAgent {
 	 * 
 	 * @param port
 	 */
-	public int[] getRMIRegistryPorts() {
+	public final int[] getRMIRegistryPorts() {
 		final int size = registries.size();
 		final Iterator<Integer> iter = registries.keySet().iterator();
-		int[] portnumbers = new int[size];
+		final int[] portnumbers = new int[size];
 		for (int i = 0; i < size; i++) {
 			portnumbers[i] = iter.next().intValue();
 		}
 		return portnumbers;
 	}
 
-	private final Map<String,JMXConnectorServer> connectors = new HashMap<String,JMXConnectorServer>();
+	private final Map<String, JMXConnectorServer> connectors = new HashMap<String, JMXConnectorServer>();
 
 	/**
-	 * Create a JMX connector and start it
+	 * Create a JMX connector and starts it
 	 */
-	protected void addConnector(final String urlstr) throws Exception {
+	protected final void addConnector(final String urlstr) throws Exception {
 		final JMXServiceURL url = new JMXServiceURL(urlstr);
-		final JMXConnectorServer cs = JMXConnectorServerFactory.newJMXConnectorServer(url, null, mbs);
+		final JMXConnectorServer cs = JMXConnectorServerFactory.newJMXConnectorServer(url, environment, mbs);
 		cs.start();
 		connectors.put(urlstr, cs);
 		printTrace("Connector on " + urlstr + " is started");
@@ -258,7 +292,7 @@ public abstract class AbstractAgent {
 	/**
 	 * Remove a JMX connector and stop it
 	 */
-	protected void removeConnector(final String urlstr) {
+	protected final void removeConnector(final String urlstr) {
 		final JMXConnectorServer connector = connectors.remove(urlstr);
 		if (connector == null) {
 			return;
@@ -266,31 +300,16 @@ public abstract class AbstractAgent {
 		try {
 			connector.stop();
 			printTrace("Connector on " + urlstr + " is stopped");
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			LOG.error(e);
 			return;
 		}
 	}
 
-	// /**
-	// * Create a Http adaptor and start it
-	// */
-	// public void addHttpAdaptor(int port)
-	// throws Exception {
-	// // Register and start the HTML adaptor
-	// HtmlAdaptorServer adapter = new HtmlAdaptorServer();
-	// ObjectName adapterName = new ObjectName(
-	// "Adaptors:type=HTML,port=" + port);
-	// adapter.setPort(port);
-	// mbs.registerMBean(adapter, adapterName);
-	// adapter.start();
-	// printtrace("HttpAdaptor on port " + port);
-	// }
-
 	/**
 	 * Create a Snmp adaptor and start it
 	 */
-	public void addSnmpAdaptor(final int port) throws Exception {
+	public final void addSnmpAdaptor(final int port) throws Exception {
 		// Register and start the SNMP adaptor
 		final SnmpAdaptorServer adapter = new SnmpAdaptorServer();
 		final ObjectName adapterName = new ObjectName("Adaptors:type=SNMP,port=" + port);
@@ -303,7 +322,7 @@ public abstract class AbstractAgent {
 	/**
 	 * Remove a Snmp adaptor and start it
 	 */
-	public void removeSnmpAdaptor(final int port) {
+	public final void removeSnmpAdaptor(final int port) {
 		if (LOG.isTraceEnabled()) {
 			LOG.trace(new StringBuilder("removeSnmpAdaptor() with ").append(port).toString());
 		}
@@ -312,23 +331,91 @@ public abstract class AbstractAgent {
 	public abstract void run();
 
 	// Utility method: so that the application continues to run
-	public static void waitForEnterPressed() {
+	public static final void waitForEnterPressed() {
 		try {
 			printTrace("Press return to exit ...");
 			System.in.read();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			LOG.error(e);
 		}
 	}
 
 	public abstract void stop();
 
-	private static boolean TRACE = true;
+	private static final boolean TRACE = true;
 
-	protected static void printTrace(final String msg) {
+	protected static final void printTrace(final String msg) {
 		if (TRACE && LOG.isInfoEnabled()) {
 			LOG.info(msg);
 		}
+	}
+
+	private static final Lock LOCK_FREE_PORT = new ReentrantLock();
+
+	/**
+	 * Gets a free port at the time of call to this method. The logic leverages
+	 * the built in java.net.ServerSocket implementation which binds a server
+	 * socket to a free port when instantiated with a port <code> 0 </code>.
+	 * <P>
+	 * Note that this method guarantees the availability of the port only at the
+	 * time of call. The method does not bind to this port.
+	 * <p>
+	 * Checking for free port can fail for several reasons which may indicate
+	 * potential problems with the system. This method acknowledges the fact and
+	 * following is the general contract:
+	 * <li> Best effort is made to find a port which can be bound to. All the
+	 * exceptional conditions in the due course are considered SEVERE.
+	 * <li> If any exceptional condition is experienced, <code> 0 </code> is
+	 * returned, indicating that the method failed for some reasons and the
+	 * callers should take the corrective action. (The method need not always
+	 * throw an exception for this).
+	 * <li> Method is synchronized on this class.
+	 * 
+	 * @return integer depicting the free port number available at this time 0
+	 *         otherwise.
+	 */
+	protected static final int getFreePort() {
+		int freePort = 0;
+		boolean portFound = false;
+		ServerSocket serverSocket = null;
+		LOCK_FREE_PORT.lock();
+		try {
+			try {
+				/*
+				 * following call normally returns the free port, to which the
+				 * ServerSocket is bound.
+				 */
+				serverSocket = new ServerSocket(0);
+				freePort = serverSocket.getLocalPort();
+				portFound = true;
+			} catch (final Exception e) {
+				/*
+				 * Squelch the exception
+				 */
+				LOG.error(e.getLocalizedMessage(), e);
+			} finally {
+				if (!portFound) {
+					freePort = 0;
+				}
+				try {
+					if (serverSocket != null) {
+						serverSocket.close();
+						if (!serverSocket.isClosed()) {
+							throw new Exception("local exception ...");
+						}
+					}
+				} catch (final Exception e) {
+					/*
+					 * Squelch the exception
+					 */
+					freePort = 0;
+				}
+			}
+			return freePort;
+		} finally {
+			LOCK_FREE_PORT.unlock();
+		}
+
 	}
 
 }
