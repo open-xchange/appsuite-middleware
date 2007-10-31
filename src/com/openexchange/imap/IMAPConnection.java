cafa7998123c25f4cee2ea76b949ab26575d1251
thorben <thorben>
2007-10-31 11:49:31 +0000

    removed test data

--- src/com/openexchange/imap/IMAPConnection.java
+++ src/com/openexchange/imap/IMAPConnection.java
@@ -246,9 +246,6 @@ public final class IMAPConnection extends MailConnection<IMAPFolderStorage, IMAP
 					LOG.error(e.getMessage(), e);
 				}
 			}
-			
-			tmpPass = "oxtest40";
-			
 			imapStore.connect(getMailServer(), getMailServerPort(), getLogin(), tmpPass);
 			connected = true;
 			/*
