893280a8fa4b1ab40870333a8e5e68ee62f87153
offspring <offspring>
2007-11-19 12:53:40 +0000

    add source for sessiond bundle

---
+++ sessiond/src/com/openexchange/sessiond/impl/SessionImpl.java
@@ -0,0 +1,182 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.sessiond.impl;
+
+import com.openexchange.groupware.contexts.Context;
+import com.openexchange.groupware.ldap.Credentials;
+import com.openexchange.groupware.upload.ManagedUploadFile;
+import com.openexchange.sessiond.Session;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * SessionImpl
+ *
+ * @author <a href="mailto:sebastian.kauss@open-xchange.org">Sebastian Kauss</a>
+ */
+public class SessionImpl implements Session {
+	
+    protected Context context;
+    
+    protected Credentials cred;
+    
+    protected int userId;
+    
+    protected String sessionId;
+    
+    protected String secret;
+    
+    protected String randomToken;
+    
+	private static final Log LOG = LogFactory.getLog(SessionImpl.class);
+	
+	private final transient Map<String, ManagedUploadFile> ajaxUploadFiles;
+
+	private final Map<String, Object> parameters;
+
+	public SessionImpl(int userId, Credentials cred, Context context, String sessionId, String secret, String randomToken) {
+        this.userId = userId;
+		this.cred = cred;
+        this.context = context;
+        this.sessionId = sessionId;
+        this.secret = secret;
+        this.randomToken = randomToken;
+        
+		parameters = new ConcurrentHashMap<String, Object>();
+		ajaxUploadFiles = new ConcurrentHashMap<String, ManagedUploadFile>();
+	}
+
+    public Context getContext() {
+        return context;
+    }
+
+    public Credentials getCredentials() {
+        return cred;
+    }
+
+    public Object getParameter(String name) {
+        return parameters.get(name);
+    }
+
+    public String getRandomToken() {
+        return randomToken;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public String getSessionID() {
+        return sessionId;
+    }
+
+    public ManagedUploadFile getUploadedFile(String id) {
+		final ManagedUploadFile uploadFile = ajaxUploadFiles.get(id);
+		if (null != uploadFile) {
+			uploadFile.touch();
+		}
+		return uploadFile;
+    }
+
+    public int getUserID() {
+        return userId;
+    }
+
+    public void putUploadedFile(String id, ManagedUploadFile uploadFile) {
+		ajaxUploadFiles.put(id, uploadFile);
+		uploadFile.startTimerTask(id, ajaxUploadFiles);
+		if (LOG.isInfoEnabled()) {
+			LOG.info(new StringBuilder(256).append("Upload file \"").append(uploadFile).append("\" with ID=")
+					.append(id).append(" added to session and timer task started").toString());
+		}
+    }
+
+    public ManagedUploadFile removeUploadedFile(String id) {
+		final ManagedUploadFile uploadFile = ajaxUploadFiles.remove(id);
+		if (null != uploadFile) {
+			/*
+			 * Cancel timer task
+			 */
+			uploadFile.cancelTimerTask();
+			if (LOG.isInfoEnabled()) {
+				LOG.info(new StringBuilder(256).append("Upload file \"").append(uploadFile).append("\" with ID=")
+						.append(id).append(" removed from session and timer task canceled").toString());
+			}
+		}
+		return uploadFile;
+    }
+
+    public void removeUploadedFileOnly(String id) {
+    }
+
+    public void setParameter(String name, Object value) {
+        parameters.put(name, value);
+    }
+
+    public boolean touchUploadedFile(String id) {
+		final ManagedUploadFile uploadFile = ajaxUploadFiles.get(id);
+		if (null != uploadFile) {
+			uploadFile.touch();
+			return true;
+		}
+		return false;
+    }
+
+    public void removeRandomToken() {
+        randomToken = null;
+    }
+}
+
+
+
+
+
---
+++ sessiond/src/com/openexchange/sessiond/impl/SessiondConnectorImpl.java
@@ -0,0 +1,103 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.sessiond.impl;
+
+import com.openexchange.groupware.contexts.Context;
+import com.openexchange.groupware.ldap.Credentials;
+import com.openexchange.sessiond.Session;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * SessiondConnectorImpl
+ *
+ * @author <a href="mailto:sebastian.kauss@open-xchange.org">Sebastian Kauss</a>
+ */
+public class SessiondConnectorImpl implements SessiondConnectorInterface {
+	
+	private static final Log LOG = LogFactory.getLog(SessiondConnectorImpl.class);
+	
+	public SessiondConnectorImpl() {
+		
+	}
+	
+    public String addSession(final int userId, final Credentials cred, final Context context, final String clientHost) throws SessiondException {
+        return SessionHandler.addSession(userId, cred, context, clientHost);
+    }
+
+    public boolean refreshSession(final String sessionId) {
+        return SessionHandler.refreshSession(sessionId);
+    }
+
+    public boolean removeSession(final String sessionId) {
+        return SessionHandler.clearSession(sessionId);
+    }
+
+    public Session getSession(final String sessionId) {
+        final SessionControlObject sessionControlObject = SessionHandler.getSession(sessionId, true);
+        if (sessionControlObject != null) {
+            return  sessionControlObject.getSession();
+        } 
+        return null;
+    }
+
+    public Session getSessionByRandomToken(final String randomToken) {
+        return SessionHandler.getSessionByRandomToken(randomToken);
+    }
+
+    public int getNumberOfActiveSessions() {
+        return 9999;
+    }
+}
+
+
+
+
+
