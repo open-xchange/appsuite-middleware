b7d08fd0819ceec3b1b317291027cf50e8c89f61
offspring <offspring>
2007-12-03 16:48:36 +0000

    add configjump bundle

---
+++ configjump/src/com/openexchange/configjump/internal/EmptyImpl.java
@@ -0,0 +1,89 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.configjump.internal;
+
+import java.net.URL;
+
+import com.openexchange.configjump.ConfigJumpInterface;
+import com.openexchange.configjump.ConfigJumpException;
+import com.openexchange.configjump.ConfigJumpException.Code;
+
+/**
+ * Empty implementation for the setup link.
+ * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
+ */
+public class EmptyImpl implements ConfigJumpInterface {
+
+    /**
+     * Default constructor.
+     */
+    public EmptyImpl() {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public URL getLink(final Object... values) throws ConfigJumpException {
+        throw new ConfigJumpException(Code.NOT_IMPLEMENTED);
+    }
+
+    public int getContextId(final Object... values) {
+        return (Integer) values[0];
+    }
+
+    public String getUserLogin(final Object... values) {
+        return (String) values[1];
+    }
+
+    public String getPassword(final Object... values) {
+        return (String) values[2];
+    }
+}
