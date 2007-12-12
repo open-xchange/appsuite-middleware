fddae0487ba3cfd46ce1fa81a6eba84a6ab089df
thorben <thorben>
2007-12-12 15:05:49 +0000

    base type

--- server/src/com/openexchange/mail/cache/SessionMailCache.java
+++ server/src/com/openexchange/mail/cache/SessionMailCache.java
@@ -173,7 +173,7 @@ public final class SessionMailCache {
 	 *            The mail cache entry
 	 */
 	@SuppressWarnings("unchecked")
-	public void get(final SessionMailCacheEntry<Object> entry) {
+	public void get(final SessionMailCacheEntry entry) {
 		final Lock readLock = getKeyLock(entry.getKey()).readLock();
 		readLock.lock();
 		try {
@@ -192,7 +192,7 @@ public final class SessionMailCache {
 	 *            The mail cache entry
 	 */
 	@SuppressWarnings("unchecked")
-	public void remove(final SessionMailCacheEntry<Object> entry) {
+	public void remove(final SessionMailCacheEntry entry) {
 		final Lock writeLock = getKeyLock(entry.getKey()).writeLock();
 		writeLock.lock();
 		try {
