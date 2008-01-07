36a9812ed61d996b0509a32685bc7e4418232d42
cutmasta <cutmasta>
2008-01-07 12:12:11 +0000

    refactored equals method to gnerated one by eclipse

--- src/com/openexchange/admin/rmi/dataobjects/Group.java
+++ src/com/openexchange/admin/rmi/dataobjects/Group.java
@@ -335,57 +335,45 @@ public class Group extends ExtendableDataObject implements NameAndIdObject {
     }
 
 
-    @Override
-    public final boolean equals(final Object obj) {
-        if (obj instanceof Group) {
-            final Group grp = (Group) obj;
-            if (!checkEqualsWithNull(this.id, grp.id)) {
-                return false;
-            }
-            if (!checkEqualsWithNull(this.name, grp.name)) {
-                return false;
-            }
-            if (!checkEqualsWithNull(this.displayname, grp.displayname)) {
-                return false;
-            }
-            if (!checkEqualsWithNullForArray(this.members, grp.members)) {
-                return false;
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((displayname == null) ? 0 : displayname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + Arrays.hashCode(members);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
-    private boolean checkEqualsWithNull(final Object a, final Object b) {
-        if (null == a) {
-            if (null != b) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            if (!a.equals(b)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    }
 
-    private boolean checkEqualsWithNullForArray(final Object[] a, final Object[] b) {
-        if (null == a) {
-            if (null != b) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-            if (!Arrays.equals(a, b)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Group other = (Group) obj;
+		if (displayname == null) {
+			if (other.displayname != null)
+				return false;
+		} else if (!displayname.equals(other.displayname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (!Arrays.equals(members, other.members))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
