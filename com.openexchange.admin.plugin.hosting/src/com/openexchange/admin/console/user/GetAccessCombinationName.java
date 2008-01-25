c1654e022321ebc5db1c873d3d612f2874f655ea
Manuel Kraft <manuel.kraft@open-xchange.com>
2008-01-25 14:46:40 +0000

    added NEW access combination name CLT tools for user interface

---
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/console/user/GetAccessCombinationName.java
@@ -0,0 +1,39 @@
+
+package com.openexchange.admin.console.user;
+
+import java.rmi.RemoteException;
+
+import com.openexchange.admin.console.AdminParser;
+import com.openexchange.admin.rmi.OXUserInterface;
+import com.openexchange.admin.rmi.dataobjects.Context;
+import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.dataobjects.User;
+import com.openexchange.admin.rmi.exceptions.DatabaseUpdateException;
+import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
+import com.openexchange.admin.rmi.exceptions.InvalidDataException;
+import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
+import com.openexchange.admin.rmi.exceptions.NoSuchUserException;
+import com.openexchange.admin.rmi.exceptions.StorageException;
+
+public class GetAccessCombinationName extends DeleteCore {
+
+    public static void main(final String[] args) {
+        new GetAccessCombinationName(args);
+    }
+
+    public GetAccessCombinationName(final String[] args2) {
+
+        final AdminParser parser = new AdminParser("getaccesscombinationnameforuser");
+
+        commonfunctions(parser, args2);
+    }
+
+    @Override
+    protected void maincall(final AdminParser parser, final OXUserInterface oxusr, final Context ctx, final User usr, final Credentials auth) throws RemoteException, StorageException, InvalidCredentialsException, NoSuchContextException, InvalidDataException, DatabaseUpdateException, NoSuchUserException {
+        oxusr.delete(ctx, usr, auth);
+    }
+
+    @Override
+    protected void setFurtherOptions(AdminParser parser) {
+    }
+}
