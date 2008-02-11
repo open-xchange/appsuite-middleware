35a3af49d86681ead6f79c95d8794eb42015e49e
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2008-02-11 13:44:37 +0000

    add empty TestEventAdmin

---
+++ openexchange-test/unittests/com/openexchange/tools/events/TestEventAdmin.java
@@ -0,0 +1,17 @@
+package com.openexchange.tools.events;
+
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
+
+public class TestEventAdmin implements EventAdmin {
+
+	public void postEvent(Event arg0) {
+		
+
+	}
+
+	public void sendEvent(Event arg0) {
+
+	}
+
+}
