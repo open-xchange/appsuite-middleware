0c10fef36d3dd9bdc5b37bb073ebc68cc006ee3b
offspring <offspring>
2008-02-12 14:57:21 +0000

    add javadoc for sessiond interface

--- global/src/com/openexchange/sessiond/SessiondConnectorInterface.java
+++ global/src/com/openexchange/sessiond/SessiondConnectorInterface.java
@@ -61,18 +61,73 @@ import com.openexchange.sessiond.exception.SessiondException;
 
 public interface SessiondConnectorInterface {
 	
+	/**
+	 * Creates a new session object in the sessiond storage with the given session parameters
+	 * 
+	 * @param userId -
+	 *            The User ID
+	 * @param loginName -
+	 *            The login name of the user
+	 * @param password -
+	 *            The password of the user
+	 * @param context -
+	 *            The context of the user
+	 * @param clientHost -
+	 *            The hostname of the incoming client
+	 * @return The session ID of the newly created session as a <code>String</code>
+	 * @throws SessiondException
+	 */
     public String addSession(final int userId, final String loginName, final String password, final Context context, final String clientHost) throws SessiondException;
     
+	/**
+	 * Refreshes the timestamp of the session with the given session ID
+	 * 
+	 * @param sessionId -
+	 *            The Session ID
+	 * @return True if the session timestamp was updated or false if the session ID was invalid or exired
+	 */
     public boolean refreshSession(final String sessionId);
     
+	/**
+	 * Removes the session with the given session ID
+	 * 
+	 * @param sessionId -
+	 *            The Session ID
+	 * @return True if the session was removed or false if the session ID doesn't exist
+	 */
     public boolean removeSession(final String sessionId);
     
+	/**
+	 * Get the session object related to the given session ID
+	 * 
+	 * @param sessionId -
+	 *            The Session ID
+	 * @return Return the session object or null if no session exists for the given ID or if the session is expired
+	 */
     public Session getSession(final String sessionId);
-    
+
+	/**
+	 * Get the session object related to the given random token
+	 * 
+	 * @param randomToken -
+	 *            The random token of the session
+	 * @return Return the session object or null if no session exists for the given random token or if 
+	 * the random token is already expired
+	 */
     public Session getSessionByRandomToken(final String randomToken);
     
+	/**
+	 * The number of active sessions
+	 * 
+	 * @return Return the number of active sessions 
+	 */
     public int getNumberOfActiveSessions();
-    
+
+	/**
+	 * An array with the number of active sessions in a container
+	 * 
+	 * @return Return the number of active sessions in a container
+	 */
     public int[] getNumberOfSessionsInContainer();
 
 } 
