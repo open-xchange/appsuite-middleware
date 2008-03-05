e5fdc7ed3846545cf80cc05a4f9cf5f4a618c775
Marcus Klein <marcus.klein@open-xchange.com>
2008-03-05 10:29:01 +0000

    Fixed missing fields in list request for testing them.

--- openexchange-test/src/com/openexchange/ajax/appointment/recurrence/Bug10760Test.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/recurrence/Bug10760Test.java
@@ -1,15 +1,12 @@
 package com.openexchange.ajax.appointment.recurrence;
 
+import java.util.ArrayList;
 import java.util.Date;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.util.List;
 
 import com.openexchange.groupware.container.AppointmentObject;
 
 public class Bug10760Test extends AbstractRecurrenceTest {
-	
-	private static final Log LOG = LogFactory.getLog(Bug10760Test.class);
 	
 	public Bug10760Test(String name) {
 		super(name);
@@ -35,8 +32,23 @@ public class Bug10760Test extends AbstractRecurrenceTest {
 		
 		appointmentObj.setRecurrencePosition(2);
 		final int newObjectId = updateAppointment(getWebConversation(), appointmentObj, objectId, appointmentFolderId, timeZone, getHostName(), getSessionId());
-		
-		final AppointmentObject[] appointmentArray = listAppointment(getWebConversation(), appointmentFolderId, APPOINTMENT_FIELDS, new Date(), new Date(), timeZone, false, getHostName(), getSessionId());
+		final List<Integer> tmp = new ArrayList<Integer>();
+		for (int i = 0; i < APPOINTMENT_FIELDS.length; i++) {
+		    tmp.add(Integer.valueOf(APPOINTMENT_FIELDS[i]));
+		}
+		final Integer[] checkedFields = new Integer[] {
+		    Integer.valueOf(AppointmentObject.RECURRENCE_ID),
+		    Integer.valueOf(AppointmentObject.RECURRENCE_POSITION) };
+        for (int i = 0; i < checkedFields.length; i++) {
+    		if (!tmp.contains(checkedFields[i])) {
+    		    tmp.add(checkedFields[i]);
+    		}
+        }
+        final int[] fields = new int[tmp.size()];
+        for (int i = 0; i < fields.length; i++) {
+            fields[i] = tmp.get(i).intValue();
+        }
+		final AppointmentObject[] appointmentArray = listAppointment(getWebConversation(), appointmentFolderId, fields, new Date(), new Date(), timeZone, false, getHostName(), getSessionId());
 		for (int a = 0; a < appointmentArray.length; a++) {
 			if (appointmentArray[a].getObjectID() == newObjectId) {
 				assertEquals("recurrence id is not equals expected", objectId, appointmentArray[a].getRecurrenceID());
