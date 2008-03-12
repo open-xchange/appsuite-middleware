469ee71d08d3d4903b443ec527ac602e84fb5130
Sebastian Kauss <sebastian.kauss@open-xchange.com>
2008-03-12 13:21:55 +0000

    add test testContactWithAttachment

--- openexchange-test/src/com/openexchange/webdav/xml/contact/NewTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/contact/NewTest.java
@@ -1,5 +1,7 @@
 package com.openexchange.webdav.xml.contact;
 
+import java.io.ByteArrayInputStream;
+
 import com.openexchange.groupware.Types;
 import com.openexchange.groupware.attach.AttachmentMetadata;
 import com.openexchange.groupware.attach.impl.AttachmentImpl;
@@ -11,7 +13,6 @@ import com.openexchange.webdav.xml.AttachmentTest;
 import com.openexchange.webdav.xml.ContactTest;
 import com.openexchange.webdav.xml.FolderTest;
 import com.openexchange.webdav.xml.XmlServlet;
-import java.io.ByteArrayInputStream;
 
 public class NewTest extends ContactTest {
 	
@@ -80,5 +81,35 @@ public class NewTest extends ContactTest {
 			assertExceptionMessage(exc.getMessage(), XmlServlet.CONFLICT_STATUS);
 		}
 	}	
+	
+	public void testContactWithAttachment() throws Exception {
+		ContactObject contactObj = createContactObject("testContactWithAttachment");
+		final int objectId = insertContact(webCon, contactObj, PROTOCOL + hostName, login, password);
+		contactObj.setObjectID(objectId);
+		contactObj.setNumberOfAttachments(1);
+		
+		final AttachmentMetadata attachmentMeta = new AttachmentImpl();
+		attachmentMeta.setAttachedId(objectId);
+		attachmentMeta.setFolderId(contactFolderId);
+		attachmentMeta.setFileMIMEType("text/plain");
+		attachmentMeta.setModuleId(Types.CONTACT);
+		attachmentMeta.setFilename("test.txt");
+		
+		final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("test".getBytes());
+		AttachmentTest.insertAttachment(webCon, attachmentMeta, byteArrayInputStream, getHostName(), getLogin(), getPassword());
+		
+		final ContactObject loadContact = loadContact(getWebConversation(), objectId, contactFolderId, getHostName(), getLogin(), getPassword());
+		final ContactObject[] contactArray = listContact(getWebConversation(), contactFolderId, loadContact.getLastModified(), true, false, getHostName(), getLogin(), getPassword());
+		
+		boolean found = false;
+		for (int a = 0; a < contactArray.length; a++) {
+			if (contactArray[a].getObjectID() == objectId) {
+				compareObject(contactObj, contactArray[a]);
+				found = true;
+			}
+		}
+		
+		assertTrue("task not found" , found);
+	}
 }
 
