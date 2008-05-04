ab7588620db5f0f6da0abe2e8346efd964d527b4
thorben <thorben>
2008-05-04 22:35:47 +0000

    Added parsing of a black list/white list string

--- server/src/com/openexchange/mail/text/HTMLFilter.java
+++ server/src/com/openexchange/mail/text/HTMLFilter.java
@@ -54,8 +54,11 @@ import static java.util.Arrays.asList;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -76,212 +79,22 @@ import javax.swing.text.html.parser.ParserDelegator;
  */
 public final class HTMLFilter {
 
-	private static final class HTMLTag extends HTML.Tag {
-
-		/**
-		 * Initializes a new {@link HTMLTag}
-		 * 
-		 * @param name
-		 *            The name
-		 */
-		public HTMLTag(final String name) {
-			super(name);
-		}
-
-	}
-
-	private static final Set<HTML.Tag> DEFAULT_WHITELIST_TAGS = new HashSet<HTML.Tag>(asList(new HTML.Tag[] {
-			HTML.Tag.A, new HTMLTag("abbr"), new HTMLTag("acronym"), HTML.Tag.ADDRESS, HTML.Tag.AREA, HTML.Tag.B,
-			HTML.Tag.BIG, HTML.Tag.BLOCKQUOTE, HTML.Tag.BODY, HTML.Tag.BR, new HTMLTag("button"), HTML.Tag.CAPTION,
-			HTML.Tag.CENTER, HTML.Tag.CITE, HTML.Tag.CODE, new HTMLTag("col"), new HTMLTag("colgroup"), HTML.Tag.DD,
-			new HTMLTag("del"), HTML.Tag.DFN, HTML.Tag.DIR, HTML.Tag.DIV, HTML.Tag.DL, HTML.Tag.DT, HTML.Tag.EM,
-			new HTMLTag("fieldset"), HTML.Tag.FONT, HTML.Tag.FORM, HTML.Tag.H1, HTML.Tag.H2, HTML.Tag.H3, HTML.Tag.H4,
-			HTML.Tag.H5, HTML.Tag.H6, HTML.Tag.HEAD, HTML.Tag.HR, HTML.Tag.HTML, HTML.Tag.I, HTML.Tag.IMG,
-			HTML.Tag.INPUT, new HTMLTag("ins"), HTML.Tag.KBD, new HTMLTag("label"), new HTMLTag("legend"), HTML.Tag.LI,
-			HTML.Tag.MAP, HTML.Tag.MENU, HTML.Tag.OL, new HTMLTag("optgroup"), HTML.Tag.OPTION, HTML.Tag.P,
-			HTML.Tag.PRE, new HTMLTag("q"), HTML.Tag.S, HTML.Tag.SAMP, HTML.Tag.SELECT, HTML.Tag.SMALL, HTML.Tag.SPAN,
-			HTML.Tag.STRIKE, HTML.Tag.STRONG, HTML.Tag.SUB, HTML.Tag.TABLE, new HTMLTag("tbody"), HTML.Tag.TD,
-			HTML.Tag.TEXTAREA, new HTMLTag("tfoot"), HTML.Tag.TH, new HTMLTag("thead"), HTML.Tag.TR, HTML.Tag.TT,
-			HTML.Tag.U, HTML.Tag.UL, HTML.Tag.VAR }));
-
-	private static final Set<HTML.Attribute> DEFAULT_WHITELIST_ATTRIBUTES = new HashSet<HTML.Attribute>(
-			asList(new HTML.Attribute[] { HTML.Attribute.ACTION, HTML.Attribute.ALIGN, HTML.Attribute.ALT,
-					HTML.Attribute.BORDER, HTML.Attribute.CELLPADDING, HTML.Attribute.CELLSPACING,
-					HTML.Attribute.CHECKED, HTML.Attribute.CLASS, HTML.Attribute.CLEAR, HTML.Attribute.COLS,
-					HTML.Attribute.COLSPAN, HTML.Attribute.COLOR, HTML.Attribute.COMPACT, HTML.Attribute.COORDS,
-					HTML.Attribute.DIR, HTML.Attribute.ENCTYPE, HTML.Attribute.HEIGHT, HTML.Attribute.HREF,
-					HTML.Attribute.HSPACE, HTML.Attribute.ID, HTML.Attribute.ISMAP, HTML.Attribute.LANG,
-					HTML.Attribute.MAXLENGTH, HTML.Attribute.METHOD, HTML.Attribute.MULTIPLE, HTML.Attribute.NAME,
-					HTML.Attribute.NOHREF, HTML.Attribute.NOSHADE, HTML.Attribute.NOWRAP, HTML.Attribute.PROMPT,
-					HTML.Attribute.REL, HTML.Attribute.REV, HTML.Attribute.ROWS, HTML.Attribute.ROWSPAN,
-					HTML.Attribute.SELECTED, HTML.Attribute.SHAPE, HTML.Attribute.SIZE, HTML.Attribute.SRC,
-					HTML.Attribute.START, HTML.Attribute.TARGET, HTML.Attribute.TITLE, HTML.Attribute.TYPE,
-					HTML.Attribute.USEMAP, HTML.Attribute.VALIGN, HTML.Attribute.VALUE, HTML.Attribute.VSPACE,
-					HTML.Attribute.WIDTH }));
-
-	private static final HTMLEditorKit.Parser PARSER = new ParserDelegator();
-
-	private final int capacity;
-
-	/**
-	 * Initializes a new {@link HTMLFilter}
-	 * 
-	 * @param capacity
-	 *            The initial capacity
-	 */
-	public HTMLFilter(final int capacity) {
-		super();
-		this.capacity = capacity;
-	}
-
-	private static final Pattern PATTERN_DOCTYPE = Pattern.compile("<!DOCTYPE[^>]+>", Pattern.CASE_INSENSITIVE);
-
-	/**
-	 * Applies specified white list to given HTML content
-	 * 
-	 * @param html
-	 *            The HTML content
-	 * @param whitelist
-	 *            The white list
-	 * @return The stripped HTML content according to specified white list
-	 * @throws IOException
-	 *             If an I/O error occurs
-	 */
-	public String applyWhitelist(final String html, final Map<HTML.Tag, Set<HTML.Attribute>> whitelist)
-			throws IOException {
-		/*
-		 * Determine doctype
-		 */
-		final Matcher m = PATTERN_DOCTYPE.matcher(html);
-		final String doctype;
-		if (m.find()) {
-			doctype = m.group();
-		} else {
-			doctype = null;
-		}
-		final HTMLFilterParserCallback parserCallback = new HTMLFilterParserCallback(true, capacity, whitelist);
-		PARSER.parse(new StringReader(html), parserCallback, true);
-		return parserCallback.getHTML(doctype);
-	}
-
-	/**
-	 * Applies specified black list to given HTML content
-	 * 
-	 * @param html
-	 *            The HTML content
-	 * @param blacklist
-	 *            The black list
-	 * @return The stripped HTML content according to specified black list
-	 * @throws IOException
-	 *             If an I/O error occurs
-	 */
-	public String applyBlacklist(final String html, final Map<HTML.Tag, Set<HTML.Attribute>> blacklist)
-			throws IOException {
-		/*
-		 * Determine doctype
-		 */
-		final Matcher m = PATTERN_DOCTYPE.matcher(html);
-		final String doctype;
-		if (m.find()) {
-			doctype = m.group();
-		} else {
-			doctype = null;
-		}
-		final HTMLFilterParserCallback parserCallback = new HTMLFilterParserCallback(false, capacity, blacklist);
-		PARSER.parse(new StringReader(html), parserCallback, true);
-		return parserCallback.getHTML(doctype);
-	}
-
 	private static final class HTMLFilterParserCallback extends ParserCallback {
+
+		private final Map<String, Set<String>> filter;
 
 		private final StringBuilder htmlBuilder;
 
 		private final boolean isWhitelist;
 
-		private final Map<HTML.Tag, Set<HTML.Attribute>> filter;
-
 		private int level;
 
 		public HTMLFilterParserCallback(final boolean isWhitelist, final int capacity,
-				final Map<HTML.Tag, Set<HTML.Attribute>> filter) {
+				final Map<String, Set<String>> filter) {
 			super();
 			this.isWhitelist = isWhitelist;
-			this.htmlBuilder = new StringBuilder(capacity);
+			htmlBuilder = new StringBuilder(capacity);
 			this.filter = filter;
-		}
-
-		public String getHTML(final String doctype) {
-			if (null != doctype) {
-				htmlBuilder.insert(0, doctype);
-			}
-			return prettyPrint(htmlBuilder.toString(), "UTF-8");
-		}
-
-		public void reset() {
-			htmlBuilder.setLength(0);
-		}
-
-		@Override
-		public void handleStartTag(final HTML.Tag tag, final MutableAttributeSet a, final int pos) {
-			if (level > 0) {
-				level++;
-				return;
-			}
-			if (isWhitelist) {
-				if (filter.containsKey(tag)) {
-					addStartTag(tag, a, false, filter.get(tag));
-				} else if (DEFAULT_WHITELIST_TAGS.contains(tag)) {
-					addDefaultStartTag(tag, a, false);
-				} else {
-					level++;
-				}
-			} else {
-				if (filter.containsKey(tag)) {
-					level++;
-				} else {
-					addCompleteStartTag(tag, a, false);
-				}
-			}
-		}
-
-		@Override
-		public void handleEndTag(final HTML.Tag tag, final int pos) {
-			if (level == 0) {
-				htmlBuilder.append("</").append(tag.toString()).append('>');
-			} else {
-				level--;
-			}
-		}
-
-		@Override
-		public void handleSimpleTag(final HTML.Tag tag, final MutableAttributeSet a, final int pos) {
-			if (level > 0) {
-				return;
-			}
-			if (isWhitelist) {
-				if (filter.containsKey(tag)) {
-					addStartTag(tag, a, true, filter.get(tag));
-				} else if (DEFAULT_WHITELIST_TAGS.contains(tag)) {
-					addDefaultStartTag(tag, a, true);
-				}
-			} else {
-				if (!filter.containsKey(tag)) {
-					addCompleteStartTag(tag, a, true);
-				}
-			}
-		}
-
-		@Override
-		public void handleText(final char[] data, final int pos) {
-			if (level == 0) {
-				htmlBuilder.append(data);
-			}
-		}
-
-		@Override
-		public void handleComment(final char[] data, final int pos) {
-			System.out.println("Comment: " + new String(data));
-			htmlBuilder.append(data);
 		}
 
 		/**
@@ -324,7 +137,7 @@ public final class HTMLFilter {
 			htmlBuilder.append('<').append(tag.toString());
 			for (final Enumeration<?> e = a.getAttributeNames(); e.hasMoreElements();) {
 				final Object attributeName = e.nextElement();
-				if (DEFAULT_WHITELIST_ATTRIBUTES.contains(attributeName)) {
+				if (DEFAULT_WHITELIST_ATTRIBUTES.contains(attributeName.toString())) {
 					htmlBuilder.append(' ').append(attributeName.toString()).append("=\"").append(
 							a.getAttribute(attributeName).toString()).append('"');
 				}
@@ -350,14 +163,15 @@ public final class HTMLFilter {
 		 *            refer to default allowed attributes
 		 */
 		private void addStartTag(final HTML.Tag tag, final MutableAttributeSet a, final boolean simple,
-				final Set<HTML.Attribute> attribs) {
+				final Set<String> attribs) {
 			if (null == attribs) {
 				addDefaultStartTag(tag, a, simple);
 			} else {
 				htmlBuilder.append('<').append(tag.toString());
 				for (final Enumeration<?> e = a.getAttributeNames(); e.hasMoreElements();) {
 					final Object attributeName = e.nextElement();
-					if (attribs.contains(attributeName) || DEFAULT_WHITELIST_ATTRIBUTES.contains(attributeName)) {
+					if (attribs.contains(attributeName.toString())
+							|| DEFAULT_WHITELIST_ATTRIBUTES.contains(attributeName.toString())) {
 						htmlBuilder.append(' ').append(attributeName.toString()).append("=\"").append(
 								a.getAttribute(attributeName).toString()).append('"');
 					}
@@ -368,6 +182,268 @@ public final class HTMLFilter {
 				htmlBuilder.append('>');
 			}
 		}
+
+		public String getHTML(final String doctype) {
+			if (null != doctype) {
+				htmlBuilder.insert(0, doctype);
+			}
+			return prettyPrint(htmlBuilder.toString(), "UTF-8");
+		}
+
+		@Override
+		public void handleComment(final char[] data, final int pos) {
+			// TODO: htmlBuilder.append(data);
+		}
+
+		@Override
+		public void handleEndTag(final HTML.Tag tag, final int pos) {
+			if (level == 0) {
+				htmlBuilder.append("</").append(tag.toString()).append('>');
+			} else {
+				level--;
+			}
+		}
+
+		@Override
+		public void handleSimpleTag(final HTML.Tag tag, final MutableAttributeSet a, final int pos) {
+			if (level > 0) {
+				return;
+			}
+			if (isWhitelist) {
+				if (filter.containsKey(tag)) {
+					addStartTag(tag, a, true, filter.get(tag));
+				} else if (DEFAULT_WHITELIST_TAGS.contains(tag)) {
+					addDefaultStartTag(tag, a, true);
+				}
+			} else {
+				if (!filter.containsKey(tag)) {
+					addCompleteStartTag(tag, a, true);
+				}
+			}
+		}
+
+		@Override
+		public void handleStartTag(final HTML.Tag tag, final MutableAttributeSet a, final int pos) {
+			if (level > 0) {
+				level++;
+				return;
+			}
+			if (isWhitelist) {
+				if (filter.containsKey(tag)) {
+					addStartTag(tag, a, false, filter.get(tag));
+				} else if (DEFAULT_WHITELIST_TAGS.contains(tag)) {
+					addDefaultStartTag(tag, a, false);
+				} else {
+					level++;
+				}
+			} else {
+				if (filter.containsKey(tag)) {
+					level++;
+				} else {
+					addCompleteStartTag(tag, a, false);
+				}
+			}
+		}
+
+		@Override
+		public void handleText(final char[] data, final int pos) {
+			if (level == 0) {
+				htmlBuilder.append(data);
+			}
+		}
+
+		public void reset() {
+			htmlBuilder.setLength(0);
+		}
+	}
+
+	private static final Set<String> DEFAULT_WHITELIST_ATTRIBUTES = new HashSet<String>(asList(new String[] {
+			HTML.Attribute.ACTION.toString(), HTML.Attribute.ALIGN.toString(), HTML.Attribute.ALT.toString(),
+			HTML.Attribute.BORDER.toString(), HTML.Attribute.CELLPADDING.toString(),
+			HTML.Attribute.CELLSPACING.toString(), HTML.Attribute.CHECKED.toString(), HTML.Attribute.CLASS.toString(),
+			HTML.Attribute.CLEAR.toString(), HTML.Attribute.COLS.toString(), HTML.Attribute.COLSPAN.toString(),
+			HTML.Attribute.COLOR.toString(), HTML.Attribute.COMPACT.toString(), HTML.Attribute.COORDS.toString(),
+			HTML.Attribute.DIR.toString(), HTML.Attribute.ENCTYPE.toString(), HTML.Attribute.HEIGHT.toString(),
+			HTML.Attribute.HREF.toString(), HTML.Attribute.HSPACE.toString(), HTML.Attribute.ID.toString(),
+			HTML.Attribute.ISMAP.toString(), HTML.Attribute.LANG.toString(), HTML.Attribute.MAXLENGTH.toString(),
+			HTML.Attribute.METHOD.toString(), HTML.Attribute.MULTIPLE.toString(), HTML.Attribute.NAME.toString(),
+			HTML.Attribute.NOHREF.toString(), HTML.Attribute.NOSHADE.toString(), HTML.Attribute.NOWRAP.toString(),
+			HTML.Attribute.PROMPT.toString(), HTML.Attribute.REL.toString(), HTML.Attribute.REV.toString(),
+			HTML.Attribute.ROWS.toString(), HTML.Attribute.ROWSPAN.toString(), HTML.Attribute.SELECTED.toString(),
+			HTML.Attribute.SHAPE.toString(), HTML.Attribute.SIZE.toString(), HTML.Attribute.SRC.toString(),
+			HTML.Attribute.START.toString(), HTML.Attribute.TARGET.toString(), HTML.Attribute.TITLE.toString(),
+			HTML.Attribute.TYPE.toString(), HTML.Attribute.USEMAP.toString(), HTML.Attribute.VALIGN.toString(),
+			HTML.Attribute.VALUE.toString(), HTML.Attribute.VSPACE.toString(), HTML.Attribute.WIDTH.toString() }));
+
+	private static final Set<String> DEFAULT_WHITELIST_TAGS = new HashSet<String>(asList(new String[] {
+			HTML.Tag.A.toString(), "abbr", "acronym", HTML.Tag.ADDRESS.toString(), HTML.Tag.AREA.toString(),
+			HTML.Tag.B.toString(), HTML.Tag.BIG.toString(), HTML.Tag.BLOCKQUOTE.toString(), HTML.Tag.BODY.toString(),
+			HTML.Tag.BR.toString(), "button", HTML.Tag.CAPTION.toString(), HTML.Tag.CENTER.toString(),
+			HTML.Tag.CITE.toString(), HTML.Tag.CODE.toString(), "col", "colgroup", HTML.Tag.DD.toString(), "del",
+			HTML.Tag.DFN.toString(), HTML.Tag.DIR.toString(), HTML.Tag.DIV.toString(), HTML.Tag.DL.toString(),
+			HTML.Tag.DT.toString(), HTML.Tag.EM.toString(), "fieldset", HTML.Tag.FONT.toString(),
+			HTML.Tag.FORM.toString(), HTML.Tag.H1.toString(), HTML.Tag.H2.toString(), HTML.Tag.H3.toString(),
+			HTML.Tag.H4.toString(), HTML.Tag.H5.toString(), HTML.Tag.H6.toString(), HTML.Tag.HEAD.toString(),
+			HTML.Tag.HR.toString(), HTML.Tag.HTML.toString(), HTML.Tag.I.toString(), HTML.Tag.IMG.toString(),
+			HTML.Tag.INPUT.toString(), "ins", HTML.Tag.KBD.toString(), "label", "legend", HTML.Tag.LI.toString(),
+			HTML.Tag.MAP.toString(), HTML.Tag.MENU.toString(), HTML.Tag.OL.toString(), "optgroup",
+			HTML.Tag.OPTION.toString(), HTML.Tag.P.toString(), HTML.Tag.PRE.toString(), "q", HTML.Tag.S.toString(),
+			HTML.Tag.SAMP.toString(), HTML.Tag.SELECT.toString(), HTML.Tag.SMALL.toString(), HTML.Tag.SPAN.toString(),
+			HTML.Tag.STRIKE.toString(), HTML.Tag.STRONG.toString(), HTML.Tag.SUB.toString(), HTML.Tag.TABLE.toString(),
+			"tbody", HTML.Tag.TD.toString(), HTML.Tag.TEXTAREA.toString(), "tfoot", HTML.Tag.TH.toString(), "thead",
+			HTML.Tag.TR.toString(), HTML.Tag.TT.toString(), HTML.Tag.U.toString(), HTML.Tag.UL.toString(),
+			HTML.Tag.VAR.toString() }));
+
+	private static final Set<String> EMPTY_SET = Collections.unmodifiableSet(new HashSet<String>(0));
+
+	private static final HTMLEditorKit.Parser PARSER = new ParserDelegator();
+
+	private static final Pattern PATTERN_DOCTYPE = Pattern.compile("<!DOCTYPE[^>]+>", Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * Parses specified list; e.g:
+	 * 
+	 * <pre>
+	 * &quot;a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],..&quot;
+	 * </pre>
+	 * 
+	 * @param list
+	 *            The list string
+	 * @return The parsed map
+	 */
+	private static Map<String, Set<String>> parse(final String list) {
+		final String[] tags = list.split("\\s*,\\s*");
+		final Map<String, Set<String>> map = new HashMap<String, Set<String>>(tags.length);
+		NextTag: for (final String tagStr : tags) {
+			final int bracketStart = tagStr.indexOf('[');
+			if (bracketStart == -1) {
+				map.put(tagStr.trim().toLowerCase(Locale.ENGLISH), EMPTY_SET);
+			} else {
+				final String tag = tagStr.substring(0, bracketStart).trim().toLowerCase(Locale.ENGLISH);
+				final int bracketEnd = tagStr.indexOf(']', bracketStart + 1);
+				if (bracketEnd == -1) {
+					/*
+					 * Invalid list string. Skip parsing attributes.
+					 */
+					map.put(tag, EMPTY_SET);
+					continue NextTag;
+				}
+				final String[] attribs = tagStr.substring(bracketStart + 1, bracketEnd).split("\\s*\\|\\s*");
+				final Set<String> attributeSet = new HashSet<String>(attribs.length);
+				for (final String attribStr : attribs) {
+					attributeSet.add(attribStr.trim().toLowerCase(Locale.ENGLISH));
+				}
+				map.put(tag, attributeSet);
+			}
+		}
+		return map;
+	}
+
+	private final int capacity;
+
+	/**
+	 * Initializes a new {@link HTMLFilter}
+	 * 
+	 * @param capacity
+	 *            The initial capacity
+	 */
+	public HTMLFilter(final int capacity) {
+		super();
+		this.capacity = capacity;
+	}
+
+	/**
+	 * Applies specified black list to given HTML content
+	 * 
+	 * @param html
+	 *            The HTML content
+	 * @param blacklist
+	 *            The black list
+	 * @return The stripped HTML content according to specified black list
+	 * @throws IOException
+	 *             If an I/O error occurs
+	 */
+	public String applyBlacklist(final String html, final Map<String, Set<String>> blacklist) throws IOException {
+		/*
+		 * Determine doctype
+		 */
+		final Matcher m = PATTERN_DOCTYPE.matcher(html);
+		final String doctype;
+		if (m.find()) {
+			doctype = m.group();
+		} else {
+			doctype = null;
+		}
+		final HTMLFilterParserCallback parserCallback = new HTMLFilterParserCallback(false, capacity, blacklist);
+		PARSER.parse(new StringReader(html), parserCallback, true);
+		return parserCallback.getHTML(doctype);
+	}
+
+	/**
+	 * Applies specified black list to given HTML content
+	 * 
+	 * @param html
+	 *            The HTML content
+	 * @param blacklist
+	 *            The black list's string representation; e.g.
+	 * 
+	 * <pre>
+	 * a[name|href|target|title|onclick],img[src|alt|title|hspace|vspace|width|height|align|name],...
+	 * </pre>
+	 * 
+	 * @return The stripped HTML content according to specified black list
+	 * @throws IOException
+	 *             If an I/O error occurs
+	 */
+	public String applyBlacklist(final String html, final String blacklist) throws IOException {
+		return applyBlacklist(html, parse(blacklist));
+	}
+
+	/**
+	 * Applies specified white list to given HTML content
+	 * 
+	 * @param html
+	 *            The HTML content
+	 * @param whitelist
+	 *            The white list
+	 * @return The stripped HTML content according to specified white list
+	 * @throws IOException
+	 *             If an I/O error occurs
+	 */
+	public String applyWhitelist(final String html, final Map<String, Set<String>> whitelist) throws IOException {
+		/*
+		 * Determine doctype
+		 */
+		final Matcher m = PATTERN_DOCTYPE.matcher(html);
+		final String doctype;
+		if (m.find()) {
+			doctype = m.group();
+		} else {
+			doctype = null;
+		}
+		final HTMLFilterParserCallback parserCallback = new HTMLFilterParserCallback(true, capacity, whitelist);
+		PARSER.parse(new StringReader(html), parserCallback, true);
+		return parserCallback.getHTML(doctype);
+	}
+
+	/**
+	 * Applies specified white list to given HTML content
+	 * 
+	 * @param html
+	 *            The HTML content
+	 * @param whitelist
+	 *            The white list's string representation; e.g.
+	 * 
+	 * <pre>
+	 * a[name|href|target|title|onclick],img[src|alt|title|hspace|vspace|width|height|align|name],...
+	 * </pre>
+	 * 
+	 * @return The stripped HTML content according to specified white list
+	 * @throws IOException
+	 *             If an I/O error occurs
+	 */
+	public String applyWhitelist(final String html, final String whitelist) throws IOException {
+		return applyWhitelist(html, parse(whitelist));
 	}
 
 }
