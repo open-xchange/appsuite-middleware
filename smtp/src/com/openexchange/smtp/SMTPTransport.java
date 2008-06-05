50acc82f95052befb2c9241e9dec71a17566a965
thorben <thorben>
2008-06-05 20:34:15 +0000

    Organized imports

--- smtp/src/com/openexchange/smtp/SMTPTransport.java
+++ smtp/src/com/openexchange/smtp/SMTPTransport.java
@@ -59,7 +59,6 @@ import java.io.UnsupportedEncodingException;
 import java.security.Security;
 import java.text.DateFormat;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.Queue;
