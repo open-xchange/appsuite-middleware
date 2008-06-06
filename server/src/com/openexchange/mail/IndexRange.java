f40f34c13bb51fb07c34a7db53f999e0f8178bb3
thorben <thorben>
2008-06-06 14:39:27 +0000

    Enhanced by common Object methods

--- server/src/com/openexchange/mail/IndexRange.java
+++ server/src/com/openexchange/mail/IndexRange.java
@@ -88,6 +88,36 @@ public final class IndexRange {
 		this.end = end;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + end;
+		result = prime * result + start;
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final IndexRange other = (IndexRange) obj;
+		if (end != other.end) {
+			return false;
+		}
+		if (start != other.start) {
+			return false;
+		}
+		return true;
+	}
+
 	/**
 	 * Gets the start index
 	 * 
@@ -106,4 +136,9 @@ public final class IndexRange {
 		return end;
 	}
 
+	@Override
+	public String toString() {
+		return new StringBuilder(super.toString()).append(" start=").append(start).append(", end=").append(end)
+				.toString();
+	}
 }
