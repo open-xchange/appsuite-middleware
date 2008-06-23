7bb44ab3fbe27714e3c6e1f9b681e838d45b1f13
marcus <marcus>
2008-06-23 08:25:56 +0000

    Fixed bug 9553 for contacts.

--- server/src/com/openexchange/groupware/contact/ContactException.java
+++ server/src/com/openexchange/groupware/contact/ContactException.java
@@ -60,7 +60,10 @@ public class ContactException extends OXException {
 	public static final String NON_CONTACT_FOLDER_MSG = "You are not allowed to store this contact in a non-contact folder: folder id %1$d in context %2$d with user %3$d";
 	public static final String NO_PERMISSION_MSG = "You do not have permission to create objects in this folder %1$d in context %2$d with user %3$d";
 	public static final String NO_READ_PERMISSION_MSG = "You do not have permission to read objects in folder %1$d in context %2$d with user %3$d";
-	public static final String OBJECT_HAS_CHANGED_MSG = "The object has changed on server side since it was last fetched.";
+    public static final String OBJECT_HAS_CHANGED_MSG =
+          "Edit Conflict. Your change cannot be completed because somebody else"
+        + " has made a conflicting change to the same item. Please refresh or "
+        + "synchronize and try again.";
 	public static final String NO_DELETE_PERMISSION_MSG = "You do not have permission to delete objects from folder %1$d in context %2$d with user %3$d";
 	public static final String EVENT_QUEUE = "Unable to initialize Event queue";
 	public static final String INIT_CONNECTION_FROM_DBPOOL = "Unable to pick up a connection from the DBPool";
