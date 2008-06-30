7e637368e082674d1a4131ac9b82e9fe3a34a1c8
thorben <thorben>
2008-06-30 18:51:04 +0000

    Fixed missing error code

--- global/src/com/openexchange/sessiond/exception/SessiondException.java
+++ global/src/com/openexchange/sessiond/exception/SessiondException.java
@@ -54,87 +54,101 @@ import com.openexchange.groupware.EnumComponent;
 
 /**
  * Exception if something not works as expected with the session.
+ * 
  * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
  */
 public class SessiondException extends AbstractOXException {
 
-    /**
-     * For Serialization.
-     */
-    private static final long serialVersionUID = 6320550676305333711L;
+	/**
+	 * For Serialization.
+	 */
+	private static final long serialVersionUID = 6320550676305333711L;
 
-    /**
-     * Initializes a new exception using the information provided by the cause.
-     * @param cause the cause of the exception.
-     */
-    public SessiondException(final AbstractOXException cause) {
-        super(cause);
-    }
+	/**
+	 * Initializes a new exception using the information provided by the cause.
+	 * 
+	 * @param cause
+	 *            the cause of the exception.
+	 */
+	public SessiondException(final AbstractOXException cause) {
+		super(cause);
+	}
 
-    /**
-     * Constructor with all parameters.
-     * @param code code
-     */
-    public SessiondException(final Code code) {
-        super(EnumComponent.SESSION, code.getCategory(), code.getDetailNumber(), code.getMessage(), null);
-    }
+	/**
+	 * Constructor with all parameters.
+	 * 
+	 * @param code
+	 *            code
+	 */
+	public SessiondException(final Code code) {
+		super(EnumComponent.SESSION, code.getCategory(), code.getDetailNumber(), code.getMessage(), null);
+	}
 
-    /**
-     * Constructor with all parameters.
-     * @param code code
-     * @param cause the cause.
-     * @param msgArgs arguments for the exception message.
-     */
-    public SessiondException(final Category category, final String message, final int detailNumber, final Throwable cause) {
-        super(EnumComponent.SESSION, category, detailNumber, message, cause);
-    }
+	/**
+	 * Constructor with all parameters.
+	 * 
+	 * @param code
+	 *            code
+	 * @param cause
+	 *            the cause.
+	 * @param msgArgs
+	 *            arguments for the exception message.
+	 */
+	public SessiondException(final Category category, final String message, final int detailNumber,
+			final Throwable cause) {
+		super(EnumComponent.SESSION, category, detailNumber, message, cause);
+	}
 
-    /**
-     * Constructor with all parameters.
-     * @param code code
-     * @param cause the cause.
-     * @param msgArgs arguments for the exception message.
-     */
-    public SessiondException(final Code code, final Throwable cause,
-        final Object... msgArgs) {
-        super(EnumComponent.SESSION, code.getCategory(), code.getDetailNumber(), code.getMessage(), cause);
-        setMessageArgs(msgArgs);
-    }
+	/**
+	 * Constructor with all parameters.
+	 * 
+	 * @param code
+	 *            code
+	 * @param cause
+	 *            the cause.
+	 * @param msgArgs
+	 *            arguments for the exception message.
+	 */
+	public SessiondException(final Code code, final Throwable cause, final Object... msgArgs) {
+		super(EnumComponent.SESSION, code.getCategory(), code.getDetailNumber(), code.getMessage(), cause);
+		setMessageArgs(msgArgs);
+	}
 
-	public SessiondException(final EnumComponent component, final Category category,
-			final int number, final String message, final Throwable cause, final Object... msgArgs) {
-        super(component, category, number, message, cause);
-        setMessageArgs(msgArgs);
+	public SessiondException(final EnumComponent component, final Category category, final int number,
+			final String message, final Throwable cause, final Object... msgArgs) {
+		super(component, category, number, message, cause);
+		setMessageArgs(msgArgs);
 	}
 
 	public enum Code {
-		SESSIOND_EXCEPTION("Sessiond Exception", 1,
-            AbstractOXException.Category.CODE_ERROR),
-		MAX_SESSION_EXCEPTION(
-            "Max Session size reached", 2,
-            AbstractOXException.Category.CODE_ERROR),
-    	SESSIOND_CONFIG_EXCEPTION("Sessiond Config Exception", 3,
-    	            AbstractOXException.Category.CODE_ERROR),
-    	MISSING_PROPERTY("Missing property '%s'", 4, AbstractOXException.Category.SETUP_ERROR),
-    	/**
-    	 * Unknown event topic %s
-    	 */
-    	UNKNOWN_EVENT_TOPIC("Unknown event topic %s", 5, AbstractOXException.Category.CODE_ERROR),
-    	/**
-    	 * Password could not be changed
-    	 */
-    	PASSWORD_UPDATE_FAILED("Password could not be changed", 6, AbstractOXException.Category.CODE_ERROR);
+		SESSIOND_EXCEPTION("Sessiond Exception", 1, AbstractOXException.Category.CODE_ERROR), MAX_SESSION_EXCEPTION(
+				"Max Session size reached", 2, AbstractOXException.Category.CODE_ERROR), SESSIOND_CONFIG_EXCEPTION(
+				"Sessiond Config Exception", 3, AbstractOXException.Category.CODE_ERROR), MISSING_PROPERTY(
+				"Missing property '%s'", 4, AbstractOXException.Category.SETUP_ERROR),
+		/**
+		 * Unknown event topic %s
+		 */
+		UNKNOWN_EVENT_TOPIC("Unknown event topic %s", 5, AbstractOXException.Category.CODE_ERROR),
+		/**
+		 * Password could not be changed
+		 */
+		PASSWORD_UPDATE_FAILED("Password could not be changed", 6, AbstractOXException.Category.CODE_ERROR),
+		/**
+		 * Max. session size for user %1$s in context %2$s exceeded
+		 */
+		MAX_SESSION_PER_USER_EXCEPTION("Max. session size for user %1$s in context %2$s exceeded", 7,
+				AbstractOXException.Category.CODE_ERROR);
 
 		/**
 		 * Message of the exception.
 		 */
 		private final String message;
-		
+
 		/**
 		 * Category of the exception.
 		 */
 		private final Category category;
-		
+
 		/**
 		 * Detail number of the exception.
 		 */
@@ -142,13 +156,15 @@ public class SessiondException extends AbstractOXException {
 
 		/**
 		 * Default constructor.
-		 * @param message message.
-		 * @param category category.
-		 * @param detailNumber detail number.
+		 * 
+		 * @param message
+		 *            message.
+		 * @param category
+		 *            category.
+		 * @param detailNumber
+		 *            detail number.
 		 */
-		private Code(final String message,
-				final int detailNumber,
-				final Category category)  {
+		private Code(final String message, final int detailNumber, final Category category) {
 			this.message = message;
 			this.category = category;
 			this.detailNumber = detailNumber;
