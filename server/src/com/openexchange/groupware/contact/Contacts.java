30706799720c2337db6e9c0d9bff2f1d22d79271
thorben <thorben>
2008-07-02 13:06:30 +0000

    Bugfix #11528: Denying editing of system contact's primary email address

--- server/src/com/openexchange/groupware/contact/Contacts.java
+++ server/src/com/openexchange/groupware/contact/Contacts.java
@@ -673,7 +673,8 @@ public final class Contacts {
 		 * throw EXCEPTIONS.createOXConflictException(63,ctx.getContextId()); }
 		 */
 
-		if (FolderObject.SYSTEM_LDAP_FOLDER_ID == fid && co.containsEmail1() && (co.getEmail1() != null)) {
+		if (FolderObject.SYSTEM_LDAP_FOLDER_ID == fid && co.containsEmail1() && (co.getEmail1() != null)
+				&& ctx.getMailadmin() != user) {
 			final Connection readCon;
 			try {
 				readCon = DBPool.pickup(ctx);
