1951bf88cfeac7fc226c5184b2c67a5a57da4939
Francisco Laguna <francisco.laguna@open-xchange.com>
2008-07-10 11:21:02 +0000

    Partial Bugfix #11569

--- openexchange-test/unittests/com/openexchange/groupware/infostore/SearchEngineTest.java
+++ openexchange-test/unittests/com/openexchange/groupware/infostore/SearchEngineTest.java
@@ -60,16 +60,21 @@ import com.openexchange.groupware.contexts.Context;
 import com.openexchange.groupware.contexts.impl.ContextStorage;
 import com.openexchange.groupware.infostore.search.impl.SearchEngineImpl;
 import com.openexchange.groupware.infostore.utils.Metadata;
+import com.openexchange.groupware.infostore.facade.impl.InfostoreFacadeImpl;
+import com.openexchange.groupware.infostore.database.impl.DocumentMetadataImpl;
 import com.openexchange.groupware.ldap.User;
 import com.openexchange.groupware.ldap.UserStorage;
 import com.openexchange.groupware.tx.DBPoolProvider;
 import com.openexchange.groupware.tx.DBProvider;
+import com.openexchange.groupware.tx.TransactionException;
 import com.openexchange.groupware.userconfiguration.UserConfiguration;
 import com.openexchange.groupware.userconfiguration.UserConfigurationStorage;
 import com.openexchange.test.TestInit;
 import com.openexchange.tools.oxfolder.OXFolderAccess;
 import com.openexchange.tools.session.ServerSession;
 import com.openexchange.tools.session.ServerSessionFactory;
+import com.openexchange.tools.iterator.SearchIterator;
+import com.openexchange.tools.iterator.SearchIteratorException;
 
 /**
  * @author Francisco Laguna <francisco.laguna@open-xchange.com>
@@ -94,6 +99,7 @@ public class SearchEngineTest extends TestCase {
     private List<FolderObject> cleanFolders = null;
 
     private DBProvider provider = null;
+    private InfostoreFacade infostore = null;
 
     @Override
     public void setUp() throws Exception {
@@ -123,6 +129,7 @@ public class SearchEngineTest extends TestCase {
 
         provider = new DBPoolProvider();
         searchEngine = new SearchEngineImpl(provider);
+        infostore = new InfostoreFacadeImpl(provider);
 
     }
 
@@ -138,6 +145,50 @@ public class SearchEngineTest extends TestCase {
         assertSurvivesOrder(new Metadata[]{Metadata.FOLDER_ID_LITERAL, Metadata.ID_LITERAL, Metadata.TITLE_LITERAL});
         assertSurvivesOrder(new Metadata[]{Metadata.FOLDER_ID_LITERAL, Metadata.TITLE_LITERAL, Metadata.ID_LITERAL});
         assertSurvivesOrder(new Metadata[]{Metadata.FOLDER_ID_LITERAL, Metadata.TITLE_LITERAL});
+    }
+
+    // Bug 11569
+
+    public void testSearchForPercent() throws OXException, SearchIteratorException {
+        DocumentMetadata doc1 = createWithTitle("100%");
+                                createWithTitle("Hallo");
+        
+
+        SearchIterator iter = searchEngine.search("%",new Metadata[]{Metadata.ID_LITERAL, Metadata.TITLE_LITERAL}, folderId, Metadata.TITLE_LITERAL, SearchEngine.ASC,0,10,ctx, user, userConfig);
+
+        assertTrue(iter.hasNext());
+        DocumentMetadata gotDoc = (DocumentMetadata) iter.next();
+
+        assertFalse(iter.hasNext());
+
+        assertEquals(doc1.getId(), gotDoc.getId());
+    }
+
+    private DocumentMetadata createWithTitle(String title) throws OXException {
+        DocumentMetadata metadata = new DocumentMetadataImpl() ;
+        metadata.setTitle(title);
+        metadata.setFolderId(folderId);
+        try {
+            infostore.startTransaction();
+            infostore.saveDocumentMetadata(metadata, InfostoreFacade.NEW, session);
+            infostore.commit();
+            clean.add( metadata );
+        } catch (OXException x) {
+            try {
+                infostore.rollback();
+            } catch (TransactionException e) {
+                e.printStackTrace();
+            }
+            throw x;
+        } finally {
+            try {
+                infostore.finish();
+            } catch (TransactionException e) {
+                e.printStackTrace();
+            }
+        }
+        return metadata;
+
     }
 
     private void assertSurvivesOrder(final Metadata[] metadata) {
