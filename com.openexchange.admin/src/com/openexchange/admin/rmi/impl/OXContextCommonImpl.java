1df90cb227f2b53f5fab1bc31970a41ad2c9c79b
Carsten Hoeger <carsten.hoeger@open-xchange.com>
2008-10-08 14:08:54 +0000

    list, create and delete now working with reseller bundle

--- com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXContextCommonImpl.java
+++ com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXContextCommonImpl.java
@@ -61,6 +61,7 @@ import org.osgi.framework.ServiceReference;
 
 import com.openexchange.admin.daemons.AdminDaemon;
 import com.openexchange.admin.plugins.OXContextPluginInterface;
+import com.openexchange.admin.plugins.PluginException;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
 import com.openexchange.admin.rmi.dataobjects.Database;
@@ -153,10 +154,6 @@ public abstract class OXContextCommonImpl extends OXCommonImpl {
             log.error(e.getMessage(), e);
             throw e;
         }
-    }
-    
-    protected boolean isBundleRegistered() {
-        return true;
     }
     
     /**
