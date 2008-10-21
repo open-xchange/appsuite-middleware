26d476845fa4844505771904ddce064c08f9ab1d
thorben <thorben>
2008-10-21 14:45:01 +0000

    Bugfix #12357: Adding image attachments to an inline-forwarded mail if it its content-disposition is INLINE but specifies a file name

--- server/src/com/openexchange/mail/parser/handlers/NonInlineForwardPartHandler.java
+++ server/src/com/openexchange/mail/parser/handlers/NonInlineForwardPartHandler.java
@@ -146,7 +146,7 @@ public final class NonInlineForwardPartHandler implements MailMessageHandler {
 
 	public boolean handleImagePart(final MailPart part, final String imageCID, final String baseContentType,
 			final boolean isInline, final String fileName, final String id) throws MailException {
-		if (!isInline || imageCID != null) {
+		if (!isInline || imageCID != null || part.getContentDisposition().containsFilenameParameter()) {
 			/*
 			 * Add if disposition is non-inline or a content ID is present
 			 */
