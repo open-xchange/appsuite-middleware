c99c55659a5e411fe829457e15a5c09e1718e5a9
dennis <dennis>
2008-10-21 14:40:21 +0000

    - added a new test case

--- com.openexchange.admin.contextrestore/test/com/openexchange/admin/contextrestore/Contextrestore.java
+++ com.openexchange.admin.contextrestore/test/com/openexchange/admin/contextrestore/Contextrestore.java
@@ -156,6 +156,85 @@ public class Contextrestore {
     }
 
     /**
+     * This test does a restore on an already existing context. The difference to the test before is that here the context has data before the dump
+     * 
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    public void testRestoreWithBeforeData() throws IOException, InterruptedException {
+        final ShellExecutor shellExecutor = new ShellExecutor();
+        ArrayOutput retval = shellExecutor.executeprocargs(new String[]{"/opt/open-xchange/sbin/createuser", "-A", CONTEXT_ADMIN_USERNAME , "-P", CONTEXT_ADMIN_PW, "-c", 
+                newcontextid, "-u", "test1", "-d", "test1", "-g", "test1", "-s", "test1", "-p", CONTEXT_ADMIN_PW, "-e", "xyz27@bla.de"});
+        assertTrue("User creation failed", 0 == retval.exitstatus);
+        System.out.println("New user created");
+        
+        retval = shellExecutor.executeprocargs(new String[]{"/opt/open-xchange/sbin/creategroup", "-A", CONTEXT_ADMIN_USERNAME, "-P", CONTEXT_ADMIN_PW, "-n", "test", 
+                "-d", "test", "-c", newcontextid});
+        assertTrue("Group creation failed", 0 == retval.exitstatus);
+        System.out.println("New group created");
+        
+        retval = shellExecutor.executeprocargs(new String[]{"/opt/open-xchange/sbin/createresource", "-A", CONTEXT_ADMIN_USERNAME, "-P", CONTEXT_ADMIN_PW, "-c",
+                newcontextid, "-n", "test", "-d", "test", "-e", "ressource@test.de"});
+        assertTrue("Resource creation failed", 0 == retval.exitstatus);
+        System.out.println("New resource created");
+        
+        // First we dump the database...
+        retval = shellExecutor.executeprocargs(new String[]{"/bin/bash" , "-c" ,
+                "mysqldump --user=" + username + " -p" + password  + " --databases information_schema choeger_7 mysql --single-transaction > " + userdumpfile});
+        assertTrue("Dump creation for user databases failed due to the following reason: " + retval.errOutput, 0 == retval.exitstatus);
+        System.out.println("User dump created");
+        
+        retval = shellExecutor.executeprocargs(new String[]{"/bin/bash" , "-c" ,
+                "mysqldump --user=" + username + " -p" + password  + " --databases information_schema configdb mysql --single-transaction > " + configdbdumpfile});
+        assertTrue("Dump creation for configdb failed", 0 == retval.exitstatus);
+        System.out.println("Config dump created");
+        
+        // ... then we add some new data to the context ...
+        retval = shellExecutor.executeprocargs(new String[]{"/opt/open-xchange/sbin/createuser", "-A", CONTEXT_ADMIN_USERNAME , "-P", CONTEXT_ADMIN_PW, "-c", 
+                newcontextid, "-u", "test2", "-d", "test2", "-g", "test2", "-s", "test2", "-p", CONTEXT_ADMIN_PW, "-e", "xyz127@bla.de"});
+        assertTrue("User creation failed", 0 == retval.exitstatus);
+        System.out.println("New user created");
+        
+        retval = shellExecutor.executeprocargs(new String[]{"/opt/open-xchange/sbin/creategroup", "-A", CONTEXT_ADMIN_USERNAME, "-P", CONTEXT_ADMIN_PW, "-n", "test2", 
+                "-d", "test2", "-c", newcontextid});
+        assertTrue("Group creation failed", 0 == retval.exitstatus);
+        System.out.println("New group created");
+        
+        retval = shellExecutor.executeprocargs(new String[]{"/opt/open-xchange/sbin/createresource", "-A", CONTEXT_ADMIN_USERNAME, "-P", CONTEXT_ADMIN_PW, "-c",
+                newcontextid, "-n", "test2", "-d", "test2", "-e", "ressource2@test.de"});
+        assertTrue("Resource creation failed", 0 == retval.exitstatus);
+        System.out.println("New resource created");
+        
+        // ... afterwards the restore takes places ...
+        retval = shellExecutor.executeprocargs(new String[]{"/opt/open-xchange/sbin/contextrestore", "-A", "oxadminmaster" , "-P", oxmasterpassword, "-c",
+                newcontextid, "-f", userdumpfile + ',' + configdbdumpfile});
+        assertTrue("Restore failed", 0 == retval.exitstatus);
+        System.out.println("Restore done");
+        
+        // ... finally we create new dumps
+        retval = shellExecutor.executeprocargs(new String[]{"/bin/bash" , "-c" ,
+                "mysqldump --user=" + username + " -p" + password  + " --databases information_schema choeger_7 mysql --single-transaction > " + userdumpfile2});
+        assertTrue("Dump creation failed", 0 == retval.exitstatus);
+        System.out.println("2nd user dump created");
+        
+        retval = shellExecutor.executeprocargs(new String[]{"/bin/bash" , "-c" ,
+                "mysqldump --user=" + username + " -p" + password  + " --databases information_schema configdb mysql --single-transaction > " + configdbdumpfile2});
+        assertTrue("Dump creation for configdb failed", 0 == retval.exitstatus);
+        System.out.println("2nd config dump created");
+        
+        // If the dump we made at first is equal to the dump created afterwards everything is fine
+        retval = shellExecutor.executeprocargs(new String[]{"/usr/bin/diff", "-I", "^.*Dump completed on.*$", userdumpfile, userdumpfile2});
+        assertTrue("The user dumps are different: " + retval.stdOutput, 0 == retval.exitstatus);
+        System.out.println("First diff ok");
+        
+        retval = shellExecutor.executeprocargs(new String[]{"/usr/bin/diff", "-I", "^.*Dump completed on.*$", configdbdumpfile, configdbdumpfile2});
+        assertTrue("The config dumps are different: " + retval.stdOutput, 0 == retval.exitstatus);
+        System.out.println("Second diff ok");
+        
+    }
+
+    /**
      * This test does a restore on a context which was deleted beforehand
      * 
      * @throws IOException
