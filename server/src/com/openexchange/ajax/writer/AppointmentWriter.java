a1049603bc5b9dd33dbf972a24f3cdd37e624789
francisco <francisco>
2008-10-24 10:45:50 +0000

    Don't crash when start and end date are not set.

--- server/src/com/openexchange/ajax/writer/AppointmentWriter.java
+++ server/src/com/openexchange/ajax/writer/AppointmentWriter.java
@@ -87,7 +87,7 @@ public class AppointmentWriter extends CalendarWriter {
 
 	public void writeArray(final AppointmentObject appointmentObj, final int cols[], final Date betweenStart,
 			final Date betweenEnd, final JSONArray jsonArray) throws JSONException {
-		if (appointmentObj.getFullTime()) {
+		if (appointmentObj.getFullTime() && betweenStart != null && betweenEnd != null) {
 			if (CalendarCommonCollection.inBetween(appointmentObj.getStartDate().getTime(), appointmentObj.getEndDate()
 					.getTime(), betweenStart.getTime(), betweenEnd.getTime())) {
 				writeArray(appointmentObj, cols, jsonArray);
