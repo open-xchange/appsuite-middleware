c25c0838f45fe030d44af59392003d95808c1769
marcus <marcus>
2008-11-25 15:12:13 +0000

    Added DATABASE UPDATE TASK CorrectIndexes that will drop useless indexes and create new helpful ones.

---
+++ server/src/com/openexchange/groupware/update/tasks/CorrectIndexes.java
@@ -0,0 +1,174 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.groupware.update.tasks;
+
+import static com.openexchange.tools.sql.DBUtils.autocommit;
+import static com.openexchange.tools.sql.DBUtils.rollback;
+import static com.openexchange.tools.update.Tools.createIndex;
+import static com.openexchange.tools.update.Tools.dropIndex;
+import static com.openexchange.tools.update.Tools.existsIndex;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.commons.logging.Log;
+
+import com.openexchange.database.Database;
+import com.openexchange.groupware.AbstractOXException;
+import com.openexchange.groupware.EnumComponent;
+import com.openexchange.groupware.OXExceptionSource;
+import com.openexchange.groupware.OXThrowsMultiple;
+import com.openexchange.groupware.AbstractOXException.Category;
+import com.openexchange.groupware.update.Schema;
+import com.openexchange.groupware.update.UpdateTask;
+import com.openexchange.groupware.update.exception.Classes;
+import com.openexchange.groupware.update.exception.UpdateExceptionFactory;
+
+/**
+ *
+ * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
+ */
+@OXExceptionSource(classId = Classes.UPDATE_TASK, component = EnumComponent.UPDATE)
+public class CorrectIndexes implements UpdateTask {
+
+    /**
+     * Logger.
+     */
+    private static final Log LOG = LogFactory.getLog(CorrectIndexes.class);
+
+    private static final UpdateExceptionFactory EXCEPTION = new UpdateExceptionFactory(CorrectIndexes.class);
+
+    /**
+     * Default constructor.
+     */
+    public CorrectIndexes() {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int addedWithVersion() {
+        return 30;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getPriority() {
+        return UpdateTaskPriority.NORMAL.priority;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @OXThrowsMultiple(category = { Category.CODE_ERROR },
+        desc = { "" },
+        exceptionId = { 1 },
+        msg = { "An SQL error occurred: %1$s." }
+    )
+    public void perform(final Schema schema, final int contextId)
+        throws AbstractOXException {
+        final Connection con = Database.get(contextId, true);
+        try {
+            con.setAutoCommit(false);
+            correctAppointmentIndexes(con);
+            con.commit();
+        } catch (final SQLException e) {
+            rollback(con);
+            throw EXCEPTION.create(1, e, e.getMessage());
+        } finally {
+            autocommit(con);
+            Database.back(contextId, true, con);
+        }
+    }
+
+    private void correctAppointmentIndexes(final Connection con) {
+        final String table = "prg_dates";
+        final String[] oldcolumns1 = new String[] { "timestampfield01" };
+        final String[] newcolumns1 = new String[] { "cid", "timestampfield01" };
+        try {
+            final String index2Drop = existsIndex(con, table, oldcolumns1);
+            if (null != index2Drop) {
+                LOG.info("Dropping old index " + index2Drop
+                    + " on table prg_dates.");
+                dropIndex(con, table, index2Drop);
+            }
+            final String index2Create = existsIndex(con, table, newcolumns1);
+            if (null == index2Create) {
+                LOG.info("Creating new index (cid,timestampfield01) on table"
+                    + " prg_dates.");
+                createIndex(con, table, newcolumns1);
+            }
+        } catch (final SQLException e) {
+            LOG.error("Problem correcting indexes on table prg_dates.", e);
+        }
+        final String[] oldcolumns2 = new String[] { "timestampfield02" };
+        final String[] newcolumns2 = new String[] { "cid", "timestampfield02" };
+        try {
+            final String index2Drop = existsIndex(con, table, oldcolumns2);
+            if (null != index2Drop) {
+                LOG.info("Dropping old index " + index2Drop
+                    + " on table prg_dates.");
+                dropIndex(con, table, index2Drop);
+            }
+            final String index2Create = existsIndex(con, table, newcolumns2);
+            if (null == index2Create) {
+                LOG.info("Creating new index (cid,timestampfield02) on table"
+                    + " prg_dates.");
+                createIndex(con, table, newcolumns2);
+            }
+        } catch (final SQLException e) {
+            LOG.error("Problem correcting indexes on table prg_dates.", e);
+        }
+    }
+}
--- server/src/com/openexchange/tools/update/Tools.java
+++ server/src/com/openexchange/tools/update/Tools.java
@@ -49,12 +49,19 @@
 
 package com.openexchange.tools.update;
 
+import static com.openexchange.tools.sql.DBUtils.closeSQLStuff;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import com.openexchange.tools.sql.DBUtils;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * This class contains some tools to ease update of database.
@@ -69,7 +76,7 @@ public final class Tools {
         super();
     }
 
-    public static boolean isNullable(final Connection con, final String table,
+    public static final boolean isNullable(final Connection con, final String table,
         final String column) throws SQLException {
         final DatabaseMetaData meta = con.getMetaData();
         ResultSet result = null;
@@ -84,24 +91,127 @@ public final class Tools {
                     + column + " in table " + table + '.');
             }
         } finally {
-            DBUtils.closeSQLStuff(result);
+            closeSQLStuff(result);
         }
         return retval;
     }
 
-    public static boolean tableExists(final Connection con, final String table) throws SQLException {
-    	final DatabaseMetaData metaData = con.getMetaData();
-    	ResultSet rs = null;
-    	boolean retval = false;
-    	try{
-    		rs = metaData.getTables(null, null, table, new String[] {TABLE});
-    		retval = (rs.next() && rs.getString("TABLE_NAME").equals(table));
-    	} finally {
-    		DBUtils.closeSQLStuff(rs);
-    	}
-    	return retval;
+    /**
+     * @param con readable database connection.
+     * @param table table name that indexes should be tested.
+     * @param columns column names that the index must cover.
+     * @return the name of an index that matches the given columns or
+     * <code>null</code> if no matching index is found.
+     * @throws SQLException if some SQL problem occurs.
+     * @throws NullPointerException if one of the columns is <code>null</code>.
+     */
+    public static final String existsIndex(final Connection con, final String table,
+        final String[] columns) throws SQLException {
+        final DatabaseMetaData metaData = con.getMetaData();
+        final Map<String, ArrayList<String>> indexes = new HashMap<String, ArrayList<String>>();
+        ResultSet result = null;
+        try {
+            result = metaData.getIndexInfo(null, null, table, false, false);
+            while (result.next()) {
+                final String indexName = result.getString(6);
+                final int columnPos = result.getInt(8);
+                final String columnName = result.getString(9);
+                ArrayList<String> foundColumns = indexes.get(indexName);
+                if (null == foundColumns) {
+                    foundColumns = new ArrayList<String>();
+                    indexes.put(indexName, foundColumns);
+                }
+                while (foundColumns.size() < columnPos) {
+                    foundColumns.add(null);
+                }
+                foundColumns.set(columnPos - 1, columnName);
+            }
+        } finally {
+            closeSQLStuff(result);
+        }
+        String foundIndex = null;
+        final Iterator<Entry<String, ArrayList<String>>> iter = indexes.entrySet().iterator();
+        while (null == foundIndex && iter.hasNext()) {
+            final Entry<String, ArrayList<String>> entry = iter.next();
+            final ArrayList<String> foundColumns = entry.getValue();
+            if (columns.length != foundColumns.size()) {
+                continue;
+            }
+            boolean matches = true;
+            for (int i = 0; matches && i < columns.length; i++) {
+                matches = columns[i].equals(foundColumns.get(i));
+            }
+            if (matches) {
+                foundIndex = entry.getKey();
+            }
+        }
+        return foundIndex;
     }
-    
+
+    /**
+     * This method drops an index with the given name. Beware, this method is
+     * vulnerable to SQL injection because table and index name can not be set
+     * through a {@link PreparedStatement}.
+     * @param con writable database connection.
+     * @param table table name that index should be dropped.
+     * @param index name of the index to drop.
+     * @throws SQLException if some SQL problem occurs.
+     */
+    public static final void dropIndex(final Connection con, final String table,
+        final String index) throws SQLException {
+        final String sql = "ALTER TABLE `" + table + "` DROP INDEX `" + index
+            + "`";
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            stmt.execute(sql);
+        } finally {
+            closeSQLStuff(null, stmt);
+        }
+    }
+
+    /**
+     * This method creates a new index on a table. Beware, this method is
+     * vulnerable to SQL injection because table and column names can not be set
+     * through a {@link PreparedStatement}.
+     * @param con writable database connection.
+     * @param table name of the table that should get a new index.
+     * @param columns names of the columns the index should cover.
+     * @throws SQLException if some SQL problem occurs.
+     */
+    public static final void createIndex(final Connection con, final String table,
+        final String[] columns) throws SQLException {
+        final StringBuilder sql = new StringBuilder("ALTER TABLE `");
+        sql.append(table);
+        sql.append("` ADD INDEX (`");
+        for (String column : columns) {
+            sql.append(column);
+            sql.append("`,`");
+        }
+        sql.setLength(sql.length() - 2);
+        sql.append(')');
+        Statement stmt = null;
+        try {
+            stmt = con.createStatement();
+            stmt.execute(sql.toString());
+        } finally {
+            closeSQLStuff(null, stmt);
+        }
+    }
+
+    public static final boolean tableExists(final Connection con, final String table) throws SQLException {
+        final DatabaseMetaData metaData = con.getMetaData();
+        ResultSet rs = null;
+        boolean retval = false;
+        try{
+            rs = metaData.getTables(null, null, table, new String[] {TABLE});
+            retval = (rs.next() && rs.getString("TABLE_NAME").equals(table));
+        } finally {
+            closeSQLStuff(rs);
+        }
+        return retval;
+    }
+
     private static final int NULLABLE = 11;
     private static final String TABLE = "TABLE";
 }
