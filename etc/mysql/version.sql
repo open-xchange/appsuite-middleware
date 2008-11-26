e11b4f97d9fe4f135bc63b0e52d895802a8890ad
marcus <marcus>
2008-11-26 15:33:02 +0000

    Consolidation of schema creating scripts.

--- etc/mysql/version.sql
+++ etc/mysql/version.sql
@@ -1,7 +1,9 @@
+#@(#) Table for storing the schema version.
+
 CREATE TABLE version (
-	version INT4 UNSIGNED NOT NULL,
-	locked BOOLEAN NOT NULL,
-	gw_compatible BOOLEAN NOT NULL,
-	admin_compatible BOOLEAN NOT NULL,
-	server VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE = InnoDB;
+    version INT4 UNSIGNED NOT NULL,
+    locked BOOLEAN NOT NULL,
+    gw_compatible BOOLEAN NOT NULL,
+    admin_compatible BOOLEAN NOT NULL,
+    server VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
