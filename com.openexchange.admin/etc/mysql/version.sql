7a1e9a9fcb9d503df259679b35c9ad50940301a3
marcus <marcus>
2008-12-01 12:04:15 +0000

    Consolidation of schema creating scripts.

--- com.openexchange.admin/etc/mysql/version.sql
+++ com.openexchange.admin/etc/mysql/version.sql
@@ -6,4 +6,4 @@ CREATE TABLE version (
     gw_compatible BOOLEAN NOT NULL,
     admin_compatible BOOLEAN NOT NULL,
     server VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE = InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci;
