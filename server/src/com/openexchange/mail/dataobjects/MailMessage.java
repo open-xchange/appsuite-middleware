e5b6f410f355d851b453fdf11eb09b531fee001e
thorben <thorben>
2008-12-19 14:15:15 +0000

    Enhanced JavaDoc comments

--- server/src/com/openexchange/mail/dataobjects/MailMessage.java
+++ server/src/com/openexchange/mail/dataobjects/MailMessage.java
@@ -1370,7 +1370,8 @@ public abstract class MailMessage extends MailPart {
      * folder. The ID returned by this method is used in storages to refer to a
      * mail.
      * <p>
-     * <b>Note</b> that max. 52 bits may be used in returned value.
+     * <b>Note</b> that max. 52 bits may be used in returned value which implies
+     * a max. allowed value of <code>4503599627370495</code>.
      * 
      * @return The ID of this mail or <code>-1</code> if not available.
      */
@@ -1381,7 +1382,8 @@ public abstract class MailMessage extends MailPart {
      * folder. The ID returned by this method is used in storages to refer to a
      * mail.
      * <p>
-     * <b>Note</b> that max. 52 bits may be used in specified value.
+     * <b>Note</b> that max. 52 bits may be used in specified value which
+     * implies a max. allowed value of <code>4503599627370495</code>.
      * 
      * @param id The mail ID or <code>-1</code> to indicate its absence
      */
