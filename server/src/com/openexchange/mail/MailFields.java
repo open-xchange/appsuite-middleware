a77bbaaf4a9974a6ec6ea21362363484ec4307ae
thorben <thorben>
2009-01-14 11:14:45 +0000

    Added hashcode() and equals()

--- server/src/com/openexchange/mail/MailFields.java
+++ server/src/com/openexchange/mail/MailFields.java
@@ -173,4 +173,31 @@ public final class MailFields {
         }
         return l.toArray(new MailField[l.size()]);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(arr);
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MailFields other = (MailFields) obj;
+        if (!Arrays.equals(arr, other.arr)) {
+            return false;
+        }
+        return true;
+    }
+
 }
