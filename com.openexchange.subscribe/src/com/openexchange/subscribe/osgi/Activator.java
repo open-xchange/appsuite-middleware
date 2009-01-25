ccc10168f4b84460383155c009435d3605bc7ab8
francisco <francisco>
2009-01-25 21:10:45 +0000

    export handler service

--- com.openexchange.subscribe/src/com/openexchange/subscribe/osgi/Activator.java
+++ com.openexchange.subscribe/src/com/openexchange/subscribe/osgi/Activator.java
@@ -6,6 +6,8 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import com.openexchange.subscribe.SubscribeService;
 import com.openexchange.subscribe.SubscribeServiceImpl;
+import com.openexchange.subscribe.SubscriptionHandler;
+import com.openexchange.subscribe.parser.MicroformatContactParser;
 
 /**
  * @author <a href="mailto:martin.herfurth@open-xchange.org">Martin Herfurth</a>
@@ -13,6 +15,7 @@ import com.openexchange.subscribe.SubscribeServiceImpl;
 public class Activator implements BundleActivator {
 
     private ServiceRegistration registryPublish;
+    private ServiceRegistration handlerPublish;
 
     /*
      * (non-Javadoc)
@@ -20,7 +23,7 @@ public class Activator implements BundleActivator {
      */
     public void start(BundleContext context) throws Exception {
         registryPublish = context.registerService(SubscribeService.class.getName(), new SubscribeServiceImpl(), null);
-        
+        handlerPublish =  context.registerService(SubscriptionHandler.class.getName(), new MicroformatContactParser(), null);
     }
 
     /*
@@ -29,6 +32,7 @@ public class Activator implements BundleActivator {
      */
     public void stop(BundleContext context) throws Exception {
         registryPublish.unregister();
+        handlerPublish.unregister();
     }
 
 }
