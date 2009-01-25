a07e89c51b20f992f8c230c8156ee6e4c9a91589
martin <martin>
2009-01-25 18:55:34 +0000

    activator

--- com.openexchange.subscribe.json/src/com/openexchange/subscribe/json/osgi/Activator.java
+++ com.openexchange.subscribe.json/src/com/openexchange/subscribe/json/osgi/Activator.java
@@ -1,22 +1,137 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
 package com.openexchange.subscribe.json.osgi;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import javax.servlet.Servlet;
+import org.osgi.service.http.HttpService;
+import com.openexchange.server.osgiservice.DeferredActivator;
+import com.openexchange.subscribe.SubscribeService;
+import com.openexchange.subscribe.json.SubscribeJSONServlet;
 
-public class Activator implements BundleActivator {
+/**
+ * @author <a href="mailto:martin.herfurth@open-xchange.org">Martin Herfurth</a>
+ */
+public class Activator extends DeferredActivator {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-	}
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(Activator.class);
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-	}
+    private static final String ALIAS = "ajax/subscriptions";
 
+    private Servlet subscribeServlet;
+
+    private static final Class<?>[] NEEDED_SERVICES = { HttpService.class, SubscribeService.class };
+
+    @Override
+    protected Class<?>[] getNeededServices() {
+        return NEEDED_SERVICES;
+    }
+
+    @Override
+    protected void handleAvailability(final Class<?> clazz) {
+        SubscribeService subscribeService = getService(SubscribeService.class);
+        SubscribeJSONServlet.setSubscribeService(subscribeService);
+
+        final HttpService httpService = getService(HttpService.class);
+        try {
+            httpService.registerServlet(ALIAS, (subscribeServlet = new SubscribeJSONServlet()), null, null);
+            LOG.info(SubscribeJSONServlet.class.getName() + " successfully re-registered due to re-appearing of " + clazz.getName());
+        } catch (final Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+
+    }
+
+    @Override
+    protected void handleUnavailability(final Class<?> clazz) {
+        final HttpService httpService = getService(HttpService.class);
+        if (httpService != null && subscribeServlet != null) {
+            httpService.unregister(ALIAS);
+            subscribeServlet = null;
+            LOG.info(SubscribeJSONServlet.class.getName() + " unregistered due to disappearing of " + clazz.getName());
+        }
+    }
+
+    @Override
+    protected void startBundle() throws Exception {
+        SubscribeService subscribeService = getService(SubscribeService.class);
+        SubscribeJSONServlet.setSubscribeService(subscribeService);
+
+        if (subscribeService == null) {
+            return;
+        }
+
+        try {
+            final HttpService httpService = getService(HttpService.class);
+            if (httpService == null) {
+                return;
+            }
+            httpService.registerServlet(ALIAS, (subscribeServlet = new SubscribeJSONServlet()), null, null);
+            LOG.info(SubscribeJSONServlet.class.getName() + " successfully registered");
+        } catch (final Exception e) {
+            LOG.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    @Override
+    protected void stopBundle() throws Exception {
+        try {
+            final HttpService httpService = getService(HttpService.class);
+            if (httpService != null && subscribeServlet != null) {
+                httpService.unregister(ALIAS);
+                subscribeServlet = null;
+                LOG.info(SubscribeJSONServlet.class.getName() + " unregistered due to bundle stop");
+            }
+        } catch (final Exception e) {
+            LOG.error(e.getMessage(), e);
+            throw e;
+        }
+    }
 }
