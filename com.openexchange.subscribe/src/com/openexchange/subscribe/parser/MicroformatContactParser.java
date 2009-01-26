e1b48401bed3e6513e752eea8ef20a637da6412a
tierlieb <tierlieb>
2009-01-26 09:57:29 +0000

    added update or insert instead of simple insert. Untested.

--- com.openexchange.subscribe/src/com/openexchange/subscribe/parser/MicroformatContactParser.java
+++ com.openexchange.subscribe/src/com/openexchange/subscribe/parser/MicroformatContactParser.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Collection;
+import java.util.Date;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -20,6 +22,8 @@ import com.openexchange.subscribe.SubscribeService;
 import com.openexchange.subscribe.Subscription;
 import com.openexchange.subscribe.SubscriptionHandler;
 import com.openexchange.subscribe.SubscriptionSession;
+import com.openexchange.tools.iterator.SearchIterator;
+import com.openexchange.tools.iterator.SearchIteratorException;
 
 
 public class MicroformatContactParser implements SubscriptionHandler {
@@ -34,33 +38,82 @@ public class MicroformatContactParser implements SubscriptionHandler {
         this.service = service;
     }
  
+    /**
+     * Estimates whether two contacts are the same, albeit one might contain updated data.
+     * @param first
+     * @param second
+     * @return
+     */
+    protected boolean isSame(ContactObject first, ContactObject second){
+        return first.getGivenName().equals(second.getGivenName()) 
+        &&  first.getSurName().equals(second.getSurName());
+    }
+    
+    /**
+     * Read the site of a subscription and return its content as a string
+     * @param subscription
+     * @return
+     * @throws IOException
+     */
+    protected String readSubscription(Subscription subscription) throws IOException{
+        URL url = new URL(subscription.getUrl());
+        URLConnection connection = url.openConnection();
+        BufferedReader buffy = new BufferedReader( new InputStreamReader( connection.getInputStream() ) );
+        StringBuilder bob = new StringBuilder();
+        String line = buffy.readLine();
+        while (line != null){
+            bob.append (line);
+            bob.append ("\n");
+            line = buffy.readLine();
+        }
+        return bob.toString();
+    }
+    
+    /**
+     * Update or insert contacts from a subscription
+     * @param subscription
+     * @throws ContextException
+     * @throws OXException
+     */
+    protected void storeContacts(Subscription subscription, Collection<ContactObject> updatedContacts) throws ContextException, OXException{
+        Session session = new SubscriptionSession(subscription);
+        RdbContactSQLInterface storage = new RdbContactSQLInterface(session);
+        
+        for(ContactObject updatedContact: updatedContacts){
+            SearchIterator<ContactObject> existingContacts = storage.getContactsInFolder(subscription.getFolderId(), 0, 0, 0, null, ContactObject.ALL_COLUMNS);
+            boolean foundMatch = false;
+            while( existingContacts.hasNext() && ! foundMatch ){
+                ContactObject existingContact = null;
+                try {
+                    existingContact = existingContacts.next();
+                } catch (SearchIteratorException e) {
+                    e.printStackTrace();
+                }
+                if( existingContact == null)
+                    continue;
+                if( isSame(existingContact, updatedContact)){
+                    foundMatch = true;
+                    updatedContact.setObjectID( existingContact.getObjectID() );
+                    storage.updateContactObject(updatedContact, subscription.getFolderId(), new Date() );
+                }
+            }
+            if(foundMatch)
+                continue;
+            updatedContact.setParentFolderID( subscription.getFolderId() );
+            storage.insertContactObject(updatedContact);
+        }
+    }
+    
     /* (non-Javadoc)
      * @see com.openexchange.subscribe.parser.SubscriptionHandler#handleSubscription(com.openexchange.subscribe.Subscription)
      */
     public void handleSubscription(Subscription subscription){
         try {
-            //read from url
-            URL url = new URL(subscription.getUrl());
-            URLConnection connection = url.openConnection();
-            BufferedReader buffy = new BufferedReader( new InputStreamReader( connection.getInputStream() ) );
-            StringBuilder bob = new StringBuilder();
-            String line = buffy.readLine();
-            while (line != null){
-                bob.append (line);
-                bob.append ("\n");
-                line = buffy.readLine();
-            }
+            String website = readSubscription(subscription);
             
-            //parse for contacts
-            parse(bob.toString());
+            parse( website );
             
-            //store contacts in DB
-            Session session = new SubscriptionSession(subscription);
-            RdbContactSQLInterface storage = new RdbContactSQLInterface(session);
-            for(ContactObject co: getContacts() ){
-                co.setParentFolderID( subscription.getFolderId() );
-                storage.insertContactObject(co);
-            }
+            storeContacts(subscription, this.getContacts());
             
         } catch (IOException e) {
             // TODO Auto-generated catch block
