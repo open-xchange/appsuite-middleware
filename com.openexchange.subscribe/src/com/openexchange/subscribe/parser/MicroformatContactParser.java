b835742029b7f0e3e1dcca4bd0bbb0f283e1c220
tierlieb <tierlieb>
2009-01-26 10:05:21 +0000

    added update or insert instead of simple insert. Untested.

--- com.openexchange.subscribe/src/com/openexchange/subscribe/parser/MicroformatContactParser.java
+++ com.openexchange.subscribe/src/com/openexchange/subscribe/parser/MicroformatContactParser.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Collection;
