7d6fb219c75f70ca42ecb04108f07ead2403fa67
dennis <dennis>
2009-01-27 16:44:28 +0000

    - fixed output to be consistent with the other command line tools

--- com.openexchange.admin.reseller/src/com/openexchange/admin/reseller/console/Create.java
+++ com.openexchange.admin.reseller/src/com/openexchange/admin/reseller/console/Create.java
@@ -49,20 +49,10 @@
 
 package com.openexchange.admin.reseller.console;
 
-import java.net.MalformedURLException;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 import com.openexchange.admin.console.AdminParser;
-import com.openexchange.admin.console.CmdLineParser.IllegalOptionValueException;
-import com.openexchange.admin.console.CmdLineParser.UnknownOptionException;
 import com.openexchange.admin.reseller.rmi.OXResellerInterface;
 import com.openexchange.admin.reseller.rmi.dataobjects.ResellerAdmin;
-import com.openexchange.admin.reseller.rmi.exceptions.OXResellerException;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
-import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
-import com.openexchange.admin.rmi.exceptions.InvalidDataException;
-import com.openexchange.admin.rmi.exceptions.MissingOptionException;
-import com.openexchange.admin.rmi.exceptions.StorageException;
 
 
 /**
@@ -106,40 +96,11 @@ public class Create extends ResellerAbstraction {
             // method lists available Restrictions and calls System.exit()
             listRestrictionsIfSpecified(parser, rsi, auth);
         
-            rsi.create(adm, auth);
-            
-        } catch (IllegalOptionValueException e) {
-            printError("Illegal option value : " + e.getMessage(), parser);
-            parser.printUsage();
-            sysexit(SYSEXIT_ILLEGAL_OPTION_VALUE);
-        } catch (UnknownOptionException e) {
-            printError("Unrecognized options on the command line: " + e.getMessage(), parser);
-            parser.printUsage();
-            sysexit(SYSEXIT_UNKNOWN_OPTION);
-        } catch (MissingOptionException e) {
-            printError(e.getMessage(), parser);
-            parser.printUsage();
-            sysexit(SYSEXIT_MISSING_OPTION);
-        } catch (MalformedURLException e) {
-            printServerException(e, parser);
-            sysexit(1);
-        } catch (RemoteException e) {
-            printServerException(e, parser);
-            sysexit(SYSEXIT_REMOTE_ERROR);
-        } catch (NotBoundException e) {
-            printServerException(e, parser);
-            sysexit(1);
-        } catch (InvalidDataException e) {
-            printServerException(e, parser);
-            sysexit(SYSEXIT_INVALID_DATA);
-        } catch (StorageException e) {
-            printServerException(e, parser);
-            sysexit(SYSEXIT_SERVERSTORAGE_ERROR);
-        } catch (InvalidCredentialsException e) {
-            printServerException(e, parser);
-            sysexit(SYSEXIT_INVALID_CREDENTIALS);
-        } catch (OXResellerException e) {
-            printServerException(e, parser);
+            final ResellerAdmin create = rsi.create(adm, auth);
+            createMessageForStdout(String.valueOf(create.getId()), null, "created", parser);
+            sysexit(0);
+        } catch (final Exception e) {
+            printErrors(null, null, e, parser);
             sysexit(1);
         }
     }
