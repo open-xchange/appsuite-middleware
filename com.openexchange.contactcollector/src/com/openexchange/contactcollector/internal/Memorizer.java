bc8e9305d90c0ffad7c75236dc3e6cacfd1f92f7
marcus <marcus>
2009-02-02 14:04:32 +0000

    Fixed bug 13115: Adding check if a found contact can be updated.

--- com.openexchange.contactcollector/src/com/openexchange/contactcollector/internal/Memorizer.java
+++ com.openexchange.contactcollector/src/com/openexchange/contactcollector/internal/Memorizer.java
@@ -53,9 +53,8 @@ import java.util.List;
 import javax.mail.internet.InternetAddress;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import com.openexchange.api2.OXException;
 import com.openexchange.api2.RdbContactSQLInterface;
-import com.openexchange.groupware.AbstractOXException.Category;
+import com.openexchange.groupware.AbstractOXException;
 import com.openexchange.groupware.contact.ContactException;
 import com.openexchange.groupware.contact.ContactInterface;
 import com.openexchange.groupware.contact.ContactServices;
@@ -67,10 +66,13 @@ import com.openexchange.groupware.contexts.impl.ContextException;
 import com.openexchange.groupware.contexts.impl.ContextStorage;
 import com.openexchange.groupware.search.ContactSearchObject;
 import com.openexchange.groupware.settings.SettingException;
+import com.openexchange.groupware.userconfiguration.UserConfiguration;
+import com.openexchange.groupware.userconfiguration.UserConfigurationStorage;
 import com.openexchange.preferences.ServerUserSetting;
+import com.openexchange.server.impl.OCLPermission;
 import com.openexchange.session.Session;
 import com.openexchange.tools.iterator.SearchIterator;
-import com.openexchange.tools.iterator.SearchIteratorException;
+import com.openexchange.tools.oxfolder.OXFolderAccess;
 
 /**
  * {@link Memorizer}
@@ -104,68 +106,65 @@ public class Memorizer implements Runnable {
         for (final InternetAddress address : addresses) {
             try {
                 memorizeContact(address, session);
-            } catch (final OXException e) {
-                LOG.info("Error during Contact Collection", e);
+            } catch (final AbstractOXException e) {
+                LOG.error(e.getMessage(), e);
             }
         }
     }
 
-    private int memorizeContact(final InternetAddress address, final Session session) throws OXException {
+    private int memorizeContact(final InternetAddress address, final Session session) throws AbstractOXException {
         final ContactObject contact = transformInternetAddress(address);
-        Context ctx = null;
+        final Context ctx;
         try {
             ctx = ContextStorage.getStorageContext(session.getContextId());
         } catch (final ContextException ct) {
             throw new ContactException(ct);
         }
-
+        final UserConfiguration userConfig = UserConfigurationStorage.getInstance().getUserConfiguration(session.getUserId(), ctx);
         ContactInterface contactInterface = ContactServices.getInstance().getService(contact.getParentFolderID(), ctx.getContextId());
         if (contactInterface == null) {
             contactInterface = new RdbContactSQLInterface(session, ctx);
         }
-
-        final ContactSearchObject searchObject = new ContactSearchObject();
-        /*
-        searchObject.setEmail1(contact.getEmail1());
-        searchObject.setEmail2(contact.getEmail1());
-        searchObject.setEmail3(contact.getEmail1());
-        searchObject.setOrSearch(true);
-        searchObject.setFolder(contact.getParentFolderID());
-        */
-        searchObject.setEmailAutoComplete(true);
-        searchObject.setDynamicSearchField(new int[] { ContactObject.EMAIL1, ContactObject.EMAIL2, ContactObject.EMAIL3, });
-        searchObject.setDynamicSearchFieldValue(new String[] { contact.getEmail1(), contact.getEmail1(), contact.getEmail1() });
         contactInterface.setSession(session);
-        final int[] columns = new int[] {
-            DataObject.OBJECT_ID, FolderChildObject.FOLDER_ID, DataObject.LAST_MODIFIED, ContactObject.USERFIELD20 };
-        final SearchIterator<ContactObject> iterator = contactInterface.getContactsByExtendedSearch(searchObject, 0, null, columns);
 
-        if (iterator.hasNext()) {
-            ContactObject foundContact;
+        ContactObject foundContact = null;
+        {
+            final ContactSearchObject searchObject = new ContactSearchObject();
+            searchObject.setEmailAutoComplete(true);
+            searchObject.setDynamicSearchField(new int[] { ContactObject.EMAIL1, ContactObject.EMAIL2, ContactObject.EMAIL3, });
+            searchObject.setDynamicSearchFieldValue(new String[] { contact.getEmail1(), contact.getEmail1(), contact.getEmail1() });
+            final int[] columns = new int[] { 
+                DataObject.OBJECT_ID, FolderChildObject.FOLDER_ID, DataObject.LAST_MODIFIED, ContactObject.USERFIELD20 };
+            final SearchIterator<ContactObject> iterator = contactInterface.getContactsByExtendedSearch(searchObject, 0, null, columns);
             try {
-                foundContact = iterator.next();
-            } catch (final SearchIteratorException e) {
-                throw new ContactException(e);
-            }
-
-            int count = 0;
-            if (foundContact.getUserField20() != null && !foundContact.getUserField20().trim().equals("")) {
-                try {
-                    count = Integer.parseInt(foundContact.getUserField20());
-                } catch (final NumberFormatException nfe) {
-                    throw new ContactException(Category.INTERNAL_ERROR, -1, "Error during parsing of a value", nfe);
+                if (iterator.hasNext()) {
+                    foundContact = iterator.next();
                 }
+            } finally {
+                iterator.close();
             }
-
-            foundContact.setUserField20(Integer.toString(count + 1));
-            contactInterface.updateContactObject(foundContact, foundContact.getParentFolderID(), foundContact.getLastModified());
-            return 0;
         }
 
-        contact.setUserField20(String.valueOf(1));
-        contactInterface.insertContactObject(contact);
-
-        return contact.getObjectID();
+        final int retval;
+        if (null == foundContact) {
+            contactInterface.insertContactObject(contact);
+            retval = contact.getObjectID();
+        } else {
+            try {
+                foundContact.setUserField20(String.valueOf(Integer.parseInt(foundContact.getUserField20() + 1)));
+            } catch (final NumberFormatException nfe) {
+                foundContact.setUserField20(String.valueOf(1));
+            }
+            final OCLPermission perm = new OXFolderAccess(ctx).getFolderPermission(
+                foundContact.getParentFolderID(),
+                session.getUserId(),
+                userConfig);
+            if (perm.canWriteAllObjects()) {
+                contactInterface.updateContactObject(foundContact, foundContact.getParentFolderID(), foundContact.getLastModified());
+            }
+            retval = foundContact.getObjectID();
+        }
+        return retval;
     }
 
     private int getFolderId() {
@@ -193,14 +192,12 @@ public class Memorizer implements Runnable {
 
     private ContactObject transformInternetAddress(final InternetAddress address) {
         final ContactObject retval = new ContactObject();
-
         retval.setEmail1(address.getAddress());
-
-        if (address.getPersonal() != null && !address.getPersonal().trim().equals("")) {
+        if (address.getPersonal() != null && !"".equals(address.getPersonal().trim())) {
             retval.setDisplayName(address.getPersonal());
         }
-
         retval.setParentFolderID(getFolderId());
+        retval.setUserField20(String.valueOf(1));
         return retval;
     }
 }
