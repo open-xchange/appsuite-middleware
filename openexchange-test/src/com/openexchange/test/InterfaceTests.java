ee6ae963195b29de0ff8c1fefffffbe58333a2dc
Marcus Klein <marcus.klein@open-xchange.com>
2009-02-03 15:12:02 +0000

    using interface test suite.

--- openexchange-test/src/com/openexchange/test/InterfaceTests.java
+++ openexchange-test/src/com/openexchange/test/InterfaceTests.java
@@ -59,35 +59,32 @@ public class InterfaceTests {
         // First of all the smoke tests.
         tests.addTest(com.openexchange.SmokeTestSuite.suite());
 
-        tests.addTest(com.openexchange.ajax.appointment.AppointmentAJAXSuite.suite());
-        tests.addTest(com.openexchange.ajax.config.ConfigTestSuite.suite());
-        tests.addTest(com.openexchange.ajax.contact.ContactAJAXSuite.suite());
-        tests.addTestSuite(com.openexchange.ajax.group.FunctionTest.class);
-        tests.addTest(com.openexchange.ajax.infostore.InfostoreAJAXSuite.suite());
-        tests.addTest(com.openexchange.ajax.reminder.ReminderAJAXSuite.suite());
-		tests.addTest(com.openexchange.ajax.reminder.ReminderBugTestSuite.suite());
-        tests.addTestSuite(com.openexchange.ajax.session.LoginTest.class);
-        tests.addTestSuite(com.openexchange.ajax.session.RedirectTest.class);
         tests.addTestSuite(com.openexchange.ajax.FolderTest.class);
         tests.addTestSuite(com.openexchange.ajax.LinkTest.class);
         tests.addTestSuite(com.openexchange.ajax.MultipleTest.class);
         tests.addTestSuite(com.openexchange.ajax.ResourceTest.class);
-		tests.addTestSuite(com.openexchange.ajax.UserTest.class);
+        tests.addTestSuite(com.openexchange.ajax.UserTest.class);
 
-		tests.addTest(com.openexchange.ajax.importexport.ImportExportServerSuite.suite());
-
-		tests.addTestSuite(com.openexchange.ajax.attach.SimpleAttachmentTest.class);
-		tests.addTestSuite(com.openexchange.ajax.attach.TaskAttachmentTest.class);
+        tests.addTest(com.openexchange.ajax.appointment.AppointmentAJAXSuite.suite());
+        tests.addTestSuite(com.openexchange.ajax.attach.SimpleAttachmentTest.class);
+        tests.addTestSuite(com.openexchange.ajax.attach.TaskAttachmentTest.class);
+        tests.addTest(com.openexchange.ajax.config.ConfigTestSuite.suite());
+        tests.addTest(com.openexchange.ajax.contact.ContactAJAXSuite.suite());
+        tests.addTest(com.openexchange.ajax.group.GroupTestSuite.suite());
+        tests.addTest(com.openexchange.ajax.importexport.ImportExportServerSuite.suite());
+        tests.addTest(com.openexchange.ajax.infostore.InfostoreAJAXSuite.suite());
+        tests.addTest(com.openexchange.ajax.links.LinksTestSuite.suite());
+        tests.addTest(com.openexchange.ajax.mail.filter.MailFilterTestSuite.suite());
+        tests.addTest(com.openexchange.ajax.reminder.ReminderAJAXSuite.suite());
+        tests.addTest(com.openexchange.ajax.session.SessionTestSuite.suite());
         tests.addTest(com.openexchange.ajax.task.TaskTestSuite.suite());
 
 		tests.addTest(com.openexchange.webdav.xml.appointment.AppointmentWebdavSuite.suite());
-		tests.addTest(com.openexchange.webdav.xml.appointment.AppointmentBugTestSuite.suite());
 		tests.addTest(com.openexchange.webdav.xml.contact.ContactWebdavSuite.suite());
 		tests.addTest(com.openexchange.webdav.xml.folder.FolderWebdavSuite.suite());
 		tests.addTest(com.openexchange.webdav.xml.task.TaskWebdavSuite.suite());
 		tests.addTest(com.openexchange.webdav.xml.attachment.AttachmentWebdavSuite.suite());
 		tests.addTestSuite(com.openexchange.webdav.xml.GroupUserTest.class);
-//		tests.addTestSuite(com.openexchange.webdav.client.SmokeTest.class);
 		tests.addTestSuite(com.openexchange.webdav.client.NaughtyClientTest.class);
         
 		return tests;
