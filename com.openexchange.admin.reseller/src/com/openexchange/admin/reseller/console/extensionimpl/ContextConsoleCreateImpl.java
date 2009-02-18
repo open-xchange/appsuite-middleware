10d1b0b8d2e2124f5a1f82ecc5677d25b358b349
dennis <dennis>
2009-02-18 11:18:05 +0000

    - removed getData call because getData on a non-existing context makes no sensee

--- com.openexchange.admin.reseller/src/com/openexchange/admin/reseller/console/extensionimpl/ContextConsoleCreateImpl.java
+++ com.openexchange.admin.reseller/src/com/openexchange/admin/reseller/console/extensionimpl/ContextConsoleCreateImpl.java
@@ -1,9 +1,5 @@
 package com.openexchange.admin.reseller.console.extensionimpl;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 import java.util.HashSet;
 import com.openexchange.admin.console.AdminParser;
 import com.openexchange.admin.console.AdminParser.NeededQuadState;
@@ -14,14 +10,10 @@ import com.openexchange.admin.reseller.console.ResellerAbstraction;
 import com.openexchange.admin.reseller.rmi.dataobjects.Restriction;
 import com.openexchange.admin.reseller.rmi.exceptions.OXResellerException;
 import com.openexchange.admin.reseller.rmi.extensions.OXContextExtension;
-import com.openexchange.admin.rmi.OXContextInterface;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
 import com.openexchange.admin.rmi.exceptions.DuplicateExtensionException;
-import com.openexchange.admin.rmi.exceptions.InvalidCredentialsException;
 import com.openexchange.admin.rmi.exceptions.InvalidDataException;
-import com.openexchange.admin.rmi.exceptions.NoSuchContextException;
-import com.openexchange.admin.rmi.exceptions.StorageException;
 
 
 public class ContextConsoleCreateImpl implements ContextConsoleCreateInterface {
@@ -38,15 +30,7 @@ public class ContextConsoleCreateImpl implements ContextConsoleCreateInterface {
         try {
             final HashSet<Restriction> addres = ResellerAbstraction.parseRestrictions(parser, this.addRestrictionsOption);
             
-            final OXContextInterface oxctx = (OXContextInterface) Naming.lookup("rmi://localhost:1099/" + OXContextInterface.RMI_NAME);
-            final Context data = oxctx.getData(ctx, auth);
-            final HashSet<Restriction> dbres;
-            final OXContextExtension dbctxext = (OXContextExtension) data.getFirstExtensionByName(OXContextExtension.class.getName());
-            if (null == dbctxext) {
-                dbres = new HashSet<Restriction>();
-            } else {
-                dbres = dbctxext.getRestriction();
-            }
+            final HashSet<Restriction> dbres = new HashSet<Restriction>();
             final HashSet<Restriction> restrictions = ResellerAbstraction.handleAddEditRemoveRestrictions(dbres, addres, null, null);
             if (null == firstExtensionByName) {
                 ctx.addExtension(new OXContextExtension(restrictions));
@@ -55,22 +39,10 @@ public class ContextConsoleCreateImpl implements ContextConsoleCreateInterface {
             }
         } catch (final InvalidDataException e) {
             throw new OXConsolePluginException(e);
-        } catch (final RemoteException e) {
-            throw new OXConsolePluginException(e);
-        } catch (final MalformedURLException e) {
-            throw new OXConsolePluginException(e);
-        } catch (final InvalidCredentialsException e) {
-            throw new OXConsolePluginException(e);
-        } catch (final StorageException e) {
-            throw new OXConsolePluginException(e);
         } catch (final OXResellerException e) {
             throw new OXConsolePluginException("A reseller exception occured: " + e.getMessage());
-        } catch (final NotBoundException e) {
-            throw new OXConsolePluginException(e);
         } catch (final DuplicateExtensionException e) {
             // Throw this one, but this should never occur as we check beforehand
-            throw new OXConsolePluginException(e);
-        } catch (final NoSuchContextException e) {
             throw new OXConsolePluginException(e);
         }
 
