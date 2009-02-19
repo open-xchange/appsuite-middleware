3bf7596542bebebcbf2d1aa1de9a567eecc66cc7
Dennis Sieben <dennis.sieben@open-xchange.com>
2009-02-19 20:37:48 +0000

    - removed no more needed check

--- com.openexchange.admin/src/com/openexchange/admin/console/context/ContextAbstraction.java
+++ com.openexchange.admin/src/com/openexchange/admin/console/context/ContextAbstraction.java
@@ -203,9 +203,7 @@ public abstract class ContextAbstraction extends UserAbstraction {
         columns.add("used_quota");
         columns.add("name");
         columns.add("lmappings");
-        if (ctxs.length > 0) {
-            columns.addAll(getCSVColumnsOfAllExtensions(parser));
-        }
+        columns.addAll(getCSVColumnsOfAllExtensions(parser));
         final ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
     
         for (final Context ctx_tmp : ctxs) {
