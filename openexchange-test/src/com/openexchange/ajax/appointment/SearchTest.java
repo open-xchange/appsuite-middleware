64d9264d780a2577b404cf4a830ff1ccde4c4eae
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-02-23 10:47:47 +0000

    added legal header, ran formatter

--- openexchange-test/src/com/openexchange/ajax/appointment/SearchTest.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/SearchTest.java
@@ -1,11 +1,58 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
 package com.openexchange.ajax.appointment;
 
 import java.util.Date;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
-
 import com.openexchange.ajax.AppointmentTest;
 import com.openexchange.ajax.appointment.action.SearchRequest;
 import com.openexchange.ajax.appointment.action.SearchResponse;
@@ -16,42 +63,51 @@ import com.openexchange.groupware.container.AppointmentObject;
 
 public class SearchTest extends AppointmentTest {
 
-	private static final Log LOG = LogFactory.getLog(SearchTest.class);
-	
-	public SearchTest(final String name) {
-		super(name);
-	}
-	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-	
-	public void testSimpleSearch() throws Exception {
-		final AppointmentObject appointmentObj = new AppointmentObject();
-		final String date = String.valueOf(System.currentTimeMillis());
-		appointmentObj.setTitle("testSimpleSearch" + date);
-		appointmentObj.setStartDate(new Date(startTime));
-		appointmentObj.setEndDate(new Date(endTime));
-		appointmentObj.setParentFolderID(appointmentFolderId);
-		appointmentObj.setIgnoreConflicts(true);
-		
-		final int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
-		
-		final AppointmentObject[] appointmentArray = searchAppointment(getWebConversation(), "testSimpleSearch" + date, appointmentFolderId, new Date(), new Date(), APPOINTMENT_FIELDS, timeZone, PROTOCOL + getHostName(), getSessionId());
-		assertTrue("appointment array size is 0", appointmentArray.length > 0);
-		
-		deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
-	}
+    private static final Log LOG = LogFactory.getLog(SearchTest.class);
+
+    public SearchTest(final String name) {
+        super(name);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testSimpleSearch() throws Exception {
+        final AppointmentObject appointmentObj = new AppointmentObject();
+        final String date = String.valueOf(System.currentTimeMillis());
+        appointmentObj.setTitle("testSimpleSearch" + date);
+        appointmentObj.setStartDate(new Date(startTime));
+        appointmentObj.setEndDate(new Date(endTime));
+        appointmentObj.setParentFolderID(appointmentFolderId);
+        appointmentObj.setIgnoreConflicts(true);
+
+        final int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, PROTOCOL + getHostName(), getSessionId());
+
+        final AppointmentObject[] appointmentArray = searchAppointment(
+            getWebConversation(),
+            "testSimpleSearch" + date,
+            appointmentFolderId,
+            new Date(),
+            new Date(),
+            APPOINTMENT_FIELDS,
+            timeZone,
+            PROTOCOL + getHostName(),
+            getSessionId());
+        assertTrue("appointment array size is 0", appointmentArray.length > 0);
+
+        deleteAppointment(getWebConversation(), objectId, appointmentFolderId, PROTOCOL + getHostName(), getSessionId());
+    }
 
     // Node 2652
     public void testLastModifiedUTC() throws Exception {
         final AJAXClient client = new AJAXClient(new AJAXSession(getWebConversation(), getSessionId()));
-        final int cols[] = new int[]{ AppointmentObject.OBJECT_ID, AppointmentObject.FOLDER_ID, AppointmentObject.LAST_MODIFIED_UTC};
+        final int cols[] = new int[] { AppointmentObject.OBJECT_ID, AppointmentObject.FOLDER_ID, AppointmentObject.LAST_MODIFIED_UTC };
 
         final AppointmentObject appointmentObj = createAppointmentObject("testShowLastModifiedUTC");
         appointmentObj.setStartDate(new Date());
-        appointmentObj.setEndDate(new Date(System.currentTimeMillis() + 60*60*1000));
+        appointmentObj.setEndDate(new Date(System.currentTimeMillis() + 60 * 60 * 1000));
         appointmentObj.setIgnoreConflicts(true);
         final int objectId = insertAppointment(getWebConversation(), appointmentObj, timeZone, getHostName(), getSessionId());
         try {
@@ -63,7 +119,7 @@ public class SearchTest extends AppointmentTest {
             final int size = arr.length();
             assertTrue(size > 0);
 
-            for(int i = 0; i < size; i++ ){
+            for (int i = 0; i < size; i++) {
                 final JSONArray objectData = arr.optJSONArray(i);
                 assertNotNull(objectData);
                 assertNotNull(objectData.opt(2));
