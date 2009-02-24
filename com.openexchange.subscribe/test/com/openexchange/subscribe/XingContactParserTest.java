8ded9e754fb134bfd4b11a9965e305c2c36a370a
karsten <karsten>
2009-02-24 16:32:20 +0000

    removed credentials

--- com.openexchange.subscribe/test/com/openexchange/subscribe/XingContactParserTest.java
+++ com.openexchange.subscribe/test/com/openexchange/subscribe/XingContactParserTest.java
@@ -14,8 +14,8 @@ public class XingContactParserTest extends TestCase{
 	public void testGetXingContacts(){
 		XingContactParser parser = new XingContactParser();
 		//TODO: Insert valid credentials for www.xing.com here
-		String xingUser="karsten.will@gmx.de";
-		String xingPassword ="P1lotXIN";
+		String xingUser="";
+		String xingPassword ="";
 		try {
 			ContactObject[] contacts = parser.getXingContactsForUser(xingUser, xingPassword);
 			assertTrue("There should be at least one contact.", contacts.length >= 1);
