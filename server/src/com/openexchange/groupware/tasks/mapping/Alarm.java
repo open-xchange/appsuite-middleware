0eba7feaef78c4d38567bf70e7d1932a8a32cc0a
marcus <marcus>
2009-02-26 20:07:40 +0000

    some optimizations.

--- server/src/com/openexchange/groupware/tasks/mapping/Alarm.java
+++ server/src/com/openexchange/groupware/tasks/mapping/Alarm.java
@@ -1,3 +1,52 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
 package com.openexchange.groupware.tasks.mapping;
 
 import java.sql.PreparedStatement;
@@ -7,26 +56,30 @@ import java.util.Date;
 import com.openexchange.groupware.tasks.Mapper;
 import com.openexchange.groupware.tasks.Task;
 
-
+/**
+ * Mapper implementation for the alarm.
+ * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
+ */
 public class Alarm implements Mapper<Date>{
 
-    public boolean equals(Task task1, Task task2) {
-        Date d1 = task1.getAlarm();
-        Date d2 = task2.getAlarm();
-        if( d1 == d2) {
-            return true;
-        }
-        if(d1 == null && d2 != null) {
-            return false;
+    public Alarm() {
+        super();
+    }
+
+    public boolean equals(final Task task1, final Task task2) {
+        final Date d1 = task1.getAlarm();
+        final Date d2 = task2.getAlarm();
+        if (d1 == null) {
+            return null == d2;
         }
         return d1.equals(d2);
     }
 
-    public void fromDB(ResultSet result, int pos, Task task) throws SQLException {
+    public void fromDB(final ResultSet result, final int pos, final Task task) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
-    public Date get(Task task) {
+    public Date get(final Task task) {
         return task.getAlarm();
     }
 
@@ -38,16 +91,15 @@ public class Alarm implements Mapper<Date>{
         return Task.ALARM;
     }
 
-    public boolean isSet(Task task) {
+    public boolean isSet(final Task task) {
         return task.containsAlarm();
     }
 
-    public void set(Task task, Date value) {
+    public void set(final Task task, final Date value) {
         task.setAlarm(value);
     }
 
-    public void toDB(PreparedStatement stmt, int pos, Task task) throws SQLException {
+    public void toDB(final PreparedStatement stmt, final int pos, final Task task) throws SQLException {
         throw new UnsupportedOperationException();
     }
-
 }
