d7c9817fff3ffc9f538d35e04a2098ee4cdfb09d
Dennis Sieben <dennis.sieben@open-xchange.com>
2009-03-02 10:07:51 +0000

    - improved version

--- openexchange-test/unittests/com/openexchange/mail/messagestorage/MailAppendTest.java
+++ openexchange-test/unittests/com/openexchange/mail/messagestorage/MailAppendTest.java
@@ -49,8 +49,6 @@
 
 package com.openexchange.mail.messagestorage;
 
-import java.util.Arrays;
-
 import com.openexchange.mail.MailField;
 import com.openexchange.mail.api.MailAccess;
 import com.openexchange.mail.dataobjects.MailMessage;
@@ -60,74 +58,74 @@ import com.openexchange.mail.mime.MessageHeaders;
  * {@link MailAppendTest}
  * 
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
- * 
  */
 public final class MailAppendTest extends MessageStorageTest {
 
-	private static final String INBOX = "INBOX";
+    private static final String INBOX = "INBOX";
 
-	private static final MailField[] FIELDS_ID = { MailField.ID };
-
-	/**
+    /**
 	 * 
 	 */
-	public MailAppendTest() {
-		super();
-	}
+    public MailAppendTest() {
+        super();
+    }
 
-	/**
-	 * @param name
-	 */
-	public MailAppendTest(final String name) {
-		super(name);
-	}
+    /**
+     * @param name
+     */
+    public MailAppendTest(final String name) {
+        super(name);
+    }
 
-	public void testMailAppend() {
-	    
-	    // At first we should test the append - get - delete operation with one mail only so that we see, that the basic functions
-	    // are working
-		try {
-		    final MailField[][] test = generateVariations();
-			final MailAccess<?, ?> mailAccess = getMailAccess();
-			try {
-				final MailMessage[] testmessages = getMessages(getTestMailDir(), -1);
-				final long[] uids = mailAccess.getMessageStorage().appendMessages(INBOX, testmessages);
-				try {
-				    final MailField[] fieldsfull = {MailField.FULL};
-				    final MailField[] fieldWithoutUidFolderAndFlags = { MailField.CONTENT_TYPE, MailField.FROM,
-				            MailField.TO, MailField.CC, MailField.BCC, MailField.SUBJECT, MailField.SIZE, MailField.SENT_DATE,
-				            MailField.THREAD_LEVEL, MailField.DISPOSITION_NOTIFICATION_TO, MailField.PRIORITY, MailField.COLOR_LABEL,
-				            MailField.HEADERS, MailField.BODY };
+    public void testMailAppend() {
 
-					for (int i = 0; i < uids.length; i++) {
-						final MailMessage m = mailAccess.getMessageStorage().getMessage(INBOX, uids[i], true);
-						m.removeHeader(MessageHeaders.HDR_X_OX_MARKER);
-						System.out.println("Mail #" + m.getMailId() + ": " + m.getSubject());
-						compareMailMessages(testmessages[i], m, fieldWithoutUidFolderAndFlags, fieldsfull, "test message", "fetched message", true);
-					}
+        // At first we should test the append - get - delete operation with one mail only so that we see, that the basic functions
+        // are working
+        try {
+            final MailField[][] test = generateVariations();
+            final MailAccess<?, ?> mailAccess = getMailAccess();
+            try {
+                final MailMessage[] testmessages = getMessages(getTestMailDir(), -1);
+                final long[] uids = mailAccess.getMessageStorage().appendMessages(INBOX, testmessages);
+                try {
+                    final MailField[] fieldsfull = { MailField.FULL };
+                    final MailField[] fieldWithoutUidFolderAndFlags = {
+                        MailField.CONTENT_TYPE, MailField.FROM, MailField.TO, MailField.CC, MailField.BCC, MailField.SUBJECT,
+                        MailField.SIZE, MailField.SENT_DATE, MailField.THREAD_LEVEL, MailField.DISPOSITION_NOTIFICATION_TO,
+                        MailField.PRIORITY, MailField.COLOR_LABEL, MailField.HEADERS, MailField.BODY };
 
-					final MailMessage[] fetchedMails = mailAccess.getMessageStorage().getMessages(INBOX, uids,
-							FIELDS_ID);
-					for (int i = 0; i < fetchedMails.length; i++) {
-						System.out.println("Fetched: " + fetchedMails[i].getMailId());
-						fetchedMails[i].removeHeader(MessageHeaders.HDR_X_OX_MARKER);
-						compareMailMessages(testmessages[i], fetchedMails[i], fieldWithoutUidFolderAndFlags, FIELDS_ID, "test messages", "fetched messages" + i, true);
-					}
-				} finally {
-					mailAccess.getMessageStorage().deleteMessages(INBOX, uids, true);
-				}
-			} finally {
-				/*
-				 * close
-				 */
-				mailAccess.close(false);
-			}
+                    for (int i = 0; i < uids.length; i++) {
+                        final MailMessage m = mailAccess.getMessageStorage().getMessage(INBOX, uids[i], true);
+                        m.removeHeader(MessageHeaders.HDR_X_OX_MARKER);
+                        System.out.println("Mail #" + m.getMailId() + ": " + m.getSubject());
+                        compareMailMessages(testmessages[i], m, fieldWithoutUidFolderAndFlags, fieldsfull, "test message", "fetched message", true);
+                    }
+                    // Check only for the one values here (that is the size of the enum out of all combinations):
+                    for (int o = 0; o < MailField.values().length; o++) {
+                        MailField[] fields = test[o];
+                        final MailMessage[] fetchedMails = mailAccess.getMessageStorage().getMessages(INBOX, uids, fields);
+                        assertTrue("The size of the uids is not equal with the size of the fetched mails which is returned from the getMessages method", fetchedMails.length == uids.length);
+                        for (int i = 0; i < fetchedMails.length; i++) {
+                            System.out.println("Fetched: " + fetchedMails[i].getMailId());
+                            fetchedMails[i].removeHeader(MessageHeaders.HDR_X_OX_MARKER);
+                            compareMailMessages(testmessages[i], fetchedMails[i], fieldWithoutUidFolderAndFlags, fields, "test messages", "fetched messages " + i, true);
+                        }
+                    }
+                    // TODO: Random test out of all combinations:
+                } finally {
+                    mailAccess.getMessageStorage().deleteMessages(INBOX, uids, true);
+                }
+            } finally {
+                /*
+                 * close
+                 */
+                mailAccess.close(false);
+            }
 
-		} catch (final Exception e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-	}
-
+        } catch (final Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
 
 }
