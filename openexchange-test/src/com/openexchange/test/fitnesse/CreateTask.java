5f8db9238107f5231f09b0e244e0dfc2fbcd60ab
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-03-04 10:54:32 +0000

    now able to really create a task. A lot of hard-coded stuff in there, though.

--- openexchange-test/src/com/openexchange/test/fitnesse/CreateTask.java
+++ openexchange-test/src/com/openexchange/test/fitnesse/CreateTask.java
@@ -5,8 +5,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import com.openexchange.ajax.framework.AJAXClient;
-import com.openexchange.ajax.framework.AJAXClient.User;
+import com.openexchange.ajax.framework.AJAXSession;
 import com.openexchange.ajax.kata.tasks.TaskCreateStep;
+import com.openexchange.ajax.session.LoginTools;
+import com.openexchange.ajax.session.actions.LoginRequest;
+import com.openexchange.configuration.AJAXConfig;
+import com.openexchange.configuration.ConfigurationException;
 import com.openexchange.groupware.tasks.Task;
 import com.openexchange.test.fixtures.Fixture;
 import com.openexchange.test.fixtures.Fixtures;
@@ -14,6 +18,18 @@ import com.openexchange.test.fixtures.TaskFixtureFactory;
 
 
 public class CreateTask extends AbstractTableTable {
+    
+    public CreateTask()  {
+        super();
+        try {
+            System.setProperty("test.propfile", "/Users/development/workspace/openexchange-test/conf/test.properties");
+            AJAXConfig.init();
+        } catch (ConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+ 
     @Override
     public List doTable(List<List<String>> table) throws Exception {
         Map<String, String> readAsMap = readAsMap(table);
@@ -24,7 +40,9 @@ public class CreateTask extends AbstractTableTable {
         Fixture<Task> entry = fixtures.getEntry("task");
         Task task = entry.getEntry();
         TaskCreateStep taskCreateStep = new TaskCreateStep(task, "create task step", null);
-        taskCreateStep.perform( new AJAXClient( User.User1 ) );
+        AJAXSession session = new AJAXSession();
+        session.setId(LoginTools.login(session, new LoginRequest("thorben","netline")).getSessionId());
+        taskCreateStep.perform( new AJAXClient( session ) );
         return new LinkedList();
     }
 
