24380731f3841a9213f1e3d926aedca60ca86d0d
thorben <thorben>
2009-03-04 10:04:29 +0000

    Added utility method to fold a string to 76 characters per line

--- server/src/com/openexchange/mail/mime/utils/MIMEMessageUtility.java
+++ server/src/com/openexchange/mail/mime/utils/MIMEMessageUtility.java
@@ -465,6 +465,79 @@ public final class MIMEMessageUtility {
     }
 
     /**
+     * Folds a string at linear whitespace so that each line is no longer than 76 characters, if possible. If there are more than 76
+     * non-whitespace characters consecutively, the string is folded at the first whitespace after that sequence. The parameter
+     * <tt>used</tt> indicates how many characters have been used in the current line; it is usually the length of the header name.
+     * <p>
+     * Note that line breaks in the string aren't escaped; they probably should be.
+     * 
+     * @param used The characters used in line so far
+     * @param foldMe The string to fold
+     * @return The folded string
+     */
+    public static String fold(final int used, final String foldMe) {
+        int end;
+        char c;
+        /*
+         * Strip trailing spaces and newlines
+         */
+        for (end = foldMe.length() - 1; end >= 0; end--) {
+            c = foldMe.charAt(end);
+            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
+                break;
+            }
+        }
+        String s;
+        if (end != foldMe.length() - 1) {
+            s = foldMe.substring(0, end + 1);
+        } else {
+            s = foldMe;
+        }
+        /*
+         * Check if the string fits now
+         */
+        if (used + s.length() <= 76) {
+            return s;
+        }
+        /*
+         * Fold the string
+         */
+        final StringBuilder sb = new StringBuilder(s.length() + 4);
+        char lastc = 0;
+        int usedChars = used;
+        while (usedChars + s.length() > 76) {
+            int lastspace = -1;
+            for (int i = 0; i < s.length(); i++) {
+                if (lastspace != -1 && usedChars + i > 76) {
+                    break;
+                }
+                c = s.charAt(i);
+                if ((c == ' ' || c == '\t') && !(lastc == ' ' || lastc == '\t')) {
+                    lastspace = i;
+                }
+                lastc = c;
+            }
+            if (lastspace == -1) {
+                /*
+                 * No space, use the whole thing
+                 */
+                sb.append(s);
+                s = "";
+                usedChars = 0;
+                break;
+            }
+            sb.append(s.substring(0, lastspace));
+            sb.append("\r\n");
+            lastc = s.charAt(lastspace);
+            sb.append(lastc);
+            s = s.substring(lastspace + 1);
+            usedChars = 1;
+        }
+        sb.append(s);
+        return sb.toString();
+    }
+
+    /**
      * Unfolds a folded header. Any line breaks that aren't escaped and are followed by whitespace are removed.
      * 
      * @param headerLine The header line to unfold
