24b59428247b33edeff9153abe5488c9ccd5bda0
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-03-09 15:57:33 +0000

    Changed FitNesse to use our json.jar Rewrote FitNesse tests to work as shining examples

--- openexchange-test/src/com/openexchange/fitnesse/wrappers/FitnesseResult.java
+++ openexchange-test/src/com/openexchange/fitnesse/wrappers/FitnesseResult.java
@@ -32,7 +32,7 @@ public class FitnesseResult {
         values = new LinkedList<String>();
         
         for (int i = 0; i < length; i++) {
-            header.add(PASS);
+            header.add(NEUTRAL);
             values.add(defaultValue);    
         }
     }
