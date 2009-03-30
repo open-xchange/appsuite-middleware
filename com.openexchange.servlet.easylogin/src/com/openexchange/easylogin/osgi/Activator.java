07220dce7640b351907ff11969757dc115632470
choeger <choeger>
2009-03-30 13:17:09 +0000

    moved from addons to here

---
+++ com.openexchange.servlet.easylogin/src/com/openexchange/easylogin/osgi/Activator.java
@@ -0,0 +1,159 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.easylogin.osgi;
+
+import org.osgi.service.http.HttpService;
+
+import com.openexchange.easylogin.EasyLogin;
+import com.openexchange.server.osgiservice.DeferredActivator;
+
+/**
+ * {@link Activator}
+ * 
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * 
+ */
+public class Activator extends DeferredActivator {
+
+	private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory
+			.getLog(Activator.class);
+
+	private static final String ALIAS = "servlet/easylogin";
+
+	/**
+	 * Initializes a new {@link Activator}
+	 */
+	public Activator() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	private static final Class<?>[] NEEDED_SERVICES = { HttpService.class };
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.openexchange.server.osgiservice.DeferredActivator#getNeededServices()
+	 */
+	@Override
+	protected Class<?>[] getNeededServices() {
+		return NEEDED_SERVICES;
+	}
+
+	@Override
+	protected void handleUnavailability(final Class<?> clazz) {
+		try {
+			/*
+			 * Unregister servlet
+			 */
+			final HttpService httpService = getService(HttpService.class);
+			httpService.unregister(ALIAS);
+			LOG.info(EasyLogin.class.getName() + " successfully unregistered");
+		} catch (final Exception e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	protected void handleAvailability(final Class<?> clazz) {
+		try {
+			/*
+			 * Register servlet
+			 */
+			LOG.info("going to register HolgiServlet.....");
+			final HttpService httpService = getService(HttpService.class);
+			httpService.registerServlet(ALIAS, new EasyLogin(), null, null);
+			LOG.info(EasyLogin.class.getName() + " successfully re-registered");
+		} catch (final Exception e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.openexchange.server.osgiservice.DeferredActivator#startBundle()
+	 */
+	@Override
+	protected void startBundle() throws Exception {
+		try {
+			/*
+			 * Http service is available: Register servlet
+			 */
+			final HttpService httpService = getService(HttpService.class);
+			httpService.registerServlet(ALIAS, new EasyLogin(), null, null);
+			LOG.info(EasyLogin.class.getName() + " successfully registered");
+		} catch (final Exception e) {
+			LOG.error(e.getMessage(), e);
+			throw e;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.openexchange.server.osgiservice.DeferredActivator#stopBundle()
+	 */
+	@Override
+	protected void stopBundle() throws Exception {
+		try {
+			/*
+			 * Unregister servlet
+			 */
+			final HttpService httpService = getService(HttpService.class);
+			httpService.unregister(ALIAS);
+			LOG.info(EasyLogin.class.getName() + " successfully unregistered");
+		} catch (final Exception e) {
+			LOG.error(e.getMessage(), e);
+			throw e;
+		}
+	}
+
+}
