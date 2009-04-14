19851c24523249a9f5764ccbd5e079dcd02e481f
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-04-14 12:57:13 +0000

    added clean-up so the remains of this test don't break other tests

--- openexchange-test/src/com/openexchange/ajax/contact/Bug4409Test.java
+++ openexchange-test/src/com/openexchange/ajax/contact/Bug4409Test.java
@@ -6,9 +6,18 @@ import org.apache.commons.logging.LogFactory;
 import com.openexchange.ajax.ContactTest;
 import com.openexchange.groupware.container.ContactObject;
 
+/**
+ * 
+ * {@link Bug4409Test}
+ *
+ * @author Offspring
+ * @author <a href="mailto:tobias.prinz@open-xchange.com">Tobias Prinz</a> - clean-up added
+ *
+ */
 public class Bug4409Test extends ContactTest {
 
 	private static final Log LOG = LogFactory.getLog(Bug4409Test.class);
+    private int objectId = -1;
 	
 	public Bug4409Test(final String name) {
 		super(name);
@@ -24,8 +33,17 @@ public class Bug4409Test extends ContactTest {
 		contactObj.setSurName("testBug4409");
 		contactObj.setParentFolderID(contactFolderId);
 		
-		final int objectId = insertContact(getWebConversation(), contactObj, getHostName(), getSessionId());
+		objectId  = insertContact(getWebConversation(), contactObj, getHostName(), getSessionId());
 		
 		loadImage(getWebConversation(),objectId, contactFolderId, getHostName(), getSessionId());
 	}
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        if(objectId != -1){
+            deleteContact(getWebConversation(), objectId, contactFolderId, getHostName(), getSessionId());
+        }
+    }
+	
 }
