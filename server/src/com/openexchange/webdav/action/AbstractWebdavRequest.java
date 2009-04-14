3a91304f6e9c7d13f4cb4a611ea22739b0a54895
marcus <marcus>
2009-04-14 07:25:09 +0000

    fixed wrong used method.

--- server/src/com/openexchange/webdav/action/AbstractWebdavRequest.java
+++ server/src/com/openexchange/webdav/action/AbstractWebdavRequest.java
@@ -65,72 +65,72 @@ import com.openexchange.webdav.protocol.WebdavResource;
 import com.openexchange.xml.jdom.JDOMParser;
 
 public abstract class AbstractWebdavRequest implements WebdavRequest {
-	private WebdavResource res;
-	private WebdavResource dest;
-	private final WebdavFactory factory;
-	
-	public AbstractWebdavRequest(final WebdavFactory factory) {
-		this.factory = factory;
-	}
+    private WebdavResource res;
+    private WebdavResource dest;
+    private final WebdavFactory factory;
+    
+    public AbstractWebdavRequest(final WebdavFactory factory) {
+        this.factory = factory;
+    }
 
-	public WebdavResource getResource() throws WebdavProtocolException {
-		if(res != null) {
-			return res;
-		}
-		return res = factory.resolveResource(getUrl());
-	}
-	
-	public WebdavResource getDestination() throws WebdavProtocolException {
-		if(null == getDestinationUrl()) {
-			return null;
-		}
-		if(dest != null) {
-			return dest;
-		}
-		return dest = factory.resolveResource(getDestinationUrl());
-	}
-	
-	public WebdavCollection getCollection() throws WebdavProtocolException {
-		if(res != null && res.isCollection()) {
-			return (WebdavCollection) res;
-		}
-		return (WebdavCollection) (res = factory.resolveCollection(getUrl()));
-	}
-	
-	public Document getBodyAsDocument() throws JDOMException, IOException {
-		return ServerServiceRegistry.getInstance().getService(JDOMParser.class).parse(getBody());
-	}
-	
-	public IfHeader getIfHeader() throws IfHeaderParseException {
-		final String ifHeader = getHeader("If");
-		if(ifHeader == null) {
-			return null;
-		}
-		return new IfHeaderParser().parse(getHeader("If"));
-	}
-	
-	public int getDepth(final int def){
-		final String depth = getHeader("depth");
-		if(null == depth) {
-			return def;
-		}
-		return "Infinity".equalsIgnoreCase(depth) ? WebdavCollection.INFINITY : Integer.parseInt(depth);
-	}
-	
-	public WebdavFactory getFactory(){
-		return factory;
-	}
-	
-	public boolean hasBody() {
-	    if(getHeader("Content-Length") == null) {
-	        return false;
-	    }
-	    int length;
-	    try {
-	        length = Integer.parseInt(getHeader("Content-Length"));
-	    } catch (NumberFormatException e) {
-	        length = -1;
-	    }
+    public WebdavResource getResource() throws WebdavProtocolException {
+        if(res != null) {
+            return res;
+        }
+        return res = factory.resolveResource(getUrl());
+    }
+    
+    public WebdavResource getDestination() throws WebdavProtocolException {
+        if(null == getDestinationUrl()) {
+            return null;
+        }
+        if(dest != null) {
+            return dest;
+        }
+        return dest = factory.resolveResource(getDestinationUrl());
+    }
+    
+    public WebdavCollection getCollection() throws WebdavProtocolException {
+        if(res != null && res.isCollection()) {
+            return (WebdavCollection) res;
+        }
+        return (WebdavCollection) (res = factory.resolveCollection(getUrl()));
+    }
+    
+    public Document getBodyAsDocument() throws JDOMException, IOException {
+        return ServerServiceRegistry.getInstance().getService(JDOMParser.class).parse(getBody());
+    }
+    
+    public IfHeader getIfHeader() throws IfHeaderParseException {
+        final String ifHeader = getHeader("If");
+        if(ifHeader == null) {
+            return null;
+        }
+        return new IfHeaderParser().parse(getHeader("If"));
+    }
+    
+    public int getDepth(final int def){
+        final String depth = getHeader("depth");
+        if(null == depth) {
+            return def;
+        }
+        return "Infinity".equalsIgnoreCase(depth) ? WebdavCollection.INFINITY : Integer.parseInt(depth);
+    }
+    
+    public WebdavFactory getFactory(){
+        return factory;
+    }
+    
+    public boolean hasBody() {
+        if(getHeader("Content-Length") == null) {
+            return false;
+        }
+        int length;
+        try {
+            length = Integer.parseInt(getHeader("Content-Length"));
+        } catch (NumberFormatException e) {
+            length = -1;
+        }
         return length != -1;
-	}
+    }
 }
