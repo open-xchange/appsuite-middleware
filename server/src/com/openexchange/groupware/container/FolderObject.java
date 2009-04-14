7b266038f94c29dc98bfc6661835e656ee282e4f
tierlieb <tierlieb>
2009-04-14 14:10:26 +0000

    cleaned-up imports

--- server/src/com/openexchange/groupware/container/FolderObject.java
+++ server/src/com/openexchange/groupware/container/FolderObject.java
@@ -59,7 +59,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
