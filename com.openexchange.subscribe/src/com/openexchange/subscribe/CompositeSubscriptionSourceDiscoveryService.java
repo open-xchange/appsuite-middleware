0aa525db0d2e6a504f31dd193c6e629ca241f2e6
francisco <francisco>
2009-04-28 15:05:09 +0000

    Added ability to compound subscription source discoverers

---
+++ com.openexchange.subscribe/src/com/openexchange/subscribe/CompositeSubscriptionSourceDiscoveryService.java
@@ -0,0 +1,105 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.subscribe;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import com.openexchange.groupware.container.FolderObject;
+
+
+/**
+ * {@link CompositeSubscriptionSourceDiscoveryService}
+ *
+ * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
+ *
+ */
+public class CompositeSubscriptionSourceDiscoveryService implements SubscriptionSourceDiscoveryService {
+
+    private List<SubscriptionSourceDiscoveryService> services = new ArrayList<SubscriptionSourceDiscoveryService>();
+    
+    /* (non-Javadoc)
+     * @see com.openexchange.subscribe.SubscriptionSourceDiscoveryService#getSource(java.lang.String)
+     */
+    public SubscriptionSource getSource(String identifier) {
+        for(SubscriptionSourceDiscoveryService subDiscoverer : services) {
+            if(subDiscoverer.knowsSource(identifier)) {
+                return subDiscoverer.getSource(identifier);
+            }
+        }
+        return null;
+    }
+
+    public List<SubscriptionSource> getSources(FolderObject folder) {
+        List<SubscriptionSource> allSources = new LinkedList<SubscriptionSource>();
+        for(SubscriptionSourceDiscoveryService subDiscoverer : services) {
+            allSources.addAll(subDiscoverer.getSources(folder));
+        }
+        return allSources;
+    }
+
+    public boolean knowsSource(String identifier) {
+        for(SubscriptionSourceDiscoveryService subDiscoverer : services) {
+            if(subDiscoverer.knowsSource(identifier)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void addSubscriptionSourceDiscoveryService(SubscriptionSourceDiscoveryService service) {
+        services.add(service);
+    }
+    
+    public void removeSubscriptionSourceDiscoveryService(SubscriptionSourceDiscoveryService service) {
+        services.remove(service);
+    }
+
+}
---
+++ com.openexchange.subscribe/test/com/openexchange/subscribe/CompositeSubscriptionSourceDiscoveryServiceTest.java
@@ -0,0 +1,142 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.subscribe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import junit.framework.TestCase;
+
+
+/**
+ * {@link CompositeSubscriptionSourceDiscoveryServiceTest}
+ *
+ * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
+ *
+ */
+public class CompositeSubscriptionSourceDiscoveryServiceTest extends TestCase {
+    private CompositeSubscriptionSourceDiscoveryService compositeDiscoverer;
+
+    public void setUp() {
+        SimSubscriptionSourceDiscoveryService discoverer1 = new SimSubscriptionSourceDiscoveryService();
+        SimSubscriptionSourceDiscoveryService discoverer2 = new SimSubscriptionSourceDiscoveryService();
+        
+        discoverer1.addSource(source("com.openexchange.example.source1.1"));
+        discoverer1.addSource(source("com.openexchange.example.source1.2"));
+        
+        discoverer2.addSource(source("com.openexchange.example.source2.1"));
+        discoverer2.addSource(source("com.openexchange.example.source2.2"));
+        discoverer2.addSource(source("com.openexchange.example.source2.3"));
+        
+        this.compositeDiscoverer = new CompositeSubscriptionSourceDiscoveryService();
+        compositeDiscoverer.addSubscriptionSourceDiscoveryService(discoverer1);
+        compositeDiscoverer.addSubscriptionSourceDiscoveryService(discoverer2);
+    }
+    
+    public void testCompositeList() {
+        List<SubscriptionSource> allSources = compositeDiscoverer.getSources(null);
+        assertNotNull("compositeDiscoverer returned null!", allSources);
+        assertSources(allSources, "com.openexchange.example.source1.1", "com.openexchange.example.source1.2", "com.openexchange.example.source2.1", "com.openexchange.example.source2.2", "com.openexchange.example.source2.3");
+    }
+    
+    public void testCompositeKnowsSource() {
+        assertKnows(compositeDiscoverer, "com.openexchange.example.source1.1");
+        assertKnows(compositeDiscoverer, "com.openexchange.example.source1.2");
+        assertKnows(compositeDiscoverer, "com.openexchange.example.source2.1");
+        assertKnows(compositeDiscoverer, "com.openexchange.example.source2.2");
+        assertKnows(compositeDiscoverer, "com.openexchange.example.source2.3");
+
+        assertDoesNotKnow(compositeDiscoverer, "com.openexchange.example.source3.1");
+    }
+
+    public void testCompositeGet() {
+        assertNotNull("Failed getting com.openexchange.example.source1.1", compositeDiscoverer.getSource("com.openexchange.example.source1.1"));
+        assertNotNull("Failed getting com.openexchange.example.source1.1", compositeDiscoverer.getSource("com.openexchange.example.source1.2"));
+        assertNotNull("Failed getting com.openexchange.example.source1.1", compositeDiscoverer.getSource("com.openexchange.example.source2.1"));
+        assertNotNull("Failed getting com.openexchange.example.source1.1", compositeDiscoverer.getSource("com.openexchange.example.source2.2"));
+        assertNotNull("Failed getting com.openexchange.example.source1.1", compositeDiscoverer.getSource("com.openexchange.example.source2.3"));
+        
+        assertNull("Got com.openexchange.example.source3.1 ?!?", compositeDiscoverer.getSource("com.openexchange.example.source3.1"));
+        
+    }
+    
+    
+    private static void assertKnows(SubscriptionSourceDiscoveryService discoverer, String id) {
+        assertTrue("Did not know: "+id, discoverer.knowsSource(id));
+    }
+    
+    private static void assertDoesNotKnow(SubscriptionSourceDiscoveryService discoverer, String id) {
+        assertFalse("Did know: "+id, discoverer.knowsSource(id));
+    }
+    
+    private static void assertSources(List<SubscriptionSource> sources, String...expectedIdentifiers) {
+        List<String> identifier = new ArrayList<String>();
+        for(SubscriptionSource source : sources) {
+            identifier.add(source.getIdentifier());
+        }
+        List<String> expectedList = Arrays.asList(expectedIdentifiers);
+        assertEquals("Expected: "+expectedList+" Got: "+identifier, sources.size(), expectedIdentifiers.length);
+        
+        Set<String> actual = new HashSet<String>(identifier);
+        for(String expected : expectedIdentifiers) {
+            assertTrue("Expected: "+expectedList+" Got: "+identifier +" Missing: "+expected, actual.remove(expected));
+        }
+        assertTrue("Expected: "+expectedList+" Got: "+identifier, actual.isEmpty());
+        
+    }
+
+    private SubscriptionSource source(String identifier) {
+        SubscriptionSource source = new SubscriptionSource();
+        source.setIdentifier(identifier);
+        return source;
+    }
+}
