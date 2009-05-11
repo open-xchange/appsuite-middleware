51ed54d2c4b13e5dff7c06e0c8b46d10fc785ff3
Francisco Laguna <francisco.laguna@open-xchange.com>
2009-05-11 10:09:40 +0000

    new tables for generic configuration storage

---
+++ com.openexchange.admin/etc/mysql/genconf.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `genconf_attributes_strings` (
+   `cid` int(10) unsigned NOT NULL,
+   `id` int(10) unsigned NOT NULL,
+   `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `value` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `widget` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+   KEY (`cid`,`id`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `genconf_attributes_bools` (
+   `cid` int(10) unsigned NOT NULL,
+   `id` int(10) unsigned NOT NULL,
+   `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `value` tinyint(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `widget` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+   KEY (`cid`,`id`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE `sequence_genconf` (
+  `cid` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
