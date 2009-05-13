877cab7c99e74c7a32fcdd8a3de6fe14fb62d46a
Martin Herfurth <martin.herfurth@open-xchange.com>
2009-05-13 12:05:20 +0000

    Update Task for subscriptions

---
+++ openexchange-test/unittests/com/openexchange/groupware/update/CreateSubscribeTableTest.java
@@ -0,0 +1,102 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.groupware.update;
+
+import java.sql.SQLException;
+import com.openexchange.groupware.AbstractOXException;
+import com.openexchange.groupware.update.tasks.CreateSubscribeTableTask;
+
+/**
+ * @author <a href="mailto:martin.herfurth@open-xchange.org">Martin Herfurth</a>
+ */
+public class CreateSubscribeTableTest extends UpdateTest {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    public void tearDown() throws Exception {
+        execSafe("DROP TABLE subscriptions");
+        execSafe("DROP TABLE sequence_subscriptions");
+        super.tearDown();
+    }
+    
+    public void testShouldCreateSubscriptionsTable() throws AbstractOXException {
+        executeTask();
+        try {
+            exec("SELECT cid, id, user_id, configuration_id, source_id, folder_id, last_update FROM subscriptions");
+        } catch (SQLException x) {
+            fail("Expected table subscriptions, but doesn't seem to exist: "+x.toString());
+        }
+    }
+    
+    public void testShouldCreateSequenceTable() throws AbstractOXException {
+        executeTask();
+        try {
+            exec("SELECT cid, id FROM sequence_subscriptions");
+        } catch (SQLException x) {
+            fail("Expected table sequence_subscriptions, but doesn't seem to exist: "+x.toString());
+        }
+    }
+    
+    public void testShouldCreateEntryInSequenceTableForEveryContext() throws AbstractOXException, SQLException {
+        executeTask();
+        assertResult("SELECT 1 FROM sequence_subscriptions WHERE cid = "+existing_ctx_id);
+    }
+    
+    private void executeTask() throws AbstractOXException {
+        new CreateSubscribeTableTask().perform(schema, existing_ctx_id);
+    }
+    
+    public void testShouldBeRunnableTwice() throws AbstractOXException {
+        executeTask();
+        executeTask();
+    }
+}
