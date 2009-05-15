e06ca2e2c15bbd3ffe037c2f170488ebc1fb39f6
Manuel Kraft <manuel.kraft@open-xchange.com>
2009-05-15 13:05:41 +0000

    added missing utils test cases

--- com.openexchange.admin.soap/test/php/UnitTests/UtilTests.php
+++ com.openexchange.admin.soap/test/php/UnitTests/UtilTests.php
@@ -31,7 +31,7 @@ class UtilTests extends PHPUnit_Framework_TestCase {
                 if (is_array($result)) {
                     foreach ($result['return'] as $val_obj) {
                         if($val_obj->id == $result_register->id){
-                            $this->verifyCreatedServer($server,$val_obj);
+                            $this->verifyServer($server,$val_obj);
                             $found_server = true;
                             $loaded_server = $val_obj;
                             break;
@@ -39,7 +39,7 @@ class UtilTests extends PHPUnit_Framework_TestCase {
                     }
                 } else {
                     if($result->id == $result_register->id){
-                        $this->verifyCreatedServer($server,$result);
+                        $this->verifyServer($server,$result);
                         $loaded_server = $result;
                         break;
                     }else{
@@ -131,14 +131,14 @@ class UtilTests extends PHPUnit_Framework_TestCase {
                     // loop through all stores and try to verify
                     foreach ($list_result['return'] as $val_obj) {
                         if($val_obj->id == $filestore->id){
-                            $this->verifyCreatedFilestore($filestore, $val_obj);
+                            $this->verifyFilestore($filestore, $val_obj);
                             $foundstore = true;
                             break;
                         }
                     }
                 }else{
                     // just got one store as reply
-                    $this->verifyCreatedFilestore($filestore, $list_result);
+                    $this->verifyFilestore($filestore, $list_result);
                     $foundstore = true;
                 }
             }else{
@@ -189,22 +189,200 @@ class UtilTests extends PHPUnit_Framework_TestCase {
         }
     }
 
-    
+    function changeFilestoreAndVerify($filestore){
 
-    public function testChangeFilestore() {
-        // TODO
+        global $SOAPHOST;
+        global $OXMASTER_ADMIN;
+        global $OXMASTER_ADMIN_PASS;
+
+        $result = getUtilClient($SOAPHOST)->changeFilestore($filestore, getCredentialsObject($OXMASTER_ADMIN, $OXMASTER_ADMIN_PASS));
+        $found_store = false;
+        if (is_soap_fault($list_result)) {
+            fail("Error while change filestore");
+        }else{
+            // now load filestore again and return
+            // filestore was change, now list all filestores
+            $list_result = getUtilClient($SOAPHOST)->listFilestore("*", getCredentialsObject($OXMASTER_ADMIN,$OXMASTER_ADMIN_PASS));
+            if (!is_soap_fault($list_result)){
+                if (is_array($list_result)) {
+                    // loop through all stores and do not find it, if found, test fails
+                    foreach ($list_result['return'] as $val_obj) {
+                        if($val_obj->id == $filestore->id){
+                            $this->verifyFilestore($filestore, $val_obj);
+                            $found_store = true;
+                            break;
+                        }
+                    }
+                }else{
+                    if($list_result->id == $filestore->id){
+                        $this->verifyFilestore($filestore, $list_result);
+                    }
+                }
+            }else{
+                fail("Error listing filestores");
+            }
+        }
+
+        $this->assertTrue($found_store);
+
     }
 
-    public function testRegisterDatabase() {
-        // TODO
+    function registerDatabaseAndVerify($database){
+
+        global $SOAPHOST;
+        global $OXMASTER_ADMIN;
+        global $OXMASTER_ADMIN_PASS;
+
+        $result = getUtilClient($SOAPHOST)->registerDatabase($database, getCredentialsObject($OXMASTER_ADMIN, $OXMASTER_ADMIN_PASS));
+        $found_db = false;
+        if (is_soap_fault($list_result)) {
+            fail("Error while registering database");
+        }else{
+            // set database id
+            $database->id = $result->id;
+            // now load database again and verify
+            $list_result = getUtilClient($SOAPHOST)->listDatabase("*", getCredentialsObject($OXMASTER_ADMIN,$OXMASTER_ADMIN_PASS));
+            if (!is_soap_fault($list_result)){
+                if (is_array($list_result)) {
+                    // loop through all dbs 
+                    foreach ($list_result['return'] as $val_obj) {
+                        if($val_obj->name == $database->name){
+                            // found database
+                            $this->verifyDatabase($database, $val_obj);
+                            $found_db = true;
+                            break;
+                        }
+                    }
+                }else{
+                    if($list_result->name == $database->name){
+                        $this->verifyDatabase($filestore, $list_result);
+                    }
+                }
+            }else{
+                fail("Error listing databases");
+            }
+        }
+
+        return $database;
+
+
+
+    }
+
+
+    function unregisterDatabaseAndVerify($database){
+
+        global $SOAPHOST;
+        global $OXMASTER_ADMIN;
+        global $OXMASTER_ADMIN_PASS;
+
+        $result = getUtilClient($SOAPHOST)->unregisterDatabase($database, getCredentialsObject($OXMASTER_ADMIN, $OXMASTER_ADMIN_PASS));
+        
+        if (is_soap_fault($list_result)) {
+            fail("Error while unregistering database");
+        }else{
+            
+            // now load database again and verify
+            $list_result = getUtilClient($SOAPHOST)->listDatabase("*", getCredentialsObject($OXMASTER_ADMIN,$OXMASTER_ADMIN_PASS));
+            if (!is_soap_fault($list_result)){
+                if (is_array($list_result)) {
+                    // loop through all dbs
+                    foreach ($list_result['return'] as $val_obj) {
+                        if($val_obj->id == $database->id){                            
+                            fail("database was not unregistered");
+                        }
+                    }
+                }else{
+                    if($list_result->id == $database->id){
+                       fail("database was not unregistered!");
+                    }
+                }
+            }else{
+                fail("Error listing databases");
+            }
+        }
+
+    }
+
+
+
+
+
+    public function atestRegisterDatabase() {
+        srand(microtime()*1000000);
+        $random_id = rand(1, 99999);
+
+        $db = new Database();
+        $db->name = "name_by_soap" . $random_id;
+        $db->password = "password_by_soap." . $random_id;
+        $db->login = "login_by_soap" . $random_id;
+        $db->url = "jdbc:mysql://soaphost" . $random_id . "/?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useUnicode=true&useServerPrepStmts=false&useTimezone=true&serverTimezone=UTC&connectTimeout=15000&socketTimeout=15000";
+        $db->masterId = 0;
+        $db->master = true;
+        $db->clusterWeight = 100;
+        $db->maxUnits = 1000;
+        $db->poolHardLimit = 11;
+        $db->poolInitial = 2;
+        $db->poolMax = 5;
+        $db->driver = "com.mysql.jdbc.Driver";
+
+        $db = $this->registerDatabaseAndVerify($db);      
+        
     }
 
     public function testUnregisterDatabase() {
-        // TODO
-    }
+        srand(microtime()*1000000);
+        $random_id = rand(1, 99999);
 
-    public function testChangeDatabase() {
-        // TODO
+        $db = new Database();
+        $db->name = "name_by_soap" . $random_id;
+        $db->password = "password_by_soap." . $random_id;
+        $db->login = "login_by_soap" . $random_id;
+        $db->url = "jdbc:mysql://soaphost" . $random_id . "/?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useUnicode=true&useServerPrepStmts=false&useTimezone=true&serverTimezone=UTC&connectTimeout=15000&socketTimeout=15000";
+        $db->masterId = 0;
+        $db->master = true;
+        $db->clusterWeight = 100;
+        $db->maxUnits = 1000;
+        $db->poolHardLimit = 11;
+        $db->poolInitial = 2;
+        $db->poolMax = 5;
+        $db->driver = "com.mysql.jdbc.Driver";
+
+        // register database and verify creation
+        $db = $this->registerDatabaseAndVerify($db);
+
+        // unregister and verify
+        $this->unregisterDatabaseAndVerify($db);
+    }  
+
+    /**
+     * Registerfilestore, change it and load the changed store again and
+     * verify the loaded store against the changed
+     */
+    public function atestChangeFilestore() {
+        srand(microtime()*1000000);
+        $random_id = rand(1, 99999);
+
+        $store_dir = "/tmp/ox_tests_mount_".$random_id;
+        // create the dir temporary and delete a the end
+        mkdir($store_dir, 0700);
+
+        $store = new Filestore();
+        $store->url = "file://".$store_dir;
+        $store->size = 10000;
+        $store->maxContexts = 1337;
+
+        // create filestore and verify
+        $store = $this->registerFilestoreAndVerify($store);
+
+        // now change filestore data and verify
+        $store->url = $store->url."_changed";
+        $store->size = 1000;
+        $store->maxContexts = 7331;
+
+        $changed_store = $this->changeFilestoreAndVerify($store);
+
+
     }
 
 
@@ -222,7 +400,7 @@ class UtilTests extends PHPUnit_Framework_TestCase {
      *
      *
      */
-    public function testRegisterFilestore() {
+    public function atestRegisterFilestore() {
 
         srand(microtime()*1000000);
         $random_id = rand(1, 99999);
@@ -251,7 +429,7 @@ class UtilTests extends PHPUnit_Framework_TestCase {
      *
      *
      */
-    public function testUnregisterFilestore() {
+    public function atestUnregisterFilestore() {
 
         srand(microtime()*1000000);
         $random_id = rand(1, 99999);
@@ -325,17 +503,32 @@ class UtilTests extends PHPUnit_Framework_TestCase {
 
     }
 
-    public function verifyCreatedServer($expected, $server_response) {
+    public function verifyServer($expected, $server_response) {
         $this->assertEquals($expected->name, $server_response->name);
     }
 
-    public function verifyCreatedFilestore($expected, $server_response) {
+    public function verifyFilestore($expected, $server_response) {
         $this->assertEquals($expected->id, $server_response->id);
         $this->assertEquals($expected->url, $server_response->url);
         $this->assertEquals($expected->size, $server_response->size);
         $this->assertEquals($expected->maxContexts, $server_response->maxContexts);
     }
-
+    
+    public function verifyDatabase($expected, $server_response) {
+        $this->assertEquals($expected->id, $server_response->id);
+        $this->assertEquals($expected->password, $server_response->password);
+        $this->assertEquals($expected->login, $server_response->login);
+        $this->assertEquals($expected->name, $server_response->name);
+        $this->assertEquals($expected->url, $server_response->url);
+        $this->assertEquals($expected->masterId, $server_response->masterId);
+        $this->assertEquals($expected->master, $server_response->master);
+        $this->assertEquals($expected->clusterWeight, $server_response->clusterWeight);
+        $this->assertEquals($expected->maxUnits, $server_response->maxUnits);
+        $this->assertEquals($expected->poolHardLimit, $server_response->poolHardLimit);
+        $this->assertEquals($expected->poolInitial, $server_response->poolInitial);
+        $this->assertEquals($expected->poolMax, $server_response->poolMax);
+        $this->assertEquals($expected->driver, $server_response->driver);
+    }
 
 
 }
