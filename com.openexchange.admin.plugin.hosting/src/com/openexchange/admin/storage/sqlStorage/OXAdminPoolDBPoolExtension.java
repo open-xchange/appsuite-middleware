e8753612b1a459838ec3cdf7b0b28fbf2d3309db
Marcus Klein <marcus.klein@open-xchange.com>
2009-05-19 10:01:13 +0000

    moved database pooling classes and refactored configuration.

--- com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/storage/sqlStorage/OXAdminPoolDBPoolExtension.java
+++ com.openexchange.admin.plugin.hosting/src/com/openexchange/admin/storage/sqlStorage/OXAdminPoolDBPoolExtension.java
@@ -52,10 +52,10 @@ package com.openexchange.admin.storage.sqlStorage;
 import com.openexchange.admin.rmi.exceptions.PoolException;
 import com.openexchange.admin.storage.sqlStorage.OXAdminPoolInterfaceExtension;
 import com.openexchange.admin.tools.PropertyHandler;
-import com.openexchange.database.Database;
+import com.openexchange.database.DBPoolingException;
+import com.openexchange.database.DatabaseServiceImpl;
 import java.sql.Connection;
 
-import com.openexchange.server.impl.DBPoolingException;
 
 public class OXAdminPoolDBPoolExtension extends OXAdminPoolDBPool implements OXAdminPoolInterfaceExtension {
     
@@ -65,7 +65,7 @@ public class OXAdminPoolDBPoolExtension extends OXAdminPoolDBPool implements OXA
     
     public int getDBPoolIdForContextId(int context_id) throws PoolException {
         try{
-            return Database.resolvePool(context_id,true);
+            return DatabaseServiceImpl.resolvePool(context_id,true);
         }catch(DBPoolingException db){
             throw new PoolException(""+db.getMessage());
         }
@@ -73,19 +73,19 @@ public class OXAdminPoolDBPoolExtension extends OXAdminPoolDBPool implements OXA
 
     public Connection getWRITEConnectionForPoolId(int db_pool_id,String schema_name) throws PoolException {
         try{
-            return Database.get(db_pool_id,schema_name);
+            return DatabaseServiceImpl.get(db_pool_id,schema_name);
         }catch(DBPoolingException db){
             throw new PoolException(""+db.getMessage());
         }
     }
 
     public void pushWRITEConnectionForPoolId(int db_pool_id,Connection conny) throws PoolException {        
-        Database.back(db_pool_id,conny);
+        DatabaseServiceImpl.back(db_pool_id,conny);
     }
 
     public void resetPoolMappingForContext(int context_id) throws PoolException {
         try{
-            Database.reset(context_id);
+            DatabaseServiceImpl.reset(context_id);
         }catch(DBPoolingException db){
             throw new PoolException(""+db.getMessage());
         }
@@ -93,7 +93,7 @@ public class OXAdminPoolDBPoolExtension extends OXAdminPoolDBPool implements OXA
     
    public String getSchemeForContextId(int context_id) throws PoolException{
        try{
-            return Database.getSchema(context_id);
+            return DatabaseServiceImpl.getSchema(context_id);
         }catch(DBPoolingException db){
             throw new PoolException(""+db.getMessage());
         }
