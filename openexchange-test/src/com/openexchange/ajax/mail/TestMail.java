73f7330879cb26b88d463b81cd050c2f11634c70
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-05-26 10:58:25 +0000

    added equals() (and hash())

--- openexchange-test/src/com/openexchange/ajax/mail/TestMail.java
+++ openexchange-test/src/com/openexchange/ajax/mail/TestMail.java
@@ -557,4 +557,44 @@ public class TestMail implements IdentitySource<TestMail> {
         setId(entry.getId());
         setFolder(entry.getFolder());
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((folder == null) ? 0 : folder.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        TestMail other = (TestMail) obj;
+        if (folder == null) {
+            if (other.folder != null) {
+                return false;
+            }
+        } else if (!folder.equals(other.folder)) {
+            return false;
+        }
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
