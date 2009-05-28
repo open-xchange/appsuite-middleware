662fe3e64849adac4fc3128c698c0013814e250b
francisco <francisco>
2009-05-28 10:29:37 +0000

    Register Crypto Component

--- com.openexchange.crypto/src/com/openexchange/crypto/osgi/Activator.java
+++ com.openexchange.crypto/src/com/openexchange/crypto/osgi/Activator.java
@@ -4,8 +4,10 @@ package com.openexchange.crypto.osgi;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import com.openexchange.crypto.CryptoErrorMessage;
 import com.openexchange.crypto.CryptoService;
 import com.openexchange.crypto.CryptoServiceImpl;
+import com.openexchange.exceptions.osgi.ComponentRegistration;
 
 /**
  * @author <a href="mailto:martin.herfurth@open-xchange.org">Martin Herfurth</a>
@@ -13,13 +15,16 @@ import com.openexchange.crypto.CryptoServiceImpl;
 public class Activator implements BundleActivator {
     
     private ServiceRegistration cryptoRegistration;
+    private ComponentRegistration componentRegistration;
 
     public void start(BundleContext context) throws Exception {
         cryptoRegistration = context.registerService(CryptoService.class.getName(), new CryptoServiceImpl(), null);
+        componentRegistration = new ComponentRegistration(context, "CRP", "com.openexchange.crypto", CryptoErrorMessage.EXCEPTIONS);
     }
 
     public void stop(BundleContext context) throws Exception {
         cryptoRegistration.unregister();
+        componentRegistration.unregister();
     }
 
 }
