c0cb9dd1f88fe7b501679f07ca38e161150ddfd6
marcus <marcus>
2009-06-05 15:00:42 +0000

    documented empty interface.

--- global/src/com/openexchange/exceptions/LocalizableStrings.java
+++ global/src/com/openexchange/exceptions/LocalizableStrings.java
@@ -50,9 +50,12 @@
 package com.openexchange.exceptions;
 
 /**
- * Marker interface to tell the string exctractor that this class contains strings that have to be translated.
+ * Marker interface to tell the string extractor that this class contains strings that have to be translated.
  * 
  * @author Francisco Laguna <francisco.laguna@open-xchange.com>
  */
 public interface LocalizableStrings {
+
+    // Empty interface for classes containing string to translate.
+
 }
