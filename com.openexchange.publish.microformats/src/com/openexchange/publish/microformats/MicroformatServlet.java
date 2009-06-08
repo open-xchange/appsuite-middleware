4c26c79e8439aa9c91dfe11f5e51a871944ac2cf
francisco <francisco>
2009-06-08 09:33:56 +0000

    Bug 13753

--- com.openexchange.publish.microformats/src/com/openexchange/publish/microformats/MicroformatServlet.java
+++ com.openexchange.publish.microformats/src/com/openexchange/publish/microformats/MicroformatServlet.java
@@ -50,10 +50,11 @@
 package com.openexchange.publish.microformats;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.ServletException;
@@ -71,91 +72,94 @@ import com.openexchange.publish.microformats.tools.UncloseableWriter;
 import com.openexchange.templating.OXTemplate;
 import com.openexchange.templating.TemplateService;
 
-
 /**
  * {@link MicroformatServlet}
- *
+ * 
  * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
- *
  */
 public class MicroformatServlet extends HttpServlet {
+
     private static final Map<String, OXMFPublicationService> publishers = new HashMap<String, OXMFPublicationService>();
+
     private static final Map<String, String> templateNames = new HashMap<String, String>();
-    
+
     private static final Log LOG = LogFactory.getLog(MicroformatServlet.class);
-    
+
     private static final String MODULE = "module";
+
     private static final String SITE = "site";
+
     private static final String SECRET = "secret";
 
     private static final String CONTEXTID = "ctx";
 
     private static final String PROTECTED = "protected";
-    
+
     private static ContextService contexts = null;
+
     private static PublicationDataLoaderService dataLoader = null;
+
     private static TemplateService templateService = null;
 
     public static void setContextService(ContextService service) {
         contexts = service;
     }
-    
+
     public static void setPublicationDataLoaderService(PublicationDataLoaderService service) {
         dataLoader = service;
     }
-    
+
     public static void setTemplateService(TemplateService service) {
         templateService = service;
     }
-    
+
     public static void registerType(String module, OXMFPublicationService publisher, String templateName) {
         publishers.put(module, publisher);
         templateNames.put(module, templateName);
     }
-    
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             resp.setContentType("text/html; charset=UTF-8");
             Map<String, String> args = getPublicationArguments(req);
             String module = args.get(MODULE);
-            
+
             OXMFPublicationService publisher = publishers.get(module);
-            if(publisher == null) {
-                resp.getWriter().println("Don't know how to handle module "+module);
+            if (publisher == null) {
+                resp.getWriter().println("Don't know how to handle module " + module);
                 return;
             }
             Context ctx = contexts.getContext(Integer.parseInt(args.get(CONTEXTID)));
             Publication publication = publisher.getPublication(ctx, args.get(SITE));
-            if(publication == null) {
+            if (publication == null) {
                 resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                resp.getWriter().println("Don't know site "+args.get(SITE));
-                
+                resp.getWriter().println("Don't know site " + args.get(SITE));
+
                 return;
             }
-            if(!checkProtected(publication, args, resp)) {
+            if (!checkProtected(publication, args, resp)) {
                 return;
             }
-            
+
             Collection<? extends Object> loaded = dataLoader.load(publication);
-            
+
             HashMap<String, Object> variables = new HashMap<String, Object>();
             variables.put(getCollectionName(module), loaded);
-            
+
             String templateName = templateNames.get(module);
             OXTemplate template = templateService.loadTemplate(templateName);
-            
+
             template.process(variables, new UncloseableWriter(resp.getWriter()));
-            
+
         } catch (Throwable t) {
             LOG.error(t.getMessage(), t);
-            resp.getWriter().println("An exception occurred: "+t.toString());
+            resp.getWriter().println("An exception occurred: " + t.toString());
         }
     }
 
-
     private String getCollectionName(String module) {
-        if(module.equals("contacts")) {
+        if (module.equals("contacts")) {
             return "contacts";
         }
         return null;
@@ -163,9 +167,9 @@ public class MicroformatServlet extends HttpServlet {
 
     private boolean checkProtected(Publication publication, Map<String, String> args, HttpServletResponse resp) throws IOException {
         Map<String, Object> configuration = publication.getConfiguration();
-        if(configuration.containsKey(PROTECTED) && (Boolean) configuration.get("protected")) {
+        if (configuration.containsKey(PROTECTED) && (Boolean) configuration.get("protected")) {
             String secret = (String) configuration.get(SECRET);
-            if(!secret.equals(args.get(SECRET))) {
+            if (!secret.equals(args.get(SECRET))) {
                 resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 resp.getWriter().println("Don't know site this publication");
                 return false;
@@ -174,26 +178,52 @@ public class MicroformatServlet extends HttpServlet {
         return true;
     }
 
-
-    private Map<String, String> getPublicationArguments(HttpServletRequest req) {
+    private Map<String, String> getPublicationArguments(HttpServletRequest req) throws UnsupportedEncodingException {
         String[] path = req.getPathInfo().split("/");
         List<String> normalized = new ArrayList<String>(path.length);
-        for(int i = 0; i < path.length; i++) {
-            if(!path[i].equals("")) {
+        for (int i = 0; i < path.length; i++) {
+            if (!path[i].equals("")) {
                 normalized.add(path[i]);
             }
         }
-        
+
         int startIndex = normalized.indexOf("publications");
-        if(startIndex == -1) {
-            throw new IllegalArgumentException("This does not look like a valid path: "+req.getPathInfo());
+        if (startIndex == -1) {
+            throw new IllegalArgumentException("This does not look like a valid path: " + req.getPathInfo());
         }
-        String site = Strings.join(normalized.subList(startIndex+3, normalized.size()), "/");
+        String site = Strings.join(decode(normalized.subList(startIndex + 3, normalized.size()), req), "/");
         Map<String, String> args = new HashMap<String, String>();
-        args.put(MODULE, normalized.get(startIndex+1));
-        args.put(CONTEXTID, normalized.get(startIndex+2));
+        args.put(MODULE, normalized.get(startIndex + 1));
+        args.put(CONTEXTID, normalized.get(startIndex + 2));
         args.put(SITE, site);
         args.put(SECRET, req.getParameter(SECRET));
         return args;
+    }
+
+    // FIXME: Get Default Encoding from config service
+    private Collection<String> decode(List<String> subList, HttpServletRequest req) throws UnsupportedEncodingException {
+        String encoding = req.getCharacterEncoding() == null ? "UTF-8" : req.getCharacterEncoding();
+        List<String> decoded = new ArrayList<String>();
+        for(String component : subList) {
+            String decodedComponent = decode(component, encoding);
+            decoded.add(decodedComponent);
+        }
+        return decoded;
+    }
+    
+    // FIXME use server service for this
+    private String decode(String string, String encoding) throws UnsupportedEncodingException {
+        String[] chunks = string.split("\\+");
+        StringBuilder decoded = new StringBuilder(string.length());
+        boolean endsWithPlus = string.endsWith("+");
+        for (int i = 0; i < chunks.length; i++) {
+            String chunk = chunks[i];
+            decoded.append(URLDecoder.decode(chunk, encoding));
+            if(i != chunks.length - 1 || endsWithPlus) {
+                decoded.append('+');
+            }
+        }
+        
+        return decoded.toString();
     }
 }
