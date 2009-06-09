6d0d572d715afff48a98786660d7a991ef1cb067
Francisco Laguna <francisco.laguna@open-xchange.com>
2009-06-09 12:15:25 +0000

    Added pub&sub tables

---
+++ com.openexchange.admin/etc/mysql/publications.sql
@@ -0,0 +1,17 @@
+CREATE TABLE publications ( 
+ id INT(10) UNSIGNED NOT NULL,   
+ cid INT(10) UNSIGNED NOT NULL,  
+ user_id INT(10) UNSIGNED NOT NULL,  
+ entity VARCHAR(255) NOT NULL,  
+ module VARCHAR(255) NOT NULL,  
+ configuration_id INT(10) UNSIGNED NOT NULL,   
+ target_id VARCHAR(255) NOT NULL,  
+ PRIMARY KEY (id),  
+ FOREIGN KEY(cid, user_id) REFERENCES user(cid, id))   
+ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    
+CREATE TABLE sequence_publications ( 
+ cid int(10) unsigned NOT NULL,  
+ id int(10) unsigned NOT NULL,  
+ PRIMARY KEY  (cid))  
+ ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
