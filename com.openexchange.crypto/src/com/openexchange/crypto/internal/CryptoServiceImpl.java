0ddc978e44af39f5c27a2c6c4d2e190b8552d450
martin <martin>
2009-06-09 08:43:40 +0000

    clean up

--- com.openexchange.crypto/src/com/openexchange/crypto/internal/CryptoServiceImpl.java
+++ com.openexchange.crypto/src/com/openexchange/crypto/internal/CryptoServiceImpl.java
@@ -59,10 +59,7 @@ import java.security.Key;
 import java.security.SecureRandom;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.interfaces.PBEKey;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
 import com.openexchange.crypto.CryptoException;
@@ -72,6 +69,11 @@ import de.rtner.security.auth.spi.PBKDF2Engine;
 import de.rtner.security.auth.spi.PBKDF2Parameters;
 
 /**
+ * This Service provides Methods for encrypting and decrypting of Strings.
+ * 
+ * Warning: Do not change the parameters (IV, salt, algorithms, ...) in productive environment,
+ * because decryption of former encrypted date will be impossible.
+ * 
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  * @author <a href="mailto:martin.herfurth@open-xchange.org">Martin Herfurth</a>
  */
@@ -123,7 +125,7 @@ public class CryptoServiceImpl implements CryptoService {
     private final IvParameterSpec IV = new IvParameterSpec(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
 
     /**
-     * Salt
+     * Default Salt, if no salt is given or "used". But password encryption needs always encryption.
      */
     private final byte[] SALT = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
 
