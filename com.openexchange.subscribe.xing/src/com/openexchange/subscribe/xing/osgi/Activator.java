5f6015353d3a93f6f71ef890f58df45b1e1ff18b
francisco <francisco>
2009-06-18 17:41:41 +0000

    Try to foce xerces to use xerces classes from OSGi classloader by specifying bogus OX implementation of configuration interface

--- com.openexchange.subscribe.xing/src/com/openexchange/subscribe/xing/osgi/Activator.java
+++ com.openexchange.subscribe.xing/src/com/openexchange/subscribe/xing/osgi/Activator.java
@@ -1,23 +1,10 @@
 
 package com.openexchange.subscribe.xing.osgi;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.List;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.util.tracker.ServiceTracker;
 import com.openexchange.exceptions.osgi.ComponentRegistration;
-import com.openexchange.server.osgiservice.DeferredActivator;
-import com.openexchange.server.osgiservice.ServiceDependentRegistration;
-import com.openexchange.server.osgiservice.Whiteboard;
 import com.openexchange.subscribe.SubscribeService;
 import com.openexchange.subscribe.xing.XingContactParser;
 import com.openexchange.subscribe.xing.XingSubscribeService;
