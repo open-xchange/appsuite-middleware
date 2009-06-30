e761fb006a905eac994129c26088ed13de7a97df
Marcus Klein <marcus.klein@open-xchange.com>
2009-06-30 18:06:34 +0000

    wrote AJAX interface test for bug 14002.

---
+++ openexchange-test/src/com/openexchange/ajax/task/Bug14002Test.java
@@ -0,0 +1,119 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax.task;
+
+import java.io.IOException;
+import java.util.TimeZone;
+import org.json.JSONException;
+import org.xml.sax.SAXException;
+import com.openexchange.ajax.framework.AJAXClient;
+import com.openexchange.ajax.framework.CommonListResponse;
+import com.openexchange.ajax.framework.ListIDs;
+import com.openexchange.ajax.task.actions.DeleteRequest;
+import com.openexchange.ajax.task.actions.GetRequest;
+import com.openexchange.ajax.task.actions.GetResponse;
+import com.openexchange.ajax.task.actions.InsertRequest;
+import com.openexchange.ajax.task.actions.InsertResponse;
+import com.openexchange.ajax.task.actions.ListRequest;
+import com.openexchange.groupware.tasks.Create;
+import com.openexchange.groupware.tasks.Task;
+import com.openexchange.tools.servlet.AjaxException;
+import com.openexchange.tools.servlet.OXJSONException;
+
+/**
+ * Tests percentage completed set to 0.
+ * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
+ */
+public class Bug14002Test extends AbstractTaskTest {
+
+    private AJAXClient client;
+
+    private int folderId;
+
+    private TimeZone tz;
+
+    private Task task;
+
+    public Bug14002Test(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        client = getClient();
+        folderId = client.getValues().getPrivateTaskFolder();
+        tz = client.getValues().getTimeZone();
+        task = Create.createWithDefaults(folderId, "Bug 14002 test task");
+        task.setPercentComplete(0);
+        final InsertResponse insertR = client.execute(new InsertRequest(task, tz));
+        insertR.fillTask(task);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        client.execute(new DeleteRequest(task));
+        super.tearDown();
+    }
+
+    public void testPercentageComplete() throws AjaxException, IOException, SAXException, JSONException, OXJSONException {
+        GetRequest request = new GetRequest(task.getParentFolderID(), task.getObjectID());
+        GetResponse response = client.execute(request);
+        Task toTest = response.getTask(tz);
+        assertTrue("Percentage complete is missing.", toTest.containsPercentComplete());
+        assertEquals("Percentage complete has wrong value.", task.getPercentComplete(), toTest.getPercentComplete());
+        ListIDs ids = ListIDs.l(new int[] { task.getParentFolderID(), task.getObjectID() });
+        ListRequest request2 = new ListRequest(ids, new int[] { Task.PERCENT_COMPLETED });
+        CommonListResponse response2 = client.execute(request2);
+        Object percentage = response2.getValue(0, Task.PERCENT_COMPLETED);
+        assertNotNull("Percentage complete is missing.", percentage);
+        assertEquals("Percentage complete has wrong value.", task.getPercentComplete(), ((Integer) percentage).intValue());
+    }
+}
--- openexchange-test/src/com/openexchange/ajax/task/TaskTestSuite.java
+++ openexchange-test/src/com/openexchange/ajax/task/TaskTestSuite.java
@@ -107,6 +107,7 @@ public final class TaskTestSuite {
         tests.addTestSuite(Bug12364Test.class);
         tests.addTestSuite(Bug12727Test.class);
         tests.addTestSuite(Bug12926Test.class);
+        tests.addTestSuite(Bug14002Test.class);
         return tests;
     }
 }
