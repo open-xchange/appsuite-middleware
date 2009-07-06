d27639662423f528bc015983615ca899e153c8aa
marcus <marcus>
2009-07-06 11:21:22 +0000

    Fixed bug 14077: Tasks in public folders now get context administrator as creating or changing user if original user is removed.

--- server/src/com/openexchange/groupware/tasks/TasksDelete.java
+++ server/src/com/openexchange/groupware/tasks/TasksDelete.java
@@ -49,6 +49,7 @@
 
 package com.openexchange.groupware.tasks;
 
+import static com.openexchange.java.Autoboxing.I;
 import java.sql.Connection;
 import java.util.Date;
 import java.util.Set;
@@ -83,9 +84,7 @@ public class TasksDelete implements DeleteListener {
     /**
      * {@inheritDoc}
      */
-    public void deletePerformed(final DeleteEvent event,
-        final Connection readCon, final Connection writeCon)
-        throws DeleteFailedException {
+    public void deletePerformed(DeleteEvent event, Connection readCon, Connection writeCon) throws DeleteFailedException {
         switch (event.getType()) {
         case DeleteEvent.TYPE_USER:
             deleteUser(event, writeCon);
@@ -106,8 +105,7 @@ public class TasksDelete implements DeleteListener {
      * @param con writable database connection.
      * @throws DeleteFailedException if the delete gives an error.
      */
-    private void deleteUser(final DeleteEvent event, final Connection con)
-        throws DeleteFailedException {
+    private void deleteUser(DeleteEvent event, Connection con) throws DeleteFailedException {
         try {
             // First remove the user from the participants of tasks. Then only
             // tasks exist that have other users as participants or no one.
@@ -133,21 +131,16 @@ public class TasksDelete implements DeleteListener {
     /**
      * Removes the user from task participants.
      * @param event Event.
-     * @param readCon readable database connection.
-     * @param writeCon writable database connection.
+     * @param con writable database connection.
      * @throws TaskException if an exception occurs.
      */
-    private void removeUserFromParticipants(final DeleteEvent event,
-        final Connection writeCon)
-        throws TaskException {
+    private void removeUserFromParticipants(DeleteEvent event, Connection con) throws TaskException {
         final Context ctx = event.getContext();
         final int userId = event.getId();
         for (final StorageType type : StorageType.values()) {
-            final int[] tasks = partStor.findTasksWithParticipant(ctx,
-                writeCon, userId, type);
+            final int[] tasks = partStor.findTasksWithParticipant(ctx, con, userId, type);
             for (final int task : tasks) {
-                partStor.deleteInternal(ctx, writeCon, task, userId , type,
-                    true);
+                partStor.deleteInternal(ctx, con, task, userId , type, true);
             }
         }
     }
@@ -158,14 +151,12 @@ public class TasksDelete implements DeleteListener {
      * @param con writable database connection.
      * @throws TaskException if a problem occurs.
      */
-    private void assignToAdmin(final DeleteEvent event,
-        final Connection con) throws TaskException {
-        final Session session = getSession(event);
+    private void assignToAdmin(DeleteEvent event, Connection con) throws TaskException {
+        final Session session = event.getSession();
         final Context ctx = event.getContext();
         final int userId = event.getId();
         for (final StorageType type : StorageType.TYPES_AD) {
-            final int[][] result = foldStor.searchFolderByUser(ctx, con,
-                userId, type);
+            final int[][] result = foldStor.searchFolderByUser(ctx, con, userId, type);
             for (final int[] folderAndTask : result) {
                 final int folderId = folderAndTask[0];
                 FolderObject folder = null;
@@ -175,8 +166,7 @@ public class TasksDelete implements DeleteListener {
                     // Nothing to do.
                 }
                 final int taskId = folderAndTask[1];
-                final Set<Folder> folders = foldStor.selectFolder(ctx, con,
-                    taskId, type);
+                final Set<Folder> folders = foldStor.selectFolder(ctx, con, taskId, type);
                 if (folders.size() == 0) {
                     final String folderName;
                     if (null == folder) {
@@ -184,28 +174,19 @@ public class TasksDelete implements DeleteListener {
                     } else {
                         folderName = folder.getFolderName();
                     }
-                    throw new TaskException(TaskException.Code.NO_PERMISSION,
-                        Integer.valueOf(taskId), folderName, Integer
-                        .valueOf(folderId));
+                    throw new TaskException(TaskException.Code.NO_PERMISSION, I(taskId), folderName, I(folderId));
                 } else if (folders.size() > 1) {
                     // Participant with userId already removed by
                     // removeUserFromParticipants()
-                    foldStor.deleteFolder(ctx, con, taskId, folderId,
-                        type);
-                } else if (ctx.getMailadmin() == userId
-                    || StorageType.DELETED == type
-                    || null == folder) {
-                    TaskLogic.removeTask(session, ctx, con, folderId,
-                        taskId, type);
+                    foldStor.deleteFolder(ctx, con, taskId, folderId, type);
+                } else if (ctx.getMailadmin() == userId || StorageType.DELETED == type || null == folder) {
+                    TaskLogic.removeTask(session, ctx, con, folderId, taskId, type);
                 } else if (Tools.isFolderPublic(folder)) {
-                    foldStor.deleteFolder(ctx, con, taskId, folderId,
-                        type);
-                    final Folder aFolder = new Folder(folderId, ctx
-                        .getMailadmin());
+                    foldStor.deleteFolder(ctx, con, taskId, folderId, type);
+                    final Folder aFolder = new Folder(folderId, ctx.getMailadmin());
                     foldStor.insertFolder(ctx, con, taskId, aFolder, type);
                 } else if (Tools.isFolderPrivate(folder)) {
-                    TaskLogic.removeTask(session, ctx, con, folderId,
-                        taskId, type);
+                    TaskLogic.removeTask(session, ctx, con, folderId, taskId, type);
                 } else {
                     throw new TaskException(Code.UNIMPLEMENTED);
                 }
@@ -214,34 +195,20 @@ public class TasksDelete implements DeleteListener {
     }
 
     /**
+     * Remove deleted user from remaining tasks in the attributes createdFrom and lastModified.
      * @param event Event.
-     * @return the session object.
-     */
-    private Session getSession(final DeleteEvent event) {
-        return event.getSession();
-    }
-
-    /**
-     * Remove deleted user from remaining tasks in the attributes createdFrom and
-     * lastModified.
-     * @param event Event.
-     * @param readCon readable database connection.
-     * @param writeCon writable database connection.
+     * @param con writable database connection.
      * @throws TaskException if an exception occurs.
      */
-    private void changeCFMB(final DeleteEvent event, final Connection writeCon)
-        throws TaskException {
-        final int[] modified = new int[] { Task.CREATED_BY, Task.MODIFIED_BY,
-            Task.LAST_MODIFIED };
+    private void changeCFMB(DeleteEvent event, Connection con) throws TaskException {
+        final int[] modified = new int[] { Task.CREATED_BY, Task.MODIFIED_BY, Task.LAST_MODIFIED };
         final Context ctx = event.getContext();
         final int userId = event.getId();
         final TaskStorage stor = TaskStorage.getInstance();
         for (final StorageType type : StorageType.TYPES_AD) {
-            final int[] tasks = TaskSearch.getInstance().findDelegatedTasks(ctx,
-                writeCon, userId, type);
+            final int[] tasks = TaskSearch.getInstance().findUserTasks(ctx, con, userId, type);
             for  (final int taskId : tasks) {
-                final Task update = stor.selectTask(ctx, writeCon, taskId,
-                    type);
+                final Task update = stor.selectTask(ctx, con, taskId, type);
                 final Date lastModified = update.getLastModified();
                 if (update.getCreatedBy() == userId) {
                     update.setCreatedBy(ctx.getMailadmin());
@@ -250,8 +217,7 @@ public class TasksDelete implements DeleteListener {
                     update.setModifiedBy(ctx.getMailadmin());
                 }
                 update.setLastModified(new Date());
-                stor.updateTask(ctx, writeCon, update, lastModified, modified,
-                    type);
+                stor.updateTask(ctx, con, update, lastModified, modified, type);
             }
         }
     }
@@ -263,26 +229,21 @@ public class TasksDelete implements DeleteListener {
      * @param writeCon writable database connection.
      * @throws DeleteFailedException if the delete gives an error.
      */
-    private void deleteGroup(final DeleteEvent event, final Connection readCon,
-        final Connection writeCon) throws DeleteFailedException {
+    private void deleteGroup(DeleteEvent event, Connection readCon, Connection writeCon) throws DeleteFailedException {
         final Context ctx = event.getContext();
         final int groupId = event.getId();
         final ParticipantStorage partStor = ParticipantStorage.getInstance();
         try {
             for (final StorageType type : StorageType.values()) {
-                final int[] tasks = partStor.findTasksWithGroup(ctx, readCon,
-                    groupId, type);
+                final int[] tasks = partStor.findTasksWithGroup(ctx, readCon, groupId, type);
                 for (final int task : tasks) {
-                    Set<InternalParticipant> participants = partStor
-                        .selectInternal(ctx, readCon, task, type);
-                    participants = TaskLogic.extractWithGroup(participants,
-                        groupId);
+                    Set<InternalParticipant> participants = partStor.selectInternal(ctx, readCon, task, type);
+                    participants = TaskLogic.extractWithGroup(participants, groupId);
                     removeGroup(participants);
-                    partStor.updateInternal(ctx, writeCon, task, participants,
-                        type);
+                    partStor.updateInternal(ctx, writeCon, task, participants, type);
                 }
             }
-        } catch (final TaskException e) {
+        } catch (TaskException e) {
             throw new DeleteFailedException(e);
         }
     }
@@ -291,7 +252,7 @@ public class TasksDelete implements DeleteListener {
      * Removes the group from the participants.
      * @param participants task internal participants.
      */
-    private void removeGroup(final Set<InternalParticipant> participants) {
+    private void removeGroup(Set<InternalParticipant> participants) {
         for (final InternalParticipant participant : participants) {
             participant.setGroupId(null);
         }
