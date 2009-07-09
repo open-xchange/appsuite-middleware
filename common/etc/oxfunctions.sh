139774572f6e529781948ab109f898c827a398b1
schweigi <schweigi>
2009-07-09 10:05:22 +0000

    Extended the oxsysreport script that it works with OXAE

--- common/etc/oxfunctions.sh
+++ common/etc/oxfunctions.sh
@@ -71,10 +71,16 @@ DEBIAN=1
 REDHAT=2
 SUSE=4
 LSB=8
+UCS=16
 ox_system_type() {
     local ret=0
+    local isucs=$(uname -r|grep ucs)
     if [ -f /etc/debian_version ]; then
 	ret=$(( $ret | $DEBIAN ))
+    if [ -f /etc/debian_version ] && [ -z $isucs ]; then
+        ret=$(( $ret | $DEBIAN ))
+    elif [ -n $isucs ]; then
+        ret=$(( $ret | $UCS))
     elif [ -f /etc/SuSE-release ]; then
 	ret=$(( $ret | $SUSE ))
 	ret=$(( $ret | $LSB ))
