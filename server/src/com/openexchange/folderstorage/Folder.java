4dc4c6f88c64ccd1fdfded0f66ecc62188116ae2
thorben <thorben>
2009-07-28 10:34:57 +0000

    Enhanced JavaDoc and/or comment

--- server/src/com/openexchange/folderstorage/Folder.java
+++ server/src/com/openexchange/folderstorage/Folder.java
@@ -134,7 +134,6 @@ public interface Folder {
      * Sets the name.
      * 
      * @param name The name to set
-     * @see #getLocalizedName(Locale)
      */
     public void setName(String name);
 
