21024b7abe097a209e1d292977f2b92bc719a759
thorben <thorben>
2009-08-04 07:36:54 +0000

    Bugfix #14272: Discarding non-existing user/group permission previously detected as being corrupt

--- server/src/com/openexchange/tools/oxfolder/deletelistener/sql/MergerUtility.java
+++ server/src/com/openexchange/tools/oxfolder/deletelistener/sql/MergerUtility.java
@@ -53,7 +53,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import com.openexchange.tools.oxfolder.deletelistener.Permission;
 
 /**
@@ -148,6 +147,8 @@ public final class MergerUtility {
 	 * @return The merged permission assigned to specified fallback entity
 	 * @throws SQLException
 	 *             If a SQL error occurs
+	 * @throws IllegalStateException
+	 *             If neither a permission can be found for given entity nor fallback entity
 	 */
 	public static Permission getMergedPermission(final int entity, final int fallbackEntity, final int fuid,
 			final int cid, final Connection readCon, final boolean[] delete) throws SQLException {
@@ -173,10 +174,10 @@ public final class MergerUtility {
 				rs = stmt.executeQuery();
 				if (!rs.next()) {
 					/*
-					 * Empty permission
+					 * No permission?!
 					 */
-					throw new SQLException(new StringBuilder(64).append("Entity ").append(entity).append(
-							" has no permission on folder ").append(fuid).append(" in context ").append(cid).toString());
+				    throw new IllegalStateException(new StringBuilder(64).append("Entity ").append(entity).append(
+                    " has no permission on folder ").append(fuid).append(" in context ").append(cid).toString());
 				}
 				delete[0] = false;
 				return new Permission(fallbackEntity, fuid, rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs
