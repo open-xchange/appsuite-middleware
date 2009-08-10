09294d9d0d39ae017131e3b774e2ce87fe8c0cdb
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-08-10 09:40:45 +0000

    changed test behaviour not to rely on exception (because fail() also throws an exception, depending on version)

--- openexchange-test/src/com/openexchange/ajax/contact/ManagedContactTests.java
+++ openexchange-test/src/com/openexchange/ajax/contact/ManagedContactTests.java
@@ -54,17 +54,15 @@ import com.openexchange.ajax.framework.AbstractAJAXSession;
 import com.openexchange.groupware.container.Contact;
 import com.openexchange.test.ContactTestManager;
 
-
-
 /**
- *
  * @author <a href="mailto:tobias.prinz@open-xchange.com">Tobias Prinz</a>
  */
 public class ManagedContactTests extends AbstractAJAXSession {
 
     private ContactTestManager manager;
+
     private int folderID;
-    
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -82,88 +80,83 @@ public class ManagedContactTests extends AbstractAJAXSession {
     public ManagedContactTests(String name) {
         super(name);
     }
-    
-    private Contact generateContact(){
+
+    private Contact generateContact() {
         Contact contact = new Contact();
         contact.setSurName("Surname");
         contact.setGivenName("Given name");
         contact.setParentFolderID(folderID);
         return contact;
     }
-    
-    public void testCreateAndGetContact(){
+
+    public void testCreateAndGetContact() {
         Contact expected = generateContact();
-        
+
         manager.newAction(expected);
-        
+
         Contact actual = manager.getAction(folderID, expected.getObjectID());
-        
+
         assertEquals("Surname should match", expected.getSurName(), actual.getSurName());
         assertEquals("Given name should match", expected.getGivenName(), actual.getGivenName());
     }
-    
-    public void testDeleteContact(){
+
+    public void testDeleteContact() {
         Contact expected = generateContact();
-        
+
         manager.newAction(expected);
-        
+
         manager.deleteAction(expected);
-        
-        try {
-            manager.getAction(folderID, expected.getObjectID());
-            fail("Should throw assertion exception");
-        } catch(AssertionError e) {
-            
-        }
+
+        Contact found = manager.getAction(folderID, expected.getObjectID());
+        assertNull("Should not find a contact after deletion", found);
     }
-    
-    public void testGetAllContacts(){
-        int numberBefore = manager.allAction(folderID).length;
-        
-        Contact expected = generateContact();
-        
-        manager.newAction(expected);
-        
-        Contact[] allContactsOnServer = manager.allAction(folderID);
-        
-        assertEquals("Should find exactly one more contact", numberBefore+ 1, allContactsOnServer.length);
-    }
-    
-    
-    public void testGetAllContactsWithColumns(){
+
+    public void testGetAllContacts() {
         int numberBefore = manager.allAction(folderID).length;
 
         Contact expected = generateContact();
-        
+
         manager.newAction(expected);
-        
-        Contact[] allContactsOnServer = manager.allAction(folderID, new int[]{1,4,5,20});
-        
-        assertEquals("Should find exactly one more contact", numberBefore+ 1, allContactsOnServer.length);
-        
+
+        Contact[] allContactsOnServer = manager.allAction(folderID);
+
+        assertEquals("Should find exactly one more contact", numberBefore + 1, allContactsOnServer.length);
+    }
+
+    public void testGetAllContactsWithColumns() {
+        int numberBefore = manager.allAction(folderID).length;
+
+        Contact expected = generateContact();
+
+        manager.newAction(expected);
+
+        Contact[] allContactsOnServer = manager.allAction(folderID, new int[] { 1, 4, 5, 20 });
+
+        assertEquals("Should find exactly one more contact", numberBefore + 1, allContactsOnServer.length);
+
         Contact actual = allContactsOnServer[0];
-        assertTrue("Should contain field #1",  actual.contains(1));
-        // assertTrue("Should contain field #4",  actual.contains(4));
-        //assertTrue("Should contain field #5",  actual.contains(5));
+        assertTrue("Should contain field #1", actual.contains(1));
+        // assertTrue("Should contain field #4", actual.contains(4));
+        // assertTrue("Should contain field #5", actual.contains(5));
         assertTrue("Should contain field #20", actual.contains(20));
     }
-    
-    public void testUpdateContactAndGetUpdates(){
+
+    public void testUpdateContactAndGetUpdates() {
         Contact expected = generateContact();
-        
+
         manager.newAction(expected);
-        
+
         Date updateDate = new Date();
         expected.setDisplayName("Display name");
-        
+
         manager.updateAction(expected);
-        
+
         Contact[] updated = manager.updatesAction(folderID, updateDate);
-        
+
         assertEquals("Only one contact should have been updated", 1, updated.length);
-        
+
         Contact actual = updated[0];
         assertEquals("Display name should have been updated", expected.getDisplayName(), actual.getDisplayName());
     }
-        
+
 }
