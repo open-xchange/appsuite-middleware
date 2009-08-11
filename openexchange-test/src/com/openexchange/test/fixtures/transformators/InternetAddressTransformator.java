27e5124d88ce08054e0d40d0c36347a2fb907ceb
Martin Braun <martin.braun@open-xchange.com>
2009-08-11 17:58:15 +0000

    Added capability to detect e-mail addresses that shall be used for tests with notification mails.
    Notification mails do not contain a personal prefix like 'John Doe' <jd@ox.invalid> but only the plain address <jd@ox.invalid>. Recipients or senders that shall be used for notification tests can be marked like 'to: users:users_a (notification)' at the corresponding e-mail fixtures. The ' (notification') part is detected from the fixtures and the personal prefix (displayname) is not added for these entries. This detection also works when defining multiple recipients for an e-mail.

--- openexchange-test/src/com/openexchange/test/fixtures/transformators/InternetAddressTransformator.java
+++ openexchange-test/src/com/openexchange/test/fixtures/transformators/InternetAddressTransformator.java
@@ -51,6 +51,8 @@ package com.openexchange.test.fixtures.transformators;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
@@ -66,6 +68,7 @@ import com.openexchange.test.fixtures.SimpleCredentials;
  * address (to be used as email address)
  *
  * @author tfriedrich
+ * @author Martin Braun <martin.braun@open-xchange.com>
  */
 public class InternetAddressTransformator implements Transformator {
 
@@ -78,12 +81,26 @@ public class InternetAddressTransformator implements Transformator {
 
 	public Object transform(final String value) throws FixtureException {
 		if (null == value || 1 > value.length()) { return null; }
+
 		if (false == value.contains(":")) {
 			return getAddress(value);		// try to parseICal whole string
 		}
 		String fixtureName = "users";
 		String fixtureEntry = "";
 		final String[] splitted = value.split(",");
+		
+		final Pattern patFlags = Pattern.compile("\\((\\w+)\\)");
+		final String notificationMarker = "(notification)";
+		final boolean[] notificationFlags = new boolean[splitted.length];
+		
+		for (int i = 0; i < splitted.length; i++) {		
+	        Matcher mFlag = patFlags.matcher(splitted[i]);
+	        if(mFlag.find() && mFlag.group().contains(notificationMarker)) {
+	        	notificationFlags[i] = true;
+	        	splitted[i] = splitted[i].replace(notificationMarker, "").replace(" ", "");
+	        }
+		}
+        
 		final List<InternetAddress> addresses = new ArrayList<InternetAddress>(splitted.length);
 		for (int i = 0; i < splitted.length; i++) {
 			final int idx = splitted[i].indexOf(':');
@@ -93,7 +110,7 @@ public class InternetAddressTransformator implements Transformator {
 			} else {
 				fixtureEntry = splitted[i];
 			}
-			addresses.addAll(getAddresses(fixtureName, fixtureEntry));
+			addresses.addAll(getAddresses(fixtureName, fixtureEntry, notificationFlags[i]));
 		}
 		return addresses.toArray(new InternetAddress[addresses.size()]);
     }
@@ -106,18 +123,18 @@ public class InternetAddressTransformator implements Transformator {
 		}
 	}
 	
-	private List<InternetAddress> getAddresses(final String fixtureName, final String fixtureEntry) throws FixtureException {
+	private List<InternetAddress> getAddresses(final String fixtureName, final String fixtureEntry, final boolean notificationFlags) throws FixtureException {
 		final Contact contact = ("users".equals(fixtureName)) 
 			? fixtureLoader.getFixtures(fixtureName, SimpleCredentials.class).getEntry(fixtureEntry).getEntry().asContact() //users
 			: fixtureLoader.getFixtures(fixtureName, Contact.class).getEntry(fixtureEntry).getEntry(); //contacts
 		if (null == contact) {
 			throw new FixtureException("Unable to convert " + fixtureName + ":" + fixtureEntry + " into a contact.");
 		} else {
-			return getAddresses(contact);
+			return getAddresses(contact, notificationFlags);
 		}
 	}
 
-	private List<InternetAddress> getAddresses(final Contact contact) throws FixtureException {
+	private List<InternetAddress> getAddresses(final Contact contact, final boolean notificationFlags) throws FixtureException {
 		final List<InternetAddress> addresses = new ArrayList<InternetAddress>();
 		if (contact.containsDistributionLists()) {
 			final DistributionListEntryObject[] entries = contact.getDistributionList();
@@ -135,14 +152,29 @@ public class InternetAddressTransformator implements Transformator {
 		} else {
 			try {
 				if (contact.containsEmail1()) {
-					addresses.add(new InternetAddress(contact.getEmail1(), contact.getDisplayName()));
+					if (false == notificationFlags) {
+						addresses.add(new InternetAddress(contact.getEmail1(), contact.getDisplayName()));
+					} else {
+						addresses.add(new InternetAddress(contact.getEmail1()));
+					}
+					
 				} else if (contact.containsEmail2()) {
-					addresses.add(new InternetAddress(contact.getEmail2(), contact.getDisplayName()));
+					if (false == notificationFlags) {
+						addresses.add(new InternetAddress(contact.getEmail2(), contact.getDisplayName()));
+					} else {
+						addresses.add(new InternetAddress(contact.getEmail2()));
+					}
 				} else if (contact.containsEmail3()) {
-					addresses.add(new InternetAddress(contact.getEmail3(), contact.getDisplayName()));
+					if (false == notificationFlags) {
+						addresses.add(new InternetAddress(contact.getEmail3(), contact.getDisplayName()));
+					} else {
+						addresses.add(new InternetAddress(contact.getEmail3()));
+					}
 				}
 			} catch (UnsupportedEncodingException e) {
 				throw new FixtureException(e);
+			} catch (AddressException e) {
+				e.printStackTrace();
 			}
 		}
 		if (1 > addresses.size()) {
@@ -150,21 +182,4 @@ public class InternetAddressTransformator implements Transformator {
 		}
 		return addresses;
 	}
-	
-//	private InternetAddress getAddress(final ContactObject contact) throws FixtureException {
-//		String email = null;
-//		if (contact.containsEmail1()) {
-//			email = contact.getEmail1();
-//		} else if (contact.containsEmail2()) {
-//			email = contact.getEmail2();
-//		} else if (contact.containsEmail3()) {
-//			email = contact.getEmail3();
-//		}
-//		if (null == email) { throw new FixtureException("Contacts must contain an email address"); }
-//		try {
-//			return new InternetAddress(email, contact.getDisplayName());
-//		} catch (UnsupportedEncodingException e) {
-//			throw new FixtureException(e);
-//		}
-//	}
 }
