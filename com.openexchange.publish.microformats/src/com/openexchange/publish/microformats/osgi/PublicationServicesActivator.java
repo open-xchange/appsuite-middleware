ddb90baad6adb2a19910d01ae5947bfc862d332a
tierlieb <tierlieb>
2009-08-19 09:26:46 +0000

    cleaned up

--- com.openexchange.publish.microformats/src/com/openexchange/publish/microformats/osgi/PublicationServicesActivator.java
+++ com.openexchange.publish.microformats/src/com/openexchange/publish/microformats/osgi/PublicationServicesActivator.java
@@ -2,7 +2,6 @@
 package com.openexchange.publish.microformats.osgi;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,13 +20,13 @@ import com.openexchange.publish.microformats.tools.InfostoreTemplateUtils;
 public class PublicationServicesActivator implements BundleActivator {
 
     private List<ServiceRegistration> serviceRegistrations = new ArrayList<ServiceRegistration>();
-    
+
     private OXMFPublicationService contactPublisher;
 
     private List<String> aliases = new LinkedList<String>();
 
     private OXMFPublicationService infostorePublisher;
-    
+
     public void start(BundleContext context) throws Exception {
         aliases.clear();
         contactPublisher = new OXMFPublicationService();
@@ -35,35 +34,33 @@ public class PublicationServicesActivator implements BundleActivator {
         contactPublisher.setRootURL("/publications/contacts");
         contactPublisher.setTargetDisplayName("OXMF Contacts");
         contactPublisher.setTargetId("com.openexchange.publish.microformats.contacts.online");
-        
+
         Map<String, Object> additionalVars = new HashMap<String, Object>();
         additionalVars.put("utils", new ContactTemplateUtils());
-        
+
         MicroformatServlet.registerType("contacts", contactPublisher, "contacts.tmpl", additionalVars);
         ContactPictureServlet.setContactPublisher(contactPublisher);
-        
+
         aliases.add("/publications/contacts");
-        
-        serviceRegistrations.add( context.registerService(PublicationService.class.getName(), contactPublisher, null) );
-    
+
+        serviceRegistrations.add(context.registerService(PublicationService.class.getName(), contactPublisher, null));
+
         infostorePublisher = new OXMFPublicationService();
         infostorePublisher.setFolderType("infostore");
         infostorePublisher.setRootURL("/publications/infostore");
         infostorePublisher.setTargetDisplayName("OXMF Infostore");
         infostorePublisher.setTargetId("com.openexchange.publish.microformats.infostore.online");
         InfostoreFileServlet.setInfostorePublisher(infostorePublisher);
-        
-        
-        HashMap<String,Object> infoAdditionalVars = new HashMap<String,Object>();
+
+        HashMap<String, Object> infoAdditionalVars = new HashMap<String, Object>();
         infoAdditionalVars.put("utils", new InfostoreTemplateUtils());
-        
+
         MicroformatServlet.registerType("infostore", infostorePublisher, "infostore.tmpl", infoAdditionalVars);
-        
+
         aliases.add("/publications/infostore");
-        
-        serviceRegistrations.add( context.registerService(PublicationService.class.getName(), infostorePublisher, null) );
-    
-        
+
+        serviceRegistrations.add(context.registerService(PublicationService.class.getName(), infostorePublisher, null));
+
     }
 
     public void stop(BundleContext context) throws Exception {
@@ -76,7 +73,4 @@ public class PublicationServicesActivator implements BundleActivator {
         return aliases;
     }
 
-
-
-    
 }
