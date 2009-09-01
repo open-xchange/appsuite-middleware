1cabaa2bec9c7b1975557cc25de4c279d00ad9f4
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-09-01 12:50:13 +0000

    fixed problem where meterware looks for a 'scriptable'

--- openexchange-test/src/com/openexchange/webdav/xml/ContactTest.java
+++ openexchange-test/src/com/openexchange/webdav/xml/ContactTest.java
@@ -432,7 +432,7 @@ public class ContactTest extends AbstractWebdavXMLTest {
 		ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
 		final WebRequest req = new PutMethodWebRequest(host + CONTACT_URL, bais, "text/xml");
 		req.setHeaderField(AUTHORIZATION, "Basic " + getAuthData(login, password));
-		final WebResponse resp = webCon.getResponse(req);
+		final WebResponse resp = webCon.getResource(req);
 		assertEquals(207, resp.getResponseCode());
 		bais = new ByteArrayInputStream(resp.getText().getBytes());
 		final Response[] response = ResponseParser.parse(new SAXBuilder().build(bais), Types.CONTACT);
