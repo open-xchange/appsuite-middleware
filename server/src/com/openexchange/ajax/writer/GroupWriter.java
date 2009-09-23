4d062247393782f5fa52d4e5288ba8e9de673ee7
thorben <thorben>
2009-09-23 10:42:57 +0000

    Using time zone utility class

--- server/src/com/openexchange/ajax/writer/GroupWriter.java
+++ server/src/com/openexchange/ajax/writer/GroupWriter.java
@@ -49,7 +49,6 @@
 
 package com.openexchange.ajax.writer;
 
-import static com.openexchange.tools.TimeZoneUtils.getTimeZone;
 import java.util.TimeZone;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -57,11 +56,14 @@ import org.json.JSONObject;
 import com.openexchange.ajax.fields.DataFields;
 import com.openexchange.ajax.fields.GroupFields;
 import com.openexchange.group.Group;
+import com.openexchange.groupware.calendar.CalendarCollectionService;
+import com.openexchange.server.services.ServerServiceRegistry;
 
 /**
  * {@link GroupWriter} - Writes a group object into a JSON.
  * 
- * @author <a href="mailto:sebastian.kauss@open-xchange.com"">Sebastian Kauss</a>
+ * @author <a href="mailto:sebastian.kauss@open-xchange.com"">Sebastian
+ *         Kauss</a>
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
 public class GroupWriter extends DataWriter {
@@ -70,7 +72,7 @@ public class GroupWriter extends DataWriter {
 
     public GroupWriter() {
 		super(null, null);
-		utc = getTimeZone("utc");
+		utc = ServerServiceRegistry.getInstance().getService(CalendarCollectionService.class).getTimeZone("utc");
 	}
 
 	public void writeGroup(final Group group, final JSONObject json) throws JSONException {
