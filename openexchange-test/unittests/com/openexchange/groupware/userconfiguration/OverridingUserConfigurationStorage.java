f709a3324f02723a57b56aeac251c7fd64bdf4c8
Marcus Klein <marcus.klein@open-xchange.com>
2009-10-15 15:29:02 +0000

    Added information about user module access permission to detail method of reporting JMX interface.

--- openexchange-test/unittests/com/openexchange/groupware/userconfiguration/OverridingUserConfigurationStorage.java
+++ openexchange-test/unittests/com/openexchange/groupware/userconfiguration/OverridingUserConfigurationStorage.java
@@ -1,7 +1,10 @@
 package com.openexchange.groupware.userconfiguration;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.openexchange.groupware.AbstractOXException;
 import com.openexchange.groupware.contexts.Context;
+import com.openexchange.groupware.ldap.User;
 
 public class OverridingUserConfigurationStorage extends UserConfigurationStorage{
 
@@ -12,17 +15,17 @@ public class OverridingUserConfigurationStorage extends UserConfigurationStorage
     }
 
     @Override
-	protected void startInternal() throws AbstractOXException {
+    protected void startInternal() throws AbstractOXException {
         delegate.startInternal();
     }
 
     @Override
-	protected void stopInternal() throws AbstractOXException {
+    protected void stopInternal() throws AbstractOXException {
         delegate.stopInternal();
     }
 
     @Override
-	public UserConfiguration getUserConfiguration(final int userId, final int[] groups, final Context ctx) throws UserConfigurationException {
+    public UserConfiguration getUserConfiguration(final int userId, final int[] groups, final Context ctx) throws UserConfigurationException {
         final UserConfiguration config = getOverride(userId, groups, ctx);
         if( config != null) {
             return config;
@@ -31,12 +34,21 @@ public class OverridingUserConfigurationStorage extends UserConfigurationStorage
     }
 
     @Override
-	public void clearStorage() throws UserConfigurationException {
+    public UserConfiguration[] getUserConfiguration(Context ctx, User[] users) throws UserConfigurationException {
+        List<UserConfiguration> retval = new ArrayList<UserConfiguration>();
+        for (User user : users) {
+            retval.add(getUserConfiguration(user.getId(), user.getGroups(), ctx));
+        }
+        return retval.toArray(new UserConfiguration[retval.size()]);
+    }
+
+    @Override
+    public void clearStorage() throws UserConfigurationException {
         delegate.clearStorage();
     }
 
     @Override
-	public void removeUserConfiguration(final int userId, final Context ctx) throws UserConfigurationException {
+    public void removeUserConfiguration(final int userId, final Context ctx) throws UserConfigurationException {
         delegate.removeUserConfiguration(userId,ctx);
     } 
 
