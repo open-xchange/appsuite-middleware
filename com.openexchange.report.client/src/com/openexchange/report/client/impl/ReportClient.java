32aa4a1dc73907f7d0389c6adb3f5a7073381213
benni <benni>
2009-10-21 15:51:45 +0000

    Implemented new JMX Interface. Updated print screen. Updated Transport Handler (will now use JSON).

--- com.openexchange.report.client/src/com/openexchange/report/client/impl/ReportClient.java
+++ com.openexchange.report.client/src/com/openexchange/report/client/impl/ReportClient.java
@@ -49,37 +49,33 @@
 
 package com.openexchange.report.client.impl;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.List;
+
 import javax.management.AttributeNotFoundException;
 import javax.management.InstanceNotFoundException;
 import javax.management.IntrospectionException;
 import javax.management.MBeanException;
 import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
 import javax.management.ReflectionException;
-import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONException;
+
 import com.openexchange.admin.console.AbstractJMXTools;
 import com.openexchange.admin.console.AdminParser;
 import com.openexchange.admin.console.AdminParser.NeededQuadState;
 import com.openexchange.admin.console.CmdLineParser.Option;
-import com.openexchange.report.client.configuration.ReportConfiguration;
+import com.openexchange.report.client.container.ContextDetail;
+import com.openexchange.report.client.container.Total;
+import com.openexchange.tools.CSVWriter;
+import com.openexchange.tools.console.TableWriter;
+import com.openexchange.tools.console.TableWriter.ColumnFormat;
+import com.openexchange.tools.console.TableWriter.ColumnFormat.Align;
 
 public class ReportClient extends AbstractJMXTools {
-
-	private static final String REPORT_SERVER_URL = "https://activation.open-xchange.com";
-
-	private static final String POST_LICENSE_KEYS_KEY = "license_keys";
-
-	private static final String POST_METADATA_KEY = "client_information";
-
-	private static final String URL_ENCODING = "UTF-8";
 
 	private static final char OPT_SEND_ONLY_SHORT = 's';
 
@@ -88,12 +84,16 @@ public class ReportClient extends AbstractJMXTools {
 	private static final char OPT_DISPLAY_ONLY_SHORT = 'd';
 
 	private static final String OPT_DISPLAY_ONLY_LONG = "displayonly";
+	
+	private static final char OPT_CSV_SHORT = 'c';
+
+	private static final String OPT_CSV_LONG = "csv";
 
 	private Option displayonly = null;
 
 	private Option sendonly = null;
 	
-	private ReportConfiguration reportConfiguration;
+	private Option csv = null;
 
 	public static void main(final String args[]) {
 		final AbstractJMXTools t = new ReportClient();
@@ -101,97 +101,78 @@ public class ReportClient extends AbstractJMXTools {
 	}
 
     protected void furtherOptionsHandling(final AdminParser parser, HashMap<String, String[]> env) throws InterruptedException, IOException, MalformedURLException, InstanceNotFoundException, AttributeNotFoundException, IntrospectionException, MBeanException, ReflectionException {
-        if ((null != parser.getOptionValue(this.sendonly) && (null != parser.getOptionValue(this.displayonly)))) {
-            System.err.println("More than one of the stat options given. Using the default one one only (display and send)");
+		try {
+			final MBeanServerConnection initConnection = initConnection(false, env);
+			
+        	List<Total> totals = ObjectHandler.getTotalObjects(initConnection);
+        	List<ContextDetail> contextDetails = ObjectHandler.getDetailObjects(initConnection);
 
-            final MBeanServerConnection initConnection = initConnection(false, env);
-            String metadata = sendReport(initConnection);
-            System.out.println(metadata);
-        } else {
-            int count = 0;
-            if (null != parser.getOptionValue(this.sendonly)) {
-                final MBeanServerConnection initConnection = initConnection(false, env);
-                sendReport(initConnection);
-                count++;
-            }
-            if (null != parser.getOptionValue(this.displayonly)) {
-                if (0 == count) {
-                    final MBeanServerConnection initConnection = initConnection(false, env);
-                    System.out.println(getMetadata(initConnection));
+        	if ((null != parser.getOptionValue(this.sendonly) &&
+        			(null != parser.getOptionValue(this.displayonly)))) {
+                System.err.println("More than one of the stat options given. Using the default one one only (display and send)");
+                new TransportHandler().sendReport(totals, contextDetails);
+                print(totals, contextDetails, parser);
+            } else {
+                int count = 0;
+                if (null != parser.getOptionValue(this.sendonly)) {
+                	new TransportHandler().sendReport(totals, contextDetails);
                 }
-                count++;
+                if (null != parser.getOptionValue(this.displayonly)) {
+                    if (0 == count) {
+                    	print(totals, contextDetails, parser);
+                    }
+                    count++;
+                }
+                if (0 == count) {
+                    System.err.println("No option selected. Using the default one one (display and send)");
+                    new TransportHandler().sendReport(totals, contextDetails);
+                    print(totals, contextDetails, parser);
+                }
             }
-            if (0 == count) {
-                System.err.println("No option selected. Using the default one one (display and send)");
-
-                final MBeanServerConnection initConnection = initConnection(false, env);
-                String metadata = sendReport(initConnection);
-                System.out.println(metadata);
-            }
-        }
+		} catch (MalformedObjectNameException e1) {
+			e1.printStackTrace();
+		} catch (NullPointerException e1) {
+			e1.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
     }
 
     public void setFurtherOptions(AdminParser parser) {
-        this.sendonly = setShortLongOpt(parser, OPT_SEND_ONLY_SHORT, OPT_SEND_ONLY_LONG, "Send report without displaying it (Disables default)", false, NeededQuadState.notneeded);
-        this.displayonly = setShortLongOpt(parser, OPT_DISPLAY_ONLY_SHORT, OPT_DISPLAY_ONLY_LONG, "Display report without sending it (Disables default)", false, NeededQuadState.notneeded);
+        this.sendonly = setShortLongOpt(parser,
+        		OPT_SEND_ONLY_SHORT,
+        		OPT_SEND_ONLY_LONG,
+        		"Send report without displaying it (Disables default)",
+        		false,
+        		NeededQuadState.notneeded);
+        this.displayonly = setShortLongOpt(parser, OPT_DISPLAY_ONLY_SHORT,
+        		OPT_DISPLAY_ONLY_LONG,
+        		"Display report without sending it (Disables default)",
+        		false,
+        		NeededQuadState.notneeded);
+        this.csv = setShortLongOpt(parser, OPT_CSV_SHORT,
+        		OPT_CSV_LONG,
+        		"Show as CSV",
+        		false,
+        		NeededQuadState.notneeded);
     }
-
-    private String sendReport(final MBeanServerConnection mbc) throws MalformedURLException, IOException, InstanceNotFoundException, AttributeNotFoundException, IntrospectionException, MBeanException, ReflectionException {
-        String metadata = getMetadata(mbc);
-        
-        reportConfiguration = new ReportConfiguration();
-
-        StringBuffer report = new StringBuffer();
-        report.append(POST_LICENSE_KEYS_KEY);
-        report.append("=");
-        report.append(URLEncoder.encode(reportConfiguration.getLicenseKeys(), URL_ENCODING));
-        report.append("&");
-        report.append(POST_METADATA_KEY);
-        report.append("=");
-        report.append(URLEncoder.encode(metadata, URL_ENCODING));
-
-        HttpsURLConnection httpsURLConnection = (HttpsURLConnection) new URL(REPORT_SERVER_URL).openConnection();
-        httpsURLConnection.setUseCaches(false);
-        httpsURLConnection.setDoOutput(true);
-        httpsURLConnection.setDoInput(true);
-        httpsURLConnection.setRequestMethod("POST");
-        httpsURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-        DataOutputStream stream = new DataOutputStream(httpsURLConnection.getOutputStream());
-        stream.writeBytes(report.toString());
-        stream.flush();
-        stream.close();
-
-        if (httpsURLConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-            throw new MalformedURLException("Problem contacting report server: " + httpsURLConnection.getResponseCode());
+    	
+    private void print(List<Total> totals, List<ContextDetail> contextDetails, final AdminParser parser) {
+    	if (null != parser.getOptionValue(this.csv)) {
+            new CSVWriter(System.out, ObjectHandler.createTotalList(totals)).write();
         } else {
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
-            String buffer = "";
-            while ((buffer = in.readLine()) != null) {
-                System.out.println(new StringBuilder().append(REPORT_SERVER_URL).append(" said: ").append(buffer).toString());
-            }
-            in.close();
+            new TableWriter(System.out, new ColumnFormat[] { new ColumnFormat(Align.LEFT), new ColumnFormat(Align.LEFT) }, ObjectHandler.createTotalList(totals)).write();
         }
-
-        return metadata;
-    }
-
-    private String getMetadata(final MBeanServerConnection mbc) throws InstanceNotFoundException, AttributeNotFoundException, IntrospectionException, MBeanException, ReflectionException, IOException {
-        StringBuffer report = new StringBuffer();
-
-        report.append(getStats(mbc, "com.openexchange.ajp13.monitoring.AJPv13ServerThreadsMonitor").toString());
-        report.append(getStats(mbc, "com.openexchange.ajp13.najp.AJPv13ListenerMonitor"));
-        report.append(getStats(mbc, "com.openexchange.monitoring.internal.GeneralMonitor"));
-        report.append(getStats(mbc, "com.openexchange.api2.MailInterfaceMonitor"));
-        report.append(getStats(mbc, "com.openexchange.database.internal.ConnectionPool"));
-
-        report.append(getStats(mbc, "com.sun.management.UnixOperatingSystem"));
-        report.append(getStats(mbc, "sun.management.RuntimeImpl"));
-
-        report.append(getStats(mbc, "sun.management.MemoryPoolImpl"));
-        report.append(getStats(mbc, "sun.management.ThreadImpl"));
-
-        return report.toString();
+    	
+    	System.out.println("");
+    	
+    	if (null != parser.getOptionValue(this.csv)) {
+            new CSVWriter(System.out, ObjectHandler.createDetailList(contextDetails)).write();
+        } else {
+            new TableWriter(System.out, new ColumnFormat[] {
+                new ColumnFormat(Align.LEFT), new ColumnFormat(Align.LEFT), new ColumnFormat(Align.LEFT), new ColumnFormat(Align.LEFT),
+                new ColumnFormat(Align.LEFT) }, ObjectHandler.createDetailList(contextDetails)).write();
+        }
     }
 
 }
