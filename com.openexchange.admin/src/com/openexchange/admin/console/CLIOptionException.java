df69e4d601e58e2c03d4f4d6c8259dfe986c16f3
thorben <thorben>
2009-10-26 16:42:41 +0000

    Enhanced JavaDoc and/or comment

--- com.openexchange.admin/src/com/openexchange/admin/console/CLIOptionException.java
+++ com.openexchange.admin/src/com/openexchange/admin/console/CLIOptionException.java
@@ -8,10 +8,21 @@ public abstract class CLIOptionException extends Exception {
 
     private static final long serialVersionUID = 7125577259431666541L;
 
+    /**
+     * Initializes a new {@link CLIOptionException}.
+     * 
+     * @param msg The error message
+     */
     CLIOptionException(final String msg) {
         super(msg);
     }
 
+    /**
+     * Initializes a new {@link CLIOptionException}.
+     * 
+     * @param msg The error message
+     * @param cause The cause
+     */
     CLIOptionException(final String msg, final Throwable cause) {
         super(msg, cause);
     }
