c9f1a26d5f5911dad96a97d92a85abc08aeb7a13
marcus <marcus>
2009-10-28 10:54:20 +0000

    Fixed bug 14392: Marked subscription form labels as texts to translate and change a label.

--- com.openexchange.subscribe.microformats/src/com/openexchange/subscribe/microformats/osgi/SubcriptionServicesActivator.java
+++ com.openexchange.subscribe.microformats/src/com/openexchange/subscribe/microformats/osgi/SubcriptionServicesActivator.java
@@ -1,6 +1,55 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
 
 package com.openexchange.subscribe.microformats.osgi;
 
+import static com.openexchange.subscribe.microformats.FormStrings.FORM_LABEL_URL;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import com.openexchange.datatypes.genericonf.DynamicFormDescription;
@@ -23,7 +72,6 @@ public class SubcriptionServicesActivator implements BundleActivator {
 
     private ComponentRegistration componentRegistration;
 
-
     public void start(BundleContext context) throws Exception {
         componentRegistration = new ComponentRegistration(
             context,
@@ -41,7 +89,7 @@ public class SubcriptionServicesActivator implements BundleActivator {
         contactSubscriptionSource.setFolderModule(FolderObject.CONTACT);
 
         DynamicFormDescription form = new DynamicFormDescription();
-        form.add(FormElement.input("url", "URL", true, null));
+        form.add(FormElement.input("url", FORM_LABEL_URL, true, null));
         contactSubscriptionSource.setFormDescription(form);
 
         MicroformatSubscribeService subscribeService = new MicroformatSubscribeService();
@@ -73,11 +121,11 @@ public class SubcriptionServicesActivator implements BundleActivator {
         infostoreService.addPrefix("ox_");
 
         infostoreSubscriptionSource.setSubscribeService(infostoreService);
-        
+
         context.registerService(SubscribeService.class.getName(), subscribeService, null);
         context.registerService(SubscribeService.class.getName(), infostoreService, null);
-        
-        
+
+
         context.registerService(OXMFParserFactoryService.class.getName(), parserFactory, null);
         context.registerService(OXMFFormParser.class.getName(), new CybernekoOXMFFormParser(), null);
     }
@@ -85,5 +133,4 @@ public class SubcriptionServicesActivator implements BundleActivator {
     public void stop(BundleContext context) throws Exception {
         componentRegistration.unregister();
     }
-
 }
