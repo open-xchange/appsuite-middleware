b26049a1a85a3a91a52e7177ca78d85332ceddaa
thorben <thorben>
2009-11-01 10:11:16 +0000

    rename

--- server/src/com/openexchange/tools/servlet/http/manager/ConcurrentHttpServletManager.java
+++ server/src/com/openexchange/tools/servlet/http/manager/ConcurrentHttpServletManager.java
@@ -159,7 +159,7 @@ public final class ConcurrentHttpServletManager extends AbstractHttpServletManag
 
     private final static Class<?>[] CLASS_ARR = new Class[] {};
 
-    public void putServlet(final String path, final HttpServlet servletObj) {
+    public void putServlet(final String path, final HttpServlet servlet) {
         ServletQueue servletQueue = servletPool.get(path);
         if (null != servletQueue && servletQueue.isSingleton()) {
             return;
@@ -171,27 +171,27 @@ public final class ConcurrentHttpServletManager extends AbstractHttpServletManag
                 /*
                  * Since heading condition failed the servlet must be an instance of SingleThreadModel
                  */
-                servletQueue.enqueue(servletObj);
+                servletQueue.enqueue(servlet);
             } else {
                 try {
-                    servletQueue = new ServletQueue(1, servletObj.getClass().getConstructor(CLASS_ARR), !(servletObj instanceof SingleThreadModel));
+                    servletQueue = new ServletQueue(1, servlet.getClass().getConstructor(CLASS_ARR), !(servlet instanceof SingleThreadModel));
                 } catch (final SecurityException e) {
-                    LOG.error("Default constructor could not be found for servlet class: " + servletObj.getClass().getName(), e);
+                    LOG.error("Default constructor could not be found for servlet class: " + servlet.getClass().getName(), e);
                     return;
                 } catch (final NoSuchMethodException e) {
-                    LOG.error("Default constructor could not be found for servlet class: " + servletObj.getClass().getName(), e);
+                    LOG.error("Default constructor could not be found for servlet class: " + servlet.getClass().getName(), e);
                     return;
                 }
                 final ServletConfig conf = ServletConfigLoader.getDefaultInstance().getConfig(
-                    servletObj.getClass().getCanonicalName(),
+                    servlet.getClass().getCanonicalName(),
                     path);
                 try {
-                    servletObj.init(conf);
+                    servlet.init(conf);
                 } catch (final ServletException e) {
                     LOG.error("Servlet could not be put into pool", e);
                     return;
                 }
-                servletQueue.enqueue(servletObj);
+                servletQueue.enqueue(servlet);
                 servletPool.put(path, servletQueue);
             }
         } finally {
