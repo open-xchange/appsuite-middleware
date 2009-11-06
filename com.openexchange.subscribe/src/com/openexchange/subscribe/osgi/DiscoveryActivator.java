bb32b876fb649aaa40f6ad2c3cf3b55f99168a5e
thorben <thorben>
2009-11-06 15:41:46 +0000

    Nulling for garbage collection

--- com.openexchange.subscribe/src/com/openexchange/subscribe/osgi/DiscoveryActivator.java
+++ com.openexchange.subscribe/src/com/openexchange/subscribe/osgi/DiscoveryActivator.java
@@ -11,11 +11,10 @@ import org.osgi.framework.ServiceRegistration;
 import com.openexchange.context.osgi.WhiteboardContextService;
 import com.openexchange.crypto.CryptoService;
 import com.openexchange.datatypes.genericonf.storage.osgi.tools.WhiteboardGenericConfigurationStorageService;
+import com.openexchange.exceptions.osgi.ComponentRegistration;
 import com.openexchange.groupware.container.Contact;
 import com.openexchange.groupware.infostore.InfostoreFacade;
 import com.openexchange.groupware.tx.DBProvider;
-import com.openexchange.groupware.tx.osgi.WhiteboardDBProvider;
-import com.openexchange.groupware.userconfiguration.UserConfiguration;
 import com.openexchange.server.osgiservice.Whiteboard;
 import com.openexchange.subscribe.AbstractSubscribeService;
 import com.openexchange.subscribe.FolderUpdaterService;
@@ -30,8 +29,6 @@ import com.openexchange.subscribe.internal.SubscriptionExecutionServiceImpl;
 import com.openexchange.subscribe.sql.SubscriptionSQLStorage;
 import com.openexchange.user.UserService;
 import com.openexchange.userconf.UserConfigurationService;
-
-import com.openexchange.exceptions.osgi.ComponentRegistration;;
 /**
  * @author <a href="mailto:martin.herfurth@open-xchange.org">Martin Herfurth</a>
  */
@@ -48,18 +45,18 @@ public class DiscoveryActivator implements BundleActivator {
     private Whiteboard whiteboard;
     
   
-    public void start(BundleContext context) throws Exception {
+    public void start(final BundleContext context) throws Exception {
         whiteboard = new Whiteboard(context);
         collector = new OSGiSubscriptionSourceCollector(context);
         contextService = new WhiteboardContextService(context);
-        UserService users = whiteboard.getService(UserService.class);
-        UserConfigurationService userConfigs = whiteboard.getService(UserConfigurationService.class);
-        InfostoreFacade infostore = whiteboard.getService(InfostoreFacade.class);
+        final UserService users = whiteboard.getService(UserService.class);
+        final UserConfigurationService userConfigs = whiteboard.getService(UserConfigurationService.class);
+        final InfostoreFacade infostore = whiteboard.getService(InfostoreFacade.class);
         
-        Hashtable discoveryDict = new Hashtable();
+        final Hashtable discoveryDict = new Hashtable();
         discoveryDict.put(Constants.SERVICE_RANKING, 256);
         
-        OSGiSubscriptionSourceDiscoveryCollector discoveryCollector = new OSGiSubscriptionSourceDiscoveryCollector(context);
+        final OSGiSubscriptionSourceDiscoveryCollector discoveryCollector = new OSGiSubscriptionSourceDiscoveryCollector(context);
         discoveryCollector.addSubscriptionSourceDiscoveryService(collector);
         
         discoveryRegistration = context.registerService(SubscriptionSourceDiscoveryService.class.getName(), discoveryCollector, discoveryDict);
@@ -68,31 +65,38 @@ public class DiscoveryActivator implements BundleActivator {
         
         componentRegistration = new ComponentRegistration(context, "SUB", "com.openexchange.subscribe", SubscriptionErrorMessage.EXCEPTIONS);
         
-        List<FolderUpdaterService> folderUpdaters = new ArrayList<FolderUpdaterService>(1);
+        final List<FolderUpdaterService> folderUpdaters = new ArrayList<FolderUpdaterService>(1);
         folderUpdaters.add(new StrategyFolderUpdaterService<Contact>(new ContactFolderUpdaterStrategy()));
         folderUpdaters.add(new StrategyFolderUpdaterService<DocumentMetadataHolder>(new DocumentMetadataHolderFolderUpdaterStrategy(users, userConfigs, infostore)));
         
         
-        SubscriptionExecutionServiceImpl executor = new SubscriptionExecutionServiceImpl(collector, folderUpdaters, contextService);
+        final SubscriptionExecutionServiceImpl executor = new SubscriptionExecutionServiceImpl(collector, folderUpdaters, contextService);
         executionRegistration = context.registerService(SubscriptionExecutionService.class.getName(), executor, null);
     
-        DBProvider provider = whiteboard.getService(DBProvider.class);
+        final DBProvider provider = whiteboard.getService(DBProvider.class);
         genconfStorage = new WhiteboardGenericConfigurationStorageService(context);
-        SubscriptionSQLStorage storage = new SubscriptionSQLStorage(provider, genconfStorage, discoveryCollector);
+        final SubscriptionSQLStorage storage = new SubscriptionSQLStorage(provider, genconfStorage, discoveryCollector);
     
         AbstractSubscribeService.STORAGE = storage;
         
         AbstractSubscribeService.CRYPTO = whiteboard.getService(CryptoService.class);
     }
 
-    public void stop(BundleContext context) throws Exception {
+    public void stop(final BundleContext context) throws Exception {
         whiteboard.close();
+        whiteboard = null;
         genconfStorage.close();
+        genconfStorage = null;
         componentRegistration.unregister();
+        componentRegistration = null;
         collector.close();
+        collector = null;
         contextService.close();
+        contextService = null;
         discoveryRegistration.unregister();
+        discoveryRegistration = null;
         executionRegistration.unregister();
+        executionRegistration = null;
     }
 
 }
