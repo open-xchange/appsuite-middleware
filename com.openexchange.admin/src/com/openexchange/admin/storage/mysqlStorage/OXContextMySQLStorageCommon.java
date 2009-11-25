339bbbfe6144335bbb9b4360341f99d8af57802b
Marcus Klein <marcus.klein@open-xchange.com>
2009-11-25 16:40:43 +0000

    Fixed bug 13634: Using same generic use checks as for creating normal users.

--- com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXContextMySQLStorageCommon.java
+++ com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXContextMySQLStorageCommon.java
@@ -220,25 +220,6 @@ public class OXContextMySQLStorageCommon {
         group_stmt.close();
     }
 
-    /**
-     * @param ctx
-     * @param admin_user
-     * @param con writable context database connection.
-     * @param internal_user_id
-     * @param contact_id
-     * @param uid_number
-     * @param access
-     * @throws StorageException
-     * @throws InvalidDataException
-     */
-    public final void createAdminForContext(final Context ctx, final User admin_user, final Connection con, final int internal_user_id, final int contact_id, final int uid_number, final UserModuleAccess access) throws StorageException, InvalidDataException {
-        OXUserStorageInterface oxs = OXUserStorageInterface.getInstance();
-        OXToolStorageInterface tool = OXToolStorageInterface.getInstance();
-        tool.primaryMailExists(con, ctx, admin_user.getPrimaryEmail());
-        admin_user.setDefaultSenderAddress(admin_user.getPrimaryEmail());
-        oxs.create(ctx, admin_user, access, con, internal_user_id, contact_id, uid_number);
-    }
-
     public final void deleteContextFromConfigDB(Connection configCon, int contextId) throws SQLException {
         // find out what db_schema context belongs to
         PreparedStatement stmt3 = null;
