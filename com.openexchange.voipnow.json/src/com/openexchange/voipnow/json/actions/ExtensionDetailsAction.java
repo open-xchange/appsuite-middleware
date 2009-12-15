9e30e9a426492223f15d6625e5d1a48d3c099e44
thorben <thorben>
2009-12-15 11:29:33 +0000

    minor

--- com.openexchange.voipnow.json/src/com/openexchange/voipnow/json/actions/ExtensionDetailsAction.java
+++ com.openexchange.voipnow.json/src/com/openexchange/voipnow/json/actions/ExtensionDetailsAction.java
@@ -96,7 +96,7 @@ public final class ExtensionDetailsAction extends AbstractVoipNowSOAPAction<Exte
              * Parse parameters
              */
             final String extensionId = checkStringParameter(request, "id");
-            final String identifier = checkStringParameter(request, "identifier");
+            //final String identifier = checkStringParameter(request, "identifier");
             final VoipNowServerSetting setting = getVoipNowServerSetting(session);
             /*
              * The SOAP stub for VoipNow extension access
