70e3c3dee4f81f6da3baf765602b35aa3f3cd874
marcus <marcus>
2009-12-21 14:09:16 +0000

    switched from schema version to table remembering the executed update tasks.

--- etc/mysql/version.sql
+++
@@ -1,9 +0,0 @@
-#@(#) Table for storing the schema version.
-
-CREATE TABLE version (
-    version INT4 UNSIGNED NOT NULL,
-    locked BOOLEAN NOT NULL,
-    gw_compatible BOOLEAN NOT NULL,
-    admin_compatible BOOLEAN NOT NULL,
-    server VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
