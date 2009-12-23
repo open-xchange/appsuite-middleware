5332a82a418f10405af2057bf1412fec04a31682
tierlieb <tierlieb>
2009-12-23 16:03:01 +0000

    added new method to merge lists

--- server/src/com/openexchange/groupware/contact/helpers/ContactMerger.java
+++ server/src/com/openexchange/groupware/contact/helpers/ContactMerger.java
@@ -49,8 +49,9 @@
 
 package com.openexchange.groupware.contact.helpers;
 
+import java.util.LinkedList;
+import java.util.List;
 import com.openexchange.groupware.container.Contact;
-
 
 /**
  * @author <a href="mailto:tobias.prinz@open-xchange.com">Tobias Prinz</a>
@@ -58,33 +59,43 @@ import com.openexchange.groupware.container.Contact;
 public class ContactMerger {
 
     private boolean overwrite = false;
-    
-    public ContactMerger(boolean overwrite){
+
+    public ContactMerger(boolean overwrite) {
         setOverwrite(overwrite);
     }
-    
-    public void setOverwrite(boolean overwrite){
+
+    public void setOverwrite(boolean overwrite) {
         this.overwrite = overwrite;
     }
-    
+
     /**
-     * Merges the first object with the second one and returns a merged one. 
-     * In case of conflicts: If <i>overwrite</i> is set, values from the 
-     * second object take precedence, otherwise the values from the first do.
+     * Merges the first object with the second one and returns a merged one. In case of conflicts: If <i>overwrite</i> is set, values from
+     * the second object take precedence, otherwise the values from the first do.
      * 
      * @param c1 first object to be merged. This one will be changed afterwards.
      * @param c2 second object to be merged. This one will not be changed at all.
      */
-    public Contact merge(Contact c1, Contact c2){
+    public Contact merge(Contact c1, Contact c2) {
         Contact clone = c1.clone();
-        
-        for(ContactField field : ContactField.values()){
+
+        for (ContactField field : ContactField.values()) {
             int number = field.getNumber();
-            if(c2.contains(number))
-                if(overwrite || ! c1.contains(number))
+            if (c2.contains(number))
+                if (overwrite || !c1.contains(number))
                     clone.set(number, c2.get(number));
         }
-        
+
         return clone;
+    }
+
+    public List<Contact> merge(List<Contact> list1, List<Contact> list2) {
+        if (list1.size() != list2.size())
+            throw new IllegalArgumentException("Both lists to be merged must be same length!");
+
+        LinkedList<Contact> merged = new LinkedList<Contact>();
+        for (int i = 0, length = list1.size(); i < length; i++)
+            merged.add(merge(list1.get(i), list2.get(i)));
+
+        return merged;
     }
 }
