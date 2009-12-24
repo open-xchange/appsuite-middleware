2920fdfe1eeee60ef202e156424695d391fe30d2
Tobias Prinz <tobias.prinz@open-xchange.com>
2009-12-24 11:58:35 +0000

    made the whole test more "whole"

--- openexchange-test/src/com/openexchange/ajax/roundtrip/pubsub/AbstractPubSubRoundtripTest.java
+++ openexchange-test/src/com/openexchange/ajax/roundtrip/pubsub/AbstractPubSubRoundtripTest.java
@@ -52,6 +52,7 @@ package com.openexchange.ajax.roundtrip.pubsub;
 import com.openexchange.ajax.publish.tests.AbstractPubSubTest;
 import com.openexchange.ajax.publish.tests.PublicationTestManager;
 import com.openexchange.ajax.subscribe.test.SubscriptionTestManager;
+import com.openexchange.groupware.contact.helpers.ContactField;
 import com.openexchange.groupware.container.Contact;
 
 
@@ -110,39 +111,10 @@ public class AbstractPubSubRoundtripTest extends AbstractPubSubTest{
     }
 
     protected void assertNoDataMessedUpMaximumRequirements(Contact expected, Contact actual) {
-        assertEquals(expected.getSurName(), actual.getSurName());
-        assertEquals(expected.getGivenName(), actual.getGivenName());
-        assertEquals(expected.getCompany(), actual.getCompany());
-        assertEquals(expected.getEmail1(), actual.getEmail1());
-        assertEquals(expected.getEmail2(), actual.getEmail2());
-        assertEquals(expected.getEmail3(), actual.getEmail3());
-        
-        assertEquals(expected.getDisplayName(), actual.getDisplayName());
-        assertEquals(expected.getPosition(), actual.getPosition());
-        assertEquals(expected.getTitle(), actual.getTitle());
-        assertEquals(expected.getSurName(), actual.getSurName());
-        
-        assertEquals(expected.getPosition(), actual.getPosition());
-        assertEquals(expected.getTitle(), actual.getTitle());
-        assertEquals(expected.getSuffix(), actual.getSuffix());
-        
-        assertEquals(expected.getStreetBusiness(), actual.getStreetBusiness());
-        assertEquals(expected.getStreetHome(), actual.getStreetHome());
-        assertEquals(expected.getStreetOther(), actual.getStreetOther());
-        assertEquals(expected.getPostalCodeBusiness(), actual.getPostalCodeBusiness());
-        assertEquals(expected.getPostalCodeHome(), actual.getPostalCodeHome());
-        assertEquals(expected.getPostalCodeOther(), actual.getPostalCodeOther());
-        assertEquals(expected.getCityBusiness(), actual.getCityBusiness());
-        assertEquals(expected.getCityHome(), actual.getCityHome());
-        assertEquals(expected.getCityOther(), actual.getCityOther());
-        assertEquals(expected.getCountryBusiness(), actual.getCountryBusiness());
-        assertEquals(expected.getCountryHome(), actual.getCountryHome());
-        assertEquals(expected.getCountryOther(), actual.getCountryOther());        
-        assertEquals(expected.getStateBusiness(), actual.getStateBusiness());
-        assertEquals(expected.getStateHome(), actual.getStateHome());
-        assertEquals(expected.getStateOther(), actual.getStateOther());  
-        
-        assertEquals(expected.getUserField01(), actual.getUserField01());
+        for(ContactField fieldEnum: ContactField.values()){
+            int field = fieldEnum.getNumber();
+            assertEquals("Expecting field "+fieldEnum+" to match", expected.get(field), actual.get(field));
+        }
     }
 
     
