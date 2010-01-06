93a66ac514ae16d003f5e036d3299235f04b0681
francisco <francisco>
2010-01-06 10:10:57 +0000

    Use the namespaced attribute to determine user access

--- com.openexchange.voipnow.json/src/com/openexchange/voipnow/json/preferences/VoipNowEnabled.java
+++ com.openexchange.voipnow.json/src/com/openexchange/voipnow/json/preferences/VoipNowEnabled.java
@@ -84,7 +84,7 @@ public class VoipNowEnabled implements PreferencesItemService {
         return new ReadOnlyValue() {
 
             public void getValue(final Session session, final Context ctx, final User user, final UserConfiguration userConfig, final Setting setting) throws SettingException {
-                setting.setSingleValue(Boolean.valueOf(UserAttributeAccess.getDefaultInstance().getBooleanAttribute("com.4psa.voipnow", user, false)));
+                setting.setSingleValue(Boolean.valueOf(UserAttributeAccess.getDefaultInstance().getBooleanAttribute("com.4psa.voipnow/enabled", user, false)));
             }
 
             /**
