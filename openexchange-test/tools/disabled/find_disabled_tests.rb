17769f6554beedd08ad644d1d05ff23c59de5dcf
Tobias Prinz <tobias.prinz@open-xchange.com>
2010-01-07 15:48:19 +0000

    does find disabled tests now. Has some false positives, but those would not be much manual work

--- openexchange-test/tools/disabled/find_disabled_tests.rb
+++ openexchange-test/tools/disabled/find_disabled_tests.rb
@@ -1,31 +1,30 @@
-def iterate_all_files(directory)
-  
+require 'find'
+
+def is_ox_java_file?(filename)
+  filename =~ /(com\/openexchange\/.*)\.java/
 end
 
-
-def read_file(filename)
-  
+def make_classname(filename)
+  filename =~ /(com\/openexchange\/.*)\.java/
+  reduced = Regexp.last_match(1)
+  reduced.gsub(/\//,".")
 end
 
-
-
-disabled_tests = Array.new
-
-iterate_all_files(Dir.new("../..")) do |file|
-  read_file(file) do |line|
-    if(line =~ /public\s+void\s+(\w+?)test(\w+?)\(\)/ )
-      disabled_tests << "#{file}##{$1}test#{$2}"
+def find_disabled_test(filename)
+  fh = File.open(filename)
+  classname = make_classname(filename)
+  fh.each_line do |line|
+    if line =~ /void\s+(\w+?[tT]est[^\(]+)\(\)/
+      method = Regexp.last_match(1)
+      print "#{classname}##{method}\n"
     end
   end
 end
 
+disabled_tests = Array.new
 
-
-
-
-
-
-
-
-
-
+Find.find(File.dirname(__FILE__)+"/../../src") do |file|
+  if is_ox_java_file?(file)
+    find_disabled_test(file)
+  end
+end
