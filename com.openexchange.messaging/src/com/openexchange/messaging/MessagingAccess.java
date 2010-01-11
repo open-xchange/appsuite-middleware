b09dd6252e90ef17fcedfdef88f6597b58025158
thorben <thorben>
2010-01-11 16:08:46 +0000

    Enhanced JavaDoc and/or comment

--- com.openexchange.messaging/src/com/openexchange/messaging/MessagingAccess.java
+++ com.openexchange.messaging/src/com/openexchange/messaging/MessagingAccess.java
@@ -63,22 +63,63 @@ public interface MessagingAccess {
      * Gets the message associated with specified identifier.
      * 
      * @param id The identifier
+     * @param folder The folder identifier
      * @param peek <code>true</code> to peek message content (meaning any mechanisms to mark content as read disabled); otherwise
      *            <code>false</code>
      * @return The message associated with specified identifier
      * @throws MessagingException If message cannot be returned
      */
-    public MessagingMessage getMessage(String id, boolean peek) throws MessagingException;
+    public MessagingMessage getMessage(String id, String folder, boolean peek) throws MessagingException;
 
+    /**
+     * Appends specified messages to given folder.
+     * 
+     * @param folder The folder to append to
+     * @param messages The messages to append.
+     * @throws MessagingException If appending messages fails
+     */
     public void appendMessages(String folder, MessagingMessage[] messages) throws MessagingException;
 
+    /**
+     * Copies specified messages from source folder to destination folder.
+     * 
+     * @param sourceFolder The source folder identifier
+     * @param destFolder The destination folder identifier
+     * @param messageIds The message identifiers
+     * @param fast <code>true</code> for fast copy (meaning no identifiers of corresponding messages in destination folder); otherwise
+     *            <code>false</code>
+     * @return The identifiers of corresponding messages in destination folder. Unless parameter <tt>fast</tt> is set to <code>true</code>;
+     *         then <code>null</code> is returned.
+     * @throws MessagingException If copy operation fails
+     */
     public String[] copyMessages(String sourceFolder, String destFolder, String[] messageIds, boolean fast) throws MessagingException;
 
+    /**
+     * Deletes specified messages in folder.
+     * 
+     * @param folder The folder to delete in
+     * @param messageIds The message identifiers
+     * @param hardDelete <code>true</code> to perform a hard-delete; otherwise <code>false</code> to backup in default location
+     * @throws MessagingException If delete operation fails
+     */
     public void deleteMessages(String folder, String[] messageIds, boolean hardDelete) throws MessagingException;
 
-    public List<MessagingMessage> getAllMessages(String folder, MessagingField... fields) throws MessagingException;
+    /**
+     * A convenience method to get all messages located in given folder.
+     * 
+     * @param folder The folder identifier
+     * @param indexRange The index range specifying the desired sub-list in sorted list; may be <code>null</code> to obtain complete list.
+     *            Range begins at the specified start index and extends to the message at index <code>end - 1</code>. Thus the length of the
+     *            range is <code>end - start</code>.
+     * @param sortField The sort field
+     * @param order Whether ascending or descending sort order
+     * @param fields The fields to pre-fill in returned instances of {@link MessagingMessage}
+     * @return The desired, pre-filled instances of {@link MessagingMessage}
+     * @throws MessagingException If returning all messages fails
+     */
+    public List<MessagingMessage> getAllMessages(String folder, IndexRange indexRange, MessagingField sortField, OrderDirection order, MessagingField... fields) throws MessagingException;
 
-    public MessagingMessage perform(String action, String id) throws MessagingException;
+    public MessagingMessage perform(String action, String id, String folder) throws MessagingException;
 
     public void send(MessagingMessage message) throws MessagingException;
 
