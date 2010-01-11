7650f0e2de4c6c3c55b637a3f48ebb514f20fa5c
thorben <thorben>
2010-01-11 15:02:08 +0000

    Added fields

--- com.openexchange.messaging/src/com/openexchange/messaging/MessagingField.java
+++ com.openexchange.messaging/src/com/openexchange/messaging/MessagingField.java
@@ -49,13 +49,187 @@
 
 package com.openexchange.messaging;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@link MessagingField}
- *
+ * 
  * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
 public enum MessagingField {
-    ID, COLOR_LABEL, FLAGS, RECEIVED_DATE, USER_FLAGS, BODY, HEADERS;
+
+    /**
+     * The implementation-specific unique mail ID<br>
+     * <b>[low cost]</b>
+     */
+    ID("id"),
+    /**
+     * The folder ID or fullname<br>
+     * <b>[low cost]</b>
+     */
+    FOLDER_ID("folderId"),
+    /**
+     * The Content-Type; includes whether message contains attachments or not<br>
+     * <b>[low cost]</b>
+     */
+    CONTENT_TYPE("contentType"),
+    /**
+     * From<br>
+     * <b>[low cost]</b>
+     */
+    FROM("from"),
+    /**
+     * To<br>
+     * <b>[low cost]</b>
+     */
+    TO("to"),
+    /**
+     * Cc<br>
+     * <b>[low cost]</b>
+     */
+    CC("cc"),
+    /**
+     * Bcc<br>
+     * <b>[low cost]</b>
+     */
+    BCC("bcc"),
+    /**
+     * Subject<br>
+     * <b>[low cost]</b>
+     */
+    SUBJECT("subject"),
+    /**
+     * Size<br>
+     * <b>[low cost]</b>
+     */
+    SIZE("size"),
+    /**
+     * Sent date corresponds to <code>Date</code> header<br>
+     * <b>[low cost]</b>
+     */
+    SENT_DATE("sentDate"),
+    /**
+     * Received date represent the internal mail server's timestamp on arrival<br>
+     * <b>[low cost]</b>
+     */
+    RECEIVED_DATE("receivedDate"),
+    /**
+     * Flags<br>
+     * <b>[low cost]</b>
+     */
+    FLAGS("flags"),
+    /**
+     * Thread level<br>
+     * <b>[low cost]</b>
+     */
+    THREAD_LEVEL("threadLevel"),
+    /**
+     * Email address in <code>Disposition-Notification-To</code> header<br>
+     * <b>[low cost]</b>
+     */
+    DISPOSITION_NOTIFICATION_TO("dispositionNotificationTo"),
+    /**
+     * Integer value of <code>X-Priority</code> header<br>
+     * <b>[low cost]</b>
+     */
+    PRIORITY("priority"),
+    /**
+     * Color Label<br>
+     * <b>[low cost]</b>
+     */
+    COLOR_LABEL("colorLabel"),
+    /**
+     * Account name<br>
+     * <b>[low cost]</b>
+     */
+    ACCOUNT_NAME("accountName"),
+    /**
+     * To peek the mail body (\Seen flag is left unchanged)<br>
+     * <b>[high cost]</b>
+     */
+    BODY("body"),
+    /**
+     * To fetch all message headers<br>
+     * <b>[high cost]</b>
+     */
+    HEADERS("headers"),
+    /**
+     * To fully pre-fill mail incl. headers and peeked body (\Seen flag is left unchanged)<br>
+     * <b>[high cost]</b>
+     */
+    FULL("full");
+
+    private final String name;
+
+    private MessagingField(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    /**
+     * All low cost fields
+     */
+    public static final MessagingField[] FIELDS_LOW_COST =
+        {
+            ID, FOLDER_ID, CONTENT_TYPE, FROM, TO, CC, BCC, SUBJECT, SIZE, SENT_DATE, RECEIVED_DATE, FLAGS, THREAD_LEVEL,
+            DISPOSITION_NOTIFICATION_TO, PRIORITY, COLOR_LABEL };
+
+    /**
+     * All fields except {@link #BODY} and {@link #FULL}
+     */
+    public static final MessagingField[] FIELDS_WO_BODY =
+        {
+            ID, FOLDER_ID, CONTENT_TYPE, FROM, TO, CC, BCC, SUBJECT, SIZE, SENT_DATE, RECEIVED_DATE, FLAGS, THREAD_LEVEL,
+            DISPOSITION_NOTIFICATION_TO, PRIORITY, COLOR_LABEL, HEADERS };
+
+    private static final Map<String, MessagingField> FIELDS_MAP;
+
+    static {
+        final Map<String, MessagingField> m = new HashMap<String, MessagingField>(32);
+        final MessagingField[] fields = MessagingField.values();
+        for (final MessagingField field : fields) {
+            m.put(field.name, field);
+        }
+        FIELDS_MAP = Collections.unmodifiableMap(m);
+    }
+
+    private static final MessagingField[] EMPTY_FIELDS = new MessagingField[0];
+
+    /**
+     * Creates an array of {@link MessagingField} corresponding to given names.
+     * <p>
+     * This is just a convenience method that invokes {@link #getField(String)} for every name.
+     * 
+     * @see #getField(String)
+     * @param names The names
+     * @return The array of {@link MessagingField} corresponding to given names
+     */
+    public static final MessagingField[] getFields(final String[] names) {
+        if ((names == null) || (names.length == 0)) {
+            return EMPTY_FIELDS;
+        }
+        final MessagingField[] retval = new MessagingField[names.length];
+        for (int i = 0; i < names.length; i++) {
+            retval[i] = getField(names[i]);
+        }
+        return retval;
+    }
+
+    /**
+     * Maps specified name to a field.
+     * 
+     * @param name The field name
+     * @return The mapped {@link MessagingField} or <code>null</code> if no corresponding mail field could be found
+     */
+    public static MessagingField getField(final String name) {
+        return FIELDS_MAP.get(name);
+    }
+
 }
