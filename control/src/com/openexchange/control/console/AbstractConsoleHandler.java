712c5e6406dc247e224d33d8c6f2ed97fe08dd3b
dennis <dennis>
2010-01-15 17:16:13 +0000

    - some refactoring to separate the JMX parts, so that they can be used elsewhere

--- control/src/com/openexchange/control/console/AbstractConsoleHandler.java
+++ control/src/com/openexchange/control/console/AbstractConsoleHandler.java
@@ -49,24 +49,6 @@
 
 package com.openexchange.control.console;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import javax.management.MBeanServerConnection;
-import javax.management.ObjectName;
-import javax.management.remote.JMXAuthenticator;
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXConnectorServer;
-import javax.management.remote.JMXPrincipal;
-import javax.management.remote.JMXServiceURL;
-import javax.security.auth.Subject;
-import org.apache.commons.codec.binary.Base64;
 import com.openexchange.control.console.internal.ConsoleException;
 import com.openexchange.control.console.internal.ValueObject;
 import com.openexchange.control.console.internal.ValuePairObject;
@@ -78,27 +60,13 @@ import com.openexchange.control.console.internal.ValueParser;
  * @author <a href="mailto:sebastian.kauss@open-xchange.com">Sebastian Kauss</a>
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
-public abstract class AbstractConsoleHandler {
+public abstract class AbstractConsoleHandler extends AbstractJMXHandler {
 
     /**
      * The default parameter for host, port, login, and password: <code>&quot;-h&quot;</code>, <code>&quot;-p&quot;</code>,
      * <code>&quot;-l&quot;</code>, and <code>&quot;-pw&quot;</code>.
      */
     static final String[] DEFAULT_PARAMETER = { "-h", "-p", "-l", "-pw" };
-
-    protected String jmxHost = "localhost";
-
-    protected int jmxPort = 9999;
-
-    protected String jmxLogin;
-
-    protected String jmxPassword;
-
-    protected JMXConnector jmxConnector;
-
-    protected ObjectName objectName;
-
-    protected MBeanServerConnection mBeanServerConnection;
 
     protected ValueParser valueParser;
 
@@ -107,6 +75,11 @@ public abstract class AbstractConsoleHandler {
     }
 
     protected final void init(final String args[], final boolean noArgs) throws ConsoleException {
+        String jmxHost = DEFAULT_HOST;
+        int jmxPort = DEFAULT_PORT;
+        String jmxLogin = null;
+        String jmxPassword = null;
+
         if (!noArgs && args.length == 0) {
             showHelp();
             exit();
@@ -141,127 +114,14 @@ public abstract class AbstractConsoleHandler {
         }
     }
 
-    protected final void initJMX(final String jmxHost, final int jmxPort, final String jmxLogin, final String jmxPassword) throws Exception {
-        final JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + jmxHost + ":" + jmxPort + "/server");
-
-        final Map<String, Object> environment;
-        if (jmxLogin == null || jmxPassword == null) {
-            environment = null;
-        } else {
-            environment = new HashMap<String, Object>(1);
-            environment.put(JMXConnectorServer.AUTHENTICATOR, new AbstractConsoleJMXAuthenticator(new String[] { jmxLogin, jmxPassword }));
-        }
-
-        jmxConnector = JMXConnectorFactory.connect(url, environment);
-
-        mBeanServerConnection = jmxConnector.getMBeanServerConnection();
-
-        objectName = ObjectName.getInstance("com.openexchange.control", "name", "Control");
-    }
-
     protected abstract void showHelp();
 
     protected abstract void exit();
 
     protected abstract String[] getParameter();
 
-    protected ObjectName getObjectName() {
-        return objectName;
-    }
-
-    protected MBeanServerConnection getMBeanServerConnection() {
-        return mBeanServerConnection;
-    }
-
     protected ValueParser getParser() {
         return valueParser;
     }
 
-    protected final void close() throws ConsoleException {
-        try {
-            if (jmxConnector != null) {
-                jmxConnector.close();
-            }
-        } catch (final Exception exc) {
-            throw new ConsoleException(exc);
-        }
-    }
-
-    private static final class AbstractConsoleJMXAuthenticator implements JMXAuthenticator {
-
-        private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(AbstractConsoleJMXAuthenticator.class);
-
-        private static volatile Charset US_ASCII;
-
-        private static Charset getUSASCII() {
-            if (US_ASCII == null) {
-                synchronized (AbstractConsoleJMXAuthenticator.class) {
-                    if (US_ASCII == null) {
-                        US_ASCII = Charset.forName("US-ASCII");
-                    }
-                }
-            }
-            return US_ASCII;
-        }
-
-        private final String[] credentials;
-
-        public AbstractConsoleJMXAuthenticator(final String[] credentials) {
-            super();
-            this.credentials = new String[credentials.length];
-            System.arraycopy(credentials, 0, this.credentials, 0, credentials.length);
-        }
-
-        public Subject authenticate(final Object credentials) {
-            if (!(credentials instanceof String[])) {
-                if (credentials == null) {
-                    throw new SecurityException("Credentials required");
-                }
-                throw new SecurityException("Credentials should be String[]");
-            }
-            final String[] creds = (String[]) credentials;
-            if (creds.length != 2) {
-                throw new SecurityException("Credentials should have 2 elements");
-            }
-            /*
-             * Perform authentication
-             */
-            final String username = creds[0];
-            final String password = creds[1];
-            if ((this.credentials[0].equals(username)) && (this.credentials[1].equals(makeSHAPasswd(password)))) {
-                return new Subject(true, Collections.singleton(new JMXPrincipal(username)), Collections.EMPTY_SET, Collections.EMPTY_SET);
-            }
-            throw new SecurityException("Invalid credentials");
-
-        }
-
-        private static String makeSHAPasswd(final String raw) {
-            MessageDigest md;
-
-            try {
-                md = MessageDigest.getInstance("SHA-1");
-            } catch (final NoSuchAlgorithmException e) {
-                LOG.error(e.getMessage(), e);
-                return raw;
-            }
-
-            final byte[] salt = {};
-
-            md.reset();
-            try {
-                md.update(raw.getBytes("UTF-8"));
-            } catch (final UnsupportedEncodingException e) {
-                /*
-                 * Cannot occur
-                 */
-                LOG.error(e.getMessage(), e);
-            }
-            md.update(salt);
-
-            final String ret = getUSASCII().decode(ByteBuffer.wrap(Base64.encodeBase64(md.digest()))).toString();
-
-            return ret;
-        }
-
-    }
 }
---
+++ control/src/com/openexchange/control/console/AbstractJMXHandler.java
@@ -0,0 +1,158 @@
+
+package com.openexchange.control.console;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.remote.JMXAuthenticator;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXPrincipal;
+import javax.management.remote.JMXServiceURL;
+import javax.security.auth.Subject;
+import org.apache.commons.codec.binary.Base64;
+import com.openexchange.control.console.internal.ConsoleException;
+
+/**
+ * {@link AbstractJMXHandler} - This class contains the JMX stuff for the command line tools
+ *
+ * @author <a href="mailto:dennis.sieben@open-xchange.com">Dennis Sieben</a>
+ *
+ */
+public abstract class AbstractJMXHandler {
+
+    protected static final class AbstractConsoleJMXAuthenticator implements JMXAuthenticator {
+
+        private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(AbstractConsoleJMXAuthenticator.class);
+
+        private static volatile Charset US_ASCII;
+
+        private final String[] credentials;
+
+        public AbstractConsoleJMXAuthenticator(final String[] credentials) {
+            super();
+            this.credentials = new String[credentials.length];
+            System.arraycopy(credentials, 0, this.credentials, 0, credentials.length);
+        }
+
+        private static Charset getUSASCII() {
+            if (US_ASCII == null) {
+                synchronized (AbstractConsoleJMXAuthenticator.class) {
+                    if (US_ASCII == null) {
+                        US_ASCII = Charset.forName("US-ASCII");
+                    }
+                }
+            }
+            return US_ASCII;
+        }
+
+        private static String makeSHAPasswd(final String raw) {
+            MessageDigest md;
+
+            try {
+                md = MessageDigest.getInstance("SHA-1");
+            } catch (final NoSuchAlgorithmException e) {
+                LOG.error(e.getMessage(), e);
+                return raw;
+            }
+
+            final byte[] salt = {};
+
+            md.reset();
+            try {
+                md.update(raw.getBytes("UTF-8"));
+            } catch (final UnsupportedEncodingException e) {
+                /*
+                 * Cannot occur
+                 */
+                LOG.error(e.getMessage(), e);
+            }
+            md.update(salt);
+
+            final String ret = getUSASCII().decode(ByteBuffer.wrap(Base64.encodeBase64(md.digest()))).toString();
+
+            return ret;
+        }
+
+        public Subject authenticate(final Object credentials) {
+            if (!(credentials instanceof String[])) {
+                if (credentials == null) {
+                    throw new SecurityException("Credentials required");
+                }
+                throw new SecurityException("Credentials should be String[]");
+            }
+            final String[] creds = (String[]) credentials;
+            if (creds.length != 2) {
+                throw new SecurityException("Credentials should have 2 elements");
+            }
+            /*
+             * Perform authentication
+             */
+            final String username = creds[0];
+            final String password = creds[1];
+            if ((this.credentials[0].equals(username)) && (this.credentials[1].equals(makeSHAPasswd(password)))) {
+                return new Subject(true, Collections.singleton(new JMXPrincipal(username)), Collections.EMPTY_SET, Collections.EMPTY_SET);
+            }
+            throw new SecurityException("Invalid credentials");
+
+        }
+
+    }
+
+    protected static final String DEFAULT_HOST = "localhost";
+
+    protected final static int DEFAULT_PORT = 9999;
+
+    private JMXConnector jmxConnector;
+
+    private MBeanServerConnection mBeanServerConnection;
+
+    private ObjectName objectName;
+
+    protected final void close() throws ConsoleException {
+        try {
+            if (jmxConnector != null) {
+                jmxConnector.close();
+            }
+        } catch (final Exception exc) {
+            throw new ConsoleException(exc);
+        }
+    }
+
+    protected MBeanServerConnection getMBeanServerConnection() {
+        return mBeanServerConnection;
+    }
+
+    protected ObjectName getObjectName() {
+        return objectName;
+    }
+
+    protected final void initJMX(final String jmxHost, final int jmxPort, final String jmxLogin, final String jmxPassword) throws IOException, MalformedObjectNameException, NullPointerException {
+        final JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + jmxHost + ":" + jmxPort + "/server");
+
+        final Map<String, Object> environment;
+        if (jmxLogin == null || jmxPassword == null) {
+            environment = null;
+        } else {
+            environment = new HashMap<String, Object>(1);
+            environment.put(JMXConnectorServer.AUTHENTICATOR, new AbstractConsoleJMXAuthenticator(new String[] { jmxLogin, jmxPassword }));
+        }
+
+        jmxConnector = JMXConnectorFactory.connect(url, environment);
+
+        mBeanServerConnection = jmxConnector.getMBeanServerConnection();
+
+        objectName = ObjectName.getInstance("com.openexchange.control", "name", "Control");
+    }
+
+}
