a63b7f58bc40789f62e9df45e55ffb83d678aef1
francisco <francisco>
2010-02-11 16:26:43 +0000

    Initial Drop CacheingMessageAccess. Now all that remains is integration with cacheing bundle

---
+++ com.openexchange.messaging.json/src/com/openexchange/messaging/json/cacheing/CacheingMessageAccess.java
@@ -0,0 +1,181 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2010 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.messaging.json.cacheing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import com.openexchange.messaging.IndexRange;
+import com.openexchange.messaging.MessagingException;
+import com.openexchange.messaging.MessagingField;
+import com.openexchange.messaging.MessagingMessage;
+import com.openexchange.messaging.MessagingMessageAccess;
+import com.openexchange.messaging.OrderDirection;
+import com.openexchange.messaging.SearchTerm;
+
+/**
+ * {@link CacheingMessageAccess}
+ * 
+ * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
+ */
+public class CacheingMessageAccess implements MessagingMessageAccess {
+
+    private MessagingMessageAccess delegate;
+
+    public CacheingMessageAccess(MessagingMessageAccess delegate) {
+        this.delegate = delegate;
+    }
+
+    public void appendMessages(String folder, MessagingMessage[] messages) throws MessagingException {
+        delegate.appendMessages(folder, messages);
+    }
+
+    public List<String> copyMessages(String sourceFolder, String destFolder, String[] messageIds, boolean fast) throws MessagingException {
+        return delegate.copyMessages(sourceFolder, destFolder, messageIds, fast);
+    }
+
+    public void deleteMessages(String folder, String[] messageIds, boolean hardDelete) throws MessagingException {
+        delegate.deleteMessages(folder, messageIds, hardDelete);
+    }
+
+    public List<MessagingMessage> getAllMessages(String folder, IndexRange indexRange, MessagingField sortField, OrderDirection order, MessagingField... fields) throws MessagingException {
+        clear(folder);
+        return remember(delegate.getAllMessages(folder, indexRange, sortField, order, fields));
+    }
+
+    public MessagingMessage getMessage(String folder, String id, boolean peek) throws MessagingException {
+        MessagingMessage msg = get(folder, id);
+        if (msg != null) {
+            return msg;
+        }
+
+        return remember(delegate.getMessage(folder, id, peek));
+    }
+
+    public List<MessagingMessage> getMessages(String folder, String[] messageIds, MessagingField[] fields) throws MessagingException {
+        Map<String, MessagingMessage> allMessages = new HashMap<String, MessagingMessage>();
+        List<String> idsToLoad = new LinkedList<String>();
+        
+        for (String id : messageIds) {
+            MessagingMessage cached = get(folder, id);
+            if(cached != null) {
+                allMessages.put(id, cached);
+            } else {
+                idsToLoad.add(id);
+            }
+        }
+        
+        if(!idsToLoad.isEmpty()) {
+            List<MessagingMessage> messages = delegate.getMessages(folder, idsToLoad.toArray(new String[idsToLoad.size()]), fields);
+            remember(messages);
+            if(allMessages.isEmpty()) {
+                return messages;
+            }
+            for (MessagingMessage messagingMessage : messages) {
+                allMessages.put(messagingMessage.getId(), messagingMessage);
+            }
+            
+        }
+        
+        List<MessagingMessage> messages = new ArrayList<MessagingMessage>(messageIds.length);
+        for (String id : messageIds) {
+            messages.add(allMessages.get(id));
+        }
+        
+        return messages;
+    }
+
+    private List<MessagingMessage> remember(List<MessagingMessage> messages) {
+        for (MessagingMessage messagingMessage : messages) {
+            remember(messagingMessage);
+        }
+        return messages;
+    }
+
+    public List<String> moveMessages(String sourceFolder, String destFolder, String[] messageIds, boolean fast) throws MessagingException {
+        return delegate.moveMessages(sourceFolder, destFolder, messageIds, fast);
+    }
+
+    public MessagingMessage perform(MessagingMessage message, String action) throws MessagingException {
+        return delegate.perform(message, action);
+    }
+
+    public MessagingMessage perform(String folder, String id, String action) throws MessagingException {
+        return delegate.perform(folder, id, action);
+    }
+
+    public MessagingMessage perform(String action) throws MessagingException {
+        return delegate.perform(action);
+    }
+
+    public List<MessagingMessage> searchMessages(String folder, IndexRange indexRange, MessagingField sortField, OrderDirection order, SearchTerm<?> searchTerm, MessagingField[] fields) throws MessagingException {
+        return delegate.searchMessages(folder, indexRange, sortField, order, searchTerm, fields);
+    }
+
+    public void updateMessage(MessagingMessage message, MessagingField[] fields) throws MessagingException {
+        delegate.updateMessage(message, fields);
+    }
+
+    protected MessagingMessage get(String folder, String id) {
+        return null; // TODO
+    }
+
+    protected MessagingMessage remember(MessagingMessage message) {
+        return message; // TODO
+    }
+
+    protected void clear(String folderId) {
+        // TODO Auto-generated method stub
+        return;
+    }
+
+}
---
+++ com.openexchange.messaging.json/test/com/openexchange/messaging/json/cacheing/CacheingMessageAccessTest.java
@@ -0,0 +1,217 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2010 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.messaging.json.cacheing;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import com.openexchange.messaging.IndexRange;
+import com.openexchange.messaging.MessagingException;
+import com.openexchange.messaging.MessagingMessage;
+import com.openexchange.messaging.MessagingMessageAccess;
+import com.openexchange.messaging.SimMessageAccess;
+import com.openexchange.messaging.SimpleMessagingMessage;
+import junit.framework.TestCase;
+
+
+/**
+ * {@link CacheingMessageAccessTest}
+ *
+ * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
+ */
+public class CacheingMessageAccessTest extends TestCase {
+
+    public void testGetTriesCache() throws MessagingException {
+        SimMessageAccess access = new SimMessageAccess();
+        TestCacheMessageAccess messageAccess = new TestCacheMessageAccess(access);
+        
+        SimpleMessagingMessage message = new SimpleMessagingMessage();
+        messageAccess.setCached(message);
+        
+        MessagingMessage retval = messageAccess.getMessage("folder", "id", true);
+        
+        assertSame(message, retval);
+    }
+    
+    public void testBulkLoadingTriesCache() throws MessagingException {
+        SimMessageAccess access = new SimMessageAccess();
+        TestCacheMessageAccess messageAccess = new TestCacheMessageAccess(access);
+        
+        SimpleMessagingMessage message = new SimpleMessagingMessage();
+        messageAccess.setCached(message);
+        
+        List<MessagingMessage> messages = messageAccess.getMessages("folder", new String[]{"a", "b", "c"}, null);
+        
+        assertEquals(3, messages.size());
+        for (MessagingMessage messagingMessage : messages) {
+            assertSame(message, messagingMessage);
+        }
+        
+    }
+    
+    public void testGetFallbackOnCacheMiss() throws MessagingException {
+        SimMessageAccess access = new SimMessageAccess();
+        TestCacheMessageAccess messageAccess = new TestCacheMessageAccess(access);
+        
+        SimpleMessagingMessage message = new SimpleMessagingMessage();
+        access.setTemplateMessage(message);
+        
+        MessagingMessage retval = messageAccess.getMessage("folder", "id", true);
+        
+        assertSame(message, retval);
+        List<MessagingMessage> rememberedMessages = messageAccess.getRememberedMessages();
+        
+        assertEquals(1, rememberedMessages.size());
+        
+        assertSame(message, rememberedMessages.get(0));
+    }
+    
+    public void testBulkFallbackOnCacheMiss() throws MessagingException {
+        SimMessageAccess access = new SimMessageAccess();
+        TestCacheMessageAccess messageAccess = new TestCacheMessageAccess(access);
+        
+        SimpleMessagingMessage cachedMessage = new SimpleMessagingMessage();
+        SimpleMessagingMessage storedMessage = new SimpleMessagingMessage();
+        storedMessage.setId("b");
+
+        messageAccess.setCached(cachedMessage);
+        messageAccess.forgetCachedAfterFirstHit();
+        
+        access.setTemplateMessage(storedMessage);
+        
+        messageAccess.forgetCachedAfterFirstHit();
+        
+        List<MessagingMessage> messages = messageAccess.getMessages("folder", new String[]{"a", "b"}, null);
+        
+        assertEquals(2, messages.size());
+        
+        assertSame(cachedMessage, messages.get(0));
+        assertSame(storedMessage, messages.get(1));
+    }
+    
+    public void testAllRefreshesCache() throws MessagingException {
+        SimMessageAccess access = new SimMessageAccess();
+        TestCacheMessageAccess messageAccess = new TestCacheMessageAccess(access);
+        
+        SimpleMessagingMessage cachedMessage = new SimpleMessagingMessage();
+        SimpleMessagingMessage storedMessage = new SimpleMessagingMessage();
+
+        messageAccess.setCached(cachedMessage);
+        messageAccess.forgetCachedAfterFirstHit();
+        
+        access.setTemplateMessage(storedMessage);
+        
+        messageAccess.forgetCachedAfterFirstHit();
+        
+        List<MessagingMessage> messages = messageAccess.getAllMessages("folder", IndexRange.NULL, null, null, null);
+        
+        assertEquals(1, messages.size());
+        
+        assertSame(storedMessage, messages.get(0));
+        assertEquals("folder", messageAccess.getClearedFolder());
+    }
+    
+    private static final class TestCacheMessageAccess extends CacheingMessageAccess {
+
+        
+        private MessagingMessage cached;
+        private List<MessagingMessage> rememberedMessages = new LinkedList<MessagingMessage>();
+        private String folder;
+        private String id;
+        private boolean forgetAfterFirstHit;
+        private String clearedFolder;
+
+        public TestCacheMessageAccess(MessagingMessageAccess delegate) {
+            super(delegate);
+        }
+        
+        public void forgetCachedAfterFirstHit() {
+            this.forgetAfterFirstHit = true;
+        }
+
+        public List<MessagingMessage> getRememberedMessages() {
+            return rememberedMessages;
+        }
+        
+        public void setCached(MessagingMessage message) {
+            this.cached = message;
+        }
+        
+        protected MessagingMessage remember(MessagingMessage message) {
+            rememberedMessages.add( message );
+            return message;
+        }
+        
+        @Override
+        protected MessagingMessage get(String folder, String id) {
+            MessagingMessage retval = cached;
+            if(forgetAfterFirstHit) {
+                cached = null;
+            }
+            this.folder = folder;
+            this.id = id;
+            return retval;
+        }
+        
+        @Override
+        protected void clear(String folderId) {
+            this.clearedFolder = folderId;
+        }
+        
+        public String getClearedFolder() {
+            return clearedFolder;
+        }
+        
+    }
+    
+    
+    
+}
