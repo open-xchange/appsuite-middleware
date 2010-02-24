b41faf1013c31dc7ff408a5aba67d1f67718304b
dennis <dennis>
2010-02-24 11:06:57 +0000

    - added JavaDoc

--- com.openexchange.admin/src/com/openexchange/admin/plugins/OXContextPluginInterface.java
+++ com.openexchange.admin/src/com/openexchange/admin/plugins/OXContextPluginInterface.java
@@ -61,22 +61,98 @@ import com.openexchange.admin.rmi.extensions.OXCommonExtension;
  */
 public interface OXContextPluginInterface {
 
+    /**
+     * Define the operations which should be done before the real change process.
+     * 
+     * @param ctx
+     * @param auth
+     * @throws PluginException
+     */
     public void change(final Context ctx, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context creation process.
+     * 
+     * @param ctx 
+     * @param admin_user
+     * @param auth
+     * @return An {@link Context} object must be returned. If nothing is done
+     *         in the implementing method, the context object given in
+     *         <code>ctx</code> should be returned.
+     * @throws PluginException
+     */
     public Context preCreate(final Context ctx, final User admin_user, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done after the real context creation process.
+     * 
+     * @param ctx
+     * @param admin_user
+     * @param access
+     * @param auth
+     * @return An {@link Context} object must be returned. If nothing is done
+     *         in the implementing method, the context object given in
+     *         <code>ctx</code> should be returned.
+     * @throws PluginException
+     */
     public Context postCreate(final Context ctx, final User admin_user, final UserModuleAccess access, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context delete process.
+     * 
+     * @param ctx
+     * @param auth
+     * @throws PluginException
+     */
     public void delete(final Context ctx, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context disable process.
+     * 
+     * @param ctx
+     * @param auth
+     * @throws PluginException
+     */
     public void disable(final Context ctx, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context disableAll process.
+     * 
+     * @param auth
+     * @throws PluginException
+     */
     public void disableAll(final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context enable process.
+     * 
+     * @param ctx
+     * @param auth
+     * @throws PluginException
+     */
     public void enable(final Context ctx, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done instead of the enableAll
+     * process if the credentials given are those from the master admin
+     * 
+     * @param auth
+     * @throws PluginException
+     */
     public void enableAll(final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done after the real context
+     * getData process.
+     * Note that the real context getData process will only be called if
+     * the context object wasn't filled by a list operation before.
+     * 
+     * @param ctx
+     * @param auth
+     * @return A {@link List} containing {@link OXCommonExtension} objects,
+     *         this can be null.
+     * @throws PluginException
+     */
     public List<OXCommonExtension> getData(final List<Context> ctx, final Credentials auth) throws PluginException;
 
     /**
@@ -91,15 +167,66 @@ public interface OXContextPluginInterface {
      */
     public SQLQueryExtension list(final String search_pattern, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context
+     * changeModuleAccess process.
+     * 
+     * @param ctx
+     * @param access
+     * @param auth
+     * @throws PluginException
+     */
     public void changeModuleAccess(final Context ctx, final UserModuleAccess access,final Credentials auth) throws PluginException; 
 
+    /**
+     * Define the operations which should be done before the real context
+     * changeModuleAccess process.
+     *
+     * @param ctx
+     * @param access_combination_name
+     * @param auth
+     * @throws PluginException
+     */
     public void changeModuleAccess(final Context ctx, final String access_combination_name,final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context
+     * downgrade process.
+     * 
+     * @param ctx
+     * @param auth
+     * @throws PluginException
+     */
     public void downgrade(final Context ctx, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context
+     * getAccessCombinationName process.
+     * 
+     * @param ctx
+     * @param auth
+     * @return Currently not used, so can be null
+     * @throws PluginException
+     */
     public String getAccessCombinationName(final Context ctx, final Credentials auth) throws PluginException;
 
+    /**
+     * Define the operations which should be done before the real context
+     * getModuleAccess process.
+     * 
+     * @param ctx
+     * @param auth
+     * @return Currently not used, so can be null
+     * @throws PluginException
+     */
     public UserModuleAccess getModuleAccess(final Context ctx, final Credentials auth) throws PluginException;
     
+    /**
+     * Define if the mandatory members for a context create should be checked or not
+     * 
+     * @param ctx
+     * @return If null or false are set, the check is ommitted otherwise the check is made
+     * @throws PluginException
+     */
     public Boolean checkMandatoryMembersContextCreate(final Context ctx) throws PluginException;
 }
