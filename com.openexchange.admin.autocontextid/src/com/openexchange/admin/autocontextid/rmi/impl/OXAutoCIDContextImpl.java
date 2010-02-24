107692d9a338bb72c30054916cc59f933acfd134
dennis <dennis>
2010-02-24 18:29:30 +0000

    - now returning an empty list in the getData method

--- com.openexchange.admin.autocontextid/src/com/openexchange/admin/autocontextid/rmi/impl/OXAutoCIDContextImpl.java
+++ com.openexchange.admin.autocontextid/src/com/openexchange/admin/autocontextid/rmi/impl/OXAutoCIDContextImpl.java
@@ -49,6 +49,7 @@
 
 package com.openexchange.admin.autocontextid.rmi.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.openexchange.admin.autocontextid.storage.interfaces.OXAutoCIDStorageInterface;
 import com.openexchange.admin.plugins.OXContextPluginInterface;
@@ -184,7 +185,7 @@ public class OXAutoCIDContextImpl implements OXContextPluginInterface {
      * com.openexchange.admin.rmi.dataobjects.Credentials)
      */
     public List<OXCommonExtension> getData(final List<Context> ctxs, final Credentials auth) throws PluginException {
-        return null;
+        return new ArrayList<OXCommonExtension>();
     }
 
     /*
