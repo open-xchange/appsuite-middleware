4eec4d4a47b40d6c7a99dfea86ddf0e9e0ab025f
dennis <dennis>
2010-02-24 19:43:12 +0000

    - changed id generation table

--- com.openexchange.admin.autocontextid/etc/mysql/autocid.sql
+++ com.openexchange.admin.autocontextid/etc/mysql/autocid.sql
@@ -1,7 +1,7 @@
 CREATE TABLE context_sequence (
-    id INT4 UNSIGNED AUTO_INCREMENT,
+    id INT4 UNSIGNED,
     PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO context_sequence VALUES (0);
 
@@ -9,14 +9,7 @@ DROP PROCEDURE IF EXISTS get_context_id;
 DELIMITER //
 CREATE PROCEDURE get_context_id() NOT DETERMINISTIC MODIFIES SQL DATA
 BEGIN
-    DECLARE identifier INT4 UNSIGNED;
-    SET identifier = 0;
-    SELECT id INTO identifier FROM context_sequence FOR UPDATE;
-    IF 0 = identifier THEN
-        INSERT INTO context_sequence (id) VALUES (identifier);
-    END IF;
-    SET identifier = identifier + 1;
-    UPDATE context_sequence SET id = identifier;
-    SELECT identifier;
+    UPDATE context_sequence SET id=id+1;
+    SELECT id FROM context_sequence;
 END//
 DELIMITER ;
