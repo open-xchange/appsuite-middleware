6f0cd9390c2c1667338559c5ef7f3f3e17fec6f0
dennis <dennis>
2010-02-24 17:56:27 +0000

    - removed reseller name

--- com.openexchange.admin.autocontextid/etc/mysql/autocid.sql
+++ com.openexchange.admin.autocontextid/etc/mysql/autocid.sql
@@ -3,20 +3,20 @@ CREATE TABLE context_sequence (
     PRIMARY KEY (id)
 );
 
-INSERT INTO reseller_context_sequence VALUES (0);
+INSERT INTO context_sequence VALUES (0);
 
-DROP PROCEDURE IF EXISTS get_reseller_context_id;
+DROP PROCEDURE IF EXISTS get_context_id;
 DELIMITER //
-CREATE PROCEDURE get_reseller_context_id() NOT DETERMINISTIC MODIFIES SQL DATA
+CREATE PROCEDURE get_context_id() NOT DETERMINISTIC MODIFIES SQL DATA
 BEGIN
     DECLARE identifier INT4 UNSIGNED;
     SET identifier = 0;
-    SELECT id INTO identifier FROM reseller_context_sequence FOR UPDATE;
+    SELECT id INTO identifier FROM context_sequence FOR UPDATE;
     IF 0 = identifier THEN
-        INSERT INTO reseller_context_sequence (id) VALUES (identifier);
+        INSERT INTO context_sequence (id) VALUES (identifier);
     END IF;
     SET identifier = identifier + 1;
-    UPDATE reseller_context_sequence SET id = identifier;
+    UPDATE context_sequence SET id = identifier;
     SELECT identifier;
 END//
 DELIMITER ;
