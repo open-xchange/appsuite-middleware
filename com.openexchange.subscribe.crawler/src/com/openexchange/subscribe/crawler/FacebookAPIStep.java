d62bfd27cd6c884623826027b47418dab736f622
karsten <karsten>
2010-03-10 08:37:59 +0000

    fix for [Bug 14457] Facebook crawler throws ConverterException

--- com.openexchange.subscribe.crawler/src/com/openexchange/subscribe/crawler/FacebookAPIStep.java
+++ com.openexchange.subscribe.crawler/src/com/openexchange/subscribe/crawler/FacebookAPIStep.java
@@ -233,7 +233,7 @@ public class FacebookAPIStep extends AbstractStep<Contact[], Object> implements 
                     try {
                         OXContainerConverter.loadImageFromURL(contact, user.getPic().getValue());
                     } catch (final ConverterException e) {
-                        LOG.error(e.getMessage(), e);
+                        LOG.error("No valid picture could be found at this URL : " + user.getPic().getValue());
                     }
                 }
                 contactObjects.add(contact);
