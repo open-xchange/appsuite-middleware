a165bbf86a3291d207e62c2e110e9cace50d26cb
marcus <marcus>
2010-03-31 13:57:47 +0000

    whitespace

--- server/src/com/openexchange/groupware/tx/RequestDBProvider.java
+++ server/src/com/openexchange/groupware/tx/RequestDBProvider.java
@@ -67,305 +67,303 @@ import com.openexchange.groupware.contexts.Context;
 @OXExceptionSource(classId=Classes.COM_OPENEXCHANGE_GROUPWARE_TX_REQUESTDBPROVIDER, component= EnumComponent.TRANSACTION)
 
 @OXThrowsMultiple(
-		category={Category.SUBSYSTEM_OR_SERVICE_DOWN,Category.SUBSYSTEM_OR_SERVICE_DOWN,Category.SUBSYSTEM_OR_SERVICE_DOWN},
-		desc={"","",""},
-		exceptionId={0,1,2},
-		msg={"Cannot commit transaction to write DB", "Cannot rollback transaction in write DB", "Cannot finish transaction"}
+        category={Category.SUBSYSTEM_OR_SERVICE_DOWN,Category.SUBSYSTEM_OR_SERVICE_DOWN,Category.SUBSYSTEM_OR_SERVICE_DOWN},
+        desc={"","",""},
+        exceptionId={0,1,2},
+        msg={"Cannot commit transaction to write DB", "Cannot rollback transaction in write DB", "Cannot finish transaction"}
 )
 public class RequestDBProvider implements DBProvider {
 
-	private static final ThreadLocal<DBTransaction> txIds = new ThreadLocal<DBTransaction>();
+    private static final ThreadLocal<DBTransaction> txIds = new ThreadLocal<DBTransaction>();
 
-	private static final Log LOG = LogFactory.getLog(RequestDBProvider.class);
-	private static final TXExceptionFactory EXCEPTIONS = new TXExceptionFactory(RequestDBProvider.class);
+    private static final Log LOG = LogFactory.getLog(RequestDBProvider.class);
+    private static final TXExceptionFactory EXCEPTIONS = new TXExceptionFactory(RequestDBProvider.class);
     private boolean commits = true;
 
 
     public static class DBTransaction {
-		public Connection writeConnection;
-		public Connection readConnection;
-		public Context ctx;
-		public boolean transactional;
-		
-		List<Throwable> readConnectionStacks = new ArrayList<Throwable>();
-		public boolean autoCommit;
+        public Connection writeConnection;
+        public Connection readConnection;
+        public Context ctx;
+        public boolean transactional;
+        
+        List<Throwable> readConnectionStacks = new ArrayList<Throwable>();
+        public boolean autoCommit;
 
         public boolean commit = true;
 
         public DBTransaction() {
-			for(int i = 0; i < 10; i++) {
-				readConnectionStacks.add(null);
-			}
-		}
-	}
-	
-	private final ThreadLocal<Integer> readCount = new ThreadLocal<Integer>(){
+            for(int i = 0; i < 10; i++) {
+                readConnectionStacks.add(null);
+            }
+        }
+    }
+    
+    private final ThreadLocal<Integer> readCount = new ThreadLocal<Integer>(){
 
-		@Override
-		protected final Integer initialValue() {
-			return 0;
-		}
-		
-	};
-	
-		
-	public RequestDBProvider(){}
-	
-	public RequestDBProvider(final DBProvider provider) {
-		setProvider(provider);
-	}
-	
-	private boolean transactional;
-	
-	private DBProvider provider;
+        @Override
+        protected final Integer initialValue() {
+            return 0;
+        }
+        
+    };
+    
+        
+    public RequestDBProvider(){}
+    
+    public RequestDBProvider(final DBProvider provider) {
+        setProvider(provider);
+    }
+    
+    private boolean transactional;
+    
+    private DBProvider provider;
 
-	public DBProvider getProvider() {
-		return this.provider;
-	}
-	
-	public void setProvider(final DBProvider provider){
-		this.provider = provider;
-	}
+    public DBProvider getProvider() {
+        return this.provider;
+    }
+    
+    public void setProvider(final DBProvider provider){
+        this.provider = provider;
+    }
 
-	
-	// Service
-	
-	public void startTransaction() throws TransactionException {
-		final DBTransaction tx = createTransaction();
-		tx.transactional = transactional;
-		tx.commit = commits;
+    
+    // Service
+    
+    public void startTransaction() throws TransactionException {
+        final DBTransaction tx = createTransaction();
+        tx.transactional = transactional;
+        tx.commit = commits;
         txIds.set(tx);
-	}
-	
-	public void commit() throws TransactionException{
-		checkThreadDeath();
-		commit(getActiveTransaction());
-	}
-	
-	private void checkThreadDeath() {
-		final Thread current = Thread.currentThread();
-		if (current instanceof AJPv13ListenerThread) {
-			final AJPv13ListenerThread ajpv13Thread = (AJPv13ListenerThread) current;
-			if(ajpv13Thread.isDead()) {
-				try {
-					rollback();
-				} catch (final TransactionException x) {
-					LOG.debug("",x);
-				}
-				try {
-					finish();
-				} catch (final TransactionException x) {
-					LOG.debug("",x);
-				}
-				
-				throw new ThreadDeath();
-				
-			}
-		}
-	}
+    }
+    
+    public void commit() throws TransactionException{
+        checkThreadDeath();
+        commit(getActiveTransaction());
+    }
+    
+    private void checkThreadDeath() {
+        final Thread current = Thread.currentThread();
+        if (current instanceof AJPv13ListenerThread) {
+            final AJPv13ListenerThread ajpv13Thread = (AJPv13ListenerThread) current;
+            if(ajpv13Thread.isDead()) {
+                try {
+                    rollback();
+                } catch (final TransactionException x) {
+                    LOG.debug("",x);
+                }
+                try {
+                    finish();
+                } catch (final TransactionException x) {
+                    LOG.debug("",x);
+                }
+                
+                throw new ThreadDeath();
+                
+            }
+        }
+    }
 
-	public void rollback() throws TransactionException{
-		rollback(getActiveTransaction());
-	}
-	
-	protected DBTransaction getActiveTransaction(){
-		return txIds.get();
-	}
-	
-	// Abstract Service
-	
-	
-	protected DBTransaction createTransaction() {
-		final DBTransaction tx = new DBTransaction();
-		return tx;
-	}
+    public void rollback() throws TransactionException{
+        rollback(getActiveTransaction());
+    }
+    
+    protected DBTransaction getActiveTransaction(){
+        return txIds.get();
+    }
+    
+    // Abstract Service
+    
+    
+    protected DBTransaction createTransaction() {
+        final DBTransaction tx = new DBTransaction();
+        return tx;
+    }
 
-	protected void commit(final DBTransaction tx) throws TransactionException {
-		try {
-			if (tx.writeConnection != null && !tx.writeConnection.getAutoCommit()) {
-				if(tx.commit) {
+    protected void commit(final DBTransaction tx) throws TransactionException {
+        try {
+            if (tx.writeConnection != null && !tx.writeConnection.getAutoCommit()) {
+                if(tx.commit) {
                     tx.writeConnection.commit();                        
                 }
             }
-		} catch (final SQLException e) {
-			throw EXCEPTIONS.create(0,e);
-		}
-	}
+        } catch (final SQLException e) {
+            throw EXCEPTIONS.create(0,e);
+        }
+    }
 
-	protected void rollback(final DBTransaction tx) throws TransactionException {
-		if(tx == null) {
-			return;
-		}
-		try {
-			if(tx.writeConnection!=null) {
-				if(tx.writeConnection.getAutoCommit()) {
-					throw new IllegalStateException("This request cannot be rolled back because it wasn't part of a transaction");
-				}
-				tx.writeConnection.rollback();
-			}
-		} catch (final SQLException e) {
-			throw EXCEPTIONS.create(1,e);
-		}
-	}
-	
-	public Connection getReadConnection(final Context ctx) throws TransactionException{
-		checkThreadDeath();
-		final DBTransaction tx = getActiveTransaction();
-		int rc = readCount.get();
-		if(tx != null && tx.ctx == null) {
-			tx.ctx = ctx;
-		}
-		if(tx != null && tx.writeConnection != null) {
-			return tx.writeConnection;
-		}
-		if(tx != null && tx.readConnection != null) {
-			final Throwable t = new Throwable();
-			rc++;
-			t.fillInStackTrace();
-			tx.readConnectionStacks.set(rc, t);
-			readCount.set(rc);
-			return tx.readConnection;
-		}
-		
-		final Connection readCon = getProvider().getReadConnection(ctx);
-		if(LOG.isDebugEnabled()) {
-			LOG.debug("---> "+readCon);
-		}
-		if(tx != null) {
-			tx.readConnection = readCon;
-			rc++;
-			final Throwable t = new Throwable();
-			t.fillInStackTrace();
-			tx.readConnectionStacks.set(rc, t);
-			readCount.set(rc);
-		}
-		return readCon;
-	}
-	
-	public Connection getWriteConnection(final Context ctx) throws TransactionException{
-		checkThreadDeath();
-		final DBTransaction tx = getActiveTransaction();
-		if(tx == null) {
-			return getProvider().getWriteConnection(ctx);
-		}
-		final int rc = readCount.get();
-		if(rc>0) {
-			throw new IllegalStateException("Don't use a read and write connection in parallel. Read Connections in use: "+rc, tx.readConnectionStacks.get(rc));
-		}
-		if(tx.writeConnection != null) {
-			return tx.writeConnection;
-		}
-		tx.writeConnection = getProvider().getWriteConnection(ctx);
-		try {
-			tx.autoCommit = tx.writeConnection.getAutoCommit();
-			if(tx.writeConnection.getAutoCommit() == tx.transactional) {
-				tx.writeConnection.setAutoCommit(!tx.transactional);
-			}
-		} catch (final SQLException e) {
-			return null;
-		}
-		if(tx != null && tx.ctx == null) {
-			tx.ctx = ctx;
-		}
-		return tx.writeConnection;
-	}
-	
-	public void releaseReadConnection(final Context ctx, final Connection con){
-		final DBTransaction tx = getActiveTransaction();
-		//if(tx == null) {
-		//	throw new IllegalStateException("There is no transaction active at the moment.");
-		//}
-		if(tx != null && tx.writeConnection != null && tx.writeConnection.equals(con)) {
-			return;
-		}
-		if(tx != null && tx.readConnection != null && tx.readConnection.equals(con)) {
-			int rc = readCount.get();
-			rc--;
-			if(rc==0) {
-				if(LOG.isDebugEnabled()) {
-					LOG.debug("<--- "+con);
-				}
-				getProvider().releaseReadConnection(ctx,con);
-				tx.readConnection=null;
-			}
-			readCount.set(rc);
-			return;
-		}
-		if(tx != null) {
-			return;
-		}
-		if(LOG.isDebugEnabled()) {
-			LOG.debug("<--- "+con);
-		}
-		getProvider().releaseReadConnection(ctx,con);
-		
-	}
-	
-	public void releaseWriteConnection(final Context ctx, final Connection con){
-		final DBTransaction tx = getActiveTransaction();
-		if(tx == null) {
-			getProvider().releaseWriteConnection(ctx, con);
-		}
-	}
-	
-	public void finish() throws TransactionException {
-		final DBTransaction tx = getActiveTransaction();
-		if(tx == null) {
-			return;
-		}
-		try {
-			if (tx.writeConnection != null) {
-				if(tx.writeConnection.getAutoCommit() != tx.autoCommit) {
-					tx.writeConnection.setAutoCommit(tx.autoCommit);
-				}
-				getProvider().releaseWriteConnection(tx.ctx,tx.writeConnection);
+    protected void rollback(final DBTransaction tx) throws TransactionException {
+        if(tx == null) {
+            return;
+        }
+        try {
+            if(tx.writeConnection!=null) {
+                if(tx.writeConnection.getAutoCommit()) {
+                    throw new IllegalStateException("This request cannot be rolled back because it wasn't part of a transaction");
+                }
+                tx.writeConnection.rollback();
+            }
+        } catch (final SQLException e) {
+            throw EXCEPTIONS.create(1,e);
+        }
+    }
+    
+    public Connection getReadConnection(final Context ctx) throws TransactionException{
+        checkThreadDeath();
+        final DBTransaction tx = getActiveTransaction();
+        int rc = readCount.get();
+        if(tx != null && tx.ctx == null) {
+            tx.ctx = ctx;
+        }
+        if(tx != null && tx.writeConnection != null) {
+            return tx.writeConnection;
+        }
+        if(tx != null && tx.readConnection != null) {
+            final Throwable t = new Throwable();
+            rc++;
+            t.fillInStackTrace();
+            tx.readConnectionStacks.set(rc, t);
+            readCount.set(rc);
+            return tx.readConnection;
+        }
+        
+        final Connection readCon = getProvider().getReadConnection(ctx);
+        if(LOG.isDebugEnabled()) {
+            LOG.debug("---> "+readCon);
+        }
+        if(tx != null) {
+            tx.readConnection = readCon;
+            rc++;
+            final Throwable t = new Throwable();
+            t.fillInStackTrace();
+            tx.readConnectionStacks.set(rc, t);
+            readCount.set(rc);
+        }
+        return readCon;
+    }
+    
+    public Connection getWriteConnection(final Context ctx) throws TransactionException{
+        checkThreadDeath();
+        final DBTransaction tx = getActiveTransaction();
+        if(tx == null) {
+            return getProvider().getWriteConnection(ctx);
+        }
+        final int rc = readCount.get();
+        if(rc>0) {
+            throw new IllegalStateException("Don't use a read and write connection in parallel. Read Connections in use: "+rc, tx.readConnectionStacks.get(rc));
+        }
+        if(tx.writeConnection != null) {
+            return tx.writeConnection;
+        }
+        tx.writeConnection = getProvider().getWriteConnection(ctx);
+        try {
+            tx.autoCommit = tx.writeConnection.getAutoCommit();
+            if(tx.writeConnection.getAutoCommit() == tx.transactional) {
+                tx.writeConnection.setAutoCommit(!tx.transactional);
+            }
+        } catch (final SQLException e) {
+            return null;
+        }
+        if(tx != null && tx.ctx == null) {
+            tx.ctx = ctx;
+        }
+        return tx.writeConnection;
+    }
+    
+    public void releaseReadConnection(final Context ctx, final Connection con){
+        final DBTransaction tx = getActiveTransaction();
+        //if(tx == null) {
+        //    throw new IllegalStateException("There is no transaction active at the moment.");
+        //}
+        if(tx != null && tx.writeConnection != null && tx.writeConnection.equals(con)) {
+            return;
+        }
+        if(tx != null && tx.readConnection != null && tx.readConnection.equals(con)) {
+            int rc = readCount.get();
+            rc--;
+            if(rc==0) {
+                if(LOG.isDebugEnabled()) {
+                    LOG.debug("<--- "+con);
+                }
+                getProvider().releaseReadConnection(ctx,con);
+                tx.readConnection=null;
+            }
+            readCount.set(rc);
+            return;
+        }
+        if(tx != null) {
+            return;
+        }
+        if(LOG.isDebugEnabled()) {
+            LOG.debug("<--- "+con);
+        }
+        getProvider().releaseReadConnection(ctx,con);
+        
+    }
+    
+    public void releaseWriteConnection(final Context ctx, final Connection con){
+        final DBTransaction tx = getActiveTransaction();
+        if(tx == null) {
+            getProvider().releaseWriteConnection(ctx, con);
+        }
+    }
+    
+    public void finish() throws TransactionException {
+        final DBTransaction tx = getActiveTransaction();
+        if(tx == null) {
+            return;
+        }
+        try {
+            if (tx.writeConnection != null) {
+                if(tx.writeConnection.getAutoCommit() != tx.autoCommit) {
+                    tx.writeConnection.setAutoCommit(tx.autoCommit);
+                }
+                getProvider().releaseWriteConnection(tx.ctx,tx.writeConnection);
                 tx.writeConnection = null;
-			}
-			if (tx.readConnection != null) {
-				getProvider().releaseReadConnection(tx.ctx,tx.readConnection);
-				readCount.set(0);
-				tx.readConnection = null;
-			}
-		} catch (final SQLException e) {
-			throw EXCEPTIONS.create(2,e);
-		}
-		txIds.set(null);
-		final DBProvider prov = getProvider();
-		if(prov instanceof Closeable) {
-			try {
-				((Closeable)prov).close();
-			} catch (final IOException e) {
-				LOG.debug("",e);
-			}
-		}
-	}
-	
-	public void setTransactional(final boolean transactional) {
-		this.transactional = transactional;
-	}
-	
-	public boolean isTransactional(){
-		final DBTransaction tx = getActiveTransaction();
-		if(tx != null && tx.transactional) {
-			return true;
-		}
-		return this.transactional;
-	}
-	
-	public void setRequestTransactional(final boolean transactional) {
-		final DBTransaction tx = getActiveTransaction();
-		if(tx == null) {
-			throw new IllegalStateException("No Transaction Active");
-		}
-		if(tx.writeConnection != null && transactional) {
-			throw new IllegalStateException("Cannot switch on transaction after a write occurred");
-		}
-		tx.transactional = transactional;
-	}
-
+            }
+            if (tx.readConnection != null) {
+                getProvider().releaseReadConnection(tx.ctx,tx.readConnection);
+                readCount.set(0);
+                tx.readConnection = null;
+            }
+        } catch (final SQLException e) {
+            throw EXCEPTIONS.create(2,e);
+        }
+        txIds.set(null);
+        final DBProvider prov = getProvider();
+        if(prov instanceof Closeable) {
+            try {
+                ((Closeable)prov).close();
+            } catch (final IOException e) {
+                LOG.debug("",e);
+            }
+        }
+    }
+    
+    public void setTransactional(final boolean transactional) {
+        this.transactional = transactional;
+    }
+    
+    public boolean isTransactional(){
+        final DBTransaction tx = getActiveTransaction();
+        if(tx != null && tx.transactional) {
+            return true;
+        }
+        return this.transactional;
+    }
+    
+    public void setRequestTransactional(final boolean transactional) {
+        final DBTransaction tx = getActiveTransaction();
+        if(tx == null) {
+            throw new IllegalStateException("No Transaction Active");
+        }
+        if(tx.writeConnection != null && transactional) {
+            throw new IllegalStateException("Cannot switch on transaction after a write occurred");
+        }
+        tx.transactional = transactional;
+    }
 
     public void setCommitsTransaction(final boolean commits) {
         this.commits = commits;
     }
-
 }
