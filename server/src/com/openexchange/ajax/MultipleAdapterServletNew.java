32e30480097607e9725cb33fb03dd0a840ed77b1
marcus <marcus>
2010-04-06 13:28:52 +0000

    fixed not commited changes

--- server/src/com/openexchange/ajax/MultipleAdapterServletNew.java
+++ server/src/com/openexchange/ajax/MultipleAdapterServletNew.java
@@ -138,7 +138,7 @@ public abstract class MultipleAdapterServletNew extends PermissionServlet {
      *            HTTP POST method); otherwise <code>false</code> to generate an appropriate {@link Object} from request's body
      * @throws IOException If an I/O error occurs
      */
-    protected final void handle(final HttpServletRequest req, final HttpServletResponse resp, final boolean preferStream) throws IOException {
+    protected final void handle(final HttpServletRequest req, final HttpServletResponse resp, final boolean preferStream) throws IOException, ServletException {
         final Response response = new Response();
         try {
             final String action = req.getParameter(PARAMETER_ACTION);
@@ -180,11 +180,11 @@ public abstract class MultipleAdapterServletNew extends PermissionServlet {
      * @param resp The HTTP response object
      * @return
      */
-    protected boolean handleIndividually(String action, HttpServletRequest req, HttpServletResponse resp) {
+    protected boolean handleIndividually(String action, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         return false;
     }
 
-    private AJAXRequestData parseRequest(final HttpServletRequest req, final boolean preferStream) throws IOException {
+    protected AJAXRequestData parseRequest(final HttpServletRequest req, final boolean preferStream) throws IOException {
         final AJAXRequestData retval = new AJAXRequestData();
         retval.setSecure(req.isSecure());
         /*
