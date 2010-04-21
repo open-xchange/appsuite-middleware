b0835ab9ee3b0373190e4e886754da2575a9eb43
karsten <karsten>
2010-04-21 08:55:47 +0000

    corrected testname

--- com.openexchange.subscribe.crawler/test/com/openexchange/subscribe/crawler/TOnlineDETEst.java
+++ com.openexchange.subscribe.crawler/test/com/openexchange/subscribe/crawler/TOnlineDETEst.java
@@ -56,7 +56,7 @@ package com.openexchange.subscribe.crawler;
  * @author <a href="mailto:karsten.will@open-xchange.com">Karsten Will</a>
  */
 public class TOnlineDETEst extends GenericSubscribeServiceTestHelpers{
-    public void testGMX(){
+    public void testTOnlineDE(){
         checkSingleCrawler("t-online.de");
     }
 
