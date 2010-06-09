ea8295e212223400bfadfdc82708ad843187d904
Tobias Prinz <tobias.prinz@open-xchange.com>
2010-06-09 10:55:52 +0000

    changed reading from file to string

--- openexchange-test/src/com/openexchange/ajax/importexport/Bug15229Test.java
+++ openexchange-test/src/com/openexchange/ajax/importexport/Bug15229Test.java
@@ -21,13 +21,32 @@ public class Bug15229Test extends AbstractVCardImportTest {
      * Test escaped colons in URLs
      */
     public void testColons() throws TestException, SAXException, JSONException, Exception {
-        InputStream is = new FileInputStream("testData/Bug15229.vcf");
+        String bug = "BEGIN:VCARD\n" + 
+            "\n" + 
+            "VERSION:3.0\n" + 
+            "\n" + 
+            "N:Lšfflad;Klaus;(piraten);;\n" + 
+            "\n" + 
+            "FN:Klaus (piraten) Lšfflad\n" + 
+            "\n" + 
+            "EMAIL;type=INTERNET;type=HOME;type=pref:klaus@der-kapitaen.de\n" + 
+            "\n" + 
+            "TEL;type=CELL;type=pref:+49 151 22632571\n" + 
+            "\n" + 
+            "item1.URL;type=pref:http\\://wiki.piratenpartei.de/Benutzer\\:Magister_Navis\n" + 
+            "\n" + 
+            "item1.X-ABLabel:Piraten\n" + 
+            "\n" + 
+            "END:VCARD\n" + 
+             "\n";
+        
+        InputStream is = new ByteArrayInputStream(bug.getBytes());
         byte[] buf = new byte[1024];
         is.read(buf);
         List<String> folders = new ArrayList<String>(); 
         
 
-         ImportResult[] importResult = importVCard(
+        ImportResult[] importResult = importVCard(
                 getWebConversation(),
                 new ByteArrayInputStream(buf),
                 contactFolderId,
@@ -36,8 +55,6 @@ public class Bug15229Test extends AbstractVCardImportTest {
                 getHostName(),
                 getSessionId());
 
-        assertNull("Parsing Exception", importResult[0].getException());
-        
+        assertNull("Parsing Exception", importResult[0].getException());   
     }
-
 }
