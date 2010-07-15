e3b30f4bc1df36383ff2272596f79145e41cd42b
marcus <marcus>
2010-07-15 11:36:10 +0000

    use enough session containers to give finders a chance to run

--- sessiond/test/com/openexchange/sessiond/impl/Bug16158Test.java
+++ sessiond/test/com/openexchange/sessiond/impl/Bug16158Test.java
@@ -78,7 +78,7 @@ public class Bug16158Test extends TestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        sessionData = new SessionData(100, 1);
+        sessionData = new SessionData(1000, 1);
         threadPoolService = new SimThreadPoolService();
         sessionData.addThreadPoolService(threadPoolService);
         SessionIdGenerator idGenerator = new UUIDSessionIdGenerator();
