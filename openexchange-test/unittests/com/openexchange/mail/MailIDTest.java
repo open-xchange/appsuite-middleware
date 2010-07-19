4646acf045515315a002b311d3201188e3b6dc86
Marcus Klein <marcus.klein@open-xchange.com>
2010-07-19 08:30:51 +0000

    fixed whitespace

--- openexchange-test/unittests/com/openexchange/mail/MailIDTest.java
+++ openexchange-test/unittests/com/openexchange/mail/MailIDTest.java
@@ -66,168 +66,160 @@ import com.openexchange.sessiond.impl.SessionObject;
  * {@link MailIDTest}
  * 
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
- * 
  */
 public final class MailIDTest extends AbstractMailTest {
 
-	private static final String INBOX = "INBOX";
+    private static final String INBOX = "INBOX";
 
-	private static final String TEMP_FOLDER = "TempFolder";
+    private static final String TEMP_FOLDER = "TempFolder";
 
-	/**
-	 * 
-	 */
-	public MailIDTest() {
-		super();
-	}
+    public MailIDTest() {
+        super();
+    }
 
-	/**
-	 * @param name
-	 */
-	public MailIDTest(final String name) {
-		super(name);
-	}
+    public MailIDTest(final String name) {
+        super(name);
+    }
 
-	public void testMailID() throws MailException, MessagingException, IOException {
-			final SessionObject session = getSession();
-			final MailMessage[] mails = getMessages(getTestMailDir(), -1);
+    public void testMailID() throws MailException, MessagingException, IOException {
+        final SessionObject session = getSession();
+        final MailMessage[] mails = getMessages(getTestMailDir(), -1);
 
-			final MailAccess<?, ?> mailAccess = MailAccess.getInstance(session);
-			mailAccess.connect();
-			try {
-				/*
-				 * Create a new folder
-				 */
-				String fullname = null;
-				String parentFullname = null;
-				{
-					final MailFolder inbox = mailAccess.getFolderStorage().getFolder(INBOX);
-					if (inbox.isHoldsFolders()) {
-						fullname = new StringBuilder(inbox.getFullname()).append(inbox.getSeparator()).append(
-								TEMP_FOLDER).toString();
-						parentFullname = INBOX;
-					} else {
-						fullname = TEMP_FOLDER;
-						parentFullname = MailFolder.DEFAULT_FOLDER_ID;
-					}
+        final MailAccess<?, ?> mailAccess = MailAccess.getInstance(session);
+        mailAccess.connect();
+        try {
+            /*
+             * Create a new folder
+             */
+            String fullname = null;
+            String parentFullname = null;
+            {
+                final MailFolder inbox = mailAccess.getFolderStorage().getFolder(INBOX);
+                if (inbox.isHoldsFolders()) {
+                    fullname = new StringBuilder(inbox.getFullname()).append(inbox.getSeparator()).append(
+                            TEMP_FOLDER).toString();
+                    parentFullname = INBOX;
+                } else {
+                    fullname = TEMP_FOLDER;
+                    parentFullname = MailFolder.DEFAULT_FOLDER_ID;
+                }
 
-					final MailFolderDescription mfd = new MailFolderDescription();
-					mfd.setExists(false);
-					mfd.setParentFullname(parentFullname);
-					mfd.setSeparator(inbox.getSeparator());
-					mfd.setSubscribed(false);
-					mfd.setName(TEMP_FOLDER);
+                final MailFolderDescription mfd = new MailFolderDescription();
+                mfd.setExists(false);
+                mfd.setParentFullname(parentFullname);
+                mfd.setSeparator(inbox.getSeparator());
+                mfd.setSubscribed(false);
+                mfd.setName(TEMP_FOLDER);
 
-					final MailPermission p = MailProviderRegistry.getMailProviderBySession(session, MailAccount.DEFAULT_ID)
-							.createNewMailPermission();
-					p.setEntity(getUser());
-					p.setAllPermission(OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION,
-							OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
-					p.setFolderAdmin(true);
-					p.setGroupPermission(false);
-					mfd.addPermission(p);
-					mailAccess.getFolderStorage().createFolder(mfd);
-				}
-				try {
-					/*
-					 * Find new folder
-					 */
-					boolean found = false;
-					final MailFolder[] folders = mailAccess.getFolderStorage().getSubfolders(parentFullname, true);
-					for (int i = 0; i < folders.length; i++) {
-						final MailFolder mf = folders[i];
-						assertTrue("Missing default folder flag", mf.containsDefaultFolder());
-						assertTrue("Missing deleted count", mf.containsDeletedMessageCount());
-						assertTrue("Missing exists flag", mf.containsExists());
-						assertTrue("Missing fullname", mf.containsFullname());
-						assertTrue("Missing holds folders flag", mf.containsHoldsFolders());
-						assertTrue("Missing holds messages flag", mf.containsHoldsMessages());
-						assertTrue("Missing message count", mf.containsMessageCount());
-						assertTrue("Missing name", mf.containsName());
-						assertTrue("Missing new message count", mf.containsNewMessageCount());
-						if (mf instanceof IMAPMailFolder) {
-							assertTrue("Missing non-existent flag", ((IMAPMailFolder) mf).containsNonExistent());
-						}
-						assertTrue("Missing own permission", mf.containsOwnPermission());
-						assertTrue("Missing parent fullname", mf.containsParentFullname());
-						assertTrue("Missing permissions", mf.containsPermissions());
-						assertTrue("Missing root folder flag", mf.containsRootFolder());
-						assertTrue("Missing separator flag", mf.containsSeparator());
-						assertTrue("Missing subfolder flag", mf.containsSubfolders());
-						assertTrue("Missing subscribed flag", mf.containsSubscribed());
-						assertTrue("Missing subscribed subfolders flag", mf.containsSubscribedSubfolders());
-						assertTrue("Missing supports user flags flag", mf.containsSupportsUserFlags());
-						assertTrue("Missing unread message count", mf.containsUnreadMessageCount());
-						if (fullname.equals(mf.getFullname())) {
-							found = true;
-							assertFalse("Subscribed, but shouldn't be", MailProperties.getInstance().isSupportSubscription() ? mf
-									.isSubscribed() : false);
-						}
-					}
-					assertTrue("Newly created subfolder not found!", found);
-					/*
-					 * Append mails to new folder
-					 */
-					final String[] uids = mailAccess.getMessageStorage().appendMessages(fullname, mails);
-					try {
-						/*
-						 * All UIDs should differ
-						 */
-						for (int i = 0; i < uids.length; i++) {
-							for (int j = 0; j < uids.length; j++) {
-								if (j != i) {
-									assertTrue("ID of appended mail #" + (i + 1) + " is equal to ID of appended mail #"
-											+ (j + 1), uids[i] != uids[j]);
-								}
-							}
-						}
-					} finally {
-						/*
-						 * Delete appended mails
-						 */
-						mailAccess.getMessageStorage().deleteMessages(fullname, uids, true);
-					}
-					/*
-					 * Append again
-					 */
-					final String[] uids2 = mailAccess.getMessageStorage().appendMessages(fullname, mails);
-					try {
-						/*
-						 * All UIDs should differ
-						 */
-						for (int i = 0; i < uids2.length; i++) {
-							for (int j = 0; j < uids2.length; j++) {
-								if (j != i) {
-									assertTrue("ID of appended mail #" + (i + 1) + " is equal to ID of appended mail #"
-											+ (j + 1), uids2[i] != uids2[j]);
-								}
-							}
-						}
-						/*
-						 * ... and should differ compared to previous IDs
-						 */
-						for (int i = 0; i < uids.length; i++) {
-							for (int j = 0; j < uids2.length; j++) {
-								assertTrue("An ID occured twice in folder" + fullname, uids[i] != uids2[j]);
-							}
-						}
-					} finally {
-						/*
-						 * Delete appended mails
-						 */
-						mailAccess.getMessageStorage().deleteMessages(fullname, uids2, true);
-					}
-				} finally {
-					if (fullname != null) {
-						mailAccess.getFolderStorage().deleteFolder(fullname, true);
-					}
-				}
-			} finally {
-				/*
-				 * close
-				 */
-				mailAccess.close(false);
-			}
-	}
-
+                final MailPermission p = MailProviderRegistry.getMailProviderBySession(session, MailAccount.DEFAULT_ID)
+                        .createNewMailPermission();
+                p.setEntity(getUser());
+                p.setAllPermission(OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION,
+                        OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
+                p.setFolderAdmin(true);
+                p.setGroupPermission(false);
+                mfd.addPermission(p);
+                mailAccess.getFolderStorage().createFolder(mfd);
+            }
+            try {
+                /*
+                 * Find new folder
+                 */
+                boolean found = false;
+                final MailFolder[] folders = mailAccess.getFolderStorage().getSubfolders(parentFullname, true);
+                for (int i = 0; i < folders.length; i++) {
+                    final MailFolder mf = folders[i];
+                    assertTrue("Missing default folder flag", mf.containsDefaultFolder());
+                    assertTrue("Missing deleted count", mf.containsDeletedMessageCount());
+                    assertTrue("Missing exists flag", mf.containsExists());
+                    assertTrue("Missing fullname", mf.containsFullname());
+                    assertTrue("Missing holds folders flag", mf.containsHoldsFolders());
+                    assertTrue("Missing holds messages flag", mf.containsHoldsMessages());
+                    assertTrue("Missing message count", mf.containsMessageCount());
+                    assertTrue("Missing name", mf.containsName());
+                    assertTrue("Missing new message count", mf.containsNewMessageCount());
+                    if (mf instanceof IMAPMailFolder) {
+                        assertTrue("Missing non-existent flag", ((IMAPMailFolder) mf).containsNonExistent());
+                    }
+                    assertTrue("Missing own permission", mf.containsOwnPermission());
+                    assertTrue("Missing parent fullname", mf.containsParentFullname());
+                    assertTrue("Missing permissions", mf.containsPermissions());
+                    assertTrue("Missing root folder flag", mf.containsRootFolder());
+                    assertTrue("Missing separator flag", mf.containsSeparator());
+                    assertTrue("Missing subfolder flag", mf.containsSubfolders());
+                    assertTrue("Missing subscribed flag", mf.containsSubscribed());
+                    assertTrue("Missing subscribed subfolders flag", mf.containsSubscribedSubfolders());
+                    assertTrue("Missing supports user flags flag", mf.containsSupportsUserFlags());
+                    assertTrue("Missing unread message count", mf.containsUnreadMessageCount());
+                    if (fullname.equals(mf.getFullname())) {
+                        found = true;
+                        assertFalse("Subscribed, but shouldn't be", MailProperties.getInstance().isSupportSubscription() ? mf
+                                .isSubscribed() : false);
+                    }
+                }
+                assertTrue("Newly created subfolder not found!", found);
+                /*
+                 * Append mails to new folder
+                 */
+                final String[] uids = mailAccess.getMessageStorage().appendMessages(fullname, mails);
+                try {
+                    /*
+                     * All UIDs should differ
+                     */
+                    for (int i = 0; i < uids.length; i++) {
+                        for (int j = 0; j < uids.length; j++) {
+                            if (j != i) {
+                                assertTrue("ID of appended mail #" + (i + 1) + " is equal to ID of appended mail #"
+                                        + (j + 1), uids[i] != uids[j]);
+                            }
+                        }
+                    }
+                } finally {
+                    /*
+                     * Delete appended mails
+                     */
+                    mailAccess.getMessageStorage().deleteMessages(fullname, uids, true);
+                }
+                /*
+                 * Append again
+                 */
+                final String[] uids2 = mailAccess.getMessageStorage().appendMessages(fullname, mails);
+                try {
+                    /*
+                     * All UIDs should differ
+                     */
+                    for (int i = 0; i < uids2.length; i++) {
+                        for (int j = 0; j < uids2.length; j++) {
+                            if (j != i) {
+                                assertTrue("ID of appended mail #" + (i + 1) + " is equal to ID of appended mail #"
+                                        + (j + 1), uids2[i] != uids2[j]);
+                            }
+                        }
+                    }
+                    /*
+                     * ... and should differ compared to previous IDs
+                     */
+                    for (int i = 0; i < uids.length; i++) {
+                        for (int j = 0; j < uids2.length; j++) {
+                            assertTrue("An ID occured twice in folder" + fullname, uids[i] != uids2[j]);
+                        }
+                    }
+                } finally {
+                    /*
+                     * Delete appended mails
+                     */
+                    mailAccess.getMessageStorage().deleteMessages(fullname, uids2, true);
+                }
+            } finally {
+                if (fullname != null) {
+                    mailAccess.getFolderStorage().deleteFolder(fullname, true);
+                }
+            }
+        } finally {
+            /*
+             * close
+             */
+            mailAccess.close(false);
+        }
+    }
 }
