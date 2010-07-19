f79eb5d6bcabf5f7e6248b33989276708806c01b
Marcus Klein <marcus.klein@open-xchange.com>
2010-07-19 08:46:51 +0000

    restricting used mails for test

--- openexchange-test/unittests/com/openexchange/mail/AbstractMailTest.java
+++ openexchange-test/unittests/com/openexchange/mail/AbstractMailTest.java
@@ -55,6 +55,7 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -76,266 +77,266 @@ import com.openexchange.sessiond.impl.SessionObjectWrapper;
 
 /**
  * {@link AbstractMailTest}
- * 
+ *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
- * 
+ *
  */
 public abstract class AbstractMailTest extends TestCase {
 
-	private String server;
+    private String server;
 
-	private int port;
+    private int port;
 
-	private String login;
+    private String login;
 
-	private int secondUser;
+    private int secondUser;
 
-	private String password;
+    private String password;
 
-	private int user;
+    private int user;
 
-	private int cid;
+    private int cid;
 
-	private String testMailDir;
+    private String testMailDir;
 
-	/**
-	 * 
-	 */
-	public AbstractMailTest() {
-		super();
-	}
+    /**
+     *
+     */
+    public AbstractMailTest() {
+        super();
+    }
 
-	/**
-	 * @param name
-	 */
-	public AbstractMailTest(final String name) {
-		super(name);
-	}
+    /**
+     * @param name
+     */
+    public AbstractMailTest(final String name) {
+        super(name);
+    }
 
-	@Override
-	protected void setUp() throws Exception {
-		/*
-		 * Init
-		 */
-		Init.startServer();
-		/*
-		 * Init test environment
-		 */
-		MailConfig.init();
-		server = MailConfig.getProperty(MailConfig.Property.SERVER);
-		port = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.PORT));
-		login = MailConfig.getProperty(MailConfig.Property.LOGIN);
-		secondUser = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.SECOND_USER));
-		password = MailConfig.getProperty(MailConfig.Property.PASSWORD);
-		user = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.USER));
-		cid = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.CONTEXT));
-		testMailDir = MailConfig.getProperty(MailConfig.Property.TEST_MAIL_DIR);
-	}
+    @Override
+    protected void setUp() throws Exception {
+        /*
+         * Init
+         */
+        Init.startServer();
+        /*
+         * Init test environment
+         */
+        MailConfig.init();
+        server = MailConfig.getProperty(MailConfig.Property.SERVER);
+        port = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.PORT));
+        login = MailConfig.getProperty(MailConfig.Property.LOGIN);
+        secondUser = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.SECOND_USER));
+        password = MailConfig.getProperty(MailConfig.Property.PASSWORD);
+        user = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.USER));
+        cid = Integer.parseInt(MailConfig.getProperty(MailConfig.Property.CONTEXT));
+        testMailDir = MailConfig.getProperty(MailConfig.Property.TEST_MAIL_DIR);
+    }
 
-	@Override
-	protected void tearDown() throws Exception {
-		Init.stopServer();
-	}
+    @Override
+    protected void tearDown() throws Exception {
+        Init.stopServer();
+    }
 
-	/**
-	 * Gets the login
-	 * 
-	 * @return the login
-	 */
-	protected final String getLogin() {
-		return login;
-	}
+    /**
+     * Gets the login
+     *
+     * @return the login
+     */
+    protected final String getLogin() {
+        return login;
+    }
 
-	/**
-	 * Gets the localIP
-	 * 
-	 * @return the login
-	 */
-	protected final String getLocalIP() {
-		return "127.0.0.1";
-	}
+    /**
+     * Gets the localIP
+     *
+     * @return the login
+     */
+    protected final String getLocalIP() {
+        return "127.0.0.1";
+    }
 
-	/**
-	 * Gets the second login
-	 * 
-	 * @return the second login
-	 */
-	protected final int getSecondUser() {
-		return secondUser;
-	}
+    /**
+     * Gets the second login
+     *
+     * @return the second login
+     */
+    protected final int getSecondUser() {
+        return secondUser;
+    }
 
-	/**
-	 * Gets the password
-	 * 
-	 * @return the password
-	 */
-	protected final String getPassword() {
-		return password;
-	}
+    /**
+     * Gets the password
+     *
+     * @return the password
+     */
+    protected final String getPassword() {
+        return password;
+    }
 
-	/**
-	 * Gets the port
-	 * 
-	 * @return the port
-	 */
-	protected final int getPort() {
-		return port;
-	}
+    /**
+     * Gets the port
+     *
+     * @return the port
+     */
+    protected final int getPort() {
+        return port;
+    }
 
-	/**
-	 * Gets the server
-	 * 
-	 * @return the server
-	 */
-	protected final String getServer() {
-		return server;
-	}
+    /**
+     * Gets the server
+     *
+     * @return the server
+     */
+    protected final String getServer() {
+        return server;
+    }
 
-	/**
-	 * Gets the cid
-	 * 
-	 * @return the cid
-	 */
-	protected final int getCid() {
-		return cid;
-	}
+    /**
+     * Gets the cid
+     *
+     * @return the cid
+     */
+    protected final int getCid() {
+        return cid;
+    }
 
-	/**
-	 * Gets the user
-	 * 
-	 * @return the user
-	 */
-	protected final int getUser() {
-		return user;
-	}
+    /**
+     * Gets the user
+     *
+     * @return the user
+     */
+    protected final int getUser() {
+        return user;
+    }
 
-	/**
-	 * Gets a newly created session for user obtained by {@link #getUser()}
-	 * 
-	 * @return A newly created session for user obtained by {@link #getUser()}
-	 */
-	protected final SessionObject getSession() {
-		final SessionObject session = SessionObjectWrapper.createSessionObject(getUser(), new ContextImpl(getCid()),
-				"mail-test-session");
-		session.setPassword(getPassword());
-		session.setLocalIp(getLocalIP());
-		return session;
-	}
+    /**
+     * Gets a newly created session for user obtained by {@link #getUser()}
+     *
+     * @return A newly created session for user obtained by {@link #getUser()}
+     */
+    protected final SessionObject getSession() {
+        final SessionObject session = SessionObjectWrapper.createSessionObject(getUser(), new ContextImpl(getCid()),
+                "mail-test-session");
+        session.setPassword(getPassword());
+        session.setLocalIp(getLocalIP());
+        return session;
+    }
 
-	/**
-	 * Gets the user's mail settings
-	 * 
-	 * @return The user's mail settings
-	 */
-	protected final UserSettingMail getUserSettingMail() throws UserConfigurationException {
-		return UserSettingMailStorage.getInstance().getUserSettingMail(getUser(), getCid());
-	}
+    /**
+     * Gets the user's mail settings
+     *
+     * @return The user's mail settings
+     */
+    protected final UserSettingMail getUserSettingMail() throws UserConfigurationException {
+        return UserSettingMailStorage.getInstance().getUserSettingMail(getUser(), getCid());
+    }
 
-	/**
-	 * Gets the test mail directory
-	 * 
-	 * @return the test mail directory
-	 */
-	protected final String getTestMailDir() {
-		return testMailDir;
-	}
+    /**
+     * Gets the test mail directory
+     *
+     * @return the test mail directory
+     */
+    protected final String getTestMailDir() {
+        return testMailDir;
+    }
 
-	private static final String STR_TRUE = "true";
+    private static final String STR_TRUE = "true";
 
-	private static final String STR_FALSE = "false";
+    private static final String STR_FALSE = "false";
 
-	private static Properties sessionProperties;
+    private static Properties sessionProperties;
 
-	/**
-	 * Gets the default session properties
-	 * 
-	 * @return The default session properties
-	 */
-	protected static final Properties getDefaultSessionProperties() {
-		synchronized (AbstractMailTest.class) {
-			if (sessionProperties == null) {
-				/*
-				 * Define session properties
-				 */
-				System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_BASE64_IGNOREERRORS, STR_TRUE);
-				System.getProperties().put(MIMESessionPropertyNames.PROP_ALLOWREADONLYSELECT, STR_TRUE);
-				System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_ENCODEEOL_STRICT, STR_TRUE);
-				System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_DECODETEXT_STRICT, STR_FALSE);
-				System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_CHARSET, "UTF-8");
-				/*
-				 * Define imap session properties
-				 */
-				sessionProperties = ((Properties) (System.getProperties().clone()));
-				/*
-				 * A connected IMAPStore maintains a pool of IMAP protocol
-				 * objects for use in communicating with the IMAP server. The
-				 * IMAPStore will create the initial AUTHENTICATED connection
-				 * and seed the pool with this connection. As folders are opened
-				 * and new IMAP protocol objects are needed, the IMAPStore will
-				 * provide them from the connection pool, or create them if none
-				 * are available. When a folder is closed, its IMAP protocol
-				 * object is returned to the connection pool if the pool is not
-				 * over capacity.
-				 */
-				sessionProperties.put(MIMESessionPropertyNames.PROP_MAIL_IMAP_CONNECTIONPOOLSIZE, "1");
-				/*
-				 * A mechanism is provided for timing out idle connection pool
-				 * IMAP protocol objects. Timed out connections are closed and
-				 * removed (pruned) from the connection pool.
-				 */
-				sessionProperties.put(MIMESessionPropertyNames.PROP_MAIL_IMAP_CONNECTIONPOOLTIMEOUT, "1000");
-				return sessionProperties;
-			}
-			return sessionProperties;
-		}
-	}
+    /**
+     * Gets the default session properties
+     *
+     * @return The default session properties
+     */
+    protected static final Properties getDefaultSessionProperties() {
+        synchronized (AbstractMailTest.class) {
+            if (sessionProperties == null) {
+                /*
+                 * Define session properties
+                 */
+                System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_BASE64_IGNOREERRORS, STR_TRUE);
+                System.getProperties().put(MIMESessionPropertyNames.PROP_ALLOWREADONLYSELECT, STR_TRUE);
+                System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_ENCODEEOL_STRICT, STR_TRUE);
+                System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_DECODETEXT_STRICT, STR_FALSE);
+                System.getProperties().put(MIMESessionPropertyNames.PROP_MAIL_MIME_CHARSET, "UTF-8");
+                /*
+                 * Define imap session properties
+                 */
+                sessionProperties = ((Properties) (System.getProperties().clone()));
+                /*
+                 * A connected IMAPStore maintains a pool of IMAP protocol
+                 * objects for use in communicating with the IMAP server. The
+                 * IMAPStore will create the initial AUTHENTICATED connection
+                 * and seed the pool with this connection. As folders are opened
+                 * and new IMAP protocol objects are needed, the IMAPStore will
+                 * provide them from the connection pool, or create them if none
+                 * are available. When a folder is closed, its IMAP protocol
+                 * object is returned to the connection pool if the pool is not
+                 * over capacity.
+                 */
+                sessionProperties.put(MIMESessionPropertyNames.PROP_MAIL_IMAP_CONNECTIONPOOLSIZE, "1");
+                /*
+                 * A mechanism is provided for timing out idle connection pool
+                 * IMAP protocol objects. Timed out connections are closed and
+                 * removed (pruned) from the connection pool.
+                 */
+                sessionProperties.put(MIMESessionPropertyNames.PROP_MAIL_IMAP_CONNECTIONPOOLTIMEOUT, "1000");
+                return sessionProperties;
+            }
+            return sessionProperties;
+        }
+    }
 
-	/**
-	 * Reads MIME messages (<code>*.eml</code> files) from specified directory
-	 * 
-	 * @param dir
-	 *            The directory containing <code>*.eml</code> files
-	 * @param limit
-	 *            The limit or <code>-1</code> to read all available
-	 *            <code>*.eml</code> files
-	 * @return The read MIME messages
-	 * @throws MessagingException
-	 *             If a messaging error occurs
-	 * @throws IOException
-	 *             If an I/O error occurs
-	 * @throws MailException
-	 *             If conversion from RFC822 message fails
-	 */
-	protected static final MailMessage[] getMessages(final String dir, final int limit) throws MessagingException,
-			IOException, MailException {
-		final File fdir = new File(dir);
-		final File[] messageFiles = fdir.listFiles(new FilenameFilter() {
-			public boolean accept(final File dir, final String name) {
-				return name.toLowerCase().endsWith(".eml");
-			}
-		});
-		final int len = limit < 0 ? messageFiles.length : Math.min(messageFiles.length, limit);
-		final MimeMessage[] msgs = new MimeMessage[len];
-		final Session session = Session.getInstance(getDefaultSessionProperties());
-		for (int i = 0; i < msgs.length; i++) {
-			InputStream in = null;
-			try {
-				in = new FileInputStream(messageFiles[i]);
-				msgs[i] = new MimeMessage(session, in);
-			} finally {
-				if (null != in) {
-					try {
-						in.close();
-					} catch (final IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-		final MailMessage[] retval = new MailMessage[msgs.length];
-		for (int i = 0; i < retval.length; i++) {
-			retval[i] = MIMEMessageConverter.convertMessage(msgs[i]);
-		}
-		return retval;
-	}
+    /**
+     * Reads MIME messages (<code>*.eml</code> files) from specified directory
+     *
+     * @param dir
+     *            The directory containing <code>*.eml</code> files
+     * @param limit
+     *            The limit or <code>-1</code> to read all available
+     *            <code>*.eml</code> files
+     * @return The read MIME messages
+     * @throws MessagingException
+     *             If a messaging error occurs
+     * @throws IOException
+     *             If an I/O error occurs
+     * @throws MailException
+     *             If conversion from RFC822 message fails
+     */
+    protected static final MailMessage[] getMessages(final String dir, final int limit) throws MessagingException,
+            IOException, MailException {
+        final File fdir = new File(dir);
+        final File[] messageFiles = fdir.listFiles(new FilenameFilter() {
+            public boolean accept(final File dir, final String name) {
+                return Pattern.compile("mail.*\\.eml").matcher(name).matches();
+            }
+        });
+        final int len = limit < 0 ? messageFiles.length : Math.min(messageFiles.length, limit);
+        final MimeMessage[] msgs = new MimeMessage[len];
+        final Session session = Session.getInstance(getDefaultSessionProperties());
+        for (int i = 0; i < msgs.length; i++) {
+            InputStream in = null;
+            try {
+                in = new FileInputStream(messageFiles[i]);
+                msgs[i] = new MimeMessage(session, in);
+            } finally {
+                if (null != in) {
+                    try {
+                        in.close();
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        final MailMessage[] retval = new MailMessage[msgs.length];
+        for (int i = 0; i < retval.length; i++) {
+            retval[i] = MIMEMessageConverter.convertMessage(msgs[i]);
+        }
+        return retval;
+    }
 }
