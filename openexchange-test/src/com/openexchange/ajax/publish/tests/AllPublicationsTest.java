5695c7abf9096deaf007e03e2eba703fa4c64bdc
Steffen Templin <steffen.templin@open-xchange.com>
2010-08-25 15:29:46 +0000

    Improved error message

--- openexchange-test/src/com/openexchange/ajax/publish/tests/AllPublicationsTest.java
+++ openexchange-test/src/com/openexchange/ajax/publish/tests/AllPublicationsTest.java
@@ -205,7 +205,7 @@ public class AllPublicationsTest extends AbstractPublicationTest {
         // get all publications
         pubMgr.allAction(Integer.MAX_VALUE, Arrays.asList(new String[]{"id","entity", "entityModule", "displayName", "target"}));
         AllPublicationsResponse resp = (AllPublicationsResponse) pubMgr.getLastResponse();
-        assertFalse("Should work", resp.hasError());
+        assertFalse("Error at all-request: " + resp.getException().getMessage(), resp.hasError());
         List<JSONArray> all = resp.getAll();
         List<Integer> foundIds = new ArrayList<Integer>();
         
