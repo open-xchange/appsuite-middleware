b57274b22b20e40c24c983c5c67097bdb135a56b
Tobias Prinz <tobias.prinz@open-xchange.com>
2010-09-06 13:38:51 +0000

    US4165: Resources now allow "updates" request, groups still pending

--- openexchange-test/src/com/openexchange/ajax/ResourceTest.java
+++ openexchange-test/src/com/openexchange/ajax/ResourceTest.java
@@ -2,8 +2,12 @@ package com.openexchange.ajax;
 
 
 import java.io.ByteArrayInputStream;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.meterware.httpunit.GetMethodWebRequest;
@@ -14,6 +18,7 @@ import com.meterware.httpunit.WebResponse;
 import com.openexchange.ajax.container.Response;
 import com.openexchange.ajax.fields.DataFields;
 import com.openexchange.ajax.fields.ParticipantsFields;
+import com.openexchange.resource.Resource;
 import com.openexchange.tools.URLParameter;
 
 public class ResourceTest extends AbstractAJAXTest {
@@ -48,6 +53,22 @@ public class ResourceTest extends AbstractAJAXTest {
 		final com.openexchange.resource.Resource r = loadResource(getWebConversation(), resources[0].getIdentifier(), PROTOCOL + getHostName(), getSessionId());
 	}
 	
+	public void testUpdates() throws Exception{
+        Resource resources[] = searchResource(getWebConversation(), "*", PROTOCOL + getHostName(), getSessionId());
+        int numberFoundViaSearch = resources.length;
+        Resource[] resources2 = getUpdates(getWebConversation(), PROTOCOL + getHostName(), new Date(0), getSessionId() ).toArray(new Resource[]{});
+	    int numberFoundViaUpdates = resources2.length;
+	    assertEquals("Should find the same amount of resources when searching for * as for updates since day 0", numberFoundViaSearch, numberFoundViaUpdates);
+        Resource[] resources3 = getUpdates(getWebConversation(), PROTOCOL + getHostName(), new Date(), getSessionId() ).toArray(new Resource[]{});
+        assertEquals("Should find no resource created just now", 0, resources3.length);
+	}
+	
+	
+	
+	
+	
+	
+	
 	public static com.openexchange.resource.Resource[] searchResource(final WebConversation webCon, final String searchpattern, String host, final String session) throws Exception {
 		host = appendPrefix(host);
 		
@@ -73,17 +94,7 @@ public class ResourceTest extends AbstractAJAXTest {
 		assertNotNull("timestamp is null", response.getTimestamp());
 		
 		final JSONArray jsonArray = (JSONArray)response.getData();
-		final com.openexchange.resource.Resource[] r = new com.openexchange.resource.Resource[jsonArray.length()];
-		for (int a = 0; a < r.length; a++) {
-			final JSONObject jObj = jsonArray.getJSONObject(a);
-			r[a] = new com.openexchange.resource.Resource();
-			r[a].setIdentifier(jObj.getInt(ParticipantsFields.ID));
-			if (jObj.has(ParticipantsFields.DISPLAY_NAME)) {
-				r[a].setDisplayName(jObj.getString(ParticipantsFields.DISPLAY_NAME));
-			}
-		}
-		
-		return r;
+		return extractResources(jsonArray).toArray(new Resource[]{});
 	}
 	
 	public static com.openexchange.resource.Resource[] listResource(final WebConversation webCon, final int[] id, String host, final String session) throws Exception {
@@ -115,17 +126,8 @@ public class ResourceTest extends AbstractAJAXTest {
 		assertNotNull("timestamp", response.getTimestamp());
 		
 		final JSONArray jsonArray = (JSONArray)response.getData();
-		final com.openexchange.resource.Resource[] r = new com.openexchange.resource.Resource[jsonArray.length()];
-		for (int a = 0; a < r.length; a++) {
-			final JSONObject jObj = jsonArray.getJSONObject(a);
-			r[a] = new com.openexchange.resource.Resource();
-			r[a].setIdentifier(jObj.getInt(ParticipantsFields.ID));
-			if (jObj.has(ParticipantsFields.DISPLAY_NAME)) {
-				r[a].setDisplayName(jObj.getString(ParticipantsFields.DISPLAY_NAME));
-			}
-		}
 		
-		return r;
+		return extractResources(jsonArray).toArray(new Resource[]{});
 	}
 	
 	public static com.openexchange.resource.Resource loadResource(final WebConversation webCon, final int groupId, String host, final String session) throws Exception {
@@ -151,15 +153,54 @@ public class ResourceTest extends AbstractAJAXTest {
 		
 		final JSONObject jsonObj = (JSONObject)response.getData();
 		
-		final com.openexchange.resource.Resource r = new com.openexchange.resource.Resource();
-		assertTrue("id is not in json object", jsonObj.has(ParticipantsFields.ID));
-		r.setIdentifier(jsonObj.getInt(ParticipantsFields.ID));
-		
-		if (jsonObj.has(ParticipantsFields.DISPLAY_NAME)) {
-			r.setDisplayName(jsonObj.getString(ParticipantsFields.DISPLAY_NAME));
-		}
-		
-		return r;
+		return extractResource(jsonObj);
 	}
+	
+	   public static List<Resource> getUpdates(final WebConversation webCon, String host, Date lastModified, final String session) throws Exception {
+	        host = appendPrefix(host);
+	        
+	        final URLParameter parameter = new URLParameter();
+	        parameter.setParameter(AJAXServlet.PARAMETER_SESSION, session);
+	        parameter.setParameter(AJAXServlet.PARAMETER_ACTION, AJAXServlet.ACTION_UPDATES);
+	        parameter.setParameter(AJAXServlet.PARAMETER_TIMESTAMP, lastModified.getTime());
+	        
+	        final WebRequest req = new GetMethodWebRequest(host + RESOURCE_URL + parameter.getURLParameters());
+	        final WebResponse resp = webCon.getResponse(req);
+	        
+	        assertEquals(200, resp.getResponseCode());
+	        
+	        final Response response = Response.parse(resp.getText());
+	        
+	        if (response.hasError()) {
+	            fail("json error: " + response.getErrorMessage());
+	        }
+	        
+	        assertNotNull("timestamp is null", response.getTimestamp());
+	        
+	        final JSONArray jsonArr = (JSONArray)response.getData();
+	        
+	        return extractResources(jsonArr);
+	    }
+	   
+	   private static List<Resource> extractResources(JSONArray array) throws JSONException{
+	       LinkedList<Resource> resources = new LinkedList<Resource>();
+	       for(int i = 0, length = array.length(); i < length; i++){
+	           resources.add(extractResource(array.getJSONObject(i)));
+	       }
+	       return resources;
+	   }
+	   
+	   private static Resource extractResource(JSONObject jObj) throws JSONException{
+           Resource res = new com.openexchange.resource.Resource();
+           res.setIdentifier(jObj.getInt(ParticipantsFields.ID));
+           if (jObj.has(ParticipantsFields.DISPLAY_NAME)) {
+               res.setDisplayName(jObj.getString(ParticipantsFields.DISPLAY_NAME));
+           }
+           if (jObj.has(ParticipantsFields.LAST_MODIFIED)) {
+               res.setDisplayName(jObj.getString(ParticipantsFields.LAST_MODIFIED));
+           }
+           return res;
+	   }
+
 }
 
