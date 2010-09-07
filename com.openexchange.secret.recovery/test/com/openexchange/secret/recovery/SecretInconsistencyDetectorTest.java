f9c71ad044814815fc76a409c710ce3f1c83cabf
francisco <francisco>
2010-09-07 16:06:39 +0000

    *** empty log message ***

---
+++ com.openexchange.secret.recovery/test/com/openexchange/secret/recovery/SecretInconsistencyDetectorTest.java
@@ -0,0 +1,122 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2010 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.secret.recovery;
+
+import org.junit.Before;
+import org.junit.Test;
+import com.openexchange.groupware.AbstractOXException;
+import com.openexchange.groupware.contexts.Context;
+import com.openexchange.groupware.contexts.SimContext;
+import com.openexchange.groupware.ldap.SimUser;
+import com.openexchange.groupware.ldap.User;
+import com.openexchange.secret.SimSecretService;
+import com.openexchange.secret.recovery.impl.DefaultSecretInconsistencyDetector;
+import com.openexchange.tools.session.ServerSession;
+import com.openexchange.tools.session.SimServerSession;
+
+import static org.junit.Assert.*;
+
+/**
+ * {@link SecretInconsistencyDetectorTest}
+ *
+ * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
+ */
+public class SecretInconsistencyDetectorTest {
+    
+    private DefaultSecretInconsistencyDetector detector = new DefaultSecretInconsistencyDetector();
+    private ServerSession session;
+    
+    private Context ctx = new SimContext(1);
+    private SimUser user = new SimUser();
+    
+    @Before
+    public void setUp() {
+        detector.setSecretService(new SimSecretService());
+    }
+    
+    @Test
+    public void shouldDetectWhenSecretStringsCanNotBeDecrypted() throws AbstractOXException {
+        detector.addCheck(new PassingSecretConsistencyCheck());
+        detector.addCheck(new FailingSecretConsistencyCheck());
+        
+        session = new SimServerSession(ctx, user, null);
+        boolean diagnosis = detector.isSecretWorking(session);
+        assertFalse(diagnosis);
+    }
+    
+    @Test
+    public void shouldDetectWhenAllIsWell() throws AbstractOXException {
+        detector.addCheck(new PassingSecretConsistencyCheck());
+        detector.addCheck(new PassingSecretConsistencyCheck());
+        
+        session = new SimServerSession(ctx, user, null);
+        boolean diagnosis = detector.isSecretWorking(session);
+        assertTrue(diagnosis);
+    }   
+    
+    
+    private static final class FailingSecretConsistencyCheck implements SecretConsistencyCheck {
+
+        public boolean checkSecretCanDecryptStrings(ServerSession session, String secret) throws AbstractOXException {
+            return false;
+        }
+        
+    }
+    
+    private static final class PassingSecretConsistencyCheck implements SecretConsistencyCheck {
+
+        public boolean checkSecretCanDecryptStrings(ServerSession session, String secret) throws AbstractOXException {
+            return true;
+        }
+        
+    }
+    
+    
+}
