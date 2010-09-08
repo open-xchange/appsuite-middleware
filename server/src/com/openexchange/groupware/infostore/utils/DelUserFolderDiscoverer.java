c13ed1df71dc4a6f461506a102caa8793aa944fa
marcus <marcus>
2010-09-08 10:15:15 +0000

    removed annotation based exception framework from InfostoreException

--- server/src/com/openexchange/groupware/infostore/utils/DelUserFolderDiscoverer.java
+++ server/src/com/openexchange/groupware/infostore/utils/DelUserFolderDiscoverer.java
@@ -55,13 +55,10 @@ import java.util.List;
 import java.util.Queue;
 import com.openexchange.api2.OXException;
 import com.openexchange.groupware.AbstractOXException;
-import com.openexchange.groupware.EnumComponent;
-import com.openexchange.groupware.OXExceptionSource;
 import com.openexchange.groupware.container.FolderObject;
 import com.openexchange.groupware.contexts.Context;
-import com.openexchange.groupware.infostore.Classes;
 import com.openexchange.groupware.infostore.InfostoreException;
-import com.openexchange.groupware.infostore.InfostoreExceptionFactory;
+import com.openexchange.groupware.infostore.InfostoreExceptionCodes;
 import com.openexchange.groupware.ldap.User;
 import com.openexchange.groupware.ldap.UserStorage;
 import com.openexchange.groupware.tx.DBProvider;
@@ -72,11 +69,7 @@ import com.openexchange.server.impl.OCLPermission;
 import com.openexchange.tools.iterator.FolderObjectIterator;
 import com.openexchange.tools.oxfolder.OXFolderIteratorSQL;
 
-@OXExceptionSource(classId = Classes.COM_OPENEXCHANGE_GROUPWARE_INFOSTORE_UTILS_DELUSERFOLDERDISCOVERER, component = EnumComponent.INFOSTORE)
 public class DelUserFolderDiscoverer extends DBService {
-
-    private static final InfostoreExceptionFactory EXCEPTIONS = new InfostoreExceptionFactory(
-            DelUserFolderDiscoverer.class);
 
     public DelUserFolderDiscoverer() {
         super();
@@ -113,7 +106,7 @@ public class DelUserFolderDiscoverer extends DBService {
         } catch (final AbstractOXException x) {
             throw new InfostoreException(x);
         } catch (final SQLException e) {
-            throw EXCEPTIONS.create(0, e);
+            throw InfostoreExceptionCodes.SQL_PROBLEM.create(e, "");
         }
 
         return discovered;
