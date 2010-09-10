fce53c74a4f88b5166455a308e19bb0dd85d21aa
marcus <marcus>
2010-09-10 12:31:09 +0000

    fixed compile problem

--- global/sim/com/openexchange/sessiond/AbstractSimSessiondService.java
+++ global/sim/com/openexchange/sessiond/AbstractSimSessiondService.java
@@ -49,6 +49,7 @@
 
 package com.openexchange.sessiond;
 
+import java.util.Collection;
 import com.openexchange.groupware.contexts.Context;
 import com.openexchange.session.Session;
 
@@ -130,6 +131,11 @@ public class AbstractSimSessiondService implements SessiondService{
     public int removeUserSessions(int userId, Context ctx) {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    public Collection<Session> getSessions(int userId, int contextId) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
