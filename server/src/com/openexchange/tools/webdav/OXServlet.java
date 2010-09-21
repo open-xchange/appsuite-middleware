54326a6575a4227b04c41a6edb97ac5ac3bee200
thorben <thorben>
2010-09-21 10:30:37 +0000

    Preparation for digest support

--- server/src/com/openexchange/tools/webdav/OXServlet.java
+++ server/src/com/openexchange/tools/webdav/OXServlet.java
@@ -64,7 +64,6 @@ import com.openexchange.groupware.AbstractOXException.Category;
 import com.openexchange.java.util.UUIDs;
 import com.openexchange.login.Interface;
 import com.openexchange.login.LoginRequest;
-import com.openexchange.login.LoginResult;
 import com.openexchange.login.internal.LoginPerformer;
 import com.openexchange.server.ServiceException;
 import com.openexchange.server.services.ServerServiceRegistry;
@@ -72,6 +71,8 @@ import com.openexchange.session.Session;
 import com.openexchange.sessiond.SessiondService;
 import com.openexchange.tools.StringCollection;
 import com.openexchange.tools.encoding.Base64;
+import com.openexchange.tools.webdav.digest.Authorization;
+import com.openexchange.tools.webdav.digest.DigestUtility;
 import com.openexchange.webdav.WebdavException;
 import com.openexchange.xml.jdom.JDOMParser;
 
@@ -79,12 +80,70 @@ import com.openexchange.xml.jdom.JDOMParser;
  * This servlet can be used as super class for all OX webdav servlets.
  * 
  * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein </a>
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
 public abstract class OXServlet extends WebDavServlet {
 
     private static final long serialVersionUID = 301910346402779362L;
 
     private static final transient Log LOG = LogFactory.getLog(OXServlet.class);
+
+    /**
+     * Simple {@link LoginRequest} implementation.
+     */
+    private static final class LoginRequestImpl implements LoginRequest {
+
+        private final String login;
+
+        private final HttpServletRequest req;
+
+        private final String pass;
+
+        private final Interface interfaze;
+
+        public LoginRequestImpl(final String login, final String pass, final Interface interfaze, final HttpServletRequest req) {
+            this.login = login;
+            this.req = req;
+            this.pass = pass;
+            this.interfaze = interfaze;
+        }
+
+        public String getUserAgent() {
+            return req.getHeader("user-agent");
+        }
+
+        public String getPassword() {
+            return pass;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public Interface getInterface() {
+            return interfaze;
+        }
+
+        public String getClientIP() {
+            return req.getRemoteAddr();
+        }
+
+        public String getAuthId() {
+            return UUIDs.getUnformattedString(UUID.randomUUID());
+        }
+
+        public String getClient() {
+            return null;
+        }
+
+        public String getVersion() {
+            return null;
+        }
+
+        public String getHash() {
+            return null;
+        }
+    }
 
     /**
      * Store the session object under this name in the request.
@@ -94,12 +153,35 @@ public abstract class OXServlet extends WebDavServlet {
     /**
      * Authentication identifier.
      */
-    private static final String authIdentifier = "OX WebDAV";
+    private static final String basicRealm = "OX WebDAV";
+
+    private static final String digestRealm = "Open-Xchange";
 
     protected static final String COOKIE_SESSIONID = "sessionid";
 
+    /**
+     * Name of the header containing the authorization data.
+     */
+    private static final String AUTH_HEADER = "authorization";
+
+    /**
+     * Basic type for authorization.
+     */
+    private static final String BASIC_AUTH = "basic";
+
+    /**
+     * Digest type for authorization.
+     */
+    private static final String DIGEST_AUTH = "digest";
+
+    private final LoginPerformer loginPerformer;
+
+    /**
+     * Initializes a new {@link OXServlet}.
+     */
     protected OXServlet() {
         super();
+        loginPerformer = LoginPerformer.getInstance();
     }
 
     /**
@@ -135,37 +217,40 @@ public abstract class OXServlet extends WebDavServlet {
     }
 
     /**
-     * Does the whole authentication mechanism.
+     * Performs authentication.
      * 
-     * @param req http servlet request.
-     * @param resp http servlet response.
-     * @return <code>true</code> if the authentication can be done correctly.
-     * @throws IOException if a communication problem occurs.
+     * @param req The HTTP servlet request.
+     * @param resp The HTTP servlet response.
+     * @return <code>true</code> if the authentication was successful; otherwise <code>false</code>.
+     * @throws IOException If an I/O error occurs
      */
     protected boolean doAuth(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
         Session session;
         try {
             session = findSessionByCookie(req, resp);
-        } catch (ServiceException e) {
+        } catch (final ServiceException e) {
             LOG.error(e.getMessage(), e);
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
             return false;
         }
         if (null == session) {
+            /*
+             * No session found by cookie
+             */
             final LoginRequest loginRequest;
             try {
                 loginRequest = parseLogin(req);
-            } catch (WebdavException e) {
+            } catch (final WebdavException e) {
                 LOG.debug(e.getMessage(), e);
-                addUnauthorizedHeader(resp);
+                addUnauthorizedHeader(req, resp);
                 resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization Required!");
                 return false;
             }
             try {
                 session = addSession(loginRequest);
-            } catch (LoginException e) {
+            } catch (final LoginException e) {
                 if (e.getCategory() == Category.USER_INPUT) {
-                    addUnauthorizedHeader(resp);
+                    addUnauthorizedHeader(req, resp);
                     resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization Required!");
                     return false;
                 }
@@ -175,12 +260,15 @@ public abstract class OXServlet extends WebDavServlet {
             }
             resp.addCookie(new Cookie(COOKIE_SESSIONID, session.getSessionID()));
         } else {
+            /*
+             * Session found by cookie
+             */
             final String address = req.getRemoteAddr();
             if (null == address || !address.equals(session.getLocalIp())) {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("Wrong client IP address.");
                 }
-                addUnauthorizedHeader(resp);
+                addUnauthorizedHeader(req, resp);
                 resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization Required!");
                 return false;
             }
@@ -209,41 +297,34 @@ public abstract class OXServlet extends WebDavServlet {
     }
 
     /**
-     * Decodes the base64 encoded authorization header. The leading basic will be removed if it is present.
+     * Checks if the client sends a correct digest authorization header.
      * 
-     * @param auth the base64 encoded value of the authorization header
-     * @return a string array with user and password
-     * @throws IOException if the base64 can't be decoded
+     * @param auth Authorization header.
+     * @return <code>true</code> if the client sent a correct authorization header.
      */
-    protected static String[] decodeAuthorization(final String auth) throws IOException {
-        if (!checkForBasicAuthorization(auth)) {
-            throw new IOException("Authorization header is missing the leading \"basic\"!");
+    private static boolean checkForDigestAuthorization(final String auth) {
+        if (null == auth) {
+            return false;
         }
-        final byte[] decoded = Base64.decode(auth.substring(6));
-        final String userpass = new String(decoded, "UTF-8").trim();
-        final int delimiter = userpass.indexOf(':');
-        String user = "";
-        String pass = "";
-        if (-1 != delimiter) {
-            user = userpass.substring(0, delimiter);
-            pass = userpass.substring(delimiter + 1);
+        if (auth.length() <= DIGEST_AUTH.length()) {
+            return false;
         }
-        return new String[] { user, pass };
+        if (!auth.substring(0, DIGEST_AUTH.length()).equalsIgnoreCase(DIGEST_AUTH)) {
+            return false;
+        }
+        return true;
     }
 
     /**
      * Checks if the login contains only valid values.
-     * @param pass password of the user
      * 
+     * @param pass password of the user
      * @return false if the login contains illegal values.
      */
     protected static boolean checkLogin(final String pass) {
         // check if the user wants to login without password.
         // ldap bind doesn't fail with empty password. so check it here.
-        if (pass == null || StringCollection.isEmpty(pass)) {
-            return false;
-        }
-        return true;
+        return (pass != null && !StringCollection.isEmpty(pass));
     }
 
     /**
@@ -251,53 +332,91 @@ public abstract class OXServlet extends WebDavServlet {
      * 
      * @param resp the response to that the header should be added.
      */
-    protected static void addUnauthorizedHeader(final HttpServletResponse resp) {
-        resp.setHeader("WWW-Authenticate", "Basic realm=\"" + authIdentifier + "\"");
+    protected static void addUnauthorizedHeader(final HttpServletRequest req, final HttpServletResponse resp) {
+        final StringBuilder builder = new StringBuilder(64);
+        builder.append("Basic realm=\"").append(basicRealm).append('"');
+        resp.setHeader("WWW-Authenticate", builder.toString());
+        /*-
+         * Digest realm="testrealm@host.com",
+         * qop="auth,auth-int",
+         * nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093",
+         * opaque="5ccc069c403ebaf9f0171e9517f40e41" 
+         */
+        builder.setLength(0);
+        builder.append("Digest realm=\"").append(digestRealm).append('"').append(", ");
+        builder.append("qop=\"auth,auth-int\"").append(", ");
+        builder.append("nonce=\"").append(DigestUtility.getInstance().generateNOnce(req)).append('"').append(", ");
+        final String opaque = UUIDs.getUnformattedString(UUID.randomUUID());
+        builder.append("opaque=\"").append(opaque).append('"').append(", ");
+        builder.append("stale=\"false\"").append(", ");
+        builder.append("algorithm=\"MD5\"");
+        resp.addHeader("WWW-Authenticate", builder.toString());
     }
 
     private LoginRequest parseLogin(final HttpServletRequest req) throws WebdavException, IOException {
         final String auth = req.getHeader(AUTH_HEADER);
-        if (!checkForBasicAuthorization(auth)) {
+        if (null == auth) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("Authentication header missing.");
             }
-            throw new WebdavException(WebdavException.Code.MISSING_HEADER_FIELD, AUTH_HEADER);
+            throw new WebdavException(WebdavException.Code.MISSING_HEADER_FIELD, "Authorization");
         }
-        final String[] userpass = OXServlet.decodeAuthorization(auth);
-        final String login = userpass[0];
-        final String pass = userpass[1];
-        if (!checkLogin(pass)) {
-            throw new WebdavException(WebdavException.Code.EMPTY_PASSWORD);
+        if (checkForBasicAuthorization(auth)) {
+            /*
+             * Basic auth
+             */
+            final byte[] decoded = Base64.decode(auth.substring(6));
+            final String userpass = new String(decoded, "UTF-8").trim();
+            final int delimiter = userpass.indexOf(':');
+            String login = "";
+            String pass = "";
+            if (-1 != delimiter) {
+                login = userpass.substring(0, delimiter);
+                pass = userpass.substring(delimiter + 1);
+            }
+            if (!checkLogin(pass)) {
+                throw new WebdavException(WebdavException.Code.EMPTY_PASSWORD);
+            }
+            return new LoginRequestImpl(login, pass, getInterface(), req);
         }
-        return new LoginRequest() {
-            public String getUserAgent() {
-                return req.getHeader("user-agent");
+        if (false && checkForDigestAuthorization(auth)) {
+            /*
+             * Digest auth
+             */
+            final DigestUtility digestUtility = DigestUtility.getInstance();
+            final Authorization authorization = digestUtility.parseDigestAuthorization(auth);
+            /*
+             * Determine user by "username"
+             */
+            final String userName = authorization.getUser();
+            final String password = digestUtility.getPasswordByUserName(userName);
+            if (!checkLogin(password)) {
+                throw new WebdavException(WebdavException.Code.UNSUPPORTED_AUTH_MECH, "Digest");
             }
-            public String getPassword() {
-                return pass;
+            /*
+             * Calculate MD5
+             */
+            final String serverDigest = digestUtility.generateServerDigest(req, password);
+            /*
+             * Compare to client "response"
+             */
+            if (!serverDigest.equals(authorization.getResponse())) {
+                throw new WebdavException(WebdavException.Code.AUTH_FAILED, userName);
             }
-            public String getLogin() {
-                return login;
-            }
-            public Interface getInterface() {
-                return OXServlet.this.getInterface();
-            }
-            public String getClientIP() {
-                return req.getRemoteAddr();
-            }
-            public String getAuthId() {
-                return UUIDs.getUnformattedString(UUID.randomUUID());
-            }
-            public String getClient() {
-                return null;
-            }
-            public String getVersion() {
-                return null;
-            }
-            public String getHash() {
-                return null;
-            }
-        };
+            /*
+             * Return appropriate login request to generate a session
+             */
+            return new LoginRequestImpl(userName, password, getInterface(), req);
+        }
+        /*
+         * No known auth mechanism
+         */
+        final int pos = auth.indexOf(' ');
+        final String mech = pos > 0 ? auth.substring(0, pos) : auth;
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Unsupported Authentication header.");
+        }
+        throw new WebdavException(WebdavException.Code.UNSUPPORTED_AUTH_MECH, mech);
     }
 
     /**
@@ -306,19 +425,18 @@ public abstract class OXServlet extends WebDavServlet {
      * @param login login name of the user.
      * @param pass plain text password of the user.
      * @param ipAddress client IP.
-     * @return the initilized session or <code>null</code>.
+     * @return the initialized session or <code>null</code>.
      * @throws LoginException if an error occurs while creating the session.
      */
-    private Session addSession(LoginRequest request) throws LoginException {
-        LoginResult result = LoginPerformer.getInstance().doLogin(request);
-        return result.getSession();
+    private Session addSession(final LoginRequest request) throws LoginException {
+        return loginPerformer.doLogin(request).getSession();
     }
 
-    private Session findSessionByCookie(HttpServletRequest req, HttpServletResponse resp) throws ServiceException {
+    private Session findSessionByCookie(final HttpServletRequest req, final HttpServletResponse resp) throws ServiceException {
         final Cookie[] cookies = req.getCookies();
         String sessionId = null;
         if (null != cookies) {
-            for (Cookie cookie : cookies) {
+            for (final Cookie cookie : cookies) {
                 if (COOKIE_SESSIONID.equals(cookie.getName())) {
                     sessionId = cookie.getValue();
                     break;
@@ -364,15 +482,5 @@ public abstract class OXServlet extends WebDavServlet {
         }
         return doc;
     }
-
-    /**
-     * Name of the header containing the authorization data.
-     */
-    private static final String AUTH_HEADER = "authorization";
-
-    /**
-     * Basic type for authorization.
-     */
-    private static final String BASIC_AUTH = "basic";
 
 }
