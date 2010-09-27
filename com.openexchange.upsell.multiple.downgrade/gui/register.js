0e4ade4694314984ca0107315593134507be3c5b
cutmasta <cutmasta>
2010-09-27 13:01:55 +0000

    *** empty log message ***

--- com.openexchange.upsell.multiple.downgrade/gui/register.js
+++ com.openexchange.upsell.multiple.downgrade/gui/register.js
@@ -1,5 +1,5 @@
 // the downgrade page for demo purposes
-var innode = new ox.Configuration.InnerNode("configuration/com.openexchange.upsell.multiple.downgrade", _("OX Access Level"));
+var innode = new ox.Configuration.InnerNode("configuration/com.openexchange.upsell.multiple.downgrade", _("OX Access-Level"));
 
 var downgradepanel = new ox.Configuration.LeafNode("configuration/com.openexchange.upsell.multiple.downgrade/downgrade_panel", _("Downgrade"));
 
@@ -8,7 +8,7 @@ var downpage = new ox.Configuration.Page(downgradepanel, _("Change Access Permis
 var intro = new ox.UI.Text(_("The Downgrade Premium function will downgrade an Open-Xchange Premium Groupware context (tenant) to a simple Open-Xchange PIM context (tenant). Please re-login to the context to use the downgraded version. At any time you can UPGRADE again to the Premium Groupware by simply clicking the upsell triggers (e.g. InfoStore icon in the main module bar) and follow the upsell process."));
 downpage.addWidget(intro);
 
-var button = new ox.UI.Button(_("Downgrade Context NOW!"));
+var button = new ox.UI.Button(_("Downgrade Context"));
 
 button.click = function (){
 
