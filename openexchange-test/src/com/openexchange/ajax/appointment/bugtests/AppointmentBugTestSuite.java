312010e6b0b8ea9353bf39ef3252ba6cdd8ed457
Martin Herfurth <martin.herfurth@open-xchange.com>
2010-12-06 14:27:56 +0000

    Bugfix 17264

--- openexchange-test/src/com/openexchange/ajax/appointment/bugtests/AppointmentBugTestSuite.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/bugtests/AppointmentBugTestSuite.java
@@ -122,6 +122,7 @@ public class AppointmentBugTestSuite extends TestSuite{
         tests.addTestSuite(Bug16249Test.class);
         tests.addTestSuite(Bug16579Test.class);
         tests.addTestSuite(Bug17175Test.class);
+        tests.addTestSuite(Bug17264Test.class);
         return tests;
     }
 }
