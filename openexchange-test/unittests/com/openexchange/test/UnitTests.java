c0543a105541fc4b31d95f263313f89f1d73b122
Tobias Prinz <tobias.prinz@open-xchange.com>
2011-01-18 14:07:53 +0000

    removed test for Params because of dependency bullsh*t

--- openexchange-test/unittests/com/openexchange/test/UnitTests.java
+++ openexchange-test/unittests/com/openexchange/test/UnitTests.java
@@ -52,7 +52,6 @@ package com.openexchange.test;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import com.openexchange.ajax.framework.ParamsTest;
 import com.openexchange.groupware.importexport.ImportExportStandaloneSuite;
 import com.openexchange.groupware.infostore.WebdavFolderAliasesTest;
 import com.openexchange.pubsub.TemplateTestSuite;
@@ -168,7 +167,6 @@ public class UnitTests {
         tests.addTestSuite(com.openexchange.webdav.infostore.integration.LockExpiryTest.class);
         
         tests.addTest(ImportExportStandaloneSuite.suite());
-        tests.addTestSuite(ParamsTest.class);
         tests.addTest(TemplateTestSuite.suite());
         
         // Slow Tests
