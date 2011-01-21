2852a09ed9362273ba9cc9564c8c207a114cbfbc
tierlieb <tierlieb>
2011-01-21 13:38:03 +0000

    helper class for transforming streams

---
+++ common/src/com/openexchange/java/Streams.java
@@ -0,0 +1,18 @@
+package com.openexchange.java;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Streams {
+	
+	public static byte[] stream2bytes(InputStream is) throws IOException{
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		int buff;
+		
+		while((buff = is.read()) != -1)
+			bos.write(buff);
+		
+		return bos.toByteArray(); 
+	}
+}
