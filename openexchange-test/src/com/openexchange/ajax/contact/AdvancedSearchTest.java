71afa238dc0744ce93aab91990aefc195150105e
Tobias Prinz <tobias.prinz@open-xchange.com>
2011-02-16 15:22:35 +0000

    updated test

--- openexchange-test/src/com/openexchange/ajax/contact/AdvancedSearchTest.java
+++ openexchange-test/src/com/openexchange/ajax/contact/AdvancedSearchTest.java
@@ -1,6 +1,9 @@
 package com.openexchange.ajax.contact;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
 import org.json.JSONObject;
 import com.openexchange.ajax.contact.action.AdvancedSearchRequest;
@@ -20,18 +23,15 @@ public class AdvancedSearchTest extends AbstractManagedContactTest{
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
-		Contact alice = ContactTestManager.generateContact();
+		Contact alice = ContactTestManager.generateContact(folderID);
 		alice.setGivenName("Alice");
-		alice.setParentFolderID(folderID);
 		
-		Contact bob = ContactTestManager.generateContact();
+		Contact bob = ContactTestManager.generateContact(folderID);
 		bob.setGivenName("Bob");
 		bob.setSurName(BOB_LASTNAME);
-		bob.setParentFolderID(folderID);
 		
-		Contact charlie = ContactTestManager.generateContact();
+		Contact charlie = ContactTestManager.generateContact(folderID);
 		charlie.setGivenName("Charlie");
-		charlie.setParentFolderID(folderID);
 		
 		manager.newAction(alice,bob,charlie);
 	}
@@ -112,6 +112,126 @@ public class AdvancedSearchTest extends AbstractManagedContactTest{
 		
 		assertEquals(BOB_LASTNAME, actual);
 	}
+	
+	public void testSearchAlphabetRange() throws Exception{
+		ContactField field = ContactField.GIVEN_NAME;
+		JSONObject filter = new JSONObject(
+				"{'filter' : [ 'and', " +
+					"['>=' , {'field' : '"+field.getAjaxName()+"'} , 'A'], " +
+					"['<' , {'field' : '"+field.getAjaxName()+"'}, 'C'], " +
+					"['=' , {'field' : '"+ContactField.FOLDER_ID.getAjaxName()+"'}, "+folderID+"]" +
+				"]})");
+		
+		AdvancedSearchRequest request = new AdvancedSearchRequest(filter, new int[]{field.getNumber()}, -1, null);
+		CommonSearchResponse response = getClient().execute(request);
+		assertFalse("Should work", response.hasError());
+		
+		Object[][] resultTable = response.getArray();
+		assertNotNull("Should find at least a result", resultTable);
+		assertEquals("Should find two results", 2, resultTable.length);
+		
+		int columnPos = response.getColumnPos(field.getNumber());
+		Set<String> names = new HashSet<String>();
+		names.add( (String) resultTable[0][columnPos] );
+		names.add( (String) resultTable[1][columnPos] );
+		
+		assertTrue(names.contains("Bob"));
+		assertTrue(names.contains("Alice"));
+	}
 
+	public void testSearchOrdering() throws Exception{
+		manager.newAction(
+			ContactTestManager.generateContact(folderID, "Elvis"),
+			ContactTestManager.generateContact(folderID, "Feelvis"),
+			ContactTestManager.generateContact(folderID, "Gelvis"),
+			ContactTestManager.generateContact(folderID, "Geena"),
+			ContactTestManager.generateContact(folderID, "Hellvis")
+		);
+		ContactField field = ContactField.SUR_NAME;
+		JSONObject filter = new JSONObject(
+				"{'filter' : [ 'and', " +
+					"['>=' , {'field' : '"+field.getAjaxName()+"'} , 'E'], " +
+					"['<' , {'field' : '"+field.getAjaxName()+"'}, 'I'], " +
+					"['NOT' , ['=' , {'field' : '"+field.getAjaxName()+"'}, 'Geena']], " +
+					"['=' , {'field' : '"+ContactField.FOLDER_ID.getAjaxName()+"'}, "+folderID+"]" +
+				"]})");
+		
+		AdvancedSearchRequest request = new AdvancedSearchRequest(filter, new int[]{field.getNumber()}, field.getNumber(), "asc");
+		CommonSearchResponse response = getClient().execute(request);
+		assertFalse("Should work", response.hasError());
+		
+		Object[][] resultTable = response.getArray();
+		assertNotNull("Should find at least a result", resultTable);
+		assertEquals("Should find four results", 4, resultTable.length);
+		
+		int columnPos = response.getColumnPos(field.getNumber());
+		
+		assertTrue("Result should appear in the right order", resultTable[0][columnPos].equals("Elvis"));
+		assertTrue("Result should appear in the right order", resultTable[1][columnPos].equals("Feelvis"));
+		assertTrue("Result should appear in the right order", resultTable[2][columnPos].equals("Gelvis"));
+		assertTrue("Result should appear in the right order", resultTable[3][columnPos].equals("Hellvis"));
+		
+		/* invert it */
+		request = new AdvancedSearchRequest(filter, new int[]{field.getNumber()}, field.getNumber(), "desc");
+		response = getClient().execute(request);
+		assertFalse("Should work", response.hasError());
+		
+		resultTable = response.getArray();
+		assertNotNull("Should find at least a result", resultTable);
+		assertEquals("Should find four results", 4, resultTable.length);
+		
+		columnPos = response.getColumnPos(field.getNumber());
+		
+		assertTrue("Result should appear in the right order", resultTable[0][columnPos].equals("Hellvis"));
+		assertTrue("Result should appear in the right order", resultTable[1][columnPos].equals("Gelvis"));
+		assertTrue("Result should appear in the right order", resultTable[2][columnPos].equals("Feelvis"));
+		assertTrue("Result should appear in the right order", resultTable[3][columnPos].equals("Elvis"));
 
+	}
+	
+	public void testSearchOrderingWithKana() throws Exception{
+		manager.newAction(
+				ContactTestManager.generateContact(folderID, "\u30ef"),
+				ContactTestManager.generateContact(folderID, "\u30e9"),
+				ContactTestManager.generateContact(folderID, "\u30e4"),
+				ContactTestManager.generateContact(folderID, "\u30de"),
+				ContactTestManager.generateContact(folderID, "\u30cf"),
+				ContactTestManager.generateContact(folderID, "\u30ca"),
+				ContactTestManager.generateContact(folderID, "\u30bf"),
+				ContactTestManager.generateContact(folderID, "\u30b5"),
+				ContactTestManager.generateContact(folderID, "\u30ab"),
+				ContactTestManager.generateContact(folderID, "\u30a2")
+		);
+		
+		String[] letters = new String[]{"\u30a2", "\u30ab", "\u30b5", "\u30bf", "\u30ca", "\u30cf", "\u30de", "\u30e4", "\u30e9", "\u30ef"};
+		
+		ContactField field = ContactField.SUR_NAME;
+		LinkedList<JSONObject> filters = new LinkedList<JSONObject>();
+		for(int i = 0; i < letters.length -1; i++)
+			filters.add( new JSONObject(
+				"{'filter' : [ 'and', " +
+					"['>=' , {'field' : '"+field.getAjaxName()+"'} , '"+letters[i]+"'], " +
+					"['<' , {'field' : '"+field.getAjaxName()+"'}, '"+letters[i+1]+"'], " +
+					"['=' , {'field' : '"+ContactField.FOLDER_ID.getAjaxName()+"'}, "+folderID+"]" +
+				"]})"));
+		
+		int currentPosition = 0;
+		for(JSONObject filter: filters){
+			String ident = "Step #"+currentPosition + " from "+letters[currentPosition]+" to "+letters[currentPosition+1]+": ";
+			AdvancedSearchRequest request = new AdvancedSearchRequest(filter, new int[]{field.getNumber()}, field.getNumber(), "asc");
+			CommonSearchResponse response = getClient().execute(request);
+			assertFalse(ident+"Should work", response.hasError());
+			
+			Object[][] resultTable = response.getArray();
+			assertNotNull(ident+"Should find at least a result", resultTable);
+			assertEquals(ident+"Should find one result", 1, resultTable.length);
+			
+			int columnPos = response.getColumnPos(field.getNumber());
+			
+			assertEquals(ident+"Result should appear in the right order", letters[currentPosition], resultTable[0][columnPos]);
+			currentPosition++;
+		}
+	}
+	
+	
 }
