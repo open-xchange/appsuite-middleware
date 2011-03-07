ca2f34996b7da9518753549118895439231bb844
steffen <steffen>
2011-03-07 08:49:22 +0000

    Bugfix 18219 for PTF 650: Unable to access publications created by PIM users

--- com.openexchange.publish.microformats/src/com/openexchange/publish/microformats/MicroformatServlet.java
+++ com.openexchange.publish.microformats/src/com/openexchange/publish/microformats/MicroformatServlet.java
@@ -28,7 +28,7 @@
  *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
  *    given Attribution for the derivative code and a license granting use.
  *
- *     Copyright (C) 2004-2006 Open-Xchange, Inc.
+ *     Copyright (C) 2004-2010 Open-Xchange, Inc.
  *     Mail: info@open-xchange.com
  *
  *
@@ -51,11 +51,11 @@ package com.openexchange.publish.microformats;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,14 +65,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import com.openexchange.context.ContextService;
+import com.openexchange.api2.OXException;
+import com.openexchange.config.ConfigurationService;
+import com.openexchange.groupware.contact.ContactInterface;
+import com.openexchange.groupware.contact.ContactInterfaceDiscoveryService;
+import com.openexchange.groupware.container.Contact;
+import com.openexchange.groupware.container.FolderObject;
 import com.openexchange.groupware.contexts.Context;
+import com.openexchange.groupware.ldap.User;
+import com.openexchange.groupware.ldap.UserException;
 import com.openexchange.java.Strings;
 import com.openexchange.publish.Publication;
 import com.openexchange.publish.PublicationDataLoaderService;
+import com.openexchange.publish.microformats.osgi.StringTranslator;
 import com.openexchange.publish.microformats.tools.UncloseableWriter;
+import com.openexchange.publish.tools.PublicationSession;
 import com.openexchange.templating.OXTemplate;
-import com.openexchange.templating.TemplateService;
+import com.openexchange.user.UserService;
 
 /**
  * {@link MicroformatServlet}
@@ -81,57 +90,86 @@ import com.openexchange.templating.TemplateService;
  */
 public class MicroformatServlet extends OnlinePublicationServlet {
 
-    private static final Map<String, OXMFPublicationService> publishers = new HashMap<String, OXMFPublicationService>();
+    private static final long serialVersionUID = 6727750981539640363L;
 
-    private static final Map<String, String> templateNames = new HashMap<String, String>();
+    private static final Map<String, OXMFPublicationService> publishers = new HashMap<String, OXMFPublicationService>();
 
     private static final Log LOG = LogFactory.getLog(MicroformatServlet.class);
 
     private static final String MODULE = "module";
 
     private static final String SITE = "site";
-    
+
     private static final String CONTEXTID = "ctx";
 
     private static PublicationDataLoaderService dataLoader = null;
 
-    private static TemplateService templateService = null;
-    
+
     private static Map<String, Map<String, Object>> additionalTemplateVariables = new HashMap<String, Map<String, Object>>();
+
+    private static UserService userService;
+
+    private static StringTranslator translator;
+
+    private static ConfigurationService configService;
+
+    private static ContactInterfaceDiscoveryService contacts;
 
     private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     static {
         DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
-    
-    public static void setPublicationDataLoaderService(PublicationDataLoaderService service) {
+
+    private static final DateFormat TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd H:m:s.S z");
+    static {
+        TIME_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
+    public MicroformatServlet() {
+        super();
+    }
+
+    public static void setPublicationDataLoaderService(final PublicationDataLoaderService service) {
         dataLoader = service;
     }
 
-    public static void setTemplateService(TemplateService service) {
-        templateService = service;
+
+    public static void setUserService(final UserService service) {
+        userService = service;
     }
 
-    public static void registerType(String module, OXMFPublicationService publisher, String templateName, Map<String, Object> additionalVars) {
+    public static void setStringTranslator(final StringTranslator trans) {
+        translator = trans;
+    }
+    
+    public static void setConfigService(final ConfigurationService confService) {
+        configService = confService;
+    }
+
+    public static void registerType(final String module, final OXMFPublicationService publisher, final Map<String, Object> additionalVars) {
         publishers.put(module, publisher);
-        templateNames.put(module, templateName);
         additionalTemplateVariables.put(module, additionalVars);
     }
 
+    public static void setContactInterfaceDiscoveryService(final ContactInterfaceDiscoveryService service) {
+        contacts = service;
+    }
+
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
         try {
             resp.setContentType("text/html; charset=UTF-8");
-            Map<String, String> args = getPublicationArguments(req);
-            String module = args.get(MODULE);
+            final Map<String, String> args = getPublicationArguments(req);
+            final String module = args.get(MODULE);
 
-            OXMFPublicationService publisher = publishers.get(module);
+            final OXMFPublicationService publisher = publishers.get(module);
             if (publisher == null) {
                 resp.getWriter().println("Don't know how to handle module " + module);
                 return;
             }
-            Context ctx = contexts.getContext(Integer.parseInt(args.get(CONTEXTID)));
-            Publication publication = publisher.getPublication(ctx, args.get(SITE));
+            final Context ctx = contexts.getContext(Integer.parseInt(args.get(CONTEXTID)));
+            final Publication publication = publisher.getPublication(ctx, args.get(SITE));
             if (publication == null) {
                 resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 resp.getWriter().println("Don't know site " + args.get(SITE));
@@ -142,55 +180,88 @@ public class MicroformatServlet extends OnlinePublicationServlet {
                 return;
             }
 
-            Collection<? extends Object> loaded = dataLoader.load(publication);
+            final Collection<? extends Object> loaded = dataLoader.load(publication);
 
-            HashMap<String, Object> variables = new HashMap<String, Object>();
+            final HashMap<String, Object> variables = new HashMap<String, Object>();
+            final User user = getUser(publication);
+            final Contact userContact = getContact(new PublicationSession(publication), publication.getContext());
+            
             variables.put(getCollectionName(module), loaded);
             variables.put("publication", publication);
             variables.put("request", req);
             variables.put("dateFormat", DATE_FORMAT);
-            if(additionalTemplateVariables.containsKey(module)) {
+            variables.put("timeFormat", TIME_FORMAT);
+            String admin = configService.getProperty("PUBLISH_REVOKE");
+            if(admin == null || admin.equals("")) {
+                admin = userService.getUser(ctx.getMailadmin(), ctx).getMail();
+            }
+            final String privacyText = formatPrivacyText(
+                getPrivacyText(user),
+                admin,
+                user,
+                new Date());
+            variables.put("privacy", privacyText ); //TODO Use lastmodified once someone implements this.
+            variables.put("userContact", userContact); 
+
+            if (additionalTemplateVariables.containsKey(module)) {
                 variables.putAll(additionalTemplateVariables.get(module));
             }
-            String templateName = templateNames.get(module);
-            OXTemplate template = templateService.loadTemplate(templateName);
-
+            
+            final OXTemplate template = publisher.loadTemplate(publication);
             template.process(variables, new UncloseableWriter(resp.getWriter()));
 
-        } catch (Throwable t) {
+        } catch (final Throwable t) {
             LOG.error(t.getMessage(), t);
             resp.getWriter().println("An exception occurred: ");
             t.printStackTrace(resp.getWriter());
         }
     }
 
-    private String getCollectionName(String module) {
-        if (module.equals("contacts")) {
-            return "contacts";
-        }
-        return null;
+    private Contact getContact(final PublicationSession publicationSession, final Context context) throws OXException {
+        final ContactInterface contactInterface = contacts.getContactInterfaceProvider(FolderObject.SYSTEM_LDAP_FOLDER_ID, context.getContextId()).newContactInterface(publicationSession);
+        final Contact contact = contactInterface.getUserById(publicationSession.getUserId(), false);
+        return contact;
     }
 
 
-    private Map<String, String> getPublicationArguments(HttpServletRequest req) throws UnsupportedEncodingException {
-        String[] path = req.getPathInfo().split("/");
-        List<String> normalized = new ArrayList<String>(path.length);
+    private String formatPrivacyText(final String privacyText, final String adminAddress, final User user, final Date creationDate) {
+        final String date = new SimpleDateFormat("yyyy-MM-dd").format(creationDate);
+        final String retVal = String.format(privacyText, adminAddress, user.getMail(), date);
+        return "<p>" +retVal.replaceAll("\n", "</p><p>") + "</p>";
+    }
+
+    private String getCollectionName(final String module) {
+        return module;
+    }
+
+    private Map<String, String> getPublicationArguments(final HttpServletRequest req) throws UnsupportedEncodingException {
+        final String[] path = SPLIT.split(req.getPathInfo(), 0);
+        final List<String> normalized = new ArrayList<String>(path.length);
         for (int i = 0; i < path.length; i++) {
             if (!path[i].equals("")) {
                 normalized.add(path[i]);
             }
         }
 
-        int startIndex = normalized.indexOf("publications");
-        if (startIndex == -1) {
-            throw new IllegalArgumentException("This does not look like a valid path: " + req.getPathInfo());
-        }
-        String site = Strings.join(decode(normalized.subList(startIndex + 3, normalized.size()), req), "/");
-        Map<String, String> args = new HashMap<String, String>();
-        args.put(MODULE, normalized.get(startIndex + 1));
-        args.put(CONTEXTID, normalized.get(startIndex + 2));
+        final String site = Strings.join(decode(normalized.subList(2, normalized.size()), req), "/");
+        final Map<String, String> args = new HashMap<String, String>();
+        args.put(MODULE, normalized.get(0));
+        args.put(CONTEXTID, normalized.get(1));
         args.put(SITE, site);
         args.put(SECRET, req.getParameter(SECRET));
         return args;
     }
+
+    private User getUser(final Publication publication) throws UserException {
+        final Context context = publication.getContext();
+        final int uid = publication.getUserId();
+        final User user = userService.getUser(uid, context);
+        return user;
+    }
+    
+    private String getPrivacyText(final User user){
+        return translator.translate(user.getLocale(), MicroformatStrings.DISCLAIMER_PRIVACY);
+    }
+
+
 }
