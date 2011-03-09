bbfd7f45005a453f48fe101f1d3ed20a8b33b9ac
tierlieb <tierlieb>
2011-03-09 09:22:01 +0000

    added unimplemented method

--- com.openexchange.contacts.ldap/src/com/openexchange/contacts/ldap/contacts/LdapContactInterface.java
+++ com.openexchange.contacts.ldap/src/com/openexchange/contacts/ldap/contacts/LdapContactInterface.java
@@ -786,10 +786,9 @@ public class LdapContactInterface implements ContactInterface {
         throw new UnsupportedOperationException();
 	}
 
-
-	public SearchIterator<Contact> searchContacts(SearchTerm term, int orderBy,
-			String orderDir, int[] cols) throws OXException {
-		// TODO Auto-generated method stub
-		return null;
+	public <T> SearchIterator<Contact> getContactsByExtendedSearch(
+			SearchTerm<T> searchterm, int orderBy, String orderDir,
+			String collation, int[] cols) throws OXException {
+        throw new UnsupportedOperationException();
 	}
 }
