3f0f82c830739b1bd32879fa648585f432391309
tierlieb <tierlieb>
2011-03-16 10:53:35 +0000

    fixes to enable Viktor to test bug18109

--- common/src/com/openexchange/l10n/SuperCollator.java
+++ common/src/com/openexchange/l10n/SuperCollator.java
@@ -1,4 +1,5 @@
 package com.openexchange.l10n;
+
 /*
  *
  *    OPEN-XCHANGE legal information
@@ -48,31 +49,29 @@ package com.openexchange.l10n;
  *
  */
 
-
-
 import java.util.Locale;
 
 /**
- * This enum combines collations, charsets and locales for Java and MySQL
- * into one consistent package. This way, you always know which collation 
- * to use for a Java locale or the other way around.
- * Note that the only unique attribute is the mysql collation: Both locales
- * and charsets might occur more than once.
+ * This enum combines collations, charsets and locales for Java and MySQL into
+ * one consistent package. This way, you always know which collation to use for
+ * a Java locale or the other way around. Note that the only unique attribute is
+ * the mysql collation: Both locales and charsets might occur more than once.
  * 
  * @author tobiasp
- *
+ * 
  */
 public enum SuperCollator {
-	PRC_1(Locale.PRC, "gb2312", "gb2312_chinese_ci"),
-	PRC_2(Locale.PRC, "gbk", "gbk_chinese_ci"),
-	CHINA_TAIWAN(Locale.TAIWAN, "big5", "big5_chinese_ci"),
-	CHINA_2(Locale.CHINESE, "gb2312", "gb2312_chinese_ci"),
-	CHINA_3(Locale.CHINA, "gb2312", "gb2312_chinese_ci"),
-	DEFAULT(Locale.getDefault(), "utf8", "utf8_general_ci");
-	
+	PRC_1		("PRC", Locale.PRC, "gb2312", "gb2312_chinese_ci"), 
+	PRC_2		("PRC", Locale.PRC, "gbk","gbk_chinese_ci"), 
+	CHINA_TAIWAN("TAIWAN",Locale.TAIWAN, "big5","big5_chinese_ci"), 
+	CHINA_2		("CHINESE", Locale.CHINESE, "gb2312","gb2312_chinese_ci"), 
+	CHINA_3		("CHINA", Locale.CHINA, "gb2312",	"gb2312_chinese_ci"), 
+	DEFAULT		("DEFAULT", Locale.getDefault(), "utf8","utf8_general_ci");
+
 	private String collation;
 	private String sqlCharset;
 	private Locale locale;
+	private String name;
 
 	public String getSqlCollation() {
 		return collation;
@@ -86,27 +85,93 @@ public enum SuperCollator {
 		return locale;
 	}
 
-	SuperCollator(Locale local, String sqlCharset, String collation){
+	public String getName() {
+		return name;
+	}
+	SuperCollator(String name, Locale local, String sqlCharset, String collation) {
+		this.name = name;
 		this.locale = local;
 		this.sqlCharset = sqlCharset;
 		this.collation = collation;
 	}
-	
-	public static SuperCollator get(String something){
-		for(SuperCollator loc : values()){
-			if(loc.getSqlCollation().equalsIgnoreCase(something))
-				return loc;
-			if(loc.getSqlCharset().equalsIgnoreCase(something))
-				return loc;
-			
-			Locale javaLocale = loc.getJavaLocale();
-			if(javaLocale.getCountry().equalsIgnoreCase(something))
-				return loc;
-			if(javaLocale.getLanguage().equalsIgnoreCase(something))
-				return loc;
-			if(javaLocale.getVariant().equalsIgnoreCase(something))
+
+	public static SuperCollator get(String something) {
+		SuperCollator result = null;
+
+		result = getBySqlCollation(something);
+		if (result != null)
+			return result;
+
+		result = getBySqlCharset(something);
+		if (result != null)
+			return result;
+
+		result = getByJavaLocale(something);
+		if (result != null)
+			return result;
+
+		result = getByName(something);
+		if (result != null)
+			return result;
+		
+		return result;
+	}
+
+	public static SuperCollator getByJavaLocale(String something) {
+		String[] parts = something.split("_");
+
+		String lang = null, country = null, variant = null;
+		Locale javaLocale;
+
+		if (parts.length > 0)
+			lang = parts[0];
+		if (parts.length > 1)
+			country = parts[1];
+		if (parts.length > 2)
+			variant = parts[2];
+
+		for (SuperCollator loc : values()) {
+			javaLocale = loc.getJavaLocale();
+			if (javaLocale.getVariant().equalsIgnoreCase(variant)
+				&& javaLocale.getCountry().equalsIgnoreCase(country)
+				&& javaLocale.getLanguage().equalsIgnoreCase(lang))
 				return loc;
 		}
+
+		for (SuperCollator loc : values()) {
+			javaLocale = loc.getJavaLocale();
+			if (javaLocale.getCountry().equalsIgnoreCase(country)
+				&& javaLocale.getLanguage().equalsIgnoreCase(lang))
+				return loc;
+		}
+
+		for (SuperCollator loc : values()) {
+			javaLocale = loc.getJavaLocale();
+			if (javaLocale.getLanguage().equalsIgnoreCase(lang))
+				return loc;
+		}
+
+		return null;
+	}
+
+	public static SuperCollator getBySqlCollation(String something) {
+		for (SuperCollator loc : values())
+			if (loc.getSqlCollation().equalsIgnoreCase(something))
+				return loc;
+		return null;
+	}
+
+	public static SuperCollator getBySqlCharset(String something) {
+		for (SuperCollator loc : values())
+			if (loc.getSqlCharset().equalsIgnoreCase(something))
+				return loc;
+		return null;
+	}
+	
+	public static SuperCollator getByName(String something) {
+		for (SuperCollator loc : values())
+			if (loc.getName().equalsIgnoreCase(something))
+				return loc;
 		return null;
 	}
 
