c745e3f693b9bd20d470eb9fdbc2dfa361b24c31
Martin Herfurth <martin.herfurth@open-xchange.com>
2011-03-18 11:01:47 +0000

    removed une... unnecce... unnecese... unnecesse... not needed rmi call

--- com.openexchange.admin.soap/src/com/openexchange/admin/soap/OXUser.java
+++ com.openexchange.admin.soap/src/com/openexchange/admin/soap/OXUser.java
@@ -422,29 +422,6 @@ public class OXUser extends OXSOAPRMIMapper {
             return ((OXUserInterface)rmistub).listAll(ctx, auth);
         }
     }
-    
-
-    /**
-     * 
-     * @param ctx Context object
-     * @param filter The call affects only users with exactly this access combination. -1 for no filter.
-     * @param addAccess Access rights to be added
-     * @param removeAccess Access rights to be removed
-     * @throws InvalidCredentialsException 
-     * @throws StorageException 
-     * @throws InvalidDataException 
-     * @throws DatabaseUpdateException 
-     * @throws NoSuchContextException 
-     */
-    public void changeModuleAccessGlobal(int filter, UserModuleAccess addAccess, UserModuleAccess removeAccess, Credentials auth) throws RemoteException, InvalidCredentialsException, StorageException {
-        reconnect();
-        try {
-            ((OXUserInterface)rmistub).changeModuleAccessGlobal(filter, addAccess, removeAccess, auth);
-        } catch (ConnectException e) {
-            reconnect(true);
-            ((OXUserInterface)rmistub).changeModuleAccessGlobal(filter, addAccess, removeAccess, auth);
-        }
-    }
 
     /**
      * 
