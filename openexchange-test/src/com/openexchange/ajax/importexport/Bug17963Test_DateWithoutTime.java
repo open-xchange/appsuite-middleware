e65a9ad179d5059e7bc4bb2678da9fbf4b31ee6e
Tobias Prinz <tobias.prinz@open-xchange.com>
2011-03-25 16:30:09 +0000

    fix for 17963 (EXDATE date without time)

---
+++ openexchange-test/src/com/openexchange/ajax/importexport/Bug17963Test_DateWithoutTime.java
@@ -0,0 +1,32 @@
+package com.openexchange.ajax.importexport;
+
+import com.openexchange.ajax.appointment.recurrence.ManagedAppointmentTest;
+import com.openexchange.ajax.importexport.actions.ICalImportRequest;
+import com.openexchange.ajax.importexport.actions.ICalImportResponse;
+import com.openexchange.groupware.importexport.ImportResult;
+
+public class Bug17963Test_DateWithoutTime extends ManagedAppointmentTest{
+
+	public Bug17963Test_DateWithoutTime(String name) {
+		super(name);
+	}
+
+	public void testDateWithoutTime() throws Exception{
+		String ical = 
+		"BEGIN:VCALENDAR\n"+
+		"VERSION:2.0\n"+
+		"BEGIN:VEVENT\n"+
+		"DTSTART;TZID=Europe/Rome:20100202T103000\n"+
+		"DTEND;TZID=Europe/Rome:20100202T120000\n"+
+		"RRULE:FREQ=WEEKLY;BYDAY=TU;UNTIL=20100705T215959Z\n"+
+		"EXDATE:20111128\n"+
+		"DTSTAMP:20110105T174810Z\n"+
+		"SUMMARY:Team-Meeting\n"+
+		"END:VEVENT\n";
+		
+		ICalImportRequest request = new ICalImportRequest(folder.getObjectID(), ical);
+		ICalImportResponse response = getClient().execute(request);
+		ImportResult[] imports = response.getImports();
+		assertEquals(1, imports.length);
+	}
+}
