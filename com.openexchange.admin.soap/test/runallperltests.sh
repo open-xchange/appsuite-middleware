c76b400cde1d5ce78f999a4696b3281387e1db8b
Carsten Hoeger <carsten.hoeger@open-xchange.com>
2011-03-30 07:39:24 +0000

    updated testcases to be run from jenkins

---
+++ com.openexchange.admin.soap/test/runallperltests.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+runcmd() {
+   cmd="$1"
+   arg="$2"
+   ./$cmd $arg|| { echo "running $cmd failed"; exit 1; }
+}
+
+cmdpath="$1/perl"
+test -z "$1" && cmdpath=./perl
+
+cd $cmdpath
+
+
+runcmd registerdatabase
+runcmd registerfilestore | cut -d" " -f 2 > /tmp/fnr
+read FNR < /tmp/fnr && rm -f /tmp/fnr
+runcmd registerserver
+runcmd createcontext
+runcmd createuser
+runcmd creategroup
+runcmd createresource
+runcmd changeresource
+runcmd changeuser
+runcmd listcontext
+runcmd listdatabase
+runcmd listfilestore
+runcmd listgroup
+runcmd listresource
+runcmd listserver
+runcmd listuser
+runcmd deletegroup
+runcmd deleteresource
+runcmd deleteuser
+runcmd deletecontext
+runcmd unregisterdatabase
+runcmd unregisterfilestore $FNR
+runcmd unregisterserver
+
+cd -
