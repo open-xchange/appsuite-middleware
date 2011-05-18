a06f9c48b7771b7fca7f66918589a73a238b01a1
Marcus Klein <marcus.klein@open-xchange.com>
2011-05-18 10:34:17 +0000

    removed additional AJAX client from constructor

--- openexchange-test/src/com/openexchange/ajax/mail/CountMailTest.java
+++ openexchange-test/src/com/openexchange/ajax/mail/CountMailTest.java
@@ -1,13 +1,58 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2011 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
 package com.openexchange.ajax.mail;
 
 import java.io.IOException;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.xml.sax.SAXException;
 import com.openexchange.ajax.folder.actions.API;
-import com.openexchange.ajax.framework.AJAXClient;
-import com.openexchange.ajax.framework.AJAXClient.User;
-import com.openexchange.configuration.ConfigurationException;
 import com.openexchange.folder.json.FolderField;
 import com.openexchange.tools.servlet.AjaxException;
 
@@ -22,9 +67,8 @@ public class CountMailTest extends AbstractMailTest {
 
     protected String folder;
 
-    public CountMailTest(final String name) throws ConfigurationException, AjaxException, IOException, SAXException, JSONException {
+    public CountMailTest(final String name) {
         super(name);
-        this.client = new AJAXClient(User.User2);
     }
     
     @Override
@@ -38,16 +82,16 @@ public class CountMailTest extends AbstractMailTest {
         clearFolder(folder);
         super.tearDown();
     }
-    
+
     public void testCounting() throws Exception {
         clearFolder(folder);
-        assertEquals("Should be empty", 0, count(folder) );
+        assertEquals("Should be empty", 0, count(folder));
 
-        for(int number = 1; number < 10; number++){
+        for (int number = 1; number < 10; number++) {
             sendMail(generateMail());
             assertEquals("Does not contain the expected number of elements in folder "+folder, number, count(folder) );    
         }
-        
+
         clearFolder(folder);
         assertEquals("Should be empty again", 0, count(folder) );
     }
@@ -59,5 +103,4 @@ public class CountMailTest extends AbstractMailTest {
                 new com.openexchange.ajax.folder.actions.GetRequest(API.OX_NEW, folder, new int[] { FolderField.TOTAL.getColumn() })).getData();
         return data.getInt(FolderField.TOTAL.getName());
     }
-
 }
