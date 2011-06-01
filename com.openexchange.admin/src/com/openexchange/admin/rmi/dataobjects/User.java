16e3af05c505ffe3dee2cac573e457dca8c3e976
marcus <marcus>
2011-06-01 19:32:30 +0000

    removed unused import

--- com.openexchange.admin/src/com/openexchange/admin/rmi/dataobjects/User.java
+++ com.openexchange.admin/src/com/openexchange/admin/rmi/dataobjects/User.java
@@ -53,7 +53,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
