06accaadb07cf0cd378d75ab9f3f3b9940c26997
marcus <marcus>
2011-06-01 19:32:08 +0000

    fixed wrong logic on mail import

--- server/src/com/openexchange/ajax/Mail.java
+++ server/src/com/openexchange/ajax/Mail.java
@@ -3486,7 +3486,7 @@ public class Mail extends PermissionServlet implements UploadListener {
 
         private final BlockingQueue<MimeMessage> queue;
 
-        private AbstractOXException exception = null;
+        private AbstractOXException exception;
 
         AppenderTask(final MailServletInterface mailInterface, final String folder, final boolean force, final int flags, final BlockingQueue<MimeMessage> queue) {
             super();
@@ -3513,7 +3513,7 @@ public class Mail extends PermissionServlet implements UploadListener {
                     }
                     queue.drainTo(messages);
                     for (final MimeMessage message : messages) {
-                        message.getHeader("Date", null);
+                        final String s = message.getHeader("Date", null);
                         final MailMessage mm = MIMEMessageConverter.convertMessage(message);
                         mails.add(mm);
                     }
@@ -3544,16 +3544,12 @@ public class Mail extends PermissionServlet implements UploadListener {
         public void setThreadName(final ThreadRenamer threadRenamer) {
             threadRenamer.rename("Mail Import Thread");
         }
-        
-        public boolean hasException() {
-            return exception != null;
-        }
     }
 
     private final Response actionPostImportMail(final ServerSession session, final HttpServletRequest req, final ParamContainer paramContainer) {
         final Response response = new Response();
         JSONValue responseData = null;
-        
+        AppenderTask task;
         try {
             final String folder = paramContainer.checkStringParam(PARAMETER_FOLDERID);
             final int flags;
@@ -3578,7 +3574,7 @@ public class Mail extends PermissionServlet implements UploadListener {
                 if (ServletFileUpload.isMultipartContent(req)) {
                     final ThreadPoolService service = ServerServiceRegistry.getInstance().getService(ThreadPoolService.class, true);
                     final BlockingQueue<MimeMessage> queue = new ArrayBlockingQueue<MimeMessage>(100);
-                    final AppenderTask task = new AppenderTask(MailServletInterface.getInstance(session), folder, force, flags, queue);
+                    task = new AppenderTask(MailServletInterface.getInstance(session), folder, force, flags, queue);
                     future = service.submit(task);
                     try {
                         final FileItemIterator iter = servletFileUpload.getItemIterator(req);
@@ -3594,13 +3590,8 @@ public class Mail extends PermissionServlet implements UploadListener {
                                 message.setFrom(defaultSendAddr);
                             }
                             message.setFileName(filename);
-
-                            while (!task.hasException() && !queue.offer(message)) {
-                                continue;
-                            }
-                            
-                            if (task.hasException()) {
-                                keepgoing = false;
+                            while (keepgoing && !queue.offer(message, 1, TimeUnit.SECONDS)) {
+                                keepgoing = !future.isDone();
                             }
                         }
                     } finally {
@@ -4057,7 +4048,7 @@ public class Mail extends PermissionServlet implements UploadListener {
                  * measure the size.
                  */
                 file.setFileSize(0);
-                if (!set.contains(Field.TITLE)) {
+                if (!set.contains(Metadata.TITLE_LITERAL)) {
                     file.setTitle(mailPart.getFileName());
                 }
                 file.setFolderId(destFolderID);
