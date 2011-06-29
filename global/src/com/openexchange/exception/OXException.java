7f853a27ce139973a6be722b28afe1aef815d99b
thorben <thorben.betten@open-xchange.com>
2011-06-29 22:58:37 +0200

    foo

--- global/src/com/openexchange/exception/OXException.java
+++ global/src/com/openexchange/exception/OXException.java
@@ -114,6 +114,7 @@ public class OXException extends RuntimeException implements OXExceptionConstant
         properties = new HashMap<String, String>(8);
         categories = new LinkedList<Category>();
         categories.add(CATEGORY_DEFAULT);
+        // foo bar
     }
 
     public String getDetailedMessage() {
