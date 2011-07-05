886765f8bfe1401476b219d12031de6e532de9d0
karsten <karsten>
2011-07-05 15:42:49 +0000

    EMailFolderContactSource and dependencies

--- com.openexchange.contact.aggregator/src/com/openexchange/contact/aggregator/osgi/Activator.java
+++ com.openexchange.contact.aggregator/src/com/openexchange/contact/aggregator/osgi/Activator.java
@@ -2,21 +2,30 @@ package com.openexchange.contact.aggregator.osgi;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import com.openexchange.messaging.registry.MessagingServiceRegistry;
+import com.openexchange.server.osgiservice.HousekeepingActivator;
 
-public class Activator implements BundleActivator {
+public class Activator extends HousekeepingActivator {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-	}
+    private static final Class[] NEEDED = {MessagingServiceRegistry.class};
+    
+    /* (non-Javadoc)
+     * @see com.openexchange.server.osgiservice.DeferredActivator#getNeededServices()
+     */
+    @Override
+    protected Class<?>[] getNeededServices() {        
+        return NEEDED;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-	}
+    /* (non-Javadoc)
+     * @see com.openexchange.server.osgiservice.DeferredActivator#startBundle()
+     */
+    @Override
+    protected void startBundle() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+	
 
 }
--- com.openexchange.contact.aggregator/src/com/openexchange/contact/aggregator/osgi/EMailFolderContactSource.java
+++ com.openexchange.contact.aggregator/src/com/openexchange/contact/aggregator/osgi/EMailFolderContactSource.java
@@ -49,20 +49,111 @@
 
 package com.openexchange.contact.aggregator.osgi;
 
+import java.util.ArrayList;
 import java.util.List;
+import javax.mail.internet.InternetAddress;
+import com.openexchange.groupware.AbstractOXException;
 import com.openexchange.groupware.container.Contact;
+import com.openexchange.mail.MailException;
+import com.openexchange.mail.MailListField;
+import com.openexchange.mail.MailServletInterface;
+import com.openexchange.mail.dataobjects.MailMessage;
+import com.openexchange.tools.iterator.SearchIterator;
 import com.openexchange.tools.session.ServerSession;
-
 
 /**
  * {@link EMailFolderContactSource}
- *
+ * 
  * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
+ * @author <a href="mailto:karsten.will@open-xchange.com">Karsten Will</a>
  */
 public class EMailFolderContactSource implements ContactSource {
 
+    private Activator activator;
+    private MailFolderDiscoverer discoverer;
+
+    public EMailFolderContactSource(Activator activator, MailFolderDiscoverer discoverer) {
+        this.activator = activator;
+        this.discoverer = discoverer;
+    }
+
     public List<Contact> getContacts(ServerSession session) {
-        // TODO Auto-generated method stub
+
+        List<String> mailFolders = getAllVisibleEmailFolders(session);
+        return getContactsFromMailFolders(session, mailFolders);
+    }
+
+    /**
+     * @param session
+     * @param mailFolders
+     * @return
+     */
+    private List<Contact> getContactsFromMailFolders(ServerSession session, List<String> mailFolders) {
+        List<Contact> contacts = new ArrayList<Contact>();
+        for (String mailFolder : mailFolders) {
+            List<Contact> tempContacts = getAllContactsFromOneFolder(mailFolder, session);
+            contacts.addAll(tempContacts);
+        }
+        return contacts;
+    }
+
+    /**
+     * @return
+     */
+    private List<Contact> getAllContactsFromOneFolder(String folder, ServerSession session) {
+        List<Contact> contacts = new ArrayList<Contact>();
+        try {
+            MailServletInterface mailInterface = MailServletInterface.getInstance(session);
+            int[] fields = {
+                MailListField.FROM.getField(), MailListField.TO.getField(), MailListField.CC.getField(), MailListField.BCC.getField() };
+            SearchIterator<MailMessage> messages = mailInterface.getAllMessages(folder, 0, 0, fields, new int[] {});
+            
+            while (messages.hasNext()){
+                MailMessage message = messages.next();
+                
+                InternetAddress[] froms = message.getFrom();
+                for (InternetAddress from : froms){
+                    Contact contact = new Contact();
+                    contact.setDisplayName(from.getPersonal());
+                    contact.setEmail1(from.getAddress());
+                }
+                InternetAddress[] tos = message.getTo();
+                for (InternetAddress to : tos){
+                    Contact contact = new Contact();
+                    contact.setDisplayName(to.getPersonal());
+                    contact.setEmail1(to.getAddress());
+                }
+                InternetAddress[] ccs = message.getCc();
+                for (InternetAddress cc : ccs){
+                    Contact contact = new Contact();
+                    contact.setDisplayName(cc.getPersonal());
+                    contact.setEmail1(cc.getAddress());
+                }
+                InternetAddress[] bccs = message.getBcc();
+                for (InternetAddress bcc : bccs){
+                    Contact contact = new Contact();
+                    contact.setDisplayName(bcc.getPersonal());
+                    contact.setEmail1(bcc.getAddress());
+                }
+            }
+        } catch (MailException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (AbstractOXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return contacts;
+    }
+
+ 
+    private List<String> getAllVisibleEmailFolders(ServerSession session) {
+        try {
+            return discoverer.getMailFolder(session);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return null;
     }
 
