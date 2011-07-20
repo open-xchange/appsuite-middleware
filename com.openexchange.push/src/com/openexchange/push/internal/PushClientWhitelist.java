37ff137ad48eb75c8d52a62730f62f6b94386f92
thorben <thorben>
2011-07-20 13:22:36 +0000

    Enhanced JavaDoc and/or comment

--- com.openexchange.push/src/com/openexchange/push/internal/PushClientWhitelist.java
+++ com.openexchange.push/src/com/openexchange/push/internal/PushClientWhitelist.java
@@ -83,38 +83,76 @@ public final class PushClientWhitelist {
         map = new ConcurrentHashMap<Pattern, Pattern>(4);
     }
 
-    public Pattern addIfAbsent(final Pattern pattern) {
-        return map.putIfAbsent(pattern, pattern);
+    /**
+     * Adds specified pattern if no such pattern is already contained.
+     * 
+     * @param pattern The pattern to add
+     * @return <code>true</code> for successful insertion; otherwise <code>false</code>
+     */
+    public boolean add(final Pattern pattern) {
+        return (null == map.putIfAbsent(pattern, pattern));
     }
 
+    /**
+     * Gets this white-list's size.
+     * 
+     * @return The size
+     */
     public int size() {
         return map.size();
     }
 
+    /**
+     * Checks if this white-list contains specified pattern.
+     * 
+     * @param pattern The pattern
+     * @return <code>true</code> if contained; otherwise <code>false</code>
+     */
     public boolean contains(final Pattern pattern) {
         return map.containsKey(pattern);
     }
 
-    public Pattern add(final Pattern pattern) {
-        return map.put(pattern, pattern);
+    /**
+     * Removes specified pattern.
+     * 
+     * @param pattern The pattern
+     * @return <code>true</code> if specified pattern was removed; otherwise <code>false</code>
+     */
+    public boolean remove(final Pattern pattern) {
+        return (null != map.remove(pattern));
     }
 
-    public Pattern remove(final Pattern pattern) {
-        return map.remove(pattern);
-    }
-
+    /**
+     * Clears this white-list.
+     */
     public void clear() {
         map.clear();
     }
 
+    /**
+     * Checks if this white-list is empty.
+     * 
+     * @return <code>true</code> if this white-list is empty; otherwise <code>false</code>
+     */
     public boolean isEmpty() {
         return map.isEmpty();
     }
 
+    /**
+     * Gets currently contained patterns.
+     * 
+     * @return Currently contained patterns.
+     */
     public Set<Pattern> getPatterns() {
         return new HashSet<Pattern>(map.keySet());
     }
 
+    /**
+     * Checks if specified client identifier is matched by one of contained patterns.
+     * 
+     * @param clientId The client identifier
+     * @return <code>true</code> if specified client identifier is matched by one of contained patterns; otherwise <code>false</code>
+     */
     public boolean isAllowed(final String clientId) {
         if (null == clientId) {
             return false;
