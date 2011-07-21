30c067ecab9aff5fbc48da463a76eff1a05196cd
thorben <thorben>
2011-07-21 11:53:02 +0000

    logging service

---
+++ com.openexchange.threadpool/src/com/openexchange/log/internal/LoggerTask.java
@@ -0,0 +1,197 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2010 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.log.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.commons.logging.Log;
+import com.openexchange.log.Loggable;
+import com.openexchange.threadpool.AbstractTask;
+import com.openexchange.threadpool.ThreadRenamer;
+
+/**
+ * {@link LoggerTask}
+ * 
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ */
+final class LoggerTask extends AbstractTask<Object> {
+
+    /**
+     * The poison element.
+     */
+    public static final Loggable POISON = new Loggable() {
+        
+        public Throwable getThrowable() {
+            return null;
+        }
+        
+        public String getMessage() {
+            return null;
+        }
+        
+        public Log getLog() {
+            return null;
+        }
+        
+        public Level getLevel() {
+            return null;
+        }
+    };
+
+    private final BlockingQueue<Loggable> queue;
+
+    private final AtomicBoolean keepgoing;
+
+    /**
+     * Initializes a new {@link LoggerTask}.
+     * 
+     * @param queue
+     */
+    protected LoggerTask(final BlockingQueue<Loggable> queue) {
+        super();
+        keepgoing = new AtomicBoolean();
+        this.queue = queue;
+    }
+
+    /**
+     * Stops this task.
+     */
+    protected void stop() {
+        keepgoing.set(false);
+        /*
+         * Feed poison element to enforce quit
+         */
+        try {
+            queue.put(POISON);
+        } catch (final InterruptedException e) {
+            /*
+             * Cannot occur, but keep interrupted state
+             */
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Override
+    public void setThreadName(final ThreadRenamer threadRenamer) {
+        threadRenamer.rename("OX-Logger");
+    }
+
+    public Object call() throws Exception {
+        final List<Loggable> loggables = new ArrayList<Loggable>(16);
+        while (keepgoing.get()) {
+            if (queue.isEmpty()) {
+                // Blocking wait for at least 1 loggable to arrive.
+                final Loggable loggable = queue.take();
+                if (POISON == loggable) {
+                    return null;
+                }
+                loggables.add(loggable);
+            }
+            queue.drainTo(loggables);
+            final boolean quit = loggables.remove(POISON);
+            for (final Loggable loggable : loggables) {
+                switch (loggable.getLevel()) {
+                case FATAL:
+                    if (null == loggable.getThrowable()) {
+                        loggable.getLog().fatal(loggable.getMessage());
+                    } else {
+                        loggable.getLog().fatal(loggable.getMessage(), loggable.getThrowable());
+                    }
+                    break;
+                case ERROR:
+                    if (null == loggable.getThrowable()) {
+                        loggable.getLog().error(loggable.getMessage());
+                    } else {
+                        loggable.getLog().error(loggable.getMessage(), loggable.getThrowable());
+                    }
+                    break;
+                case WARNING:
+                    if (null == loggable.getThrowable()) {
+                        loggable.getLog().warn(loggable.getMessage());
+                    } else {
+                        loggable.getLog().warn(loggable.getMessage(), loggable.getThrowable());
+                    }
+                    break;
+                case INFO:
+                    if (null == loggable.getThrowable()) {
+                        loggable.getLog().info(loggable.getMessage());
+                    } else {
+                        loggable.getLog().info(loggable.getMessage(), loggable.getThrowable());
+                    }
+                    break;
+                case DEBUG:
+                    if (null == loggable.getThrowable()) {
+                        loggable.getLog().debug(loggable.getMessage());
+                    } else {
+                        loggable.getLog().debug(loggable.getMessage(), loggable.getThrowable());
+                    }
+                    break;
+                case TRACE:
+                    if (null == loggable.getThrowable()) {
+                        loggable.getLog().trace(loggable.getMessage());
+                    } else {
+                        loggable.getLog().trace(loggable.getMessage(), loggable.getThrowable());
+                    }
+                    break;
+                default:
+                    // No-op
+                }
+            }
+            loggables.clear();
+            if (quit) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+}
