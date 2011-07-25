053fdbeaf18e135a84ce0e76daaa1d1ca0d2bffb
thorben <thorben>
2011-07-25 08:02:44 +0000

    Typo

--- server/src/com/openexchange/groupware/importexport/ImportExportExceptionMessages.java
+++ server/src/com/openexchange/groupware/importexport/ImportExportExceptionMessages.java
@@ -137,9 +137,10 @@ public class ImportExportExceptionMessages implements LocalizableStrings {
     // Invalid date format detected: "%1$s". Ignoring value.
     public static final String INVALID_DATE_MSG = "Invalid date format detected: \"%1$s\". Ignoring value.";
 
-    public static final String CONTACT_INTERFACE_MISSING = "Could not load ContactInterface to write store contacts";
+    // Could not load ContactInterface to write storage contacts.
+    public static final String CONTACT_INTERFACE_MISSING = "Could not load ContactInterface to write storage contacts.";
 
-    public static final String COULD_NOT_WRITE = "Could not write entry into database";
+    public static final String COULD_NOT_WRITE = "Could not write entry into database.";
     
     //  Ignoring invalid value for field "%1$s": %2$s
     public static final String IGNORE_FIELD_MSG = "Ignoring invalid value for field \"%1$s\": %2$s";
