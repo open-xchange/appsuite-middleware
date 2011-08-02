944c31e769a67c76d7b4f91acd1175488e6f8de0
thorben <thorben.betten@open-xchange.com>
2011-08-02 18:43:38 +0200

    Minor changes

--- server/src/com/openexchange/server/osgi/AuthenticationCustomizer.java
+++ server/src/com/openexchange/server/osgi/AuthenticationCustomizer.java
@@ -59,9 +59,10 @@ import com.openexchange.authentication.service.Authentication;
 
 /**
  * Authentication service tracker putting the service into the static authentication class.
+ * 
  * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
  */
-public class AuthenticationCustomizer implements ServiceTrackerCustomizer {
+public class AuthenticationCustomizer implements ServiceTrackerCustomizer<AuthenticationService, AuthenticationService> {
 
     private static final Log LOG = com.openexchange.log.Log.valueOf(LogFactory.getLog(AuthenticationCustomizer.class));
 
@@ -73,8 +74,8 @@ public class AuthenticationCustomizer implements ServiceTrackerCustomizer {
     }
 
     @Override
-    public Object addingService(final ServiceReference reference) {
-        final AuthenticationService auth = (AuthenticationService) context.getService(reference);
+    public AuthenticationService addingService(final ServiceReference<AuthenticationService> reference) {
+        final AuthenticationService auth = context.getService(reference);
         if (Authentication.setService(auth)) {
             return auth;
         }
@@ -83,14 +84,13 @@ public class AuthenticationCustomizer implements ServiceTrackerCustomizer {
     }
 
     @Override
-    public void modifiedService(final ServiceReference reference,
-        final Object service) {
+    public void modifiedService(final ServiceReference<AuthenticationService> reference, final AuthenticationService service) {
         // Nothing to do.
     }
 
     @Override
-    public void removedService(final ServiceReference reference, final Object service) {
-        final AuthenticationService auth = (AuthenticationService) service;
+    public void removedService(final ServiceReference<AuthenticationService> reference, final AuthenticationService service) {
+        final AuthenticationService auth = service;
         if (!Authentication.dropService(auth)) {
             LOG.error("Removed authentication services was not active!");
         }
