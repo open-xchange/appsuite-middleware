2c3b437f12f47af63a67fcb64ea1cd407281efd5
thorben <thorben.betten@open-xchange.com>
2011-08-04 17:50:22 +0200

    extended dispatcher framework

--- server/src/com/openexchange/server/impl/Starter.java
+++ server/src/com/openexchange/server/impl/Starter.java
@@ -173,7 +173,6 @@ public class Starter implements Initialization {
      */
     new com.openexchange.filemanagement.internal.ManagedFileInitialization(),
     new com.openexchange.mailaccount.internal.MailAccountStorageInit(),
-    new com.openexchange.mailaccount.json.init.MailAccountServletInit(),
     new com.openexchange.multiple.internal.MultipleHandlerInit(),
     new com.openexchange.groupware.impl.id.IDGeneratorInit() };
 
