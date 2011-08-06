8905409893adff329f40f6b81b088637d2a1a9de
Thorben Betten <thorben.betten@open-xchange.com>
2011-08-06 17:06:34 +0200

    Thread-local log properties

--- server/src/com/openexchange/tools/servlet/OXJSONExceptionCodes.java
+++ server/src/com/openexchange/tools/servlet/OXJSONExceptionCodes.java
@@ -1,7 +1,6 @@
 package com.openexchange.tools.servlet;
 
 import com.openexchange.exception.Category;
-import com.openexchange.exception.LogLevel;
 import com.openexchange.exception.OXException;
 import com.openexchange.exception.OXExceptionCode;
 import com.openexchange.exception.OXExceptionFactory;
@@ -10,33 +9,50 @@ import com.openexchange.exception.OXExceptionFactory;
  * Error codes for JSON exceptions.
  */
 public enum OXJSONExceptionCodes implements OXExceptionCode {
-    /** Exception while writing JSON. */
+    /**
+     * Exception while writing JSON.
+     */
     JSON_WRITE_ERROR("Exception while writing JSON.", Category.CATEGORY_ERROR, 1),
-    /** Exception while parsing JSON: "%s". */
+    /**
+     * Exception while parsing JSON: "%s".
+     */
     JSON_READ_ERROR("Exception while parsing JSON: \"%s\".", Category.CATEGORY_ERROR, 2),
-    /** Invalid cookie. */
+    /**
+     * Invalid cookie.
+     * */
     INVALID_COOKIE("Invalid cookie.", Category.CATEGORY_TRY_AGAIN, 3),
-    /** Exception while building JSON. */
+    /**
+     * Exception while building JSON.
+     */
     JSON_BUILD_ERROR("Exception while building JSON.", Category.CATEGORY_ERROR, 4),
-    /** Value "%1$s" of attribute %s contains non digit characters. */
+    /**
+     * Value "%1$s" of attribute %s contains non digit characters. */
     CONTAINS_NON_DIGITS("Value \"%1$s\" of attribute %2$s contains non digit characters.", Category.CATEGORY_USER_INPUT, 5),
-    /** Too many digits within field %1$s. */
+    /**
+     * Too many digits within field %1$s.
+     */
     TOO_BIG_NUMBER("Too many digits within field %1$s.", Category.CATEGORY_USER_INPUT, 6),
-    /** Unable to parse value "%1$s" within field %2$s as a number. */
+    /**
+     * Unable to parse value "%1$s" within field %2$s as a number.
+     */
     NUMBER_PARSING("Unable to parse value \"%1$s\" within field %2$s as a number.", Category.CATEGORY_ERROR, 7),
-    /** Invalid value \"%2$s\" in JSON attribute \"%1$s\". */
-    INVALID_VALUE("Invalid value \"%2$s\" in JSON attribute \"%1$s\".", Category.CATEGORY_USER_INPUT, 8);
+    /**
+     * Invalid value \"%2$s\" in JSON attribute \"%1$s\".
+     */
+    INVALID_VALUE("Invalid value \"%2$s\" in JSON attribute \"%1$s\".", Category.CATEGORY_USER_INPUT, 8),
+    /**
+     * Missing field "%1$s" in JSON data.
+     */
+    MISSING_FIELD("Missing field \"%1$s\" in JSON data.", Category.CATEGORY_ERROR, 9);
 
     private final String message;
     private final Category category;
     private final int number;
-    private final boolean display;
 
     private OXJSONExceptionCodes(final String message, final Category category, final int detailNumber) {
         this.message = message;
         this.category = category;
         number = detailNumber;
-        display = category.getLogLevel().implies(LogLevel.DEBUG);
     }
 
     @Override
