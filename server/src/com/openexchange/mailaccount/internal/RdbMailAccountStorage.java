0878e8a2640bb8a58a5a5a6c7c23cea38fb3c256
Thorben Betten <thorben.betten@open-xchange.com>
2011-08-06 15:19:49 +0200

    Thread-local log properties

--- server/src/com/openexchange/mailaccount/internal/RdbMailAccountStorage.java
+++ server/src/com/openexchange/mailaccount/internal/RdbMailAccountStorage.java
@@ -67,6 +67,7 @@ import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
