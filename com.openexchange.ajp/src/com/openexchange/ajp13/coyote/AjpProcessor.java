7f0de5875585789b96fdaf7d9c79d33d6a4c641a
Thorben Betten <thorben.betten@open-xchange.com>
2011-08-10 14:58:40 +0200

    New exception framework

--- com.openexchange.ajp/src/com/openexchange/ajp13/coyote/AjpProcessor.java
+++ com.openexchange.ajp/src/com/openexchange/ajp13/coyote/AjpProcessor.java
@@ -748,9 +748,9 @@ public final class AjpProcessor implements com.openexchange.ajp13.watcher.Task {
                         final byte[] bytes = baos.toByteArray();
                         parseQueryString(new String(bytes, charEnc));
                         /*
-                         * TODO: Apply already read data to request to make them re-available?
+                         * Apply already read data to request to make them re-available?
                          */
-                        // request.dumpToBuffer(bytes);
+                        request.dumpToBuffer(bytes);
                     }
                     servlet.service(request, response);
                     response.flushBuffer();
