208039416d8314e4896ae13303c2a91b6c92030e
Thorben Betten <thorben.betten@open-xchange.com>
2011-08-12 10:52:46 +0200

    New exception framework

--- server/src/com/openexchange/groupware/tasks/json/actions/SearchAction.java
+++ server/src/com/openexchange/groupware/tasks/json/actions/SearchAction.java
@@ -51,9 +51,7 @@ package com.openexchange.groupware.tasks.json.actions;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.json.JSONException;
 import org.json.JSONObject;
 import com.openexchange.ajax.AJAXServlet;
@@ -147,7 +145,6 @@ public final class SearchAction extends AbstractTaskAction {
         System.arraycopy(columnsToLoad, 0, internalColumns, 0, columnsToLoad.length);
         internalColumns[columnsToLoad.length] = DataObject.LAST_MODIFIED;
 
-        final Map<String, List<Task>> taskMap = new HashMap<String, List<Task>>(1);
         final List<Task> taskList = new ArrayList<Task>();
         SearchIterator<Task> it = null;
         try {
@@ -165,8 +162,7 @@ public final class SearchAction extends AbstractTaskAction {
                 }
             }
 
-            taskMap.put("tasks", taskList);
-            return new AJAXRequestResult(taskMap, timestamp, "tasks");
+            return new AJAXRequestResult(taskList, timestamp, "task");
         } finally {
             if (it != null) {
                 it.close();
