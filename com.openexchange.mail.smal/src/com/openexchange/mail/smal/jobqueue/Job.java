941d92971a4cd175f4dabddb1c70fb120f4dc2b5
Thorben Betten <thorben.betten@open-xchange.com>
2011-08-19 21:09:22 +0200

    OSGi generics

--- com.openexchange.mail.smal/src/com/openexchange/mail/smal/jobqueue/Job.java
+++ com.openexchange.mail.smal/src/com/openexchange/mail/smal/jobqueue/Job.java
@@ -53,7 +53,6 @@ import java.io.Serializable;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicReference;
 import org.apache.commons.logging.LogFactory;
-import org.osgi.framework.ServiceReference;
 import com.openexchange.log.Log;
 import com.openexchange.mail.smal.SMALServiceLookup;
 import com.openexchange.mail.smal.adapter.IndexAdapter;
@@ -110,7 +109,7 @@ public abstract class Job implements Task<Object>, Comparable<Job>, Serializable
      * Gets this job's ranking.
      * <p>
      * The job ranking is used by the {@link JobQueue} to determine the <i>natural order</i> of services, see
-     * {@link ServiceReference#compareTo}.
+     * {@link Comparable#compareTo(Object)}.
      * <p>
      * The default ranking is zero (0). A job with a ranking of {@code Integer.MAX_VALUE} is very likely to be immediately executed, whereas
      * a job with a ranking of {@code Integer.MIN_VALUE} is very unlikely to be executed.
