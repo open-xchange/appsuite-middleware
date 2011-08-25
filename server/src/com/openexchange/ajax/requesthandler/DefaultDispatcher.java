65540b3e07bec9d7415c35f3eaf96f1774630ffe
Thorben Betten <thorben.betten@open-xchange.com>
2011-08-25 18:26:46 +0200

    SMIME

--- server/src/com/openexchange/ajax/requesthandler/DefaultDispatcher.java
+++ server/src/com/openexchange/ajax/requesthandler/DefaultDispatcher.java
@@ -102,10 +102,6 @@ public class DefaultDispatcher implements Dispatcher {
 
     @Override
     public AJAXRequestResult perform(final AJAXRequestData request, final AJAXState state, final ServerSession session) throws OXException {
-        
-        System.out.println("Incoming AJAX request: module=" + request.getModule() + ", action=" + request.getAction());
-        
-        
         List<AJAXActionCustomizer> outgoing = new ArrayList<AJAXActionCustomizer>(customizerFactories.size());
         final List<AJAXActionCustomizer> todo = new LinkedList<AJAXActionCustomizer>();
         /*
@@ -205,11 +201,6 @@ public class DefaultDispatcher implements Dispatcher {
                 }
             }
         }
-        
-        
-        System.out.println("\tOutgoing AJAX request: module=" + request.getModule() + ", action=" + request.getAction());
-        
-        
         return result;
     }
 
