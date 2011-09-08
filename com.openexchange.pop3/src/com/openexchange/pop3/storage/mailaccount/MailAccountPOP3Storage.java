35ad48a7aef92d6ebea3316424159f2bfc33b3d7
Thorben Betten <thorben.betten@open-xchange.com>
2011-09-08 19:03:09 +0200

    Lazy initialization of MailAccess instance

--- com.openexchange.pop3/src/com/openexchange/pop3/storage/mailaccount/MailAccountPOP3Storage.java
+++ com.openexchange.pop3/src/com/openexchange/pop3/storage/mailaccount/MailAccountPOP3Storage.java
@@ -117,7 +117,7 @@ public class MailAccountPOP3Storage implements POP3Storage {
 
     private final POP3Access pop3Access;
 
-    private final MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> defaultMailAccess;
+    private MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> defaultMailAccess;
 
     private final int pop3AccountId;
 
@@ -135,7 +135,7 @@ public class MailAccountPOP3Storage implements POP3Storage {
         this.pop3Access = pop3Access;
         pop3AccountId = pop3Access.getAccountId();
         final Session session = pop3Access.getSession();
-        defaultMailAccess = MailAccess.getInstance(session);
+        // defaultMailAccess = MailAccess.getInstance(session);
         this.properties = properties;
         {
             String tmp = properties.getProperty(POP3StoragePropertyNames.PROPERTY_PATH);
@@ -157,7 +157,15 @@ public class MailAccountPOP3Storage implements POP3Storage {
         separator = 0;
     }
 
+    private MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> getDefaultMailAccess() throws OXException {
+        if (null == defaultMailAccess) {
+            defaultMailAccess = MailAccess.getInstance(pop3Access.getSession());
+        }
+        return defaultMailAccess;
+    }
+
     private String composeUniquePath(final int pop3AccountId, final int user, final int cid) throws OXException {
+        final MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> defaultMailAccess = getDefaultMailAccess();
         defaultMailAccess.connect(false);
         try {
             final String trashFullname = defaultMailAccess.getFolderStorage().getTrashFolder();
@@ -234,7 +242,7 @@ public class MailAccountPOP3Storage implements POP3Storage {
      */
     public char getSeparator() throws OXException {
         if (0 == separator) {
-            separator = defaultMailAccess.getFolderStorage().getFolder("INBOX").getSeparator();
+            separator = getDefaultMailAccess().getFolderStorage().getFolder("INBOX").getSeparator();
         }
         return separator;
     }
@@ -242,6 +250,7 @@ public class MailAccountPOP3Storage implements POP3Storage {
     @Override
     public void drop() throws OXException {
         if (null != path) {
+            final MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> defaultMailAccess = getDefaultMailAccess();
             if (defaultMailAccess.isConnected()) {
                 try {
                     defaultMailAccess.getFolderStorage().deleteFolder(path, true);
@@ -282,17 +291,41 @@ public class MailAccountPOP3Storage implements POP3Storage {
 
     @Override
     public void close() {
-        defaultMailAccess.close(true);
+        if (null != messageStorage) {
+            try {
+                messageStorage.releaseResources();
+            } catch (final Exception e) {
+                // Ignore
+            }
+            messageStorage = null;
+        }
+        if (null != folderStorage) {
+            try {
+                folderStorage.releaseResources();
+            } catch (final Exception e) {
+                // Ignore
+            }
+            folderStorage = null;
+        }
+        if (null != defaultMailAccess) {
+            try {
+                defaultMailAccess.close(true);
+            } catch (final Exception e) {
+                // Ignroe
+            }
+            defaultMailAccess = null;
+        }
     }
 
     @Override
     public int getUnreadMessagesCount(final String fullname) throws OXException {
         final String realFullname = getRealFullname(fullname);
-        return defaultMailAccess.getUnreadMessagesCount(realFullname);
+        return getDefaultMailAccess().getUnreadMessagesCount(realFullname);
     }
 
     @Override
     public void connect() throws OXException {
+        final MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> defaultMailAccess = getDefaultMailAccess();
         defaultMailAccess.connect(false);
         try {
             // Check path existence
@@ -408,7 +441,7 @@ public class MailAccountPOP3Storage implements POP3Storage {
     @Override
     public IMailFolderStorage getFolderStorage() throws OXException {
         if (null == folderStorage) {
-            folderStorage = new MailAccountPOP3FolderStorage(defaultMailAccess.getFolderStorage(), this, pop3Access);
+            folderStorage = new MailAccountPOP3FolderStorage(getDefaultMailAccess().getFolderStorage(), this, pop3Access);
         }
         return folderStorage;
     }
@@ -417,7 +450,7 @@ public class MailAccountPOP3Storage implements POP3Storage {
     public IMailMessageStorage getMessageStorage() throws OXException {
         if (null == messageStorage) {
             messageStorage = new MailAccountPOP3MessageStorage(
-                defaultMailAccess.getMessageStorage(),
+                getDefaultMailAccess().getMessageStorage(),
                 this,
                 pop3AccountId,
                 pop3Access.getSession());
@@ -426,7 +459,7 @@ public class MailAccountPOP3Storage implements POP3Storage {
     }
 
     IMailMessageStorage getInternalMessageStorage() throws OXException {
-        return defaultMailAccess.getMessageStorage();
+        return getDefaultMailAccess().getMessageStorage();
     }
 
     @Override
