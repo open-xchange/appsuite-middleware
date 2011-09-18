d3026039295eddd873051947147a3229cc48177b
Francisco Laguna <francisco.laguna@open-xchange.com>
2011-09-18 15:14:48 +0200

    added underscore lib

--- com.openexchange.scripting.rhino/src/com/openexchange/scripting/rhino/SharedScope.java
+++ com.openexchange.scripting.rhino/src/com/openexchange/scripting/rhino/SharedScope.java
@@ -3,6 +3,8 @@ package com.openexchange.scripting.rhino;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ScriptableObject;
 
+import com.openexchange.scripting.rhino.libs.Underscore;
+
 public class SharedScope {
 	public static final ScriptableObject SHARED_SCOPE;
 	static {
@@ -10,10 +12,11 @@ public class SharedScope {
 		try {
 			 cx = Context.enter();
 			 SHARED_SCOPE = cx.initStandardObjects(null, true);
-
+			 
 			// Force the LiveConnect stuff to be loaded. 
 			String loadMe = "RegExp; getClass; java; Packages; JavaAdapter;";
 			cx.evaluateString(SHARED_SCOPE , loadMe, "lazyLoad", 0, null);
+			Underscore.initialize(SHARED_SCOPE);
 		} finally {
 			Context.exit();
 		}
