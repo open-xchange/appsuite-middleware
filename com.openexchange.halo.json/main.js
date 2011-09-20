4fd2a51fca5e351489576606dd6250724181c228
Francisco Laguna <francisco.laguna@open-xchange.com>
2011-09-20 14:57:16 +0200

    basic halo implementation

---
+++ com.openexchange.halo.json/main.js
@@ -0,0 +1,27 @@
+define("com.openexchange.halo.json/main", ["osgi", "httpAPI"], function (osgi, httpAPI) {
+   var Contact = Packages.com.openexchange.groupware.container.Contact;
+   var ContactParser = Packages.com.openexchange.ajax.parser.ContactParser;
+
+   osgi.services(["com.openexchange.halo.ContactHalo"], function (contactHalo) {
+      
+      httpAPI.defineModule("halo/contact", {
+          services : function (req, session) {
+              return contactHalo.getProviders(session);
+          },
+          investigate : function (req, session) {
+              var contact = new Contact();
+              var parser = new ContactParser();
+              var provider = req.getParameter("provider");
+              
+              if (req.getData() != null) {
+                  parser.parse(contact, req.getData());
+              } else {
+                  contact.setEmail1(req.getParameter("email1"));
+              }
+              
+              return contactHalo.investigate(provider, contact, session);
+          }
+      });
+       
+   });
+});
