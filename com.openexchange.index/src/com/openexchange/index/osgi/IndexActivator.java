73d961fddde33c807641abd4640940e0f4c52708
Thorben Betten <thorben.betten@open-xchange.com>
2011-09-24 00:10:53 +0200

    solr

--- com.openexchange.index/src/com/openexchange/index/osgi/IndexActivator.java
+++ com.openexchange.index/src/com/openexchange/index/osgi/IndexActivator.java
@@ -1,11 +1,11 @@
 package com.openexchange.index.osgi;
 
-import com.openexchange.database.DatabaseService;
-import com.openexchange.index.ConfigIndexService;
-import com.openexchange.index.internal.ConfigIndexServiceImpl;
-import com.openexchange.server.osgiservice.HousekeepingActivator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import com.openexchange.database.DatabaseService;
+import com.openexchange.index.ConfigIndexService;
+import com.openexchange.index.internal.StaticConfigIndexService;
+import com.openexchange.server.osgiservice.HousekeepingActivator;
 
 public class IndexActivator extends HousekeepingActivator {
     
@@ -18,8 +18,10 @@ public class IndexActivator extends HousekeepingActivator {
 
     @Override
     protected void startBundle() throws Exception {
-        LOG.info("Starting Bundle com.openexchange.index.osgi.");       
-        registerService(ConfigIndexService.class, new ConfigIndexServiceImpl(getService(DatabaseService.class)));
+        LOG.info("Starting Bundle com.openexchange.index.osgi.");
+        // Only for testing purpose...
+        // registerService(ConfigIndexService.class, new ConfigIndexServiceImpl(getService(DatabaseService.class)));
+        registerService(ConfigIndexService.class, new StaticConfigIndexService());
     }
 
 
