bc9e42a5038779e4d4b51c2c3068a4c8366e91a7
Francisco Laguna <francisco.laguna@open-xchange.com>
2011-10-09 10:21:25 +0200

    restore

---
+++ com.openexchange.halo.json/main.js
@@ -0,0 +1,32 @@
+define("com.openexchange.halo.json/main", ["osgi", "httpAPI"], function (osgi, httpAPI) {
+   var Contact = Packages.com.openexchange.groupware.container.Contact;
+   var ContactParser = Packages.com.openexchange.ajax.parser.ContactParser;
+
+   osgi.services(["com.openexchange.halo.ContactHalo"], function (contactHalo) {
+      
+      httpAPI.defineModule("halo/contact", {
+          services : function (req, session) {
+              var retval = [];
+              var providers = contactHalo.getProviders(session).iterator();
+              while(providers.hasNext()) {
+                  retval.push(providers.next()+"");
+              }
+              return retval;
+          },
+          investigate : function (req, session) {
+              var contact = new Contact();
+              var parser = new ContactParser();
+              var provider = req.getParameter("provider");
+              
+              if (req.getData() != null) {
+                  parser.parse(contact, req.getData());
+              } else {
+                  contact.setEmail1(req.getParameter("email1"));
+              }
+              
+              return contactHalo.investigate(provider, contact, req, session);
+          }
+      });
+   });
+});
+
