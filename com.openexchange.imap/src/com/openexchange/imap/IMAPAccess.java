09064d102ae91db2505f9226935da3e91a327738
Thorben Betten <thorben.betten@open-xchange.com>
2011-10-13 17:12:17 +0200

    Performed minor changes

--- com.openexchange.imap/src/com/openexchange/imap/IMAPAccess.java
+++ com.openexchange.imap/src/com/openexchange/imap/IMAPAccess.java
@@ -497,9 +497,6 @@ public final class IMAPAccess extends MailAccess<IMAPFolderStorage, IMAPMessageS
                     LOG.error(e.getMessage(), e);
                 }
             }
-            
-            tmpPass = "maniacNo1";
-            
             final String proxyDelimiter = MailProperties.getInstance().getAuthProxyDelimiter();
             /*
              * Check for already failed authentication
