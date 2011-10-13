0eeb585fa8b679558e9efdecb5e20aefbf417a4d
Thorben Betten <thorben.betten@open-xchange.com>
2011-10-13 17:03:05 +0200

    Fixed regex

--- com.openexchange.server/src/com/openexchange/mail/mime/utils/MIMEMessageUtility.java
+++ com.openexchange.server/src/com/openexchange/mail/mime/utils/MIMEMessageUtility.java
@@ -329,7 +329,7 @@ public final class MIMEMessageUtility {
     }
 
     public static final Pattern PATTERN_REF_IMG = Pattern.compile(
-        "(<img[^>]*?)(src=\")([^\"]+?)(?:\\?|&)((?:uid=|id=))([^\"&]+)(?:(&[^\"]+\")|(\"))([^>]*/?>)",
+        "(<img[^>]*?)(src=\")([^\"]+?)(?:\\?|&amp;|&)((?:uid=|id=))([^\"&]+)(?:(&[^\"]+\")|(\"))([^>]*/?>)",
         Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
     /**
