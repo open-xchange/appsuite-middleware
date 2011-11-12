c1a65639f3dea28776627afac5fcebae83690223
Thorben Betten <thorben.betten@open-xchange.com>
2011-11-12 12:27:52 +0100

    Fix for bug 20704: Improved error message to better determine what part of Facebook configuration is wrong/missing

--- com.openexchange.messaging.facebook/src/com/openexchange/messaging/facebook/FacebookMessagingExceptionCodes.java
+++ com.openexchange.messaging.facebook/src/com/openexchange/messaging/facebook/FacebookMessagingExceptionCodes.java
@@ -132,7 +132,17 @@ public enum FacebookMessagingExceptionCodes implements OXExceptionCode {
     /**
      * XML parse error: %1$s.
      */
-    XML_PARSE_ERROR(FacebookMessagingExceptionMessages.XML_PARSE_ERROR_MSG, CATEGORY_ERROR, 16);
+    XML_PARSE_ERROR(FacebookMessagingExceptionMessages.XML_PARSE_ERROR_MSG, CATEGORY_ERROR, 16),
+    /**
+     * Missing facebook configuration. Please re-create facebook account.
+     */
+    MISSING_CONFIG(FacebookMessagingExceptionMessages.MISSING_CONFIG_MSG, CATEGORY_ERROR, 17),
+    /**
+     * Missing facebook configuration parameter "%1$s". Please re-create facebook account.
+     */
+    MISSING_CONFIG_PARAM(FacebookMessagingExceptionMessages.MISSING_CONFIG_PARAM_MSG, CATEGORY_ERROR, 18),
+    
+    ;
 
 
     private final Category category;
