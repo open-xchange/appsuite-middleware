761eac841058e55ce5e2d443d1d8b361794d313a
Thorben Betten <thorben.betten@open-xchange.com>
2011-11-23 11:59:09 +0100

    Added test classes for new folder request

---
+++ openexchange-test/src/com/openexchange/ajax/folder/actions/SubscribeRequest.java
@@ -0,0 +1,126 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2011 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax.folder.actions;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import org.json.JSONArray;
+import com.openexchange.ajax.AJAXServlet;
+import com.openexchange.ajax.Folder;
+import com.openexchange.ajax.container.Response;
+import com.openexchange.ajax.framework.AbstractAJAXParser;
+import com.openexchange.folderstorage.FolderStorage;
+
+/**
+ * @author <a href="mailto:marcus@open-xchange.org">Marcus Klein</a>
+ */
+public class SubscribeRequest extends AbstractFolderRequest<SubscribeResponse> {
+
+    private final String tree;
+
+    private final String parent;
+    
+    private final boolean failOnError;
+
+    private final Set<String> folderIds;
+
+    public SubscribeRequest(final API api, final String tree, final boolean failOnError) {
+        this(api, tree, FolderStorage.ROOT_ID, failOnError);
+    }
+
+    public SubscribeRequest(final API api, final String tree, final String parent, final boolean failOnError) {
+        super(api);
+        this.tree = tree;
+        this.parent = parent;
+        this.failOnError = failOnError;
+        folderIds = new LinkedHashSet<String>(4);
+    }
+
+    /**
+     * Adds specified folder identifier.
+     * 
+     * @param folderId The folder identifier
+     * @return This request with folder identifier added
+     */
+    public SubscribeRequest addFolderId(final String folderId) {
+        folderIds.add(folderId);
+        return this;
+    }
+
+    public Object getBody() {
+        final JSONArray jArray = new JSONArray();
+        for (final String folderId : folderIds) {
+            jArray.put(folderId);
+        }
+        return jArray;
+    }
+
+    public Method getMethod() {
+        return Method.PUT;
+    }
+
+    @Override
+    protected void addParameters(final List<Parameter> params) {
+        params.add(new Parameter(AJAXServlet.PARAMETER_ACTION, "subscribe"));
+        params.add(new Parameter(Folder.PARAMETER_PARENT, parent));
+        params.add(new Parameter("tree", tree));
+    }
+
+    public AbstractAJAXParser<? extends SubscribeResponse> getParser() {
+        return new AbstractAJAXParser<SubscribeResponse>(failOnError) {
+
+            @Override
+            protected SubscribeResponse createResponse(final Response response) {
+                return new SubscribeResponse(response);
+            }
+        };
+    }
+}
