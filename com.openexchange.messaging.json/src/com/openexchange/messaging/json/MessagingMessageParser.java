17e787a2a450b0ae00b974b62232fa33e92c6496
Thorben Betten <thorben.betten@open-xchange.com>
2011-11-29 19:02:44 +0100

    Enhanced parsing

--- com.openexchange.messaging.json/src/com/openexchange/messaging/json/MessagingMessageParser.java
+++ com.openexchange.messaging.json/src/com/openexchange/messaging/json/MessagingMessageParser.java
@@ -79,6 +79,7 @@ import com.openexchange.messaging.MessagingHeader;
 import com.openexchange.messaging.MessagingHeader.KnownHeader;
 import com.openexchange.messaging.MessagingMessage;
 import com.openexchange.messaging.MessagingPart;
+import com.openexchange.messaging.MultipartContent;
 import com.openexchange.messaging.ParameterizedMessagingMessage;
 import com.openexchange.messaging.StringContent;
 import com.openexchange.messaging.StringMessageHeader;
@@ -93,7 +94,7 @@ import com.openexchange.tools.stream.UnsynchronizedByteArrayOutputStream;
 /**
  * A parser to parse JSON representations of MessagingMessages. Note that parsing can be customized by registering one or more
  * {@link MessagingHeaderParser} and one or more {@link MessagingContentParser}.
- *
+ * 
  * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
@@ -169,6 +170,13 @@ public class MessagingMessageParser {
         setValues(message, registry, messageJSON);
 
         /*
+         * Parse possible file references
+         */
+        if (messageJSON.hasAndNotNull("attachments")) {
+            parseAttachments(message, messageJSON, registry);
+        }
+
+        /*
          * Parse possible captcha element
          */
         if (messageJSON.hasAndNotNull("captcha")) {
@@ -187,6 +195,82 @@ public class MessagingMessageParser {
         }
 
         return message;
+    }
+
+    private void parseAttachments(final MimeMessagingMessage message, final JSONObject messageJSON, final MessagingInputStreamRegistry registry) throws OXException, JSONException, IOException {
+        final Object object = messageJSON.opt("attachments");
+        if (object instanceof JSONArray) {
+            final JSONArray attachments = (JSONArray) object;
+            final int length = attachments.length();
+            if (length > 0) {
+                final MimeMultipartContent mimeMultipartContent = new MimeMultipartContent("mixed");
+                /*
+                 * Add previous message content
+                 */
+                {
+                    final MessagingContent content = message.getContent();
+                    if (content instanceof MultipartContent) {
+                        final MultipartContent multipartContent = (MultipartContent) content;
+                        if ("mixed".equals(multipartContent.getSubType())) {
+                            final int count = multipartContent.getCount();
+                            for (int i = 0; i < count; i++) {
+                                mimeMultipartContent.addBodyPart((MimeMessagingBodyPart) multipartContent.get(i));
+                            }
+                        } else {
+                            final MimeMessagingBodyPart bodyPart = new MimeMessagingBodyPart(mimeMultipartContent);
+                            bodyPart.setContent((MimeMultipartContent) multipartContent);
+                            mimeMultipartContent.addBodyPart(bodyPart);
+                        }
+                    } else {
+                        final MimeMessagingBodyPart bodyPart = new MimeMessagingBodyPart(mimeMultipartContent);
+                        bodyPart.setContent(content, message.getContentType().toString());
+                        bodyPart.setDisposition(message.getDisposition());
+                        mimeMultipartContent.addBodyPart(bodyPart);
+                    }
+                }
+                /*
+                 * Add referenced attachments to multipart
+                 */
+                for (int i = 0; i < length; i++) {
+                    final String identifier = attachments.getString(i);
+                    final Object registryEntry = registry.getRegistryEntry(identifier);
+                    final MimeMessagingBodyPart bodyPart = new MimeMessagingBodyPart(mimeMultipartContent);
+                    if (registryEntry instanceof ManagedFile) {
+                        final ManagedFile managedFile = (ManagedFile) registryEntry;
+                        final MessagingContent attachmentContent = new ManagedFileContentImpl(managedFile);
+                        bodyPart.setContent(attachmentContent, managedFile.getContentType());
+                        bodyPart.setDisposition(managedFile.getContentDisposition());
+                        bodyPart.setFileName(managedFile.getFileName());
+                    } else {
+                        /*
+                         * Initialize input stream
+                         */
+                        final InputStream in = new BufferedInputStream(registry.get(identifier));
+                        try {
+                            final ByteArrayOutputStream out = new UnsynchronizedByteArrayOutputStream(8192 << 1);
+                            final byte[] buf = new byte[8192];
+                            int read;
+                            while ((read = in.read(buf, 0, buf.length)) != -1) {
+                                out.write(buf, 0, read);
+                            }
+                            final MessagingContent attachmentContent = new ByteArrayContent(out.toByteArray());
+                            bodyPart.setContent(attachmentContent, "application/octet-stream");
+                            bodyPart.setDisposition(MessagingPart.ATTACHMENT);
+                        } finally {
+                            try {
+                                in.close();
+                            } catch (final IOException e) {
+                                org.apache.commons.logging.LogFactory.getLog(MessagingMessageParser.BinaryContentParser.class).error(
+                                    "Couldn't close input stream.",
+                                    e);
+                            }
+                        }
+                    }
+                    mimeMultipartContent.addBodyPart(bodyPart);
+                    message.setContent(mimeMultipartContent);
+                }
+            }
+        }
     }
 
     protected void setValues(final MimeMessagingBodyPart bodyPart, final MessagingInputStreamRegistry registry, final JSONObject messageJSON) throws JSONException, OXException, IOException {
@@ -221,7 +305,7 @@ public class MessagingMessageParser {
 
     /**
      * Adds a {@link MessagingHeaderParser} to the list of known parsers. In this way new headers may be parsed in a custom manner
-     *
+     * 
      * @param parser
      */
     public void addHeaderParser(final MessagingHeaderParser parser) {
@@ -235,7 +319,7 @@ public class MessagingMessageParser {
     /**
      * Adds a {@link MessagingContentParser} to the list of known parsers. In this way new {@link MessagingContent} types may be parsed in a
      * custom manner
-     *
+     * 
      * @param parser
      */
     public void addContentParser(final MessagingContentParser parser) {
@@ -399,23 +483,7 @@ public class MessagingMessageParser {
                     final Object registryEntry = registry.getRegistryEntry(identifier);
                     if (registryEntry instanceof ManagedFile) {
                         final ManagedFile managedFile = (ManagedFile) registryEntry;
-                        return new ManagedFileContent() {
-                            
-                            @Override
-                            public InputStream getData() throws OXException {
-                                return managedFile.getInputStream();
-                            }
-                            
-                            @Override
-                            public String getFileName() {
-                                return managedFile.getFileName();
-                            }
-                            
-                            @Override
-                            public String getContentType() {
-                                return managedFile.getContentType();
-                            }
-                        };
+                        return new ManagedFileContentImpl(managedFile);
                     }
                 }
                 /*
@@ -467,7 +535,6 @@ public class MessagingMessageParser {
 
         @Override
         public MessagingContent parse(final MessagingBodyPart partlyParsedMessage, final Object content, final MessagingInputStreamRegistry registry) throws JSONException, OXException, IOException {
-
             final MimeMultipartContent multipartContent = new MimeMultipartContent();
             final JSONArray multipartJSON = (JSONArray) content;
             for (int i = 0, size = multipartJSON.length(); i < size; i++) {
@@ -481,6 +548,36 @@ public class MessagingMessageParser {
             return multipartContent;
         }
 
+    }
+
+    private static final class ManagedFileContentImpl implements ManagedFileContent {
+
+        private final ManagedFile managedFile;
+
+        /**
+         * Initializes a new {@link ManagedFileContentImplementation}.
+         * 
+         * @param managedFile
+         */
+        public ManagedFileContentImpl(final ManagedFile managedFile) {
+            super();
+            this.managedFile = managedFile;
+        }
+
+        @Override
+        public InputStream getData() throws OXException {
+            return managedFile.getInputStream();
+        }
+
+        @Override
+        public String getFileName() {
+            return managedFile.getFileName();
+        }
+
+        @Override
+        public String getContentType() {
+            return managedFile.getContentType();
+        }
     }
 
 }
