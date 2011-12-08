7c841c18e3931cd917a103d7666b62967c3f5bfa
Marcus Klein <marcus.klein@open-xchange.com>
2011-12-08 10:26:26 +0000

    Fix for bug 19733: Catching NumberFormatException when trying to get IMAP port.

--- com.openexchange.admin.soap/src/com/openexchange/admin/soap/dataobjects/User.java
+++ com.openexchange.admin.soap/src/com/openexchange/admin/soap/dataobjects/User.java
@@ -2554,7 +2554,11 @@ public class User {
         if (this.imapServer != null) {
             final Matcher matcher = URL_PATTERN.matcher(this.imapServer);
             if (matcher.matches() && null != matcher.group(4)) {
-                return Integer.parseInt(matcher.group(4));
+                try {
+                    return Integer.parseInt(matcher.group(4));
+                } catch (final NumberFormatException e) {
+                    return 143;
+                }
             }
         }
         return 143;
