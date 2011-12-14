91f9ebef7940ea893485b7dbe741d28c95ab6d94
Thorben Betten <thorben.betten@open-xchange.com>
2011-12-14 11:27:47 +0100

    as

--- com.openexchange.ajp/src/com/openexchange/ajp13/coyote/AjpProcessor.java
+++ com.openexchange.ajp/src/com/openexchange/ajp13/coyote/AjpProcessor.java
@@ -864,7 +864,6 @@ public final class AjpProcessor implements com.openexchange.ajp13.watcher.Task {
                     if (longRunningAccepted) {
                         AjpLongRunningRegistry.getInstance().deregisterLongRunning(request);
                     }
-                    stage = Stage.STAGE_SERVICE_ENDED;
                 }
             }
             /*
