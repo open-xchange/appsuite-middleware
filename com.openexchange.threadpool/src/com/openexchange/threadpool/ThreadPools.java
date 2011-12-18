607bc1e987f9dc6cb1e50e7707b6b3e0bf6e013a
Thorben Betten <thorben.betten@open-xchange.com>
2011-12-18 11:08:53 +0100

    keep flag

--- com.openexchange.threadpool/src/com/openexchange/threadpool/ThreadPools.java
+++ com.openexchange.threadpool/src/com/openexchange/threadpool/ThreadPools.java
@@ -84,6 +84,8 @@ public final class ThreadPools {
      * @param t The unexpectedly interrupted thread
      */
     public static void unexpectedlyInterrupted(final Thread t) {
+        // Keep interrupted flag
+        t.interrupt();
         if (t instanceof InterruptorAware) {
             final StackTraceElement[] interruptorStack = ((InterruptorAware) t).getInterruptorStack();
             if (null != interruptorStack) {
