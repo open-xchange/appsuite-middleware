92d27d4e1dc701820c98720eb1e743d48dd046dd
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2011-12-19 09:57:46 +0100

    Added openTrackers()

--- com.openexchange.pop3/src/com/openexchange/pop3/osgi/POP3Activator.java
+++ com.openexchange.pop3/src/com/openexchange/pop3/osgi/POP3Activator.java
@@ -148,6 +148,7 @@ public final class POP3Activator extends HousekeepingActivator {
              */
             track(POP3StorageProvider.class, customizer);
             track(SessiondService.class);
+            openTrackers();
             /*
              * Register
              */
