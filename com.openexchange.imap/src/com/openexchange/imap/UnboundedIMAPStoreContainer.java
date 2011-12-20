07c9732e381d0e8e42fba84bde71a4fa93605253
Thorben Betten <thorben.betten@open-xchange.com>
2011-12-20 17:29:55 +0100

    debug logging

--- com.openexchange.imap/src/com/openexchange/imap/UnboundedIMAPStoreContainer.java
+++ com.openexchange.imap/src/com/openexchange/imap/UnboundedIMAPStoreContainer.java
@@ -120,6 +120,9 @@ public class UnboundedIMAPStoreContainer extends AbstractIMAPStoreContainer {
 
     @Override
     public void closeElapsed(final long stamp, final StringBuilder debugBuilder) {
+        if (DEBUG) {
+            LOG.debug("IMAPStoreContainer.closeElapsed(): " + queue.size() + " IMAPStore instances in queue for " + login + "@" + server + ":" + port);
+        }
         IMAPStoreWrapper imapStoreWrapper;
         do {
             imapStoreWrapper = queue.pollIfElapsed(stamp);
