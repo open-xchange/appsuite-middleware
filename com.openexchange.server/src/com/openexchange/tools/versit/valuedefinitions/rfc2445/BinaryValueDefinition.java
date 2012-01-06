5879eaeca6e4fd4a3af6b596a49d299df98e84ad
Tobias <tobias.friedrich@open-xchange.com>
2012-01-06 16:32:31 +0100

    Fix for bug 21127: Skip intermediate Latin-1 conversion for inline encoded binary property values

--- com.openexchange.server/src/com/openexchange/tools/versit/valuedefinitions/rfc2445/BinaryValueDefinition.java
+++ com.openexchange.server/src/com/openexchange/tools/versit/valuedefinitions/rfc2445/BinaryValueDefinition.java
@@ -50,10 +50,17 @@
 package com.openexchange.tools.versit.valuedefinitions.rfc2445;
 
 import java.io.IOException;
+
+import com.openexchange.tools.encoding.Base64;
 import com.openexchange.tools.versit.Encoding;
+import com.openexchange.tools.versit.FoldingWriter;
+import com.openexchange.tools.versit.Parameter;
 import com.openexchange.tools.versit.Property;
+import com.openexchange.tools.versit.Scanner;
 import com.openexchange.tools.versit.StringScanner;
 import com.openexchange.tools.versit.ValueDefinition;
+import com.openexchange.tools.versit.VersitException;
+import com.openexchange.tools.versit.encodings.BASE64Encoding;
 
 /**
  * @author Viktor Pracht
@@ -74,4 +81,52 @@ public class BinaryValueDefinition extends ValueDefinition {
         return new String((byte[]) value, com.openexchange.java.Charsets.ISO_8859_1);
     }
 
+    @Override
+    public void write(final FoldingWriter fw, final Property property) throws IOException {
+    	String value = null;
+        final Parameter encodingParam = property.getParameter("ENCODING");
+        if (encodingParam != null) {
+            final String enc_name = encodingParam.getValue(0).getText();
+            final Encoding encoding = getEncoding(enc_name);
+            if (encoding == null) {
+                throw new IOException("Unknown encoding: " + enc_name);
+            }           
+            final Object propValue = property.getValue(); 
+            if (null != propValue && BASE64Encoding.class.equals(encoding.getClass()) && byte[].class.equals(propValue.getClass())) {
+            	// no need to encode to Latin-1 and back
+            	value = Base64.encode((byte[])propValue);
+            } else {
+            	// use default write method
+                value = writeValue(property.getValue());
+            }
+        } else {
+            value = writeValue(property.getValue());
+        }
+        fw.writeln(value);
+    }
+    
+    public Object parse(final Scanner s, final Property property) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        while (!(s.peek < ' ' && s.peek != '\t' || s.peek == 0x7f)) {
+            sb.append((char) s.read());
+        }
+        final String text = sb.toString();
+        final Parameter encodingParam = property.getParameter("ENCODING");
+        if (encodingParam != null) {
+            final String EncName = encodingParam.getValue(0).getText();
+            final Encoding encoding = getEncoding(EncName);
+            if (encoding == null) {
+                throw new VersitException(s, "Unknown encoding: " + EncName);
+            }
+            if (BASE64Encoding.class.equals(encoding.getClass())) {
+            	// no need to encode to Latin-1 and back
+            	return Base64.decode(text);
+            } else {
+            	// use encoding with default createValue method
+            	return createValue(new StringScanner(s, encoding.decode(text)), property);
+            }
+        } else {
+        	return createValue(new StringScanner(s, text), property);
+        }    	
+    }
 }
