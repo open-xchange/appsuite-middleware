1262a4ca69cf3da3742dd1ef692fdb89cad840e7
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2012-02-09 11:59:04 +0100

    - Add method to DeferredActivator for outstanding services - JMX interface

---
+++ com.openexchange.osgi/src/com/openexchange/osgi/OsgiActivator.java
@@ -0,0 +1,112 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2011 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.osgi;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import com.openexchange.management.ManagementService;
+
+/**
+ * {@link OsgiActivator} - Activator for OSGi-Bundle
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ */
+public class OsgiActivator extends HousekeepingActivator {
+
+    private static final Log LOG = com.openexchange.log.Log.valueOf(LogFactory.getLog(OsgiActivator.class));
+
+
+    /**
+     * Initializes a new {@link OsgiActivator}.
+     */
+    public OsgiActivator() {
+        super();
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.osgi.DeferredActivator#getNeededServices()
+     */
+    @Override
+    protected Class<?>[] getNeededServices() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.osgi.DeferredActivator#startBundle()
+     */
+    @Override
+    protected void startBundle() throws Exception {
+        try {
+            if (LOG.isInfoEnabled()) {
+                LOG.info("starting bundle: com.openexchange.osgi");
+            }
+            track(ManagementService.class, new ManagementRegisterer(context));
+            openTrackers();
+        } catch (final Exception e) {
+            LOG.error("OsgiActivator: start: ", e);
+            throw e;
+        }
+    }
+
+    @Override
+    protected void stopBundle() throws Exception {
+        try {
+            if (LOG.isInfoEnabled()) {
+                LOG.info("stopping bundle: com.openexchange.osgi");
+            }
+//            closeTrackers();
+        } catch (final Exception e) {
+            LOG.error("OsgiActivator: stop: ", e);
+            throw e;
+        }
+    }
+
+}
