e496f85c02b89c515d6c767db1fdbb991dc605bb
Thorben Betten <thorben.betten@open-xchange.com>
2012-02-23 19:47:07 +0100

    rename

--- com.openexchange.ajp/servlet2.5/com/openexchange/ajp13/coyote/HttpServletRequestImpl.java
+++ com.openexchange.ajp/servlet2.5/com/openexchange/ajp13/coyote/HttpServletRequestImpl.java
@@ -1039,8 +1039,8 @@ public final class HttpServletRequestImpl implements HttpServletRequest {
          * First look-up HttpSessionManagement if a session already exists
          */
         final AjpProcessor ajpProcessor = response.getAjpProcessor();
-        final Cookie sessionCookie = ajpProcessor.getHttpSessionCookie();
-        final String httpSessionId = sessionCookie.getValue();
+        final Cookie jsessionIdCookie = ajpProcessor.getHttpSessionCookie();
+        final String httpSessionId = jsessionIdCookie.getValue();
         final HttpSessionWrapper httpSession = HttpSessionManagement.getHttpSession(httpSessionId);
         if (httpSession != null) {
             if (!HttpSessionManagement.isHttpSessionExpired(httpSession)) {
@@ -1073,16 +1073,16 @@ public final class HttpServletRequestImpl implements HttpServletRequest {
             /*
              * Add JSESSIONID cookie
              */
-            configureCookie(sessionCookie);
-            response.addCookie(sessionCookie);
+            configureCookie(jsessionIdCookie);
+            response.addCookie(jsessionIdCookie);
         }
         return session;
     }
 
     private static final String DEFAULT_PATH = "/";
 
-    private static void configureCookie(final Cookie sessionCookie) {
-        sessionCookie.setPath(DEFAULT_PATH);
+    private static void configureCookie(final Cookie jsessionIdCookie) {
+        jsessionIdCookie.setPath(DEFAULT_PATH);
         final ConfigurationService configurationService = AJPv13ServiceRegistry.getInstance().getService(ConfigurationService.class);
         /*
          * Check if auto-login is enabled
@@ -1090,9 +1090,9 @@ public final class HttpServletRequestImpl implements HttpServletRequest {
         if (null != configurationService && configurationService.getBoolProperty("com.openexchange.sessiond.autologin", false)) {
             final int maxAge =
                 (int) (ConfigTools.parseTimespan(configurationService.getProperty("com.openexchange.cookie.ttl", "1W")) / 1000);
-            sessionCookie.setMaxAge(maxAge);
+            jsessionIdCookie.setMaxAge(maxAge);
         } else {
-            sessionCookie.setMaxAge(-1); // cookies auto-expire
+            jsessionIdCookie.setMaxAge(-1); // cookies auto-expire
         }
     }
 
