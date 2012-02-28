0f24d1afaf5ec961b81eccb0730d84e76dd0c938
tobias.prinz <tobias.prinz@open-xchange.com>
2012-02-28 11:54:36 +0100

    Still using my e-mail in to: field, because Sinatra has not been tested in the wild yet.

---
+++ tools/scripts/pivotal-events/pivotal-event-handler.rb
@@ -0,0 +1,303 @@
+testdata = <<-EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110983</id>
+  <version type="integer">462</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 14:00:40 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz edited &quot;Pivotal tracker event hook is used to inform QA&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">25557009</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/25557009</url>
+      <description>Just collecting data for our event framework</description>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110853</id>
+  <version type="integer">461</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 14:00:22 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz accepted &quot;Pivotal tracker event hook is used to inform QA&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">25557009</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/25557009</url>
+      <accepted_at type="datetime">2012/02/27 14:00:22 UTC</accepted_at>
+      <current_state>accepted</current_state>
+      <labels>epic story</labels>
+    </story>
+    <story>
+      <distraction/>
+    </story>
+    <story>
+      <id type="integer">666</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/666</url>
+      <accepted_at type="datetime">2012/02/27 14:00:22 UTC</accepted_at>
+      <current_state>accepted</current_state>
+      <labels>qa</labels>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110815</id>
+  <version type="integer">460</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 14:00:18 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz delivered &quot;Pivotal tracker event hook is used to inform QA&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">25557009</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/25557009</url>
+      <current_state>delivered</current_state>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110799</id>
+  <version type="integer">459</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 14:00:15 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz finished &quot;Pivotal tracker event hook is used to inform QA&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">25557009</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/25557009</url>
+      <current_state>finished</current_state>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110785</id>
+  <version type="integer">458</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 14:00:14 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz started &quot;Pivotal tracker event hook is used to inform QA&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">25557009</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/25557009</url>
+      <current_state>started</current_state>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110725</id>
+  <version type="integer">457</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 14:00:07 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz edited &quot;Review mail app&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">23997621</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/23997621</url>
+      <owned_by></owned_by>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110625</id>
+  <version type="integer">456</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 13:59:51 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz edited &quot;Review mail app&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">23997621</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/23997621</url>
+      <current_state>unstarted</current_state>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110515</id>
+  <version type="integer">455</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 13:59:33 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz started &quot;Review mail app&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">23997621</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/23997621</url>
+      <current_state>started</current_state>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166110319</id>
+  <version type="integer">454</version>
+  <event_type>story_update</event_type>
+  <occurred_at type="datetime">2012/02/27 13:59:01 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz edited &quot;Pivotal tracker event hook is used to inform QA&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">25557009</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/25557009</url>
+      <estimate type="integer">1</estimate>
+    </story>
+  </stories>
+</activity>
+@NEXT@
+<?xml version="1.0" encoding="UTF-8"?>
+<activity>
+  <id type="integer">166103933</id>
+  <version type="integer">453</version>
+  <event_type>story_create</event_type>
+  <occurred_at type="datetime">2012/02/27 13:42:18 UTC</occurred_at>
+  <author>Tobias Prinz</author>
+  <project_id type="integer">452209</project_id>
+  <description>Tobias Prinz added &quot;Pivotal tracker event hook is used to inform QA&quot;</description>
+  <stories type="array">
+    <story>
+      <id type="integer">25557009</id>
+      <url>http://www.pivotaltracker.com/services/v3/projects/452209/stories/25557009</url>
+      <name>Pivotal tracker event hook is used to inform QA</name>
+      <story_type>feature</story_type>
+      <current_state>unscheduled</current_state>
+      <requested_by>Tobias Prinz</requested_by>
+    </story>
+  </stories>
+</activity>
+EOS
+
+data = testdata.split("@NEXT@")
+
+require 'rubygems'
+require 'hpricot'
+require 'base64'
+require 'open-uri'
+require 'net/smtp'
+require 'net/http'
+require 'cgi'
+require 'sinatra'
+
+#
+# settings
+#
+piv_username = Base64.decode64("dG9iaWFzLnByaW56QG9wZW4teGNoYW5nZS5jb20=")
+piv_password = Base64.decode64("dHlnZ2Vy")
+mail_username = Base64.decode64("dG9iaWFzLnByaW56QG9wZW4teGNoYW5nZS5jb20=")
+mail_password = Base64.decode64("dHlnZ2Vy")
+
+@piv_con = PivotalConnection.new( piv_username, piv_password )
+@mail_con = MailConnection.new( mail_username, mail_password )
+@handlers = [ QualityEventHandler.new ]
+
+#
+# classes
+#
+class PivotalConnection
+  attr_accessor :token
+  
+  def initialize(username, password)
+    open("https://www.pivotaltracker.com/services/v3/tokens/active", :http_basic_authentication=>[username, password]) do |handler|
+      content = handler.read
+      content =~ /<guid>(.+?)<\/guid>/
+      @token = $1
+    end
+  end
+  
+  def set_labels(labels, url)
+    uri = URI(url+"?story[labels]=#{CGI::escape(labels)}")
+    req = Net::HTTP::Put.new(uri.request_uri)
+    req['X-TrackerToken'] = @token
+    req['Content-Length'] = 0
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+      http.request(req)
+    }
+  end
+  
+end
+
+
+class MailConnection
+  attr_accessor :login, :password
+  
+  def initialize(login, password)
+    @login = login
+    @password = password
+  end
+  
+  def send_qa_message(title,link)
+    recipient = "tierlieb@open-xchange.com" #qualityassurance@open-xchange.com
+    message = <<-MESSAGE_END
+From: Pivotal Events<#{@login}>
+To: QA <#{recipient}>
+Subject: [Pivotal Event] Test this: #{title}
+The following user story has been marked as "accepted". The next step is to test it:
+#{link}
+MESSAGE_END
+    
+    smtp = Net::SMTP.new('smtp.open-xchange.com', 25)
+    smtp.start('open-xchange.com', @login, @password, :plain) { |smtp| smtp.send_message(message, @login, [recipient])}
+
+  end
+end
+
+
+class QualityEventHandler
+  
+  def is_responsible_for(activity)
+    activity.search("//story/current_state").any?{|elem| elem.inner_html == 'accepted'}
+  end
+  
+  def action(activity, piv_con, mail_con)
+    stories = activity.search("//story")
+    stories.each do |story|
+      next unless story.at("current_state") != nil && story.at("current_state").inner_html == 'accepted' # Condition: Must have been changed to "accepted""
+      next unless story.at("labels") == nil || !story.at("labels").inner_html.split(",").include?("qa")  # Condition: Must not yet have the label "qa"
+
+      labels = story.at("labels") == nil ? "qa" : story.at("labels").inner_html + ",qa"
+      url = story.at("url").inner_html.chomp
+      piv_con.set_labels(labels, url) 
+      
+      description = activity.at("description").inner_html.chomp
+      clickable_url = url.sub(/http:/,"https:").sub(/services\/v3\//,"")
+      mail_con.send_qa_message(description,clickable_url)
+    end
+  end
+end
+
+#
+# main
+#
+post '/' do
+  doc = Hpricot(request.body)
+  @handlers.each { |handler| handler.action(doc, @piv_con, @mail_con) if handler.is_responsible_for(doc)}
+end
+
