3e31d045ba7efd4a32e0396d9b44f9ab28e190c7
Thorben Betten <thorben.betten@open-xchange.com>
2012-03-01 12:52:43 +0100

    minor changes

--- com.openexchange.mq/src/com/openexchange/mq/osgi/MQActivator.java
+++ com.openexchange.mq/src/com/openexchange/mq/osgi/MQActivator.java
@@ -109,16 +109,23 @@ public final class MQActivator extends HousekeepingActivator {
 
     @Override
     protected void stopBundle() throws Exception {
-        final MQServerStartup serverStartup = this.serverStartup;
-        if (null != serverStartup) {
-            serverStartup.stop();
-            this.serverStartup = null;
+        final Log log = com.openexchange.log.Log.valueOf(LogFactory.getLog(MQActivator.class));
+        log.info("Stopping bundle: " + MQConstants.BUNDLE_SYMBOLIC_NAME);
+        try {
+            final MQServerStartup serverStartup = this.serverStartup;
+            if (null != serverStartup) {
+                serverStartup.stop();
+                this.serverStartup = null;
+            }
+            removeService(MQService.class);
+            MQServiceLookup.setServiceLookup(null);
+            MQServiceLookup.setMQService(null);
+            MQService.SERVICE_REFERENCE.set(null);
+            super.stopBundle();
+        } catch (final Exception e) {
+            log.error("Error stopping bundle: " + MQConstants.BUNDLE_SYMBOLIC_NAME);
+            throw e;
         }
-        removeService(MQService.class);
-        MQServiceLookup.setServiceLookup(null);
-        MQServiceLookup.setMQService(null);
-        MQService.SERVICE_REFERENCE.set(null);
-        super.stopBundle();
     }
 
 }
