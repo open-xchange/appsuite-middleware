6d506a5a91fba8ca50abdbdf19234972e2b6dce5
martin.herfurth <martin.herfurth@open.xchange.com>
2012-03-15 17:41:26 +0100

    Replaced OSGi helpers with HousekeepingAcitvator

--- com.openexchange.carddav/src/com/openexchange/carddav/osgi/CarddavActivator.java
+++ com.openexchange.carddav/src/com/openexchange/carddav/osgi/CarddavActivator.java
@@ -2,7 +2,7 @@ package com.openexchange.carddav.osgi;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
+import org.osgi.service.http.HttpService;
 import com.openexchange.carddav.mixins.AddressbookHomeSet;
 import com.openexchange.carddav.servlet.CardDAV;
 import com.openexchange.carddav.servlet.CarddavPerformer;
@@ -10,7 +10,6 @@ import com.openexchange.config.cascade.ConfigViewFactory;
 import com.openexchange.contact.ContactService;
 import com.openexchange.folderstorage.FolderService;
 import com.openexchange.osgi.HousekeepingActivator;
-import com.openexchange.tools.service.ServletRegistration;
 import com.openexchange.user.UserService;
 import com.openexchange.webdav.directory.PathRegistration;
 import com.openexchange.webdav.protocol.helpers.PropertyMixin;
@@ -19,8 +18,7 @@ import com.openexchange.webdav.protocol.osgi.OSGiPropertyMixin;
 public class CarddavActivator extends HousekeepingActivator {
 
     private static final Log LOG = LogFactory.getLog(CarddavActivator.class);
-    private static final Class<?>[] NEEDED = new Class[] { 
-    	FolderService.class, ConfigViewFactory.class, UserService.class, ContactService.class };
+    private static final Class<?>[] NEEDED = new Class[] { HttpService.class, FolderService.class, ConfigViewFactory.class, UserService.class, ContactService.class };
 
     private OSGiPropertyMixin mixin;
 
@@ -35,7 +33,7 @@ public class CarddavActivator extends HousekeepingActivator {
             CardDAV.setServiceLookup(this);
             CarddavPerformer.setServices(this);
             
-            rememberTracker(new ServletRegistration(context, new CardDAV(), "/servlet/dav/carddav"));
+            getService(HttpService.class).registerServlet("/servlet/dav/carddav", new CardDAV(), null, null);
             
             CarddavPerformer performer = CarddavPerformer.getInstance();
             mixin = new OSGiPropertyMixin(context, performer);
