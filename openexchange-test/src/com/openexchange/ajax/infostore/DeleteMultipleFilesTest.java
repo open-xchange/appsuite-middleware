2569c445fef9be639c05b0797fc581af900dc990
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2012-03-28 11:05:39 +0200

    Improved DeleteRequest to delete multiple object with one request

---
+++ openexchange-test/src/com/openexchange/ajax/infostore/DeleteMultipleFilesTest.java
@@ -0,0 +1,129 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax.infostore;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.json.JSONArray;
+import org.junit.Test;
+import com.openexchange.ajax.InfostoreAJAXTest;
+import com.openexchange.ajax.framework.AJAXClient;
+import com.openexchange.ajax.framework.AJAXClient.User;
+import com.openexchange.ajax.infostore.actions.DeleteInfostoreRequest;
+import com.openexchange.ajax.infostore.actions.DeleteInfostoreResponse;
+import com.openexchange.ajax.infostore.actions.NewInfostoreRequest;
+import com.openexchange.ajax.infostore.actions.NewInfostoreResponse;
+import com.openexchange.groupware.infostore.database.impl.DocumentMetadataImpl;
+
+
+/**
+ * {@link DeleteMultipleFilesTest}
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ */
+public class DeleteMultipleFilesTest extends InfostoreAJAXTest {
+    
+    private AJAXClient client;
+    private List<Integer> itemIds;
+    private List<Integer> folderIds;
+
+    /**
+     * Initializes a new {@link DeleteMultipleFilesTest}.
+     * @param name
+     */
+    public DeleteMultipleFilesTest(String name) {
+        super(name);
+        
+    }
+    
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        client = new AJAXClient(User.User1);
+        itemIds = new ArrayList<Integer>();
+        folderIds = new ArrayList<Integer>();
+        
+        File ods = new File("testData/TestODS.ods");
+        DocumentMetadataImpl data1 = new DocumentMetadataImpl();
+        data1.setFileName(ods.getName());
+        data1.setFolderId(client.getValues().getPrivateInfostoreFolder());
+        data1.setCreationDate(new Date());
+        NewInfostoreRequest newReq1 = new NewInfostoreRequest(data1, ods);
+        NewInfostoreResponse newRes1 = client.execute(newReq1);
+        itemIds.add(newRes1.getID());
+        folderIds.add((int) data1.getFolderId());
+        
+        File odt = new File("testData/TestODT.odt");
+        DocumentMetadataImpl data2 = new DocumentMetadataImpl();
+        data2.setFileName(odt.getName());
+        data2.setFolderId(client.getValues().getPrivateInfostoreFolder());
+        data2.setCreationDate(new Date());
+        NewInfostoreRequest newReq2 = new NewInfostoreRequest(data2, odt);
+        NewInfostoreResponse newRes2 = client.execute(newReq2);
+        itemIds.add(newRes2.getID());
+        folderIds.add((int) data2.getFolderId());
+    }
+    
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    @Test
+    public void testDeleteMultipleFiles() throws Exception {
+        DeleteInfostoreRequest delReq = new DeleteInfostoreRequest(itemIds, folderIds, new Date());
+        DeleteInfostoreResponse delRes = client.execute(delReq);
+        JSONArray json = (JSONArray) delRes.getData();
+        assertTrue("Delete failed: " + delRes.getResponse().toString(), json.isNull(0));
+    }
+
+}
