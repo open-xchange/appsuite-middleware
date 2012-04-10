f5889f7e348435fe55117b4983f34e80b2ea0317
martin.herfurth <martin.herfurth@open.xchange.com>
2012-04-10 11:31:26 +0200

    Fixed Bug 21620

--- openexchange-test/src/com/openexchange/ajax/appointment/bugtests/AppointmentBugTestSuite.java
+++ openexchange-test/src/com/openexchange/ajax/appointment/bugtests/AppointmentBugTestSuite.java
@@ -133,6 +133,7 @@ public class AppointmentBugTestSuite extends TestSuite {
 		tests.addTestSuite(Bug20980Test_DateOnMissingDSTHour.class);
         tests.addTestSuite(Bug21264Test.class);
         tests.addTestSuite(Bug21614Test.class);
+        tests.addTestSuite(Bug21620Test.class);
 		return tests;
 	}
 }
