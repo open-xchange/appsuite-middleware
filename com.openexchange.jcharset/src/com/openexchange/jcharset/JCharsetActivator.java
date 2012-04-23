e1130a474873f19635b849f3cb261d51474cab5a
Thorben Betten <thorben.betten@open-xchange.com>
2012-04-23 14:37:15 +0200

    Restored

--- com.openexchange.jcharset/src/com/openexchange/jcharset/JCharsetActivator.java
+++ com.openexchange.jcharset/src/com/openexchange/jcharset/JCharsetActivator.java
@@ -51,7 +51,7 @@ package com.openexchange.jcharset;
 
 import java.nio.charset.spi.CharsetProvider;
 import org.apache.commons.logging.Log;
-import com.openexchange.log.LogFactory;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
