7f846a6201aa1fe177c364a38cbe9a143a739def
Francisco Laguna <francisco.laguna@open-xchange.com>
2012-05-09 15:49:12 +0200

    oauth_proxy actions

---
+++ com.openexchange.oauth.json/src/com/openexchange/oauth/json/proxy/OAuthProxyRequest.java
@@ -0,0 +1,20 @@
+package com.openexchange.oauth.json.proxy;
+
+import com.openexchange.ajax.requesthandler.AJAXRequestData;
+import com.openexchange.oauth.OAuthAccount;
+import com.openexchange.oauth.OAuthService;
+
+public class OAuthProxyRequest {
+	private AJAXRequestData req;
+	private OAuthService oauthService;
+	
+	public OAuthProxyRequest(AJAXRequestData req, OAuthService oauthService) {
+		this.req = req;
+		this.oauthService = oauthService;
+	}
+	
+	public OAuthAccount getAccount() {
+		// TODO
+		return null;
+	}
+}
