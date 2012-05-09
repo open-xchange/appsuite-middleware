3f574a127be3c82d37d4fd3d8d052b8c9aac395a
Marcus Klein <marcus.klein@open-xchange.com>
2012-05-09 20:08:53 +0200

    Fix for bug #21695: Due date for tasks must not be corrected to one day earlier as for full day appointments.

--- com.openexchange.server/test/com/openexchange/i18n/tools/replacement/TaskEndDateReplacementTest.java
+++ com.openexchange.server/test/com/openexchange/i18n/tools/replacement/TaskEndDateReplacementTest.java
@@ -47,39 +47,37 @@
  *
  */
 
-package com.openexchange;
+package com.openexchange.i18n.tools.replacement;
 
-import junit.framework.JUnit4TestAdapter;
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import com.openexchange.ajax.parser.ContactSearchtermSqlConverterTest;
-import com.openexchange.ajax.parser.TaskLastModifiedTest;
-import com.openexchange.groupware.ldap.UserAttributeDiffTest;
-import com.openexchange.i18n.tools.replacement.TaskEndDateReplacementTest;
-import com.openexchange.tools.collections.OXCollectionsTest;
-import com.openexchange.tools.iterator.SearchIteratorDelegatorTest;
-import com.openexchange.tools.net.URIParserTest;
+import static org.junit.Assert.assertEquals;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import org.junit.Test;
+import com.openexchange.groupware.i18n.Notifications;
+import com.openexchange.java.util.TimeZones;
 
 /**
- * {@link UnitTests}
+ * {@link TaskEndDateReplacementTest}
  *
  * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
  */
-public class UnitTests {
+public class TaskEndDateReplacementTest {
 
-    private UnitTests() {
-        super();
-    }
-
-    public static Test suite() {
-        final TestSuite tests = new TestSuite();
-        tests.addTestSuite(TaskLastModifiedTest.class);
-        tests.addTest(new JUnit4TestAdapter(TaskEndDateReplacementTest.class));
-        tests.addTestSuite(SearchIteratorDelegatorTest.class);
-        tests.addTestSuite(OXCollectionsTest.class);
-        tests.addTestSuite(ContactSearchtermSqlConverterTest.class);
-        tests.addTest(new JUnit4TestAdapter(URIParserTest.class));
-        tests.addTest(new JUnit4TestAdapter(UserAttributeDiffTest.class));
-        return tests;
+    @Test
+    public final void testGetReplacement() {
+        Locale locale = Locale.ENGLISH;
+        TimeZone tz = TimeZones.UTC;
+        Calendar cal = new GregorianCalendar(tz, locale);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        EndDateReplacement replacement = new EndDateReplacement(cal.getTime(), true, true, locale, tz);
+        DateFormat format = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+        String expected = String.format(locale, Notifications.FORMAT_DUE_DATE, format.format(cal.getTime()));
+        assertEquals("Due date for tasks is printed wrong in task notification mails.", expected, replacement.getReplacement());
     }
 }
