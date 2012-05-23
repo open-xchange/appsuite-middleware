024b4aa207e4900b2432ab5eab07d81f8b8045c0
Thorben Betten <thorben.betten@open-xchange.com>
2012-05-23 22:21:27 +0200

    Added utility class

---
+++ com.openexchange.global/src/com/openexchange/json/cache/JsonCacheUtils.java
@@ -0,0 +1,109 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.json.cache;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import com.openexchange.java.Charsets;
+
+/**
+ * {@link JsonCacheUtils} - Utility class for JSON cache.
+ * 
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ */
+public final class JsonCacheUtils {
+
+    /**
+     * Initializes a new {@link JsonCacheUtils}.
+     */
+    private JsonCacheUtils() {
+        super();
+    }
+
+    /**
+     * Gets the MD5 sum of passed arguments.
+     * 
+     * @param args The arguments
+     * @return The MD5 sum's hex representation
+     */
+    public static String getMD5Sum(final String... args) {
+        if (null == args || 0 == args.length) {
+            return "";
+        }
+        try {
+            final MessageDigest md = MessageDigest.getInstance("MD5");
+            for (final String arg : args) {
+                md.update(arg.getBytes(Charsets.UTF_8));
+            }
+            return asHex(md.digest());
+        } catch (final NoSuchAlgorithmException e) {
+            return "";
+        }
+    }
+
+    private static final char[] HEX_CHARS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+    /**
+     * Turns array of bytes into string representing each byte as unsigned hex number.
+     * 
+     * @param hash Array of bytes to convert to hex-string
+     * @return Generated hex string
+     */
+    public static String asHex(final byte hash[]) {
+        final int length = hash.length;
+        final char buf[] = new char[length * 2];
+        for (int i = 0, x = 0; i < length; i++) {
+            buf[x++] = HEX_CHARS[(hash[i] >>> 4) & 0xf];
+            buf[x++] = HEX_CHARS[hash[i] & 0xf];
+        }
+        return new String(buf);
+    }
+
+}
