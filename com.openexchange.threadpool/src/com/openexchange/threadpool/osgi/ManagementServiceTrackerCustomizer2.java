bddc46c8c23942a86669085e04b7d90200050fbc
Thorben Betten <thorben.betten@open-xchange.com>
2012-05-29 09:02:40 +0200

    Session thread count enhancements

---
+++ com.openexchange.threadpool/src/com/openexchange/threadpool/osgi/ManagementServiceTrackerCustomizer2.java
@@ -0,0 +1,159 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.threadpool.osgi;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import com.openexchange.management.ManagementService;
+import com.openexchange.session.SessionThreadCounter;
+import com.openexchange.sessionCount.SessionThreadCountMBean;
+import com.openexchange.sessionCount.SessionThreadCountMBeanImpl;
+
+/**
+ * {@link ManagementServiceTrackerCustomizer2} - The {@link ServiceTrackerCustomizer customizer} for {@link ManagementService}.
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ */
+public final class ManagementServiceTrackerCustomizer2 implements ServiceTrackerCustomizer<ManagementService, ManagementService> {
+
+    private final BundleContext context;
+
+    private final SessionThreadCounter counter;
+
+    private ObjectName objectName;
+
+    /**
+     * Initializes a new {@link ManagementServiceTrackerCustomizer}.
+     *
+     * @param context The bundle context
+     * @param counter The service
+     */
+    public ManagementServiceTrackerCustomizer2(final BundleContext context, final SessionThreadCounter counter) {
+        super();
+        this.context = context;
+        this.counter = counter;
+    }
+
+    @Override
+    public ManagementService addingService(final ServiceReference<ManagementService> reference) {
+        final ManagementService service = context.getService(reference);
+        /*
+         * Register MBean for thread pool
+         */
+        registerCacheMBean(service);
+        return service;
+    }
+
+    @Override
+    public void modifiedService(final ServiceReference<ManagementService> reference, final ManagementService service) {
+        // Nothing to do
+    }
+
+    @Override
+    public void removedService(final ServiceReference<ManagementService> reference, final ManagementService service) {
+        if (null != service && ManagementService.class.isInstance(service)) {
+            try {
+                /*
+                 * Unregister MBean for thread pool
+                 */
+                unregisterCacheMBean(service);
+            } finally {
+                context.ungetService(reference);
+            }
+        }
+    }
+
+    void registerCacheMBean(final ManagementService management) {
+        if (objectName == null) {
+            final org.apache.commons.logging.Log LOG =
+                com.openexchange.log.Log.valueOf(org.apache.commons.logging.LogFactory.getLog(ManagementServiceTrackerCustomizer.class));
+            try {
+                objectName = getObjectName(SessionThreadCountMBeanImpl.class.getName(), SessionThreadCountMBean.SESSION_THREAD_COUNT_DOMAIN);
+                management.registerMBean(objectName, new SessionThreadCountMBeanImpl(counter));
+            } catch (final MalformedObjectNameException e) {
+                LOG.error(e.getMessage(), e);
+            } catch (final NotCompliantMBeanException e) {
+                LOG.error(e.getMessage(), e);
+            } catch (final Exception e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+    }
+
+    void unregisterCacheMBean(final ManagementService management) {
+        if (objectName != null) {
+            try {
+                management.unregisterMBean(objectName);
+            } catch (final Exception e) {
+                com.openexchange.log.Log.valueOf(org.apache.commons.logging.LogFactory.getLog(ManagementServiceTrackerCustomizer.class)).error(e.getMessage(), e);
+            } finally {
+                objectName = null;
+            }
+        }
+    }
+
+    /**
+     * Creates an appropriate instance of {@link ObjectName} from specified class name and domain name.
+     *
+     * @param className The class name to use as object name
+     * @param domain The domain name
+     * @return An appropriate instance of {@link ObjectName}
+     * @throws MalformedObjectNameException If instantiation of {@link ObjectName} fails
+     */
+    private static ObjectName getObjectName(final String className, final String domain) throws MalformedObjectNameException {
+        final int pos = className.lastIndexOf('.');
+        return new ObjectName(domain, "name", pos == -1 ? className : className.substring(pos + 1));
+    }
+
+}
---
+++ com.openexchange.threadpool/src/com/openexchange/threadpool/osgi/SessionThreadCountEventHandler.java
@@ -0,0 +1,144 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2020 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.threadpool.osgi;
+
+import java.util.Set;
+import org.apache.commons.logging.Log;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.osgi.util.tracker.ServiceTracker;
+import com.openexchange.session.Session;
+import com.openexchange.session.SessionThreadCounter;
+import com.openexchange.session.ThreadCountEntry;
+import com.openexchange.sessionCount.SessionThreadCounterImpl;
+import com.openexchange.sessiond.SessiondService;
+
+
+/**
+ * {@link SessionThreadCountEventHandler}
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ */
+public final class SessionThreadCountEventHandler extends ServiceTracker<SessiondService, SessiondService> implements EventHandler {
+
+    private static final Log LOG = com.openexchange.log.Log.loggerFor(SessionThreadCountEventHandler.class);
+
+    private final SessionThreadCounterImpl counterImpl;
+
+    private final int threshold;
+    
+    /**
+     * Initializes a new {@link SessionThreadCountEventHandler}.
+     */
+    public SessionThreadCountEventHandler(final BundleContext context, final int threshold, final SessionThreadCounterImpl counterImpl) {
+        super(context, SessiondService.class, null);
+        this.counterImpl = counterImpl;
+        this.threshold = threshold;
+    }
+
+    @Override
+    public void handleEvent(final Event event) {
+        final Set<Thread> threads = ((ThreadCountEntry) event.getProperty(SessionThreadCounter.EVENT_PROP_ENTRY)).getThreads();
+        final int num = threads.size();
+        if (num < threshold) {
+            return;
+        }
+        final SessiondService sessiondService = getService();
+        if (null == sessiondService) {
+            return;
+        }
+        final String sessionId = (String) event.getProperty(SessionThreadCounter.EVENT_PROP_SESSION_ID);
+        final Session session = sessiondService.getSession(sessionId);
+        if (null == session) {
+            counterImpl.remove(sessionId);
+            return;
+        }
+        final StringBuilder info = new StringBuilder(1024);
+        info.append("Detected ").append(num).append(" threads belonging to session \"").append(sessionId);
+        info.append("\" (user=").append(session.getUserId()).append(", context=").append(session.getContextId()).append("):\n");
+
+        for (final Thread thread : threads) {
+            info.append("\n--------------------------------------------------------------------------\n");
+            appendStackTrace(thread.getStackTrace(), info);
+        }
+        LOG.warn(info.toString());
+    }
+
+    private static void appendStackTrace(final StackTraceElement[] trace, final StringBuilder sb) {
+        if (null == trace) {
+            sb.append("<missing stack trace>\n");
+            return;
+        }
+        for (final StackTraceElement ste : trace) {
+            final String className = ste.getClassName();
+            if (null != className) {
+                sb.append("\tat ").append(className).append('.').append(ste.getMethodName());
+                if (ste.isNativeMethod()) {
+                    sb.append("(Native Method)");
+                } else {
+                    final String fileName = ste.getFileName();
+                    if (null == fileName) {
+                        sb.append("(Unknown Source)");
+                    } else {
+                        final int lineNumber = ste.getLineNumber();
+                        sb.append('(').append(fileName);
+                        if (lineNumber >= 0) {
+                            sb.append(':').append(lineNumber);
+                        }
+                        sb.append(')');
+                    }
+                }
+                sb.append('\n');
+            }
+        }
+    }
+
+}
