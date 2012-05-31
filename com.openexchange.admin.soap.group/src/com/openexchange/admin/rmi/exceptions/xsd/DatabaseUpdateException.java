21ecec7c1934b0c1a34a8a151d3df63b1ef2e1fe
Thorben Betten <thorben.betten@open-xchange.com>
2012-05-31 15:13:51 +0200

    Group SOAP bundle

---
+++ com.openexchange.admin.soap.group/src/com/openexchange/admin/rmi/exceptions/xsd/DatabaseUpdateException.java
@@ -0,0 +1,35 @@
+
+package com.openexchange.admin.rmi.exceptions.xsd;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+import com.openexchange.admin.soap.group.Exception;
+
+
+/**
+ * <p>Java-Klasse für DatabaseUpdateException complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="DatabaseUpdateException">
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://soap.admin.openexchange.com}Exception">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "DatabaseUpdateException")
+public class DatabaseUpdateException
+    extends Exception
+{
+
+
+}
---
+++ com.openexchange.admin.soap.group/src/com/openexchange/admin/soap/group/ObjectFactory.java
@@ -0,0 +1,272 @@
+
+package com.openexchange.admin.soap.group;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.openexchange.admin.soap.group package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.openexchange.admin.soap.group
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link ListGroupsForUser }
+     * 
+     */
+    public ListGroupsForUser createListGroupsForUser() {
+        return new ListGroupsForUser();
+    }
+
+    /**
+     * Create an instance of {@link InvalidDataException }
+     * 
+     */
+    public InvalidDataException createInvalidDataException() {
+        return new InvalidDataException();
+    }
+
+    /**
+     * Create an instance of {@link Change }
+     * 
+     */
+    public Change createChange() {
+        return new Change();
+    }
+
+    /**
+     * Create an instance of {@link NoSuchContextException }
+     * 
+     */
+    public NoSuchContextException createNoSuchContextException() {
+        return new NoSuchContextException();
+    }
+
+    /**
+     * Create an instance of {@link ListAll }
+     * 
+     */
+    public ListAll createListAll() {
+        return new ListAll();
+    }
+
+    /**
+     * Create an instance of {@link GetMultipleData }
+     * 
+     */
+    public GetMultipleData createGetMultipleData() {
+        return new GetMultipleData();
+    }
+
+    /**
+     * Create an instance of {@link StorageException }
+     * 
+     */
+    public StorageException createStorageException() {
+        return new StorageException();
+    }
+
+    /**
+     * Create an instance of {@link GetDefaultGroupResponse }
+     * 
+     */
+    public GetDefaultGroupResponse createGetDefaultGroupResponse() {
+        return new GetDefaultGroupResponse();
+    }
+
+    /**
+     * Create an instance of {@link NoSuchUserException }
+     * 
+     */
+    public NoSuchUserException createNoSuchUserException() {
+        return new NoSuchUserException();
+    }
+
+    /**
+     * Create an instance of {@link NoSuchGroupException }
+     * 
+     */
+    public NoSuchGroupException createNoSuchGroupException() {
+        return new NoSuchGroupException();
+    }
+
+    /**
+     * Create an instance of {@link CreateResponse }
+     * 
+     */
+    public CreateResponse createCreateResponse() {
+        return new CreateResponse();
+    }
+
+    /**
+     * Create an instance of {@link Delete }
+     * 
+     */
+    public Delete createDelete() {
+        return new Delete();
+    }
+
+    /**
+     * Create an instance of {@link GetMembersResponse }
+     * 
+     */
+    public GetMembersResponse createGetMembersResponse() {
+        return new GetMembersResponse();
+    }
+
+    /**
+     * Create an instance of {@link RemoveMember }
+     * 
+     */
+    public RemoveMember createRemoveMember() {
+        return new RemoveMember();
+    }
+
+    /**
+     * Create an instance of {@link RemoteException }
+     * 
+     */
+    public RemoteException createRemoteException() {
+        return new RemoteException();
+    }
+
+    /**
+     * Create an instance of {@link GetMultipleDataResponse }
+     * 
+     */
+    public GetMultipleDataResponse createGetMultipleDataResponse() {
+        return new GetMultipleDataResponse();
+    }
+
+    /**
+     * Create an instance of {@link List }
+     * 
+     */
+    public List createList() {
+        return new List();
+    }
+
+    /**
+     * Create an instance of {@link Create }
+     * 
+     */
+    public Create createCreate() {
+        return new Create();
+    }
+
+    /**
+     * Create an instance of {@link GetDefaultGroup }
+     * 
+     */
+    public GetDefaultGroup createGetDefaultGroup() {
+        return new GetDefaultGroup();
+    }
+
+    /**
+     * Create an instance of {@link DeleteMultiple }
+     * 
+     */
+    public DeleteMultiple createDeleteMultiple() {
+        return new DeleteMultiple();
+    }
+
+    /**
+     * Create an instance of {@link ListAllResponse }
+     * 
+     */
+    public ListAllResponse createListAllResponse() {
+        return new ListAllResponse();
+    }
+
+    /**
+     * Create an instance of {@link GetMembers }
+     * 
+     */
+    public GetMembers createGetMembers() {
+        return new GetMembers();
+    }
+
+    /**
+     * Create an instance of {@link AddMember }
+     * 
+     */
+    public AddMember createAddMember() {
+        return new AddMember();
+    }
+
+    /**
+     * Create an instance of {@link ListGroupsForUserResponse }
+     * 
+     */
+    public ListGroupsForUserResponse createListGroupsForUserResponse() {
+        return new ListGroupsForUserResponse();
+    }
+
+    /**
+     * Create an instance of {@link InvalidCredentialsException }
+     * 
+     */
+    public InvalidCredentialsException createInvalidCredentialsException() {
+        return new InvalidCredentialsException();
+    }
+
+    /**
+     * Create an instance of {@link GetData }
+     * 
+     */
+    public GetData createGetData() {
+        return new GetData();
+    }
+
+    /**
+     * Create an instance of {@link ListResponse }
+     * 
+     */
+    public ListResponse createListResponse() {
+        return new ListResponse();
+    }
+
+    /**
+     * Create an instance of {@link DatabaseUpdateException }
+     * 
+     */
+    public DatabaseUpdateException createDatabaseUpdateException() {
+        return new DatabaseUpdateException();
+    }
+
+    /**
+     * Create an instance of {@link GetDataResponse }
+     * 
+     */
+    public GetDataResponse createGetDataResponse() {
+        return new GetDataResponse();
+    }
+
+    /**
+     * Create an instance of {@link Exception }
+     * 
+     */
+    public Exception createException() {
+        return new Exception();
+    }
+
+}
