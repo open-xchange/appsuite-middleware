a2f0698b285afb7d024263861c797906a26de420
Thorben Betten <thorben.betten@open-xchange.com>
2012-05-31 16:42:14 +0200

    SOAP Bundle for admin task management

---
+++ com.openexchange.admin.soap.taskmgmt/src/java/util/concurrent/xsd/ExecutionException.java
@@ -0,0 +1,35 @@
+
+package java.util.concurrent.xsd;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+import com.openexchange.admin.soap.taskmgmt.Exception;
+
+
+/**
+ * <p>Java-Klasse für ExecutionException complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="ExecutionException">
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://soap.admin.openexchange.com}Exception">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ExecutionException")
+public class ExecutionException
+    extends Exception
+{
+
+
+}
---
+++ com.openexchange.admin.soap.taskmgmt/src/java/util/concurrent/xsd/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://concurrent.util.java/xsd")
+package java.util.concurrent.xsd;
