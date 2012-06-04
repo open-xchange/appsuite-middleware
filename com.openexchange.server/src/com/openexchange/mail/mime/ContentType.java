a39207130e05f5b6ee49ced81e1dbac4c50c98e2
Thorben Betten <thorben.betten@open-xchange.com>
2012-06-04 19:09:35 +0200

    Fix for bug 22095: Resolved possible java.lang.StringIndexOutOfBoundsException & improved logging

--- com.openexchange.server/src/com/openexchange/mail/mime/ContentType.java
+++ com.openexchange.server/src/com/openexchange/mail/mime/ContentType.java
@@ -419,7 +419,11 @@ public class ContentType extends ParameterizedHeader {
                 }
                 baseType = new StringBuilder(16).append(primaryType).append(DELIMITER).append(subType).toString();
                 if (paramList) {
-                    parameterList = pos < cts.length() ? new ParameterList(cts.substring(pos + 1)) : new ParameterList();
+                    try {
+                        parameterList = pos < cts.length() ? new ParameterList(cts.substring(pos + 1)) : new ParameterList();
+                    } catch (final RuntimeException e) {
+                        throw MailExceptionCode.INVALID_CONTENT_TYPE.create(e, contentType);
+                    }
                 }
                 return;
             } catch (final OXException e) {
