d689847f0c3ccc4b00fdfcda3c4786de5dec24f9
Thorben Betten <thorben.betten@open-xchange.com>
2012-06-05 16:44:02 +0200

    minor

--- com.openexchange.server/src/com/openexchange/ajax/requesthandler/responseRenderers/APIResponseRenderer.java
+++ com.openexchange.server/src/com/openexchange/ajax/requesthandler/responseRenderers/APIResponseRenderer.java
@@ -55,8 +55,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.FileUploadBase;
-import org.apache.commons.fileupload.servlet.ServletRequestContext;
 import org.apache.commons.logging.Log;
 import org.json.JSONException;
 import com.openexchange.ajax.AJAXServlet;
@@ -70,7 +68,7 @@ import com.openexchange.tools.UnsynchronizedStringWriter;
 
 /**
  * {@link APIResponseRenderer}
- *
+ * 
  * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
  */
 public class APIResponseRenderer implements ResponseRenderer {
@@ -80,11 +78,11 @@ public class APIResponseRenderer implements ResponseRenderer {
      */
     private static final Log LOG = com.openexchange.exception.Log.valueOf(LogFactory.getLog(APIResponseRenderer.class));
 
-	private static final String JSONP = "jsonp";
+    private static final String JSONP = "jsonp";
 
-	private static final String CALLBACK = "callback";
+    private static final String CALLBACK = "callback";
 
-	private static final String PLAIN_JSON = "plainJson";
+    private static final String PLAIN_JSON = "plainJson";
 
     /**
      * Initializes a new {@link APIResponseRenderer}.
@@ -106,7 +104,12 @@ public class APIResponseRenderer implements ResponseRenderer {
     @Override
     public void write(final AJAXRequestData request, final AJAXRequestResult result, final HttpServletRequest req, final HttpServletResponse resp) {
         final Boolean plainJson = (Boolean) result.getParameter(PLAIN_JSON);
-        writeResponse((Response) result.getResultObject(), request.getAction(), req, resp, null == plainJson ? false : plainJson.booleanValue());
+        writeResponse(
+            (Response) result.getResultObject(),
+            request.getAction(),
+            req,
+            resp,
+            null == plainJson ? false : plainJson.booleanValue());
     }
 
     /**
@@ -118,7 +121,7 @@ public class APIResponseRenderer implements ResponseRenderer {
      * <li>The HTTP Servlet request has the <code>"respondWithHTML"</code> parameter set to <code>"true"</code></li>
      * <li>The HTTP Servlet request contains non-<code>null</code> <code>"callback"</code> parameter</li>
      * </ul>
-     *
+     * 
      * @param response The response to write
      * @param action The request's action
      * @param req The HTTP Servlet request
@@ -132,7 +135,7 @@ public class APIResponseRenderer implements ResponseRenderer {
         try {
             if (plainJson) {
                 ResponseWriter.write(response, resp.getWriter());
-            } else if (FileUploadBase.isMultipartContent(new ServletRequestContext(req)) || isRespondWithHTML(req) || req.getParameter(CALLBACK) != null) {
+            } else if (isMultipartContent(req) || isRespondWithHTML(req) || req.getParameter(CALLBACK) != null) {
                 resp.setContentType(AJAXServlet.CONTENTTYPE_HTML);
                 String callback = req.getParameter(CALLBACK);
                 if (callback == null) {
@@ -164,6 +167,28 @@ public class APIResponseRenderer implements ResponseRenderer {
         } catch (final IOException e) {
             LOG.error(e.getMessage(), e);
         }
+    }
+
+    /**
+     * Part of HTTP content type header.
+     */
+    private static final String MULTIPART = "multipart/";
+
+    /**
+     * Utility method that determines whether the request contains multipart content
+     * 
+     * @param request The request to be evaluated.
+     * @return <code>true</code> if the request is multipart; <code>false</code> otherwise.
+     */
+    private static final boolean isMultipartContent(final HttpServletRequest request) {
+        final String contentType = request.getContentType();
+        if (contentType == null) {
+            return false;
+        }
+        if (contentType.toLowerCase().startsWith(MULTIPART)) {
+            return true;
+        }
+        return false;
     }
 
     private static boolean isRespondWithHTML(final HttpServletRequest req) {
