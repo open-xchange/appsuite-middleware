25b78a17f8657c79e5e85852c56a705b61f6aea7
Tobias <tobias.friedrich@open-xchange.com>
2012-06-12 15:32:34 +0200

    using user timezone when parsing iCal

--- com.openexchange.caldav/src/com/openexchange/caldav/CaldavResource.java
+++ com.openexchange.caldav/src/com/openexchange/caldav/CaldavResource.java
@@ -78,7 +78,6 @@ import com.openexchange.api2.AppointmentSQLInterface;
 import com.openexchange.data.conversion.ical.ConversionError;
 import com.openexchange.data.conversion.ical.ConversionWarning;
 import com.openexchange.data.conversion.ical.ICalEmitter;
-import com.openexchange.data.conversion.ical.ICalParser;
 import com.openexchange.data.conversion.ical.ICalSession;
 import com.openexchange.exception.Category;
 import com.openexchange.exception.OXException;
@@ -142,8 +141,6 @@ public class CaldavResource extends AbstractResource {
 
     private static final Log LOG = LogFactory.getLog(CaldavResource.class);
 
-    private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
-    
     private final GroupwareCaldavFactory factory;
 
     private Appointment appointment;
@@ -826,17 +823,17 @@ public class CaldavResource extends AbstractResource {
         }
     }
 
-    private List<CalendarDataObject> parse(final String iCal) throws ConversionError {
+    private List<CalendarDataObject> parse(String iCal) throws ConversionError {
     	/*
     	 * apply patches
     	 */
-    	final String patchedICal = Patches.Incoming.applyAll(iCal);    	
+    	String patchedICal = Patches.Incoming.applyAll(iCal);    	
     	/*
     	 * parse appointments
     	 */
-        final ICalParser parser = factory.getIcalParser();
-        return parser.parseAppointments(
-        		patchedICal, UTC, factory.getContext(), new ArrayList<ConversionError>(), new ArrayList<ConversionWarning>());
+        TimeZone timeZone = TimeZone.getTimeZone(this.factory.getUser().getTimeZone());
+        return factory.getIcalParser().parseAppointments(
+        		patchedICal, timeZone, factory.getContext(), new ArrayList<ConversionError>(), new ArrayList<ConversionWarning>());
     }
     
 
