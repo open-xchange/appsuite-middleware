7858f9ec92ef3d3550a9bf30edd6a3c692adefea
Thorben Betten <thorben.betten@open-xchange.com>
2012-07-25 10:47:59 +0200

    utilities

--- com.openexchange.ajp/src/com/openexchange/ajp13/servlet/http/HttpDateFormatRegistry.java
+++ com.openexchange.ajp/src/com/openexchange/ajp13/servlet/http/HttpDateFormatRegistry.java
@@ -55,6 +55,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import com.openexchange.ajp13.AJPv13ServiceRegistry;
 import com.openexchange.config.ConfigurationService;
 import com.openexchange.tools.TimeZoneUtils;
@@ -100,15 +103,21 @@ public final class HttpDateFormatRegistry {
         return singleton;
     }
 
+    private final ConcurrentMap<String, BrowserDetector> detectors;
+
     private final DateFormat defaultDateFormat;
 
     private final DateFormat netscapeDateFormat;
+
+    private final DateFormat msieDateFormat;
 
     /**
      * Initializes a new {@link HttpDateFormatRegistry}.
      */
     private HttpDateFormatRegistry() {
         super();
+        detectors = new ConcurrentHashMap<String, BrowserDetector>(16);
+        final TimeZone gmtTimeZone = TimeZoneUtils.getTimeZone("GMT");
         {
             final SimpleDateFormat headerDateFormat = new SimpleDateFormat("EEE',' dd MMMM yyyy HH:mm:ss z", Locale.ENGLISH);
             final DateFormatSymbols dfs = headerDateFormat.getDateFormatSymbols();
@@ -136,14 +145,46 @@ public final class HttpDateFormatRegistry {
             shortMonths[Calendar.DECEMBER] = "Dec";
             dfs.setShortMonths(shortMonths);
             headerDateFormat.setDateFormatSymbols(dfs);
-            headerDateFormat.setTimeZone(TimeZoneUtils.getTimeZone("GMT"));
+            headerDateFormat.setTimeZone(gmtTimeZone);
             defaultDateFormat = headerDateFormat;
         }
         /*
          * Taken from org.apache.commons.httpclient.cookie.NetscapeDraftSpec
          */
-        netscapeDateFormat = new SimpleDateFormat("EEE, dd-MMM-yyyy HH:mm:ss z", Locale.US);
-        netscapeDateFormat.setTimeZone(TimeZoneUtils.getTimeZone("GMT"));
+        final ConfigurationService service = AJPv13ServiceRegistry.getInstance().getService(ConfigurationService.class);
+        final class Helper {
+            String getPattern(final String name, final String defaultPattern) {
+                if (null == service) {
+                    return defaultPattern;
+                }
+                String property = service.getProperty(name, defaultPattern);
+                if (property.charAt(0) == '"') {
+                    property = property.substring(1, property.length() - 1);
+                }
+                return property;
+            }
+        }
+        final Helper helper = new Helper();
+        netscapeDateFormat = new SimpleDateFormat(helper.getPattern("com.openexchange.cookie.expires.netscapePattern", "EEE, dd-MMM-yyyy HH:mm:ss z"), Locale.US);
+        netscapeDateFormat.setTimeZone(gmtTimeZone);
+
+        msieDateFormat = new SimpleDateFormat(helper.getPattern("com.openexchange.cookie.expires.msie8Pattern", "EEE, dd MMM yyyy HH:mm:ss zzz"), Locale.US);
+        msieDateFormat.setTimeZone(gmtTimeZone);
+    }
+
+    private BrowserDetector getDetector(final String userAgent) {
+        if (null == userAgent) {
+            return null;
+        }
+        BrowserDetector browserDetector = detectors.get(userAgent);
+        if (null == browserDetector) {
+            final BrowserDetector newBrowserDetector = new BrowserDetector(userAgent);
+            browserDetector = detectors.putIfAbsent(userAgent, newBrowserDetector);
+            if (null == browserDetector) {
+                browserDetector = newBrowserDetector;
+            }
+        }
+        return browserDetector;
     }
 
     /**
@@ -174,20 +215,26 @@ public final class HttpDateFormatRegistry {
     public void appendCookieMaxAge(final int maxAgeSecs, final String userAgent, final StringBuilder composer) {
         if (null == userAgent) {
             appendNetscapeCookieMaxAge(maxAgeSecs, composer);
+            return;
         }
         /*
-         * TODO: Invoke appendNetscapeCookieMaxAge() or appendRFC2109CookieMaxAge() dependent on user agent
+         * Invoke dependent on user agent
          */
+        final BrowserDetector detector = getDetector(userAgent);
+        if (detector.isMSIE() && detector.getBrowserVersion() <= 8.0F) {
+            appendMSIE8CookieMaxAge(maxAgeSecs, composer);
+            return;
+        }
         appendNetscapeCookieMaxAge(maxAgeSecs, composer);
     }
 
-    private volatile String zeroMaxAgeExpires;
+    private volatile String netscapeZeroMaxAgeExpires;
 
-    private String zeroMaxAgeExpires() {
-        String tmp = zeroMaxAgeExpires;
+    private String netscapeZeroMaxAgeExpires() {
+        String tmp = netscapeZeroMaxAgeExpires;
         if (null == tmp) {
             synchronized (netscapeDateFormat) {
-                tmp = zeroMaxAgeExpires;
+                tmp = netscapeZeroMaxAgeExpires;
                 if (null == tmp) {
                     final ConfigurationService service = AJPv13ServiceRegistry.getInstance().getService(ConfigurationService.class);
                     final boolean b = service.getBoolProperty("com.openexchange.ajp.cookie.enableExactZeroMaxAge", true);
@@ -195,7 +242,7 @@ public final class HttpDateFormatRegistry {
                      * expires=Sat, 01-Jan-2000 00:00:00 GMT
                      */
                     tmp = netscapeDateFormat.format(new Date(b ? 0L : 10000L));
-                    zeroMaxAgeExpires = tmp;
+                    netscapeZeroMaxAgeExpires = tmp;
                 }
             }
         }
@@ -210,7 +257,7 @@ public final class HttpDateFormatRegistry {
      */
     private void appendNetscapeCookieMaxAge(final int maxAgeSecs, final StringBuilder composer) {
         if (maxAgeSecs == 0) {
-            composer.append("; expires=").append(zeroMaxAgeExpires());
+            composer.append("; expires=").append(netscapeZeroMaxAgeExpires());
         } else {
             synchronized (netscapeDateFormat) {
                 /*
@@ -218,6 +265,47 @@ public final class HttpDateFormatRegistry {
                  */
                 final long millis = System.currentTimeMillis() + (maxAgeSecs * 1000L);
                 composer.append("; expires=").append(netscapeDateFormat.format(new Date(millis)));
+            }
+        }
+    }
+
+    private volatile String msie8ZeroMaxAgeExpires;
+
+    private String msie8ZeroMaxAgeExpires() {
+        String tmp = msie8ZeroMaxAgeExpires;
+        if (null == tmp) {
+            synchronized (msieDateFormat) {
+                tmp = msie8ZeroMaxAgeExpires;
+                if (null == tmp) {
+                    final ConfigurationService service = AJPv13ServiceRegistry.getInstance().getService(ConfigurationService.class);
+                    final boolean b = service.getBoolProperty("com.openexchange.ajp.cookie.enableExactZeroMaxAge", true);
+                    /*
+                     * expires=Sat, 01-Jan-2000 00:00:00 GMT
+                     */
+                    tmp = netscapeDateFormat.format(new Date(b ? 0L : 10000L));
+                    msie8ZeroMaxAgeExpires = tmp;
+                }
+            }
+        }
+        return tmp;
+    }
+
+    /**
+     * Appends expiry according to MSIE <= v8.0 specification; e.g. <code>"expires=Thu, 26 Apr 2012 18:35:06 GMT"</code>.
+     *
+     * @param maxAgeSecs The max-age seconds
+     * @param composer The composing string builder
+     */
+    private void appendMSIE8CookieMaxAge(final int maxAgeSecs, final StringBuilder composer) {
+        if (maxAgeSecs == 0) {
+            composer.append("; expires=").append(msie8ZeroMaxAgeExpires());
+        } else {
+            synchronized (msieDateFormat) {
+                /*
+                 * expires=Sat, 01-Jan-2000 00:00:00 GMT
+                 */
+                final long millis = System.currentTimeMillis() + (maxAgeSecs * 1000L);
+                composer.append("; expires=").append(msieDateFormat.format(new Date(millis)));
             }
         }
     }
