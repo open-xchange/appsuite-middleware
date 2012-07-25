5fc392a91daca2dcf38723bb44544fb5ca4ebaa4
Thorben Betten <thorben.betten@open-xchange.com>
2012-07-25 13:53:34 +0200

    smadd

--- com.openexchange.ajp/src/com/openexchange/ajp13/coyote/AjpProcessor.java
+++ com.openexchange.ajp/src/com/openexchange/ajp13/coyote/AjpProcessor.java
@@ -1668,6 +1668,10 @@ public final class AjpProcessor implements com.openexchange.ajp13.watcher.Task {
                                 LOG.debug(new StringBuilder("\n\tDifferent JVM route detected. Removing JSESSIONID cookie: ").append(id));
                             }
                             current.setPath("/");
+                            final String domain = getDomainValue(serverName, prefixWithDot());
+                            if (null != domain) {
+                                current.setDomain(domain);
+                            }
                             current.setMaxAge(0); // delete
                             current.setSecure(forceHttps || request.isSecure());
                             response.addCookie(current);
@@ -1684,6 +1688,10 @@ public final class AjpProcessor implements com.openexchange.ajp13.watcher.Task {
                                 LOG.debug(new StringBuilder("\n\tExpired or invalid cookie -> Removing JSESSIONID cookie: ").append(current.getValue()));
                             }
                             current.setPath("/");
+                            final String domain = getDomainValue(serverName, prefixWithDot());
+                            if (null != domain) {
+                                current.setDomain(domain);
+                            }
                             current.setMaxAge(0); // delete
                             current.setSecure(forceHttps || request.isSecure());
                             response.addCookie(current);
@@ -1706,6 +1714,10 @@ public final class AjpProcessor implements com.openexchange.ajp13.watcher.Task {
                                 LOG.debug(new StringBuilder("\n\tMissing JVM route in JESSIONID cookie").append(current.getValue()));
                             }
                             current.setPath("/");
+                            final String domain = getDomainValue(serverName, prefixWithDot());
+                            if (null != domain) {
+                                current.setDomain(domain);
+                            }
                             current.setMaxAge(0); // delete
                             current.setSecure(forceHttps || request.isSecure());
                             response.addCookie(current);
@@ -1722,6 +1734,10 @@ public final class AjpProcessor implements com.openexchange.ajp13.watcher.Task {
                                 LOG.debug(new StringBuilder("\n\tExpired or invalid cookie -> Removing JSESSIONID cookie: ").append(current.getValue()));
                             }
                             current.setPath("/");
+                            final String domain = getDomainValue(serverName, prefixWithDot());
+                            if (null != domain) {
+                                current.setDomain(domain);
+                            }
                             current.setMaxAge(0); // delete
                             current.setSecure(forceHttps || request.isSecure());
                             response.addCookie(current);
