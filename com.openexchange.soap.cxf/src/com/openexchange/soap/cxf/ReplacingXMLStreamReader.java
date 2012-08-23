5d3cc647e00b9727d68089c9c062b9f426fc5af5
Marcus Klein <marcus.klein@open-xchange.com>
2012-08-23 17:45:46 +0200

    removed failed tries

--- com.openexchange.soap.cxf/src/com/openexchange/soap/cxf/ReplacingXMLStreamReader.java
+++ com.openexchange.soap.cxf/src/com/openexchange/soap/cxf/ReplacingXMLStreamReader.java
@@ -49,333 +49,104 @@
 
 package com.openexchange.soap.cxf;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
 import java.util.Stack;
 import javax.xml.XMLConstants;
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import org.apache.cxf.message.Exchange;
+import javax.xml.stream.util.StreamReaderDelegate;
 import org.apache.cxf.service.model.BindingOperationInfo;
-import org.apache.cxf.service.model.MessagePartInfo;
-import org.apache.cxf.service.model.ServiceModelUtil;
-import org.apache.cxf.staxutils.DepthXMLStreamReader;
-import org.apache.ws.commons.schema.XmlSchemaAnnotated;
 import org.apache.ws.commons.schema.XmlSchemaComplexType;
 import org.apache.ws.commons.schema.XmlSchemaElement;
 import org.apache.ws.commons.schema.XmlSchemaSequence;
+import org.apache.ws.commons.schema.XmlSchemaSequenceMember;
 
-final class ReplacingXMLStreamReader extends DepthXMLStreamReader {
+/**
+ * {@link ReplacingXMLStreamReader}
+ *
+ * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
+ */
+public class ReplacingXMLStreamReader extends StreamReaderDelegate {
 
-    private final Exchange exchange;
-    private final Stack<List<ParsingEvent>> pushedAheadEvents;
-    private final Stack<ParsingEvent> pushedBackEvents;
-    private boolean replacing = false;
-    private ParsingEvent currentEvent;
-    private QName lastNonGeneric;
-    private int genericAttributePos;
-    private final NamespaceContext namespaceContext;
-    private final List<Integer> attributesIndexes;
-    private boolean attributesIndexed;
+    private final Stack<ReplacingElement> stack = new Stack<ReplacingElement>();
+    private ReplacingElement current;
 
-    protected ReplacingXMLStreamReader(Exchange exchange, final XMLStreamReader reader) {
+    public ReplacingXMLStreamReader(BindingOperationInfo bop, XMLStreamReader reader) {
         super(reader);
-        this.exchange = exchange;
-        pushedAheadEvents = new Stack<List<ParsingEvent>>();
-        pushedBackEvents = new Stack<ParsingEvent>();
-        attributesIndexes = new ArrayList<Integer>();
-        namespaceContext = reader.getNamespaceContext();
-    }
-
-    @Override
-    public XMLStreamReader getReader() {
-        return this;
+        QName name = super.getName();
+        ReplacingElement method = new ReplacingElement(name, name);
+        stack.push(method);
+        method.setXmlSchema((XmlSchemaElement) bop.getOperationInfo().getInput().getMessagePart(0).getXmlSchema());
+        current = null;
     }
 
     @Override
     public int next() throws XMLStreamException {
-        if (!pushedBackEvents.empty()) {
-            // consume events from the pushed back stack
-            currentEvent = pushedBackEvents.pop();
-            return currentEvent.getEvent();
-        }
         int event = super.next();
         if (XMLStreamConstants.START_ELEMENT == event) {
-            attributesIndexed = false;
-            final QName theName = super.getName();
-            if (isGeneric(theName)) {
-                replacing = true;
-                genericAttributePos++;
-                QName expected = getExpected(lastNonGeneric);
-                String prefix = theName.getPrefix();
-                if (isEmpty(prefix) && isEmpty(theName.getNamespaceURI()) && !isEmpty(expected.getNamespaceURI())) {
-                    // prefix = namespaceContext.getPrefix(expected.getNamespaceURI());
-                    // if (prefix == null) {
-                    // prefix = namespaceContext.findUniquePrefix(expected.getNamespaceURI());
-                    // }
-                    prefix = "";
-                } else if (prefix.length() > 0 && expected.getNamespaceURI().length() == 0) {
-                    prefix = "";
-                }
-                expected = new QName(expected.getNamespaceURI(), expected.getLocalPart(), prefix);
-                if (isEmptyQName(expected)) {
-                    // skip the current element (deep drop)
-                    final int depth = getDepth();
-                    while (depth != getDepth() || super.next() != XMLStreamConstants.END_ELEMENT) {
-                        // get to the matching end element event
-                    }
-                    event = next();
-                } else {
-                    currentEvent = createStartElementEvent(expected);
-                    if (theName.equals(expected)) {
-                        pushedAheadEvents.push(null);
-                    } else {
-                        pushedAheadEvents.push(Collections.singletonList(createEndElementEvent(expected)));
-                    }
-                }
-            } else if (replacing && isEmptyURI(theName)) {
-                // TODO
-                QName expected = getExpected(theName);
-                currentEvent = createStartElementEvent(expected);
-                pushedAheadEvents.push(Collections.singletonList(createEndElementEvent(expected)));
+            final QName name = super.getName();
+            ReplacingElement element = new ReplacingElement(name);
+            stack.push(element);
+            if (isGeneric(name)) {
+                QName expected = getExpected(stack.get(stack.size() - 2));
+                element.setExpected(expected);
+                element.setXmlSchema(getChildSchema(stack.get(stack.size() - 2), expected));
+                current = element;
+            } else if (isEmptyURI(name)) {
+                XmlSchemaElement schema = getChildSchema(stack.get(stack.size() - 2), name);
+                QName expected = schema.getQName();
+                element.setExpected(expected);
+                element.setXmlSchema(schema);
+                current = element;
             } else {
-                currentEvent = null;
-                lastNonGeneric = theName;
-                genericAttributePos = 0;
+                element.setExpected(name);
+                current = null;
             }
-        } else if (XMLStreamConstants.END_ELEMENT == event) {
-            final QName theName = super.getName();
-
-            final List<ParsingEvent> pe = pushedAheadEvents.pop();
-            if (null != pe) {
-                pushedBackEvents.addAll(pe);
-                currentEvent = pushedBackEvents.pop();
-                event = currentEvent.getEvent();
-            }
+        } else if (XMLStreamConstants.END_ELEMENT == event && !stack.empty()) {
+            current = stack.pop();
         } else {
-            currentEvent = null;
+            current = null;
         }
         return event;
     }
 
-    private QName getExpected(final QName name) {
-        final BindingOperationInfo bop;
-        if (null == name) {
-            bop = exchange.getBindingOperationInfo();
-        } else {
-            // TODO is this ever used?
-            bop = ServiceModelUtil.getOperation(exchange, name);
-        }
-        List<MessagePartInfo> parts = bop.getOperationInfo().getInput().getMessageParts();
-        for (MessagePartInfo part : parts) {
-            XmlSchemaAnnotated schema = part.getXmlSchema();
-            if (schema instanceof XmlSchemaElement && ((XmlSchemaElement) schema).getSchemaType() instanceof XmlSchemaComplexType) {
-                XmlSchemaComplexType cplxType = (XmlSchemaComplexType) ((XmlSchemaElement) schema).getSchemaType();
-                XmlSchemaSequence seq = (XmlSchemaSequence) cplxType.getParticle();
-                XmlSchemaElement child = (XmlSchemaElement) seq.getItems().get(genericAttributePos - 1);
-                return child.getQName();   
-            }
-            throw new UnsupportedOperationException("Don't know how to handle part " + part.toString());
-        }
-        throw new UnsupportedOperationException("Can not find parts for " + name);
-    }
-
     @Override
     public String getLocalName() {
-        if (currentEvent != null) {
-            return currentEvent.getName().getLocalPart();
-        } else {
-            return super.getLocalName();
-        }
-    }
-
-    @Override
-    public String getPrefix() {
-        final QName name = readCurrentElement();
-        String prefix = name.getPrefix();
-        if (prefix.length() == 0 && getNamespaceURI().length() > 0) {
-            prefix = getNamespaceContext().getPrefix(getNamespaceURI());
-            if (prefix == null) {
-                prefix = "";
-            }
-        }
-        return prefix;
+        return null == current ? super.getLocalName() : current.getExpected().getLocalPart();
     }
 
     @Override
     public String getNamespaceURI() {
-        if (currentEvent != null) {
-            return currentEvent.getName().getNamespaceURI();
-        } else {
-            return super.getNamespaceURI();
-        }
+        return null == current ? super.getNamespaceURI() : current.getExpected().getNamespaceURI();
     }
 
-    private QName readCurrentElement() {
-        if (currentEvent != null) {
-            return currentEvent.getName();
+    private static QName getExpected(ReplacingElement parent) throws XMLStreamException {
+        XmlSchemaElement schema = parent.getXmlSchema();
+        if (schema.getSchemaType() instanceof XmlSchemaComplexType) {
+            XmlSchemaComplexType cplxType = (XmlSchemaComplexType) schema.getSchemaType();
+            XmlSchemaSequence seq = (XmlSchemaSequence) cplxType.getParticle();
+            XmlSchemaElement child = (XmlSchemaElement) seq.getItems().get(parent.nextChildPosition());
+            return child.getQName();
         }
-        final String ns = super.getNamespaceURI();
-        final String name = super.getLocalName();
-        final String prefix = super.getPrefix();
-        return new QName(ns, name, prefix == null ? "" : prefix);
+        throw new XMLStreamException("Complex type expected.");
     }
 
-    @Override
-    public QName getName() {
-        return new QName(getNamespaceURI(), getLocalName());
-    }
-
-    @Override
-    public int getAttributeCount() {
-        if (!pushedBackEvents.empty()) {
-            return 0;
-        }
-        checkAttributeIndexRange(-1);
-        return attributesIndexes.size();
-    }
-
-    @Override
-    public String getAttributeLocalName(final int index) {
-        if (!pushedBackEvents.empty()) {
-            throwIndexException(index, 0);
-        }
-        checkAttributeIndexRange(index);
-
-        return getAttributeName(index).getLocalPart();
-    }
-
-    @Override
-    public QName getAttributeName(final int index) {
-        if (!pushedBackEvents.empty()) {
-            throwIndexException(index, 0);
-        }
-        checkAttributeIndexRange(index);
-        final QName aname = super.getAttributeName(attributesIndexes.get(index).intValue());
-        return aname;
-    }
-
-    @Override
-    public String getAttributeNamespace(final int index) {
-        if (!pushedBackEvents.empty()) {
-            throwIndexException(index, 0);
-        }
-        checkAttributeIndexRange(index);
-
-        return getAttributeName(index).getNamespaceURI();
-    }
-
-    @Override
-    public String getAttributePrefix(final int index) {
-        if (!pushedBackEvents.empty()) {
-            throwIndexException(index, 0);
-        }
-        checkAttributeIndexRange(index);
-
-        final QName aname = getAttributeName(index);
-        if (XMLConstants.NULL_NS_URI.equals(aname.getNamespaceURI())) {
-            return "";
-        } else {
-            return namespaceContext.getPrefix(aname.getNamespaceURI());
-        }
-    }
-
-    @Override
-    public String getAttributeType(final int index) {
-        if (!pushedBackEvents.empty()) {
-            throwIndexException(index, 0);
-        }
-        checkAttributeIndexRange(index);
-        return super.getAttributeType(attributesIndexes.get(index).intValue());
-    }
-
-    @Override
-    public String getAttributeValue(final int index) {
-        if (!pushedBackEvents.empty()) {
-            throwIndexException(index, 0);
-        }
-        checkAttributeIndexRange(index);
-        return super.getAttributeValue(attributesIndexes.get(index).intValue());
-    }
-
-    @Override
-    public String getAttributeValue(final String namespace, final String localName) {
-        if (!pushedBackEvents.empty()) {
-            return null;
-        }
-        checkAttributeIndexRange(-1);
-        // TODO need reverse lookup
-        return super.getAttributeValue(namespace, localName);
-    }
-
-    @Override
-    public String getText() {
-        if (currentEvent != null) {
-            return currentEvent.getValue();
-        }
-        return super.getText();
-    }
-
-    @Override
-    public char[] getTextCharacters() {
-        if (currentEvent != null && currentEvent != null) {
-            return currentEvent.getValue().toCharArray();
-        }
-        return super.getTextCharacters();
-    }
-
-    @Override
-    public int getTextCharacters(final int sourceStart, final char[] target, final int targetStart, final int length) throws XMLStreamException {
-        if (currentEvent != null && currentEvent != null) {
-            int len = currentEvent.getValue().length() - sourceStart;
-            if (len > length) {
-                len = length;
+    private static XmlSchemaElement getChildSchema(ReplacingElement parent, QName name) {
+        XmlSchemaElement schema = parent.getXmlSchema();
+        if (schema.getSchemaType() instanceof XmlSchemaComplexType) {
+            XmlSchemaComplexType cplxType = (XmlSchemaComplexType) schema.getSchemaType();
+            XmlSchemaSequence seq = (XmlSchemaSequence) cplxType.getParticle();
+            for (XmlSchemaSequenceMember member : seq.getItems()) {
+                XmlSchemaElement element = (XmlSchemaElement) member;
+                if (element.getName().equals(name.getLocalPart())) {
+                    return element;
+                }
             }
-            currentEvent.getValue().getChars(sourceStart, sourceStart + len, target, targetStart);
-            return len;
         }
-
-        return super.getTextCharacters(sourceStart, target, targetStart, length);
+        return null;
     }
-
-    @Override
-    public int getTextLength() {
-        if (currentEvent != null && currentEvent.getValue() != null) {
-            return currentEvent.getValue().length();
-        }
-        return super.getTextLength();
-    }
-
-    /**
-     * Checks the index range for the current attributes set. If the attributes are not indexed for the current element context, they
-     * will be indexed.
-     * 
-     * @param index
-     */
-    private void checkAttributeIndexRange(final int index) {
-        if (!attributesIndexed) {
-            attributesIndexes.clear();
-            final int count = super.getAttributeCount();
-            for (int i = 0; i < count; i++) {
-                attributesIndexes.add(Integer.valueOf(i));
-            }
-            attributesIndexed = true;
-        }
-        if (index >= attributesIndexes.size()) {
-            throwIndexException(index, attributesIndexes.size());
-        }
-    }
-
-    private void throwIndexException(final int index, final int size) {
-        throw new IllegalArgumentException("Invalid index " + index + "; current element has only " + size + " attributes");
-    }
-
-    // private static final Pattern PATTERN_GENERIC = Pattern.compile(Pattern.quote("c-gensym") + "[0-9]+");
 
     private static boolean isGeneric(final QName name) {
         final String localPart = name.getLocalPart();
@@ -384,34 +155,5 @@ final class ReplacingXMLStreamReader extends DepthXMLStreamReader {
 
     private static boolean isEmptyURI(QName name) {
         return XMLConstants.NULL_NS_URI.equals(name.getNamespaceURI());
-    }
-
-    private static boolean isEmptyQName(final QName qname) {
-        return XMLConstants.NULL_NS_URI.equals(qname.getNamespaceURI()) && "".equals(qname.getLocalPart());
-    }
-
-    private static ParsingEvent createStartElementEvent(final QName name) {
-        return new ParsingEvent(XMLStreamConstants.START_ELEMENT, name, null);
-    }
-
-    private static ParsingEvent createEndElementEvent(final QName name) {
-        return new ParsingEvent(XMLStreamConstants.END_ELEMENT, name, null);
-    }
-
-    private static boolean isEmpty(final String string) {
-        if (null == string) {
-            return true;
-        }
-        final int len = string.length();
-        boolean isWhitespace = true;
-        for (int i = 0; isWhitespace && i < len; i++) {
-            isWhitespace = Character.isWhitespace(string.charAt(i));
-        }
-        return isWhitespace;
-    }
-
-    @Override
-    public String toString() {
-        return ReplacingXMLStreamReader.class.getName();
     }
 }
