23617ee339ce00fbb8879eb10733f7c96d20265e
Thorben Betten <thorben.betten@open-xchange.com>
2012-08-27 09:00:14 +0200

    JavaDoc

--- com.openexchange.file.storage.cmis/src/com/openexchange/file/storage/cmis/http/ResourceReleasingInputStream.java
+++ com.openexchange.file.storage.cmis/src/com/openexchange/file/storage/cmis/http/ResourceReleasingInputStream.java
@@ -1,3 +1,52 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
 package com.openexchange.file.storage.cmis.http;
 
 import java.io.BufferedInputStream;
@@ -7,6 +56,11 @@ import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.DefaultHttpClient;
 import com.openexchange.java.Streams;
 
+/**
+ * {@link ResourceReleasingInputStream}
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ */
 public final class ResourceReleasingInputStream extends BufferedInputStream {
 
     private static final int defaultBufferSize = 8192;
@@ -15,12 +69,27 @@ public final class ResourceReleasingInputStream extends BufferedInputStream {
 
     private final HttpRequestBase httpRequest;
 
+    /**
+     * Initializes a new {@link ResourceReleasingInputStream}.
+     * 
+     * @param in The input stream
+     * @param httpRequest The associated HTTP request
+     * @param httpClient The associated HTTP client
+     */
     public ResourceReleasingInputStream(InputStream in, HttpRequestBase httpRequest, DefaultHttpClient httpClient) {
         this(in, defaultBufferSize, httpRequest, httpClient);
     }
 
+    /**
+     * Initializes a new {@link ResourceReleasingInputStream}.
+     * 
+     * @param in The input stream
+     * @param size The initial buffer size (greater than zero)
+     * @param httpRequest The associated HTTP request
+     * @param httpClient The associated HTTP client
+     */
     public ResourceReleasingInputStream(InputStream in, final int size, HttpRequestBase httpRequest, DefaultHttpClient httpClient) {
-        super(in, size);
+        super(toBufferedInputStream(in, size), 1);
         this.httpRequest = httpRequest;
         this.httpClient = httpClient;
     }
