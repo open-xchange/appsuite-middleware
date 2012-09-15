359f3b4f604b63932cd9d085186f1df895b677af
Thorben Betten <thorben.betten@open-xchange.com>
2012-09-15 19:33:37 +0200

    Fix for bug 22947: Added null guard

--- com.openexchange.soap.cxf/src/com/openexchange/soap/cxf/ReplacingXMLStreamReader.java
+++ com.openexchange.soap.cxf/src/com/openexchange/soap/cxf/ReplacingXMLStreamReader.java
@@ -130,7 +130,7 @@ public class ReplacingXMLStreamReader extends StreamReaderDelegate {
 
     private static XmlSchemaElement getChildSchema(ReplacingElement parent, String name) {
         XmlSchemaElement schema = parent.getXmlSchema();
-        if (schema.getSchemaType() instanceof XmlSchemaComplexType) {
+        if ((null != schema) && (schema.getSchemaType() instanceof XmlSchemaComplexType)) {
             XmlSchemaComplexType cplxType = (XmlSchemaComplexType) schema.getSchemaType();
             XmlSchemaSequence seq = (XmlSchemaSequence) cplxType.getParticle();
             int rememberPosition = parent.nextChildPosition();
