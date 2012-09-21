1adc1c6e5dbb5bcfd0efdd61e33c09850134836d
Thorben Betten <thorben.betten@open-xchange.com>
2012-09-21 16:05:50 +0200

    log fomat

--- com.openexchange.threadpool/src/com/openexchange/log/internal/LoggerTask.java
+++ com.openexchange.threadpool/src/com/openexchange/log/internal/LoggerTask.java
@@ -55,6 +55,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Pattern;
 import org.apache.commons.logging.Log;
 import com.openexchange.log.LogPropertyName;
 import com.openexchange.log.Loggable;
@@ -244,6 +245,8 @@ final class LoggerTask extends AbstractTask<Object> {
         return null;
     }
 
+    private static final Pattern CRLF = Pattern.compile("\r?\n");
+
     private static String prependLocation(final String message, final Loggable loggable, final LogPropertyName.LogLevel logLevel) {
         final StringBuilder sb = new StringBuilder(64);
         final StackTraceElement[] trace = loggable.getCallerTrace();
@@ -268,14 +271,14 @@ final class LoggerTask extends AbstractTask<Object> {
                             sb.append(')');
                         }
                     }
-                    sb.append('\n');
+                    sb.append('\n').append(' ');
                     logClass = className;
                     break;
                 }
             }
         }
         if (null != message) {
-            sb.append(message);
+            sb.append(CRLF.matcher(message).replaceAll("$0 "));
         }
         return sb.toString();
     }
