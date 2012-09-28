0644424dd384a9e0d226c884c71081ef8f741157
Thorben Betten <thorben.betten@open-xchange.com>
2012-09-28 12:45:45 +0200

    static discovery bundle

--- com.openexchange.cluster.discovery.static/src/com/openexchange/cluster/discovery/sstatic/osgi/StaticClusterDiscoveryActivator.java
+++ com.openexchange.cluster.discovery.static/src/com/openexchange/cluster/discovery/sstatic/osgi/StaticClusterDiscoveryActivator.java
@@ -47,46 +47,73 @@
  *
  */
 
-package com.openexchange.cluster.discovery.empty.osgi;
+package com.openexchange.cluster.discovery.sstatic.osgi;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import com.openexchange.cluster.discovery.ClusterDiscoveryService;
 import com.openexchange.cluster.discovery.ClusterListener;
+import com.openexchange.config.ConfigurationService;
 import com.openexchange.osgi.HousekeepingActivator;
 
 /**
- * {@link EmptyClusterDiscoveryActivator}
+ * {@link StaticClusterDiscoveryActivator}
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
-public class EmptyClusterDiscoveryActivator extends HousekeepingActivator {
+public class StaticClusterDiscoveryActivator extends HousekeepingActivator {
 
     @Override
     protected Class<?>[] getNeededServices() {
-        return null;
+        return new Class<?>[] { ConfigurationService.class };
     }
 
     @Override
     protected void startBundle() throws Exception {
+        final ConfigurationService service = getService(ConfigurationService.class);
+        final String snodes = service.getProperty("com.openexchange.cluster.discovery.static.nodes", "");
+        final List<InetAddress> nodes;
+        if (isEmpty(snodes)) {
+            nodes = Collections.emptyList();
+        } else {
+            final String[] strings = snodes.split(" *, *");
+            final List<InetAddress> list = new ArrayList<InetAddress>(strings.length);
+            for (final String snode : strings) {
+                list.add(InetAddress.getByName(snode));
+            }
+            nodes = Collections.unmodifiableList(list);
+        }
         registerService(ClusterDiscoveryService.class, new ClusterDiscoveryService() {
 
             @Override
             public List<InetAddress> getNodes() {
-                return Collections.emptyList();
+                return nodes;
             }
 
             @Override
-            public void addListener(ClusterListener listener) {
+            public void addListener(final ClusterListener listener) {
                 // Ignore
             }
 
             @Override
-            public void removeListener(ClusterListener listener) {
+            public void removeListener(final ClusterListener listener) {
                 // Ignore
             }
         });
+    }
+
+    private static boolean isEmpty(final String string) {
+        if (null == string) {
+            return true;
+        }
+        final int len = string.length();
+        boolean isWhitespace = true;
+        for (int i = 0; isWhitespace && i < len; i++) {
+            isWhitespace = Character.isWhitespace(string.charAt(i));
+        }
+        return isWhitespace;
     }
 
 }
