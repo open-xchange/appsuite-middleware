824b1c883ff6fb22e6f1c86f2f70c9a52b62c51c
Thorben Betten <thorben.betten@open-xchange.com>
2012-11-08 18:23:22 +0100

    Catch StackoverflowError

--- com.openexchange.imap/src/com/openexchange/imap/IMAPMessageStorage.java
+++ com.openexchange.imap/src/com/openexchange/imap/IMAPMessageStorage.java
@@ -396,8 +396,12 @@ public final class IMAPMessageStorage extends IMAPFolderWorker implements IMailM
                     return content;
                 }
                 if (subtype.startsWith("htm")) {
-                    return new Renderer(new Segment(new Source(content), 0, content.length())).setMaxLineLength(9999).setIncludeHyperlinkURLs(
-                        false).toString();
+                    try {
+                        return new Renderer(new Segment(new Source(content), 0, content.length())).setMaxLineLength(9999).setIncludeHyperlinkURLs(
+                            false).toString();
+                    } catch (final StackOverflowError parserOverflow) {
+                        // Ignore
+                    }
                     // content = PATTERN_CRLF.matcher(content).replaceAll("");// .replaceAll("(  )+", "");
                 }
                 try {
