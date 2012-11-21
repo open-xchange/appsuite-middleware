f6ca35d574d4b62fac3414e5a69c1629e57e3d91
Thorben Betten <thorben.betten@open-xchange.com>
2012-11-21 22:19:20 +0100

    Fix for bug 23683: Orderly handle possible unchecked exceptions

--- com.openexchange.subscribe/src/com/openexchange/subscribe/SubscriptionErrorStrings.java
+++ com.openexchange.subscribe/src/com/openexchange/subscribe/SubscriptionErrorStrings.java
@@ -124,4 +124,7 @@ public class SubscriptionErrorStrings implements LocalizableStrings {
 	public static final String PERMISSION_DENIED = "You do not have the permission to complete this operation.";
 	
 	public static final String NO_OAUTH_ACCOUNT_GIVEN = "You need to create an OAuth-account first to access this service.";
+
+	// An unexpected error occurred: %1$s.
+    public static final String UNEXPECTED_ERROR = "An unexpected error occurred: %1$s.";
 }
