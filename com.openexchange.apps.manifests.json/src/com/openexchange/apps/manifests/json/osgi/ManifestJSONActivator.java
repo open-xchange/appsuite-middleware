b136795bc54ffd86dc1a4516eda0217f1cc3ff6b
Thorben Betten <thorben.betten@open-xchange.com>
2012-12-06 13:31:39 +0100

    minor

--- com.openexchange.apps.manifests.json/src/com/openexchange/apps/manifests/json/osgi/ManifestJSONActivator.java
+++ com.openexchange.apps.manifests.json/src/com/openexchange/apps/manifests/json/osgi/ManifestJSONActivator.java
@@ -52,69 +52,64 @@ package com.openexchange.apps.manifests.json.osgi;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.json.JSONArray;
 import com.openexchange.ajax.requesthandler.osgiservice.AJAXModuleActivator;
 import com.openexchange.capabilities.CapabilityService;
 import com.openexchange.config.ConfigurationService;
+import com.openexchange.java.Streams;
+import com.openexchange.java.StringAllocator;
 import com.openexchange.log.LogFactory;
 
 /**
  * {@link ManifestJSONActivator}
- *
+ * 
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
 public class ManifestJSONActivator extends AJAXModuleActivator {
-	
-	private static final Log LOG = LogFactory.getLog(ManifestJSONActivator.class);
-	
-	@Override
-	protected Class<?>[] getNeededServices() {
-		return new Class<?>[]{ConfigurationService.class, CapabilityService.class};
-	}
 
-	@Override
-	protected void startBundle() throws Exception {
-		registerModule(new ManifestActionFactory(this, readManifests()), "apps/manifests");
-	}
+    private static final Log LOG = LogFactory.getLog(ManifestJSONActivator.class);
 
-	private JSONArray readManifests() {
-		File file = new File(getService(ConfigurationService.class).getProperty("com.openexchange.apps.manifestPath"));
-		
-		JSONArray array = new JSONArray();
-		if (file.exists()) {
-			for(File f: file.listFiles()) {
-				read(f, array);
-			}
-		}
-		return array;
-	}
+    @Override
+    protected Class<?>[] getNeededServices() {
+        return new Class<?>[] { ConfigurationService.class, CapabilityService.class };
+    }
 
-	private void read(File f, JSONArray array) {
-		BufferedReader r = null;
-		StringBuilder b = new StringBuilder();
-		try {
-			r = new BufferedReader(new FileReader(f));
-			int c = -1;
-			while((c = r.read()) != -1) {
-				b.append((char) c);
-			}
-			JSONArray fileContent = new JSONArray(b.toString());
-			for(int i = 0, size = fileContent.length(); i < size; i++) {
-				array.put(fileContent.get(i));
-			}
-		} catch (Throwable e) {
-			LOG.error(e.getMessage(), e);
-		} finally {
-			if (r != null) {
-				try {
-					r.close();
-				} catch (IOException e1) {
-				}
-			}
-		}
-	}
+    @Override
+    protected void startBundle() throws Exception {
+        registerModule(new ManifestActionFactory(this, readManifests()), "apps/manifests");
+    }
 
+    private JSONArray readManifests() {
+        File file = new File(getService(ConfigurationService.class).getProperty("com.openexchange.apps.manifestPath"));
+
+        JSONArray array = new JSONArray();
+        if (file.exists()) {
+            for (File f : file.listFiles()) {
+                read(f, array);
+            }
+        }
+        return array;
+    }
+
+    private void read(File f, JSONArray array) {
+        BufferedReader r = null;
+        StringAllocator b = new StringAllocator();
+        try {
+            r = new BufferedReader(new FileReader(f));
+            int c = -1;
+            while ((c = r.read()) != -1) {
+                b.append((char) c);
+            }
+            JSONArray fileContent = new JSONArray(b.toString());
+            for (int i = 0, size = fileContent.length(); i < size; i++) {
+                array.put(fileContent.get(i));
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            Streams.close(r);
+        }
+    }
 
 }
