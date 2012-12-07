66a2f2de687231fbd76805b4dc10df30a2ebb4ad
Thorben Betten <thorben.betten@open-xchange.com>
2012-12-07 13:06:10 +0100

    fixed

--- com.openexchange.server/src/com/openexchange/webdav/action/WebdavGetAction.java
+++ com.openexchange.server/src/com/openexchange/webdav/action/WebdavGetAction.java
@@ -116,9 +116,7 @@ public class WebdavGetAction extends WebdavHeadAction {
 			}
 
 		} catch (final IOException e) {
-			final WebdavProtocolException wpe = WebdavProtocolException.Code.GENERAL_ERROR.create(req.getUrl(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			wpe.initCause(e);
-            throw wpe;
+			throw WebdavProtocolException.Code.GENERAL_ERROR.create(req.getUrl(), HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e, new Object[0]);
 		} finally {
 			if(out != null) {
 				try {
