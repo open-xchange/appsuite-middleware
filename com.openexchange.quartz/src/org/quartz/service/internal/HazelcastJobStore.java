32f54ce6a1eef1fb9ee529c9849e5d5b2f39b50d
Steffen Templin <steffen.templin@open-xchange.com>
2012-12-12 19:10:04 +0100

    Fixed consistency task

--- com.openexchange.quartz/src/org/quartz/service/internal/HazelcastJobStore.java
+++ com.openexchange.quartz/src/org/quartz/service/internal/HazelcastJobStore.java
@@ -1558,8 +1558,8 @@ public class HazelcastJobStore implements JobStore {
                 try {
                     Set<TriggerKey> hazelcastKeys = triggersByKey.keySet(new AcquiredAndExecutingTriggersPredicate(nodeIp));
                     clusterKeys = new HashSet<TriggerKey>(hazelcastKeys);
-                    clusterKeys.remove(locallyAcquiredTriggers);
-                    clusterKeys.remove(locallyExecutingTriggers);
+                    clusterKeys.removeAll(locallyAcquiredTriggers.keySet());
+                    clusterKeys.removeAll(locallyExecutingTriggers.keySet());
                 } finally {
                     lock.unlock();
                 }
