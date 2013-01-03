1b3af8122a9a7a18ce0e620f4559c55c2e0f1bee
Thorben Betten <thorben.betten@open-xchange.com>
2013-01-03 18:22:41 +0100

    changed author

--- com.openexchange.secret.recovery/src/com/openexchange/secret/recovery/SecretCleanUpService.java
+++ com.openexchange.secret.recovery/src/com/openexchange/secret/recovery/SecretCleanUpService.java
@@ -55,7 +55,7 @@ import com.openexchange.tools.session.ServerSession;
 /**
  * {@link SecretCleanUpService} - Cleans-up non-working encrypted items.
  * 
- * @author <a href="mailto:francisco.laguna@open-xchange.com">Francisco Laguna</a>
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
 public interface SecretCleanUpService {
 
