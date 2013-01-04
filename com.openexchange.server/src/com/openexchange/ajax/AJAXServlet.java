23e81a627023568a698fee5cbcc4bf9be4e870f5
Thorben Betten <thorben.betten@open-xchange.com>
2013-01-04 12:31:32 +0100

    formatting

--- com.openexchange.server/src/com/openexchange/ajax/AJAXServlet.java
+++ com.openexchange.server/src/com/openexchange/ajax/AJAXServlet.java
@@ -410,11 +410,8 @@ public abstract class AJAXServlet extends HttpServlet implements UploadRegistry 
      *      &lt;/html&gt;
 	 * </pre>
 	 */
-	public static final String JS_FRAGMENT = "<!DOCTYPE HTML PUBLIC "
-			+ "\"-//W3C//DTD HTML 4.01//EN\" "
-			+ "\"http://www.w3.org/TR/html4/strict.dtd\"><html><head>"
-			+ "<META http-equiv=\"Content-Type\" "
-			+ "content=\"text/html; charset=UTF-8\">"
+	public static final String JS_FRAGMENT = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"><html><head>"
+			+ "<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">"
 			+ "<script type=\"text/javascript\">"
 			+ "(parent.callback_**action** || window.opener && "
 			+ "window.opener.callback_**action**)(**json**)"
