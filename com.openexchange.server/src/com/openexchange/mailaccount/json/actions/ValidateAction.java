94cacce92c65054302ecd28bd77eeaaf0418800e
Thorben Betten <thorben.betten@open-xchange.com>
2013-01-09 12:29:54 +0100

    check for an empty string rather than null

--- com.openexchange.server/src/com/openexchange/mailaccount/json/actions/ValidateAction.java
+++ com.openexchange.server/src/com/openexchange/mailaccount/json/actions/ValidateAction.java
@@ -284,16 +284,4 @@ public final class ValidateAction extends AbstractMailAccountTreeAction {
         return validated;
     }
 
-    private static boolean isEmpty(final String string) {
-        if (null == string) {
-            return true;
-        }
-        final int len = string.length();
-        boolean isWhitespace = true;
-        for (int i = 0; isWhitespace && i < len; i++) {
-            isWhitespace = Character.isWhitespace(string.charAt(i));
-        }
-        return isWhitespace;
-    }
-
 }
