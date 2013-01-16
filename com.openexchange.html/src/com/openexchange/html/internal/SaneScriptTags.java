42500c3aa3fbed9442a5c051c270497f43b6e3c1
Thorben Betten <thorben.betten@open-xchange.com>
2013-01-16 11:19:23 +0100

    isEmpty

--- com.openexchange.html/src/com/openexchange/html/internal/SaneScriptTags.java
+++ com.openexchange.html/src/com/openexchange/html/internal/SaneScriptTags.java
@@ -74,7 +74,7 @@ public final class SaneScriptTags {
      * @return The sanitized HTML content
      */
     public static String saneScriptTags(final String html) {
-        if (null == html) {
+        if (isEmpty(html)) {
             return html;
         }
         String s = html;
@@ -141,6 +141,18 @@ public final class SaneScriptTags {
         } while (m.find());
         m.appendTail(sb);
         return sb.toString();
+    }
+
+    private static boolean isEmpty(final String string) {
+        if (null == string) {
+            return true;
+        }
+        final int len = string.length();
+        boolean isWhitespace = true;
+        for (int i = 0; isWhitespace && i < len; i++) {
+            isWhitespace = Character.isWhitespace(string.charAt(i));
+        }
+        return isWhitespace;
     }
 
 }
