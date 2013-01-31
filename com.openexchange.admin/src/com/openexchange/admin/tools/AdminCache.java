1c40238baf5c9aedd182f7622e499dd7a7db8f75
Thorben Betten <thorben.betten@open-xchange.com>
2013-01-31 15:49:29 +0100

    Fix for bug 24714: Check for "null" as user's password mechanism, too

--- com.openexchange.admin/src/com/openexchange/admin/tools/AdminCache.java
+++ com.openexchange.admin/src/com/openexchange/admin/tools/AdminCache.java
@@ -92,6 +92,7 @@ import com.openexchange.admin.storage.sqlStorage.OXAdminPoolDBPool;
 import com.openexchange.admin.storage.sqlStorage.OXAdminPoolInterface;
 import com.openexchange.config.ConfigurationService;
 import com.openexchange.java.Streams;
+import com.openexchange.java.StringAllocator;
 import com.openexchange.log.LogFactory;
 import com.openexchange.tools.sql.DBUtils;
 
@@ -599,7 +600,7 @@ public class AdminCache {
      */
     public String encryptPassword(final PasswordMechObject user) throws StorageException, NoSuchAlgorithmException, UnsupportedEncodingException {
         final String passwordMech = user.getPasswordMech();
-        if (passwordMech == null) {
+        if (isEmpty(passwordMech) || "null".equals(toLowerCase(passwordMech))) {
             String pwmech = getProperties().getUserProp(AdminProperties.User.DEFAULT_PASSWORD_MECHANISM, "SHA");
             pwmech = "{" + pwmech + "}";
             if (pwmech.equalsIgnoreCase(PasswordMechObject.CRYPT_MECH)) {
@@ -724,6 +725,33 @@ public class AdminCache {
 
     public boolean isMasterAdmin(final Credentials auth) {
         return masterAuthenticationDisabled || getMasterCredentials().getLogin().equals(auth.getLogin());
+    }
+
+    /** ASCII-wise to lower-case */
+    private static String toLowerCase(final CharSequence chars) {
+        if (null == chars) {
+            return null;
+        }
+        final int length = chars.length();
+        final StringAllocator builder = new StringAllocator(length);
+        for (int i = 0; i < length; i++) {
+            final char c = chars.charAt(i);
+            builder.append((c >= 'A') && (c <= 'Z') ? (char) (c ^ 0x20) : c);
+        }
+        return builder.toString();
+    }
+
+    /** Check for an empty string */
+    private static boolean isEmpty(final String string) {
+        if (null == string) {
+            return true;
+        }
+        final int len = string.length();
+        boolean isWhitespace = true;
+        for (int i = 0; isWhitespace && i < len; i++) {
+            isWhitespace = Character.isWhitespace(string.charAt(i));
+        }
+        return isWhitespace;
     }
 
 }
