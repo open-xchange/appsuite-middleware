302b8ef6d303ce16c726e2e4e5dbf3a18d83f88b
Thorben Betten <thorben.betten@open-xchange.com>
2013-02-03 13:07:57 +0100

    revert

--- com.openexchange.server/src/com/openexchange/ajax/Multiple.java
+++ com.openexchange.server/src/com/openexchange/ajax/Multiple.java
@@ -90,7 +90,11 @@ import com.openexchange.multiple.MultipleHandlerFactoryService;
 import com.openexchange.multiple.PathAware;
 import com.openexchange.multiple.internal.MultipleHandlerRegistry;
 import com.openexchange.server.services.ServerServiceRegistry;
+<<<<<<< HEAD
 import com.openexchange.threadpool.BoundedCompletionService;
+=======
+import com.openexchange.threadpool.ThreadPoolCompletionService;
+>>>>>>> cc18b351... revert
 import com.openexchange.threadpool.ThreadPools;
 import com.openexchange.tools.servlet.AjaxExceptionCodes;
 import com.openexchange.tools.servlet.OXJSONExceptionCodes;
@@ -207,7 +211,12 @@ public class Multiple extends SessionServlet {
                         serialTasks.add(jsonInOut);
                     } else {
                         if (null == concurrentTasks) {
-                            concurrentTasks = new BoundedCompletionService<Object>(ThreadPools.getThreadPool(), 4).setTrackable(true);
+                            final int concurrencyLevel = CONCURRENCY_LEVEL;
+                            if (length <= concurrencyLevel) {
+                                concurrentTasks = new ThreadPoolCompletionService<Object>(ThreadPools.getThreadPool()).setTrackable(true);
+                            } else {
+                                concurrentTasks = new ThreadPoolCompletionService<Object>(ThreadPools.getThreadPool()).setTrackable(true);//new BoundedCompletionService<Object>(ThreadPools.getThreadPool(), concurrencyLevel).setTrackable(true);
+                            }
                         }
                         concurrentTasks.submit(new CallableImpl(jsonInOut, session, module, req));
                         concurrentTasksCount++;
