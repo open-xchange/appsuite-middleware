3163e225625518392f8644010581acd5e23e3aff
Thorben Betten <thorben.betten@open-xchange.com>
2013-02-07 15:27:10 +0100

    volatile modifier

--- com.openexchange.push.udp/src/com/openexchange/push/udp/osgi/TimerCustomizer.java
+++ com.openexchange.push.udp/src/com/openexchange/push/udp/osgi/TimerCustomizer.java
@@ -50,10 +50,10 @@
 package com.openexchange.push.udp.osgi;
 
 import org.apache.commons.logging.Log;
-import com.openexchange.log.LogFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import com.openexchange.log.LogFactory;
 import com.openexchange.push.udp.PushConfiguration;
 import com.openexchange.push.udp.PushDiscoverySender;
 import com.openexchange.push.udp.PushInit;
@@ -69,8 +69,7 @@ public final class TimerCustomizer implements ServiceTrackerCustomizer<TimerServ
     private static final Log LOG = com.openexchange.log.Log.valueOf(LogFactory.getLog(TimerCustomizer.class));
 
     private final BundleContext context;
-
-    private PushDiscoverySender sender;
+    private volatile PushDiscoverySender sender;
 
     /**
      * Initializes a new {@link TimerCustomizer}.
@@ -86,8 +85,9 @@ public final class TimerCustomizer implements ServiceTrackerCustomizer<TimerServ
         final PushConfiguration config = PushInit.getInstance().getConfig();
         if (config.isMultiCastEnabled()) {
             LOG.info("Starting push multicast discovery sender.");
-            sender = new PushDiscoverySender(config);
+            final PushDiscoverySender sender = new PushDiscoverySender(config);
             sender.startSender(timer);
+            this.sender = sender;
         } else {
             LOG.info("Push multicast discovery is disabled.");
         }
@@ -101,10 +101,11 @@ public final class TimerCustomizer implements ServiceTrackerCustomizer<TimerServ
 
     @Override
     public void removedService(final ServiceReference<TimerService> reference, final TimerService service) {
+        final PushDiscoverySender sender = this.sender;
         if (null != sender) {
             LOG.info("Stopping push multicast discovery sender.");
             sender.stopSender();
-            sender = null;
+            this.sender = null;
         }
         context.ungetService(reference);
     }
