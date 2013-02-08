ba13f2f243bfe93638ba73fbf64340e4ad4253de
Thorben Betten <thorben.betten@open-xchange.com>
2013-02-08 17:14:17 +0100

    minor changes

--- com.openexchange.halo/src/com/openexchange/halo/AbstractContactHalo.java
+++ com.openexchange.halo/src/com/openexchange/halo/AbstractContactHalo.java
@@ -46,52 +46,97 @@
  *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+
 package com.openexchange.halo;
 
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
-
+import java.util.Set;
 import com.openexchange.exception.OXException;
 import com.openexchange.groupware.container.Contact;
 import com.openexchange.groupware.ldap.User;
 import com.openexchange.tools.session.ServerSession;
 
-public abstract class AbstractContactHalo implements HaloContactDataSource{
+public abstract class AbstractContactHalo implements HaloContactDataSource {
 
-	public AbstractContactHalo() {
-		super();
-	}
+    /**
+     * Initializes a new {@link AbstractContactHalo}.
+     */
+    protected AbstractContactHalo() {
+        super();
+    }
 
-	protected boolean isUserThemselves(User user, List<String> addresses) {
-		List<String> ownAddresses = new LinkedList<String>();
-		ownAddresses.addAll( Arrays.asList(user.getAliases()));
-		ownAddresses.add(user.getMail());
-		for(String requested: addresses){
-			if(!ownAddresses.contains(requested)){
-				return false;
-			}
-		}
-		return true;
-	}
+    protected boolean isUserThemselves(final User user, final List<String> addresses) {
+        final Set<String> ownAddresses = new HashSet<String>(8);
+        for (final String alias : user.getAliases()) {
+            if (!isEmpty(alias)) {
+                ownAddresses.add(toLowerCase(alias));
+            }
+        }
+        ownAddresses.add(toLowerCase(user.getMail()));
+        for (final String requested : addresses) {
+            if (!ownAddresses.contains(toLowerCase(requested))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	protected List<String> getEMailAddresses(Contact contact) {
-		List<String> addresses = new LinkedList<String>();
-		if (contact.containsEmail1()) {
-			addresses.add(contact.getEmail1());
-		}
-		if (contact.containsEmail2()) {
-			addresses.add(contact.getEmail2());
-		}
-		if (contact.containsEmail3()) {
-			addresses.add(contact.getEmail3());
-		}
-		return addresses;
-	}
+    protected List<String> getEMailAddresses(final Contact contact) {
+        final Set<String> addresses = new LinkedHashSet<String>(8);
+        if (contact.containsEmail1()) {
+            final String s = contact.getEmail1();
+            if (null != s) {
+                addresses.add(s);
+            }
+        }
+        if (contact.containsEmail2()) {
+            final String s = contact.getEmail2();
+            if (null != s) {
+                addresses.add(s);
+            }
+        }
+        if (contact.containsEmail3()) {
+            final String s = contact.getEmail3();
+            if (null != s) {
+                addresses.add(s);
+            }
+        }
+        return new ArrayList<String>(addresses);
+    }
 
-	@Override
-	public boolean isAvailable(ServerSession session) throws OXException {
-		return true;
-	}
+    @Override
+    public boolean isAvailable(final ServerSession session) throws OXException {
+        return true;
+    }
+
+    /** Check for an empty string */
+    protected static boolean isEmpty(final String string) {
+        if (null == string) {
+            return true;
+        }
+        final int len = string.length();
+        boolean isWhitespace = true;
+        for (int i = 0; isWhitespace && i < len; i++) {
+            isWhitespace = Character.isWhitespace(string.charAt(i));
+        }
+        return isWhitespace;
+    }
+
+    /** ASCII-wise to lower-case */
+    protected static String toLowerCase(final CharSequence chars) {
+        if (null == chars) {
+            return null;
+        }
+        final int length = chars.length();
+        final StringBuilder builder = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            final char c = chars.charAt(i);
+            builder.append((c >= 'A') && (c <= 'Z') ? (char) (c ^ 0x20) : c);
+        }
+        return builder.toString();
+    }
 
 }
