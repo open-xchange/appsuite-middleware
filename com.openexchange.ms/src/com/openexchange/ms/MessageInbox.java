1e6f3e9c3e15137e6a58eb4923866008fc3fe79f
Thorben Betten <thorben.betten@open-xchange.com>
2013-02-23 02:02:51 +0100

    JavaDoc

--- com.openexchange.ms/src/com/openexchange/ms/MessageInbox.java
+++ com.openexchange.ms/src/com/openexchange/ms/MessageInbox.java
@@ -53,7 +53,7 @@ import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 /**
- * {@link MessageInbox} - The message Inbox for peer-to-peer messages.
+ * {@link MessageInbox} - The (read-only) message Inbox for peer-to-peer messages.
  * 
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
