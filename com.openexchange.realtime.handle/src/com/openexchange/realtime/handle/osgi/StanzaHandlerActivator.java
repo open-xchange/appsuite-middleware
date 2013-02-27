eb49fe8e5745bd710fce100bf932406f26e02a3f
Marc Arens <marc.arens@open-xchange.com>
2013-02-27 17:28:33 +0100

    Add c.o.r.handle skeleton

---
+++ com.openexchange.realtime.handle/src/com/openexchange/realtime/handle/osgi/StanzaHandlerActivator.java
@@ -0,0 +1,30 @@
+package com.openexchange.realtime.handle.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class StanzaHandlerActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		StanzaHandlerActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		StanzaHandlerActivator.context = null;
+	}
+
+}
