0fe12989014d6830ae9d06397eeb61292f306f53
Marc Arens <marc.arens@open-xchange.com>
2013-03-07 19:35:43 +0100

    Use loggerFor

--- com.openexchange.realtime.hazelcast/src/com/openexchange/realtime/hazelcast/directory/HazelcastResourceDirectory.java
+++ com.openexchange.realtime.hazelcast/src/com/openexchange/realtime/hazelcast/directory/HazelcastResourceDirectory.java
@@ -76,7 +76,7 @@ import com.openexchange.realtime.util.IDMap;
  */
 public class HazelcastResourceDirectory extends DefaultResourceDirectory {
     
-    private static final Log LOG = LogFactory.getLog(HazelcastResourceDirectory.class);
+    private static final Log LOG = com.openexchange.log.Log.loggerFor(HazelcastResourceDirectory.class);
 
     /** Mapping of general IDs to full IDs e.q marc.arens@premium <-> ox://marc.arens@premuim/random. */
     private static final String ID_MAP = "rtIDMapping-0";
