414ca4f920cf91ece41734423abdfa61ef62cf49
Thorben Betten <thorben.betten@open-xchange.com>
2013-03-15 18:24:34 +0100

    Added to RMI, CLI and SOAP

---
+++ com.openexchange.admin.soap.context/src/com/openexchange/admin/soap/context/soap/ChangeQuota.java
@@ -0,0 +1,182 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.admin.soap.context.soap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import com.openexchange.admin.soap.context.dataobjects.Context;
+import com.openexchange.admin.soap.context.dataobjects.Credentials;
+
+/**
+ * <p>Java-Klasse f\u00fcr anonymous complex type.
+ *
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ *
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="ctx" type="{http://dataobjects.soap.admin.openexchange.com/xsd}Context" minOccurs="0"/>
+ *         &lt;element name="module" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="quotaValue" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="auth" type="{http://dataobjects.rmi.admin.openexchange.com/xsd}Credentials" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ *
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "ctx",
+    "module",
+    "quotaValue",
+    "auth"
+})
+@XmlRootElement(name = "changeQuota")
+public final class ChangeQuota {
+
+    /**
+     * Initializes a new {@link ChangeQuota}.
+     */
+    public ChangeQuota() {
+        super();
+    }
+
+    @XmlElement(nillable = true)
+    protected Context ctx;
+    @XmlElement(nillable = true)
+    protected String module;
+    @XmlElement(nillable = true)
+    protected String quotaValue;
+    @XmlElement(nillable = true)
+    protected Credentials auth;
+
+    /**
+     * Ruft den Wert der ctx-Eigenschaft ab.
+     *
+     * @return
+     *     possible object is
+     *     {@link Context }
+     *
+     */
+    public Context getCtx() {
+        return ctx;
+    }
+
+    /**
+     * Legt den Wert der ctx-Eigenschaft fest.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link Context }
+     *
+     */
+    public void setCtx(Context value) {
+        this.ctx = value;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    /**
+     * Gets the quotaValue
+     *
+     * @return The quotaValue
+     */
+    public String getQuotaValue() {
+        return quotaValue;
+    }
+    
+    /**
+     * Sets the quotaValue
+     *
+     * @param quotaValue The quotaValue to set
+     */
+    public void setQuotaValue(String quotaValue) {
+        this.quotaValue = quotaValue;
+    }
+
+    /**
+     * Ruft den Wert der auth-Eigenschaft ab.
+     *
+     * @return
+     *     possible object is
+     *     {@link Credentials }
+     *
+     */
+    public Credentials getAuth() {
+        return auth;
+    }
+
+    /**
+     * Legt den Wert der auth-Eigenschaft fest.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link Credentials }
+     *
+     */
+    public void setAuth(Credentials value) {
+        this.auth = value;
+    }
+
+}
