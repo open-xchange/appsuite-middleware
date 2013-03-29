214637d27050cafee861230d4bb2464a92e62f0e
Marc Arens <marc.arens@open-xchange.com>
2013-03-29 17:09:33 +0100

    Adapt AtmosphereConfig to the needs of the new SessionValidator

--- com.openexchange.realtime.atmosphere/src/com/openexchange/realtime/atmosphere/AtmosphereConfig.java
+++ com.openexchange.realtime.atmosphere/src/com/openexchange/realtime/atmosphere/AtmosphereConfig.java
@@ -49,25 +49,31 @@
 
 package com.openexchange.realtime.atmosphere;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.commons.logging.Log;
 import com.openexchange.config.ConfigurationService;
 import com.openexchange.configuration.ClientWhitelist;
 import com.openexchange.configuration.CookieHashSource;
 import com.openexchange.exception.OXException;
+import com.openexchange.java.Strings;
 import com.openexchange.realtime.RealtimeExceptionCodes;
 import com.openexchange.realtime.atmosphere.osgi.AtmosphereServiceRegistry;
 import com.openexchange.server.Initialization;
+import com.openexchange.sessiond.impl.IPRange;
 import com.openexchange.sessiond.impl.SubnetMask;
 
 /**
- * {@link GrizzlyConfig} Collects and exposes configuration parameters needed by GrizzlOX
- *
- * @author <a href="mailto:marc	.arens@open-xchange.com">Marc Arens</a>
+ * {@link AtmosphereConfig} Collects and exposes configuration parameters needed by Atmosphere
+ * 
+ * @author <a href="mailto:marc.arens@open-xchange.com">Marc Arens</a>
  */
 public class AtmosphereConfig implements Initialization {
 
     private static final Log LOG = com.openexchange.log.Log.loggerFor(AtmosphereConfig.class);
+
+    private final static String SESSION_WHITELIST_FILE = "noipcheck.cnf";
 
     private static final AtmosphereConfig instance = new AtmosphereConfig();
 
@@ -79,12 +85,18 @@ public class AtmosphereConfig implements Initialization {
 
     // server.properties
     private boolean isIPCheckEnabled;
+
     private CookieHashSource cookieHashSource;
-    private ClientWhitelist clientWhitelist; 
+
+    private ClientWhitelist clientWhitelist;
+
     private SubnetMask allowedSubnet;
-    private String ipv4Mask; 
-    private String iPv6Mask;
-    
+
+    private String ipMaskV4;
+
+    private String ipMaskV6;
+
+    private List<IPRange> ipRangeWhitelist;
 
     @Override
     public void start() throws OXException {
@@ -110,82 +122,109 @@ public class AtmosphereConfig implements Initialization {
         }
 
         // server properties
-        this.isIPCheckEnabled = configService.getBoolProperty("com.openexchange.IPCheck", true);
-        this.cookieHashSource = CookieHashSource.parse(configService.getProperty(" com.openexchange.cookie.hash", "calculate"));
-        this.clientWhitelist = new ClientWhitelist().add(configService.getProperty("com.openexchange.IPCheckWhitelist", ""));
-        final String ipMaskV4 = configService.getProperty("com.openexchange.IPMaskV4", "");
-        final String ipMaskV6 = configService.getProperty("com.openexchange.IPMaskV6","");
-        this.allowedSubnet = new SubnetMask(ipMaskV4, ipMaskV6);
-        
+        isIPCheckEnabled = configService.getBoolProperty("com.openexchange.IPCheck", true);
+        cookieHashSource = CookieHashSource.parse(configService.getProperty(" com.openexchange.cookie.hash", "calculate"));
+        clientWhitelist = new ClientWhitelist().add(configService.getProperty("com.openexchange.IPCheckWhitelist", ""));
+        ipMaskV4 = configService.getProperty("com.openexchange.IPMaskV4", "");
+        ipMaskV6 = configService.getProperty("com.openexchange.IPMaskV6", "");
+        allowedSubnet = new SubnetMask(ipMaskV4, ipMaskV6);
+        ipRangeWhitelist = new LinkedList<IPRange>();
+        initIpRangeWhitelist(configService);
+
+    }
+
+    /**
+     * Init the IP ranges that should be excluded from the ip Checking during session validation.
+     */
+    private void initIpRangeWhitelist(ConfigurationService configService) {
+        if (isIPCheckEnabled) {
+            String text = configService.getText(SESSION_WHITELIST_FILE);
+            if (text != null) {
+                if (LOG.isInfoEnabled()) {
+                    LOG.info("Exceptions from IP Check have been defined.");
+                }
+                final String[] lines = Strings.splitByCRLF(text);
+                for (String line : lines) {
+                    line = line.replaceAll("\\s", "");
+                    if (!line.equals("") && (line.length() == 0 || line.charAt(0) != '#')) {
+                        ipRangeWhitelist.add(IPRange.parseRange(line));
+                    }
+                }
+            }
+        }
     }
 
     /**
      * Gets the started
-     *
+     * 
      * @return The started
      */
     public AtomicBoolean getStarted() {
         return started;
     }
-    
-    
+
     /**
      * Gets the isIPCheckEnabled
-     *
+     * 
      * @return The isIPCheckEnabled
      */
     public boolean isIPCheckEnabled() {
         return isIPCheckEnabled;
     }
 
-    
     /**
      * Gets the cookieHashSource
-     *
+     * 
      * @return The cookieHashSource
      */
     public CookieHashSource getCookieHashSource() {
         return cookieHashSource;
     }
 
-    
     /**
-     * Gets the clientWhitelist
-     *
-     * @return The clientWhitelist
+     * Gets the clientWhitelist, used to exclude single clients from IP checks.
+     * 
+     * @return The clientWhitelist, used to exclude single clients from IP checks
      */
     public ClientWhitelist getClientWhitelist() {
         return clientWhitelist;
     }
 
-    
     /**
      * Gets the allowedSubnet
-     *
+     * 
      * @return The allowedSubnet
      */
     public SubnetMask getAllowedSubnet() {
         return allowedSubnet;
     }
 
-    
     /**
-     * Gets the ipv4Mask
-     *
-     * @return The ipv4Mask
+     * Gets the ipMaskV4
+     * 
+     * @return The ipMaskV4
      */
-    public String getIpv4Mask() {
-        return ipv4Mask;
+    public String getIpMaskV4() {
+        return ipMaskV4;
+    }
+
+    /**
+     * Gets the ipMaskV6
+     * 
+     * @return The ipMaskV6
+     */
+    public String getIpMaskV6() {
+        return ipMaskV6;
     }
 
     
     /**
-     * Gets the iPv6Mask
+     * Gets the ipRangeWhitelist used to exclude clients from IP checks.
      *
-     * @return The iPv6Mask
+     * @return The ipRangeWhitelist used to exclude clients from IP checks.
      */
-    public String getiPv6Mask() {
-        return iPv6Mask;
+    public List<IPRange> getIpRangeWhitelist() {
+        return ipRangeWhitelist;
     }
 
 }
