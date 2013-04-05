bbca7c146c937b25b9c4decf789ccf039ac1bac1
Thorben Betten <thorben.betten@open-xchange.com>
2013-04-05 13:05:06 +0200

    Fix for bug 25710: Added missing Servlet path and bounded SOAP interface to localhost only

---
+++ com.openexchange.soap.cxf/src/com/openexchange/soap/cxf/servlet/BoundedCXFNonSpringServlet.java
@@ -0,0 +1,132 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2020 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.soap.cxf.servlet;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import com.openexchange.groupware.notify.hostname.HostnameService;
+import com.openexchange.java.StringAllocator;
+
+/**
+ * {@link BoundedCXFNonSpringServlet}
+ * 
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ */
+public class BoundedCXFNonSpringServlet extends CXFNonSpringServlet {
+
+    private static final long serialVersionUID = 958156888628637006L;
+
+    private final Set<String> allowedAddresses;
+
+    public BoundedCXFNonSpringServlet(final HostnameService hostnameService) {
+        super();
+        if (null == hostnameService) {
+            this.allowedAddresses = Collections.<String> emptySet();
+        } else {
+            final HashSet<String> newSet = new HashSet<String>(4);
+            try {
+                newSet.add(toLowerCase(hostnameService.getHostname(-1, -1)));
+            } catch (final Exception e) {
+                final Log log = com.openexchange.log.Log.loggerFor(BoundedCXFNonSpringServlet.class);
+                log.warn("Couldn't get server host name.", e);
+            }
+            this.allowedAddresses = newSet;
+        }
+    }
+
+    @Override
+    protected void invoke(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+        if (!allowedAddresses.isEmpty()) {
+            String remoteHost = toLowerCase(request.getRemoteHost());
+            if (isEmpty(remoteHost)) {
+                remoteHost = toLowerCase(request.getRemoteAddr());
+            }
+            if (!"localhost".equals(remoteHost) && !"127.0.0.1".equals(remoteHost) && !"::1".equals(remoteHost) && !allowedAddresses.contains(remoteHost)) {
+                throw new ServletException("SOAP interface must not be accessed from host: " + remoteHost);
+            }
+        }
+        // Invoke...
+        super.invoke(request, response);
+    }
+
+    /** Check for an empty string */
+    private static boolean isEmpty(final String string) {
+        if (null == string) {
+            return true;
+        }
+        final int len = string.length();
+        boolean isWhitespace = true;
+        for (int i = 0; isWhitespace && i < len; i++) {
+            isWhitespace = Character.isWhitespace(string.charAt(i));
+        }
+        return isWhitespace;
+    }
+
+    /** ASCII-wise to lower-case */
+    private static String toLowerCase(final CharSequence chars) {
+        if (null == chars) {
+            return null;
+        }
+        final int length = chars.length();
+        final StringAllocator builder = new StringAllocator(length);
+        for (int i = 0; i < length; i++) {
+            final char c = chars.charAt(i);
+            builder.append((c >= 'A') && (c <= 'Z') ? (char) (c ^ 0x20) : c);
+        }
+        return builder.toString();
+    }
+
+}
