d26f337f347b2f4907fb7db0b86edda23a822da5
Martin Herfurth <martin.herfurth@open.xchange.com>
2013-04-17 10:58:47 +0200

    Check null value.

--- com.openexchange.server/src/com/openexchange/ajax/writer/AppointmentWriter.java
+++ com.openexchange.server/src/com/openexchange/ajax/writer/AppointmentWriter.java
@@ -209,10 +209,10 @@ public class AppointmentWriter extends CalendarWriter {
             writeParameter(CalendarFields.RECURRENCE_DATE_POSITION, appointmentObject.getRecurrenceDatePosition(),
                     jsonObj);
         }
-        if (appointmentObject.containsChangeExceptions() && appointmentObject.getChangeException().length > 0) {
+        if (appointmentObject.containsChangeExceptions() && appointmentObject.getChangeException() != null &&appointmentObject.getChangeException().length > 0) {
             jsonObj.put(CalendarFields.CHANGE_EXCEPTIONS, getExceptionAsJSONArray(appointmentObject.getChangeException()));
         }
-        if (appointmentObject.containsDeleteExceptions() && appointmentObject.getDeleteException().length > 0) {
+        if (appointmentObject.containsDeleteExceptions() && appointmentObject.getDeleteException() != null && appointmentObject.getDeleteException().length > 0) {
             jsonObj.put(CalendarFields.DELETE_EXCEPTIONS,  getExceptionAsJSONArray(appointmentObject.getDeleteException()));
         }
         if (appointmentObject.containsParticipants()) {
