7cd418508bf32572ea96b5d36bac02aff3b15c50
Marcus Klein <marcus.klein@open-xchange.com>
2013-04-22 11:06:04 +0200

    do not use methods of Java 7

--- org.mapdb/src/org/mapdb/StoreDirect.java
+++ org.mapdb/src/org/mapdb/StoreDirect.java
@@ -22,6 +22,7 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
