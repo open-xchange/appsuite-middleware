ed80904e3a212e3b0a1c8c5c1e02c1a9d8f44bda
Thorben Betten <thorben.betten@open-xchange.com>
2013-05-15 16:25:17 +0200

    prepared tst

---
+++ com.openexchange.server/test/com/openexchange/ajax/requesthandler/responseRenderers/FileResponseRendererTest.java
@@ -0,0 +1,103 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2020 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax.requesthandler.responseRenderers;
+
+import junit.framework.TestCase;
+import com.openexchange.ajax.container.ByteArrayFileHolder;
+import com.openexchange.ajax.requesthandler.AJAXRequestData;
+import com.openexchange.ajax.requesthandler.AJAXRequestResult;
+
+
+/**
+ * {@link FileResponseRendererTest}
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ */
+public class FileResponseRendererTest extends TestCase {
+
+    /**
+     * Initializes a new {@link FileResponseRendererTest}.
+     */
+    public FileResponseRendererTest() {
+        super();
+    }
+
+    public void no_testProperContentLength() {
+        try {
+            final String html = "foo\n" +
+                "<object/data=\"data:text/html;base64,PHNjcmlwdD5hbGVydCgiWFNTIFNjaHdhY2hzdGVsbGUiKTwvc2NyaXB0Pg==\"></object>\n" +
+                "bar";
+            byte[] bytes = html.getBytes("ISO-8859-1");
+            final int length = bytes.length;
+
+            ByteArrayFileHolder fileHolder = new ByteArrayFileHolder(bytes);
+            fileHolder.setContentType("text/html; charset=ISO-8859-1");
+            fileHolder.setDelivery("view");
+            fileHolder.setDisposition("inline");
+            fileHolder.setName("document.html");
+
+            FileResponseRenderer fileResponseRenderer = new FileResponseRenderer();
+
+            AJAXRequestData requestData = new AJAXRequestData();
+            AJAXRequestResult result = new AJAXRequestResult(fileHolder, "file");
+            SimHttpServletRequest req = new SimHttpServletRequest();
+            SimHttpServletResponse resp = new SimHttpServletResponse();
+            fileResponseRenderer.writeFileHolder(fileHolder, requestData, result, req, resp);
+
+            int contentLength = resp.getContentLength();
+            assertTrue("Unexpected Content-Length: " + contentLength, contentLength > 0);
+            assertTrue("Unexpected Content-Length: " + contentLength + ", but should be less than " + length, length > contentLength);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+}
