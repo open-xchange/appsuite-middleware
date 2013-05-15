1f487586e2a00e184e954fef2ea22d3043546024
Karsten Will <karsten.will@open-xchange.com>
2013-05-15 12:56:11 +0200

    changed copyright date to 2013

--- com.openexchange.oauth.linkedin/src/com/openexchange/oauth/linkedin/LinkedInXMLParser.java
+++ com.openexchange.oauth.linkedin/src/com/openexchange/oauth/linkedin/LinkedInXMLParser.java
@@ -28,7 +28,7 @@
  *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
  *    given Attribution for the derivative code and a license granting use.
  *
- *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Copyright (C) 2004-2013 Open-Xchange, Inc.
  *     Mail: info@open-xchange.com
  *
  *
