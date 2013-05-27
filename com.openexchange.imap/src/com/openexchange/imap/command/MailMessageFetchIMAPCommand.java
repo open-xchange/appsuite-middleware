98043df3311dae5e00d692cdc30d2e88e9c0686e
Thorben Betten <thorben.betten@open-xchange.com>
2013-05-27 17:51:22 +0200

    Avoid possible NPE (cherry picked from commit 67ca7b4422f2ea5781edac6307bbaf5504442fdd)

--- com.openexchange.imap/src/com/openexchange/imap/command/MailMessageFetchIMAPCommand.java
+++ com.openexchange.imap/src/com/openexchange/imap/command/MailMessageFetchIMAPCommand.java
@@ -72,14 +72,15 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.InternetHeaders;
 import javax.mail.internet.MailDateFormat;
+import javax.mail.internet.ParameterList;
 import com.openexchange.exception.OXException;
 import com.openexchange.java.StringAllocator;
+import com.openexchange.java.Strings;
 import com.openexchange.mail.dataobjects.IDMailMessage;
 import com.openexchange.mail.dataobjects.MailMessage;
 import com.openexchange.mail.mime.ContentType;
 import com.openexchange.mail.mime.MessageHeaders;
 import com.openexchange.mail.mime.MimeMailException;
-import com.openexchange.mail.mime.MimeTypes;
 import com.openexchange.mail.mime.QuotedInternetAddress;
 import com.openexchange.mail.mime.converters.MimeMessageConverter;
 import com.openexchange.mail.mime.utils.MimeMessageUtility;
@@ -909,22 +910,21 @@ public final class MailMessageFetchIMAPCommand extends AbstractIMAPCommand<MailM
         @Override
         public void handleItem(final Item item, final IDMailMessage msg, final org.apache.commons.logging.Log logger) throws OXException {
             final BODYSTRUCTURE bs = (BODYSTRUCTURE) item;
-            final com.openexchange.java.StringAllocator sb = new com.openexchange.java.StringAllocator();
-            sb.append(bs.type).append('/').append(bs.subtype);
-            if (bs.cParams != null) {
-                sb.append(bs.cParams);
-            }
-            try {
-                final String contentType = sb.toString();
-                msg.setContentType(new ContentType(contentType));
-                msg.addHeader("Content-Type", contentType);
-            } catch (final OXException e) {
-                if (logger.isWarnEnabled()) {
-                    logger.warn(e.getMessage(), e);
+            final ContentType contentType = new ContentType().setPrimaryType(bs.type).setSubType(bs.subtype);
+            {
+                final ParameterList cParams = bs.cParams;
+                if (cParams != null) {
+                    for (final Enumeration<?> names = cParams.getNames(); names.hasMoreElements();) {
+                        final String name = names.nextElement().toString();
+                        final String value = cParams.get(name);
+                        if (!isEmpty(value)) {
+                            contentType.setParameter(name, value);
+                        }
+                    }
                 }
-                msg.setContentType(new ContentType(MimeTypes.MIME_DEFAULT));
-                msg.addHeader("Content-Type", MimeTypes.MIME_DEFAULT);
             }
+            msg.setContentType(contentType);
+            msg.addHeader("Content-Type", contentType.toString(true));
             msg.setHasAttachment(bs.isMulti() && (MULTI_SUBTYPE_MIXED.equalsIgnoreCase(bs.subtype) || MimeMessageUtility.hasAttachments(bs)));
         }
 
@@ -1145,6 +1145,19 @@ public final class MailMessageFetchIMAPCommand extends AbstractIMAPCommand<MailM
             }
         }
         return null;
+    }
+
+    /** Check for an empty string */
+    protected static boolean isEmpty(final String string) {
+        if (null == string) {
+            return true;
+        }
+        final int len = string.length();
+        boolean isWhitespace = true;
+        for (int i = 0; isWhitespace && i < len; i++) {
+            isWhitespace = Strings.isWhitespace(string.charAt(i));
+        }
+        return isWhitespace;
     }
 
 }
