2c6067b24613190a83be2776439dd77da4229155
Thorben Betten <thorben.betten@open-xchange.com>
2013-05-27 11:46:06 +0200

    Minor change as preparation for bug fix 26526 (cherry picked from commit b7138553dfdfe0b90049a9c061c0831904245cae)

--- com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXUserMySQLStorage.java
+++ com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXUserMySQLStorage.java
@@ -85,6 +85,7 @@ import com.openexchange.admin.daemons.AdminDaemon;
 import com.openexchange.admin.properties.AdminProperties;
 import com.openexchange.admin.rmi.dataobjects.Context;
 import com.openexchange.admin.rmi.dataobjects.Credentials;
+import com.openexchange.admin.rmi.dataobjects.Group;
 import com.openexchange.admin.rmi.dataobjects.User;
 import com.openexchange.admin.rmi.dataobjects.UserModuleAccess;
 import com.openexchange.admin.rmi.exceptions.PoolException;
@@ -1223,13 +1224,16 @@ public class OXUserMySQLStorage extends OXUserSQLStorage implements OXMySQLDefau
 
                 // Get user's default group ID anf check its existence
                 final int def_group_id;
-                if (usrdata.getDefault_group() == null) {
-                    // Set to context's default group
-                    def_group_id = tool.getDefaultGroupForContext(ctx, con);
-                } else {
-                    def_group_id = usrdata.getDefault_group().getId().intValue();
-                    if (!tool.existsGroup(ctx, con, def_group_id)) {
-                        throw new StorageException("No such group with ID " + def_group_id + " in context " + ctx.getId());
+                {
+                    final Group defaultGroup = usrdata.getDefault_group();
+                    if (defaultGroup == null) {
+                        // Set to context's default group
+                        def_group_id = tool.getDefaultGroupForContext(ctx, con);
+                    } else {
+                        def_group_id = defaultGroup.getId().intValue();
+                        if (!tool.existsGroup(ctx, con, def_group_id)) {
+                            throw new StorageException("No such group with ID " + def_group_id + " in context " + ctx.getId());
+                        }
                     }
                 }
 
