f6d001baec3aaf5c75a3a51fee7b5da42042ea92
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2013-06-03 17:58:58 +0200

    Create table services for settings & tasks

---
+++ com.openexchange.admin/src/com/openexchange/admin/mysql/CreateSettingsTables.java
@@ -0,0 +1,175 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.admin.mysql;
+
+import com.openexchange.database.AbstractCreateTableImpl;
+
+
+/**
+ * {@link CreateSettingsTables}
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ */
+public class CreateSettingsTables extends AbstractCreateTableImpl {
+    
+    private static final String userConfigurationTableName = "user_configuration";
+    private static final String userSettingMailTableName = "user_setting_mail";
+    private static final String userSettingMailSignatureTableName= "user_setting_mail_signature";
+    private static final String userSettingSpellcheckTableName = "user_setting_spellcheck";
+    private static final String userSettingAdminTableName = "user_setting_admin";
+    private static final String userSettingTableName = "user_setting";
+    private static final String userSettingServerTableName = "user_setting_server";
+    
+    private static final String createUserConfigurationTable = "CREATE TABLE user_configuration ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "permissions INT4 UNSIGNED NOT NULL,"
+       + "PRIMARY KEY (cid, user),"
+       + "FOREIGN KEY (cid, user) REFERENCES user (cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createUserSettingMailTable = "CREATE TABLE user_setting_mail ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "bits INT4 UNSIGNED DEFAULT 0,"
+       + "send_addr VARCHAR(256) NOT NULL,"
+       + "reply_to_addr VARCHAR(256) DEFAULT NULL,"
+       + "msg_format TINYINT(4) UNSIGNED DEFAULT 1,"
+       + "display_msg_headers VARCHAR(256) DEFAULT NULL,"
+       + "auto_linebreak INT4 UNSIGNED DEFAULT 80,"
+       + "std_trash VARCHAR(128) NOT NULL,"
+       + "std_sent VARCHAR(128) NOT NULL,"
+       + "std_drafts VARCHAR(128) NOT NULL,"
+       + "std_spam VARCHAR(128) NOT NULL,"
+       + "confirmed_spam VARCHAR(128) NOT NULL,"
+       + "confirmed_ham VARCHAR(128) NOT NULL,"
+       + "upload_quota INT4 DEFAULT -1,"
+       + "upload_quota_per_file INT4 DEFAULT -1,"
+       + "PRIMARY KEY (cid, user),"
+       + "FOREIGN KEY (cid, user) REFERENCES user (cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createUserSettingMailSignatureTable = "CREATE TABLE user_setting_mail_signature ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "id VARCHAR(64) NOT NULL,"
+       + "signature VARCHAR(1024) NOT NULL,"
+       + "PRIMARY KEY (cid, user, id),"
+       + "FOREIGN KEY (cid, user) REFERENCES user_setting_mail (cid, user)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createUserSettingSpellcheckTable = "CREATE TABLE user_setting_spellcheck ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "user_dic TEXT,"
+       + "PRIMARY KEY (cid, user),"
+       + "FOREIGN KEY (cid, user) REFERENCES user (cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createUserSettingAdminTable = "CREATE TABLE user_setting_admin ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "PRIMARY KEY (cid, user),"
+       + "FOREIGN KEY (cid, user) REFERENCES user (cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createUserSettingTable = "CREATE TABLE user_setting ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "user_id INT4 UNSIGNED NOT NULL,"
+       + "path_id INT4 UNSIGNED NOT NULL,"
+       + "value TEXT,"
+       + "PRIMARY KEY (cid, user_id, path_id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createUserSettingServerTable = "CREATE TABLE user_setting_server ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "contact_collect_folder INT4 UNSIGNED,"
+       + "contact_collect_enabled BOOL,"
+       + "defaultStatusPrivate INT4 UNSIGNED DEFAULT 0,"
+       + "defaultStatusPublic INT4 UNSIGNED DEFAULT 0,"
+       + "contactCollectOnMailTransport BOOL DEFAULT TRUE,"
+       + "contactCollectOnMailAccess BOOL DEFAULT TRUE,"
+       + "folderTree INT4,"
+       + "FOREIGN KEY(cid, user) REFERENCES user(cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+
+    /**
+     * Initializes a new {@link CreateSettingsTables}.
+     */
+    public CreateSettingsTables() {
+        super();
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.database.CreateTableService#requiredTables()
+     */
+    @Override
+    public String[] requiredTables() {
+        return new String[] { "user" };
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.database.CreateTableService#tablesToCreate()
+     */
+    @Override
+    public String[] tablesToCreate() {
+        return new String[] { userConfigurationTableName, userSettingMailTableName, userSettingMailSignatureTableName, userSettingSpellcheckTableName, userSettingAdminTableName, userSettingTableName, userSettingServerTableName };
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.database.AbstractCreateTableImpl#getCreateStatements()
+     */
+    @Override
+    protected String[] getCreateStatements() {
+        return new String[] { createUserConfigurationTable, createUserSettingMailTable, createUserSettingMailSignatureTable, createUserSettingSpellcheckTable, createUserSettingAdminTable, createUserSettingTable, createUserSettingServerTable };
+    }
+
+}
---
+++ com.openexchange.admin/src/com/openexchange/admin/mysql/CreateTaskTables.java
@@ -0,0 +1,261 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.admin.mysql;
+
+import com.openexchange.database.AbstractCreateTableImpl;
+
+
+/**
+ * {@link CreateTaskTables}
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ */
+public class CreateTaskTables extends AbstractCreateTableImpl {
+    
+    private static final String taskTableName = "task";
+    private static final String taskFolderTableName = "task_folder";
+    private static final String taskParticipantTableName = "task_participant";
+    private static final String taskEParticipantTableName = "task_eparticipant";
+    private static final String taskRemovedParticipantTableName = "task_removedparticipant";
+    private static final String delTaskTableName = "del_task";
+    private static final String delTaskFolderTableName = "del_task_folder";
+    private static final String delTaskParticipantTableName = "del_task_participant";
+    private static final String delTaskEParticipantTableName = "del_task_eparticipant";
+    
+    private static final String createTaskTable = "CREATE TABLE task ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "id INT4 UNSIGNED NOT NULL,"
+       + "uid VARCHAR(255) NOT NULL,"
+       + "filename VARCHAR(255),"
+       + "private BOOLEAN NOT NULL,"
+       + "creating_date DATETIME NOT NULL,"
+       + "last_modified INT8 NOT NULL,"
+       + "created_from INT4 UNSIGNED NOT NULL,"
+       + "changed_from INT4 UNSIGNED NOT NULL,"
+       + "start DATETIME,"
+       + "end DATETIME,"
+       + "completed DATETIME,"
+       + "title VARCHAR(256),"
+       + "description TEXT,"
+       + "state INTEGER,"
+       + "priority INTEGER,"
+       + "progress INTEGER,"
+       + "categories VARCHAR(255),"
+       + "project INT4 UNSIGNED,"
+       + "target_duration BIGINT,"
+       + "actual_duration BIGINT,"
+       + "target_costs FLOAT,"
+       + "actual_costs FLOAT,"
+       + "currency VARCHAR(10),"
+       + "trip_meter VARCHAR(255),"
+       + "billing VARCHAR(255),"
+       + "companies VARCHAR(255),"
+       + "color_label INT1 UNSIGNED,"
+       + "recurrence_type INT1 UNSIGNED NOT NULL,"
+       + "recurrence_interval INT4 UNSIGNED,"
+       + "recurrence_days INT1 UNSIGNED,"
+       + "recurrence_dayinmonth INT1 UNSIGNED,"
+       + "recurrence_month INT1 UNSIGNED,"
+       + "recurrence_until DATETIME,"
+       + "recurrence_count INT2 UNSIGNED,"
+       + "number_of_attachments INT1 UNSIGNED NOT NULL,"
+       + "PRIMARY KEY (cid,id),"
+       + "INDEX (cid,last_modified)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createTaskFolderTable = "CREATE TABLE task_folder ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "id INT4 UNSIGNED NOT NULL,"
+       + "folder INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "PRIMARY KEY (cid,id,folder),"
+       + "INDEX (cid,folder),"
+       + "FOREIGN KEY (cid, id) REFERENCES task (cid, id),"
+       + "FOREIGN KEY (cid, folder) REFERENCES oxfolder_tree (cid, fuid)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createTaskParticipantTable = "CREATE TABLE task_participant ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "task INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "group_id INT4 UNSIGNED,"
+       + "accepted INT1 UNSIGNED NOT NULL,"
+       + "description VARCHAR(255),"
+       + "PRIMARY KEY (cid,task,user),"
+       + "FOREIGN KEY (cid, task) REFERENCES task (cid, id),"
+       + "FOREIGN KEY (cid,user) REFERENCES user (cid,id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createTaskEParticipantTable = "CREATE TABLE task_eparticipant ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "task INT4 UNSIGNED NOT NULL,"
+       + "mail VARCHAR(255) NOT NULL,"
+       + "display_name VARCHAR(255),"
+       + "PRIMARY KEY (cid,task,mail),"
+       + "FOREIGN KEY (cid,task) REFERENCES task (cid,id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createTaskRemovedParticipantTable = "CREATE TABLE task_removedparticipant ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "task INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "group_id INT4 UNSIGNED,"
+       + "accepted INT1 UNSIGNED NOT NULL,"
+       + "description VARCHAR(255),"
+       + "folder INT4 UNSIGNED NOT NULL,"
+       + "PRIMARY KEY (cid,task,user),"
+       + "INDEX (cid,folder),"
+       + "FOREIGN KEY (cid, task) REFERENCES task (cid, id),"
+       + "FOREIGN KEY (cid,user) REFERENCES user(cid,id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createDelTaskTable = "CREATE TABLE del_task ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "id INT4 UNSIGNED NOT NULL,"
+       + "uid VARCHAR(255) NOT NULL,"
+       + "filename VARCHAR(255),"
+       + "private BOOLEAN NOT NULL,"
+       + "creating_date DATETIME NOT NULL,"
+       + "last_modified INT8 NOT NULL,"
+       + "created_from INT4 UNSIGNED NOT NULL,"
+       + "changed_from INT4 UNSIGNED NOT NULL,"
+       + "start DATETIME,"
+       + "end DATETIME,"
+       + "completed DATETIME,"
+       + "title VARCHAR(256),"
+       + "description TEXT,"
+       + "state INTEGER,"
+       + "priority INTEGER,"
+       + "progress INTEGER,"
+       + "categories VARCHAR(255),"
+       + "project INT4 UNSIGNED,"
+       + "target_duration BIGINT,"
+       + "actual_duration BIGINT,"
+       + "target_costs FLOAT,"
+       + "actual_costs FLOAT,"
+       + "currency VARCHAR(10),"
+       + "trip_meter VARCHAR(255),"
+       + "billing VARCHAR(255),"
+       + "companies VARCHAR(255),"
+       + "color_label INT1 UNSIGNED,"
+       + "recurrence_type INT1 UNSIGNED NOT NULL,"
+       + "recurrence_interval INT4 UNSIGNED,"
+       + "recurrence_days INT1 UNSIGNED,"
+       + "recurrence_dayinmonth INT1 UNSIGNED,"
+       + "recurrence_month INT1 UNSIGNED,"
+       + "recurrence_until DATETIME,"
+       + "recurrence_count INT2 UNSIGNED,"
+       + "number_of_attachments INT1 UNSIGNED NOT NULL,"
+       + "PRIMARY KEY (cid,id),"
+       + "INDEX (cid,last_modified)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createDelTaskFolderTable = "CREATE TABLE del_task_folder ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "id INT4 UNSIGNED NOT NULL,"
+       + "folder INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "PRIMARY KEY (cid,id,folder),"
+       + "INDEX (cid,folder),"
+       + "FOREIGN KEY (cid, id) REFERENCES del_task (cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+
+    private static final String createDelTaskParticipantTable = "CREATE TABLE del_task_participant ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "task INT4 UNSIGNED NOT NULL,"
+       + "user INT4 UNSIGNED NOT NULL,"
+       + "group_id INT4 UNSIGNED,"
+       + "accepted INT1 UNSIGNED NOT NULL,"
+       + "description VARCHAR(255),"
+       + "PRIMARY KEY (cid,task,user),"
+       + "FOREIGN KEY (cid, task) REFERENCES del_task (cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    private static final String createDelTaskEParticipantTable = "CREATE TABLE del_task_eparticipant ("
+       + "cid INT4 UNSIGNED NOT NULL,"
+       + "task INT4 UNSIGNED NOT NULL,"
+       + "mail VARCHAR(255) NOT NULL,"
+       + "display_name VARCHAR(255),"
+       + "PRIMARY KEY (cid,task,mail),"
+       + "FOREIGN KEY (cid, task) REFERENCES del_task (cid, id)"
+     + ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+    
+    /**
+     * Initializes a new {@link CreateTaskTables}.
+     */
+    public CreateTaskTables() {
+        super();
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.database.CreateTableService#requiredTables()
+     */
+    @Override
+    public String[] requiredTables() {
+        return new String[] { "user", "oxfolder_tree" };
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.database.CreateTableService#tablesToCreate()
+     */
+    @Override
+    public String[] tablesToCreate() {
+        return new String[] { taskTableName, taskFolderTableName, taskParticipantTableName, taskEParticipantTableName, taskRemovedParticipantTableName, delTaskTableName, delTaskFolderTableName, delTaskParticipantTableName, delTaskEParticipantTableName };
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.database.AbstractCreateTableImpl#getCreateStatements()
+     */
+    @Override
+    protected String[] getCreateStatements() {
+        return new String[] { createTaskTable, createTaskFolderTable, createTaskParticipantTable, createTaskEParticipantTable, createTaskRemovedParticipantTable, createDelTaskTable, createDelTaskFolderTable, createDelTaskParticipantTable, createDelTaskEParticipantTable };
+    }
+
+}
