4b40f2a54289c30db2334ff7735a63a43f644a3a
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2013-06-18 15:16:57 +0200

    New bundle c.o.tokenlogin.json to acquire token

---
+++ com.openexchange.tokenlogin.json/src/com/openexchange/tokenlogin/json/actions/AcquireTokenAction.java
@@ -0,0 +1,101 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2013 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.tokenlogin.json.actions;
+
+import java.util.Date;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.openexchange.ajax.requesthandler.AJAXRequestResult;
+import com.openexchange.documentation.RequestMethod;
+import com.openexchange.documentation.annotations.Action;
+import com.openexchange.documentation.annotations.Parameter;
+import com.openexchange.exception.OXException;
+import com.openexchange.server.ServiceLookup;
+import com.openexchange.tokenlogin.TokenLoginService;
+import com.openexchange.tokenlogin.json.TokenLoginRequest;
+import com.openexchange.tools.servlet.OXJSONExceptionCodes;
+import com.openexchange.tools.session.ServerSession;
+
+
+/**
+ * {@link AcquireTokenAction}
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ */
+@Action(method = RequestMethod.GET, name = "acquireToken", description = "Get a one-time token for token login",
+    parameters = { @Parameter(name = "session", optional = false, description = "The user's session id") },
+    responseDescription = "The token")
+public class AcquireTokenAction extends TokenLoginAction {
+
+    /**
+     * Initializes a new {@link AcquireTokenAction}.
+     * @param lookup
+     */
+    public AcquireTokenAction(ServiceLookup lookup) {
+        super(lookup);
+    }
+
+    @Override
+    protected AJAXRequestResult perform(TokenLoginRequest request) throws OXException {
+        TokenLoginService service = getTokenLoginService();
+        ServerSession session = request.getSession();
+        String token = service.acquireToken(session);
+        
+        final JSONObject response = new JSONObject();
+        try {
+            response.put("token", token);
+        } catch (JSONException e) {
+            throw OXJSONExceptionCodes.JSON_WRITE_ERROR.create(e);
+        }
+
+        return new AJAXRequestResult(response, new Date(), "json");
+    }
+
+}
---
+++ com.openexchange.tokenlogin.json/src/com/openexchange/tokenlogin/json/actions/TokenLoginAction.java
@@ -0,0 +1,101 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2013 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.tokenlogin.json.actions;
+
+import org.apache.commons.logging.Log;
+import com.openexchange.ajax.requesthandler.AJAXActionService;
+import com.openexchange.ajax.requesthandler.AJAXRequestData;
+import com.openexchange.ajax.requesthandler.AJAXRequestResult;
+import com.openexchange.exception.OXException;
+import com.openexchange.server.ServiceExceptionCode;
+import com.openexchange.server.ServiceLookup;
+import com.openexchange.tokenlogin.TokenLoginService;
+import com.openexchange.tokenlogin.json.TokenLoginRequest;
+import com.openexchange.tools.session.ServerSession;
+
+
+/**
+ * {@link TokenLoginAction}
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ */
+public abstract class TokenLoginAction implements AJAXActionService {
+    
+    protected static final Log LOG = com.openexchange.log.Log.loggerFor(TokenLoginAction.class);
+    
+    private final ServiceLookup lookup;
+
+    /**
+     * Initializes a new {@link TokenLoginAction}.
+     */
+    public TokenLoginAction(ServiceLookup lookup) {
+        super();
+        this.lookup = lookup;
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.ajax.requesthandler.AJAXActionService#perform(com.openexchange.ajax.requesthandler.AJAXRequestData, com.openexchange.tools.session.ServerSession)
+     */
+    @Override
+    public AJAXRequestResult perform(AJAXRequestData requestData, ServerSession session) throws OXException {
+        return perform(new TokenLoginRequest(requestData, session));
+    }
+    
+    protected TokenLoginService getTokenLoginService() throws OXException {
+        try {
+            return lookup.getService(TokenLoginService.class);
+        } catch (IllegalStateException e) {
+            throw ServiceExceptionCode.SERVICE_UNAVAILABLE.create();
+        }
+    }
+    
+    protected abstract AJAXRequestResult perform(TokenLoginRequest request) throws OXException;
+
+}
