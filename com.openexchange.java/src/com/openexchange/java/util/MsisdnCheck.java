db31c8a21ce7397fe70529be039889d31d51b339
Thorben Betten <thorben.betten@open-xchange.com>
2013-06-25 12:27:55 +0200

    Fix for bug 27230: Don't validate non-rfc822 addresses

--- com.openexchange.java/src/com/openexchange/java/util/MsisdnCheck.java
+++ com.openexchange.java/src/com/openexchange/java/util/MsisdnCheck.java
@@ -49,11 +49,12 @@
 
 package com.openexchange.java.util;
 
+import java.util.regex.Pattern;
 import com.openexchange.java.Strings;
 
 /**
  * {@link MsisdnCheck} - Checks for valid MSISDN numbers.
- * 
+ *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
 public class MsisdnCheck {
@@ -65,11 +66,33 @@ public class MsisdnCheck {
         super();
     }
 
+    private static final Pattern PATTERN_VALIDATE = Pattern.compile("(^\\+[0-9]{2}|^\\+[0-9]{2}\\(0\\)|^\\(\\+[0-9]{2}\\)\\(0\\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\\-\\s]{10}$)");
+
     /**
-     * Checks if the given MSISDN number is valid
-     * 
+     * Validates specified MSISDN number.
+     * <p>
+     * Matches:
+     * <ul>
+     * <li><code>+31235256677</code></li>
+     * <li><code>+31(0)235256677</code></li>
+     * <li><code>023-5256677</code></li>
+     * </ul>
+     *
+     * @param number The possible MSISDN number to validate
+     * @return <code>true</code> if valid MSISDN number; otherwise <code>false</code>
+     */
+    public static boolean validate(final String number) {
+        if (isEmpty(number)) {
+            return false;
+        }
+        return PATTERN_VALIDATE.matcher(number).matches();
+    }
+
+    /**
+     * Checks if the given MSISDN number only consists of digits.
+     *
      * @param number the mobile phone number to check
-     * @return returns either "invalid" if the given number is not valid or the phone number in the format xxyyyzzzzzzzz
+     * @return returns either "invalid" if the given number is not valid (contains non-digits) or the phone number in the format xxyyyzzzzzzzz
      **/
     public static boolean checkMsisdn(final String number) {
         if (isEmpty(number)) {
@@ -97,8 +120,16 @@ public class MsisdnCheck {
         return isWhitespace;
     }
 
+    private static final Pattern PATTERN_CLEANUP = Pattern.compile("[+()/ ]");
+
+    /**
+     * Cleans-up specified MSISDN number.
+     *
+     * @param number The MSISDN number
+     * @return The cleaned-up MSISDN number
+     */
     public static String cleanup(String number) {
-        return number.replaceAll("[+()/ ]", "");
+        return PATTERN_CLEANUP.matcher(number).replaceAll("");
     }
 
 }
