691079133514e4e2a56382057f6254d98fae0e7f
Felix Marx <felix.marx@open-xchange.com>
2013-06-28 13:08:11 +0200

    initial jolokia implementation

---
+++ com.openexchange.jolokia/src/com/openexchange/jolokia/JolokiaConfig.java
@@ -0,0 +1,143 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.jolokia;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.commons.logging.Log;
+import com.openexchange.config.ConfigurationService;
+import com.openexchange.exception.OXException;
+import com.openexchange.jolokia.osgi.Services;
+import com.openexchange.server.Initialization;
+
+/**
+ * {@link JolokiaConfig} Collects and exposes configuration parameters needed by Jolokia
+ *
+ * @author <a href="mailto:felix.marx@open-xchange.com">Felix Marx</a>
+ */
+public class JolokiaConfig implements Initialization {
+
+    private static final Log LOG = com.openexchange.log.Log.loggerFor(JolokiaConfig.class);
+
+    private static final JolokiaConfig instance = new JolokiaConfig();
+
+    public static JolokiaConfig getInstance() {
+        return instance;
+    }
+
+    private final AtomicBoolean started = new AtomicBoolean();
+
+    // Jolokia properties
+    
+    /** boolean which will start jolokia or not*/
+    private boolean jolokiaStart = false;
+    
+    /** The Servlet Name, jolokia will try to get connected to */
+    private String jolokiaServletName = "/servlet/jolokia";
+
+
+    @Override
+    public void start() throws OXException {
+        if (!started.compareAndSet(false, true)) {
+            LOG.error(this.getClass().getName() + " already started");
+            return;
+        }
+        init();
+    }
+
+    @Override
+    public void stop() {
+        if (!started.compareAndSet(true, false)) {
+            LOG.error(this.getClass().getName() + " cannot be stopped since it has no been started before");
+            return;
+        }
+    }
+
+    private void init() throws OXException {
+        ConfigurationService configService = Services.getService(ConfigurationService.class);
+        if (configService == null) {
+            throw JolokiaExceptionCode.NEEDED_SERVICE_MISSING.create(ConfigurationService.class.getSimpleName());
+        }
+
+        // jolokia properties
+        this.jolokiaServletName = configService.getProperty("com.openexchange.jolokia.servlet.name", "/servlet/jolokia");
+        this.jolokiaStart = configService.getBoolProperty("com.openexchange.jolokia.start", false);
+
+    }
+
+    /**
+     * Gets the started
+     *
+     * @return The started
+     */
+    public AtomicBoolean getStarted() {
+        return started;
+    }
+
+    /**
+     * Gets the servletName
+     *
+     * @return The servlteName
+     */
+    public String getServletName() {
+        return jolokiaServletName;
+    }
+
+    /**
+     * Gets the boolean, if jolokia will be run or not
+     *
+     * @return true if jolokia will start and false if not
+     */
+    public boolean getJolokiaStart() {
+        return jolokiaStart;
+    }
+    
+}
