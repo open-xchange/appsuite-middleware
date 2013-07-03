4c4de89f1bbb7edeb2aa5b1a39d6e9c956a17fbb
Thorben Betten <thorben.betten@open-xchange.com>
2013-07-03 21:25:09 +0200

    typo

--- com.openexchange.capabilities/src/com/openexchange/capabilities/CapabilityFilter.java
+++ com.openexchange.capabilities/src/com/openexchange/capabilities/CapabilityFilter.java
@@ -50,7 +50,7 @@
 package com.openexchange.capabilities;
 
 /**
- * {@link CapabilityFilter} - A filter for abstract capabilities.
+ * {@link CapabilityFilter} - A filter for capabilities.
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
