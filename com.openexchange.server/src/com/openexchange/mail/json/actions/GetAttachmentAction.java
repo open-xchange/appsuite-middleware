9694487b88f5ceed25231a2f1d0a79e22c908bf3
Thorben Betten <thorben.betten@open-xchange.com>
2013-07-18 20:20:41 +0200

    Moved type to new file

--- com.openexchange.server/src/com/openexchange/mail/json/actions/GetAttachmentAction.java
+++ com.openexchange.server/src/com/openexchange/mail/json/actions/GetAttachmentAction.java
@@ -51,8 +51,6 @@ package com.openexchange.mail.json.actions;
 
 import static com.openexchange.mail.parser.MailMessageParser.generateFilename;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PushbackInputStream;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
@@ -60,9 +58,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import com.openexchange.ajax.AJAXServlet;
 import com.openexchange.ajax.Mail;
+import com.openexchange.ajax.container.ByteArrayInputStreamClosure;
 import com.openexchange.ajax.container.FileHolder;
 import com.openexchange.ajax.container.IFileHolder;
-import com.openexchange.ajax.container.ThresholdFileHolder;
 import com.openexchange.ajax.requesthandler.AJAXRequestData;
 import com.openexchange.ajax.requesthandler.AJAXRequestDataTools;
 import com.openexchange.ajax.requesthandler.AJAXRequestResult;
@@ -81,18 +79,14 @@ import com.openexchange.html.HtmlService;
 import com.openexchange.java.Charsets;
 import com.openexchange.java.Streams;
 import com.openexchange.java.StringAllocator;
-import com.openexchange.mail.FullnameArgument;
 import com.openexchange.mail.MailExceptionCode;
 import com.openexchange.mail.MailServletInterface;
-import com.openexchange.mail.api.IMailFolderStorage;
-import com.openexchange.mail.api.IMailMessageStorage;
-import com.openexchange.mail.api.MailAccess;
 import com.openexchange.mail.config.MailProperties;
 import com.openexchange.mail.dataobjects.MailPart;
 import com.openexchange.mail.json.MailRequest;
+import com.openexchange.mail.json.actions.helper.ReconnectingInputStreamClosure;
 import com.openexchange.mail.mime.ContentType;
 import com.openexchange.mail.mime.MimeTypes;
-import com.openexchange.mail.utils.MailFolderUtility;
 import com.openexchange.mail.utils.MessageUtility;
 import com.openexchange.server.ServiceLookup;
 import com.openexchange.server.services.ServerServiceRegistry;
@@ -114,63 +108,6 @@ import com.openexchange.tools.session.ServerSession;
     @Parameter(name = "filter", optional = true, description = "1 to apply HTML white-list filter rules if and only if requested attachment is of MIME type text/htm* AND parameter save is set to 0.") }, responseDescription = "The raw byte data of the document. The response type for the HTTP Request is set accordingly to the defined mimetype for this attachment, except the parameter save is set to 1.")
 @DispatcherNotes(allowPublicSession = true)
 public final class GetAttachmentAction extends AbstractMailAction implements ETagAwareAJAXActionService {
-
-    private static final class ReconnectingInputStreamClosure implements IFileHolder.InputStreamClosure {
-
-        private final ServerSession session;
-        private final String id;
-        private final String uid;
-        private final MailPart mailPart;
-        private final String folderPath;
-        private final boolean image;
-        private volatile ThresholdFileHolder tfh;
-
-        ReconnectingInputStreamClosure(MailPart mailPart, String folderPath, String uid, String id, boolean image, ServerSession session) {
-            super();
-            this.session = session;
-            this.id = id;
-            this.uid = uid;
-            this.mailPart = mailPart;
-            this.folderPath = folderPath;
-            this.image = image;
-        }
-
-        @Override
-        public InputStream newStream() throws OXException, IOException {
-            {
-                final ThresholdFileHolder tfh = this.tfh;
-                if (null != tfh) {
-                    return tfh.getStream();
-                }
-            }
-            try {
-                final PushbackInputStream in = new PushbackInputStream(mailPart.getInputStream());
-                final int check = in.read();
-                in.unread(check);
-                return in;
-            } catch (final com.sun.mail.util.FolderClosedIOException e) {
-                // Need to reconnect
-                final FullnameArgument fa = MailFolderUtility.prepareMailFolderParam(folderPath);
-                MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> ma = null;
-                try {
-                    ma = MailAccess.getInstance(session, fa.getAccountId());
-                    ma.connect(false);
-                    final ThresholdFileHolder newTfh = new ThresholdFileHolder();
-                    if (image) {
-                        newTfh.write(ma.getMessageStorage().getImageAttachment(fa.getFullName(), uid, id).getInputStream());
-                    } else {
-                        newTfh.write(ma.getMessageStorage().getAttachment(fa.getFullName(), uid, id).getInputStream());
-                    }
-                    this.tfh = newTfh;
-                    return newTfh.getStream();
-                } finally {
-                    if (null != ma) {
-                        ma.close(true);
-                    }
-                }
-            }
-        }
-    }
 
     private static final long EXPIRES_MILLIS_YEAR = AJAXRequestResult.YEAR_IN_MILLIS * 50;
     private static final String MIME_APPL_OCTET = MimeTypes.MIME_APPL_OCTET;
@@ -269,13 +206,7 @@ public final class GetAttachmentAction extends AbstractMailAction implements ETa
                         contentType.setCharsetParameter(cs);
                     }
                     size = bytes.length;
-                    isClosure = new IFileHolder.InputStreamClosure() {
-
-                        @Override
-                        public InputStream newStream() throws OXException, IOException {
-                            return Streams.newByteArrayInputStream(bytes);
-                        }
-                    };
+                    isClosure = new ByteArrayInputStreamClosure(bytes);
                 } else {
                     final boolean exactLength = AJAXRequestDataTools.parseBoolParameter(req.getParameter("exact_length"));
                     if (exactLength) {
---
+++ com.openexchange.server/src/com/openexchange/mail/json/actions/helper/ReconnectingInputStreamClosure.java
@@ -0,0 +1,129 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.mail.json.actions.helper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+import com.openexchange.ajax.container.IFileHolder;
+import com.openexchange.ajax.container.ThresholdFileHolder;
+import com.openexchange.exception.OXException;
+import com.openexchange.mail.FullnameArgument;
+import com.openexchange.mail.api.IMailFolderStorage;
+import com.openexchange.mail.api.IMailMessageStorage;
+import com.openexchange.mail.api.MailAccess;
+import com.openexchange.mail.dataobjects.MailPart;
+import com.openexchange.mail.utils.MailFolderUtility;
+import com.openexchange.tools.session.ServerSession;
+
+/**
+ * {@link ReconnectingInputStreamClosure}
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @since 7.4.0
+ */
+public final class ReconnectingInputStreamClosure implements IFileHolder.InputStreamClosure {
+
+    private final ServerSession session;
+    private final String id;
+    private final String uid;
+    private final MailPart mailPart;
+    private final String folderPath;
+    private final boolean image;
+    private volatile ThresholdFileHolder tfh;
+
+    public ReconnectingInputStreamClosure(MailPart mailPart, String folderPath, String uid, String id, boolean image, ServerSession session) {
+        super();
+        this.session = session;
+        this.id = id;
+        this.uid = uid;
+        this.mailPart = mailPart;
+        this.folderPath = folderPath;
+        this.image = image;
+    }
+
+    @Override
+    public InputStream newStream() throws OXException, IOException {
+        {
+            final ThresholdFileHolder tfh = this.tfh;
+            if (null != tfh) {
+                return tfh.getStream();
+            }
+        }
+        try {
+            final PushbackInputStream in = new PushbackInputStream(mailPart.getInputStream());
+            // Check if readable...
+            final int check = in.read();
+            // ... then push back to stream
+            in.unread(check);
+            return in;
+        } catch (final com.sun.mail.util.FolderClosedIOException e) {
+            // Need to reconnect
+            final FullnameArgument fa = MailFolderUtility.prepareMailFolderParam(folderPath);
+            MailAccess<? extends IMailFolderStorage, ? extends IMailMessageStorage> ma = null;
+            try {
+                ma = MailAccess.getInstance(session, fa.getAccountId());
+                ma.connect(false);
+                final ThresholdFileHolder newTfh = new ThresholdFileHolder();
+                if (image) {
+                    newTfh.write(ma.getMessageStorage().getImageAttachment(fa.getFullName(), uid, id).getInputStream());
+                } else {
+                    newTfh.write(ma.getMessageStorage().getAttachment(fa.getFullName(), uid, id).getInputStream());
+                }
+                this.tfh = newTfh;
+                return newTfh.getStream();
+            } finally {
+                if (null != ma) {
+                    ma.close(true);
+                }
+            }
+        }
+    }
+}
