c66547a67e3c0d9c961ec49437d346621c3074b1
Thorben Betten <thorben.betten@open-xchange.com>
2013-07-31 19:04:12 +0200

    Added missing call-back (cherry picked from commit 691f0c8d274f83786a31f33ba247040aff89b440)

--- com.openexchange.server/src/com/openexchange/mailaccount/Attribute.java
+++ com.openexchange.server/src/com/openexchange/mailaccount/Attribute.java
@@ -213,6 +213,10 @@ public enum Attribute {
             return switcher.pop3Path();
         case ADDRESSES:
             return switcher.addresses();
+        case ARCHIVE_FULLNAME_LITERAL:
+            return switcher.archiveFullname();
+        case ARCHIVE_LITERAL:
+            return switcher.archive();
         case META:
             // Ignore for virtual attribute
             return null;
