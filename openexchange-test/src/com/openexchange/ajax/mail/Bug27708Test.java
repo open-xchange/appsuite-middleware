016f30f3c121c3f36d038404e6e86751adec70f0
Marcus Klein <marcus.klein@open-xchange.com>
2013-08-02 17:18:17 +0200

    test for bug 27708

--- openexchange-test/src/com/openexchange/ajax/mail/Bug27708Test.java
+++ openexchange-test/src/com/openexchange/ajax/mail/Bug27708Test.java
@@ -49,150 +49,191 @@
 
 package com.openexchange.ajax.mail;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import com.openexchange.ajax.framework.AJAXClient;
 import com.openexchange.ajax.framework.AJAXClient.User;
-import com.openexchange.ajax.framework.AJAXSession;
-import com.openexchange.ajax.framework.Executor;
+import com.openexchange.ajax.framework.MultipleRequest;
+import com.openexchange.ajax.mail.actions.AllRequest;
+import com.openexchange.ajax.mail.actions.AllResponse;
+import com.openexchange.ajax.mail.actions.DeleteRequest;
 import com.openexchange.ajax.mail.actions.GetRequest;
 import com.openexchange.ajax.mail.actions.GetResponse;
 import com.openexchange.ajax.mail.actions.SendRequest;
 import com.openexchange.ajax.mail.actions.SendResponse;
 import com.openexchange.mail.MailJSONField;
 import com.openexchange.mail.dataobjects.MailMessage;
-
+import com.openexchange.tools.arrays.Arrays;
 
 /**
- * {@link Bug27708Test}
+ * Verifies that mixing mail content when sending an email did not occur anymore.
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @author <a href="mailto:tobias.friedrich@open-xchange.com">Tobias Friedrich</a>
+ * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
  */
 public final class Bug27708Test extends AbstractMailTest {
 
-    private AJAXClient client2;
+    private static final int NUM_THREADS = 4;
 
-    /**
-     * Default constructor.
-     *
-     * @param name Name of this test.
-     */
+    private AJAXClient[] clients;
+    private String recipient;
+    private String[] identifier;
+    private List<String[]>[] sentMails;
+
     public Bug27708Test(final String name) {
         super(name);
     }
 
+    @BeforeClass
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        client2 = new AJAXClient(User.User2);
+        clients = new AJAXClient[NUM_THREADS];
+        clients[0] = getClient();
+        clients[1] = new AJAXClient(User.User2);
+        clients[2] = new AJAXClient(User.User3);
+        clients[3] = new AJAXClient(User.User4);
+        recipient = getClient().getValues().getSendAddress();
+        identifier = new String[NUM_THREADS];
+        for (int i = 0; i < identifier.length; i++) {
+            identifier[i] = UUID.randomUUID().toString();
+        }
+        sentMails = new List[NUM_THREADS];
+        for (int i = 0; i < sentMails.length; i++) {
+            sentMails[i] = new LinkedList<String[]>();
+        }
     }
 
+    @AfterClass
     @Override
     protected void tearDown() throws Exception {
-        AJAXClient thisClient2 = client2;
-        if (null != thisClient2) {
-            thisClient2.logout();
+        for (int i = 0; i < clients.length; i++) {
+            DeleteRequest[] requests = new DeleteRequest[sentMails[i].size()];
+            for (int j = 0; j < sentMails[i].size(); j++) {
+                requests[j] = new DeleteRequest(sentMails[i].get(j), true);
+            }
+            clients[i].execute(new MultipleRequest(requests));
+        }
+        // Delete received mails.
+        String inboxFolder = getClient().getValues().getInboxFolder();
+        AllRequest request = new AllRequest(inboxFolder, new int[] { 600 }, -1, null, true);
+        AllResponse response = getClient().execute(request);
+        DeleteRequest[] delete = new DeleteRequest[response.size()];
+        for (int i = 0; i < response.size(); i++) {
+            delete[i] = new DeleteRequest(inboxFolder, (String) response.getValue(i, 600), true);
+        }
+        getClient().execute(new MultipleRequest(delete));
+        // delete mails
+        if (null != clients) {
+            for (AJAXClient client : clients) {
+                if (null != client) client.logout();
+            }
+            clients = null;
         }
         super.tearDown();
     }
 
+    @Test
     public void testBug27708() throws Exception {
-
-
-
-        for (int i = 0; i < 100; i++) {
-            final CountDownLatch startUpLatch = new CountDownLatch(1);
-            final CountDownLatch finishedLatch = new CountDownLatch(2);
-            {
-                final AJAXClient client = this.client;
-                final Runnable r1 = new Runnable() {
-
-                    @Override
-                    public void run() {
-                        try {
-                            String sendAddress = client.getValues().getSendAddress();
-                            final JSONObject jMail =
-                                new JSONObject(
-                                    "{\"from\":\""+sendAddress+"\"," + "\"to\":\""+sendAddress+"\"," + "\"cc\":\"\",\"bcc\":\"\"," + "\"subject\":\"AAAA\"," + "\"priority\":\"3\"," + "\"attachments\":" + "[" + "{\"content_type\":\"ALTERNATIVE\",\"content\":\"<html><body style=\\u0022\\u0022><div>AAAA AAAA AAAA AAAA AAAA</div>\\u000a<div>AAAA AAAA AAAA AAAA AAAA</div>\\u000a<div>AAAA AAAA AAAA AAAA AAAA</div>\\u000a<div>AAAAAAAA AAAA AAAA AAAA</div>\\u000a<div>AAAA AAAA AAAA AAAA AAAA</div>\\u000a<div>AAAA AAAA AAAA AAAA AAAA</div>\\u000a<div>AAAA AAAA AAAA AAAA AAAA</div>\\u000a<div>AAAA AAAA AAAA AAAA AAAA</div></body></html>\"}" + "]," + "\"datasources\":[]}");
-
-                            final String mailObject_25kb = jMail.toString();
-                            final SendRequest sendRequest = new SendRequest(mailObject_25kb);
-                            AJAXSession session = client.getSession();
-
-                            startUpLatch.await();
-
-                            final SendResponse response = Executor.execute(session, sendRequest);
-                            String[] folderAndID = response.getFolderAndID();
-
-                            final GetResponse getResponse =
-                                Executor.execute(session, new GetRequest(folderAndID[0], folderAndID[1], true, true));
-                            final JSONObject obj = (JSONObject) getResponse.getData();
-
-                            assertFalse("Contains unexpected content", obj.hasAndNotNull("error"));
-
-                            String retrievedContent = obj.getJSONArray("attachments").getJSONObject(0).getString("content");
-
-                            assertTrue("Contains unexpected content", retrievedContent.indexOf("BBBB") < 0);
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            fail(e.getMessage());
-                        } finally {
-                            finishedLatch.countDown();
-                        }
-                    }
-                };
-                new Thread(r1).start();
-            }
-            {
-                final AJAXClient client = this.client2;
-                final Runnable r2 = new Runnable() {
-
-                    @Override
-                    public void run() {
-                        try {
-                            String sendAddress = client.getValues().getSendAddress();
-                            final JSONObject jMail =
-                                new JSONObject(
-                                    "{\"from\":\""+sendAddress+"\"," + "\"to\":\""+sendAddress+"\"," + "\"cc\":\"\",\"bcc\":\"\"," + "\"subject\":\"BBBB\"," + "\"priority\":\"3\"," + "\"attachments\":" + "[" + "{\"content_type\":\"ALTERNATIVE\",\"content\":\"<html><body style=\\u0022\\u0022><div>BBBB BBBB BBBB BBBB BBBB</div>\\u000a<div>BBBB BBBB BBBB BBBB BBBB</div>\\u000a<div>BBBB BBBB BBBB BBBB BBBB</div>\\u000a<div>BBBBBBBB BBBB BBBB BBBB</div>\\u000a<div>BBBB BBBB BBBB BBBB BBBB</div>\\u000a<div>BBBB BBBB BBBB BBBB BBBB</div>\\u000a<div>BBBB BBBB BBBB BBBB BBBB</div>\\u000a<div>BBBB BBBB BBBB BBBB BBBB</div></body></html>\"}" + "]," + "\"datasources\":[]}");
-
-                            jMail.put(MailJSONField.FLAGS.getKey(), MailMessage.FLAG_DRAFT);
-
-                            final String mailObject_25kb = jMail.toString();
-                            final SendRequest sendRequest = new SendRequest(mailObject_25kb);
-                            AJAXSession session = client.getSession();
-
-                            startUpLatch.await();
-
-                            final SendResponse response = Executor.execute(session, sendRequest);
-                            String[] folderAndID = response.getFolderAndID();
-
-                            final GetResponse getResponse =
-                                Executor.execute(session, new GetRequest(folderAndID[0], folderAndID[1], true, true));
-                            final JSONObject obj = (JSONObject) getResponse.getData();
-
-                            assertFalse("Contains unexpected content", obj.hasAndNotNull("error"));
-
-                            String retrievedContent = obj.getJSONArray("attachments").getJSONObject(0).getString("content");
-
-                            assertTrue("Contains unexpected content", retrievedContent.indexOf("AAAA") < 0);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            fail(e.getMessage());
-                        } finally {
-                            finishedLatch.countDown();
-                        }
-                    }
-                };
-                new Thread(r2).start();
-            }
-            // Await initialization
-            Thread.sleep(2000);
-            // Start threads
-            startUpLatch.countDown();
-            // Await completion
-            finishedLatch.await();
+        Thread[] threads = new Thread[clients.length];
+        MailSender[] senders = new MailSender[clients.length];
+        for (int i = 0; i < clients.length; i++) {
+            senders[i] = new MailSender(clients[i], recipient, identifier[i], Arrays.remove(identifier, identifier[i]), sentMails[i]);
+            threads[i] = new Thread(senders[i]);
+            threads[i].start();
         }
+        int running = 0;
+        do {
+            running = 0;
+            for (int i = 0; i < threads.length; i++) {
+                if (threads[i].isAlive()) {
+                    running++;
+                }
+            }
+            Thread.sleep(100);
+        } while (running > 1);
+        for (MailSender sender : senders) {
+            sender.stop();
+        }
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+        StringBuilder sb = new StringBuilder();
+        for (MailSender sender : senders) {
+            Throwable t = sender.getThrowable();
+            if (null != t) {
+                sb.append(t.getMessage());
+                sb.append('\n');
+            }
+        }
+        Assert.assertTrue(sb.toString(), 0 == sb.length());
     }
 
+    private static final class MailSender implements Runnable {
+
+        private static final boolean DRAFT = false;
+
+        private final AJAXClient client;
+        private final String recipient;
+        private final String identifier;
+        private final String[] others;
+        private final List<String[]> mails;
+
+        private boolean running = true;
+        private Throwable throwable;
+
+
+        MailSender(AJAXClient client, String recipient, String identifier, String[] others, List<String[]> mails) {
+            super();
+            this.recipient = recipient;
+            this.identifier = identifier;
+            this.client = client;
+            this.others = others;
+            this.mails = mails;
+        }
+
+        public void stop() {
+            running = false;
+        }
+
+        public Throwable getThrowable() {
+            return throwable;
+        }
+
+        @Override
+        public void run() {
+            try {
+                for (int j = 0; j < 100000 && running; j++) {
+                    JSONObject json = createEMail(client, recipient, "Test Bug 27708", "TEXT/HTML", "<html><body style=\\u0022\\u0022><div>" + identifier + "</div></body></html>");
+                    if (DRAFT) {
+                        json.put(MailJSONField.FLAGS.getKey(), MailMessage.FLAG_DRAFT);
+                    }
+                    SendRequest sendRequest = new SendRequest(json.toString());
+                    SendResponse response = client.execute(sendRequest);
+                    String[] folderAndID = response.getFolderAndID();
+                    mails.add(folderAndID);
+                    GetResponse getResponse = client.execute(new GetRequest(folderAndID[0], folderAndID[1]));
+                    JSONArray attachments = getResponse.getAttachments();
+                    JSONObject tmp = attachments.getJSONObject(0);
+                    String body = tmp.getString(MailJSONField.CONTENT.getKey());
+                    Assert.assertTrue("Sent mail does not contain senders mail body.\n" + body, body.contains(identifier));
+                    for (String other : others) {
+                        Assert.assertFalse("Sent mail does contain others mail body.\n" + body, body.contains(other));
+                    }
+                }
+            } catch (Exception e) {
+                throwable = e;
+            } catch (AssertionError e) {
+                throwable = e;
+            }
+        }
+    }
 }
