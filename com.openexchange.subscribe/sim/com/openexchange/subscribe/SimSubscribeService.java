204e28261c875b2199b48642dc95d35076590c31
Thorben Betten <thorben.betten@open-xchange.com>
2013-08-07 15:07:24 +0200

    fixed compile error

--- com.openexchange.subscribe/sim/com/openexchange/subscribe/SimSubscribeService.java
+++ com.openexchange.subscribe/sim/com/openexchange/subscribe/SimSubscribeService.java
@@ -176,11 +176,6 @@ public class SimSubscribeService implements SubscribeService {
     }
     
     @Override
-    public void removeUnrecoverableItems(String secret, Session session) throws OXException {
-        // Ignore        
-    }
-    
-    @Override
     public void touch(Context ctx, int subscriptionId) throws OXException {
         
     }
