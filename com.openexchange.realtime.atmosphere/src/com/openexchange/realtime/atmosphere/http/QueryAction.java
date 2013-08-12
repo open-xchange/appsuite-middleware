2144e1ac635d69df0956374b586af1810662c927
Francisco Laguna <francisco.laguna@open-xchange.com>
2013-08-12 16:17:07 +0200

    first shot at bug 28186

--- com.openexchange.realtime.atmosphere/src/com/openexchange/realtime/atmosphere/http/QueryAction.java
+++ com.openexchange.realtime.atmosphere/src/com/openexchange/realtime/atmosphere/http/QueryAction.java
@@ -188,6 +188,10 @@ public class QueryAction extends RTAction {
             LOG.error(e);
             RealtimeException re = RealtimeExceptionCodes.STANZA_INTERNAL_SERVER_ERROR.create(e, e.getMessage());
             throw re;
+        } catch (Throwable e) {
+            LOG.error(e);
+            RealtimeException re = RealtimeExceptionCodes.STANZA_INTERNAL_SERVER_ERROR.create(e, e.getMessage());
+            throw re;
         } finally {
             sendLock.unlock();
         }
