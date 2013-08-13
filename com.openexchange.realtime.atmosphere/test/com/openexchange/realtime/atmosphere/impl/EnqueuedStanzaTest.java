b36f04348f54a4130ab362dad10ef50a186508ab
Francisco Laguna <francisco.laguna@open-xchange.com>
2013-08-13 11:24:57 +0200

    Revert "fix failing test"
    This reverts commit e38bd4f55a848e922e5679c9cbc34d3951841223.

--- com.openexchange.realtime.atmosphere/test/com/openexchange/realtime/atmosphere/impl/EnqueuedStanzaTest.java
+++ com.openexchange.realtime.atmosphere/test/com/openexchange/realtime/atmosphere/impl/EnqueuedStanzaTest.java
@@ -66,7 +66,7 @@ public class EnqueuedStanzaTest {
         message.setSequenceNumber(23);
         
         EnqueuedStanza enqueuedStanza = new EnqueuedStanza(message);
-        for(long i = 0; i < Integer.MAX_VALUE + 1; i++) {
+        for(int i = 0; i < 101; i++) {
             if (i < 100) {
                 assertTrue(enqueuedStanza.incCounter());
             } else {
