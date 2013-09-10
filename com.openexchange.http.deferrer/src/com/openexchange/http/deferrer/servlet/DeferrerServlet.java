7a1e3e040d8c52d60140dc3bda77a49cecf44390
Thorben Betten <thorben.betten@open-xchange.com>
2013-09-10 12:00:48 +0200

    Fix for bug 28538: Sanitize redirect URL when appearing in deferring servlet instance (cherry picked from commit daa36f24ea89360e3e00128ea90ea821648f4915)

--- com.openexchange.http.deferrer/src/com/openexchange/http/deferrer/servlet/DeferrerServlet.java
+++ com.openexchange.http.deferrer/src/com/openexchange/http/deferrer/servlet/DeferrerServlet.java
@@ -49,22 +49,16 @@
 
 package com.openexchange.http.deferrer.servlet;
 
+import static com.openexchange.ajax.AJAXServlet.encodeUrl;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.BitSet;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.codec.net.URLCodec;
-import org.apache.commons.logging.Log;
 import com.openexchange.http.deferrer.CustomRedirectURLDetermination;
-import com.openexchange.java.Charsets;
 
 /**
  * {@link DeferrerServlet}
@@ -73,130 +67,21 @@ import com.openexchange.java.Charsets;
  */
 public class DeferrerServlet extends HttpServlet {
 
-    private static final Log LOG = com.openexchange.log.Log.loggerFor(DeferrerServlet.class);
+    private static final long serialVersionUID = 1358634554782437089L;
 
     /**
-     * BitSet of www-form-url safe characters.
+     * The listing for custom handlers.
      */
-    protected static final BitSet WWW_FORM_URL;
-
-    /**
-     * BitSet of www-form-url safe characters including safe characters for an anchor.
-     */
-    protected static final BitSet WWW_FORM_URL_ANCHOR;
-
-    // Static initializer for www_form_url
-    static {
-        {
-            final BitSet bitSet = new BitSet(256);
-            // alpha characters
-            for (int i = 'a'; i <= 'z'; i++) {
-                bitSet.set(i);
-            }
-            for (int i = 'A'; i <= 'Z'; i++) {
-                bitSet.set(i);
-            }
-            // numeric characters
-            for (int i = '0'; i <= '9'; i++) {
-                bitSet.set(i);
-            }
-            // special chars
-            bitSet.set('-');
-            bitSet.set('_');
-            bitSet.set('.');
-            bitSet.set('*');
-            // blank to be replaced with +
-            bitSet.set(' ');
-            WWW_FORM_URL = bitSet;
-        }
-        {
-            final BitSet bitSet = new BitSet(256);
-            // alpha characters
-            for (int i = 'a'; i <= 'z'; i++) {
-                bitSet.set(i);
-            }
-            for (int i = 'A'; i <= 'Z'; i++) {
-                bitSet.set(i);
-            }
-            // numeric characters
-            for (int i = '0'; i <= '9'; i++) {
-                bitSet.set(i);
-            }
-            // special chars
-            bitSet.set('-');
-            bitSet.set('_');
-            bitSet.set('.');
-            bitSet.set('*');
-            // blank to be replaced with +
-            bitSet.set(' ');
-            // Anchor characters
-            bitSet.set('/');
-            bitSet.set('#');
-            bitSet.set('%');
-            bitSet.set('?');
-            bitSet.set('&');
-            WWW_FORM_URL_ANCHOR = bitSet;
-        }
-    }
-
-    private static final Pattern PATTERN_CRLF = Pattern.compile("\r?\n|(?:%0[aA])?%0[dD]");
-    private static final Pattern PATTERN_DSLASH = Pattern.compile("(?:/|%2[fF]){2}");
-
-    /**
-     * URL encodes given string.
-     * <p>
-     * Using <code>org.apache.commons.codec.net.URLCodec</code>.
-     */
-    private String encodeUrl(final String s, final boolean forAnchor) {
-        if (isEmpty(s)) {
-            return s;
-        }
-        try {
-            final String ascii;
-            if (forAnchor) {
-                // Prepare for being used as anchor/link
-                ascii = Charsets.toAsciiString(URLCodec.encodeUrl(WWW_FORM_URL_ANCHOR, s.getBytes(Charsets.ISO_8859_1)));
-            } else {
-                ascii = Charsets.toAsciiString(URLCodec.encodeUrl(WWW_FORM_URL, s.getBytes(Charsets.ISO_8859_1)));
-            }
-            // Strip possible "\r?\n" and/or "%0A?%0D"
-            String retval = PATTERN_CRLF.matcher(ascii).replaceAll("");
-            // Check for a relative URI
-            try {
-                final java.net.URI uri = new java.net.URI(retval);
-                if (uri.isAbsolute() || null != uri.getScheme() || null != uri.getHost()) {
-                    throw new IllegalArgumentException("Illegal Location value: " + s);
-                }
-            } catch (final URISyntaxException e) {
-                throw new IllegalArgumentException("Illegal Location value: " + s, e);
-            }
-            // Replace double slashes with single one
-            {
-                Matcher matcher = PATTERN_DSLASH.matcher(retval);
-                while (matcher.find()) {
-                    retval = matcher.replaceAll("/");
-                    matcher = PATTERN_DSLASH.matcher(retval);
-                }
-            }
-            return retval;
-        } catch (final IllegalArgumentException e) {
-            throw e;
-        } catch (final RuntimeException e) {
-            LOG.error("A runtime error occurred.", e);
-            return s;
-        }
-    }
-
     public static final List<CustomRedirectURLDetermination> CUSTOM_HANDLERS = new CopyOnWriteArrayList<CustomRedirectURLDetermination>();
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // create a new HttpSession if it's missing
+    protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        // Create a new HttpSession if it's missing
         req.getSession(true);
 
-        // get url to defer to
-        // redirect
-        String redirectURL = determineRedirectURL(req);
+        // Get the URL to defer to
+        // Redirect
+        final String redirectURL = determineRedirectURL(req);
         if (redirectURL == null) {
             return;
         }
@@ -204,26 +89,25 @@ public class DeferrerServlet extends HttpServlet {
         if (redirectURL.indexOf('?') >= 0) {
             concat = '&';
         }
-        Enumeration parameterNames = req.getParameterNames();
-        StringBuilder builder = new StringBuilder(redirectURL);
 
-        while (parameterNames.hasMoreElements()) {
-            String name = (String) parameterNames.nextElement();
-            if (name.equals("redirect")) {
+        final StringBuilder builder = new StringBuilder(encodeUrl(redirectURL, false, false));
+        for (final Enumeration<?> parameterNames = req.getParameterNames(); parameterNames.hasMoreElements();) {
+            final String name = (String) parameterNames.nextElement();
+            if ("redirect".equals(name)) {
                 continue;
             }
-            String parameter = req.getParameter(name);
+            final String parameter = req.getParameter(name);
             builder.append(concat);
             concat = '&';
-            builder.append(name).append('=').append(encodeUrl(parameter, true));
+            builder.append(name).append('=').append(encodeUrl(parameter, true, true));
         }
         resp.sendRedirect(builder.toString());
 
     }
 
-    private String determineRedirectURL(HttpServletRequest req) {
-        for (CustomRedirectURLDetermination determination : CUSTOM_HANDLERS) {
-            String url = determination.getURL(req);
+    private String determineRedirectURL(final HttpServletRequest req) {
+        for (final CustomRedirectURLDetermination determination : CUSTOM_HANDLERS) {
+            final String url = determination.getURL(req);
             if (url != null) {
                 return url;
             }
@@ -231,15 +115,4 @@ public class DeferrerServlet extends HttpServlet {
         return req.getParameter("redirect");
     }
 
-    private boolean isEmpty(final String string) {
-        if (null == string) {
-            return true;
-        }
-        final int len = string.length();
-        boolean isWhitespace = true;
-        for (int i = 0; isWhitespace && i < len; i++) {
-            isWhitespace = com.openexchange.java.Strings.isWhitespace(string.charAt(i));
-        }
-        return isWhitespace;
-    }
 }
