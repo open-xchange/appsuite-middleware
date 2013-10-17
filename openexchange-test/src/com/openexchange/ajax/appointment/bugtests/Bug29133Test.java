30fc7e6a0f8c3aa47394d4a4a4c3825f5355f9be
Martin Herfurth <martin.herfurth@open-xchange.com>
2013-10-17 12:13:35 +0200

    Fix for bug 29133: Special series pattern lead to reset of confirmation status.

---
+++ openexchange-test/src/com/openexchange/ajax/appointment/bugtests/Bug29133Test.java
@@ -0,0 +1,90 @@
+
+package com.openexchange.ajax.appointment.bugtests;
+
+import static com.openexchange.groupware.calendar.TimeTools.D;
+import java.util.Date;
+import org.junit.Test;
+import com.openexchange.ajax.framework.AJAXClient;
+import com.openexchange.ajax.framework.AJAXClient.User;
+import com.openexchange.ajax.framework.AbstractAJAXSession;
+import com.openexchange.groupware.container.Appointment;
+import com.openexchange.groupware.container.Participant;
+import com.openexchange.groupware.container.UserParticipant;
+import com.openexchange.test.CalendarTestManager;
+
+/**
+ * {@link Bug29133Test}
+ * 
+ * @author <a href="mailto:martin.herfurth@open-xchange.com">Martin Herfurth</a>
+ */
+public class Bug29133Test extends AbstractAJAXSession {
+
+    private CalendarTestManager ctmA;
+
+    private CalendarTestManager ctmB;
+
+    private Appointment appointmentA;
+
+    private Appointment appointmentB;
+
+    private AJAXClient clientA;
+
+    private AJAXClient clientB;
+
+    public Bug29133Test(String name) {
+        super(name);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        clientA = getClient();
+        clientB = new AJAXClient(User.User2);
+        ctmA = new CalendarTestManager(clientA);
+        ctmB = new CalendarTestManager(clientB);
+
+        appointmentA = new Appointment();
+        appointmentA.setTitle("Bug 29133 Test");
+        appointmentA.setStartDate(D("14.10.2013 08:00"));
+        appointmentA.setEndDate(D("14.10.2013 09:00"));
+        appointmentA.setRecurrenceType(Appointment.WEEKLY);
+        appointmentA.setDays(Appointment.TUESDAY);
+        appointmentA.setInterval(1);
+        appointmentA.setIgnoreConflicts(true);
+        appointmentA.setParentFolderID(clientA.getValues().getPrivateAppointmentFolder());
+        appointmentA.setUsers(new UserParticipant[] {
+            new UserParticipant(clientA.getValues().getUserId()), new UserParticipant(clientB.getValues().getUserId()) });
+        appointmentA.setParticipants(new Participant[] {
+            new UserParticipant(clientA.getValues().getUserId()), new UserParticipant(clientB.getValues().getUserId()) });
+
+        ctmA.insert(appointmentA);
+        appointmentB = ctmA.createIdentifyingCopy(appointmentA);
+        appointmentB.setParentFolderID(clientB.getValues().getPrivateAppointmentFolder());
+    }
+
+    @Test
+    public void testBug29133() throws Exception {
+        ctmB.confirm(appointmentB, Appointment.ACCEPT, "message");
+        checkStatus();
+        ctmA.confirm(appointmentA, Appointment.ACCEPT, "message");
+        checkStatus();
+        Appointment update = ctmA.createIdentifyingCopy(appointmentA);
+        update.setAlarm(30);
+        update.setLastModified(new Date(Long.MAX_VALUE));
+        ctmA.update(update);
+        checkStatus();
+    }
+
+    private void checkStatus() throws Exception {
+        Appointment load = ctmA.get(appointmentA);
+        assertEquals("Wrong amount of users.", 2, load.getUsers().length);
+        assertEquals("Wrong confirm status.", Appointment.ACCEPT, load.getUsers()[0].getConfirm());
+        assertEquals("Wrong confirm status.", Appointment.ACCEPT, load.getUsers()[1].getConfirm());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        ctmA.cleanUp();
+        super.tearDown();
+    }
+}
