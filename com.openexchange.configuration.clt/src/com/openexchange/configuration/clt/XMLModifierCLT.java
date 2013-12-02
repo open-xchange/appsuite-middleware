ed7a6172bd4ebda9a470baa5f47661819b7eee59
Marcus Klein <marcus.klein@open-xchange.com>
2013-12-02 16:20:27 +0100

    Created CLT for modifying XML configuration files.

---
+++ com.openexchange.configuration.clt/src/com/openexchange/configuration/clt/XMLModifierCLT.java
@@ -0,0 +1,193 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2013 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.configuration.clt;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+/**
+ * {@link XMLModifierCLT} is a command line tool to maintain XML configuration files. Currently it only allows to add new XML fragments at
+ * defined positions in the existing XML configuration file.
+ *
+ * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
+ */
+public class XMLModifierCLT {
+
+    private static final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    private static final TransformerFactory tf = TransformerFactory.newInstance();
+    private static final XPathFactory xf = XPathFactory.newInstance();
+
+    public XMLModifierCLT() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        System.exit(configureXML(args));
+    }
+
+    private static int configureXML(String[] args) {
+        Options options = new Options();
+        options.addOption(createOption("h", "help", false, "Prints a help text.", false));
+        options.addOption(createOption("i", "in", true, "XML document is read from this file.", true));
+        options.addOption(createOption("o", "out", true, "Modified XML document is written to this file.", true));
+        options.addOption(createOption("x", "xpath", true, "XPath to the element that should be modified.", true));
+        options.addOption(createOption("a", "add", true, "XML file that should be added to the element denotes by the XPath.", false));
+        CommandLineParser parser = new PosixParser();
+        final CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args, true);
+        } catch (ParseException e) {
+            System.out.println("Parsing the command line failed: " + e.getMessage());
+            return 1;
+        }
+        if (cmd.hasOption('h')) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("xmlModifierCLT", "Can modify XML configuration files. Currently only allows to add XML fragments.", options, null, false);
+            return 0;
+        }
+        final DocumentBuilder db;
+        try {
+            db = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            System.out.println("Can not configure XML parser: " + e.getMessage());
+            e.printStackTrace();
+            return 1;
+        }
+        final Transformer transformer;
+        try {
+            transformer = tf.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            System.out.println("Can not configure XML writer: " + e.getMessage());
+            e.printStackTrace();
+            return 1;
+        }
+        try {
+            File input = new File(cmd.getOptionValue('i'));
+            if (!input.exists() || !input.isFile() || !input.canRead()) {
+                System.out.println("Can not open input file: \"" + input.getAbsolutePath() + "\".");
+                return 1;
+            }
+            File output = new File(cmd.getOptionValue('o'));
+            if (!output.canWrite()) {
+                System.out.println("Can not write to output file: \"" + output.getAbsolutePath() + "\".");
+                return 1;
+            }
+            Document document = db.parse(input);
+            // Modify
+            XPath path = xf.newXPath();
+            XPathExpression expression = path.compile(cmd.getOptionValue('x'));
+            if (cmd.hasOption('a')) {
+                File add = new File(cmd.getOptionValue('a'));
+                if (!add.exists() || !add.isFile() || !add.canRead()) {
+                    System.out.println("Can not open XML fragment to add: \"" + add.getAbsolutePath() + "\".");
+                    return 1;
+                }
+                Element element2Add = db.parse(add).getDocumentElement();
+                Node imported = document.importNode(element2Add, true);
+                Node element = (Node) expression.evaluate(document, XPathConstants.NODE);
+                element.appendChild(imported);
+            } else {
+                // Nothing to do.
+            }
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.getOutputProperties().store(System.out, "Properties");
+            transformer.transform(new DOMSource(document), new StreamResult(output));
+        } catch (SAXException e) {
+            System.out.println("Can not parse XML document: " + e.getMessage());
+            e.printStackTrace();
+            return 1;
+        } catch (XPathExpressionException e) {
+            System.out.println("Can not parse XPath expression: " + e.getMessage());
+            e.printStackTrace();
+            return 1;
+        } catch (TransformerException e) {
+            System.out.println("Can not write XML document" + e.getMessage());
+            e.printStackTrace();
+            return 1;
+        } catch (IOException e) {
+            System.out.println("Can not read XML file: " + e.getMessage());
+            e.printStackTrace();
+            return 1;
+        }
+        return 0;
+    }
+
+    private static Option createOption(String shortArg, String longArg, boolean hasArg, String description, boolean required) {
+        Option option = new Option(shortArg, longArg, hasArg, description);
+        option.setRequired(required);
+        return option;
+    }
+}
