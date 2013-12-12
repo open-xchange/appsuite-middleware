fce2217a82cb8c74d285c8035d52429716835152
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2013-12-12 17:37:58 +0100

    removed c.o.nosql* and c.o.eav*

--- com.openexchange.nosql.cassandra.hector/src/me/prettyprint/cassandra/service/template/MappedColumnFamilyResult.java
+++
@@ -1,7 +0,0 @@
-package me.prettyprint.cassandra.service.template;
-
-public interface MappedColumnFamilyResult<K,N,V> extends ColumnFamilyResult<K, N> {
-
-  V getRow();
-  
-}
--- com.openexchange.nosql.cassandra.hector/src/me/prettyprint/cassandra/service/template/MappedSuperCfResult.java
+++
@@ -1,6 +0,0 @@
-package me.prettyprint.cassandra.service.template;
-
-public interface MappedSuperCfResult<K, SN, N, V> extends SuperCfResult<K, SN, N> {
-  
-  V getRow();
-}
