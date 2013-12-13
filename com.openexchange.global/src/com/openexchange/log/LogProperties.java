18d91c59b6ac6e0d2b93744ec9ede5f3b5673445
Thorben Betten <thorben.betten@open-xchange.com>
2013-12-13 16:08:32 +0100

    Fix for bug 30305: Added null guard for values passed to MDC map

--- com.openexchange.global/src/com/openexchange/log/LogProperties.java
+++ com.openexchange.global/src/com/openexchange/log/LogProperties.java
@@ -391,7 +391,10 @@ public final class LogProperties {
         }
         for (int i = 0; i < length; i+=2) {
             final LogProperties.Name name = (LogProperties.Name) args[i];
-            MDC.put(name.getName(), String.valueOf(args[i + 1]));
+            final Object arg = args[i + 1];
+            if (null != arg) {
+                MDC.put(name.getName(), arg.toString());
+            }
         }
     }
 
