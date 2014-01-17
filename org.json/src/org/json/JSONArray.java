b1b9f050b6b8cd24cb5e58b6d835e785e88923a7
Thorben Betten <thorben.betten@open-xchange.com>
2014-01-17 12:57:10 +0100

    Introduced "org.json.JSONValue.isEqualTo(JSONValue)"

--- org.json/src/org/json/JSONArray.java
+++ org.json/src/org/json/JSONArray.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -244,6 +245,35 @@ public class JSONArray extends AbstractJSONValue {
                 }
             }
         }
+    }
+
+    @Override
+    public boolean isEqualTo(final JSONValue jsonValue) {
+        if (jsonValue == this) {
+            return true;
+        }
+        if ((null == jsonValue) || !jsonValue.isArray()) {
+            return false;
+        }
+        final List<Object> l = jsonValue.toArray().myArrayList;
+        final ListIterator<Object> e1 = myArrayList.listIterator();
+        final ListIterator<Object> e2 = l.listIterator();
+        while (e1.hasNext() && e2.hasNext()) {
+            final Object o1 = e1.next();
+            final Object o2 = e2.next();
+            if (o1 instanceof JSONValue) {
+                if (o2 instanceof JSONValue) {
+                    if (!((JSONValue) o1).isEqualTo((JSONValue) o2)) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else if (!(isNull(o1) ? isNull(o2) : o1.equals(o2))) {
+                return false;
+            }
+        }
+        return !(e1.hasNext() || e2.hasNext());
     }
 
     /**
