9f662ca01db42bf5b90072b830de755f2097aaa3
Thorben Betten <thorben.betten@open-xchange.com>
2014-01-19 18:13:15 +0100

    removed unused improt

--- org.apache.felix.eventadmin/src/org/apache/felix/eventadmin/impl/tasks/AsyncDeliverTasks.java
+++ org.apache.felix.eventadmin/src/org/apache/felix/eventadmin/impl/tasks/AsyncDeliverTasks.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import org.apache.felix.eventadmin.impl.handler.EventHandlerProxy;
 import org.osgi.service.event.Event;
