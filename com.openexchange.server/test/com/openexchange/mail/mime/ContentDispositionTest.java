d1fc3b233c158ae14a89c2aef1d3952f4884a600
Thorben Betten <thorben.betten@open-xchange.com>
2014-01-21 16:52:11 +0100

    Fix for bug 30680: Support for several bogus "Content-Disposition" header values

--- com.openexchange.server/test/com/openexchange/mail/mime/ContentDispositionTest.java
+++ com.openexchange.server/test/com/openexchange/mail/mime/ContentDispositionTest.java
@@ -28,7 +28,7 @@
  *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
  *    given Attribution for the derivative code and a license granting use.
  *
- *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Copyright (C) 2004-2020 Open-Xchange, Inc.
  *     Mail: info@open-xchange.com
  *
  *
@@ -47,50 +47,42 @@
  *
  */
 
-package com.openexchange.server;
+package com.openexchange.mail.mime;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import junit.framework.TestCase;
+
 
 /**
- * {@link UnitTests}
+ * {@link ContentDispositionTest}
  *
- * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
-@RunWith(Suite.class)
-@SuiteClasses({
-    com.openexchange.ajax.ProcessUploadStaticTest.class,
-    com.openexchange.ajax.parser.ContactSearchtermSqlConverterTest.class,
-    com.openexchange.ajax.parser.TaskLastModifiedTest.class,
-    com.openexchange.ajax.LoginAddFragmentTest.class,
-    com.openexchange.groupware.ldap.UserAttributeDiffTest.class,
-    com.openexchange.i18n.tools.replacement.TaskEndDateReplacementTest.class,
-    com.openexchange.tools.collections.OXCollectionsTest.class,
-    com.openexchange.tools.iterator.SearchIteratorDelegatorTest.class,
-    com.openexchange.tools.net.URIParserTest.class,
-    com.openexchange.mail.utils.MsisdnUtilityTest.class,
-    com.openexchange.groupware.update.tasks.MakeFolderIdPrimaryForDelContactsTableTest.class,
-    com.openexchange.ajax.MailAttachmentTest.class,
-    com.openexchange.ajax.requesthandler.responseRenderers.FileResponseRendererTest.class,
-    com.openexchange.groupware.userconfiguration.AllowAllUserConfigurationTest.class,
-    com.openexchange.groupware.userconfiguration.UserConfigurationTest.class,
-    com.openexchange.mail.mime.ContentDispositionTest.class,
-    com.openexchange.mail.mime.MimeStructureFixerTest.class,
-    com.openexchange.mail.mime.MimeSmilFixerTest.class,
-    com.openexchange.groupware.notify.ParticipantNotifyTest.class,
-    com.openexchange.mail.json.actions.GetAttachmentActionTest.class,
-    com.openexchange.ajax.requesthandler.converters.preview.cache.FileStoreResourceCacheImplTest.class,
-    com.openexchange.server.services.SharedInfostoreJSlobTest.class,
-    com.openexchange.groupware.upload.quotachecker.MailUploadQuotaCheckerTest.class
-})
-public class UnitTests {
+public class ContentDispositionTest extends TestCase {
 
     /**
-     * Initializes a new {@link UnitTests}.
+     * Initializes a new {@link ContentDispositionTest}.
      */
-    public UnitTests() {
+    public ContentDispositionTest() {
         super();
+    }
+
+    public void testFileNameParameterFromBogusHeaders() {
+        try {
+            String hdr = "attachment; filename=Jana's application 4 cell phone.rtf";
+            String fileName = new com.openexchange.mail.mime.ContentDisposition(hdr).getFilenameParameter();
+            assertEquals("Unpexpected \"filename\" parameter.", "Jana's application 4 cell phone.rtf", fileName);
+
+            hdr = "attachment; filename*0=Test - Test.pdf";
+            fileName = new com.openexchange.mail.mime.ContentDisposition(hdr).getFilenameParameter();
+            assertEquals("Unpexpected \"filename\" parameter.", "Test - Test.pdf", fileName);
+
+            hdr = "filename=\"Scan1852.pdf\"; filename=\"Scan1852.pdf\"";
+            fileName = new com.openexchange.mail.mime.ContentDisposition(hdr).getFilenameParameter();
+            assertEquals("Unpexpected \"filename\" parameter.", "Scan1852.pdf", fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
     }
 
 }
