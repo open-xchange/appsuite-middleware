6d4adcb8295dc8ca4bdf09d9074246aa881b89dc
Thorben Betten <thorben.betten@open-xchange.com>
2014-01-22 22:22:47 +0100

    Updated copyright information: "Copyright (C) 2004-2012 Open-Xchange, Inc." -> "Copyright (C) 2004-2014 Open-Xchange, Inc."

--- com.openexchange.loxandra.util/src/com/openexchange/loxandra/util/NetUtils.java
+++ com.openexchange.loxandra.util/src/com/openexchange/loxandra/util/NetUtils.java
@@ -35,7 +35,7 @@ import java.util.Enumeration;
  *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
  *    given Attribution for the derivative code and a license granting use.
  *
- *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
  *     Mail: info@open-xchange.com
  *
  *
--- com.openexchange.loxandra.util/src/com/openexchange/loxandra/util/TokenUtils.java
+++ com.openexchange.loxandra.util/src/com/openexchange/loxandra/util/TokenUtils.java
@@ -37,7 +37,7 @@ import org.apache.commons.logging.LogFactory;
  *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
  *    given Attribution for the derivative code and a license granting use.
  *
- *     Copyright (C) 2004-2012 Open-Xchange, Inc.
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
  *     Mail: info@open-xchange.com
  *
  *
