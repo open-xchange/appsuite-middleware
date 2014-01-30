4f1226b33309a0de2ea9f634a886b130e07d5e04
Lars Hoogestraat <lars.hoogestraat@open-xchange.com>
2014-01-30 09:49:28 +0100

    added example service implementation of get action

---
+++ com.openexchange.mobilenotifier.example/src/com/openexchange/mobilenotifier/example/ExampleMailMobileNotifierService.java
@@ -0,0 +1,108 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.mobilenotifier.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.openexchange.exception.OXException;
+import com.openexchange.mobilenotifier.MobileNotifierService;
+import com.openexchange.mobilenotifier.NotifyItem;
+import com.openexchange.mobilenotifier.NotifyTemplate;
+
+/**
+ * {@link ExampleMailMobileNotifierService} - Example mail implementation of a mobile notifier service
+ * 
+ * @author <a href="mailto:lars.hoogestraat@open-xchange.com">Lars Hoogestraat</a>
+ */
+public class ExampleMailMobileNotifierService implements MobileNotifierService {
+
+    public ExampleMailMobileNotifierService() {
+        super();
+    }
+
+    @Override
+    public String getProviderName() {
+        return "mail";
+    }
+
+    @Override
+    public boolean isEnabled(int uid, int cid) {
+        return true;
+    }
+
+    @Override
+    public List<NotifyItem> getItems(int uid, int cid) throws OXException {
+        // TODO clarification: How to get the key items? (via read on templates?)
+        List<NotifyItem> list = new ArrayList<NotifyItem>();
+        NotifyItem ni = new NotifyItem();
+        ni.setKey("{{subject}}");
+        ni.setValue("Dies ist ein Betreff");
+        list.add(ni);
+        ni = new NotifyItem();
+        ni.setKey("{{receiveDate}}");
+        ni.setValue("12.04.2013 - 12:45:00");
+        list.add(ni);
+        ni = new NotifyItem();
+        ni.setKey("{{userId}}");
+        ni.setValue("1");
+        list.add(ni);
+        return list;
+    }
+
+    @Override
+    public NotifyTemplate getTemplate() throws OXException {
+        // read on template
+        return null;
+    }
+
+    @Override
+    public void putTemplate() throws OXException {
+    }
+}
