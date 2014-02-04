a30dda2c95e4a45df5e9bf26b4ac6685ba251ed0
Thorben Betten <thorben.betten@open-xchange.com>
2014-02-04 17:35:55 +0100

    Fixed logging

--- com.openexchange.imap/src/com/openexchange/imap/IMAPDefaultFolderChecker.java
+++ com.openexchange.imap/src/com/openexchange/imap/IMAPDefaultFolderChecker.java
@@ -303,9 +303,12 @@ public class IMAPDefaultFolderChecker {
                 } else {
                     if (fullName.indexOf(sep) > 0 || !fullName.equals(names[i])) {
                         // E.g. name=Sent, but fullName=INBOX/Sent or fullName=Zent
-                        LOG.warn("Found invalid full name in settings of account {}. Should be \"{}\", but is \"{}\" (user={}, context={})", Integer.valueOf(accountId), namespace + names[i], fullName, Integer.valueOf(session.getUserId()), Integer.valueOf(session.getContextId()));
+                        final String expectedFullName = namespace + names[i];
+                        if (expectedFullName.equals(fullName)) {
+                            LOG.warn("Found invalid full name in settings of account {}. Should be \"{}\", but is \"{}\" (user={}, context={})", Integer.valueOf(accountId), expectedFullName, fullName, Integer.valueOf(session.getUserId()), Integer.valueOf(session.getContextId()));
+                            indexes.add(i);
+                        }
                         fullNames[i] = null;
-                        indexes.add(i);
                     }
                 }
             }
