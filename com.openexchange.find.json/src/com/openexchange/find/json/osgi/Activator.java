e24397502b7d5d986f477213396b14d9d6332fac
Steffen Templin <steffen.templin@open-xchange.com>
2014-02-04 18:40:05 +0100

    Created com.openexchange.find and com.openexchange.find.json

---
+++ com.openexchange.find.json/src/com/openexchange/find/json/osgi/Activator.java
@@ -0,0 +1,18 @@
+package com.openexchange.find.json.osgi;
+
+import com.openexchange.ajax.requesthandler.osgiservice.AJAXModuleActivator;
+
+public class Activator extends AJAXModuleActivator {
+
+    @Override
+    protected Class<?>[] getNeededServices() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected void startBundle() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
