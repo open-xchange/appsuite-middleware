cd2faab4cfee105ad183b5dd1bc4184f39e7d083
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-02-25 11:38:17 +0100

    Fix for Bug 30773 - NullPointerException at com.openexchange.spamsettings.generic.osgi.Activator.stop()

--- com.openexchange.spamsettings.generic/src/com/openexchange/spamsettings/generic/osgi/ServletActivator.java
+++ com.openexchange.spamsettings.generic/src/com/openexchange/spamsettings/generic/osgi/ServletActivator.java
@@ -77,19 +77,23 @@ public class ServletActivator extends DeferredActivator {
 
     @Override
     protected void handleAvailability(final Class<?> clazz) {
-        LOG.warn("Absent service: {}", clazz.getName());
-
-        SpamSettingsServiceRegistry.getServiceRegistry().addService(clazz, getService(clazz));
-        servletRegisterer.registerServlet();
-        SpamSettingsModulePreferences.setModule(true);
+        if (servletRegisterer != null) {
+            LOG.warn("Absent service: {}", clazz.getName());
+    
+            SpamSettingsServiceRegistry.getServiceRegistry().addService(clazz, getService(clazz));
+            servletRegisterer.registerServlet();
+            SpamSettingsModulePreferences.setModule(true);
+        }
     }
 
     @Override
     protected void handleUnavailability(final Class<?> clazz) {
-        LOG.info("Re-available service: {}", clazz.getName());
-        servletRegisterer.unregisterServlet();
-        SpamSettingsModulePreferences.setModule(false);
-        SpamSettingsServiceRegistry.getServiceRegistry().removeService(clazz);
+        if (servletRegisterer != null) {
+            LOG.info("Re-available service: {}", clazz.getName());
+            servletRegisterer.unregisterServlet();
+            SpamSettingsModulePreferences.setModule(false);
+            SpamSettingsServiceRegistry.getServiceRegistry().removeService(clazz);
+        }
 
     }
 
@@ -111,10 +115,12 @@ public class ServletActivator extends DeferredActivator {
 
     @Override
     protected void stopBundle() throws Exception {
-        servletRegisterer.unregisterServlet();
-        servletRegisterer = null;
-        SpamSettingsModulePreferences.setModule(false);
-        SpamSettingsServiceRegistry.getServiceRegistry().clearRegistry();
-        SpamSettingsServletRegisterer.PREFIX.set(null);
+        if (servletRegisterer != null) {
+            servletRegisterer.unregisterServlet();
+            servletRegisterer = null;
+            SpamSettingsModulePreferences.setModule(false);
+            SpamSettingsServiceRegistry.getServiceRegistry().clearRegistry();
+            SpamSettingsServletRegisterer.PREFIX.set(null);
+        }
     }
 }
