52ab90f00b58c9064440ea95172cf63a7a936f31
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-02-28 17:41:28 +0100

    added helper method with failOnError argument

--- openexchange-test/src/com/openexchange/ajax/folder/Create.java
+++ openexchange-test/src/com/openexchange/ajax/folder/Create.java
@@ -120,9 +120,25 @@ public final class Create {
     public static FolderObject createPublicFolder(final AJAXClient client,
         final String name, final int module) throws OXException, IOException,
         SAXException, JSONException {
+        return createPublicFolder(client, name, module, true);
+    }
+    
+    /**
+     * This method creates a public folder
+     * @param client
+     * @param name
+     * @param module the module (e.g. CONTACT) from FolderObject.java
+     * @param failOnError whether the request will fail if the server returns an error
+     * @return
+     * @throws OXException
+     * @throws IOException
+     * @throws SAXException
+     * @throws JSONException
+     */
+    public static FolderObject createPublicFolder(final AJAXClient client, final String name, final int module, boolean failOnError) throws OXException, IOException, SAXException, JSONException {
         final FolderObject folder = setupPublicFolder(name, module, client.getValues().getUserId());
         folder.setParentFolderID(FolderObject.SYSTEM_PUBLIC_FOLDER_ID);
-        final InsertRequest request = new InsertRequest(EnumAPI.OX_OLD, folder);
+        final InsertRequest request = new InsertRequest(EnumAPI.OX_OLD, folder, failOnError);
         final InsertResponse response = client.execute(request);
         response.fillObject(folder);
         return folder;
