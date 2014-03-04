902f3a4212fcc0caf71af7b8a68fd0f026f39832
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2014-03-04 07:52:16 +0100

    consider limit for auto complete results, added auto complete for resources

--- com.openexchange.find.basic/src/com/openexchange/find/basic/AbstractContactFacetingModuleSearchDriver.java
+++ com.openexchange.find.basic/src/com/openexchange/find/basic/AbstractContactFacetingModuleSearchDriver.java
@@ -48,9 +48,12 @@ package com.openexchange.find.basic;
  *
  */
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import com.openexchange.contact.SortOptions;
+import com.openexchange.contact.SortOrder;
 import com.openexchange.exception.OXException;
 import com.openexchange.find.AutocompleteRequest;
 import com.openexchange.find.spi.AbstractModuleSearchDriver;
@@ -59,6 +62,8 @@ import com.openexchange.groupware.contact.helpers.UseCountComparator;
 import com.openexchange.groupware.container.Contact;
 import com.openexchange.groupware.container.FolderObject;
 import com.openexchange.groupware.search.ContactSearchObject;
+import com.openexchange.groupware.search.Order;
+import com.openexchange.resource.Resource;
 import com.openexchange.tools.iterator.SearchIterator;
 import com.openexchange.tools.iterator.SearchIteratorAdapter;
 import com.openexchange.tools.iterator.SearchIterators;
@@ -82,9 +87,16 @@ public abstract class AbstractContactFacetingModuleSearchDriver extends Abstract
         ContactField.MARK_AS_DISTRIBUTIONLIST, ContactField.IMAGE1_URL, ContactField.CELLULAR_TELEPHONE1, ContactField.CELLULAR_TELEPHONE2 };
 
     /**
-     * The maximum number of autocomplete search results.
+     * The default sort order used to get pre-sorted results when retrieving contacts for auto-completion.
      */
-    protected static final int MAX_RESULTS = 10;
+    private static final SortOrder[] SORT_ORDER = new SortOrder[] {
+        new SortOrder(ContactField.FOLDER_ID, Order.ASCENDING), new SortOrder(ContactField.USE_COUNT, Order.DESCENDING)
+    };
+
+    /**
+     * The default maximum number of returned autocomplete search results.
+     */
+    private static final int DEFAULT_LIMIT = 10;
 
     /**
      * Initializes a new {@link AbstractContactFacetingModuleSearchDriver}.
@@ -102,7 +114,7 @@ public abstract class AbstractContactFacetingModuleSearchDriver extends Abstract
      * @throws OXException If auto-complete search fails for any reason
      */
     protected List<Contact> autocompleteContacts(ServerSession session, AutocompleteRequest autocompleteRequest) throws OXException {
-        return searchContacts(session, autocompleteRequest.getPrefix(), true, null);
+        return searchContacts(session, autocompleteRequest.getPrefix(), true, null, autocompleteRequest.getLimit());
     }
 
     /**
@@ -115,7 +127,44 @@ public abstract class AbstractContactFacetingModuleSearchDriver extends Abstract
      */
     protected List<Contact> autocompleteUsers(ServerSession session, AutocompleteRequest autocompleteRequest) throws OXException {
         return searchContacts(session, autocompleteRequest.getPrefix(), false,
-            Collections.singletonList(String.valueOf(FolderObject.SYSTEM_LDAP_FOLDER_ID)));
+            Collections.singletonList(String.valueOf(FolderObject.SYSTEM_LDAP_FOLDER_ID)), autocompleteRequest.getLimit());
+    }
+
+    /**
+     * Performs the resources auto-complete search.
+     *
+     * @param session The session associated with this auto-complete request
+     * @param autocompleteRequest The auto-complete request
+     * @return The resulting resources
+     * @throws OXException If auto-complete search fails for any reason
+     */
+    protected List<Resource> autocompleteResources(ServerSession session, AutocompleteRequest autocompleteRequest) throws OXException {
+        Resource[] resources = Services.getResourceService().searchResources(autocompleteRequest.getPrefix(), session.getContext());
+        if (null == resources) {
+            return Collections.emptyList();
+        }
+        List<Resource> resourceList = Arrays.asList(resources);
+        if (1 < resourceList.size()) {
+            Collections.sort(resourceList, new Comparator<Resource>() {
+
+                @Override
+                public int compare(Resource o1, Resource o2) {
+                    String name1 = null != o1 ? o1.getDisplayName() : null;
+                    String name2 = null != o2 ? o2.getDisplayName() : null;
+                    if (null == name1) {
+                        return null == name2 ? 0 : -1;
+                    } else if (null == name2) {
+                        return 1;
+                    } else {
+                        return name1.compareTo(name2);
+                    }
+                }
+            });
+            if (autocompleteRequest.getLimit() > resourceList.size()) {
+                resourceList.subList(0, autocompleteRequest.getLimit()).clear();
+            }
+        }
+        return resourceList;
     }
 
     /**
@@ -129,18 +178,26 @@ public abstract class AbstractContactFacetingModuleSearchDriver extends Abstract
      * @return A list of found contacts, sorted using the {@link UseCountComparator} comparator
      * @throws OXException
      */
-    private List<Contact> searchContacts(ServerSession session, String prefix, boolean requireEmail, List<String> folderIDs) throws OXException {
+    private List<Contact> searchContacts(ServerSession session, String prefix, boolean requireEmail, List<String> folderIDs, int limit) throws OXException {
+        SortOptions sortOptions = new SortOptions(SORT_ORDER);
+        sortOptions.setLimit(0 < limit ? limit : DEFAULT_LIMIT);
         List<Contact> contacts = null;
         SearchIterator<Contact> searchIterator = null;
         try {
             searchIterator = Services.getContactService().searchContacts(
-                session, getSearchObject(prefix, requireEmail, folderIDs), CONTACT_FIELDS, new SortOptions(0, MAX_RESULTS));
+                session, getSearchObject(prefix, requireEmail, folderIDs), CONTACT_FIELDS, sortOptions);
             contacts = SearchIteratorAdapter.toList(searchIterator);
         } finally {
             SearchIterators.close(searchIterator);
         }
-        if (null != contacts && 1 < contacts.size()) {
+        if (null == contacts) {
+            return Collections.emptyList();
+        }
+        if (1 < contacts.size()) {
             Collections.sort(contacts, new UseCountComparator(true, session.getUser().getLocale()));
+            if (0 < sortOptions.getLimit() && contacts.size() > sortOptions.getLimit()) {
+                contacts.subList(0, sortOptions.getLimit()).clear();
+            }
         }
         return contacts;
     }
