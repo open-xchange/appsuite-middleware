9868b05e41829bcd84f1a387a2d6fa176fcf43cd
Thorben Betten <thorben.betten@open-xchange.com>
2014-03-07 10:30:40 +0100

    comment

--- com.openexchange.oauth.facebook/src/com/openexchange/oauth/facebook/OAuthServiceMetaDataFacebookImpl.java
+++ com.openexchange.oauth.facebook/src/com/openexchange/oauth/facebook/OAuthServiceMetaDataFacebookImpl.java
@@ -118,6 +118,7 @@ public class OAuthServiceMetaDataFacebookImpl extends AbstractOAuthServiceMetaDa
 
     @Override
     public String getScope() {
+        // See https://developers.facebook.com/docs/facebook-login/permissions/#
         // TODO: "xmpp_login" for RTC chat access
         return "offline_access,publish_stream,read_stream,status_update,user_about_me,friends_about_me," +
         		"user_activities,friends_activities,user_birthday,friends_birthday,user_education_history," +
