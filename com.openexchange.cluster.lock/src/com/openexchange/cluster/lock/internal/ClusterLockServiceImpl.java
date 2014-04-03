b225131b096500c91320f33612582fa46c90def3
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-04-03 14:22:22 +0200

    Fix for Bug 31798: partial fix, cluster lock via hazelcast

---
+++ com.openexchange.cluster.lock/src/com/openexchange/cluster/lock/internal/ClusterLockServiceImpl.java
@@ -0,0 +1,115 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.cluster.lock.internal;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import com.hazelcast.core.HazelcastInstance;
+import com.openexchange.cluster.lock.ClusterLockService;
+import com.openexchange.exception.OXException;
+
+/**
+ * {@link ClusterLockServiceImpl}
+ * 
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class ClusterLockServiceImpl implements ClusterLockService {
+
+    private final HazelcastInstance hazelcastInstance;
+
+    private final ReentrantLock reentrantLock = new ReentrantLock();
+
+    /**
+     * Initializes a new {@link ClusterLockServiceImpl}.
+     */
+    public ClusterLockServiceImpl(HazelcastInstance hazelcastInstance) {
+        super();
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.cluster.lock.ClusterLock#acquireClusterLock()
+     */
+    @Override
+    public Lock acquireClusterLock(String action) throws OXException {
+        // TODO
+        return hazelcastInstance.getLock(action);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.cluster.lock.ClusterLockService#acquirePeriodicClusterLock(java.lang.String, java.lang.Long)
+     */
+    @Override
+    public Lock acquirePeriodicClusterLock(String action, Long period) throws OXException {
+        Lock lock = null;
+        try {
+            reentrantLock.tryLock(5, TimeUnit.SECONDS);
+            Long now = System.currentTimeMillis();
+            ConcurrentMap<String, Long> map = hazelcastInstance.getMap("ClusterLocks");
+            Long timestamp = map.get(action);
+            if (timestamp != null) {
+                if (now - timestamp < period) {
+                    throw ClusterLockExceptionCodes.CLUSTER_PERIODIC_LOCKED.create(period, action, period - (now - timestamp));
+                }
+            }
+            lock = hazelcastInstance.getLock(action);
+            map.put(action, now);
+        } catch (InterruptedException e) {
+            throw ClusterLockExceptionCodes.TIMEOUT.create();
+        } finally {
+            reentrantLock.unlock();
+        }
+        return lock;
+    }
+}
---
+++ com.openexchange.cluster.lock/src/com/openexchange/cluster/lock/osgi/ClusterLockActivator.java
@@ -0,0 +1,115 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.cluster.lock.osgi;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.hazelcast.core.HazelcastInstance;
+import com.openexchange.cluster.lock.ClusterLockService;
+import com.openexchange.cluster.lock.internal.ClusterLockServiceImpl;
+import com.openexchange.hazelcast.configuration.HazelcastConfigurationService;
+import com.openexchange.osgi.HousekeepingActivator;
+
+/**
+ * {@link ClusterLockActivator}
+ * 
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class ClusterLockActivator extends HousekeepingActivator {
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.osgi.DeferredActivator#getNeededServices()
+     */
+    @Override
+    protected Class<?>[] getNeededServices() {
+        return new Class<?>[] { HazelcastConfigurationService.class };
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.osgi.DeferredActivator#startBundle()
+     */
+    @Override
+    protected void startBundle() throws Exception {
+        final Logger LOG = LoggerFactory.getLogger(ClusterLockActivator.class);
+        
+        final HazelcastConfigurationService hzConfigService = getService(HazelcastConfigurationService.class);
+        final boolean enabled = hzConfigService.isEnabled();
+        
+        if (false == enabled) {
+            LOG.warn("{} will be disabled due to disabled Hazelcast services", context.getBundle().getSymbolicName());
+        } else {
+            track(HazelcastInstance.class, new ServiceTrackerCustomizer<HazelcastInstance, HazelcastInstance>() {
+
+                @Override
+                public HazelcastInstance addingService(ServiceReference<HazelcastInstance> ref) {
+                    HazelcastInstance hzInstance = context.getService(ref);
+                    registerService(ClusterLockService.class, new ClusterLockServiceImpl(hzInstance));
+                    return hzInstance;
+                }
+
+                @Override
+                public void modifiedService(ServiceReference<HazelcastInstance> ref, HazelcastInstance srv) {
+                    // nothing
+                }
+
+                @Override
+                public void removedService(ServiceReference<HazelcastInstance> ref, HazelcastInstance srv) {
+                    unregisterService(ClusterLockService.class);
+                    context.ungetService(ref);
+                }
+            });
+            openTrackers();
+        }
+    }
+}
