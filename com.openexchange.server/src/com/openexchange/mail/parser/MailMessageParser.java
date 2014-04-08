390bd26b896bb210c0e8a68aba74ec72d124daa7
Thorben Betten <thorben.betten@open-xchange.com>
2014-04-08 11:58:54 +0200

    Change for bug 31911: Enhanced logging in case a "MSG-0006" error is thrown (content of invalid multipart is printed to log file)

--- com.openexchange.server/src/com/openexchange/mail/parser/MailMessageParser.java
+++ com.openexchange.server/src/com/openexchange/mail/parser/MailMessageParser.java
@@ -312,11 +312,15 @@ public final class MailMessageParser {
         } catch (final IOException e) {
             final String mailId = mail.getMailId();
             final String folder = mail.getFolder();
-            throw
-                MailExceptionCode.UNREADBALE_PART_CONTENT.create(
-                e,
-                null == mailId ? "" : mailId,
-                null == folder ? "" : folder);
+            throw MailExceptionCode.UNREADBALE_PART_CONTENT.create(e, null == mailId ? "" : mailId, null == folder ? "" : folder);
+        } catch (final OXException e) {
+            if (MailExceptionCode.INVALID_MULTIPART_CONTENT.equals(e)) {
+                // Strange multipart...
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Invalid multipart detected ''{}'':{}{}", e.getMessage(), System.getProperty("line.separator"), mail.getSource());
+                }
+            }
+            throw e;
         } finally {
             LogProperties.removeProperty(LogProperties.Name.MAIL_ACCOUNT_ID);
             LogProperties.removeProperty(LogProperties.Name.MAIL_MAIL_ID);
