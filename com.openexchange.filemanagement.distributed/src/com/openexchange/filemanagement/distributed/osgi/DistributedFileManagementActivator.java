7212a64fd4a3cb6470a5d45fc11b6db27ee713e2
Martin Herfurth <martin.herfurth@open-xchange.com>
2014-04-28 14:10:31 +0200

    Fix for bug 32059: Added port to connection string for distributed files.

--- com.openexchange.filemanagement.distributed/src/com/openexchange/filemanagement/distributed/osgi/DistributedFileManagementActivator.java
+++ com.openexchange.filemanagement.distributed/src/com/openexchange/filemanagement/distributed/osgi/DistributedFileManagementActivator.java
@@ -36,6 +36,7 @@ public class DistributedFileManagementActivator extends HousekeepingActivator {
 
         HazelcastConfigurationService hazelcastConfig = getService(HazelcastConfigurationService.class);
         final String prefix = getService(DispatcherPrefixService.class).getPrefix();
+        final int port = getService(ConfigurationService.class).getIntProperty("com.openexchange.connector.networkListenerPort", 8009);
         final ServiceLookup services = this;
         if (hazelcastConfig.isEnabled()) {
             track(HazelcastInstance.class, new SimpleRegistryListener<HazelcastInstance>() {
@@ -45,7 +46,7 @@ public class DistributedFileManagementActivator extends HousekeepingActivator {
                     DistributedFileManagementImpl.setHazelcastInstance(service);
                     String address = service.getCluster().getLocalMember().getInetSocketAddress().getHostName();
                     String mapName = discoverMapName(service.getConfig());
-                    address += prefix;
+                    address = address + ":" + port + prefix;
                     registerService(DistributedFileManagement.class, new DistributedFileManagementImpl(services, address, mapName));
                 }
 
