5d0ac9e754addf89280083f70b908f68b965feda
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-05-08 12:25:29 +0200

    removed c.o.push.udp remnants

--- openexchange-test/common/com/openexchange/groupware/Init.java
+++ openexchange-test/common/com/openexchange/groupware/Init.java
@@ -151,7 +151,6 @@ import com.openexchange.mailaccount.MailAccountStorageService;
 import com.openexchange.mailaccount.UnifiedInboxManagement;
 import com.openexchange.mailaccount.internal.MailAccountStorageInit;
 import com.openexchange.osgi.ServiceRegistry;
-import com.openexchange.push.udp.registry.PushServiceRegistry;
 import com.openexchange.quota.Quota;
 import com.openexchange.quota.QuotaService;
 import com.openexchange.quota.Resource;
@@ -843,7 +842,6 @@ public final class Init {
                 }
             });
             TestServiceRegistry.getInstance().addService(EventAdmin.class, TestEventAdmin.getInstance());
-            PushServiceRegistry.getServiceRegistry().addService(EventAdmin.class, TestEventAdmin.getInstance());
         }
     }
 
