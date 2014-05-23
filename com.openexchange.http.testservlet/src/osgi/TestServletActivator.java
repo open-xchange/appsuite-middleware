82a4c83f64a1f8572e2e6cf684710b81d6cf75d0
Martin Schneider <martin.schneider@open-xchange.com>
2014-05-23 09:08:09 +0200

    Use constants to prevent path differences (e. g. upper vs. lowercase)

--- com.openexchange.http.testservlet/src/osgi/TestServletActivator.java
+++ com.openexchange.http.testservlet/src/osgi/TestServletActivator.java
@@ -15,6 +15,21 @@ import com.openexchange.server.ServiceExceptionCode;
  */
 public class TestServletActivator extends HousekeepingActivator {
 
+    /**
+     * Path to diagnostic
+     */
+    private static final String STATS_DIAGNOSTIC = "/stats/diagnostic";
+
+    /**
+     * Path to Ping
+     */
+    private static final String SERVLET_PING = "/servlet/Ping";
+
+    /**
+     * Path to TestServlet
+     */
+    private static final String SERVLET_TEST_SERVLET = "/servlet/TestServlet";
+
     @Override
     protected Class<?>[] getNeededServices() {
         return new Class[] { HttpService.class };
@@ -26,18 +41,18 @@ public class TestServletActivator extends HousekeepingActivator {
         if (service == null) {
             throw ServiceExceptionCode.SERVICE_UNAVAILABLE.create(HttpService.class.getName());
         }
-        service.registerServlet("/servlet/TestServlet", new TestServlet(), null, null);
-        service.registerServlet("/servlet/Ping", new PingServlet(), null, null);
-        service.registerServlet("/stats/diagnostic", new DiagnosticServlet(), null, null);
+        service.registerServlet(SERVLET_TEST_SERVLET, new TestServlet(), null, null);
+        service.registerServlet(SERVLET_PING, new PingServlet(), null, null);
+        service.registerServlet(STATS_DIAGNOSTIC, new DiagnosticServlet(), null, null);
     }
 
     @Override
     protected void stopBundle() throws Exception {
         HttpService service = getService(HttpService.class);
         if (service != null) {
-            service.unregister("/servlet/TestServlet");
-            service.unregister("/servlet/Ping");
-            service.unregister("/stats/Diagnostic");
+            service.unregister(SERVLET_TEST_SERVLET);
+            service.unregister(SERVLET_PING);
+            service.unregister(STATS_DIAGNOSTIC);
         }
         super.stopBundle();
     }
