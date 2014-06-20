0df4b2bc2e5ff4ad6fa7d52460d3e73ea835ef90
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2014-06-20 07:54:53 +0200

    - Fix for bug 32323: Introduced "acknowledge" parameter for EDIT actions to prevent implicit client-side acknowledgments of unknown files (cherry picked from commit 2d80e7cc8cf85c63882f47a9416a9615a9676b87)
    Conflicts:
    	com.openexchange.server/ChangeLog

--- com.openexchange.drive/src/com/openexchange/drive/DriveAction.java
+++ com.openexchange.drive/src/com/openexchange/drive/DriveAction.java
@@ -73,6 +73,7 @@ public interface DriveAction<T extends DriveVersion> extends Comparable<DriveAct
     static final String PARAMETER_RESET = "reset";
     static final String PARAMETER_LENGTH = "length";
     static final String PARAMETER_STOP = "stop";
+    static final String PARAMETER_ACKNOWLEDGE = "acknowledge";
 
     static final String PARAMETER_DIRECT_LINK = "directLink";
     static final String PARAMETER_DIRECT_LINK_FRAGMENTS = "directLinkFragments";
@@ -81,7 +82,7 @@ public interface DriveAction<T extends DriveVersion> extends Comparable<DriveAct
 
     static final Set<String> PARAMETER_NAMES = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(new String[] {
         PARAMETER_PATH, PARAMETER_TOTAL_LENGTH, PARAMETER_OFFSET, PARAMETER_CONTENT_TYPE, PARAMETER_ERROR, PARAMETER_QUARANTINE,
-        PARAMETER_MODIFIED, PARAMETER_CREATED, PARAMETER_RESET, PARAMETER_LENGTH, PARAMETER_STOP,
+        PARAMETER_MODIFIED, PARAMETER_CREATED, PARAMETER_RESET, PARAMETER_LENGTH, PARAMETER_STOP, PARAMETER_ACKNOWLEDGE,
         PARAMETER_DIRECT_LINK, PARAMETER_DIRECT_LINK_FRAGMENTS, PARAMETER_PREVIEW_LINK, PARAMETER_THUMBNAIL_LINK
     })));
 
