2193b3cb4416ff694fc9dc39681ac598e1bc6cf9
Felix Marx <felix.marx@open-xchange.com>
2014-07-01 10:42:45 +0200

    fix test after Assert changed

--- openexchange-test/src/com/openexchange/ajax/contact/Bug17513Test.java
+++ openexchange-test/src/com/openexchange/ajax/contact/Bug17513Test.java
@@ -1,6 +1,5 @@
 package com.openexchange.ajax.contact;
 
-import junit.framework.AssertionFailedError;
 import com.openexchange.ajax.contact.action.InsertRequest;
 
 public class Bug17513Test extends AbstractManagedContactTest {
@@ -13,12 +12,14 @@ public class Bug17513Test extends AbstractManagedContactTest {
         "{\"anniversary\":\"1970-01-01T00:00:00.000Z\",\"last_name\":\"Aussendorf\",\"first_name\":\"Maik\",\"display_name\":\"Maik Aussendorf\",\"folder_id\":497}";
 
     public void testResultIsNotEmpty() throws Exception {
+        boolean fail = false;
         try {
             getClient().execute(new InsertRequest(json));
-            fail("Should fail, because the date is not in a format the OX can parse");
-        } catch(AssertionFailedError e){
-            assertTrue("Should fail, because the date is not in a format the OX can parse", true);
+            fail = true;
+        } catch(AssertionError e){
         }
-
+        if (fail) {
+            fail("Should fail, because the date is not in a format the OX can parse");
+        }
     }
 }
