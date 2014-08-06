449144947c54a75e047ab7a0da0f0633fd6787d1
Steffen Templin <steffen.templin@open-xchange.com>
2014-08-06 10:21:33 +0200

    Fix for bug 33918: Translate display names of subscription sources only if necessary
    (cherry picked from commit 835c36699e94158ae5ac1fee2ee434bd0ee23c75)
    
    Conflicts:
    	com.openexchange.server/ChangeLog

--- com.openexchange.subscribe.json/src/com/openexchange/subscribe/json/SubscriptionSourceJSONWriter.java
+++ com.openexchange.subscribe.json/src/com/openexchange/subscribe/json/SubscriptionSourceJSONWriter.java
@@ -104,7 +104,12 @@ public class SubscriptionSourceJSONWriter implements SubscriptionSourceJSONWrite
                 if(ID.equals(field)) {
                     row.put(source.getId());
                 } else if (DISPLAY_NAME.equals(field)) {
-                    row.put(null != translator ? translator.translate(source.getDisplayName()) : source.getDisplayName());
+                    String displayName = source.getDisplayName();
+                    if (source.isLocalizableDisplayName() && translator != null) {
+                        displayName = translator.translate(displayName);
+                    }
+
+                    row.put(displayName);
                 } else if (ICON.equals(field)) {
                     row.put(source.getIcon());
                 } else if (FORM_DESCRIPTION.equals(field)) {
