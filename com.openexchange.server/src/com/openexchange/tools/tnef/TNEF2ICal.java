a3b59b077c3cf5b410137b9dffe88b2f23a74453
Thorben Betten <thorben.betten@open-xchange.com>
2014-08-26 10:20:05 +0200

    Prevent possible java.lang.StringIndexOutOfBoundsException when parsing TNEF data

--- com.openexchange.server/src/com/openexchange/tools/tnef/TNEF2ICal.java
+++ com.openexchange.server/src/com/openexchange/tools/tnef/TNEF2ICal.java
@@ -324,16 +324,21 @@ public final class TNEF2ICal {
             /*
              * Time zone ID
              */
-            final String tzid;
+            String tzid;
             {
                 String tmp = findNamedProp("0x8234", mapiProps);
                 if (null == tmp) {
                     tzid = "GMT";
                 } else {
-                    final int p1 = tmp.indexOf('(') + 1;
-                    final int p2 = tmp.indexOf(')', p1);
-                    tmp = tmp.substring(p1, p2);
-                    tzid = java.util.TimeZone.getTimeZone(tmp).getID();
+                    try {
+                        final int p1 = tmp.indexOf('(') + 1;
+                        final int p2 = tmp.indexOf(')', p1);
+                        tmp = tmp.substring(p1, p2);
+                        tzid = java.util.TimeZone.getTimeZone(tmp).getID();
+                    } catch (Exception e) {
+                        LOG.warn("Cannot parse time zone information from: \"{}\"", tmp);
+                        tzid = "GMT";
+                    }
                 }
             }
             /*
