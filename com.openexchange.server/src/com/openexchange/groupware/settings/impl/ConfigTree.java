16789d09898a05f17c8154e1df67ef0abdd728a7
Thorben Betten <thorben.betten@open-xchange.com>
2014-08-26 09:52:34 +0200

    Added option to config tree

--- com.openexchange.server/src/com/openexchange/groupware/settings/impl/ConfigTree.java
+++ com.openexchange.server/src/com/openexchange/groupware/settings/impl/ConfigTree.java
@@ -343,6 +343,7 @@ public final class ConfigTree {
             com.openexchange.groupware.settings.tree.modules.mail.LineWrap.class,
             com.openexchange.groupware.settings.tree.modules.mail.Module.class,
             com.openexchange.groupware.settings.tree.modules.mail.MsgFormat.class,
+            com.openexchange.groupware.settings.tree.modules.mail.EnforceSecureConnection.class,
             com.openexchange.groupware.settings.tree.modules.mail.PhishingHeaders.class,
             com.openexchange.groupware.settings.tree.modules.mail.MailProtocols.class,
             com.openexchange.groupware.settings.tree.modules.mail.ReplyAllCc.class,
