367a22eb7373786ac08331993b1493e516da0544
Felix Marx <felix.marx@open-xchange.com>
2014-08-26 10:52:57 +0200

    Added debug logging to trace failing find tests#2

--- com.openexchange.server/src/com/openexchange/groupware/infostore/search/impl/SearchEngineImpl.java
+++ com.openexchange.server/src/com/openexchange/groupware/infostore/search/impl/SearchEngineImpl.java
@@ -129,6 +129,7 @@ public class SearchEngineImpl extends DBService implements InfostoreSearchEngine
 
     @Override
     public SearchIterator<DocumentMetadata> search(final int[] folderIds, final SearchTerm<?> searchTerm, final Metadata[] cols, final Metadata sortedBy, final int dir, final int start, final int end, final Context ctx, final User user, final UserPermissionBits userPermissions) throws OXException {
+        LOG.debug("Searching in folders -- Input FOLDERIDS: {}, SEARCHTERM: {}, COLS: {}, SORTBY: {}, DIR: {}, START: {}, END: {}, CTX: {}, USER: {}, USERPERMISSIONBITS: {} ", folderIds, searchTerm, cols, sortedBy, dir, start, end, ctx, user, userPermissions);
         Connection con = getReadConnection(ctx);
         List<Integer> all = new ArrayList<Integer>();
         List<Integer> own = new ArrayList<Integer>();
@@ -186,6 +187,7 @@ public class SearchEngineImpl extends DBService implements InfostoreSearchEngine
     @Override
     public SearchIterator<DocumentMetadata> search(final String query, final Metadata[] cols, final int folderId, final Metadata sortedBy, final int dir, final int start, final int end, final Context ctx, final User user, final UserPermissionBits userPermissions) throws OXException {
 
+        LOG.debug("Searching in folders -- Input QUERY: {}, COLS: {}, FOLDERID: {}, SORTBY: {}, DIR: {}, START: {}, END: {}, CTX: {}, USER: {}, USERPERMISSIONBITS: {} ", query, cols, folderId, sortedBy, dir, start, end, ctx, user, userPermissions);
         List<Integer> all = new ArrayList<Integer>();
         List<Integer> own = new ArrayList<Integer>();
 
