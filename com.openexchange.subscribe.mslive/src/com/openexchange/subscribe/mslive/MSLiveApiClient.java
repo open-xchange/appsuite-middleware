3156897efc7a24c1c46beca63938985f4d0e43f3
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-08-28 16:05:46 +0200

    verify token ttl

--- com.openexchange.subscribe.mslive/src/com/openexchange/subscribe/mslive/MSLiveApiClient.java
+++ com.openexchange.subscribe.mslive/src/com/openexchange/subscribe/mslive/MSLiveApiClient.java
@@ -49,12 +49,23 @@
 
 package com.openexchange.subscribe.mslive;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.MsLiveConnectApi;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
 import org.scribe.model.Token;
+import org.scribe.model.Verb;
 import com.openexchange.exception.OXException;
+import com.openexchange.java.Strings;
 import com.openexchange.oauth.API;
+import com.openexchange.oauth.DefaultOAuthToken;
 import com.openexchange.oauth.OAuthAccount;
+import com.openexchange.oauth.OAuthConstants;
+import com.openexchange.oauth.OAuthExceptionCodes;
 import com.openexchange.oauth.OAuthService;
 import com.openexchange.session.Session;
 import com.openexchange.subscribe.mslive.osgi.Services;
@@ -67,8 +78,10 @@ import com.openexchange.subscribe.mslive.osgi.Services;
 public class MSLiveApiClient {
 
     /**
-     * @return
-     * @throws OXException
+     * Get the default MS Live OAuth account
+     * 
+     * @return the default MS Live OAuth account
+     * @throws OXException If the account cannot be returned
      */
     public static OAuthAccount getDefaultOAuthAccount(Session session) throws OXException {
         final OAuthService service = Services.getService(OAuthService.class);
@@ -76,17 +89,55 @@ public class MSLiveApiClient {
     }
 
     /**
-     * @param oauthAccount
-     * @param session
-     * @return
-     * @throws OXException
+     * Get the access token
+     * 
+     * @param oauthAccount the OAuth account
+     * @param session The session
+     * @return a new access token
+     * @throws OXException if the access token cannot be retrieved
      */
     public static String getAccessToken(OAuthAccount account, Session session) throws OXException {
+        final OAuthService oauthService = Services.getService(OAuthService.class);
+
+        // Build service
         final ServiceBuilder serviceBuilder = new ServiceBuilder().provider(MsLiveConnectApi.class);
         serviceBuilder.apiKey(account.getMetaData().getAPIKey(session)).apiSecret(account.getMetaData().getAPISecret(session));
         org.scribe.oauth.OAuthService service = serviceBuilder.build();
-        Token accessToken = service.getAccessToken(new Token(account.getToken(), account.getSecret()), null);
-        return accessToken.getToken();
+
+        // Fetch access token
+        String token = account.getToken();
+        if (isExpired(token)) {
+            String refreshToken = account.getSecret();
+            final Token accessToken = service.getAccessToken(new Token(token, refreshToken), null);
+            if (!Strings.isEmpty(accessToken.getSecret())) {
+                refreshToken = accessToken.getSecret();
+            }
+
+            // Update account
+            Map<String, Object> arguments = new HashMap<String, Object>(3);
+            arguments.put(OAuthConstants.ARGUMENT_REQUEST_TOKEN, new DefaultOAuthToken(accessToken.getToken(), refreshToken));
+            arguments.put(OAuthConstants.ARGUMENT_SESSION, session);
+            oauthService.updateAccount(account.getId(), arguments, session.getUserId(), session.getContextId());
+        }
+
+        return token;
+    }
+    
+    private static boolean isExpired(String accessToken) throws OXException {
+        String encodedToken;
+        try {
+            encodedToken = URLEncoder.encode(accessToken, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw OAuthExceptionCodes.UNEXPECTED_ERROR.create(e);
+        }
+        OAuthRequest request = new OAuthRequest(Verb.GET, "https://apis.live.net/v5.0/me?access_token=" + encodedToken);
+        Response response = request.send();
+        if (response.getCode() == 401 || response.getCode() == 400) {
+            // 401 unauthorized
+            return true;
+        }
+
+        return false;
     }
 
 }
