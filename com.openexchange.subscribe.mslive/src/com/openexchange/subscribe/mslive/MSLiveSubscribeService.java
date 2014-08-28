7b1b6211cc6ee63fb10717dc8d99835ffe3bd9ee
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-08-28 12:56:39 +0200

    init c.o.subscribe.mslive bundle

---
+++ com.openexchange.subscribe.mslive/src/com/openexchange/subscribe/mslive/MSLiveSubscribeService.java
@@ -0,0 +1,104 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.subscribe.mslive;
+
+import java.util.Collection;
+import com.openexchange.exception.OXException;
+import com.openexchange.oauth.OAuthServiceMetaData;
+import com.openexchange.server.ServiceLookup;
+import com.openexchange.subscribe.AbstractSubscribeService;
+import com.openexchange.subscribe.Subscription;
+import com.openexchange.subscribe.SubscriptionSource;
+
+/**
+ * {@link MSLiveSubscribeService}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class MSLiveSubscribeService extends AbstractSubscribeService {
+
+    /**
+     * Initializes a new {@link MSLiveSubscribeService}.
+     * @param oAuthServiceMetaData
+     * @param services
+     */
+    public MSLiveSubscribeService(OAuthServiceMetaData oAuthServiceMetaData, ServiceLookup services) {
+        super();
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.subscribe.SubscribeService#getSubscriptionSource()
+     */
+    @Override
+    public SubscriptionSource getSubscriptionSource() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.subscribe.SubscribeService#handles(int)
+     */
+    @Override
+    public boolean handles(int folderModule) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see com.openexchange.subscribe.SubscribeService#getContent(com.openexchange.subscribe.Subscription)
+     */
+    @Override
+    public Collection<?> getContent(Subscription subscription) throws OXException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
