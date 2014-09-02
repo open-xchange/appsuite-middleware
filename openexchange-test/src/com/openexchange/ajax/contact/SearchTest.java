7b35558d97df0f86ce12020b2e81785cb44a3920
Felix Marx <felix.marx@open-xchange.com>
2014-09-02 08:07:59 +0200

    handle response to get more data

--- openexchange-test/src/com/openexchange/ajax/contact/SearchTest.java
+++ openexchange-test/src/com/openexchange/ajax/contact/SearchTest.java
@@ -134,20 +134,25 @@ public class SearchTest extends AbstractContactTest {
         try {
             int collectFolderId = -1;
             final GetResponse getResponse = client.execute(new GetRequest(Tree.ContactCollectFolder));
-            if (getResponse.hasInteger()) {
-                collectFolderId = getResponse.getInteger();
+            if (getResponse.hasValue()) {
+                if (getResponse.hasInteger()) {
+                    collectFolderId = getResponse.getInteger();
 
-                if (-1 == collectFolderId) {
-                    // Obviously contact collector folder is not present for test user
-                    return;
+                    if (-1 == collectFolderId) {
+                        // Obviously contact collector folder is not present for test user
+                        return;
+                    }
+                } else {
+                    if (getResponse.hasString()) {
+                        fail(getResponse.getString());
+                    } else {
+                        fail("Response is in wrong format:" + getResponse);
+                    }
                 }
             } else {
-                if (getResponse.hasString()) {
-                    fail(getResponse.getString());
-                } else {
-                    fail("Response is in wrong format:" + getResponse);
-                }
+                fail("Response has no data:" + getResponse);
             }
+
             contactIds = insertSearchableContacts(collectFolderId);
 
             final ContactSearchObject searchObject = new ContactSearchObject();
