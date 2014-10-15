4dcff1b3e447e1d3c3e62ee463a1ecc091277cc5
Steffen Templin <steffen.templin@open-xchange.com>
2014-10-15 18:47:44 +0200

    Created I18nTranslatorFactory as a convenient mechanism to obtain string translators

---
+++ com.openexchange.global/src/com/openexchange/i18n/I18nTranslatorFactory.java
@@ -0,0 +1,134 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.i18n;
+
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * <p>If strings shall be translated, you need to use an appropriate {@link I18nService},
+ * according to the desired locale. The {@link I18nTranslatorFactory} helps you to
+ * keep track of all {@link I18nService} instances and creates new {@link Translator}
+ * instances based on given locales.</p>
+ *
+ * <p>{@link I18nTranslatorFactory} is a {@link ServiceTracker}. Simply create a new
+ * instance in your bundles {@link BundleActivator} and open it.</p>
+ *
+ * @author <a href="mailto:steffen.templin@open-xchange.com">Steffen Templin</a>
+ * @since v7.8.0
+ */
+public class I18nTranslatorFactory extends ServiceTracker<I18nService, I18nService> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(I18nTranslatorFactory.class);
+
+    private final ConcurrentMap<Locale, I18nService> services = new ConcurrentHashMap<Locale, I18nService>();
+
+
+    /**
+     * Initializes a new {@link I18nTranslatorFactory}.
+     *
+     * @param context The bundle context
+     */
+    public I18nTranslatorFactory(BundleContext context) {
+        super(context, I18nService.class, null);
+    }
+
+    /**
+     * Returns a translator for the given locale. If no {@link I18nService} for
+     * the given locale is available or the locale is <code>null</code>,
+     * a default translator is returned, that simply
+     * returns the input string on {@link Translator#translate(String)}.
+     *
+     * @param locale The locale
+     * @return A new translator instance
+     */
+    public Translator translatorFor(Locale locale) {
+        if (locale == null) {
+            return I18nTranslator.EMPTY;
+        }
+
+        I18nService service = services.get(locale);
+        if (service == null) {
+            return I18nTranslator.EMPTY;
+        }
+
+        return new I18nTranslator(service);
+    }
+
+    @Override
+    public I18nService addingService(ServiceReference<I18nService> reference) {
+        I18nService service = super.addingService(reference);
+        if (service != null) {
+            I18nService existing = services.putIfAbsent(service.getLocale(), service);
+            if (existing != null) {
+                LOG.warn("Ignoring duplicate I18nService for locale {}.", service.getLocale());
+                context.ungetService(reference);
+                return null;
+            }
+        }
+        return service;
+    }
+
+    @Override
+    public void removedService(ServiceReference<I18nService> reference, I18nService service) {
+        if (services.remove(service.getLocale(), service)) {
+            super.removedService(reference, service);
+        }
+    }
+
+}
