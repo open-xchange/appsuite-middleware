6ca3c124febd7e88c14e1f17cde629c826bdc52e
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-10-21 08:54:41 +0200

    Revert "minor"
    This reverts commit 4085b9fcdd3c0cadb51ad734525610c1ceca20d8.

--- com.openexchange.server/src/com/openexchange/ajax/requesthandler/DispatcherServlet.java
+++ com.openexchange.server/src/com/openexchange/ajax/requesthandler/DispatcherServlet.java
@@ -82,6 +82,7 @@ import com.openexchange.exception.OXExceptionCode;
 import com.openexchange.groupware.contexts.impl.ContextImpl;
 import com.openexchange.groupware.ldap.UserImpl;
 import com.openexchange.java.Streams;
+import com.openexchange.java.StringAllocator;
 import com.openexchange.java.Strings;
 import com.openexchange.log.LogProperties;
 import com.openexchange.log.LogProperties.Name;
@@ -602,6 +603,19 @@ public class DispatcherServlet extends SessionServlet {
             }
         } catch (Exception e) {
             // Ignore
+        }
+    }
+
+    /** ASCII-wise to upper-case */
+    private String toUpperCase(CharSequence chars) {
+        if (null == chars) {
+            return null;
+        }
+        final int length = chars.length();
+        final StringAllocator builder = new StringAllocator(length);
+        for (int i = 0; i < length; i++) {
+            final char c = chars.charAt(i);
+            builder.append((c >= 'a') && (c <= 'z') ? (char) (c & 0x5f) : c);
         }
     }
 
