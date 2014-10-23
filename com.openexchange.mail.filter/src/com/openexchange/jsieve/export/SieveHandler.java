03ae9061bc6c4dd26a051d27006d6dff963878c9
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-10-23 15:39:56 +0200

    Fix for bug 35168: Consider '#' as literals and not as comments when inside of double quotes

--- com.openexchange.mail.filter/src/com/openexchange/jsieve/export/SieveHandler.java
+++ com.openexchange.mail.filter/src/com/openexchange/jsieve/export/SieveHandler.java
@@ -541,7 +541,9 @@ public class SieveHandler {
                 break;
             case '#': 
                 {
-                    inComment = true;
+                    if (!inQuote) {
+                        inComment = true;
+                    }
                     sb.append((char) ch);
                 }
                 break;
