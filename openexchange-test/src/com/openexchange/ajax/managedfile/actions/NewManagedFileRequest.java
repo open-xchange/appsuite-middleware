8482b299327e5090c8f6d33b00278924e6feeaf6
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-11-04 15:17:36 +0100

    testing mfm and snippets (stub for signature snippet)
    (cherry picked from commit 715d2ff6b0329cb3c30c4293bef762662469e6a6)

---
+++ openexchange-test/src/com/openexchange/ajax/managedfile/actions/NewManagedFileRequest.java
@@ -0,0 +1,139 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax.managedfile.actions;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONException;
+import com.openexchange.ajax.AJAXServlet;
+import com.openexchange.ajax.framework.AbstractAJAXParser;
+import com.openexchange.ajax.framework.Header;
+import com.openexchange.ajax.framework.AJAXRequest.FileParameter;
+import com.openexchange.ajax.framework.Header.SimpleHeader;
+
+/**
+ * {@link NewManagedFileRequest}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class NewManagedFileRequest extends AbstractManagedFileRequest<NewManagedFileResponse> {
+
+    private String module;
+
+    private String type;
+
+    private Object body;
+
+    /**
+     * Initializes a new {@link NewManagedFileRequest}.
+     * 
+     * @param failOnError
+     */
+    public NewManagedFileRequest(boolean failOnError) {
+        super(failOnError);
+    }
+
+    public NewManagedFileRequest(final String module, final String type, final Object body) {
+        this(true);
+        this.module = module;
+        this.type = type;
+        this.body = body;
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.ajax.framework.AJAXRequest#getMethod()
+     */
+    @Override
+    public com.openexchange.ajax.framework.AJAXRequest.Method getMethod() {
+        return Method.POST;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.ajax.framework.AJAXRequest#getParameters()
+     */
+    @Override
+    public com.openexchange.ajax.framework.AJAXRequest.Parameter[] getParameters() throws IOException, JSONException {
+        List<Parameter> list = new ArrayList<Parameter>();
+        list.add(new URLParameter(AJAXServlet.PARAMETER_ACTION, AJAXServlet.ACTION_NEW));
+        list.add(new URLParameter(AJAXServlet.PARAMETER_MODULE, module));
+        list.add(new URLParameter(AJAXServlet.PARAMETER_TYPE, type));
+        list.add(new FileParameter("file", "5161.png", new ByteArrayInputStream((byte[]) body), "image/png"));
+        return list.toArray(new Parameter[list.size()]);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.ajax.framework.AJAXRequest#getParser()
+     */
+    @Override
+    public AbstractAJAXParser<? extends NewManagedFileResponse> getParser() {
+        return new NewManagedFileParser(failOnError);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.openexchange.ajax.framework.AJAXRequest#getBody()
+     */
+    @Override
+    public Object getBody() throws IOException, JSONException {
+        return body;
+    }
+
+    @Override
+    public Header[] getHeaders() {
+        return new Header[] { new SimpleHeader("Content-Type", "multipart/form-data") };
+    }
+
+}
---
+++ openexchange-test/src/com/openexchange/ajax/snippet/TestSnippetSignature.java
@@ -0,0 +1,149 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax.snippet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import com.openexchange.ajax.framework.AbstractAJAXSession;
+import com.openexchange.ajax.managedfile.actions.NewManagedFileRequest;
+import com.openexchange.ajax.managedfile.actions.NewManagedFileResponse;
+import com.openexchange.ajax.snippet.actions.ListSnippetRequest;
+import com.openexchange.ajax.snippet.actions.ListSnippetResponse;
+import com.openexchange.ajax.snippet.actions.NewSnippetRequest;
+import com.openexchange.ajax.snippet.actions.NewSnippetResponse;
+import com.openexchange.configuration.MailConfig;
+
+/**
+ * {@link TestSnippetSignature}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class TestSnippetSignature extends AbstractAJAXSession {
+
+    /**
+     * Initializes a new {@link TestSnippetSignature}.
+     * 
+     * @param name
+     */
+    public TestSnippetSignature(String name) {
+        super(name);
+    }
+
+    public void testCreateSignatureWithImageWorkflow() throws Exception {
+        NewManagedFileRequest newMFReq = new NewManagedFileRequest("snippet", "image", readFile("contact_image.png"));
+        NewManagedFileResponse newMFResp = client.execute(newMFReq);
+        Object data = newMFResp.getData();
+        assertTrue("Response not a JSONArray", data instanceof JSONArray);
+        JSONArray array = (JSONArray) data;
+        assertEquals("The response array should only contain one element", 1, array.length());
+        final String mfID = array.getString(0);
+
+        final StringBuilder builder = new StringBuilder();
+        builder.append("<img alt=\"\" width=\"320\" src=\"").append("/ajax/file?action=get&id=").append(mfID).append("&session=").append(
+            client.getSession().getId()).append("\" />");
+
+        JSONObject misc = new JSONObject();
+        misc.put("insertion", "below");
+        misc.put("content-type", "text/html");
+
+        JSONObject body = new JSONObject();
+        body.put("type", "signature");
+        body.put("misc", misc);
+        body.put("createdby", client.getValues().getUserId());
+        body.put("content", "<h1>my test signature</h1>" + builder.toString());
+        body.put("accountid", 0);
+        body.put("displayname", "DisplayName of signatures");
+        body.put("module", "io.ox/mail");
+
+        // New signature
+        NewSnippetRequest req = new NewSnippetRequest(body);
+        NewSnippetResponse resp = client.execute(req);
+        data = resp.getData();
+        assertNotNull(data);
+        int signId = Integer.parseInt((String) data);
+        // AttachSnippetRequest attachReq = new AttachSnippetRequest(readFile("contact_image.png"), signId);
+        // AttachSnippetResponse attachResp = client.execute(attachReq);
+        // data = attachResp.getData();
+        // assertNotNull(data);
+        // String sid = (String) data;
+        array = new JSONArray();
+        array.put(signId);
+        ListSnippetRequest listReq = new ListSnippetRequest(array, true);
+        ListSnippetResponse listResp = client.execute(listReq);
+        assertNotNull(listResp);
+        data = listResp.getData();
+        JSONArray json = (JSONArray) data;
+        assertEquals(1, json.length());
+        JSONObject files = json.getJSONObject(0);
+        JSONObject file = files.getJSONArray("files").getJSONObject(0);
+        //String cid = file.getString("id");
+        // GetSnippetAttachmentRequest getAttReq = new GetSnippetAttachmentRequest(sid, cid, false);
+        // GetSnippetAttachmentResponse getAttResp = client.execute(getAttReq);
+        // assertNotNull(getAttResp);
+
+    }
+
+    private static byte[] readFile(String fileName) {
+        byte[] data = new byte[1024];
+        int readBytes;
+        try {
+            FileInputStream in = new FileInputStream(new File(MailConfig.getProperty(MailConfig.Property.TEST_MAIL_DIR) + fileName));
+            while ((readBytes = in.read(data)) != -1) {
+                System.out.println("read " + readBytes + " bytes, and placed them into temp array named data");
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
