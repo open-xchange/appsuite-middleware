bac88f782fd3630599dbb8827078bf754849d500
Thorben Betten <thorben.betten@open-xchange.com>
2014-11-05 17:05:57 +0100

    Enhanced IMAP message fetching
    (cherry picked from commit 1a09ece222026e7ae200676c7097d53ea8a1c5b9)

--- com.openexchange.server/src/com/openexchange/mail/mime/utils/MimeStorageUtility.java
+++ com.openexchange.server/src/com/openexchange/mail/mime/utils/MimeStorageUtility.java
@@ -93,7 +93,7 @@ public final class MimeStorageUtility {
         CACHE_FETCH_PROFILE.add(FetchProfile.Item.FLAGS);
         CACHE_FETCH_PROFILE.add(FetchProfile.Item.CONTENT_INFO);
         CACHE_FETCH_PROFILE.add(UIDFolder.FetchProfileItem.UID);
-        CACHE_FETCH_PROFILE.add(IMAPFolder.FetchProfileItem.SIZE);
+        CACHE_FETCH_PROFILE.add(FetchProfile.Item.SIZE);
         CACHE_FETCH_PROFILE.add(MessageHeaders.HDR_IMPORTANCE);
         CACHE_FETCH_PROFILE.add(MessageHeaders.HDR_X_PRIORITY);
         // CACHE_FETCH_PROFILE.add(IMAPFolder.FetchProfileItem.HEADERS);
