970853e128fc85bc7a63403bbc6d96a4e3416ec9
Lars Hoogestraat <lars.hoogestraat@open-xchange.com>
2014-11-06 14:51:36 +0100

    US 80624410: exported gcm library to its own bundle
    (cherry picked from commit 3190e0994493083e26e94a8f1dfb56987be73811)
    
    Conflicts:
    	open-xchange-core/open-xchange-core.psf

--- com.google.android.gcm/src/com/google/android/gcm/MulticastResult.java
+++ com.google.android.gcm/src/com/google/android/gcm/MulticastResult.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.android.gcm.server;
+package com.google.android.gcm;
 
 import java.io.Serializable;
 import java.util.ArrayList;
