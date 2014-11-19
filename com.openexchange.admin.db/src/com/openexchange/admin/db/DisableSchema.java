4fb5a11df6ebc70a271c81cea559d7e6657ed36f
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2014-11-19 13:44:49 +0100

    US 80625102: skeletons for the clts

---
+++ com.openexchange.admin.db/src/com/openexchange/admin/db/DisableSchema.java
@@ -0,0 +1,116 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2014 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.admin.db;
+
+import javax.management.MBeanException;
+import javax.management.MBeanServerConnection;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import com.openexchange.auth.mbean.AuthenticatorMBean;
+import com.openexchange.cli.AbstractMBeanCLI;
+
+/**
+ * {@link DisableSchema}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class DisableSchema extends AbstractMBeanCLI<Void> {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new DisableSchema().execute(args);
+    }
+
+    @Override
+    protected void checkOptions(CommandLine cmd) {
+    }
+
+    @Override
+    protected boolean requiresAdministrativePermission() {
+        return true;
+    }
+
+    @Override
+    protected void administrativeAuth(String login, String password, CommandLine cmd, AuthenticatorMBean authenticator) throws MBeanException {
+        authenticator.doAuthentication(login, password);
+    }
+
+    @Override
+    protected String getFooter() {
+        return "Tool to disable Open-Xchange database schemata.";
+    }
+
+    @Override
+    protected String getName() {
+        return "disableschema";
+    }
+
+    @SuppressWarnings("static-access")
+    @Override
+    protected void addOptions(Options options) {
+        options.addOption(OptionBuilder.withLongOpt("schema").withArgName("schema_name").withDescription(
+            "The name of the schema to disable").hasArg(true).isRequired(true).create("m"));
+    }
+
+    @Override
+    protected Void invoke(Options option, CommandLine cmd, MBeanServerConnection mbsc) throws Exception {
+        // TODO: flesh out
+        // - check for the precondition
+        // - fetch all cids
+        // - iterate over all cids and create a batch statement
+        // - invalidate sessions
+        // - execute batch
+        return null;
+    }
+
+}
