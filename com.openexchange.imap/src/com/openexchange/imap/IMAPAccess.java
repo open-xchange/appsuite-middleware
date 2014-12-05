153f3619ae0eed728a36e9daa11669ff72163523
Thorben Betten <thorben.betten@open-xchange.com>
2014-12-05 17:21:48 +0100

    Enriched session informationm passed to IMAP server

--- com.openexchange.imap/src/com/openexchange/imap/IMAPAccess.java
+++ com.openexchange.imap/src/com/openexchange/imap/IMAPAccess.java
@@ -926,7 +926,7 @@ public final class IMAPAccess extends MailAccess<IMAPFolderStorage, IMAPMessageS
         {
             Map<String, String> clientParams = new LinkedHashMap<String, String>(6);
             clientParams.put(IMAPClientParameters.ORIGINATING_IP.getParamName(), session.getLocalIp());
-            clientParams.put(IMAPClientParameters.SESSION_ID.getParamName(), session.getSessionID() + "-" + imapStore.hashCode());
+            clientParams.put(IMAPClientParameters.SESSION_ID.getParamName(), IMAPClientParameters.generateSessionInformation(session, imapStore));
             clientParams.put(IMAPClientParameters.NAME.getParamName(), "Open-Xchange");
             clientParams.put(IMAPClientParameters.VERSION.getParamName(), Version.getInstance().getVersionString());
             imapStore.setClientParameters(clientParams);
