12072b6755a58a2826f71fe6540122424a91c6ca
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2014-12-08 16:38:32 +0100

    US82832082: Fallback to 'preview' if method is unset

--- com.openexchange.drive/src/com/openexchange/drive/internal/JumpLinkGenerator.java
+++ com.openexchange.drive/src/com/openexchange/drive/internal/JumpLinkGenerator.java
@@ -49,6 +49,7 @@
 
 package com.openexchange.drive.internal;
 
+import com.openexchange.drive.DriveFileMetadata;
 import com.openexchange.drive.DriveSession;
 import com.openexchange.drive.management.DriveConfig;
 
@@ -61,25 +62,67 @@ import com.openexchange.drive.management.DriveConfig;
  */
 public class JumpLinkGenerator {
 
+    private static final String[] OFFICE_TEXT_MIMETYPES = { "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/vnd.oasis.opendocument.text", "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+        "application/msword", "application/rtf"};
+
+    private static final String[] OFFICE_SPREADSHEET_MIMETYPES = {"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        "application/vnd.oasis.opendocument.spreadsheet", "application/vnd.openxmlformats-officedocument.spreadsheetml.template",
+        "application/vnd.ms-excel"};
+
     private final DriveSession session;
 
     public JumpLinkGenerator(DriveSession session) {
         this.session = session;
     }
 
-    public String getJumpLink(String folderId, String fileId, String method) {
+    public String getJumpLink(String folderId, String method) {
+        return getJumpLink(folderId, null, method, null);
+    }
+
+    public String getJumpLink(String folderId, String fileId, String method, DriveFileMetadata metadata) {
         String redirectUrl = DriveConfig.getInstance().getJumpLink()
-          .replaceAll("\\[protocol\\]", session.getHostData().isSecure() ? "https" : "http")
-          .replaceAll("\\[hostname\\]", session.getHostData().getHost())
-          .replaceAll("\\[uiwebpath\\]", trimSlashes(DriveConfig.getInstance().getUiWebPath()))
-          .replaceAll("\\[app\\]", "app=io.ox/" + method)
-          .replaceAll("\\[perspective\\]", "perspective=fluid:icon")
-          .replaceAll("\\[folder\\]", "folder=" + folderId);
-        if (null != fileId) {
+            .replaceAll("\\[protocol\\]", session.getHostData().isSecure() ? "https" : "http")
+            .replaceAll("\\[hostname\\]", session.getHostData().getHost())
+            .replaceAll("\\[uiwebpath\\]", trimSlashes(DriveConfig.getInstance().getUiWebPath()))
+            .replaceAll("\\[perspective\\]", "perspective=fluid:icon")
+            .replaceAll("\\[folder\\]", "folder=" + folderId);
+
+        if (null != metadata && null != fileId) {
+            String mimeType = metadata.getMimeType();
+            switch (method) {
+            case "edit":
+                if (inArray(OFFICE_TEXT_MIMETYPES, mimeType)) {
+                    redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/office/text");
+                } else if (inArray(OFFICE_SPREADSHEET_MIMETYPES, mimeType)) {
+                    redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/office/spreadsheet");
+                } else if (mimeType.startsWith("text/")){
+                    redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/editor");
+                } else {
+                    redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/files");
+                }
+                break;
+            case "permissions":
+                redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/files/permissions");
+                break;
+            case "version_history":
+                redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/files/history");
+                break;
+            case "preview":
+            default:
+                redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/files");
+                break;
+            }
             redirectUrl = redirectUrl.replaceAll("\\[id\\]", "id=" + fileId);
         } else {
-            redirectUrl = redirectUrl.replaceAll("\\[id\\]", "");
+            if ("permissions".equals(method)) {
+                redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/files/permissions");
+            } else {
+                redirectUrl = redirectUrl.replaceAll("\\[app\\]", "app=io.ox/files");
+            }
+            redirectUrl = redirectUrl.replaceAll("&\\[id\\]", "");
         }
+
         return redirectUrl;
     }
 
@@ -93,6 +136,15 @@ public class JumpLinkGenerator {
             }
         }
         return path;
+    }
+
+    private static boolean inArray(String[] array, String value) {
+        for (String s : array) {
+            if (s.equals(value)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
