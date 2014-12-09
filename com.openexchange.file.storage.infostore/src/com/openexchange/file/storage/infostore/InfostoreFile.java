869554f1115c54ef93d156e03c0fc57cdbc64448
Thorben Betten <thorben.betten@open-xchange.com>
2014-12-09 16:20:53 +0100

    Fix for bug 35784: Examine passed "version" parameter if it represents "null"

--- com.openexchange.file.storage.infostore/src/com/openexchange/file/storage/infostore/InfostoreFile.java
+++ com.openexchange.file.storage.infostore/src/com/openexchange/file/storage/infostore/InfostoreFile.java
@@ -1,3 +1,4 @@
+
 package com.openexchange.file.storage.infostore;
 
 import java.util.Date;
@@ -8,7 +9,6 @@ import com.openexchange.file.storage.File;
 import com.openexchange.file.storage.FileStorageFileAccess;
 import com.openexchange.groupware.infostore.DocumentMetadata;
 import com.openexchange.groupware.infostore.InfostoreFacade;
-
 
 /*
  *
@@ -72,287 +72,240 @@ public class InfostoreFile extends AbstractFile implements File {
         this.document = documentMetadata;
     }
 
-
     @Override
     public String getCategories() {
         return document.getCategories();
     }
-
 
     @Override
     public int getColorLabel() {
         return document.getColorLabel();
     }
 
-
     @Override
     public String getContent() {
         return document.getContent();
     }
-
 
     @Override
     public Date getCreated() {
         return document.getCreationDate();
     }
 
-
     @Override
     public int getCreatedBy() {
         return document.getCreatedBy();
     }
-
 
     @Override
     public String getDescription() {
         return document.getDescription();
     }
 
-
     @Override
     public String getFileMD5Sum() {
         return document.getFileMD5Sum();
     }
-
 
     @Override
     public String getFileMIMEType() {
         return document.getFileMIMEType();
     }
 
-
     @Override
     public String getFileName() {
         return document.getFileName();
     }
-
 
     @Override
     public long getFileSize() {
         return document.getFileSize();
     }
 
-
     @Override
     public String getFolderId() {
         return Long.toString(document.getFolderId());
     }
-
 
     @Override
     public String getId() {
         return Integer.toString(document.getId());
     }
 
-
     @Override
     public Date getLastModified() {
         return document.getLastModified();
     }
-
 
     @Override
     public Date getLockedUntil() {
         return document.getLockedUntil();
     }
 
-
     @Override
     public int getModifiedBy() {
         return document.getModifiedBy();
     }
-
 
     @Override
     public int getNumberOfVersions() {
         return document.getNumberOfVersions();
     }
 
-
     @Override
     public String getProperty(final String key) {
         return document.getProperty(key);
     }
-
 
     @Override
     public Set<String> getPropertyNames() {
         return document.getPropertyNames();
     }
 
-
     @Override
     public long getSequenceNumber() {
         return document.getSequenceNumber();
     }
-
 
     @Override
     public String getTitle() {
         return document.getTitle();
     }
 
-
     @Override
     public String getURL() {
         return document.getURL();
     }
-
 
     @Override
     public String getVersion() {
         return Integer.toString(document.getVersion());
     }
 
-
     @Override
     public String getVersionComment() {
         return document.getVersionComment();
     }
-
 
     @Override
     public boolean isCurrentVersion() {
         return document.isCurrentVersion();
     }
 
-
     @Override
     public void setCategories(final String categories) {
         document.setCategories(categories);
     }
-
 
     @Override
     public void setColorLabel(final int color) {
         document.setColorLabel(color);
     }
 
-
     @Override
     public void setCreatedBy(final int cretor) {
         document.setCreatedBy(cretor);
     }
-
 
     @Override
     public void setCreated(final Date creationDate) {
         document.setCreationDate(creationDate);
     }
 
-
     @Override
     public void setDescription(final String description) {
         document.setDescription(description);
     }
-
 
     @Override
     public void setFileMD5Sum(final String sum) {
         document.setFileMD5Sum(sum);
     }
 
-
     @Override
     public void setFileMIMEType(final String type) {
         document.setFileMIMEType(type);
     }
-
 
     @Override
     public void setFileName(final String fileName) {
         document.setFileName(fileName);
     }
 
-
     @Override
     public void setFileSize(final long length) {
         document.setFileSize(length);
     }
 
-
     @Override
     public void setFolderId(final String folderId) {
-        if(folderId != null) {
+        if (folderId != null) {
             document.setFolderId(Long.parseLong(folderId));
         }
     }
 
-
     @Override
     public void setId(final String id) {
-        if(id == FileStorageFileAccess.NEW) {
+        if (id == FileStorageFileAccess.NEW) {
             document.setId(InfostoreFacade.NEW);
         } else {
             document.setId(Integer.parseInt(id));
         }
     }
 
-
     @Override
     public void setIsCurrentVersion(final boolean bool) {
         document.setIsCurrentVersion(bool);
     }
-
 
     @Override
     public void setLastModified(final Date now) {
         document.setLastModified(now);
     }
 
-
     @Override
     public void setLockedUntil(final Date lockedUntil) {
         document.setLockedUntil(lockedUntil);
     }
-
 
     @Override
     public void setModifiedBy(final int lastEditor) {
         document.setModifiedBy(lastEditor);
     }
 
-
     @Override
     public void setNumberOfVersions(final int numberOfVersions) {
         document.setNumberOfVersions(numberOfVersions);
     }
-
 
     @Override
     public void setTitle(final String title) {
         document.setTitle(title);
     }
 
-
     @Override
     public void setURL(final String url) {
         document.setURL(url);
     }
-
 
     @Override
     public void setVersion(final String version) {
         document.setVersion(Integer.parseInt(version));
     }
 
-
     @Override
     public void setVersionComment(final String string) {
         document.setVersionComment(string);
     }
-
 
     @Override
     public Map<String, Object> getMeta() {
         return document.getMeta();
     }
 
-
     @Override
     public void setMeta(final Map<String, Object> properties) {
-       document.setMeta(properties);
+        document.setMeta(properties);
     }
-
 
 }
