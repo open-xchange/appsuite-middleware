a2a581ef7e63a5d4ed4857a298c2201dc1188551
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2015-01-06 09:47:08 +0100

    use stringbuilder instead of string concat

--- com.openexchange.html/test/com/openexchange/html/Bug21757Test.java
+++ com.openexchange.html/test/com/openexchange/html/Bug21757Test.java
@@ -63,11 +63,12 @@ public class Bug21757Test extends AbstractSanitizing {
     public void testEnsureBodyTagPresence() {
         String htmlContent = "<center>Lorem Ipsum Dolor<table<tr><td>cell</td></tr></table></center>";
         String actual = getHtmlService().getConformHTML(htmlContent, "UTF-8");
-        String expected = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n\r\n" +
-            "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head>\r\n" +
-            "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\r\n" +
-            " </head><body><center>Lorem Ipsum Dolor<table><tbody><tr><td>cell</td></tr></tbody></table></center></body></html>";
-        System.out.println(actual);
+        StringBuilder expectedBuilder = new StringBuilder();
+        expectedBuilder.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n\r\n");
+        expectedBuilder.append("<html xmlns=\"http://www.w3.org/1999/xhtml\"><head>\r\n");
+        expectedBuilder.append("    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\r\n");
+        expectedBuilder.append(" </head><body><center>Lorem Ipsum Dolor<table><tbody><tr><td>cell</td></tr></tbody></table></center></body></html>");
+        String expected = expectedBuilder.toString();
         assertEquals("The <body> tag is missing", expected, actual);
     }
 }
