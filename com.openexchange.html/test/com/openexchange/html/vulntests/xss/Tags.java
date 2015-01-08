4d030c8aa3ccb0ea615d289b0d23c1cf6ca0dc5e
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2015-01-08 15:50:08 +0100

    US 66938626: more xss vuln tests

---
+++ com.openexchange.html/test/com/openexchange/html/vulntests/xss/Tags.java
@@ -0,0 +1,95 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2015 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.html.vulntests.xss;
+
+import org.junit.Test;
+import com.openexchange.html.AssertExpression;
+import com.openexchange.html.XSSHolder;
+
+/**
+ * {@link Tags}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class Tags extends AbstractXSSVectors {
+
+    @Test
+    public void testObjectTag() {
+        xss.add(new XSSHolder(" <OBJECT TYPE=\"text/x-scriptlet\" DATA=\"http://ha.ckers.org/scriptlet.html\"></OBJECT>"));
+        assertVectors();
+    }
+
+    @Test
+    public void testEmbedSVG() {
+        xss.add(new XSSHolder("<EMBED SRC=\"data:image/svg+xml;base64,PHN2ZyB4bWxuczpzdmc9Imh0dH A6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcv MjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hs aW5rIiB2ZXJzaW9uPSIxLjAiIHg9IjAiIHk9IjAiIHdpZHRoPSIxOTQiIGhlaWdodD0iMjAw IiBpZD0ieHNzIj48c2NyaXB0IHR5cGU9InRleHQvZWNtYXNjcmlwdCI+YWxlcnQoIlh TUyIpOzwvc2NyaXB0Pjwvc3ZnPg==\" type=\"image/svg+xml\" AllowScriptAccess=\"always\"></EMBED>"));
+        assertVectors();
+    }
+
+    @Test
+    public void testEmbedFlash() {
+        xss.add(new XSSHolder("<EMBED SRC=\"http://ha.ckers.Using an EMBED tag you can embed a Flash movie that contains XSS. Click here for a demo. If you add the attributes allowScriptAccess=\"never\" and allownetworking=\"internal\" it can mitigate this risk (thank you to Jonathan Vanasco for the info).:org/xss.swf\" AllowScriptAccess=\"always\"></EMBED>"));
+        assertVectors();
+    }
+
+    @Test
+    public void testBaseTag() {
+        xss.add(new XSSHolder("<BASE HREF=\"javascript:alert('XSS');//\">"));
+        assertVectors();
+    }
+
+    @Test
+    public void testDivTags() {
+        xss.add(new XSSHolder("<DIV STYLE=\"background-image: url(javascript:alert('XSS'))\">", AssertExpression.NOT_CONTAINED, "javascript:alert('xss')"));
+        xss.add(new XSSHolder("<DIV STYLE=\"background-image:\0075\0072\006C\0028'\006a\0061\0076\0061\0073\0063\0072\0069\0070\0074\003a\0061\006c\0065\0072\0074\0028.1027\0058.1053\0053\0027\0029'\0029\">", AssertExpression.NOT_CONTAINED, "STYLE=\"background-image:\0075\0072\006C\0028'\006a\0061\0076\0061\0073\0063\0072\0069\0070\0074\003a\0061\006c\0065\0072\0074\0028.1027\0058.1053\0053\0027\0029'\0029\""));
+        xss.add(new XSSHolder("<DIV STYLE=\"background-image: url(&#1;javascript:alert('XSS'))\">", AssertExpression.NOT_CONTAINED, "javascript:alert('XSS')"));
+        xss.add(new XSSHolder("<DIV STYLE=\"width: expression(alert('XSS'));\">", AssertExpression.NOT_CONTAINED, "expression(alert('XSS'))"));
+        assertVectors();
+    }
+}
