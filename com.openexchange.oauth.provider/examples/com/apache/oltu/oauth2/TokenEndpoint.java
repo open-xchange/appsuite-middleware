b9f17584dd31acb9053c13279452eab46b9304b5
Steffen Templin <steffen.templin@open-xchange.com>
2015-01-14 13:28:15 +0100

    Added Apache OLTU as OAuth 2.0 provider library

---
+++ com.openexchange.oauth.provider/examples/com/apache/oltu/oauth2/TokenEndpoint.java
@@ -0,0 +1,128 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2015 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.apache.oltu.oauth2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.oltu.oauth2.as.issuer.MD5Generator;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuer;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuerImpl;
+import org.apache.oltu.oauth2.as.request.OAuthTokenRequest;
+import org.apache.oltu.oauth2.as.response.OAuthASResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.message.OAuthResponse;
+
+
+/**
+ * See https://cwiki.apache.org/confluence/display/OLTU/OAuth+2.0+Authorization+Server
+ */
+public class TokenEndpoint extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        OAuthTokenRequest oauthRequest = null;
+
+        OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+
+            try {
+                   oauthRequest = new OAuthTokenRequest(request);
+
+                   validateClient(oauthRequest);
+
+                   String authzCode = oauthRequest.getCode();
+
+                   // some code
+
+                   String accessToken = oauthIssuerImpl.accessToken();
+                   String refreshToken = oauthIssuerImpl.refreshToken();
+
+                   // some code
+
+
+                    OAuthResponse r = OAuthASResponse
+                        .tokenResponse(HttpServletResponse.SC_OK)
+                        .setAccessToken(accessToken)
+                        .setExpiresIn("3600")
+                        .setRefreshToken(refreshToken)
+                        .buildJSONMessage();
+
+                response.setStatus(r.getResponseStatus());
+                PrintWriter pw = response.getWriter();
+                pw.print(r.getBody());
+                pw.flush();
+                pw.close();
+
+                 //if something goes wrong
+            } catch(OAuthProblemException ex) {
+
+                OAuthResponse r = OAuthResponse
+                    .errorResponse(401)
+                    .error(ex)
+                    .buildJSONMessage();
+
+                response.setStatus(r.getResponseStatus());
+
+                PrintWriter pw = response.getWriter();
+                pw.print(r.getBody());
+                pw.flush();
+                pw.close();
+
+                response.sendError(401);
+            }
+
+        }
+
+}
