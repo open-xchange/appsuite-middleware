58a0caf6730e872a2559bfa18c90d16fa83917ef
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2015-02-03 17:22:09 +0100

    Test request, response, parser for autologin

---
+++ openexchange-test/src/com/openexchange/ajax/session/actions/AutologinRequest.java
@@ -0,0 +1,134 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2015 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.ajax.session.actions;
+
+import com.openexchange.ajax.AJAXServlet;
+import com.openexchange.ajax.fields.LoginFields;
+import com.openexchange.ajax.framework.AbstractAJAXParser;
+
+
+/**
+ * {@link AutologinRequest}
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ * @since v7.6.2
+ */
+public class AutologinRequest extends AbstractRequest<AutologinResponse> {
+
+    private final boolean failOnError;
+
+    /**
+     * Initializes a new {@link AutologinRequest}.
+     * @param parameters
+     */
+    public AutologinRequest(Parameter[] parameters, boolean failOnError) {
+        super(parameters);
+        this.failOnError = failOnError;
+    }
+
+    public AutologinRequest(AutologinParameters parameters, boolean failOnError) {
+        this(new Parameter[] {
+            new URLParameter(AJAXServlet.PARAMETER_ACTION, AJAXServlet.ACTION_AUTOLOGIN),
+            new URLParameter(LoginFields.CLIENT_PARAM, parameters.getClient()),
+            new URLParameter(LoginFields.SHARE_TOKEN, parameters.getShare())
+        }, failOnError);
+    }
+
+    @Override
+    public AbstractAJAXParser<? extends AutologinResponse> getParser() {
+        return new AutologinResponseParser(failOnError);
+    }
+
+    public static class AutologinParameters {
+
+        String authId, client, version, share;
+
+        public AutologinParameters(String authId, String client, String version, String share) {
+            super();
+            this.authId = authId;
+            this.client = client;
+            this.version = version;
+            this.share = share;
+        }
+
+        public String getAuthId() {
+            return authId;
+        }
+
+        public void setAuthId(String authId) {
+            this.authId = authId;
+        }
+
+        public String getClient() {
+            return client;
+        }
+
+        public void setClient(String client) {
+            this.client = client;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public void setVersion(String version) {
+            this.version = version;
+        }
+
+        public String getShare() {
+            return share;
+        }
+
+        public void setShare(String share) {
+            this.share = share;
+        }
+    }
+
+}
