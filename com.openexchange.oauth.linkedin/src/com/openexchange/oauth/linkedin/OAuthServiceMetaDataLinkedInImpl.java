a08b8cd98152afecb861af0f8fb19518242e4ca3
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2015-02-04 10:39:54 +0100

    javadoc

--- com.openexchange.oauth.linkedin/src/com/openexchange/oauth/linkedin/OAuthServiceMetaDataLinkedInImpl.java
+++ com.openexchange.oauth.linkedin/src/com/openexchange/oauth/linkedin/OAuthServiceMetaDataLinkedInImpl.java
@@ -62,6 +62,7 @@ import com.openexchange.server.ServiceLookup;
  *
  * @author <a href="mailto:karsten.will@open-xchange.com">Karsten Will</a>
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
  */
 public class OAuthServiceMetaDataLinkedInImpl extends AbstractScribeAwareOAuthServiceMetaData {
 
