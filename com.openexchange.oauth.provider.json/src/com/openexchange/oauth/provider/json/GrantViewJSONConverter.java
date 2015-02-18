2b6df49ecc517661716fa55fca27ba8ce8a35fe6
Steffen Templin <steffen.templin@open-xchange.com>
2015-02-18 18:19:56 +0100

    Add com.openexchange.oauth.provider.json

---
+++ com.openexchange.oauth.provider.json/src/com/openexchange/oauth/provider/json/GrantViewJSONConverter.java
@@ -0,0 +1,149 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2015 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.oauth.provider.json;
+
+import static com.openexchange.osgi.Tools.requireService;
+import java.util.Iterator;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.openexchange.exception.OXException;
+import com.openexchange.filemanagement.ManagedFile;
+import com.openexchange.filemanagement.ManagedFileManagement;
+import com.openexchange.i18n.Translator;
+import com.openexchange.i18n.TranslatorFactory;
+import com.openexchange.mail.mime.MimeType2ExtMap;
+import com.openexchange.oauth.provider.Client;
+import com.openexchange.oauth.provider.GrantView;
+import com.openexchange.oauth.provider.Icon;
+import com.openexchange.oauth.provider.OAuthProviderService;
+import com.openexchange.oauth.provider.OAuthScopeProvider;
+import com.openexchange.oauth.provider.Scopes;
+import com.openexchange.server.ServiceLookup;
+import com.openexchange.tools.servlet.AjaxExceptionCodes;
+import com.openexchange.tools.session.ServerSession;
+
+
+/**
+ * {@link GrantViewJSONConverter}
+ *
+ * @author <a href="mailto:steffen.templin@open-xchange.com">Steffen Templin</a>
+ * @since v7.8.0
+ */
+public class GrantViewJSONConverter {
+
+    private final ServerSession session;
+
+    private final OAuthProviderService oAuthProvider;
+
+    private final Translator translator;
+
+    private final ManagedFileManagement managedFileManagement;
+
+    public GrantViewJSONConverter(ServiceLookup services, ServerSession session) throws OXException {
+        super();
+        this.session = session;
+        oAuthProvider = requireService(OAuthProviderService.class, services);
+        translator = requireService(TranslatorFactory.class, services).translatorFor(session.getUser().getLocale());
+        managedFileManagement = requireService(ManagedFileManagement.class, services);
+    }
+
+    public JSONArray convert(Iterator<GrantView> grants) throws OXException {
+        try {
+            JSONArray json = new JSONArray();
+            while (grants.hasNext()) {
+                json.put(convertGrantView(grants.next()));
+            }
+            return json;
+        } catch (JSONException e) {
+            throw AjaxExceptionCodes.JSON_ERROR.create(e, e.getMessage());
+        }
+    }
+
+    private JSONObject convertGrantView(GrantView grant) throws JSONException, OXException {
+        JSONObject json = new JSONObject();
+        json.put("client", convertClient(grant.getClient()));
+        json.put("scopes", convertScopes(grant.getScopes()));
+        return json;
+    }
+
+    private JSONArray convertScopes(Scopes scopes) throws JSONException {
+        JSONArray json = new JSONArray();
+        for (String scope : scopes.get()) {
+            OAuthScopeProvider scopeProvider = oAuthProvider.getScopeProvider(scope);
+            if (scopeProvider == null) {
+                json.put(scope);
+            } else {
+                String description = translator.translate(scopeProvider.getDescription());
+                json.put(description);
+            }
+        }
+
+        return json;
+    }
+
+    private JSONObject convertClient(Client client) throws JSONException, OXException {
+        JSONObject json = new JSONObject();
+        json.put("id", client.getId());
+        json.put("name", client.getName());
+        json.put("description", client.getDescription());
+        json.put("website", client.getWebsite());
+        json.put("icon", buildIconURL(client.getIcon()));
+        return json;
+    }
+
+    private String buildIconURL(Icon icon) throws OXException  {
+        ManagedFile managedFile = managedFileManagement.createManagedFile(icon.getInputStream());
+        managedFile.setContentType(icon.getMimeType());
+        managedFile.setFileName(MimeType2ExtMap.getFileExtension(icon.getMimeType()));
+        return managedFile.constructURL(session);
+    }
+
+}
