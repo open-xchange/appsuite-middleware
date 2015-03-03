b06675e3e3a11c78f2a24db65314fe735e88f2ea
Thorben Betten <thorben.betten@open-xchange.com>
2015-03-03 17:21:56 +0100

    Preps for session reservation service
    Preps for session reservation service

--- com.openexchange.hazelcast.serialization/src/com/openexchange/hazelcast/serialization/CustomPortable.java
+++ com.openexchange.hazelcast.serialization/src/com/openexchange/hazelcast/serialization/CustomPortable.java
@@ -106,7 +106,9 @@ public interface CustomPortable extends Portable {
      * <li><code>  14</code>: com.openexchange.realtime.hazelcast.serialization.channel.PortableStanzaDispatcher</li>
      * <li><code>  15</code>: com.openexchange.realtime.hazelcast.serialization.cleanup.PortableCleanupDispatcher</li>
      * <li><code>  16</code>: com.openexchange.realtime.hazelcast.serialization.cleanup.PortableCleanupStatus</li>
-     * <li><code> 17</code>: com.openexchange.caching.events.ms.internal.PortableCacheKey</li>
+     * <li><code>  17</code>: com.openexchange.caching.events.ms.internal.PortableCacheKey</li>
+     *
+     * <li><code>  19</code>: com.openexchange.session.reservation.impl.portable.PortableReservation</li>
      * </ul>
      *
      * @return The class ID
