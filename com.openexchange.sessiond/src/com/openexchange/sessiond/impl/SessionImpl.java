59fae241ed20ce57b2435008e1fdd875c4d701e2
Thorben Betten <thorben.betten@open-xchange.com>
2015-03-24 16:25:55 +0100

    US90882436: Added appropriate error code for absent secret string, suppress OAuth-based account in list requests and implemented connect()/close()/ping() methods for OAuth-based file storage implementations #2

--- com.openexchange.sessiond/src/com/openexchange/sessiond/impl/SessionImpl.java
+++ com.openexchange.sessiond/src/com/openexchange/sessiond/impl/SessionImpl.java
@@ -408,7 +408,7 @@ public final class SessionImpl implements PutIfAbsent {
 
     @Override
     public String getPassword() {
-        return true ? null : password;
+        return password;
     }
 
     /**
