e0f8e35fd4b454e4a780c0bdf56bf525bd900342
Thorben Betten <thorben.betten@open-xchange.com>
2015-03-31 12:31:41 +0200

    Fixed task

--- com.openexchange.push.impl/src/com/openexchange/push/impl/credstorage/rdb/groupware/CreateCredStorageTable.java
+++ com.openexchange.push.impl/src/com/openexchange/push/impl/credstorage/rdb/groupware/CreateCredStorageTable.java
@@ -64,7 +64,7 @@ public final class CreateCredStorageTable extends AbstractCreateTableImpl {
         "user INT4 UNSIGNED NOT NULL," +
         "password VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL," +
         "login VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL," +
-        "PRIMARY KEY (cid, id, client)" +
+        "PRIMARY KEY (cid, user)" +
         ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
     }
 
