bb107d3fdf90f444b6acc18a943dfc8ca4efb00e
Thorben Betten <thorben.betten@open-xchange.com>
2015-05-11 10:29:41 +0200

    US 80624410: Do not rely on \Recent \Seen flags
    (cherry picked from commit 2c298d0123787089433535be2a178fb721d45a97)

--- com.openexchange.mobilepush.events/src/com/openexchange/mobilepush/events/mail/impl/MobilePushMailEventImpl.java
+++ com.openexchange.mobilepush.events/src/com/openexchange/mobilepush/events/mail/impl/MobilePushMailEventImpl.java
@@ -55,8 +55,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.regex.Pattern;
-import javax.mail.Flags;
-import javax.mail.Flags.Flag;
 import javax.mail.internet.InternetAddress;
 import org.osgi.service.event.Event;
 import com.openexchange.event.CommonEvent;
@@ -68,7 +66,6 @@ import com.openexchange.mail.api.IMailMessageStorage;
 import com.openexchange.mail.api.MailAccess;
 import com.openexchange.mail.dataobjects.MailFolder;
 import com.openexchange.mail.dataobjects.MailMessage;
-import com.openexchange.mail.mime.converters.MimeMessageConverter;
 import com.openexchange.mail.service.MailService;
 import com.openexchange.mailaccount.MailAccount;
 import com.openexchange.mobilepush.events.MailPushUtility;
@@ -195,18 +192,7 @@ public class MobilePushMailEventImpl implements org.osgi.service.event.EventHand
                 @SuppressWarnings("unchecked")
                 Container<MailMessage> messageInfos = (Container<MailMessage>) event.getProperty(PushEventConstants.PROPERTY_CONTAINER);
                 for (MailMessage mm : messageInfos) {
-                    Flags flags = MimeMessageConverter.convertMailFlags(mm.getFlags());
-                    if(flags.contains(Flag.RECENT) && false == flags.contains(Flag.SEEN)) {
-                        String subject = mm.getSubject();
-                        String[] senderInfo = getSenderInfo(mm);
-
-                        Map<String, Object> map = new HashMap<String, Object>(6);
-                        map.put(MailPushUtility.KEY_CID, generateCidFor(mm, accountId));
-                        map.put(MailPushUtility.KEY_SUBJECT, Strings.isEmpty(subject) ? "(no subject)" : subject);
-                        map.put(MailPushUtility.KEY_SENDER, Strings.isEmpty(senderInfo[0]) ? senderInfo[1] : senderInfo[0]);
-                        map.put(MailPushUtility.KEY_UNREAD, Integer.valueOf(mm.getUnreadMessages()));
-                        props.add(map);
-                    }
+                    addNewMailProperties(mm, accountId, -1, props);
                 }
             } else if (event.containsProperty(PushEventConstants.PROPERTY_IDS)) {
                 // Check if its a new mail event
@@ -221,19 +207,7 @@ public class MobilePushMailEventImpl implements org.osgi.service.event.EventHand
                         int unread = mailAccess.getUnreadMessagesCount(INBOX);
                         if (mms != null) {
                             for (MailMessage mm : mms) {
-                                Flags flags = MimeMessageConverter.convertMailFlags(mm.getFlags());
-
-                                if(flags.contains(Flag.RECENT) && false == flags.contains(Flag.SEEN)) {
-                                    String subject = mm.getSubject();
-                                    String[] senderInfo = getSenderInfo(mm);
-
-                                    Map<String, Object> map = new HashMap<String, Object>(6);
-                                    map.put(MailPushUtility.KEY_CID, generateCidFor(mm, accountId));
-                                    map.put(MailPushUtility.KEY_SUBJECT, Strings.isEmpty(subject) ? "(no subject)" : subject);
-                                    map.put(MailPushUtility.KEY_SENDER, Strings.isEmpty(senderInfo[0]) ? senderInfo[1] : senderInfo[0]);
-                                    map.put(MailPushUtility.KEY_UNREAD, Integer.valueOf(unread));
-                                    props.add(map);
-                                }
+                                addNewMailProperties(mm, accountId, unread, props);
                             }
                         }
                     } catch (OXException e) {
@@ -251,6 +225,18 @@ public class MobilePushMailEventImpl implements org.osgi.service.event.EventHand
             }
         }
         return props;
+    }
+
+    private void addNewMailProperties(MailMessage mm, int accountId, int unread, List<Map<String, Object>> props) {
+        String subject = mm.getSubject();
+        String[] senderInfo = getSenderInfo(mm);
+
+        Map<String, Object> map = new HashMap<String, Object>(6);
+        map.put(MailPushUtility.KEY_CID, generateCidFor(mm, accountId));
+        map.put(MailPushUtility.KEY_SUBJECT, Strings.isEmpty(subject) ? "(no subject)" : subject);
+        map.put(MailPushUtility.KEY_SENDER, Strings.isEmpty(senderInfo[0]) ? senderInfo[1] : senderInfo[0]);
+        map.put(MailPushUtility.KEY_UNREAD, Integer.valueOf(unread < 0 ? mm.getUnreadMessages() : unread));
+        props.add(map);
     }
 
     private String[] getSenderInfo(MailMessage m) {
