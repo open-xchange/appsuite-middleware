6076c091b5fec53478b08bf9837b927fd9c52947
Thorben Betten <thorben.betten@open-xchange.com>
2015-05-11 16:38:06 +0200

    Do log HTTP method, too
    (cherry picked from commit 65fd1571df7bc8e841467fab43983559f4037c26)

--- com.openexchange.global/src/com/openexchange/log/LogProperties.java
+++ com.openexchange.global/src/com/openexchange/log/LogProperties.java
@@ -147,6 +147,10 @@ public final class LogProperties {
          */
         GRIZZLY_QUERY_STRING("com.openexchange.grizzly.queryString"),
         /**
+         * com.openexchange.grizzly.method
+         */
+        GRIZZLY_METHOD("com.openexchange.grizzly.method"),
+        /**
          * com.openexchange.grizzly.servletPath
          * <p>
          * The part of this request's URL that calls the servlet. This path starts with a "/" character and includes either the servlet name
@@ -494,8 +498,9 @@ public final class LogProperties {
         if (null == tempFilePath) {
             return;
         }
-        String prev = MDC.get(Name.TEMP_FILE.getName());
-        MDC.put(Name.TEMP_FILE.getName(), null == prev ? tempFilePath : new StringBuffer(prev).append(',').append(tempFilePath).toString());
+        String sName = Name.TEMP_FILE.getName();
+        String prev = MDC.get(sName);
+        MDC.put(sName, null == prev ? tempFilePath : new StringBuilder(prev).append(',').append(tempFilePath).toString());
     }
 
     /**
@@ -507,8 +512,9 @@ public final class LogProperties {
         if (null == name || null == value) {
             return;
         }
-        String prev = MDC.get(name.getName());
-        MDC.put(name.getName(), null == prev ? value : new StringBuffer(prev).append(',').append(value).toString());
+        String sName = name.getName();
+        String prev = MDC.get(sName);
+        MDC.put(sName, null == prev ? value : new StringBuilder(prev).append(',').append(value).toString());
     }
 
     /**
