8a0d96ac8fbcf550cd97fb8d91de9cb2955eeb7b
Martin Herfurth <martin.herfurth@open-xchange.com>
2015-08-04 10:46:57 +0200

    Harmonize recurring calculation.

--- com.openexchange.calendar/src/com/openexchange/calendar/CalendarOperation.java
+++ com.openexchange.calendar/src/com/openexchange/calendar/CalendarOperation.java
@@ -1995,7 +1995,7 @@ public class CalendarOperation implements SearchIterator<CalendarDataObject> {
             retval = CalendarCollectionService.CHANGE_RECURRING_TYPE;
         } else {
             calculateAndSetRealRecurringStartAndEndDate(cdao, edao);
-            //checkAndRemoveRecurrenceFields(cdao);
+            cdao.setEndDate(calculateRealRecurringEndDate(cdao, edao));
             cdao.setRecurrence(edao.getRecurrence());
             /*
              * Return specified recurring action unchanged
