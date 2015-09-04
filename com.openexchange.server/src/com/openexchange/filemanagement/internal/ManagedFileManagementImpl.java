71b9f9ea00b8d86259696d36451b80d18a76fe56
Thorben Betten <thorben.betten@open-xchange.com>
2015-09-04 11:42:10 +0200

    Get proper file name

--- com.openexchange.server/src/com/openexchange/filemanagement/internal/ManagedFileManagementImpl.java
+++ com.openexchange.server/src/com/openexchange/filemanagement/internal/ManagedFileManagementImpl.java
@@ -173,10 +173,10 @@ public final class ManagedFileManagementImpl implements ManagedFileManagement {
                         int optTimeToLive = cur.optTimeToLive();
                         long timeElapsed = now - cur.getLastAccess();
                         if (cur.isDeleted() || (timeElapsed > (optTimeToLive > 0 ? optTimeToLive : time2live))) {
+                            File file = cur.getFile();
+                            String fname = null != file && file.exists() ? file.getName() : "";
                             cur.delete();
                             iter.remove();
-                            File file = cur.getFile();
-                            String fname = null == file ? "" : file.getName();
                             logger.debug("Removed expired managed file {}", fname);
                         } else {
                             // Use getFileName() so that the underlying ManagedFile is not 'touched' again (something that will reset the LastAccess timestamp)
