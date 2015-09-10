d6503aa79a16f316d554eabfeff2a6b36f050dc7
Martin Schneider <martin.schneider@open-xchange.com>
2015-09-10 12:37:49 +0200

    Change for bug 40907: dedicated methods for password/password mech changes (adapt simservice)
    (cherry picked from commit f2eca09af5b40f25c918560957a0eb43d9924505)

--- com.openexchange.server/sim/com/openexchange/user/SimUserService.java
+++ com.openexchange.server/sim/com/openexchange/user/SimUserService.java
@@ -313,4 +313,16 @@ public class SimUserService implements UserService {
 
     }
 
+    @Override
+    public void updatePassword(User user, Context context) throws OXException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void updatePassword(Connection connection, User user, Context context) throws OXException {
+        // TODO Auto-generated method stub
+
+    }
+
 }
