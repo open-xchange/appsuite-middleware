c713b76875a3e828787ff53e4d6211061fb12625
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2015-10-23 12:08:35 +0200

    change read to write connections for 'FOR UPDATE' statement

--- com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXContextMySQLStorageCommon.java
+++ com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXContextMySQLStorageCommon.java
@@ -323,7 +323,7 @@ public class OXContextMySQLStorageCommon {
     public static void deleteEmptySchema(int poolId, String dbSchema) throws StorageException {
         final Connection con;
         try {
-            con = cache.getReadConnectionForConfigDB();
+            con = cache.getWriteConnectionForConfigDB();
         } catch (PoolException e) {
             throw new StorageException(e.getMessage(), e);
         }
@@ -343,7 +343,7 @@ public class OXContextMySQLStorageCommon {
         } finally {
             autocommit(con);
             try {
-                cache.pushReadConnectionForConfigDB(con);
+                cache.pushWriteConnectionForConfigDB(con);
             } catch (final PoolException e) {
                 log.error("Error pushing configdb connection to pool!", e);
             }
