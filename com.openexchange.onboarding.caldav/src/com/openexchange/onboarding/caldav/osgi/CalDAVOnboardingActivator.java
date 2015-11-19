101e2adda1edb38814e39812c21fc36da32de789
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2015-11-19 13:24:55 +0100

    US107189790: Added NotificationMailFactory to needed services

--- com.openexchange.onboarding.caldav/src/com/openexchange/onboarding/caldav/osgi/CalDAVOnboardingActivator.java
+++ com.openexchange.onboarding.caldav/src/com/openexchange/onboarding/caldav/osgi/CalDAVOnboardingActivator.java
@@ -53,6 +53,7 @@ import com.openexchange.capabilities.CapabilityService;
 import com.openexchange.config.ConfigurationService;
 import com.openexchange.config.cascade.ConfigViewFactory;
 import com.openexchange.mail.service.MailService;
+import com.openexchange.notification.mail.NotificationMailFactory;
 import com.openexchange.onboarding.OnboardingConfiguration;
 import com.openexchange.onboarding.caldav.CalDAVOnboardingConfiguration;
 import com.openexchange.osgi.HousekeepingActivator;
@@ -75,7 +76,7 @@ public class CalDAVOnboardingActivator extends HousekeepingActivator {
 
     @Override
     protected Class<?>[] getNeededServices() {
-        return new Class<?>[] { ConfigViewFactory.class, ConfigurationService.class, CapabilityService.class };
+        return new Class<?>[] { ConfigViewFactory.class, ConfigurationService.class, CapabilityService.class, NotificationMailFactory.class };
     }
 
     @Override
