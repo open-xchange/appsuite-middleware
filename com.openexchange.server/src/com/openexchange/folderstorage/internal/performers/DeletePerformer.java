42eaec189ec9c622b5b662c827877637c6eefcae
Thorben Betten <thorben.betten@open-xchange.com>
2015-12-04 23:34:37 +0100

    Make TransactionManager publicly visible
    (cherry picked from commit c0a8b873c168b06d70700cf61b1cb03e27c23435)

--- com.openexchange.server/src/com/openexchange/folderstorage/internal/performers/DeletePerformer.java
+++ com.openexchange.server/src/com/openexchange/folderstorage/internal/performers/DeletePerformer.java
@@ -64,7 +64,7 @@ import com.openexchange.folderstorage.SortableId;
 import com.openexchange.folderstorage.StorageParameters;
 import com.openexchange.folderstorage.cache.CacheFolderStorageRegistry;
 import com.openexchange.folderstorage.internal.CalculatePermission;
-import com.openexchange.folderstorage.internal.TransactionManager;
+import com.openexchange.folderstorage.tx.TransactionManager;
 import com.openexchange.folderstorage.virtual.VirtualFolderStorage;
 import com.openexchange.groupware.contexts.Context;
 import com.openexchange.groupware.ldap.User;
