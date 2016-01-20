a3f69f42102af47a548fc120b438f5ec70273cd2
Thorben Betten <thorben.betten@open-xchange.com>
2016-01-20 17:25:14 +0100

    Fixed tests

--- com.openexchange.server/test/com/openexchange/groupware/infostore/search/impl/ToMySqlQueryVisitorTest.java
+++ com.openexchange.server/test/com/openexchange/groupware/infostore/search/impl/ToMySqlQueryVisitorTest.java
@@ -136,8 +136,7 @@ public class ToMySqlQueryVisitorTest extends TestCase {
         visitor.visit(dtz);
         String result = visitor.getMySqlQuery();
         assertFalse("Unneccessary whitespaces in query", MULTIPLE_WHITESPACE.matcher(result).matches());
-        assertTrue("Unexpected SQL query", result.endsWith("AND (infostore.folder_id=119 OR (infostore.created_by=1 AND "
-            + "infostore.folder_id=120)) AND infostore_document.description LIKE '%bluber blah_foo%'"));
+        assertTrue("Unexpected SQL query", result.endsWith("AND infostore_document.description LIKE '%bluber blah_foo%'"));
     }
 
     public void testWithoutAllFolders() {
@@ -147,16 +146,14 @@ public class ToMySqlQueryVisitorTest extends TestCase {
         String result = visitor.getMySqlQuery();
         assertFalse("Invalid SQL query", WRONG_OPERATORS.matcher(result).matches());
         assertFalse("Unneccessary whitespaces in query", MULTIPLE_WHITESPACE.matcher(result).matches());
-        assertTrue("Unexpected SQL query", result.endsWith("AND ((infostore.created_by=1 AND "
-            + "infostore.folder_id=120)) AND infostore_document.description LIKE '%bluber blah_foo%'"));
+        assertTrue("Unexpected SQL query", result.endsWith("AND infostore_document.description LIKE '%bluber blah_foo%'"));
 
         visitor = new ToMySqlQueryVisitor(new int[0], new int[] {120}, 1, 1, "SELECT field01");
         visitor.visit(dtz);
         result = visitor.getMySqlQuery();
         assertFalse("Invalid SQL query", WRONG_OPERATORS.matcher(result).matches());
         assertFalse("Unneccessary whitespaces in query", MULTIPLE_WHITESPACE.matcher(result).matches());
-        assertTrue("Unexpected SQL query", result.endsWith("AND ((infostore.created_by=1 AND "
-            + "infostore.folder_id=120)) AND infostore_document.description LIKE '%bluber blah_foo%'"));
+        assertTrue("Unexpected SQL query", result.endsWith("AND infostore_document.description LIKE '%bluber blah_foo%'"));
     }
 
     public void testWithoutOwnFolders() {
@@ -166,17 +163,16 @@ public class ToMySqlQueryVisitorTest extends TestCase {
         String result = visitor.getMySqlQuery();
         assertFalse("Invalid SQL query", WRONG_OPERATORS.matcher(result).matches());
         assertFalse("Unneccessary whitespaces in query", MULTIPLE_WHITESPACE.matcher(result).matches());
-        assertTrue("Unexpected SQL query", result.endsWith("AND (infostore.folder_id=119) AND infostore_document.description LIKE '%bluber blah_foo%'"));
+        assertTrue("Unexpected SQL query", result.endsWith("AND infostore_document.description LIKE '%bluber blah_foo%'"));
 
         visitor = new ToMySqlQueryVisitor(new int[] { 119 }, new int[0], 1, 1, "SELECT field01");
         visitor.visit(dtz);
         result = visitor.getMySqlQuery();
         assertFalse("Invalid SQL query", WRONG_OPERATORS.matcher(result).matches());
         assertFalse("Unneccessary whitespaces in query", MULTIPLE_WHITESPACE.matcher(result).matches());
-        assertTrue("Unexpected SQL query", result.endsWith("AND (infostore.folder_id=119) AND infostore_document.description LIKE '%bluber blah_foo%'"));
+        assertTrue("Unexpected SQL query", result.endsWith("AND infostore_document.description LIKE '%bluber blah_foo%'"));
     }
 
-    private final String EXPECTED1 = "SELECT field01 FROM infostore JOIN infostore_document ON infostore_document.cid = infostore.cid AND infostore_document.infostore_id = infostore.id AND infostore_document.version_number = infostore.version WHERE infostore.cid = 1 AND (infostore.folder_id=119 OR (infostore.created_by=1 AND infostore.folder_id=120)) AND UPPER(infostore_document.filename) LIKE UPPER('%test123%') LIMIT 0,5";
     public void testWithLimit() {
         FileNameTerm term = new FileNameTerm("test123");
         ToMySqlQueryVisitor visitor = new ToMySqlQueryVisitor(new int[] { 119 }, new int[] { 120 }, 1, 1, "SELECT field01", null, InfostoreSearchEngine.NOT_SET, 0, 5);
@@ -184,10 +180,9 @@ public class ToMySqlQueryVisitorTest extends TestCase {
         String result = visitor.getMySqlQuery();
         assertFalse("Invalid SQL query", WRONG_OPERATORS.matcher(result).matches());
         assertFalse("Unneccessary whitespaces in query", MULTIPLE_WHITESPACE.matcher(result).matches());
-        assertTrue("Unexpected SQL query", result.equals(EXPECTED1));
+        assertTrue("Unexpected SQL query", result.endsWith("WHERE infostore.cid = 1 AND infostore.created_by=1 AND UPPER(infostore_document.filename) LIKE UPPER('%test123%') LIMIT 0,5"));
     }
 
-    private final String EXPECTED2 = "SELECT field01 FROM infostore JOIN infostore_document ON infostore_document.cid = infostore.cid AND infostore_document.infostore_id = infostore.id AND infostore_document.version_number = infostore.version WHERE infostore.cid = 1 AND (infostore.folder_id=119 OR (infostore.created_by=1 AND infostore.folder_id=120)) AND UPPER(infostore_document.filename) LIKE UPPER('%test123%') ORDER BY last_modified ASC LIMIT 0,5";
     public void testWithLimitAndOrder() {
         FileNameTerm term = new FileNameTerm("test123");
         ToMySqlQueryVisitor visitor = new ToMySqlQueryVisitor(new int[] { 119 }, new int[] { 120 }, 1, 1, "SELECT field01", Metadata.LAST_MODIFIED_LITERAL, InfostoreSearchEngine.ASC, 0, 5);
@@ -195,7 +190,7 @@ public class ToMySqlQueryVisitorTest extends TestCase {
         String result = visitor.getMySqlQuery();
         assertFalse("Invalid SQL query", WRONG_OPERATORS.matcher(result).matches());
         assertFalse("Unneccessary whitespaces in query", MULTIPLE_WHITESPACE.matcher(result).matches());
-        assertTrue("Unexpected SQL query", result.equals(EXPECTED2));
+        assertTrue("Unexpected SQL query", result.endsWith("WHERE infostore.cid = 1 AND infostore.created_by=1 AND UPPER(infostore_document.filename) LIKE UPPER('%test123%') ORDER BY last_modified ASC LIMIT 0,5"));
     }
 
     // see bug 32874
