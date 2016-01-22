3944032a22535f358e21ac87ca2b21faa9ac3e98
Thorben Betten <thorben.betten@open-xchange.com>
2016-01-22 15:39:38 +0100

    Fix for bug 43556: Deal with negative UIDs
    (cherry picked from commit 874f9fc5ac7c6468e7a025f69ac79bca15e0f3fc)

--- com.openexchange.imap/src/com/openexchange/imap/IMAPMessageStorage.java
+++ com.openexchange.imap/src/com/openexchange/imap/IMAPMessageStorage.java
@@ -202,6 +202,14 @@ import com.sun.mail.imap.Rights;
 import com.sun.mail.imap.protocol.BODYSTRUCTURE;
 import com.sun.mail.util.MessageRemovedIOException;
 import com.sun.mail.util.ReadableMime;
+import gnu.trove.list.TLongList;
+import gnu.trove.list.array.TLongArrayList;
+import gnu.trove.map.TLongIntMap;
+import gnu.trove.map.TLongObjectMap;
+import gnu.trove.map.hash.TLongObjectHashMap;
+import net.htmlparser.jericho.Renderer;
+import net.htmlparser.jericho.Segment;
+import net.htmlparser.jericho.Source;
 
 /**
  * {@link IMAPMessageStorage} - The IMAP implementation of message storage.
@@ -694,11 +702,12 @@ public final class IMAPMessageStorage extends IMAPFolderWorker implements IMailM
             final MailMessage[] messages;
             final MailField[] fields = fieldSet.toArray();
             if (imapConfig.asMap().containsKey("UIDPLUS")) {
+                long[] valids = filterNegativeElements(uids);
                 final TLongObjectHashMap<MailMessage> fetchedMsgs =
                     fetchValidWithFallbackFor(
                         fullName,
-                        uids,
-                        uids.length,
+                        valids,
+                        valids.length,
                         getFetchProfile(fields, headerNames, null, null, getIMAPProperties().isFastFetch()),
                         imapConfig.getImapCapabilities().hasIMAP4rev1(),
                         false);
@@ -710,7 +719,8 @@ public final class IMAPMessageStorage extends IMAPFolderWorker implements IMailM
                     messages[i] = fetchedMsgs.get(uids[i]);
                 }
             } else {
-                final TLongIntMap seqNumsMap = IMAPCommandsCollection.uids2SeqNumsMap(imapFolder, uids);
+                long[] valids = filterNegativeElements(uids);
+                final TLongIntMap seqNumsMap = IMAPCommandsCollection.uids2SeqNumsMap(imapFolder, valids);
                 final TLongObjectMap<MailMessage> fetchedMsgs =
                     fetchValidWithFallbackFor(
                         fullName,
@@ -840,10 +850,11 @@ public final class IMAPMessageStorage extends IMAPFolderWorker implements IMailM
         mailInterfaceMonitor.addUseTime(time);
         LOG.debug("IMAP fetch for {} messages took {}msec", Integer.valueOf(len), Long.valueOf(time));
         for (final MailMessage mailMessage : tmp) {
-            final IDMailMessage idmm = (IDMailMessage) mailMessage;
-            if (null != idmm) {
+            if (null != mailMessage) {
+                IDMailMessage idmm = (IDMailMessage) mailMessage;
                 map.put(seqnum ? idmm.getSeqnum() : idmm.getUid(), idmm);
             }
+
         }
         return map;
     }
@@ -4497,14 +4508,15 @@ public final class IMAPMessageStorage extends IMAPFolderWorker implements IMailM
         return map;
     }
 
-    private static <E> List<E> filterNullElements(final E[] elements) {
+    private static <E> List<E> filterNullElements(E[] elements) {
         if (null == elements) {
             return Collections.emptyList();
         }
-        final int length = elements.length;
-        final List<E> list = new LinkedList<E>();
+
+        int length = elements.length;
+        List<E> list = new ArrayList<E>(length);
         for (int i = 0; i < length; i++) {
-            final E elem = elements[i];
+            E elem = elements[i];
             if (null != elem) {
                 list.add(elem);
             }
@@ -4512,7 +4524,48 @@ public final class IMAPMessageStorage extends IMAPFolderWorker implements IMailM
         return list;
     }
 
+<<<<<<< HEAD
     private static FetchProfile checkFetchProfile(FetchProfile fetchProfile) {
+=======
+    private static long[] filterNegativeElements(long[] uids) {
+        if (null == uids) {
+            return null;
+        }
+
+        int i = 0;
+        boolean fine = true;
+        while (fine && i < uids.length) {
+            if (uids[i] < 0) {
+                fine = false;
+            } else {
+                i++;
+            }
+        }
+
+        if (fine) {
+            return uids;
+        }
+
+        TLongList valids = new TLongArrayList(uids.length);
+        valids.add(uids, 0, i);
+        for (int j = i + 1; j < uids.length; j++) {
+            long uid = uids[j];
+            if (uid >= 0) {
+                valids.add(uid);
+            }
+        }
+        return valids.toArray();
+    }
+
+    /**
+     * Checks given fetch profile to only contain fetch items and no single headers<br>
+     * In case {@link IMAPProperties#allowFetchSingleHeaders()} signals <code>true</code>.
+     *
+     * @param fetchProfile The fetch profile to check
+     * @return The checked fetch profile
+     */
+    protected static FetchProfile checkFetchProfile(FetchProfile fetchProfile) {
+>>>>>>> 874f9fc... Fix for bug 43556: Deal with negative UIDs
         if (null == fetchProfile || IMAPProperties.getInstance().allowFetchSingleHeaders()) {
             return fetchProfile;
         }
