4dda1f0f0718267ea471983bd6a0704876acb89a
Thorben Betten <thorben.betten@open-xchange.com>
2016-02-01 15:29:23 +0100

    US 107189478: Aligned wording

--- com.openexchange.client.onboarding/src/com/openexchange/client/onboarding/notification/OnboardingNotificationStrings.java
+++ com.openexchange.client.onboarding/src/com/openexchange/client/onboarding/notification/OnboardingNotificationStrings.java
@@ -68,12 +68,9 @@ public class OnboardingNotificationStrings implements LocalizableStrings {
     public static final String SALUTATION = "Dear %1$s,";
 
     // The content of the E-Mail providing the profile attachment
-    public static final String CONTENT = "to automatically configure your device, please download & install the configuration profile, which is attached to this E-Mail.";
+    public static final String CONTENT = "to automatically configure your device, please download & install the attached configuration profile.";
 
     // The subject of the E-Mail providing the profile attachment
-    public static final String SUBJECT = "Your configuration profile";
-
-    // Your on-boarding information.
-    public static final String DEFAULT = "Your onboarding information.";
+    public static final String SUBJECT = "Your device configuration";
 
 }
