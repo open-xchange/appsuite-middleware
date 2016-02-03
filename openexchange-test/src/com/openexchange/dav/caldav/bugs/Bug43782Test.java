309f49dffa8660d6acc226ece77dcdfc82936864
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2016-02-03 14:55:02 +0100

    more reminder update tests

--- openexchange-test/src/com/openexchange/dav/caldav/bugs/Bug43782Test.java
+++ openexchange-test/src/com/openexchange/dav/caldav/bugs/Bug43782Test.java
@@ -61,6 +61,7 @@ import com.openexchange.dav.SyncToken;
 import com.openexchange.dav.caldav.CalDAVTest;
 import com.openexchange.dav.caldav.ICalResource;
 import com.openexchange.dav.caldav.ical.SimpleICal.Component;
+import com.openexchange.dav.caldav.ical.SimpleICal.Property;
 import com.openexchange.groupware.calendar.TimeTools;
 import com.openexchange.groupware.container.Appointment;
 
@@ -74,7 +75,26 @@ import com.openexchange.groupware.container.Appointment;
 public class Bug43782Test extends CalDAVTest {
 
     @Test
-    public void testUpdateWithReminder() throws Exception {
+    public void testUpdateSummary() throws Exception {
+        testUpdateSummary(true);
+    }
+
+    @Test
+    public void testUpdateSummaryRemovingAcknowledged() throws Exception {
+        testUpdateSummary(false);
+    }
+
+    @Test
+    public void testUpdateStartAndEnd() throws Exception {
+        testUpdateStartAndEnd(true);
+    }
+
+    @Test
+    public void testUpdateStartAndEndRemovingAcknowledged() throws Exception {
+        testUpdateStartAndEnd(false);
+    }
+
+    private void testUpdateSummary(boolean preserveAcknowledged) throws Exception {
         /*
          * fetch sync token for later synchronization
          */
@@ -109,7 +129,9 @@ public class Bug43782Test extends CalDAVTest {
          */
         summary += "_edit";
         iCalResource.getVEvent().setSummary(summary);
-        iCalResource.getVEvent().getVAlarm().removeProperties("ACKNOWLEDGED");
+        if (false == preserveAcknowledged) {
+            iCalResource.getVEvent().getVAlarm().removeProperties("ACKNOWLEDGED");
+        }
         assertEquals("response code wrong", StatusCodes.SC_CREATED, putICalUpdate(iCalResource));
         /*
          * verify appointment on server
@@ -117,6 +139,74 @@ public class Bug43782Test extends CalDAVTest {
         appointment = getAppointment(uid);
         assertNotNull("appointment not found on server", appointment);
         assertEquals("Title wrong", summary, appointment.getTitle());
+        assertEquals("Start wrong", start, appointment.getStartDate());
+        assertEquals("End wrong", end, appointment.getEndDate());
+        assertTrue("No reminder found", appointment.containsAlarm());
+        assertEquals("Reminder wrong", 15, appointment.getAlarm());
+        /*
+         * verify appointment on client
+         */
+        iCalResource = get(uid);
+        assertNotNull("No VEVENT in iCal found", iCalResource.getVEvent());
+        assertEquals("UID wrong", uid, iCalResource.getVEvent().getUID());
+        assertEquals("SUMMARY wrong", summary, iCalResource.getVEvent().getSummary());
+        assertEquals("LOCATION wrong", location, iCalResource.getVEvent().getLocation());
+        vAlarms = iCalResource.getVEvent().getVAlarms();
+        assertEquals("Unexpected number of VALARMs found", 1, vAlarms.size());
+        assertEquals("ALARM wrong", "-PT15M", vAlarms.get(0).getPropertyValue("TRIGGER"));
+    }
+
+    private void testUpdateStartAndEnd(boolean preserveAcknowledged) throws Exception {
+        /*
+         * fetch sync token for later synchronization
+         */
+        SyncToken syncToken = new SyncToken(fetchSyncToken());
+        /*
+         * create appointment on server
+         */
+        String uid = randomUID();
+        String summary = "test";
+        String location = "ort";
+        Date start = TimeTools.D("next tuesday at 12:30");
+        Date end = TimeTools.D("next tuesday at 13:45");
+        Appointment appointment = generateAppointment(start, end, uid, summary, location);
+        appointment.setAlarm(15);
+        appointment.setAlarmFlag(true);
+        rememberForCleanUp(create(appointment));
+        /*
+         * verify appointment on client
+         */
+        Map<String, String> eTags = syncCollection(syncToken).getETagsStatusOK();
+        assertTrue("no resource changes reported on sync collection", 0 < eTags.size());
+        List<ICalResource> calendarData = calendarMultiget(eTags.keySet());
+        ICalResource iCalResource = assertContains(uid, calendarData);
+        assertNotNull("No VEVENT in iCal found", iCalResource.getVEvent());
+        assertEquals("SUMMARY wrong", summary, iCalResource.getVEvent().getSummary());
+        assertEquals("LOCATION wrong", location, iCalResource.getVEvent().getLocation());
+        List<Component> vAlarms = iCalResource.getVEvent().getVAlarms();
+        assertEquals("Unexpected number of VALARMs found", 1, vAlarms.size());
+        assertEquals("ALARM wrong", "-PT15M", vAlarms.get(0).getPropertyValue("TRIGGER"));
+        /*
+         * update appointment on client
+         */
+        start = TimeTools.D("next tuesday at 11:30");
+        Property dtStart = iCalResource.getVEvent().getProperty("DTSTART");
+        iCalResource.getVEvent().setProperty("DTSTART", format(start, dtStart.getAttribute("TZID")), dtStart.getAttributes());
+        end = TimeTools.D("next tuesday at 12:45");
+        Property dtEnd = iCalResource.getVEvent().getProperty("DTEND");
+        iCalResource.getVEvent().setProperty("DTEND", format(end, dtEnd.getAttribute("TZID")), dtEnd.getAttributes());
+        if (false == preserveAcknowledged) {
+            iCalResource.getVEvent().getVAlarm().removeProperties("ACKNOWLEDGED");
+        }
+        assertEquals("response code wrong", StatusCodes.SC_CREATED, putICalUpdate(iCalResource));
+        /*
+         * verify appointment on server
+         */
+        appointment = getAppointment(uid);
+        assertNotNull("appointment not found on server", appointment);
+        assertEquals("Title wrong", summary, appointment.getTitle());
+        assertEquals("Start wrong", start, appointment.getStartDate());
+        assertEquals("End wrong", end, appointment.getEndDate());
         assertTrue("No reminder found", appointment.containsAlarm());
         assertEquals("Reminder wrong", 15, appointment.getAlarm());
         /*
