fed2690d186a93437e983325c52b57de0f22e64d
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2016-02-15 16:24:48 +0100

    Bug #43941: Changed c.o.file.storage.json.actions.accounts.AllAction to return all filestorage accounts and exceptions.
    Exceptions will be appended to the filestorageaccounts where they occur.
    Accounts with errors will be marked with 'hasError':'true'
    
    Introduced the interface LoginAwareFileStorageServiceExtension to test
    the connection of accounts which does not depend on oauth accounts. The
    connections of this accounts will be tested when they are created or
    updated.

--- com.openexchange.file.storage.json/src/com/openexchange/file/storage/json/actions/accounts/AllAction.java
+++ com.openexchange.file.storage.json/src/com/openexchange/file/storage/json/actions/accounts/AllAction.java
@@ -55,6 +55,8 @@ import java.util.List;
 import java.util.Set;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.openexchange.ajax.requesthandler.AJAXRequestData;
 import com.openexchange.ajax.requesthandler.AJAXRequestResult;
 import com.openexchange.exception.OXException;
@@ -81,6 +83,8 @@ import com.openexchange.tools.session.ServerSession;
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
 public class AllAction extends AbstractFileStorageAccountAction {
+
+    static final Logger LOG = LoggerFactory.getLogger(AllAction.class.getName());
 
     public AllAction(final FileStorageServiceRegistry registry) {
         super(registry);
@@ -110,11 +114,11 @@ public class AllAction extends AbstractFileStorageAccountAction {
                 userAccounts = fsService.getAccountManager().getAccounts(session);
             }
 
-
             // Iterate accounts and append its JSON representation
             for (FileStorageAccount account : userAccounts) {
+                FileStorageAccountAccess access = null;
                 try {
-                    FileStorageAccountAccess access = fsService.getAccountAccess(account.getId(), session);
+                    access = fsService.getAccountAccess(account.getId(), session);
                     FileStorageFolder rootFolder = access.getRootFolder();
 
                     if (null != rootFolder) {
@@ -141,6 +145,7 @@ public class AllAction extends AbstractFileStorageAccountAction {
                         result.put(writer.write(account, rootFolder, caps));
                     }
                 } catch (OXException e) {
+                    LOG.debug(e.getMessage());
                     if (e.equalsCode(6, "OAUTH")) {
                         // "OAUTH-0006" --> OAuth account not found
                         try {
@@ -149,8 +154,31 @@ public class AllAction extends AbstractFileStorageAccountAction {
                             // Ignore
                         }
                     } else {
-                        // Set as error
-                        requestResult.setException(e);
+                        //add account with error
+                        Set<String> caps = null;
+                        if (access != null) {
+                            // Check file storage capabilities
+                            if (access instanceof CapabilityAware) {
+                                caps = new HashSet<String>();
+                                CapabilityAware capabilityAware = (CapabilityAware) access;
+                                Boolean supported = capabilityAware.supports(FileStorageCapability.FILE_VERSIONS);
+                                if (null != supported && supported.booleanValue()) {
+                                    caps.add(FileStorageCapability.FILE_VERSIONS.name());
+                                }
+                                supported = capabilityAware.supports(FileStorageCapability.EXTENDED_METADATA);
+                                if (null != supported && supported.booleanValue()) {
+                                    caps.add(FileStorageCapability.EXTENDED_METADATA.name());
+                                }
+
+                                if (((CapabilityAware) access).supports(FileStorageCapability.RANDOM_FILE_ACCESS)) {
+                                    caps.add(FileStorageCapability.RANDOM_FILE_ACCESS.name());
+                                }
+                                if (((CapabilityAware) access).supports(FileStorageCapability.LOCKS)) {
+                                    caps.add(FileStorageCapability.LOCKS.name());
+                                }
+                            }
+                        }
+                        result.put(writer.write(account, null, caps, e, session));
                     }
                 }
             }
