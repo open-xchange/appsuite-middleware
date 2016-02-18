66c11da4a143b5c7290ad804260080ada2a2d910
Thorben Betten <thorben.betten@open-xchange.com>
2016-02-18 09:01:17 +0100

    Fixed range check

--- com.openexchange.filestore.swift/src/com/openexchange/filestore/swift/SwiftFileStorage.java
+++ com.openexchange.filestore.swift/src/com/openexchange/filestore/swift/SwiftFileStorage.java
@@ -316,7 +316,7 @@ public class SwiftFileStorage implements FileStorage {
              * download parts of a single chunk
              */
             Chunk chunk = chunks.get(0);
-            if (offset > chunk.getLength() || -1 != length && length > chunk.getLength() - offset) {
+            if (offset >= chunk.getLength() || -1 != length && length > chunk.getLength() - offset) {
                 throw FileStorageCodes.INVALID_RANGE.create(offset, length, name, chunk.getLength());
             }
             long rangeStart = 0 < offset ? offset : 0;
@@ -329,7 +329,7 @@ public class SwiftFileStorage implements FileStorage {
          */
         Chunk lastChunk = chunks.get(size - 1);
         long totalLength = lastChunk.getOffset() + lastChunk.getLength();
-        if (offset > totalLength || -1 != length && length > totalLength - offset) {
+        if (offset >= totalLength || -1 != length && length > totalLength - offset) {
             throw FileStorageCodes.INVALID_RANGE.create(offset, length, name, totalLength);
         }
         long rangeStart = 0 < offset ? offset : 0;
