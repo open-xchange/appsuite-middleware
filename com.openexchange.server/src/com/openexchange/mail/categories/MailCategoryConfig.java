b4fa3749fdd348997f462f7716f9960ef16e52a9
Thorben Betten <thorben.betten@open-xchange.com>
2016-03-07 17:10:44 +0100

    MW-167: Minor preparations for mail category configuration (https://jira.open-xchange.com/browse/MW-167)

---
+++ com.openexchange.server/src/com/openexchange/mail/categories/MailCategoryConfig.java
@@ -0,0 +1,240 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2020 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.mail.categories;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * {@link MailCategoryConfig}
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @since v7.8.1
+ */
+public class MailCategoryConfig {
+
+    /**
+     * Builder for {@link MailCategoryConfig} instances;
+     */
+    public static class Builder {
+
+        private String flag;
+        private boolean force;
+        private boolean active;
+        private String name;
+        private Map<Locale, String> names;
+
+        /**
+         * Initializes a new {@link Builder}.
+         */
+        public Builder() {
+            super();
+        }
+
+        /**
+         * Sets the flag name.
+         *
+         * @param flag The flag name
+         * @return This builder
+         */
+        public Builder flag(String flag) {
+            this.flag = flag;
+            return this;
+        }
+
+        /**
+         * Sets the <code>force</code> flag.
+         *
+         * @param force The <code>force</code> flag
+         * @return This builder
+         */
+        public Builder force(boolean force) {
+            this.force = force;
+            return this;
+        }
+
+        /**
+         * Sets the <code>active</code> flag.
+         *
+         * @param active The <code>active</code> flag
+         * @return This builder
+         */
+        public Builder active(boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        /**
+         * Sets the name.
+         *
+         * @param active The name
+         * @return This builder
+         */
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
+         * Adds specified localized name.
+         *
+         * @param locale The locale
+         * @param name The localized name
+         * @return This builder
+         */
+        public Builder addLocalizedName(Locale locale, String name) {
+            Map<Locale, String> ns = this.names;
+            if (null == ns) {
+                ns = new LinkedHashMap<Locale, String>(16, 0.9F);
+                this.names = ns;
+            }
+            ns.put(locale, name);
+            return this;
+        }
+
+        /**
+         * Adds specified localized names.
+         *
+         * @param names The localized names to add
+         * @return This builder
+         */
+        public Builder addLocalizedNames(Map<Locale, String> names) {
+            if (null == names || names.isEmpty()) {
+                return this;
+            }
+
+            Map<Locale, String> ns = this.names;
+            if (null == ns) {
+                ns = new LinkedHashMap<Locale, String>(16, 0.9F);
+                this.names = ns;
+            }
+            ns.putAll(names);
+            return this;
+        }
+
+        /**
+         * Builds the {@link MailCategoryConfig} instance according to this builder's settings.
+         *
+         * @return The new {@link MailCategoryConfig} instance
+         */
+        public MailCategoryConfig build() {
+            return new MailCategoryConfig(flag, force, active, name, names);
+        }
+
+    }// End of class Builder
+
+    // ----------------------------------------------------------------------------------------------------------
+
+    private final String flag;
+    private final boolean force;
+    private final boolean active;
+    private final String name;
+    private final Map<Locale, String> names;
+
+    /**
+     * Initializes a new {@link MailCategoryConfig}.
+     */
+    MailCategoryConfig(String flag, boolean force, boolean active, String name, Map<Locale, String> names) {
+        super();
+        this.flag = flag;
+        this.force = force;
+        this.active = active;
+        this.name = name;
+        this.names = null == names ? Collections.<Locale, String> emptyMap() : names;
+    }
+
+    /**
+     * Gets the flag name (to be used for search/filter expressions).
+     *
+     * @return The flag name
+     */
+    public String getFlag() {
+        return flag;
+    }
+
+    /**
+     * Checks if the associated mail category is forced to be active.
+     *
+     * @return <code>true</code> if forcefully active; otherwise <code>false</code>
+     */
+    public boolean isForced() {
+        return force;
+    }
+
+    /**
+     * Checks if the associated mail category is active.
+     *
+     * @return <code>true</code> if active; otherwise <code>false</code>
+     */
+    public boolean isActive() {
+        return force || active;
+    }
+
+    /**
+     * Gets the en_US name.
+     *
+     * @return The en_US name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the localized names.
+     *
+     * @return The localized names
+     */
+    public Map<Locale, String> getNames() {
+        return names;
+    }
+
+}
