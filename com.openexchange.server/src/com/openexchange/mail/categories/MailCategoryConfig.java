4c5de2b68d2cca3da328deed9aafca66629f19a8
Thorben Betten <thorben.betten@open-xchange.com>
2016-03-07 17:25:59 +0100

    fixed javadoc

--- com.openexchange.server/src/com/openexchange/mail/categories/MailCategoryConfig.java
+++ com.openexchange.server/src/com/openexchange/mail/categories/MailCategoryConfig.java
@@ -58,7 +58,7 @@ import java.util.Map;
  * {@link MailCategoryConfig}
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
- * @since v7.8.1
+ * @since v7.8.2
  */
 public class MailCategoryConfig {
 
