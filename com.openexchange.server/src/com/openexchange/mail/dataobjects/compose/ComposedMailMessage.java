2eb8b433326d7982f28ad79750418907480350f1
Thorben Betten <thorben.betten@open-xchange.com>
2016-04-20 15:23:02 +0200

    Maintain recipient order

--- com.openexchange.server/src/com/openexchange/mail/dataobjects/compose/ComposedMailMessage.java
+++ com.openexchange.server/src/com/openexchange/mail/dataobjects/compose/ComposedMailMessage.java
@@ -56,7 +56,7 @@ import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import javax.mail.internet.InternetAddress;
 import com.openexchange.exception.OXException;
@@ -102,7 +102,7 @@ public abstract class ComposedMailMessage extends MailMessage {
         super();
         this.session = session;
         this.ctx = ctx;
-        recipients = new HashSet<InternetAddress>();
+        recipients = new LinkedHashSet<InternetAddress>();
         appendToSentFolder = true;
         transportToRecipients = true;
     }
