31b24e17ca7bc019bb3cd701b5fdb78c511461aa
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2016-04-25 14:03:24 +0200

    Bug #45486: Added listByAliasDomain to soap reseller api

---
+++ com.openexchange.admin.soap.reseller/src/com/openexchange/admin/soap/reseller/user/reseller/soap/ListByAliasDomainResponse.java
@@ -0,0 +1,72 @@
+
+package com.openexchange.admin.soap.reseller.user.reseller.soap;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import com.openexchange.admin.soap.reseller.user.soap.dataobjects.User;
+
+
+/**
+ * <p>Java-Klasse f\u00fcr anonymous complex type.
+ *
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ *
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://dataobjects.soap.admin.openexchange.com/xsd}User" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ *
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "_return"
+})
+@XmlRootElement(name = "listByAliasDomainResponse")
+public class ListByAliasDomainResponse {
+
+    @XmlElement(name = "return", nillable = true)
+    protected List<User> _return;
+
+    /**
+     * Gets the value of the return property.
+     *
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the return property.
+     *
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getReturn().add(newItem);
+     * </pre>
+     *
+     *
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link User }
+     *
+     *
+     */
+    public List<User> getReturn() {
+        if (_return == null) {
+            _return = new ArrayList<User>();
+        }
+        return this._return;
+    }
+
+}
