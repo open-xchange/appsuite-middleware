56d6fdc8a45dc575f7c26f106e180be3109e4a1e
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2016-06-09 15:35:45 +0200

    MW-198: Refactoring, create new version in InfostoreFacade #2

--- com.openexchange.file.storage.json/src/com/openexchange/file/storage/json/actions/files/NewAction.java
+++ com.openexchange.file.storage.json/src/com/openexchange/file/storage/json/actions/files/NewAction.java
@@ -117,6 +117,9 @@ public class NewAction extends AbstractWriteAction {
         // Construct detailed response as requested including any warnings, treat as error if not forcibly ignored by client
         AJAXRequestResult result;
         String saveAction = fileAccess.getAndFlushSaveActions(newId);
+        if (null == saveAction || Strings.isEmpty(saveAction)) {
+            saveAction = "none";
+        }
         if (null != newId && request.extendedResponse()) {
             File metadata = fileAccess.getFileMetadata(newId, FileStorageFileAccess.CURRENT_VERSION);
             result = result(metadata, (AJAXInfostoreRequest) request, saveAction);
