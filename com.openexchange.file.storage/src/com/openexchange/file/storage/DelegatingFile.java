a62380b5a8bcdc9838ed30053c6e9dca74035982
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2016-06-15 10:09:09 +0200

    delegatingfile iface to expose underlying impl

--- com.openexchange.file.storage/src/com/openexchange/file/storage/DelegatingFile.java
+++ com.openexchange.file.storage/src/com/openexchange/file/storage/DelegatingFile.java
@@ -8,7 +8,7 @@
  *
  *    In some countries OX, OX Open-Xchange, open xchange and OXtender
  *    as well as the corresponding Logos OX Open-Xchange and OX are registered
- *    trademarks of the OX Software GmbH. group of companies.
+ *    trademarks of the OX Software GmbH group of companies.
  *    The use of the Logos is not covered by the GNU General Public License.
  *    Instead, you are allowed to use these Logos according to the terms and
  *    conditions of the Creative Commons License, Version 2.5, Attribution,
@@ -47,75 +47,21 @@
  *
  */
 
-package com.openexchange.file.storage.mail;
-
-import java.util.ArrayList;
-import java.util.List;
-import com.openexchange.ajax.customizer.file.AdditionalFileField;
-import com.openexchange.ajax.requesthandler.AJAXRequestData;
-import com.openexchange.file.storage.File;
-import com.openexchange.file.storage.File.Field;
-import com.openexchange.file.storage.FileStorageUtility;
-import com.openexchange.tools.session.ServerSession;
+package com.openexchange.file.storage;
 
 /**
- * {@link MailMetadataField}
+ * {@link DelegatingFile}
  *
  * @author <a href="mailto:tobias.friedrich@open-xchange.com">Tobias Friedrich</a>
  * @since v7.8.2
  */
-public class MailMetadataField implements AdditionalFileField {
+public interface DelegatingFile extends File {
 
     /**
-     * Initializes a new {@link MailMetadataField}.
+     * Gets the underlying file delegate.
+     *
+     * @return The delegate
      */
-    public MailMetadataField() {
-        super();
-    }
-
-    @Override
-    public Field[] getRequiredFields() {
-        return new Field[0];
-    }
-
-    @Override
-    public int getColumnID() {
-        return 7030;
-    }
-
-    @Override
-    public String getColumnName() {
-        return "com.openexchange.file.storage.mail.mailMetadata";
-    }
-
-    @Override
-    public Object getValue(File file, ServerSession session) {
-        return getMailMetadata(file);
-    }
-
-    @Override
-    public List<Object> getValues(List<File> files, ServerSession session) {
-        if (null == files) {
-            return null;
-        }
-        List<Object> values = new ArrayList<Object>(files.size());
-        for (File file : files) {
-            values.add(getMailMetadata(file));
-        }
-        return values;
-    }
-
-    @Override
-    public Object renderJSON(AJAXRequestData requestData, Object value) {
-        if (MailMetadata.class.isInstance(value)) {
-            return ((MailMetadata) value).renderJSON();
-        }
-        return value;
-    }
-
-    private static MailMetadata getMailMetadata(File file) {
-        MailDriveFile mailDriveFile = FileStorageUtility.launderDelegate(file, MailDriveFile.class);
-        return null != mailDriveFile ? mailDriveFile.getMetadata() : null;
-    }
+    File getDelegate();
 
 }
