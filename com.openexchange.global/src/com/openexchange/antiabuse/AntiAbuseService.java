4f366d58821174f92a518a14713e2bd97cbf5b63
Thorben Betten <thorben.betten@open-xchange.com>
2016-07-08 14:25:47 +0200

    Extracted service-level package for anti-abuse
    Extracted service-level package for anti-abuse

---
+++ com.openexchange.global/src/com/openexchange/antiabuse/AntiAbuseService.java
@@ -0,0 +1,88 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.antiabuse;
+
+import java.util.Map;
+import com.openexchange.exception.OXException;
+
+/**
+ * {@link AntiAbuseService} - The service for anti-abuse checking and reporting.
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @since v7.8.2
+ */
+public interface AntiAbuseService {
+
+    /**
+     * Performs the <code>"allow"</code> request.
+     *
+     * @param login The login string
+     * @param password The password
+     * @param remoteAddress The remote address
+     * @param attributes The optional attributes
+     * @return The status response
+     * @throws OXException If allow request fails
+     */
+    Status allow(String login, String password, String remoteAddress, Map<String, String> attributes) throws OXException;
+
+    /**
+     * Performs the <code>"report"</code> request.
+     *
+     * @param reportValue The report value to advertise to Anti-Abuse service
+     * @param rejectedByWeakforced <code>true</code>
+     * @param login The login string
+     * @param password The password
+     * @param remoteAddress The remote address
+     * @return The status response
+     * @throws OXException If report request fails
+     */
+    void report(ReportValue reportValue, String login, String password, String remoteAddress) throws OXException;
+
+}
