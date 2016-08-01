c5d5309ad2b45fcc1f1d6363e3e6c94c24bc45ec
Martin Herfurth <martin.herfurth@open-xchange.com>
2016-08-01 14:32:45 +0200

    Initial RecurrenceService bundle (dummy).

---
+++ com.openexchange.chronos.recurrence/src/com/openexchange/chronos/recurrence/service/RecurrenceServiceImpl.java
@@ -0,0 +1,96 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.chronos.recurrence.service;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TimeZone;
+import com.openexchange.chronos.Event;
+import com.openexchange.chronos.RecurrenceService;
+
+/**
+ * {@link RecurrenceServiceImpl}
+ *
+ * @author <a href="mailto:martin.herfurth@open-xchange.com">Martin Herfurth</a>
+ * @since v7.10.0
+ */
+public class RecurrenceServiceImpl implements RecurrenceService {
+
+    @Override
+    public Iterator<Event> calculateInstances(Event master, Calendar start, Calendar end, int limit) {
+        List<Event> retval = new ArrayList<Event>();
+        retval.add(master);
+        return retval.iterator();
+    }
+
+    @Override
+    public Calendar calculateRecurrenceDatePosition(Event master, int position) {
+        Calendar retval = GregorianCalendar.getInstance(TimeZone.getTimeZone("UTC"));
+        retval.setTime(master.getStartDate());
+        removeTime(retval);
+        return retval;
+    }
+
+    @Override
+    public int calculateRecurrencePosition(Event master, Calendar datePosition) {
+        return 1;
+    }
+
+    private void removeTime(Calendar calendar) {
+        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+
+}
