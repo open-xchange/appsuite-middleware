9a8d92360efb0dce7355b367cf4a3f917abed491
Ioannis Chouklis <ioannis.chouklis@open-xchange.com.com>
2016-08-09 11:45:33 +0200

    stub oauth access for one drive

---
+++ com.openexchange.file.storage.onedrive/src/com/openexchange/file/storage/onedrive/access/OneDriveOAuthAccess.java
@@ -0,0 +1,286 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the Open-Xchange, Inc. group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2004-2016 Open-Xchange, Inc.
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.file.storage.onedrive.access;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.MsLiveConnectApi;
+import org.scribe.model.Token;
+import org.slf4j.Logger;
+import com.openexchange.exception.OXException;
+import com.openexchange.file.storage.FileStorageAccount;
+import com.openexchange.file.storage.FileStorageExceptionCodes;
+import com.openexchange.file.storage.onedrive.OneDriveConstants;
+import com.openexchange.file.storage.onedrive.osgi.Services;
+import com.openexchange.java.Strings;
+import com.openexchange.oauth.DefaultOAuthToken;
+import com.openexchange.oauth.OAuthAccount;
+import com.openexchange.oauth.OAuthConstants;
+import com.openexchange.oauth.OAuthExceptionCodes;
+import com.openexchange.oauth.OAuthService;
+import com.openexchange.oauth.access.OAuthAccess;
+import com.openexchange.oauth.access.OAuthClient;
+import com.openexchange.rest.client.httpclient.HttpClients;
+import com.openexchange.session.Session;
+
+/**
+ * {@link OneDriveOAuthAccess}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class OneDriveOAuthAccess implements OAuthAccess {
+
+    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(OneDriveOAuthAccess.class);
+
+    /** The re-check threshold in seconds (45 minutes) */
+    private static final long RECHECK_THRESHOLD = 2700;
+
+    private FileStorageAccount fsAccount;
+    private Session session;
+
+    /** The associated OAuth account */
+    private volatile OAuthAccount liveconnectOAuthAccount;
+
+    /** The last-accessed time stamp */
+    private volatile long lastAccessed;
+
+    /** The access token */
+    private volatile String accessToken;
+
+    /** The HTTP client */
+    private final OAuthClient<DefaultHttpClient> oauthClient;
+
+    /**
+     * Initialises a new {@link OneDriveOAuthAccess}.
+     */
+    public OneDriveOAuthAccess(FileStorageAccount fsAccount, Session session) throws OXException {
+        super();
+        this.fsAccount = fsAccount;
+        this.session = session;
+
+        int oauthAccountId = getAccountId();
+        // Grab Live Connect OAuth account
+        OAuthAccount liveconnectOAuthAccount;
+        {
+            OAuthService oAuthService = Services.getService(OAuthService.class);
+            liveconnectOAuthAccount = oAuthService.getAccount(oauthAccountId, session, session.getUserId(), session.getContextId());
+        }
+
+        // Assign Live Connect account
+        this.liveconnectOAuthAccount = liveconnectOAuthAccount;
+
+        // Initialize rest
+        accessToken = liveconnectOAuthAccount.getToken();
+        oauthClient = new OAuthClient<>(HttpClients.getHttpClient("Open-Xchange OneDrive Client"));
+        lastAccessed = System.nanoTime();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.oauth.access.OAuthAccess#initialise()
+     */
+    @Override
+    public void initialise() throws OXException {
+        synchronized (this) {
+            OAuthAccount newAccount = recreateTokenIfExpired(true, liveconnectOAuthAccount, session);
+            if (newAccount != null) {
+                this.liveconnectOAuthAccount = newAccount;
+                accessToken = liveconnectOAuthAccount.getToken();
+                lastAccessed = System.nanoTime();
+            }
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.oauth.access.OAuthAccess#revoke()
+     */
+    @Override
+    public void revoke() throws OXException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.oauth.access.OAuthAccess#ping()
+     */
+    @Override
+    public boolean ping() throws OXException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.oauth.access.OAuthAccess#dispose()
+     */
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.oauth.access.OAuthAccess#getClient()
+     */
+    @Override
+    public OAuthClient<?> getClient() throws OXException {
+        return oauthClient;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.oauth.access.OAuthAccess#getAccountId()
+     */
+    @Override
+    public int getAccountId() throws OXException {
+        // Get OAuth account identifier from messaging account's configuration
+        int oauthAccountId;
+        {
+            Map<String, Object> configuration = fsAccount.getConfiguration();
+            if (null == configuration) {
+                throw FileStorageExceptionCodes.MISSING_CONFIG.create(OneDriveConstants.ID, fsAccount.getId());
+            }
+            Object accountId = configuration.get("account");
+            if (null == accountId) {
+                throw FileStorageExceptionCodes.MISSING_CONFIG.create(OneDriveConstants.ID, fsAccount.getId());
+            }
+            if (accountId instanceof Integer) {
+                oauthAccountId = ((Integer) accountId).intValue();
+            } else {
+                try {
+                    oauthAccountId = Integer.parseInt(accountId.toString());
+                } catch (NumberFormatException e) {
+                    throw FileStorageExceptionCodes.MISSING_CONFIG.create(e, OneDriveConstants.ID, fsAccount.getId());
+                }
+            }
+        }
+        return oauthAccountId;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.oauth.access.OAuthAccess#ensureNotExpired()
+     */
+    @Override
+    public OAuthAccess ensureNotExpired() throws OXException {
+        long now = System.nanoTime();
+        if (TimeUnit.NANOSECONDS.toSeconds(now - lastAccessed) > RECHECK_THRESHOLD) {
+            synchronized (this) {
+                now = System.nanoTime();
+                if (TimeUnit.NANOSECONDS.toSeconds(now - lastAccessed) > RECHECK_THRESHOLD) {
+                    OAuthAccount newAccount = recreateTokenIfExpired(false, liveconnectOAuthAccount, session);
+                    if (newAccount != null) {
+                        this.liveconnectOAuthAccount = newAccount;
+                        accessToken = liveconnectOAuthAccount.getToken();
+                        lastAccessed = System.nanoTime();
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Re-creates the token if expired
+     * 
+     * @param considerExpired
+     * @param liveconnectOAuthAccount
+     * @param session
+     * @return
+     * @throws OXException
+     */
+    private OAuthAccount recreateTokenIfExpired(boolean considerExpired, OAuthAccount liveconnectOAuthAccount, Session session) throws OXException {
+        // Create Scribe Microsoft OneDrive OAuth service
+        final ServiceBuilder serviceBuilder = new ServiceBuilder().provider(MsLiveConnectApi.class);
+        serviceBuilder.apiKey(liveconnectOAuthAccount.getMetaData().getAPIKey(session)).apiSecret(liveconnectOAuthAccount.getMetaData().getAPISecret(session));
+        MsLiveConnectApi.MsLiveConnectService scribeOAuthService = (MsLiveConnectApi.MsLiveConnectService) serviceBuilder.build();
+
+        // Check expiration
+        if (considerExpired || scribeOAuthService.isExpired(liveconnectOAuthAccount.getToken())) {
+            // Expired...
+            String refreshToken = liveconnectOAuthAccount.getSecret();
+            Token accessToken;
+            try {
+                accessToken = scribeOAuthService.getAccessToken(new Token(liveconnectOAuthAccount.getToken(), liveconnectOAuthAccount.getSecret()), null);
+            } catch (org.scribe.exceptions.OAuthException e) {
+                throw OAuthExceptionCodes.INVALID_ACCOUNT_EXTENDED.create(e, liveconnectOAuthAccount.getDisplayName(), liveconnectOAuthAccount.getId());
+            }
+            if (Strings.isEmpty(accessToken.getSecret())) {
+                LOGGER.warn("Received invalid request_token from Live Connect: {}. Response:{}{}", null == accessToken.getSecret() ? "null" : accessToken.getSecret(), Strings.getLineSeparator(), accessToken.getRawResponse());
+            } else {
+                refreshToken = accessToken.getSecret();
+            }
+            // Update account
+            OAuthService oAuthService = Services.getService(OAuthService.class);
+            int accountId = liveconnectOAuthAccount.getId();
+            Map<String, Object> arguments = new HashMap<String, Object>(3);
+            arguments.put(OAuthConstants.ARGUMENT_REQUEST_TOKEN, new DefaultOAuthToken(accessToken.getToken(), refreshToken));
+            arguments.put(OAuthConstants.ARGUMENT_SESSION, session);
+            oAuthService.updateAccount(accountId, arguments, session.getUserId(), session.getContextId());
+
+            // Reload
+            return oAuthService.getAccount(accountId, session, session.getUserId(), session.getContextId());
+        }
+        return null;
+    }
+}
