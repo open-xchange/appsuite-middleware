67120dbb93095a632a23c34000a227f18cb56dee
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2016-08-19 13:12:17 +0200

    MW-261: udpate task to drop the access tokens

---
+++ com.openexchange.oauth.dropbox/src/com/openexchange/oauth/dropbox/internal/groupware/OAuthDropboxDropTokensTask.java
@@ -0,0 +1,129 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.oauth.dropbox.internal.groupware;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import com.openexchange.database.DatabaseService;
+import com.openexchange.exception.OXException;
+import com.openexchange.groupware.update.PerformParameters;
+import com.openexchange.groupware.update.UpdateExceptionCodes;
+import com.openexchange.groupware.update.UpdateTaskAdapter;
+import com.openexchange.oauth.API;
+import com.openexchange.oauth.internal.groupware.OAuthCreateTableTask2;
+import com.openexchange.tools.sql.DBUtils;
+
+/**
+ * {@link OAuthDropboxDropTokensTask}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class OAuthDropboxDropTokensTask extends UpdateTaskAdapter {
+
+    private final DatabaseService databaseService;
+
+    /**
+     * Initialises a new {@link OAuthDropboxDropTokensTask}.
+     */
+    public OAuthDropboxDropTokensTask(DatabaseService databaseService) {
+        super();
+        this.databaseService = databaseService;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.groupware.update.UpdateTaskV2#perform(com.openexchange.groupware.update.PerformParameters)
+     */
+    @Override
+    public void perform(PerformParameters params) throws OXException {
+        int contextId = params.getContextId();
+
+        // Get the writeable connection
+        Connection writeConnection = null;
+        try {
+            writeConnection = databaseService.getForUpdateTask(contextId);
+        } catch (OXException e) {
+            throw e;
+        }
+
+        // Perform the task
+        PreparedStatement statement = null;
+        try {
+            String dropTokensSQL = "UPDATE oauthAccounts SET accessToken = ? AND accessSecret = ? WHERE cid = ? AND serviceId = ?";
+            statement = writeConnection.prepareStatement(dropTokensSQL);
+
+            int parameterIndex = 0;
+            statement.setString(parameterIndex++, "");
+            statement.setString(parameterIndex++, "");
+            statement.setInt(parameterIndex++, contextId);
+            statement.setString(parameterIndex++, API.DROPBOX.getFullName());
+
+            statement.execute();
+        } catch (SQLException e) {
+            DBUtils.rollback(writeConnection);
+            throw UpdateExceptionCodes.SQL_PROBLEM.create(e, e.getMessage());
+        } finally {
+            DBUtils.closeSQLStuff(statement);
+            databaseService.backForUpdateTask(writeConnection);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.groupware.update.UpdateTaskV2#getDependencies()
+     */
+    @Override
+    public String[] getDependencies() {
+        return new String[] { OAuthCreateTableTask2.class.getName() };
+    }
+}
