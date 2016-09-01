79ee848dc54e35fe929be55b91c0ddae219b9930
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2016-09-01 16:13:29 +0200

    Partial fix for bug 48190: use the account parameter

--- com.openexchange.file.storage.boxcom/src/com/openexchange/file/storage/boxcom/access/BoxOAuthAccess.java
+++ com.openexchange.file.storage.boxcom/src/com/openexchange/file/storage/boxcom/access/BoxOAuthAccess.java
@@ -168,8 +168,8 @@ public class BoxOAuthAccess extends AbstractOAuthAccess {
      */
     private void createOAuthClient(OAuthAccount account) throws OXException {
         OAuthServiceMetaData boxMetaData = account.getMetaData();
-        BoxAPIConnection boxAPI = new BoxAPIConnection(boxMetaData.getAPIKey(session), boxMetaData.getAPISecret(session), getOAuthAccount().getToken(), getOAuthAccount().getSecret());
-        OAuthClient<BoxAPIConnection> oAuthClient = new OAuthClient<>(boxAPI, getOAuthAccount().getToken());
+        BoxAPIConnection boxAPI = new BoxAPIConnection(boxMetaData.getAPIKey(session), boxMetaData.getAPISecret(session), account.getToken(), account.getSecret());
+        OAuthClient<BoxAPIConnection> oAuthClient = new OAuthClient<>(boxAPI, account.getToken());
         setOAuthClient(oAuthClient);
     }
 }
