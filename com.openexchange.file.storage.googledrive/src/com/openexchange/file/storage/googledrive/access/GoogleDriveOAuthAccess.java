ae64f1f0b685ede3cb4f6ed279b43af4d571348e
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2016-09-05 12:22:40 +0200

    removed revoke from google drive

--- com.openexchange.file.storage.googledrive/src/com/openexchange/file/storage/googledrive/access/GoogleDriveOAuthAccess.java
+++ com.openexchange.file.storage.googledrive/src/com/openexchange/file/storage/googledrive/access/GoogleDriveOAuthAccess.java
@@ -107,11 +107,6 @@ public class GoogleDriveOAuthAccess extends AbstractOAuthAccess {
     }
 
     @Override
-    public void revoke() throws OXException {
-        // No revoke
-    }
-
-    @Override
     public boolean ping() throws OXException {
         try {
             Drive drive = this.<Drive> getClient().client;
