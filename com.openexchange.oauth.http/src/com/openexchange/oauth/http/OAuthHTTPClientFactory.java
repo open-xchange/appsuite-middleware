a2115c4d41915d97562f6975494c25528aef36c3
Thorben Betten <thorben.betten@open-xchange.com>
2016-09-09 12:07:40 +0200

    Moved OAuth HTTP client to a dedicated bundle

--- com.openexchange.oauth.http/src/com/openexchange/oauth/http/OAuthHTTPClientFactory.java
+++ com.openexchange.oauth.http/src/com/openexchange/oauth/http/OAuthHTTPClientFactory.java
@@ -47,10 +47,11 @@
  *
  */
 
-package com.openexchange.oauth;
+package com.openexchange.oauth.http;
 
 import com.openexchange.exception.OXException;
 import com.openexchange.http.client.HTTPClient;
+import com.openexchange.oauth.OAuthAccount;
 import com.openexchange.osgi.annotation.SingletonService;
 import com.openexchange.session.Session;
 
