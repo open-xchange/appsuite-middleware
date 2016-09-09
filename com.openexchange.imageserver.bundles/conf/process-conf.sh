c67356b27fafa6102f17c8296daef4f43544aefe
Kai Ahrens <kai.ahrens@open-xchange.com>
2016-09-09 17:26:12 +0200

    added new package implementation for ImageServer: com-openexchange-imageserver

---
+++ com.openexchange.imageserver.bundles/conf/process-conf.sh
@@ -0,0 +1,3 @@
+
+# Defines the Java options for the FileServer Java virtual machine.
+JAVA_XTRAOPTS="-Xms512m -Xmx4096M -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled"
