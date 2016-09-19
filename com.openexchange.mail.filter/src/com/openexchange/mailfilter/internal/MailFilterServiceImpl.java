f2fbc67d17b87230451fa73eda877015ac9a6c96
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2016-09-19 14:04:41 +0200

    Fix for bug 48832: Don't write a completely empty sieve script when purging. Instead write only a comment line
    (cherry picked from commit 947ab0d8387f23af265cf5d1eaf47c782cf73631)

--- com.openexchange.mail.filter/src/com/openexchange/mailfilter/internal/MailFilterServiceImpl.java
+++ com.openexchange.mail.filter/src/com/openexchange/mailfilter/internal/MailFilterServiceImpl.java
@@ -361,7 +361,9 @@ public final class MailFilterServiceImpl implements MailFilterService {
             try {
                 handlerConnect(sieveHandler, credentials.getSubject());
                 String activeScript = sieveHandler.getActiveScript();
-                writeScript(sieveHandler, activeScript, "");
+                SieveTextFilter sieveTextFilter = new SieveTextFilter(credentials);
+                String writeback = sieveTextFilter.writeEmptyScript();
+                writeScript(sieveHandler, activeScript, writeback);
             } catch (UnsupportedEncodingException e) {
                 throw MailFilterExceptionCode.UNSUPPORTED_ENCODING.create(e);
             } catch (OXSieveHandlerException e) {
@@ -518,7 +520,7 @@ public final class MailFilterServiceImpl implements MailFilterService {
                         LOGGER.debug("The contents of the reorder array are: {}", uids);
                         throw MailFilterExceptionCode.INVALID_REORDER_ARRAY.create(uids.length, clientrules.size(), credentials.getUserid(), credentials.getContextid());
                     }
-                    
+
                     // Identify rule groups
                     List<MailFilterGroup> groups = getMailFilterGroups(uids);
                     List<Rule> tmpList = new ArrayList<>(clientrules);
