9562bcff4983a3e3cfbae4b673018a9aa7ea1888
Thorben Betten <thorben.betten@open-xchange.com>
2016-09-23 09:08:16 +0200

    restored

--- documentation/config/parseyml.sh
+++ documentation/config/parseyml.sh
@@ -37,11 +37,7 @@ parse_yaml() {
                     printf("%s<br>", $0);
                 } else {
                      if ($1 == "Key"){
-                        VAL1 = "`" + $2 + "`"
-                        printf("<span style=\"font-weight:normal\">%s</span>", $VAL1);
-                     } else if ($1 == "Related") {
-                        VAL2 = "`" + $2 + "`"
-                        printf("%s", $VAL2);
+                        printf("<span style=\"font-weight:normal\">%s</span>", $2);
                      } else {
                         for(i=2;i<=NF;i++){
                           printf("%s ", $i);
