a2f3a1fdb5e0c85790b3b1076d24fcfe26756397
Thorben Betten <thorben.betten@open-xchange.com>
2016-09-23 16:40:01 +0200

    Fix for bug 48843: Replace possible unicode sequences to characters to apply proper sanitizing
    (cherry picked from commit 158cd0f5c5fe4cb57ae48ecd7821e1478d19f598)
    (cherry picked from commit 3c73c7c1fceb3b700f98eb9b6103b3b656e0385c)

--- com.openexchange.html/src/com/openexchange/html/HtmlServices.java
+++ com.openexchange.html/src/com/openexchange/html/HtmlServices.java
@@ -55,6 +55,8 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.concurrent.Callable;
 import net.htmlparser.jericho.HTMLElementName;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.codec.net.URLCodec;
 import com.openexchange.config.ConfigurationService;
 import com.openexchange.html.internal.WhitelistedSchemes;
@@ -73,6 +75,8 @@ public final class HtmlServices {
     private HtmlServices() {
         super();
     }
+
+    private static final Pattern UNICODE_CHAR = Pattern.compile("&(?:amp;)?#0*([1-9][0-9]+)", Pattern.CASE_INSENSITIVE);
 
     /**
      * Does URL decoding until fully decoded
@@ -101,6 +105,18 @@ public final class HtmlServices {
                         k = false;
                     }
                 } while (k && (pos = s.indexOf('%')) >= 0 && pos < s.length() - 1);
+            }
+        }
+
+        {
+            for (Matcher m; (m = UNICODE_CHAR.matcher(s)).find();) {
+                StringBuffer sb = new StringBuffer(s.length());
+                do {
+                    char c = (char) Integer.parseInt(m.group(1));
+                    m.appendReplacement(sb, String.valueOf(c));
+                } while (m.find());
+                m.appendTail(sb);
+                s = sb.toString();
             }
         }
 
@@ -168,7 +184,7 @@ public final class HtmlServices {
         return (mimeType.startsWith("image/") && mimeType.indexOf("svg") < 0) ? Result.ALLOW : Result.DENY;
     }
 
-    private static final String[] UNSAFE_TOKENS = { "javascript:", "vbscript:", "vascript:", "<script" };
+    private static final String[] UNSAFE_TOKENS = { "javascript:", "vbscript:", "<script" };
 
     /**
      * Checks if specified URL String is safe or not.
