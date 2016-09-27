66e28a05e4ef350c8c221a1e75708f0311a56b68
Thorben Betten <thorben.betten@open-xchange.com>
2016-09-27 11:47:15 +0200

    Fix for bug 49005: Improved detection of possible unicode entities and check for unsafe tokens having white-space characters removed
    (cherry picked from commit 277f766685d791d9e60baa08361c60c89cc2a38f)

--- com.openexchange.html/src/com/openexchange/html/HtmlServices.java
+++ com.openexchange.html/src/com/openexchange/html/HtmlServices.java
@@ -50,6 +50,7 @@
 package com.openexchange.html;
 
 import static com.openexchange.java.Strings.isEmpty;
+import static com.openexchange.java.Strings.isWhitespace;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -69,6 +70,12 @@ import com.openexchange.html.osgi.Services;
  */
 public final class HtmlServices {
 
+    public static void main(String[] args) {
+        boolean nonJavaScriptURL = isNonJavaScriptURL("java&#09;script:alert(document.domain)", null);
+
+        System.out.println(nonJavaScriptURL);
+    }
+
     /**
      * Initializes a new {@link HtmlServices}.
      */
@@ -76,7 +83,7 @@ public final class HtmlServices {
         super();
     }
 
-    private static final Pattern UNICODE_CHAR = Pattern.compile("&(?:amp;)?#0*([1-9][0-9]+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern UNICODE_CHAR = Pattern.compile("&(?:amp;)?#0*([1-9][0-9]*);?", Pattern.CASE_INSENSITIVE);
 
     /**
      * Does URL decoding until fully decoded
@@ -215,6 +222,7 @@ public final class HtmlServices {
         }
 
         // Check basic unsafe tokens
+        lc = dropWhitespacesFrom(lc);
         for (String unsafeToken : UNSAFE_TOKENS) {
             if (lc.indexOf(unsafeToken) >= 0) {
                 return false;
@@ -231,6 +239,43 @@ public final class HtmlServices {
         }
 
         return true;
+    }
+
+    private static String dropWhitespacesFrom(String str) {
+        if (null == str) {
+            return null;
+        }
+
+        int length = str.length();
+        StringBuilder sb = null;
+        for (int k = length, i = 0; k-- > 0; i++) {
+            char c = str.charAt(i);
+            if (isWhitespace(c)) {
+                if (null == sb) {
+                    sb = new StringBuilder(length);
+                    if (i > 0) {
+                        sb.append(str.substring(0, i));
+                    }
+                }
+            } else {
+                if (null != sb) {
+                    sb.append(c);
+                }
+            }
+        }
+
+        return null == sb ? str : sb.toString();
+    }
+
+    /**
+     * Checks if specified URL String is unsafe or not.
+     *
+     * @param val The URL String to check
+     * @param tagName The name of the tag
+     * @return <code>true</code> if unsafe; otherwise <code>false</code>
+     */
+    public static boolean isJavaScriptURL(String val, String tagName) {
+        return !isNonJavaScriptURL(val, tagName);
     }
 
     // ---------------------------------------------------------------------------------------------------------------------------- //
