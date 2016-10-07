bfa5bfccc0e00a4a8105df578f5b7b446d740ee8
Thorben Betten <thorben.betten@open-xchange.com>
2016-10-07 19:35:42 +0200

    MW-286; Accept both - boolean and integer - for "oauth" on auto-config call

--- com.openexchange.mail.autoconfig.json/src/com/openexchange/mail/autoconfig/json/osgi/Activator.java
+++ com.openexchange.mail.autoconfig.json/src/com/openexchange/mail/autoconfig/json/osgi/Activator.java
@@ -55,6 +55,7 @@ import com.openexchange.ajax.requesthandler.osgiservice.AJAXModuleActivator;
 import com.openexchange.mail.autoconfig.AutoconfigService;
 import com.openexchange.mail.autoconfig.json.actions.AutoconfigActionFactory;
 import com.openexchange.mail.autoconfig.json.converter.AutoconfigResultConverter;
+import com.openexchange.oauth.OAuthService;
 
 /**
  * {@link Activator}
@@ -63,21 +64,15 @@ import com.openexchange.mail.autoconfig.json.converter.AutoconfigResultConverter
  */
 public class Activator extends AJAXModuleActivator {
 
-    /*
-     * (non-Javadoc)
-     * @see com.openexchange.server.osgiservice.DeferredActivator#getNeededServices()
-     */
     @Override
     protected Class<?>[] getNeededServices() {
         return new Class<?>[] { AutoconfigService.class };
     }
 
-    /*
-     * (non-Javadoc)
-     * @see com.openexchange.server.osgiservice.DeferredActivator#startBundle()
-     */
     @Override
     protected void startBundle() throws Exception {
+        trackService(OAuthService.class);
+        openTrackers();
         registerModule(new AutoconfigActionFactory(this), "autoconfig");
         registerService(ResultConverter.class, new AutoconfigResultConverter());
     }
