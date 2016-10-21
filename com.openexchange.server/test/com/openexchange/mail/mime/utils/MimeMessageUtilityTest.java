519f139519da2c1a5b72fd3fcf786825c8aec349
Vitali Sjablow <vitali.sjablow@open-xchange.com>
2016-10-21 08:36:59 +0200

    Bug 48250: Added signature checks and interpretation of header information. (Cherry-picked from commit: 37e10d3b46ddab9631765f703468cb786804ad19)

--- com.openexchange.server/test/com/openexchange/mail/mime/utils/MimeMessageUtilityTest.java
+++ com.openexchange.server/test/com/openexchange/mail/mime/utils/MimeMessageUtilityTest.java
@@ -49,20 +49,55 @@
 
 package com.openexchange.mail.mime.utils;
 
+import java.io.IOException;
+import javax.mail.BodyPart;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.internet.ParameterList;
+import javax.mail.internet.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+import com.openexchange.exception.OXException;
+import com.openexchange.mail.dataobjects.MailPart;
+import com.openexchange.mail.mime.ContentDisposition;
+import com.openexchange.mail.mime.ContentType;
+import com.openexchange.mail.mime.MessageHeaders;
+import com.sun.mail.imap.protocol.BODYSTRUCTURE;
 import junit.framework.TestCase;
-
 
 /**
  * {@link MimeMessageUtilityTest}
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @author <a href="mailto:vitali.sjablow@open-xchange.com">Vitali Sjablow</a>
  * @since 7.6.1
  */
-public class MimeMessageUtilityTest extends TestCase{
+@RunWith(PowerMockRunner.class)
+public class MimeMessageUtilityTest extends TestCase {
+    
+    private static final String ATTACHMENT_WRONG_FOUND = "Attachment found where none should be";
+    private static final String TESTFILE_RTF = "testfile; filename=randomfile.rtf";
+    private static final String PLAIN_TEXT = "plain/text";
+    private static final String NOT_PROPERLY_UNFOLDED_DECODED = "Not properly unfolded/decoded.";
+    private static final String ATTACHMENT_NOT_IDENTIFIED = "Attachment was not identified";
+    @Mock
+    private Multipart multipart;
+    @Mock
+    private MailPart mailpart;
+    @Mock
+    private BODYSTRUCTURE bodystructure;
+    
+    private static final String MIXED_SUBTYPE = "mixed";
+    
+    @Before
+    public void setUp() {
+        bodystructure.subtype = MIXED_SUBTYPE;
+    }
 
-    /**
-     * Initializes a new {@link MimeMessageUtilityTest}.
-     */
     public MimeMessageUtilityTest() {
         super();
     }
@@ -75,46 +110,152 @@ public class MimeMessageUtilityTest extends TestCase{
     }
 
     public void testForBug36072_AddressUnfolding() {
-        String s = "=?UTF-8?Q?Wielkoszcz=C4=99ko=C5=9Bciskowiczkiewi?= =?UTF-8?Q?cz=C3=B3wnaOm=C3=B3jbo=C5=BCejestemno=C5=BCemwie?= "
-            + "=?UTF-8?Q?leznacz=C4=85cychznak=C3=B3wsi=C4=99znaczyb?= =?UTF-8?Q?oprzecie=C5=BCniemo=C5=BCeby=C4=87zbyt=C5=82atwo!?= <foo@bar.tld>";
+        String s = "=?UTF-8?Q?Wielkoszcz=C4=99ko=C5=9Bciskowiczkiewi?= =?UTF-8?Q?cz=C3=B3wnaOm=C3=B3jbo=C5=BCejestemno=C5=BCemwie?= " + "=?UTF-8?Q?leznacz=C4=85cychznak=C3=B3wsi=C4=99znaczyb?= =?UTF-8?Q?oprzecie=C5=BCniemo=C5=BCeby=C4=87zbyt=C5=82atwo!?= <foo@bar.tld>";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Address nor properly unfolded/decoded.", "Wielkoszcz\u0119ko\u015bciskowiczkiewicz\u00f3wnaOm\u00f3jbo\u017cejestemno\u017cemwieleznacz\u0105cychznak\u00f3wsi\u0119znaczyboprzecie\u017cniemo\u017ceby\u0107zbyt\u0142atwo! "
-            + "<foo@bar.tld>", s);
+        assertEquals("Address nor properly unfolded/decoded.", "Wielkoszcz\u0119ko\u015bciskowiczkiewicz\u00f3wnaOm\u00f3jbo\u017cejestemno\u017cemwieleznacz\u0105cychznak\u00f3wsi\u0119znaczyboprzecie\u017cniemo\u017ceby\u0107zbyt\u0142atwo! " + "<foo@bar.tld>", s);
 
-        //expected (a b)
         s = "=?ISO-8859-1?Q?a?= b";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Not properly unfolded/decoded.", "a b", s);
+        assertEquals(NOT_PROPERLY_UNFOLDED_DECODED, "a b", s);
 
-        //expected (ab)
         s = "=?ISO-8859-1?Q?a?= =?ISO-8859-1?Q?b?=";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Not properly unfolded/decoded.", "ab", s);
+        assertEquals(NOT_PROPERLY_UNFOLDED_DECODED, "ab", s);
 
-        //expected (ab)
         s = "=?ISO-8859-1?Q?a?=  =?ISO-8859-1?Q?b?=";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Not properly unfolded/decoded.", "ab", s);
+        assertEquals(NOT_PROPERLY_UNFOLDED_DECODED, "ab", s);
 
-        //expected (ab)
-        s = "=?ISO-8859-1?Q?a?=\r\n" +
-            "\t=?ISO-8859-1?Q?b?=";
+        s = "=?ISO-8859-1?Q?a?=\r\n" + "\t=?ISO-8859-1?Q?b?=";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Not properly unfolded/decoded.", "ab", s);
+        assertEquals(NOT_PROPERLY_UNFOLDED_DECODED, "ab", s);
 
-        //expected (a b)
         s = "=?ISO-8859-1?Q?a_b?=";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Not properly unfolded/decoded.", "a b", s);
+        assertEquals(NOT_PROPERLY_UNFOLDED_DECODED, "a b", s);
 
-        //expected (a b)
         s = "=?ISO-8859-1?Q?a?= =?ISO-8859-2?Q?_b?=";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Not properly unfolded/decoded.", "a b", s);
+        assertEquals(NOT_PROPERLY_UNFOLDED_DECODED, "a b", s);
 
-        //expected (a b)
         s = "a b";
         s = MimeMessageUtility.decodeMultiEncodedHeader(s);
-        assertEquals("Not properly unfolded/decoded.", "a b", s);
+        assertEquals(NOT_PROPERLY_UNFOLDED_DECODED, "a b", s);
+    }
+
+    @Test
+    public void testHasAttachment_MultipartDisposition() {
+        boolean result = false;
+        BodyPart bodyPart = Mockito.mock(BodyPart.class);
+        try {
+            Mockito.when(multipart.getBodyPart(org.mockito.Matchers.anyInt())).thenReturn(bodyPart);
+            Mockito.when(multipart.getCount()).thenReturn(1);
+            Mockito.when(bodyPart.getContentType()).thenReturn(PLAIN_TEXT);
+            Mockito.when(bodyPart.getDisposition()).thenReturn("attachment; random=value");
+            result = MimeMessageUtility.hasAttachments(multipart, MIXED_SUBTYPE);
+        } catch (MessagingException | OXException | IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(ATTACHMENT_NOT_IDENTIFIED, result);
+    }
+
+    public void testHasAttachment_MultipartFilename() {
+        boolean result = false;
+        BodyPart bodyPart = Mockito.mock(BodyPart.class);
+        try {
+            Mockito.when(multipart.getBodyPart(org.mockito.Matchers.anyInt())).thenReturn(bodyPart);
+            Mockito.when(multipart.getCount()).thenReturn(1);
+            Mockito.when(bodyPart.getContentType()).thenReturn(PLAIN_TEXT);
+            Mockito.when(bodyPart.getDisposition()).thenReturn(TESTFILE_RTF);
+            result = MimeMessageUtility.hasAttachments(multipart, MIXED_SUBTYPE);
+        } catch (MessagingException | OXException | IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(ATTACHMENT_NOT_IDENTIFIED, result);
+    }
+
+    public void testHasAttachment_MultipartSignature() {
+        boolean result = true;
+        BodyPart bodyPart = Mockito.mock(BodyPart.class);
+        try {
+            Mockito.when(multipart.getBodyPart(org.mockito.Matchers.anyInt())).thenReturn(bodyPart);
+            Mockito.when(multipart.getCount()).thenReturn(1);
+            Mockito.when(bodyPart.getContentType()).thenReturn("application/pkcs7-signature");
+            result = MimeMessageUtility.hasAttachments(multipart, MIXED_SUBTYPE);
+        } catch (MessagingException | OXException | IOException e) {
+            e.printStackTrace();
+        }
+        assertFalse(ATTACHMENT_WRONG_FOUND, result);
+    }
+    
+    public void testHasAttachment_MailpartDisposition() {
+        boolean result = false;
+        try {
+            Mockito.when(mailpart.getEnclosedCount()).thenReturn(1);
+            ContentDisposition cd = new ContentDisposition("attachment; random=value");
+            Mockito.when(mailpart.getContentDisposition()).thenReturn(cd);
+            ContentType ct = new ContentType(PLAIN_TEXT);
+            Mockito.when(mailpart.getContentType()).thenReturn(ct);
+            result = MimeMessageUtility.hasAttachments(mailpart, "inline");
+        } catch (MessagingException | OXException | IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(ATTACHMENT_NOT_IDENTIFIED, result);
+    }
+
+    public void testHasAttachment_MailpartFilename() {
+        boolean result = false;
+        try {
+            Mockito.when(mailpart.getEnclosedCount()).thenReturn(1);
+            ContentDisposition cd = new ContentDisposition(TESTFILE_RTF);
+            Mockito.when(mailpart.getContentDisposition()).thenReturn(cd);
+            ContentType ct = new ContentType(PLAIN_TEXT);
+            Mockito.when(mailpart.getContentType()).thenReturn(ct);
+            result = MimeMessageUtility.hasAttachments(mailpart, "inline");
+        } catch (MessagingException | OXException | IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(ATTACHMENT_NOT_IDENTIFIED, result);
+    }
+
+    public void testHasAttachment_MailpartSignature() {
+        boolean result = true;
+        try {
+            ContentType ct = new ContentType("Application");
+            Mockito.when(mailpart.getContentType()).thenReturn(ct);
+            result = MimeMessageUtility.hasAttachments(mailpart, "Pkcs7-Signature");
+        } catch (MessagingException | OXException | IOException e) {
+            e.printStackTrace();
+        }
+        assertFalse(ATTACHMENT_WRONG_FOUND, result);
+    }
+
+    public void testHasAttachment_BodystructureDisposition() {
+        boolean result = false;
+        setContentTypePlainText(bodystructure);
+        bodystructure.disposition = "attachment";
+        result = MimeMessageUtility.hasAttachments(bodystructure);
+        assertTrue(ATTACHMENT_NOT_IDENTIFIED, result);
+    }
+
+    public void testHasAttachment_BodystructureFilename() throws ParseException {
+        boolean result = false;
+        setContentTypePlainText(bodystructure);
+        bodystructure.dParams = new ParameterList(";filename=randomfile.rtf");
+        result = MimeMessageUtility.hasAttachments(bodystructure);
+        assertTrue(ATTACHMENT_NOT_IDENTIFIED, result);
+    }
+
+    public void testHasAttachment_BodystructureSignature() {
+        boolean result = true;
+        bodystructure.type = "APPLICATION";
+        bodystructure.subtype = "Pkcs7-Signature";
+        result = MimeMessageUtility.hasAttachments(bodystructure);
+        assertFalse(ATTACHMENT_WRONG_FOUND, result);
+    }
+    
+    private static void setContentTypePlainText(BODYSTRUCTURE bs) {
+        bs.type = "plain";
+        bs.subtype = "text";
     }
 }
