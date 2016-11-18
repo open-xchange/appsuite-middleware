073cc55665a589808c6e518c11face23506f7b8c
Thorben Betten <thorben.betten@open-xchange.com>
2016-11-18 09:42:02 +0100

    Dropped duplicate code

--- com.openexchange.server/src/com/openexchange/mail/json/compose/Utilities.java
+++ com.openexchange.server/src/com/openexchange/mail/json/compose/Utilities.java
@@ -415,9 +415,6 @@ public enum Utilities {
         if (source.containsUserFlags()) {
             composedMail.addUserFlags(source.getUserFlags());
         }
-        if (source.containsUserFlags()) {
-            composedMail.addUserFlags(source.getUserFlags());
-        }
         if (source.containsHeaders()) {
             composedMail.addHeaders(source.getHeaders());
         }
