e4a560f86deb0bb856a0a578d4efda57043a65de
Ioannis Chouklis <ioannis.chouklis@open-xchange.com.com>
2016-11-25 17:50:29 +0100

    MW-439: Preparations - Primitive CassandraService interface and CassandraServiceInitializer stub

---
+++ com.openexchange.nosql.cassandra/src/com/openexchange/nosql/cassandra/CassandraService.java
@@ -0,0 +1,99 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.nosql.cassandra;
+
+import java.util.List;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.openexchange.exception.OXException;
+
+/**
+ * {@link CassandraService}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public interface CassandraService {
+
+    /**
+     * Returns an unmodifiable {@link List} with all available {@link Cluster} instances of the service
+     * 
+     * @return an unmodifiable {@link List} with all available {@link Cluster} instances of the service
+     * @throws OXException if the {@link List} cannot be returned
+     */
+    List<Cluster> getAllClusters() throws OXException;
+
+    /**
+     * Returns the {@link Cluster} instance with the specified name
+     * 
+     * @param name The name of the {@link Cluster} instance to return
+     * @return The {@link Cluster} instance
+     * @throws OXException if there is no such {@link Cluster} or any other error is occurred
+     */
+    Cluster getCluster(String name) throws OXException;
+
+    /**
+     * Returns a {@link Session} for the {@link Cluster} with the specified name
+     * 
+     * @param clusterName The {@link Cluster}'s name
+     * @return a {@link Session} for the {@link Cluster} with the specified name
+     * @throws OXException if there is no such {@link Cluster} or if the {@link Session} cannot be returned
+     */
+    Session getSession(String clusterName) throws OXException;
+
+    /**
+     * Returns a {@link Session} for the {@link Cluster} with the specified name and for the specified keyspace
+     * 
+     * @param clusterName The {@link Cluster}'s name
+     * @param keyspace The keyspace name
+     * @return a {@link Session} for the {@link Cluster} with the specified name and for the specified keyspace
+     * @throws OXException if there is no such {@link Cluster} or keyspace or if the {@link Session} cannot be returned
+     */
+    Session getSession(String clusterName, String keyspace) throws OXException;
+}
