1adfe1c0557ffd77601c218045f859819580c254
Vitali Sjablow <vitali.sjablow@open-xchange.com>
2016-12-19 11:32:54 +0100

    MW-454: Corrected comments for new soap methods and made UserTests clean up after themselfes. (Cherry-picked from commit: 418a7c66fa1906367f40bf3049767e183abe2999)

--- com.openexchange.admin.soap.reseller/src/com/openexchange/admin/soap/reseller/user/soap/dataobjects/User.java
+++ com.openexchange.admin.soap.reseller/src/com/openexchange/admin/soap/reseller/user/soap/dataobjects/User.java
@@ -1020,7 +1020,7 @@ public class User {
     }
     
     /**
-     * Ruft den Wert der defaultFolderMode-Eigenschaft ab.
+     * Get defaultFolderMode value.
      * 
      * @return
      *      possible object is
@@ -1031,7 +1031,7 @@ public class User {
     }
 
     /**
-     * Legt den Wert der defaultFolderMode-Eigenschaft fest.
+     * Set defaultFolderMode value..
      *
      * @param value
      *     allowed object is
