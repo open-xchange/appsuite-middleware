ca8eae7a84acd7405a1741c6e94d1b216f3c2146
Thorben Betten <thorben.betten@open-xchange.com>
2017-01-09 11:19:35 +0100

    MW-403: Use personal E-Mail address for "create link" messages as well

--- com.openexchange.share.notification/src/com/openexchange/share/notification/impl/mail/MailNotificationHandler.java
+++ com.openexchange.share.notification/src/com/openexchange/share/notification/impl/mail/MailNotificationHandler.java
@@ -146,7 +146,12 @@ public class MailNotificationHandler implements ShareNotificationHandler<Interne
         if (ServerSessionAdapter.valueOf(casted.getSession()).getUserConfiguration().hasWebMail()) {
             sendMail(transportProvider.createNewMailTransport(casted.getSession()), mail);
         } else {
-            sendMail(transportProvider.createNewNoReplyTransport(casted.getContextID()), mail);
+            boolean usePersonalEmailAddress = getBoolValue("com.openexchange.share.notification.usePersonalEmailAddress", false, casted.getSession());
+            if (usePersonalEmailAddress) {
+                sendMail(transportProvider.createNewNoReplyTransport(casted.getContextID(), false), mail);
+            } else {
+                sendMail(transportProvider.createNewNoReplyTransport(casted.getContextID(), true), mail);
+            }
         }
     }
 
