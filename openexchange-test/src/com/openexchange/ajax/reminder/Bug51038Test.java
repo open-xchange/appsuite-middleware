4cc317b05ad27d66feb1d6d5ac3ed392353de7c7
Martin Schneider <martin.schneider@open-xchange.com>
2017-01-18 11:29:44 +0100

    Adapt test to new client handling

--- openexchange-test/src/com/openexchange/ajax/reminder/Bug51038Test.java
+++ openexchange-test/src/com/openexchange/ajax/reminder/Bug51038Test.java
@@ -49,13 +49,14 @@
 
 package com.openexchange.ajax.reminder;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import java.util.Calendar;
-import com.openexchange.ajax.framework.AJAXClient;
-import com.openexchange.ajax.framework.AJAXClient.User;
-import com.openexchange.ajax.reminder.actions.RangeRequest;
-import com.openexchange.ajax.reminder.actions.RangeResponse;
 import com.openexchange.ajax.framework.AbstractAJAXSession;
 import com.openexchange.ajax.framework.CommonDeleteResponse;
+import com.openexchange.ajax.reminder.actions.RangeRequest;
+import com.openexchange.ajax.reminder.actions.RangeResponse;
 import com.openexchange.groupware.calendar.TimeTools;
 import com.openexchange.groupware.container.Appointment;
 import com.openexchange.groupware.reminder.ReminderObject;
@@ -66,59 +67,50 @@ import com.openexchange.test.CalendarTestManager;
  */
 public class Bug51038Test extends AbstractAJAXSession {
 
-    private CalendarTestManager ctm1;
-    private AJAXClient client2;
     private CalendarTestManager ctm2;
     private int nextYear;
     private ReminderObject reminder;
     private Appointment appointment;
 
-    public Bug51038Test(String name) {
-        super(name);
-    }
-
     @Override
     public void setUp() throws Exception {
         nextYear = Calendar.getInstance().get(Calendar.YEAR) + 1;
         super.setUp();
-        ctm1 = new CalendarTestManager(client);
-        client2 = new AJAXClient(User.User2);
-        ctm2 = new CalendarTestManager(client2);
+        ctm2 = new CalendarTestManager(getClient2());
 
         appointment = new Appointment();
         appointment.setTitle(this.getClass().getSimpleName());
         appointment.setStartDate(TimeTools.D("17.01." + nextYear + " 11:00"));
         appointment.setEndDate(TimeTools.D("17.01." + nextYear + " 12:00"));
         appointment.setAlarm(15);
-        appointment.setParentFolderID(client.getValues().getPrivateAppointmentFolder());
+        appointment.setParentFolderID(getClient().getValues().getPrivateAppointmentFolder());
         appointment.setIgnoreConflicts(true);
 
-        ctm1.insert(appointment);
+        catm.insert(appointment);
     }
 
     public void testBug51038() throws Exception {
 
-        Calendar cal = Calendar.getInstance(client.getValues().getTimeZone());
+        Calendar cal = Calendar.getInstance(getClient().getValues().getTimeZone());
         cal.setTime(appointment.getEndDate());
         RangeRequest rangeReq = new RangeRequest(cal.getTime());
-        RangeResponse rangeResp = client.execute(rangeReq);
-        reminder = ReminderTools.searchByTarget(rangeResp.getReminder(client.getValues().getTimeZone()), appointment.getObjectID());
+        RangeResponse rangeResp = getClient().execute(rangeReq);
+        reminder = ReminderTools.searchByTarget(rangeResp.getReminder(getClient().getValues().getTimeZone()), appointment.getObjectID());
         int reminderId = reminder.getObjectId();
         
         com.openexchange.ajax.reminder.actions.DeleteRequest delReminderReq = new com.openexchange.ajax.reminder.actions.DeleteRequest(reminder, false);
-        CommonDeleteResponse response = client2.execute(delReminderReq);
+        CommonDeleteResponse response = getClient2().execute(delReminderReq);
         assertTrue("Expected error.", response.hasError());
 
         rangeReq = new RangeRequest(cal.getTime());
-        rangeResp = client.execute(rangeReq);
-        reminder = ReminderTools.searchByTarget(rangeResp.getReminder(client.getValues().getTimeZone()), appointment.getObjectID());
+        rangeResp = getClient().execute(rangeReq);
+        reminder = ReminderTools.searchByTarget(rangeResp.getReminder(getClient().getValues().getTimeZone()), appointment.getObjectID());
         assertNotNull("Missing reminder.", reminder);
         assertEquals("Wrong reminder.", reminderId, reminder.getObjectId());
     }
 
     @Override
     public void tearDown() throws Exception {
-        ctm1.cleanUp();
         ctm2.cleanUp();
         super.tearDown();
     }
