42c8b3f6e673a1366186beb1fbf9cb2bf2607114
Martin Schneider <martin.schneider@open-xchange.com>
2017-01-18 12:16:06 +0100

    Run sequentially (might be reverted later ... )

--- openexchange-test/src/com/openexchange/ajax/share/ShareTest.java
+++ openexchange-test/src/com/openexchange/ajax/share/ShareTest.java
@@ -72,6 +72,8 @@ import org.json.JSONException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 import com.openexchange.ajax.folder.Create;
 import com.openexchange.ajax.folder.actions.DeleteRequest;
 import com.openexchange.ajax.folder.actions.EnumAPI;
@@ -128,6 +130,7 @@ import com.openexchange.share.recipient.ShareRecipient;
  *
  * @author <a href="mailto:tobias.friedrich@open-xchange.com">Tobias Friedrich</a>
  */
+@RunWith(BlockJUnit4ClassRunner.class)
 public abstract class ShareTest extends AbstractSmtpAJAXSession {
 
     protected static final OCLGuestPermission[] TESTED_PERMISSIONS = new OCLGuestPermission[] { createNamedAuthorPermission("otto@example.com", "Otto Example", "secret"), createNamedGuestPermission("horst@example.com", "Horst Example", "secret"), createAnonymousGuestPermission("secret"), createAnonymousGuestPermission()
