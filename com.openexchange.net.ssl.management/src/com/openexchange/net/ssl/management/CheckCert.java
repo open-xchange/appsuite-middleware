9431f8c6dd2b797e178340743c57ba2a04aa059a
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2017-01-26 13:45:28 +0100

    MW-445: Introduced the ssl.management bundle and a rough interface for the management service

---
+++ com.openexchange.net.ssl.management/src/com/openexchange/net/ssl/management/CheckCert.java
@@ -0,0 +1,145 @@
+
+package com.openexchange.net.ssl.management;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.MessageDigest;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * {@link CheckCert}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class CheckCert {
+
+    public static void main(String[] args) throws Exception {
+        String host;
+        int port;
+        char[] passphrase;
+        if ((args.length == 1) || (args.length == 2)) {
+            String[] c = args[0].split(":");
+            host = c[0];
+            port = (c.length == 1) ? 443 : Integer.parseInt(c[1]);
+            String p = (args.length == 1) ? "changeit" : args[1];
+            passphrase = p.toCharArray();
+        } else {
+            System.out.println("Usage: java CheckCert <host>[:port] [passphrase]");
+            host = "localhost";
+            port = 443;
+            passphrase = "changeit".toCharArray();
+            //return;
+        }
+
+        File file = new File("jssecacerts");
+        if (file.isFile() == false) {
+            char SEP = File.separatorChar;
+            File dir = new File(System.getProperty("java.home") + SEP + "lib" + SEP + "security");
+            file = new File(dir, "jssecacerts");
+            if (file.isFile() == false) {
+                file = new File(dir, "cacerts");
+            }
+        }
+        System.out.println("Loading KeyStore " + file.getAbsolutePath() + "...");
+        InputStream in = new FileInputStream(file);
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(in, passphrase);
+        in.close();
+
+        SSLContext context = SSLContext.getInstance("TLS");
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(ks);
+        X509TrustManager defaultTrustManager = (X509TrustManager) tmf.getTrustManagers()[0];
+        AcceptAllTrustManager tm = new AcceptAllTrustManager(defaultTrustManager);
+        context.init(null, new TrustManager[] { tm }, null);
+        SSLSocketFactory factory = context.getSocketFactory();
+
+        System.out.println("Opening connection to " + host + ":" + port + "...");
+        SSLSocket socket = (SSLSocket) factory.createSocket(host, port);
+        socket.setSoTimeout(10000);
+        try {
+            System.out.println("Starting SSL handshake...");
+            socket.startHandshake();
+            socket.close();
+            System.out.println();
+            System.out.println("No errors, certificate is already trusted");
+        } catch (SSLException e) {
+            System.out.println("ERROR: ");
+            e.printStackTrace();
+        }
+
+        X509Certificate[] chain = tm.chain;
+        if (chain == null) {
+            System.out.println("Could not obtain server certificate chain");
+            return;
+        }
+
+        System.out.println();
+        System.out.println("Server sent " + chain.length + " certificate(s):");
+        System.out.println();
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        for (int i = 0; i < chain.length; i++) {
+            X509Certificate cert = chain[i];
+            System.out.println(" " + (i + 1) + " Subject " + cert.getSubjectDN());
+            System.out.println("   Issuer  " + cert.getIssuerDN());
+            System.out.println("Expiration Date: " + cert.getNotAfter());
+            System.out.println("Serial Number: " + cert.getSerialNumber().toString(16));
+            sha256.update(cert.getEncoded());
+            System.out.println("[   sha-256    ]: " + toHexString(sha256.digest()));
+            md5.update(cert.getEncoded());
+            System.out.println("[     md5      ]: " + toHexString(md5.digest()));
+            System.out.println();
+        }
+    }
+
+    private static final char[] HEXDIGITS = "0123456789abcdef".toCharArray();
+
+    private static String toHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * 3);
+        for (int b : bytes) {
+            b &= 0xff;
+            sb.append(HEXDIGITS[b >> 4]);
+            sb.append(HEXDIGITS[b & 15]);
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    private static class AcceptAllTrustManager implements X509TrustManager {
+
+        private final X509TrustManager tm;
+        private X509Certificate[] chain;
+
+        AcceptAllTrustManager(X509TrustManager tm) {
+            this.tm = tm;
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return tm.getAcceptedIssuers();
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            //noop
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            this.chain = chain;
+            tm.checkServerTrusted(chain, authType);
+        }
+    }
+
+}
---
+++ com.openexchange.net.ssl.management/src/com/openexchange/net/ssl/management/SSLCertificateManagementService.java
@@ -0,0 +1,105 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2017-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.net.ssl.management;
+
+import com.openexchange.exception.OXException;
+
+/**
+ * {@link SSLCertificateManagementService}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public interface SSLCertificateManagementService {
+
+    /**
+     * Checks whether the SSL {@link Certificate} with the specified fingerprint
+     * is already trusted by the specified user in the specified context
+     * 
+     * @param userId The user's identifier
+     * @param contextId The context's identifier
+     * @param fingerprint The SSL {@link Certificate}'s fingerprint
+     * @return <code>true</code> if the {@link Certificate} is trusted; <code>false</code> otherwise
+     * @throws OXException if the certificate is not found or any other error is occurred
+     */
+    boolean isTrusted(int userId, int contextId, String fingerprint) throws OXException;
+
+    /**
+     * Checks whether the SSL {@link Certificate} with the specified fingerprint
+     * already exists for the specified user in the specified context
+     * 
+     * @param userId The user identifier
+     * @param contextId The context identifier
+     * @param fingerprint The SSL {@link Certificate}'s fingerprint
+     * @return <code>true</code> if the {@link Certificate} exists; <code>false</code> otherwise
+     * @throws OXException if any error is occurred
+     */
+    boolean contains(int userId, int contextId, String fingerprint) throws OXException;
+
+    /**
+     * Stores the specified {@link Certificate} for the specified user in the specified context
+     * 
+     * @param userId The user's identifier
+     * @param contextId The context's identifier
+     * @param certificate The SSL {@link Certificate} to store
+     * @throws OXException if any error is occurred
+     */
+    void store(int userId, int contextId, Certificate certificate) throws OXException;
+
+    /**
+     * Deletes the SSL {@link Certificate} with the specified fingerprint for the specified
+     * user in the specified context
+     * 
+     * @param userId The user identifier
+     * @param contextId The context identifer
+     * @param fingerprint The SSL {@link Certificate}'s fingerprint
+     * @throws OXException if the certificate is not found or any other error is occurred
+     */
+    void delete(int userId, int contextId, String fingerprint) throws OXException;
+}
