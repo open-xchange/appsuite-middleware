a9931febbdd4a0df64ee8bfd2f3364532eb63a13
Thorben Betten <thorben.betten@open-xchange.com>
2017-02-21 12:04:50 +0100

    MW-633: Fixed wording

--- com.openexchange.server/src/com/openexchange/mail/MailExceptionStrings.java
+++ com.openexchange.server/src/com/openexchange/mail/MailExceptionStrings.java
@@ -247,15 +247,15 @@ public final class MailExceptionStrings implements LocalizableStrings {
     public static final String SUBSYSTEM_DOWN_MSG = "A temporary failure because a subsystem is down (maybe due to maintenance). Please try again later.";
 
     // Thrown when a mail access has been disabled. That is it exceeded the limit for failed authentication count.
-    public static final String MAIL_ACCESS_DISABLED_MSG = "Accessing server %1$s is disabled. Please update the account and enter valid credentials in order to use it again.";
+    public static final String MAIL_ACCESS_DISABLED_MSG = "Your mail account %2$s was disabled due to invalid credentials. Please edit the account and enter correct credentials to enable it again";
 
     // Thrown when an oauth-associated mail access has been disabled. That is it exceeded the limit for failed authentication count.
-    public static final String MAIL_ACCESS_DISABLED_OAUTH_MSG = "Accessing server %1$s is disabled. Please reauthorize the associated OAuth account %5$s in order to use it again.";
+    public static final String MAIL_ACCESS_DISABLED_OAUTH_MSG = "Your mail account %2$s was disabled because of authorization issues. Please reauthorize the account %5$s to enable it again.";
 
     // Thrown when a mail transport has been disabled. That is it exceeded the limit for failed authentication count.
-    public static final String MAIL_TRANSPORT_DISABLED_MSG = "Accessing server %1$s is disabled. Please update the account and enter valid credentials in order to use it again.";
+    public static final String MAIL_TRANSPORT_DISABLED_MSG = "Your mail transport for  %2$s was disabled due to invalid credentials. Please edit the account and enter correct credentials to enable it again";
 
     // Thrown when an oauth-associated mail transport has been disabled. That is it exceeded the limit for failed authentication count.
-    public static final String MAIL_TRANSPORT_DISABLED_OAUTH_MSG = "Accessing server %1$s is disabled. Please reauthorize the associated OAuth account %5$s in order to use it again.";
+    public static final String MAIL_TRANSPORT_DISABLED_OAUTH_MSG = "Your mail transort for %2$s was disabled because of authorization issues. Please reauthorize the account %5$s to enable it again.";
 
 }
