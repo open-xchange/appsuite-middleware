602cf02882a11f32b5a65d83a4ecf23cd8884900
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2017-02-21 13:23:15 +0100

    MW-639: c.o.userfeedback.json bundle

---
+++ com.openexchange.userfeedback.json/src/com/openexchange/userfeedback/json/actions/CollectAction.java
@@ -0,0 +1,118 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.userfeedback.json.actions;
+
+import java.util.Date;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.openexchange.ajax.requesthandler.AJAXActionService;
+import com.openexchange.ajax.requesthandler.AJAXRequestData;
+import com.openexchange.ajax.requesthandler.AJAXRequestResult;
+import com.openexchange.exception.OXException;
+import com.openexchange.java.Strings;
+import com.openexchange.server.ServiceLookup;
+import com.openexchange.tools.servlet.AjaxExceptionCodes;
+import com.openexchange.tools.session.ServerSession;
+import com.openexchange.uadetector.UserAgentParser;
+import net.sf.uadetector.ReadableUserAgent;
+
+
+/**
+ * {@link CollectAction}
+ *
+ * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ * @since v7.8.4
+ */
+public class CollectAction implements AJAXActionService {
+
+    private final ServiceLookup services;
+
+    public CollectAction(ServiceLookup services) {
+        super();
+        this.services = services;
+    }
+
+    @Override
+    public AJAXRequestResult perform(AJAXRequestData requestData, ServerSession session) throws OXException {
+        Object data = requestData.getData();
+        if (null == data || !JSONObject.class.isInstance(data)) {
+            throw AjaxExceptionCodes.MISSING_REQUEST_BODY.create();
+        }
+        String type = requestData.getParameter("type");
+        if (null == type || Strings.isEmpty(type)) {
+            throw AjaxExceptionCodes.MISSING_PARAMETER.create("type");
+        }
+        JSONObject json = (JSONObject) data;
+        Date now = new Date();
+        int ctxId = session.getContextId();
+        int userId = session.getUserId();
+        String loginName = session.getLoginName();
+        UserAgentParser parser = services.getService(UserAgentParser.class);
+        String userAgent = requestData.getUserAgent();
+        try {
+            int score = json.getInt("score");
+            String app = json.getString("app");
+            String entryPoint = json.getString("entryPoint");
+            String comment = json.getString("comment");
+            String screenResolution = json.getString("screenResolution");
+            String language = json.getString("language");
+
+            ReadableUserAgent agent = parser.parse(userAgent);
+            String os = agent.getOperatingSystem().getFamilyName();
+            String osVersion = agent.getOperatingSystem().getVersionNumber().toVersionString();
+            String browser = agent.getName();
+            String browserVersion = agent.getVersionNumber().toVersionString();
+        } catch (JSONException e) {
+            throw AjaxExceptionCodes.JSON_ERROR.create(e.getMessage(), e);
+        }
+        return new AJAXRequestResult();
+    }
+
+}
