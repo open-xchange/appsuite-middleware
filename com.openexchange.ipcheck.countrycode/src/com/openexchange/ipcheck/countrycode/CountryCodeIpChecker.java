aa61ae4db736fc3429af56ea9f74acc2f17576da
Thorben Betten <thorben.betten@open-xchange.com>
2017-03-11 10:55:03 +0100

    COV-17463: Fixed possible NPE

--- com.openexchange.ipcheck.countrycode/src/com/openexchange/ipcheck/countrycode/CountryCodeIpChecker.java
+++ com.openexchange.ipcheck.countrycode/src/com/openexchange/ipcheck/countrycode/CountryCodeIpChecker.java
@@ -194,7 +194,12 @@ public class CountryCodeIpChecker implements IPChecker, MetricAware<IPCheckMetri
      * @throws OXException To actually kick the session
      */
     private void deny(String current, String previous, Session session, DenyReason reason, Throwable t) throws OXException {
-        LOGGER.debug("The IP change from '{}' to '{}' was denied. Reason: '{}', '{}'", previous, current, reason.getMessage(), t.getMessage());
+        if (null == t) {
+            LOGGER.debug("The IP change from '{}' to '{}' was denied. Reason: '{}'", previous, current, reason.getMessage());
+        } else {
+            LOGGER.debug("The IP change from '{}' to '{}' was denied. Reason: '{}', '{}'", previous, current, reason.getMessage(), t.getMessage());
+        }
+
         switch (reason) {
             case EXCEPTION:
                 metrics.incrementDeniedException();
