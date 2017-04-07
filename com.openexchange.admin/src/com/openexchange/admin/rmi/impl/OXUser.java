14125d7605b7bf00a9ba3a1fd3b241bbf8cf4cbf
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2017-04-07 12:41:37 +0200

    Bug #52721: refactored mail standard folder sanitizing (cherry picked from commit 1f88d1e7043090cc122817573fa35bd440f8b527)

--- com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXUser.java
+++ com.openexchange.admin/src/com/openexchange/admin/rmi/impl/OXUser.java
@@ -1770,9 +1770,9 @@ public class OXUser extends OXCommonImpl implements OXUserInterface {
         if (viewFactory != null) {
             ConfigView view;
             try {
-                view = viewFactory.getView(usr.getId(), ctx.getId());
-                Boolean check = view.get("com.openexchange.imap.initWithSpecialUse", Boolean.class);
-                if (check != null && check) {
+                view = viewFactory.getView(usr.getId().intValue(), ctx.getId().intValue());
+                Boolean check = view.opt("com.openexchange.imap.initWithSpecialUse", Boolean.class, true);
+                if (check != null && check.booleanValue()) {
                     ConfigProperty<Boolean> prop = view.property("user", "com.openexchange.mail.specialuse.check", Boolean.class);
                     prop.set(Boolean.TRUE);
                 }
