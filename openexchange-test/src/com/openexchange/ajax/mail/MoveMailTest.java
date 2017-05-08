f8cf078c11ae3d79bdd3c27023ac12536d954a22
Daniel Becker <daniel.becker@open-xchange.com>
2017-05-08 16:23:20 +0200

    Use inherited manager

--- openexchange-test/src/com/openexchange/ajax/mail/MoveMailTest.java
+++ openexchange-test/src/com/openexchange/ajax/mail/MoveMailTest.java
@@ -95,8 +95,6 @@ public class MoveMailTest extends AbstractMailTest {
 
     @Test
     public void testShouldMoveFromSentToDrafts() throws OXException, IOException, SAXException, JSONException {
-        MailTestManager manager = new MailTestManager(getClient(), false);
-
         final String eml = "Date: Mon, 19 Nov 2012 21:36:51 +0100 (CET)\n" + "From: " + getSendAddress() + "\n" + "To: " + getSendAddress() + "\n" + "Message-ID: <1508703313.17483.1353357411049>\n" + "Subject: Move a mail\n" + "MIME-Version: 1.0\n" + "Content-Type: multipart/alternative; \n" + "    boundary=\"----=_Part_17482_1388684087.1353357411002\"\n" + "\n" + "------=_Part_17482_1388684087.1353357411002\n" + "MIME-Version: 1.0\n" + "Content-Type: text/plain; charset=UTF-8\n" + "Content-Transfer-Encoding: 7bit\n" + "\n" + "Move from sent to drafts\n" + "------=_Part_17482_1388684087.1353357411002\n" + "MIME-Version: 1.0\n" + "Content-Type: text/html; charset=UTF-8\n" + "Content-Transfer-Encoding: 7bit\n" + "\n" + "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\">" + " <head>\n" + "    <meta content=\"text/html; charset=UTF-8\" http-equiv=\"Content-Type\"/>\n" + " </head><body style=\"font-family: verdana,geneva; font-size: 10pt; \">\n" + " \n" + "  <div>\n" + "   Move from sent to drafts\n" + "  </div>\n" + " \n" + "</body></html>\n" + "------=_Part_17482_1388684087.1353357411002--\n";
 
         getClient().execute(new NewMailRequest(getInboxFolder(), eml, -1, true));
@@ -107,21 +105,19 @@ public class MoveMailTest extends AbstractMailTest {
         TestMail myMail = new TestMail(getFirstMailInFolder(origin));
         String oldID = myMail.getId();
 
-        TestMail movedMail = manager.move(myMail, destination);
+        TestMail movedMail = mtm.move(myMail, destination);
         String newID = movedMail.getId();
 
-        manager.get(destination, newID);
-        assertTrue("Should produce no errors when getting moved e-mail", !manager.getLastResponse().hasError());
-        assertTrue("Should produce no conflicts when getting moved e-mail", !manager.getLastResponse().hasConflicts());
+        mtm.get(destination, newID);
+        assertTrue("Should produce no errors when getting moved e-mail", !mtm.getLastResponse().hasError());
+        assertTrue("Should produce no conflicts when getting moved e-mail", !mtm.getLastResponse().hasConflicts());
 
-        manager.get(origin, oldID);
-        assertTrue("Should produce errors when trying to get moved e-mail from original place", manager.getLastResponse().hasError());
+        mtm.get(origin, oldID);
+        assertTrue("Should produce errors when trying to get moved e-mail from original place", mtm.getLastResponse().hasError());
     }
 
     @Test
     public void testShouldNotMoveToNonExistentFolder() throws OXException, IOException, SAXException, JSONException {
-        MailTestManager manager = new MailTestManager(getClient(), false);
-
         final String eml = "Date: Mon, 19 Nov 2012 21:36:51 +0100 (CET)\n" + "From: " + getSendAddress() + "\n" + "To: " + getSendAddress() + "\n" + "Message-ID: <1508703313.17483.1353357411049>\n" + "Subject: Move a mail\n" + "MIME-Version: 1.0\n" + "Content-Type: multipart/alternative; \n" + "    boundary=\"----=_Part_17482_1388684087.1353357411002\"\n" + "\n" + "------=_Part_17482_1388684087.1353357411002\n" + "MIME-Version: 1.0\n" + "Content-Type: text/plain; charset=UTF-8\n" + "Content-Transfer-Encoding: 7bit\n" + "\n" + "Move from sent to drafts\n" + "------=_Part_17482_1388684087.1353357411002\n" + "MIME-Version: 1.0\n" + "Content-Type: text/html; charset=UTF-8\n" + "Content-Transfer-Encoding: 7bit\n" + "\n" + "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\">" + " <head>\n" + "    <meta content=\"text/html; charset=UTF-8\" http-equiv=\"Content-Type\"/>\n" + " </head><body style=\"font-family: verdana,geneva; font-size: 10pt; \">\n" + " \n" + "  <div>\n" + "   Move from sent to drafts\n" + "  </div>\n" + " \n" + "</body></html>\n" + "------=_Part_17482_1388684087.1353357411002--\n";
 
         getClient().execute(new NewMailRequest(getInboxFolder(), eml, -1, true));
@@ -132,33 +128,32 @@ public class MoveMailTest extends AbstractMailTest {
         TestMail myMail = new TestMail(getFirstMailInFolder(origin));
         String oldID = myMail.getId();
 
-        manager.move(myMail, destination);
-        assertTrue("Should produce error message when trying to move to nonexistent folder", manager.getLastResponse().hasError());
-        assertEquals("Should produce proper error message ", "IMAP-1002", manager.getLastResponse().getException().getErrorCode());
+        mtm.move(myMail, destination);
+        assertTrue("Should produce error message when trying to move to nonexistent folder", mtm.getLastResponse().hasError());
+        assertEquals("Should produce proper error message ", "IMAP-1002", mtm.getLastResponse().getException().getErrorCode());
 
-        manager.get(origin, oldID);
-        assertTrue("Should still have e-mail at original location", !manager.getLastResponse().hasError());
-        assertTrue("Should produce no conflicts when getting e-mail from original location", !manager.getLastResponse().hasConflicts());
+        mtm.get(origin, oldID);
+        assertTrue("Should still have e-mail at original location", !mtm.getLastResponse().hasError());
+        assertTrue("Should produce no conflicts when getting e-mail from original location", !mtm.getLastResponse().hasConflicts());
     }
 
     @Test
-    public void testShouldNotTryToMoveToSameFolder() throws Exception {
-        MailTestManager manager = new MailTestManager(getClient(), true);
+    public void testShouldNotTryToMoveToSameFolder() throws Exception {        
         //Send mail to myself
         final String eml = ("Message-Id: <4A002517.4650.0059.1@foobar.com>\n" + "Date: Tue, 05 May 2009 11:37:58 -0500\n" + "From: #ADDR#\n" + "To: #ADDR#\n" + "Subject: Invitation for launch\n" + "Mime-Version: 1.0\n" + "Content-Type: text/plain; charset=\"UTF-8\"\n" + "Content-Transfer-Encoding: 8bit\n" + "\n" + "This is a MIME message. If you are reading this text, you may want to \n" + "consider changing to a mail reader or gateway that understands how to \n" + "properly handle MIME multipart messages.").replaceAll("#ADDR#", getSendAddress());
         NewMailResponse newMailResponse = getClient().execute(new NewMailRequest(getInboxFolder(), eml, -1, true));
         String folder = newMailResponse.getFolder();
         String id = newMailResponse.getId();
         // Check if mail exists in sent folder
-        TestMail newMail = manager.get(new String[] { folder, id });
+        TestMail newMail = mtm.get(new String[] { folder, id });
         assertNotNull("New mail may not be null and has to be found in sent-items folder", newMail);
         // Try to move to sent-items
-        TestMail movedMail = manager.move(newMail, folder);
+        TestMail movedMail = mtm.move(newMail, folder);
         assertNotNull("Moved mail may not be null", movedMail);
         // Check that mail remains in the original location
         assertEquals("Mail should not be moved and remain in the original folder.", folder, movedMail.getFolder());
         // Check that sent mail wasn't duplicated
-        assertEquals("Mail shouldn't have been duplicated", 1, manager.findSimilarMailsInSameFolder(newMail, getClient()).size());
+        assertEquals("Mail shouldn't have been duplicated", 1, mtm.findSimilarMailsInSameFolder(newMail, getClient()).size());
     }
 
 }
