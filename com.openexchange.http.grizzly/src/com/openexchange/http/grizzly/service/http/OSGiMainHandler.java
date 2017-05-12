ba00a91495b6e0acd052b566b0f57b1fc67d4519
Thorben Betten <thorben.betten@open-xchange.com>
2017-05-12 08:44:31 +0200

    null guard

--- com.openexchange.http.grizzly/src/com/openexchange/http/grizzly/service/http/OSGiMainHandler.java
+++ com.openexchange.http.grizzly/src/com/openexchange/http/grizzly/service/http/OSGiMainHandler.java
@@ -313,7 +313,7 @@ public class OSGiMainHandler extends HttpHandler implements OSGiHandler {
              */
             if (context == null) {
                 LOG.debug("No HttpContext provided, creating default");
-                context = httpService.createDefaultHttpContext();
+                context = null == httpService ? new HttpContextImpl(bundle) : httpService.createDefaultHttpContext();
             }
             registeredHttpContexts.put(context, context);
 
@@ -394,7 +394,7 @@ public class OSGiMainHandler extends HttpHandler implements OSGiHandler {
 
             if (context == null) {
                 LOG.debug("No HttpContext provided, creating default");
-                context = httpService.createDefaultHttpContext();
+                context = null == httpService ? new HttpContextImpl(bundle) : httpService.createDefaultHttpContext();
             }
 
             OSGiServletContext servletContext =
@@ -437,7 +437,7 @@ public class OSGiMainHandler extends HttpHandler implements OSGiHandler {
 
             if (context == null) {
                 LOG.debug("No HttpContext provided, creating default");
-                context = httpService.createDefaultHttpContext();
+                context = null == httpService ? new HttpContextImpl(bundle) : httpService.createDefaultHttpContext();
             }
             registeredHttpContexts.put(context, context);
 
