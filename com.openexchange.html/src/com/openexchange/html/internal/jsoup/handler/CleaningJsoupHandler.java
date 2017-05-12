98b0e425f69bfd839b6b3668944e31a01ad41eb0
Thorben Betten <thorben.betten@open-xchange.com>
2017-05-12 08:59:25 +0200

    Avoid applying regex

--- com.openexchange.html/src/com/openexchange/html/internal/jsoup/handler/CleaningJsoupHandler.java
+++ com.openexchange.html/src/com/openexchange/html/internal/jsoup/handler/CleaningJsoupHandler.java
@@ -942,7 +942,7 @@ public final class CleaningJsoupHandler implements JsoupHandler {
                 builder.append('.').append(cssPrefix).append('-').append(replaceDots(word.substring(1), cssPrefix)).append(' ');
             } else if ('#' == first) {
                 if (word.indexOf('.') < 0) { // contains no dots
-                    builder.append('#').append(cssPrefix).append('-').append(replaceDots(word.substring(1), cssPrefix)).append(' ');
+                    builder.append('#').append(cssPrefix).append('-').append(word.substring(1)).append(' ');
                 } else {
                     builder.append('#').append(cssPrefix).append('-').append(replaceDots(word.substring(1), cssPrefix)).append(' ');
                 }
