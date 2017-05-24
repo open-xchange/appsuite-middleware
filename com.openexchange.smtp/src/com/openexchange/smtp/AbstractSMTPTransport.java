3cad075d8a8de3f14ba5226229e62c7b532516eb
Thorben Betten <thorben.betten@open-xchange.com>
2017-05-24 21:02:24 +0200

    Confirm that Guard encrypt mails were handled before sending.  Prevent plaintext send if guard-backend-plugin not installed #2

--- com.openexchange.smtp/src/com/openexchange/smtp/AbstractSMTPTransport.java
+++ com.openexchange.smtp/src/com/openexchange/smtp/AbstractSMTPTransport.java
@@ -903,9 +903,12 @@ abstract class AbstractSMTPTransport extends MailTransport implements MimeSuppor
                 Result result = listenerChain.onBeforeMessageTransport(messageToSend, recipients, securitySettings, session);
 
                 // If Guard installed, and mail marked to be encrypted, reply must contain change.  Check to make sure not sending encrypted email without Guard fully installed
+                /*-
+                 *
                 if ((securitySettings != null) && securitySettings.isEncrypt() && result.getReply() == Reply.NEUTRAL) {
                     throw SMTPExceptionCode.INTERNAL_ERROR.create("Bad repsonse from Guard backend, Guard-backend-plugin installed?");
                 }
+                 */
 
                 // Examine reply of the listener chain
                 {
