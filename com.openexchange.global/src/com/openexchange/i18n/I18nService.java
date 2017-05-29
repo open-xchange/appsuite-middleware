e7df647583bcdddd29733bada034c8ec3dd9e49e
Thorben Betten <thorben.betten@open-xchange.com>
2017-05-29 10:35:44 +0200

    [Mobile API Facade]: Added i18n singleton service, which collects available I18nService instances, providing methods for exact and best-fit matches
    (cherry picked from commit a745eb9dbacc8d92b1c81e0c5790030a83a16437)

--- com.openexchange.global/src/com/openexchange/i18n/I18nService.java
+++ com.openexchange.global/src/com/openexchange/i18n/I18nService.java
@@ -49,18 +49,22 @@
 
 package com.openexchange.i18n;
 
+import java.util.Dictionary;
 import java.util.Locale;
+import org.osgi.framework.BundleContext;
 
 /**
- * Service for publishing implementations to translate texts into other languages. Implementating services must carry the property named {@link #LANGUAGE} containing the string representation of the locate returned by the {@link #getLocale()} method.
- * 
+ * Service for publishing implementations to translate texts into other languages.
+ * <p>
+ * Implementing services must carry the {@link BundleContext#registerService(Class, Object, Dictionary) property} named {@link #LANGUAGE} containing the string representation of the locale returned by the {@link #getLocale()} method.
+ *
  * @author <a href="mailto:ben.pahne@open-xchange">Ben Pahne</a>
  */
 public interface I18nService {
 
     static final String LANGUAGE = "language";
 
-    String getLocalized(String key);    
+    String getLocalized(String key);
 
     boolean hasKey(String key);
 
@@ -70,7 +74,7 @@ public interface I18nService {
      * Localizes the given key.
      * This method is also a marker for xgettext to generate the translation templates.
      * For wrapping this with additional functionality, you must just keep the method signature identical and the call must contain the strings rather than variables.
-     * 
+     *
      * @param key
      * @return
      */
@@ -81,9 +85,9 @@ public interface I18nService {
      * This method is also a marker for xgettext to generate the translation templates.
      * If you want something translated with a context you should use this method to avoid ambiguous keys which might have different translations depending on the context.
      * For wrapping this with additional functionality, you must just keep the method signature identical and the call must contain the strings rather than variables.
-     * 
+     *
      * For simple localization without context you can just use the com.openexchange.i18n.LocalizableStrings marker Interface.
-     * 
+     *
      * @param messageContext The context
      * @param key
      * @return
@@ -97,9 +101,9 @@ public interface I18nService {
      * The plural form describes the number used for the translation. 0 means singular, everything >0 is a plural form. There should only be one english plural form, but other languages might have additional forms.
      * If a plural form is not available this defaults to the highest available form.
      * For wrapping this with additional functionality, you must just keep the method signature identical and the call must contain the strings rather than variables.
-     * 
+     *
      * For simple localization without context you can just use the com.openexchange.i18n.LocalizableStrings marker Interface.
-     * 
+     *
      * @param messageContext The context
      * @param key The english singular String
      * @param keyPlural The english plural String
