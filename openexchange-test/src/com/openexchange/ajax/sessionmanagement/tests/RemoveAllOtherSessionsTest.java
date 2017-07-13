bddf3b2b3ee3d0bbbf403264013496fc58ddc1ce
Daniel Becker <daniel.becker@open-xchange.com>
2017-07-13 09:17:54 +0200

    MW-772 Reworked test & adjusted docu

--- openexchange-test/src/com/openexchange/ajax/sessionmanagement/tests/RemoveAllOtherSessionsTest.java
+++ openexchange-test/src/com/openexchange/ajax/sessionmanagement/tests/RemoveAllOtherSessionsTest.java
@@ -49,7 +49,9 @@
 
 package com.openexchange.ajax.sessionmanagement.tests;
 
+import java.io.IOException;
 import java.util.Collection;
+import org.json.JSONException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -60,8 +62,10 @@ import com.openexchange.ajax.sessionmanagement.actions.GetSessionsRequest;
 import com.openexchange.ajax.sessionmanagement.actions.GetSessionsResponse;
 import com.openexchange.ajax.sessionmanagement.actions.RemoveAllOtherSessionsRequest;
 import com.openexchange.ajax.sessionmanagement.actions.RemoveAllOtherSessionsResponse;
+import com.openexchange.exception.OXException;
 import com.openexchange.session.management.ManagedSession;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
 
 /**
  * {@link RemoveAllOtherSessionsTest}
@@ -70,6 +74,8 @@ import static org.hamcrest.Matchers.is;
  * @since v7.10.0
  */
 public class RemoveAllOtherSessionsTest extends AbstractSessionManagementTest {
+
+    private static final String BLACKLISTED_CLIENT = "randomClientForTesting";
 
     @SuppressWarnings("unused")
     private AJAXClient testClient3;
@@ -85,9 +91,7 @@ public class RemoveAllOtherSessionsTest extends AbstractSessionManagementTest {
     @After
     public void tearDown() throws Exception {
         // Logout only client2. Client 1 & 3 are being removed by the request and can not logout
-        if (null != testClient2 && null != testClient2.getSession() && null != testClient2.getSession().getId()) {
-            testClient2.logout();
-        }
+        saveLogout(testClient2);
     }
 
     @Test
@@ -102,11 +106,49 @@ public class RemoveAllOtherSessionsTest extends AbstractSessionManagementTest {
         GetSessionsResponse getResponse = testClient2.execute(getRequest);
         Assert.assertFalse(getResponse.hasError());
         Collection<ManagedSession> sessions = getResponse.getSessions();
-        Assert.assertThat("Not all clients have removed", 1, is(sessions.size()));
+        Assert.assertThat("Not all clients has been removed", 1, is(sessions.size()));
 
         for (ManagedSession session : sessions) {
             Assert.assertThat("Wrong client is still loged in", sessionId, is(session.getSessionId()));
         }
 
+    }
+
+    @Test
+    public void testRemoveAllOtherSessionsWithoutBlacklisted() throws Exception {
+        AJAXClient blackListed = null;
+        try {
+            blackListed = new AJAXClient(testUser2, BLACKLISTED_CLIENT);
+
+            String sessionId = testClient2.getSession().getId();
+            RemoveAllOtherSessionsRequest request = new RemoveAllOtherSessionsRequest(sessionId);
+            RemoveAllOtherSessionsResponse response = testClient2.execute(request);
+
+            Assert.assertFalse(response.hasError());
+
+            GetSessionsRequest getRequest = new GetSessionsRequest();
+            GetSessionsResponse getResponse = testClient2.execute(getRequest);
+            Assert.assertFalse(getResponse.hasError());
+            Collection<ManagedSession> sessions = getResponse.getSessions();
+            Assert.assertThat("Not the excpected count of active clients", 2, is(sessions.size()));
+
+            for (ManagedSession session : sessions) {
+                if (sessionId.equals(session.getSessionId()) || blackListed.getSession().getId().equals(session.getSessionId())) {
+                    continue;
+                }
+                fail("Wrong session still logged in");
+            }
+
+        } finally {
+            saveLogout(blackListed);
+        }
+
+    }
+
+    private void saveLogout(AJAXClient client) throws OXException, IOException, JSONException {
+        // Try logout blacklisted
+        if (null != client && null != client.getSession() && null != client.getSession().getId()) {
+            client.logout();
+        }
     }
 }
