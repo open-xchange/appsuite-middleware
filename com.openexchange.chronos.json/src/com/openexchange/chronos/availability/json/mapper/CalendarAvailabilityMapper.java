f0c06e4d0d8312ab4d70fce6f6c84c0b06d2863c
Ioannis Chouklis <ioannis.chouklis@open-xchange.com.com>
2017-07-14 16:32:06 +0200

    MW-801: primitive get/set http api calls

---
+++ com.openexchange.chronos.json/src/com/openexchange/chronos/availability/json/mapper/CalendarAvailabilityMapper.java
@@ -0,0 +1,176 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.chronos.availability.json.mapper;
+
+import java.util.EnumMap;
+import com.openexchange.chronos.CalendarAvailability;
+import com.openexchange.chronos.service.AvailabilityField;
+import com.openexchange.exception.OXException;
+import com.openexchange.groupware.container.Appointment;
+import com.openexchange.groupware.container.DataObject;
+import com.openexchange.groupware.tools.mappings.json.DefaultJsonMapper;
+import com.openexchange.groupware.tools.mappings.json.JsonMapping;
+import com.openexchange.groupware.tools.mappings.json.StringMapping;
+
+/**
+ * {@link CalendarAvailabilityMapper}
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ */
+public class CalendarAvailabilityMapper extends DefaultJsonMapper<CalendarAvailability, AvailabilityField> {
+
+    private static final CalendarAvailabilityMapper INSTANCE = new CalendarAvailabilityMapper();
+
+    private final AvailabilityField[] mappedFields;
+
+    /**
+     * Gets the {@link CalendarAvailabilityMapper} instance.
+     *
+     * @return The {@link CalendarAvailabilityMapper} instance.
+     */
+    public static CalendarAvailabilityMapper getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Initialises a new {@link CalendarAvailabilityMapper}.
+     */
+    public CalendarAvailabilityMapper() {
+        super();
+        mappedFields = mappings.keySet().toArray(newArray(mappings.keySet().size()));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.groupware.tools.mappings.Factory#newInstance()
+     */
+    @Override
+    public CalendarAvailability newInstance() {
+        return new CalendarAvailability();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.groupware.tools.mappings.ArrayFactory#newArray(int)
+     */
+    @Override
+    public AvailabilityField[] newArray(int size) {
+        return new AvailabilityField[size];
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.groupware.tools.mappings.json.DefaultJsonMapper#createMappings()
+     */
+    @Override
+    protected EnumMap<AvailabilityField, ? extends JsonMapping<? extends Object, CalendarAvailability>> createMappings() {
+        EnumMap<AvailabilityField, JsonMapping<? extends Object, CalendarAvailability>> mappings = new EnumMap<AvailabilityField, JsonMapping<? extends Object, CalendarAvailability>>(AvailabilityField.class);
+        // TODO: add mappings
+        mappings.put(AvailabilityField.id, new StringMapping<CalendarAvailability>("id", DataObject.OBJECT_ID) {
+
+            @Override
+            public boolean isSet(CalendarAvailability object) {
+                return object.contains(AvailabilityField.id);
+            }
+
+            @Override
+            public void set(CalendarAvailability object, String value) throws OXException {
+                object.setId(value);
+            }
+
+            @Override
+            public String get(CalendarAvailability object) {
+                return object.getId();
+            }
+
+            @Override
+            public void remove(CalendarAvailability object) {
+                object.removeId();
+            }
+        });
+
+        mappings.put(AvailabilityField.uid, new StringMapping<CalendarAvailability>("uid", Appointment.UID) {
+
+            @Override
+            public boolean isSet(CalendarAvailability object) {
+                return object.contains(AvailabilityField.uid);
+            }
+
+            @Override
+            public void set(CalendarAvailability object, String value) throws OXException {
+                object.setUid(value);
+            }
+
+            @Override
+            public String get(CalendarAvailability object) {
+                return object.getUid();
+            }
+
+            @Override
+            public void remove(CalendarAvailability object) {
+                object.removeUid();
+            }
+        });
+        return mappings;
+    }
+
+    /**
+     * Gets the mappedFields
+     *
+     * @return The mappedFields
+     */
+    public AvailabilityField[] getMappedFields() {
+        return mappedFields;
+    }
+
+}
