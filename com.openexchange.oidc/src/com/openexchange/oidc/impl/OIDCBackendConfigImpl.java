ebb4482f3bd92b1cd1d5ada6d8ab846346d9c064
Vitali Sjablow <vitali.sjablow@open-xchange.com>
2017-07-15 15:59:36 +0200

    MW-808: Added own backend specific configuration.

---
+++ com.openexchange.oidc/src/com/openexchange/oidc/impl/OIDCBackendConfigImpl.java
@@ -0,0 +1,30 @@
+package com.openexchange.oidc.impl;
+
+import com.openexchange.config.lean.LeanConfigurationService;
+import com.openexchange.oidc.OIDCBackendConfig;
+import com.openexchange.oidc.OIDCBackendProperty;
+
+public class OIDCBackendConfigImpl implements OIDCBackendConfig{
+    
+    private LeanConfigurationService leanConfigurationService; 
+    
+    public OIDCBackendConfigImpl(LeanConfigurationService leanConfigurationService) {
+        this.leanConfigurationService = leanConfigurationService;
+    }
+
+    @Override
+    public String getClientID() {
+        return leanConfigurationService.getProperty(OIDCBackendProperty.clientId);
+    }
+
+    @Override
+    public String getRedirectURI() {
+        return leanConfigurationService.getProperty(OIDCBackendProperty.redirectURI);
+    }
+
+    @Override
+    public String getAuthorizationEndpoint() {
+        return leanConfigurationService.getProperty(OIDCBackendProperty.authorizationEndpoint);
+    }
+
+}
