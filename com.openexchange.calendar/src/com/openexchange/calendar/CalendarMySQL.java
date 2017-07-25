691adf2e9559274ca3e5a67b085df0eabe6909fd
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2017-07-25 10:59:39 +0200

    throw "maintenance" error when trying to write to legacy storage during migration (cherry picked from commit 98cfceec3214f83675c5b3886ad37b4cae03e2ea)
    (cherry picked from commit d870155985e2b53605b064442a53a396d58331a5)

--- com.openexchange.calendar/src/com/openexchange/calendar/CalendarMySQL.java
+++ com.openexchange.calendar/src/com/openexchange/calendar/CalendarMySQL.java
@@ -1749,6 +1749,7 @@ public class CalendarMySQL implements CalendarSqlImp {
 
     @Override
     public final CalendarDataObject[] insertAppointment(final CalendarDataObject cdao, final Connection writecon, final Session so) throws DataTruncation, SQLException, OXException, OXException {
+        checkNotReadOnly(so);
         return insertAppointment0(cdao, writecon, so, true);
     }
 
@@ -1776,6 +1777,7 @@ public class CalendarMySQL implements CalendarSqlImp {
     }
 
     private final CalendarDataObject[] insertAppointment0(final CalendarDataObject cdao, final Connection writecon, final Session so, final boolean notify) throws DataTruncation, SQLException, OXException, OXException {
+        checkNotReadOnly(so);
         checkQuota(so, writecon);
 
         int i = 1;
@@ -2714,6 +2716,7 @@ public class CalendarMySQL implements CalendarSqlImp {
 
     @Override
     public final CalendarDataObject[] updateAppointment(final CalendarDataObject cdao, final CalendarDataObject edao, final Connection writecon, final Session so, final Context ctx, final int inFolder, final java.util.Date clientLastModified) throws SQLException, OXException {
+        checkNotReadOnly(so);
         return updateAppointment(cdao, edao, writecon, so, ctx, inFolder, clientLastModified, true, false);
     }
 
@@ -4095,6 +4098,7 @@ public class CalendarMySQL implements CalendarSqlImp {
 
     @Override
     public final Date setUserConfirmation(final int oid, final int folderId, final int uid, final int confirm, final String message, final Session so, final Context ctx) throws OXException {
+        checkNotReadOnly(so);
         checkConfirmPermission(folderId, uid, so, ctx, false);
         Connection writecon = null;
         PreparedStatement pu = null;
@@ -4191,6 +4195,7 @@ public class CalendarMySQL implements CalendarSqlImp {
      */
     @Override
     public Date setExternalConfirmation(final int oid, final int folderId, final String mail, final int confirm, final String message, final Session so, final Context ctx) throws OXException {
+        checkNotReadOnly(so);
         checkConfirmPermission(folderId, -1, so, ctx, true);
 
         final String insert = "INSERT INTO dateExternal (confirm, reason, objectId, cid, mailAddress) VALUES (?, ?, ?, ?, ?)"; // this is a
@@ -4291,6 +4296,7 @@ public class CalendarMySQL implements CalendarSqlImp {
 
     @Override
     public final long attachmentAction(final int folderId, final int oid, final int uid, final Session session, final Context c, final int numberOfAttachments) throws OXException {
+        checkNotReadOnly(session);
         int changes;
         PreparedStatement pst = null;
         int amount = 0;
@@ -4792,11 +4798,13 @@ public class CalendarMySQL implements CalendarSqlImp {
 
     @Override
     public final void deleteAppointment(final int uid, final CalendarDataObject cdao, final Connection writecon, final Session so, final Context ctx, final int inFolder, final java.util.Date clientLastModified) throws SQLException, OXException {
+        checkNotReadOnly(so);
         deleteAppointment(uid, cdao, writecon, so, ctx, inFolder, clientLastModified, true);
     }
 
     @Override
     public final void deleteAppointment(final int uid, final CalendarDataObject cdao, final Connection writecon, final Session so, final Context ctx, final int inFolder, final java.util.Date clientLastModified, final boolean checkPermissions) throws SQLException, OXException {
+        checkNotReadOnly(so);
         final Connection readcon = DBPool.pickup(ctx);
         final CalendarDataObject edao;
         PreparedStatement prep = null;
@@ -4925,6 +4933,7 @@ public class CalendarMySQL implements CalendarSqlImp {
 
     @Override
     public boolean deleteAppointmentsInFolder(final Session so, final Context ctx, final ResultSet rs, final Connection readcon, final Connection writecon, final int foldertype, final int fid) throws SQLException, OXException {
+        checkNotReadOnly(so);
         boolean modified = false;
 
         while (rs.next()) {
@@ -5899,6 +5908,21 @@ public class CalendarMySQL implements CalendarSqlImp {
 
     public static void setServiceLookup(ServiceLookup serviceLookup) {
         CalendarMySQL.SERVICES_REF.set(serviceLookup);
+    }
+
+    /**
+     * Checks that the calendar data in the context is not configured to be in an <i>read-only</i> mode or not.
+     * <p/>
+     * If so, an appropriate exception is thrown.
+     *
+     * @param session The session of the user trying to write calendar data
+     * @throws {@link OXCalendarExceptionCodes#CALENDAR_MAINTENANCE}
+     */
+    private void checkNotReadOnly(Session session) throws OXException {
+        UpdateStatus updateStatus = Updater.getInstance().getStatus(session.getContextId());
+        if (updateStatus.isExecutedSuccessfully("com.openexchange.chronos.storage.rdb.migration.ChronosStorageMigrationTask")) {
+            throw OXCalendarExceptionCodes.CALENDAR_MAINTENANCE.create();
+        }
     }
 
     private static final class PrgDatesMember implements Comparable<PrgDatesMember> {
