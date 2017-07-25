350b499c6d7a229de2a62a71b879542178f0881d
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2017-07-25 14:19:41 +0200

    inclusive start only if operating without duration

--- com.openexchange.chronos.recurrence/src/com/openexchange/chronos/recurrence/service/AbstractRecurrenceIterator.java
+++ com.openexchange.chronos.recurrence/src/com/openexchange/chronos/recurrence/service/AbstractRecurrenceIterator.java
@@ -145,7 +145,8 @@ public abstract class AbstractRecurrenceIterator<T> implements RecurrenceIterato
                     position++;
                     continue;
                 }
-                if (null != start && nextMillis + eventDuration >= start.getTimeInMillis() ||
+                if (null != start && nextMillis + eventDuration > start.getTimeInMillis() ||
+                    null != start && 0L == eventDuration && nextMillis == start.getTimeInMillis() ||
                     null != startPosition && position + 1 >= startPosition.intValue()) {
                     break;
                 } else {
