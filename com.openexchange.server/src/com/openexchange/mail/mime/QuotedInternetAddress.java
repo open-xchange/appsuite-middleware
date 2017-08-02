89369e25509dc0ac79edfdaff55fe12faf7710fc
Thorben Betten <thorben.betten@open-xchange.com>
2017-08-02 13:50:45 +0200

    Fix for bug 54879: Orderly handle quotes '"' in local part of an E-Mail address
    (cherry picked from commit c7f6faaa50014aa0a64cb3d8398fb3a9ca4b7b4a)

--- com.openexchange.server/src/com/openexchange/mail/mime/QuotedInternetAddress.java
+++ com.openexchange.server/src/com/openexchange/mail/mime/QuotedInternetAddress.java
@@ -1256,7 +1256,7 @@ public final class QuotedInternetAddress extends InternetAddress {
      * Is this a "simple" address? Simple addresses don't contain quotes or any RFC822 special characters other than '@' and '.'.
      */
     private boolean isSimple() {
-        return null == address || indexOfAny(address, SPECIALS_NO_DOT_NO_AT) < 0;
+        return null == address || indexOfAny(address, SPECIALS_NO_DOT_NO_AT_NO_QUOTE) < 0;
     }
 
     /**
@@ -1280,7 +1280,7 @@ public final class QuotedInternetAddress extends InternetAddress {
         }
     }
 
-    private static final String SPECIALS_NO_DOT_NO_AT = "()<>,;:\\\"[]";
+    private static final String SPECIALS_NO_DOT_NO_AT_NO_QUOTE = "()<>,;:\\[]";
 
     private static final String SPECIALS_NO_DOT = "()<>@,;:\\\"[]";
 
