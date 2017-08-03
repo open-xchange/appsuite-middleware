3b445deb7e0c866443e6f7fc4035f8f8d906c7ba
Thorben Betten <thorben.betten@open-xchange.com>
2017-08-03 12:00:20 +0200

    fixed compile error

--- openexchange-test/common/com/openexchange/groupware/Init.java
+++ openexchange-test/common/com/openexchange/groupware/Init.java
@@ -551,11 +551,7 @@ public final class Init {
         cascade.setProvider("server", new InMemoryConfigProvider());
         cascade.setProvider("context", new InMemoryConfigProvider());
         cascade.setProvider("user", new InMemoryConfigProvider());
-        try {
-            cascade.setSearchPath("user", "context", "server");
-        } catch (OXException e) {
-            // Cannot occur
-        }
+        cascade.setSearchPath("user", "context", "server");
         cascade.setStringParser(new BasicTypesStringParser());
         services.put(ConfigViewFactory.class, cascade);
         TestServiceRegistry.getInstance().addService(ConfigViewFactory.class, cascade);
