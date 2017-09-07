b15e3ee77d0e21944f4bc84bb7f3ed515acf1c01
Daniel Becker <daniel.becker@open-xchange.com>
2017-09-07 09:25:22 +0200

    MW-800 More detailed error messages in case of exceeded quota

--- com.openexchange.chronos/src/com/openexchange/chronos/exception/CalendarExceptionCodes.java
+++ com.openexchange.chronos/src/com/openexchange/chronos/exception/CalendarExceptionCodes.java
@@ -315,10 +315,10 @@ public enum CalendarExceptionCodes implements DisplayableOXExceptionCode {
     AUTH_FAILED("Authentication failed to access the resource at %1$s", AUTH_FAILED_MSG, Category.CATEGORY_ERROR, 4010),
     
     /**
-     * <li>The quota for context '%1$s' exceeded the limit</li>
-     * <li>There is no more quota available for new events in context %1$s.</li>
+     * <li>The quota for account '%1$s' in context '%2$s' exceeded the limit.</li>
+     * <li>There is no more quota available for account %1$s in context %2$s. Can't create new events..</li>
      */
-    INSUFFICIENT_QUOTA("There is no more quota available for new events in context %1$s.", QUOTA_EXCEEDED, Category.CATEGORY_ERROR, 5007),
+    INSUFFICIENT_QUOTA("There is no more quota available for account %1$s in context %2$s. Can't create new events.", QUOTA_EXCEEDED, Category.CATEGORY_ERROR, 5007),
 
     ;
 
