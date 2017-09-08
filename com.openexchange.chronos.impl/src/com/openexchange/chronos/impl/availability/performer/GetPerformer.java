e2518fbac27d03e7f8ea82d64d3f594b73705f72
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2017-09-08 09:51:14 +0200

    MW-801-StorageChange: Removed the obsolete combine method (combine and adjust of multiple availabilities)

--- com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/availability/performer/GetPerformer.java
+++ com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/availability/performer/GetPerformer.java
@@ -151,22 +151,8 @@ public class GetPerformer extends AbstractGetPerformer {
     public Availability getCombinedAvailableTime() throws OXException {
         int userId = getSession().getUserId();
         Availability availability = prepareForDelivery(getStorage().loadAvailable(userId));
-        combine(availability);
+        availability.setAvailable(combine(availability.getAvailable()));
         return availability;
-    }
-
-    /**
-     * Retrieves the combined {@link Availability} blocks in the specified interval for the current user
-     * 
-     * @param from The starting point of the interval
-     * @param until The ending point of the interval
-     * @return A {@link List} with the combined {@link Availability} blocks for the user in the specified interval
-     * @throws OXException
-     */
-    public List<Availability> getCombinedAvailableTimeInRange(Date from, Date until) throws OXException {
-        int userId = getSession().getUserId();
-        List<Availability> calendarAvailabilities = getStorage().loadCalendarAvailabilities(userId);
-        return combine(calendarAvailabilities);
     }
 
     /**
@@ -183,15 +169,10 @@ public class GetPerformer extends AbstractGetPerformer {
         Map<Attendee, Availability> availabilityPerAttendee = performForAttendees(attendees, from, until);
         for (Attendee attendee : attendees) {
             Availability availability = availabilityPerAttendee.get(attendee);
-            combine(availability);
+            availability.setAvailable(combine(availability.getAvailable()));
             availableTimes.put(attendee, availability);
         }
         return availableTimes;
-        //        Map<Attendee, List<Availability>> availabilitiesPerAttendee = performForAttendees(attendees, from, until);
-        //        for (Attendee attendee : attendees) {
-        //            List<Availability> calendarAvailabilities = availabilitiesPerAttendee.get(attendee);
-        //            availableTimes.put(attendee, combine(calendarAvailabilities));
-        //        }
     }
 
     /**
@@ -239,221 +220,11 @@ public class GetPerformer extends AbstractGetPerformer {
     }
 
     /**
-     * Combines the {@link Available} blocks from the specified {@link Availability}
-     * 
-     * @param availability The {@link Availability} for which to combine the blocks
-     */
-    private void combine(Availability availability) {
-        List<Available> available = availability.getAvailable();
-        List<Available> availableTime = new ArrayList<>(available.size());
-        Iterator<Available> iteratorA = available.iterator();
-        int index = 0;
-        // Keeps track of the removed objects
-        List<Available> removed = new ArrayList<>();
-        while (iteratorA.hasNext()) {
-            Available a = iteratorA.next();
-            if (removed.contains(a)) {
-                iteratorA.remove();
-                removed.remove(a);
-                continue;
-            }
-
-            List<Available> lookAheadList = available.subList(++index, available.size());
-            Iterator<Available> iteratorB = lookAheadList.iterator();
-            while (iteratorB.hasNext()) {
-                Available b = iteratorB.next();
-                // If it is completely contained then skip it
-                if (AvailabilityUtils.contained(b, a)) {
-                    removed.add(b);
-                    continue;
-                }
-                // If it in intersects, then merge
-                if (AvailabilityUtils.intersect(b, a)) {
-                    a = AvailabilityUtils.merge(b, a);
-                    removed.add(b);
-                }
-            }
-            availableTime.add(a);
-        }
-        
-        availability.setAvailable(availableTime);
-    }
-
-    /**
-     * Combines the specified {@link Availability} blocks and handles any intersects, overlaps and contains
-     * 
-     * @param calendarAvailabilities The {@link List} of the {@link Availability} blocks to combine
-     * @return The combined {@link Availability} blocks
-     * @deprecated Use {@link #combine(Availability)} instead.
-     */
-    private List<Availability> combine(List<Availability> calendarAvailabilities) {
-        // Sort by priority; higher priority will be on top
-        java.util.Collections.sort(calendarAvailabilities, Comparators.priorityComparator);
-
-        // The combined available time
-        List<Availability> availableTime = new ArrayList<>(calendarAvailabilities.size());
-        // Auxiliary list to store any splits
-        List<Availability> presAndPosts = new ArrayList<>();
-        int index = 0;
-        for (Iterator<Availability> iteratorA = calendarAvailabilities.iterator(); iteratorA.hasNext();) {
-            Availability availabilityA = iteratorA.next();
-            List<Availability> subList = calendarAvailabilities.subList(++index, calendarAvailabilities.size());
-            Iterator<Availability> iteratorB = subList.iterator();
-            while (iteratorB.hasNext()) {
-                Availability availabilityB = iteratorB.next();
-                // No intersection, carry on
-                if (!AvailabilityUtils.intersect(availabilityA, availabilityB)) {
-                    continue;
-                }
-
-                // Higher or equal priority, adjust times
-                if (AvailabilityUtils.contained(availabilityB, availabilityA)) {
-                    List<Available> flattenList = new ArrayList<>(availabilityB.getAvailable().size() + availabilityA.getAvailable().size());
-                    flattenList.addAll(availabilityB.getAvailable());
-                    flattenList.addAll(availabilityA.getAvailable());
-                    availabilityA.setAvailable(combineSlots(flattenList));
-                    iteratorB.remove();
-                } else if (AvailabilityUtils.contained(availabilityA, availabilityB)) {
-                    // Block A is entirely contained in block B, and since the list is sorted by priority
-                    // block A will always have higher or equal priority with block B, so we have to split B
-                    // Resulting in something similar to this:
-                    //          +---------------------------+
-                    //          |   Block A, Priority: 2    |                       +------------+---------------------------+-------------+
-                    //          +---------------------------+               ====>   | Block preB |         Block A           | Block postB |
-                    // +--------------------------------------------+               +------------+---------------------------+-------------+
-                    // |           Block B, Priority: 5             |               
-                    // +--------------------------------------------+
-                    List<Available> preIntersections = new ArrayList<>();
-                    List<Available> postIntersections = new ArrayList<>();
-                    List<Available> flattenList = new ArrayList<>(availabilityB.getAvailable().size() + availabilityA.getAvailable().size());
-
-                    Availability pre = availabilityB.clone();
-                    pre.setEndTime(availabilityA.getStartTime());
-                    // Find any slots that intersect with the new end time, adjust and split them 
-                    // if the availability blocks are of equal priority, then append the first split 
-                    // to the pre availability, and the last split to the preIntersections.
-                    List<Available> preSlots = new ArrayList<>();
-                    for (Iterator<Available> iterator = availabilityB.getAvailable().iterator(); iterator.hasNext();) {
-                        Available cfs = iterator.next();
-                        if (AvailabilityUtils.intersectsButNotContained(cfs.getStartTime(), cfs.getEndTime(), availabilityA.getStartTime(), availabilityA.getEndTime())) {
-                            if (availabilityA.compareTo(availabilityB) == 0) {
-                                // If the priorities are equal then we split the free slot
-                                Available preSplit = cfs.clone();
-                                preSplit.setEndTime(availabilityA.getStartTime());
-                                pre.getAvailable().add(preSplit);
-
-                                Available postSplit = cfs.clone();
-                                postSplit.setStartTime(availabilityB.getEndTime());
-                                preIntersections.add(postSplit);
-                            } else {
-                                // Otherwise the end time of the free slot will be shortened
-                                // to match the the start time of the availability block A
-                                cfs.setEndTime(availabilityA.getStartTime());
-                            }
-                        } else if (AvailabilityUtils.contained(cfs.getStartTime(), cfs.getEndTime(), pre.getStartTime(), pre.getEndTime())) {
-                            preSlots.add(cfs);
-                            iterator.remove();
-                        }
-                    }
-                    pre.setAvailable(preSlots);
-                    presAndPosts.add(pre);
-
-                    Availability post = availabilityB.clone();
-                    post.setStartTime(availabilityA.getEndTime());
-                    // Find any slots that intersect with the new start time, adjust and split them 
-                    // if the availability blocks are of equal priority,  then append the first split 
-                    // to the postIntersections, and the last split to the post availability.
-                    List<Available> postSlots = new ArrayList<>();
-                    for (Iterator<Available> iterator = availabilityB.getAvailable().iterator(); iterator.hasNext();) {
-                        Available cfs = iterator.next();
-                        if (AvailabilityUtils.intersectsButNotContained(cfs.getStartTime(), cfs.getEndTime(), availabilityA.getStartTime(), availabilityA.getEndTime())) {
-                            if (availabilityA.compareTo(availabilityB) == 0) {
-                                Available preSplit = cfs.clone();
-                                preSplit.setEndTime(availabilityB.getStartTime());
-                                postIntersections.add(preSplit);
-
-                                Available postSplit = cfs.clone();
-                                postSplit.setStartTime(availabilityA.getEndTime());
-                                post.getAvailable().add(postSplit);
-                            } else {
-                                cfs.setStartTime(availabilityA.getEndTime());
-                            }
-                        } else if (AvailabilityUtils.contained(cfs.getStartTime(), cfs.getEndTime(), post.getStartTime(), post.getEndTime())) {
-                            postSlots.add(cfs);
-                            iterator.remove();
-                        }
-                    }
-                    post.setAvailable(postSlots);
-                    presAndPosts.add(post);
-                    flattenList.addAll(availabilityA.getAvailable());
-                    flattenList.addAll(availabilityB.getAvailable());
-                    availabilityA.setAvailable(combineSlots(flattenList));
-                    // Add any splits from the intersections to the availability A
-                    availabilityA.getAvailable().addAll(preIntersections);
-                    availabilityA.getAvailable().addAll(postIntersections);
-
-                    // Remove the contained availability
-                    iteratorB.remove();
-                } else if (AvailabilityUtils.precedesAndIntersects(availabilityA, availabilityB)) {
-                    availabilityB.setStartTime(availabilityA.getEndTime());
-                    adjustSlots(availabilityA, availabilityB);
-                } else if (AvailabilityUtils.succeedsAndIntersects(availabilityA, availabilityB)) {
-                    availabilityB.setEndTime(availabilityA.getStartTime());
-                    adjustSlots(availabilityA, availabilityB);
-                }
-            }
-            availableTime.add(availabilityA);
-        }
-
-        // Add any splits
-        availableTime.addAll(presAndPosts);
-
-        // Sort by start date
-        java.util.Collections.sort(availableTime, Comparators.availabilityDateTimeComparator);
-        return availableTime;
-    }
-
-    /**
-     * Adjusts the {@link Available}s of the specified {@link Availability} blocks
-     * 
-     * @param a The {@link Availability} A
-     * @param b The {@link Availability} B
-     */
-    private void adjustSlots(Availability a, Availability b) {
-        List<Available> toAdd = new ArrayList<>();
-        for (Available freeSlotA : a.getAvailable()) {
-            for (Iterator<Available> iteratorB = b.getAvailable().iterator(); iteratorB.hasNext();) {
-                Available freeSlotB = iteratorB.next();
-                if (AvailabilityUtils.contained(freeSlotA, freeSlotB)) {
-                    // split
-                    Available pre = freeSlotB.clone();
-                    pre.setEndTime(freeSlotA.getStartTime());
-                    toAdd.add(pre);
-
-                    Available post = freeSlotB.clone();
-                    post.setStartTime(freeSlotA.getEndTime());
-                    toAdd.add(post);
-
-                    iteratorB.remove();
-                } else if (AvailabilityUtils.contained(freeSlotB, freeSlotA)) {
-                    iteratorB.remove();
-                } else if (AvailabilityUtils.precedesAndIntersects(freeSlotA, freeSlotB)) {
-                    freeSlotB.setStartTime(freeSlotA.getEndTime());
-                } else if (AvailabilityUtils.succeedsAndIntersects(freeSlotA, freeSlotB)) {
-                    freeSlotB.setEndTime(freeSlotA.getStartTime());
-                }
-            }
-            // Add any splits
-            b.getAvailable().addAll(toAdd);
-        }
-    }
-
-    /**
      * Combines the specified {@link Available}s
      * 
      * @param freeSlots The {@link Available}s to combine
      */
-    private List<Available> combineSlots(List<Available> freeSlots) {
+    private List<Available> combine(List<Available> freeSlots) {
         // Sort by starting date
         java.util.Collections.sort(freeSlots, Comparators.availableDateTimeComparator);
 
