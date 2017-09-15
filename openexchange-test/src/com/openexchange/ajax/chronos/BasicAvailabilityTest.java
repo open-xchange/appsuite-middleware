06edaddd6920deac65455ddf4b87131dcaaab8ca
Ioannis Chouklis <ioannis.chouklis@open-xchange.com.com>
2017-09-15 14:05:11 +0200

    MW-801: added more test cases for the availability http api

--- openexchange-test/src/com/openexchange/ajax/chronos/BasicAvailabilityTest.java
+++ openexchange-test/src/com/openexchange/ajax/chronos/BasicAvailabilityTest.java
@@ -49,6 +49,11 @@
 
 package com.openexchange.ajax.chronos;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -73,15 +78,130 @@ public class BasicAvailabilityTest extends AbstractChronosTest {
     @Test
     public void testSetSingleAvailableBlock() throws Exception {
         AvailabilityData availabilityData = new AvailabilityData();
-        Available available = new Available();
-        available.setStart(getDateTime(System.currentTimeMillis()));
-        available.setEnd(getDateTime(System.currentTimeMillis() + 7200));
-        availabilityData.addAvailableTimesItem(available);
+        Available expected = createAvailable(System.currentTimeMillis(), System.currentTimeMillis() + 7200);
+        availabilityData.addAvailableTimesItem(expected);
 
         chronosApi = new ChronosApi(defaultUserApi.getClient());
+
+        // Set the availability
         chronosApi.setAvailability(defaultUserApi.getSession(), availabilityData);
 
+        // Get the availability
         GetAvailabilityResponse availability = chronosApi.getAvailability(defaultUserApi.getSession());
-        System.out.println(availability.getData());
+        List<Available> availableBlocks = availability.getData();
+
+        // Assert
+        assertNotNull("The response payload is null", availableBlocks);
+        assertFalse("The availables array is empty", availableBlocks.isEmpty());
+        assertTrue("The are more than one available blocks", availableBlocks.size() == 1);
+        assertAvailable(expected, availableBlocks.get(0));
+    }
+
+    /**
+     * Tests setting a single available block with recurrence rule
+     */
+    @Test
+    public void testSetSingleAvailableBlockWithRecurrenceRule() throws Exception {
+        AvailabilityData availabilityData = new AvailabilityData();
+        Available expected = createAvailable(System.currentTimeMillis(), System.currentTimeMillis() + 7200, "FREQ=WEEKLY;BYDAY=TH");
+        availabilityData.addAvailableTimesItem(expected);
+
+        chronosApi = new ChronosApi(defaultUserApi.getClient());
+
+        // Set the availability
+        chronosApi.setAvailability(defaultUserApi.getSession(), availabilityData);
+
+        // Get the availability
+        GetAvailabilityResponse availability = chronosApi.getAvailability(defaultUserApi.getSession());
+        List<Available> availableBlocks = availability.getData();
+
+        // Assert
+        assertNotNull("The response payload is null", availableBlocks);
+        assertFalse("The availables array is empty", availableBlocks.isEmpty());
+        assertTrue("The are more than one available blocks", availableBlocks.size() == 1);
+        assertAvailable(expected, availableBlocks.get(0));
+    }
+
+    /**
+     * Tests setting multiple available blocks with recurrence rule
+     */
+    @Test
+    public void testSetMultipleAvailableBlocks() throws Exception {
+        AvailabilityData availabilityData = new AvailabilityData();
+        availabilityData.addAvailableTimesItem(createAvailable(System.currentTimeMillis() + 36000, System.currentTimeMillis() + 36000 + 7200, "FREQ=WEEKLY;BYDAY=MO"));
+        availabilityData.addAvailableTimesItem(createAvailable(System.currentTimeMillis(), System.currentTimeMillis() + 7200, "FREQ=WEEKLY;BYDAY=TH"));
+
+        chronosApi = new ChronosApi(defaultUserApi.getClient());
+
+        // Set the availability
+        chronosApi.setAvailability(defaultUserApi.getSession(), availabilityData);
+
+        // Get the availability
+        GetAvailabilityResponse availability = chronosApi.getAvailability(defaultUserApi.getSession());
+        List<Available> availableBlocks = availability.getData();
+
+        // Assert
+        assertNotNull("The response payload is null", availableBlocks);
+        assertFalse("The availables array is empty", availableBlocks.isEmpty());
+        assertAvailable(availabilityData.getAvailableTimes(), availableBlocks);
+    }
+
+    /**
+     * Creates an {@link Available} block within the specified range
+     * 
+     * @param from The starting point in time
+     * @param until The ending point in time
+     * @return The {@link Available} block
+     */
+    private Available createAvailable(long from, long until) {
+        Available available = new Available();
+        available.setStart(getDateTime(from));
+        available.setEnd(getDateTime(until));
+        available.setUser(defaultUserApi.getCalUser());
+        return available;
+    }
+
+    /**
+     * Creates an {@link Available} block within the specified range and the specified
+     * recurrence rule
+     * 
+     * @param from The starting point in time
+     * @param until The ending point in time
+     * @param rrule The recurrence rule
+     * @return The {@link Available} block
+     * @return The {@link Available} block
+     */
+    private Available createAvailable(long from, long until, String rrule) {
+        Available available = createAvailable(from, until);
+        available.setRrule(rrule);
+        return available;
+    }
+
+    /**
+     * Asserts that the specified expected {@link List} of {@link Available} blocks
+     * is equal to the specified actual {@link List} of {@link Available} blocks
+     * 
+     * @param expected The expected {@link List}
+     * @param actual The actual {@link List}
+     */
+    private void assertAvailable(List<Available> expected, List<Available> actual) {
+        assertEquals("The amount of available blocks does not match", expected.size(), actual.size());
+        for (int index = 0; index < expected.size(); index++) {
+            assertAvailable(expected.get(index), actual.get(index));
+
+        }
+    }
+
+    /**
+     * Asserts that the expected {@link Available} is equal the actual {@link Available}
+     * 
+     * @param expected The expected {@link Available}
+     * @param actual The actual {@link Available}
+     */
+    private void assertAvailable(Available expected, Available actual) {
+        assertEquals("The recurrence rule does not match", expected.getRrule(), actual.getRrule());
+        assertEquals("The start time does not match", expected.getStart(), actual.getStart());
+        assertEquals("The start time does not match", expected.getEnd(), actual.getEnd());
+        assertEquals("The user id does not match", expected.getUser(), actual.getUser());
     }
 }
