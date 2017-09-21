9874907ab1086d059bd02c78ede38048223c3160
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2017-09-21 09:43:46 +0200

    removed debug output

--- com.openexchange.dav/src/com/openexchange/dav/DAVServlet.java
+++ com.openexchange.dav/src/com/openexchange/dav/DAVServlet.java
@@ -107,9 +107,6 @@ public class DAVServlet extends OXServlet {
         DAVUserAgent davUserAgent = DAVUserAgent.parse(userAgent);
         LoginRequest loginRequest = new LoginRequestImpl(
             request, credentials.getLogin(), credentials.getPassword(), getInterface(davUserAgent), davUserAgent.getReadableName(), null, userAgent);
-
-        System.out.println("### " + davUserAgent.getReadableName());
-
         return ALLOW_ASTERISK_LOGIN_CUSTOMIZER.modifyLogin(loginRequest);
     }
 
