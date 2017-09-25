f1df27b8c493dd05191b21966004c74129ed07e3
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2017-09-25 17:01:27 +0200

    copy extended properties as-is

--- com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/performer/CreatePerformer.java
+++ com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/performer/CreatePerformer.java
@@ -204,7 +204,7 @@ public class CreatePerformer extends AbstractUpdatePerformer {
         /*
          * copy over further (unchecked) event fields
          */
-        return EventMapper.getInstance().copy(eventData, event, EventField.SUMMARY, EventField.LOCATION, EventField.DESCRIPTION, EventField.CATEGORIES, EventField.FILENAME, EventField.URL);
+        return EventMapper.getInstance().copy(eventData, event, EventField.SUMMARY, EventField.LOCATION, EventField.DESCRIPTION, EventField.CATEGORIES, EventField.FILENAME, EventField.URL, EventField.EXTENDED_PROPERTIES);
     }
 
 }
