1cf6a88a43c8dd30052fd176150ae5d9c9c9a247
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2017-09-27 11:27:10 +0200

    use the correct method for recurrence id

--- openexchange-test/src/com/openexchange/ajax/chronos/BasicAlarmTriggerTest.java
+++ openexchange-test/src/com/openexchange/ajax/chronos/BasicAlarmTriggerTest.java
@@ -229,7 +229,7 @@ public class BasicAlarmTriggerTest extends AbstractUserTimezoneAlarmTriggerTest 
         /*
          * 2. create an exception for the event recurrence of the next trigger by shifting the start time by one hour
          */
-        CalendarResult updateResult = shiftEvent(event.getId(), alarmTrigger.getRecurrence(), event, eventTime, TimeUnit.HOURS, 1, getLastTimestamp());
+        CalendarResult updateResult = shiftEvent(event.getId(), alarmTrigger.getRecurrenceId(), event, eventTime, TimeUnit.HOURS, 1, getLastTimestamp());
         assertNotNull(updateResult.getCreated());
         assertEquals(1, updateResult.getCreated().size());
         EventData exceptionEvent = updateResult.getCreated().get(0);
@@ -352,7 +352,7 @@ public class BasicAlarmTriggerTest extends AbstractUserTimezoneAlarmTriggerTest 
         /*
          * 3. create an exception for the event recurrence of the next trigger by shifting the start time by one hour
          */
-        CalendarResult updateResult = shiftEvent(event.getId(), alarmTrigger.getRecurrence(), event, eventTime, TimeUnit.HOURS, 1, getLastTimestamp());
+        CalendarResult updateResult = shiftEvent(event.getId(), alarmTrigger.getRecurrenceId(), event, eventTime, TimeUnit.HOURS, 1, getLastTimestamp());
         assertNotNull(updateResult.getCreated());
         assertEquals(1, updateResult.getCreated().size());
         EventData exceptionEvent = updateResult.getCreated().get(0);
