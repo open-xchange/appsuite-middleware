6d510cc433bb8ae28bbd5898cd1e3a53474e7e8d
Benjamin Grüdelbach <benjamin.gruedelbach@open-xchange.com>
2017-09-29 10:29:15 +0200

    PGP: Small test data for tests for unit tests

--- com.openexchange.pgp.core/test/com/openexchange/pgp/core/AbstractPGPTest.java
+++ com.openexchange.pgp.core/test/com/openexchange/pgp/core/AbstractPGPTest.java
@@ -55,7 +55,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.Security;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
@@ -271,10 +270,7 @@ public class AbstractPGPTest {
      * @return A bunch of test data
      */
     public byte[] generateTestData() {
-        //using some larger test data since this caused failing in the past due error prone stream handling
-        byte[] data = new byte[1024*1000 /* 1 MB */];
-        Arrays.fill(data, (byte)117);
-        return data;
+        return "test".getBytes();
     }
 
     /**
