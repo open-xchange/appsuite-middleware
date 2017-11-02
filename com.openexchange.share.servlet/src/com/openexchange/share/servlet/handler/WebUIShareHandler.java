e9d902989e2085d97089093686000b30a98d1806
Thorben Betten <thorben.betten@open-xchange.com>
2017-11-02 09:26:27 +0100

    Fix for bug 55928: Use '<user-id>@<context-id>' as "login_name"
    (cherry picked from commit 56fc57f40700cdeda8061338d4fd82d20e62a591)

--- com.openexchange.share.servlet/src/com/openexchange/share/servlet/handler/WebUIShareHandler.java
+++ com.openexchange.share.servlet/src/com/openexchange/share/servlet/handler/WebUIShareHandler.java
@@ -145,7 +145,7 @@ public class WebUIShareHandler extends AbstractShareHandler {
                         .share(guestInfo.getBaseToken())
                         .target(targetPath)
                         .message(MessageType.INFO, translator.translate(ShareServletStrings.NO_ACCESS_TO_SHARE_CONTACT_OWNER_CONTINUE))
-                        .loginName(guestInfo.getEmailAddress());
+                        .loginName(guestInfo.getGuestID(), guestInfo.getContextID());
                 } else {
                     location = new LoginLocation()
                         .share(guestInfo.getBaseToken())
@@ -153,7 +153,7 @@ public class WebUIShareHandler extends AbstractShareHandler {
                         .target(targetPath)
                         .message(MessageType.INFO, translator.translate(ShareServletStrings.NO_ACCESS_TO_SHARE_CONTACT_OWNER_LOG_IN));
                     if (guestInfo.getAuthentication() == AuthenticationMode.GUEST_PASSWORD) {
-                        location.loginName(guestInfo.getEmailAddress());
+                        location.loginName(guestInfo.getGuestID(), guestInfo.getContextID());
                     }
                 }
 
@@ -171,7 +171,7 @@ public class WebUIShareHandler extends AbstractShareHandler {
                 .loginType(guestInfo.getAuthentication())
                 .message(MessageType.INFO, message);
             if (guestInfo.getAuthentication() == AuthenticationMode.GUEST_PASSWORD) {
-                location.loginName(guestInfo.getEmailAddress());
+                location.loginName(guestInfo.getGuestID(), guestInfo.getContextID());
             }
             if (targetPath != null) {
                 location.target(targetPath);
