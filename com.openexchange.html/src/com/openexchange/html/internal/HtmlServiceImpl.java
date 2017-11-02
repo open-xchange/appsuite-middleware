643c86ea6919820b97bd34354594ce25a4050ca8
Thorben Betten <thorben.betten@open-xchange.com>
2017-11-02 14:08:53 +0100

    Fix for bug 55972: Drop HTML comments prior to processing

--- com.openexchange.html/src/com/openexchange/html/internal/HtmlServiceImpl.java
+++ com.openexchange.html/src/com/openexchange/html/internal/HtmlServiceImpl.java
@@ -68,6 +68,7 @@ import java.text.Normalizer;
 import java.text.Normalizer.Form;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
@@ -103,7 +104,10 @@ import org.htmlcleaner.Serializer;
 import org.htmlcleaner.SimpleHtmlSerializer;
 import org.htmlcleaner.TagNode;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Comment;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.NodeVisitor;
 import org.owasp.esapi.codecs.HTMLEntityCodec;
 import com.openexchange.config.ConfigurationService;
 import com.openexchange.exception.OXException;
@@ -568,6 +572,8 @@ public final class HtmlServiceImpl implements HtmlService {
                         html = sb.toString();
                     }
                 }
+                
+                html = removeComments(html);
 
                 // Perform one-shot sanitizing
                 html = replacePercentTags(html);
@@ -699,6 +705,29 @@ public final class HtmlServiceImpl implements HtmlService {
             LOG.warn("HTML content will be returned un-sanitized.", e);
             return htmlSanitizeResult;
         }
+    }
+
+    private static String removeComments(String html) {
+        Document document = Jsoup.parse(html);
+        final Set<Node> removedNodes = new HashSet<>(16, 0.9F);
+        document.traverse(new NodeVisitor() {
+            
+            @Override
+            public void tail(Node node, int depth) {                
+                // Ignore
+            }
+            
+            @Override
+            public void head(Node node, int depth) {
+                if (node instanceof Comment) {
+                    removedNodes.add(node);
+                }
+            }
+        });
+        for (Node node : removedNodes) {
+            node.remove();
+        }
+        return document.outerHtml();
     }
 
     private FilterJerichoHandler getHandlerFor(int initialCapacity, String optionalConfigName) {
