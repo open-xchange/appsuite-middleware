087af9d01015935bdc42e771c5772b8da9e75acf
Thorben Betten <thorben.betten@open-xchange.com>
2017-11-09 10:41:20 +0100

    Fix for bug 56056: Only allow to fetch or set attributes for another user if session-associated user is the context administrator
    (cherry picked from commit 32fd170c8432a635ccdd6408703b8372b0059d6b)

--- com.openexchange.user.json/src/com/openexchange/user/json/actions/GetAttributeAction.java
+++ com.openexchange.user.json/src/com/openexchange/user/json/actions/GetAttributeAction.java
@@ -58,6 +58,7 @@ import com.openexchange.documentation.RequestMethod;
 import com.openexchange.documentation.annotations.Action;
 import com.openexchange.documentation.annotations.Parameter;
 import com.openexchange.exception.OXException;
+import com.openexchange.groupware.ldap.UserExceptionCode;
 import com.openexchange.server.ServiceLookup;
 import com.openexchange.tools.servlet.AjaxExceptionCodes;
 import com.openexchange.tools.session.ServerSession;
@@ -94,6 +95,12 @@ public final class GetAttributeAction extends AbstractUserAction {
              * Parse parameters
              */
             final int id = checkIntParameter(AJAXServlet.PARAMETER_ID, request);
+            /*
+             * Only allow for session-associated user
+             */
+            if (id != session.getUserId() && id != session.getContext().getMailadmin()) {
+                throw UserExceptionCode.PERMISSION_ACCESS.create(Integer.valueOf(session.getContextId()));
+            }
             final String name = checkStringParameter("name", request);
             /*
              * Get user service
