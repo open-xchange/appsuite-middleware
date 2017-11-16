aac7f9b329b1c77cd6c8653663f1d44cf240a1d0
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2017-11-16 13:37:44 +0100

    MW-879: Introduced the DKIMResult

---
+++ com.openexchange.mail.authentication/src/com/openexchange/mail/authentication/DKIMResult.java
@@ -0,0 +1,99 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2017-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.mail.authentication;
+
+/**
+ * {@link DKIMResult} - The evaluation states of the DKIM signature
+ *
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ * @see <a href="https://tools.ietf.org/html/rfc6376#section-3.9">RFC 6376, Section 3.9</a>
+ */
+public enum DKIMResult {
+
+    /**
+     * A successful verification of the signature
+     */
+    SUCCESS("Success"),
+    /**
+     * A permanent, non-recoverable error such as a signature verification failure
+     */
+    PERMFAIL("Permanent Failure"),
+    /**
+     * A temporary, recoverable error such as a DNS query timeout
+     */
+    TEMPFAIL("Temporary Failure");
+
+    private final String displayName;
+
+    /**
+     * Initialises a new {@link DKIMResult}.
+     */
+    private DKIMResult(String displayName) {
+        this.displayName = displayName;
+    }
+
+    /**
+     * Gets the displayName
+     *
+     * @return The displayName
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    /**
+     * Returns the technical name of the DKIM result
+     * 
+     * @return the technical name of the DKIM result
+     */
+    public String getTechnicalName() {
+        return name().toLowerCase();
+    }
+}
