edcb1bf55cfa930486a73e891f40077480b85593
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2017-11-27 10:28:51 +0100

    minor formatting

--- com.openexchange.mail.authenticity.impl/src/com/openexchange/mail/authenticity/impl/TrustedDomainAuthenticityHandler.java
+++ com.openexchange.mail.authenticity.impl/src/com/openexchange/mail/authenticity/impl/TrustedDomainAuthenticityHandler.java
@@ -53,7 +53,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import org.slf4j.Logger;
