a7c353d9d4a14778fe4ab4b555f794bf8ae758ae
Thorben Betten <thorben.betten@open-xchange.com>
2017-11-30 11:37:14 +0100

    Fix for bug 56334: Deny redirects to network-internal end-points from remote locations
    (cherry picked from commit 1656f589e61c094f493579fb3841a9a6b367e0f6)

--- com.openexchange.rest.client/src/com/openexchange/rest/client/httpclient/HttpClients.java
+++ com.openexchange.rest.client/src/com/openexchange/rest/client/httpclient/HttpClients.java
@@ -51,6 +51,9 @@ package com.openexchange.rest.client.httpclient;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPInputStream;
 import org.apache.http.ConnectionReuseStrategy;
@@ -61,6 +64,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.ProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -82,6 +86,7 @@ import org.apache.http.cookie.SetCookie;
 import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.impl.DefaultConnectionReuseStrategy;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.impl.cookie.BasicExpiresHandler;
 import org.apache.http.impl.cookie.BrowserCompatSpec;
@@ -95,6 +100,7 @@ import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.util.TextUtils;
+import com.openexchange.java.InetAddresses;
 import com.openexchange.net.ssl.SSLSocketFactoryProvider;
 import com.openexchange.net.ssl.config.SSLConfigurationService;
 import com.openexchange.rest.client.httpclient.internal.WrappedClientsRegistry;
@@ -252,6 +258,10 @@ public final class HttpClients {
         });
         HttpClientParams.setCookiePolicy(c.getParams(), "lenient");
 
+        if (config.denyLocalRedirect) {
+            c.setRedirectStrategy(DenyLocalRedirectStrategy.DENY_LOCAL_INSTANCE);
+        }
+
         c.addResponseInterceptor(new HttpResponseInterceptor() {
 
             @Override
@@ -303,8 +313,11 @@ public final class HttpClients {
 
         private String userAgent;
 
+        private boolean denyLocalRedirect;
+
         ClientConfig() {
             super();
+            denyLocalRedirect = false;
         }
 
         /**
@@ -404,6 +417,24 @@ public final class HttpClients {
          */
         public ClientConfig setUserAgent(String userAgent) {
             this.userAgent = userAgent;
+            return this;
+        }
+
+        /**
+         * Sets whether to deny redirect attempts to a local address.
+         * <p>
+         * If set to <code>true</code> the host of every redirect URL is checked if its IP address is resolvable to a network-internal address by checking:
+         * <pre>
+         *    InetAddress inetAddress = InetAddress.getByName(extractedHost);
+         *    if (inetAddress.isAnyLocalAddress() || inetAddress.isSiteLocalAddress() || inetAddress.isLoopbackAddress() || inetAddress.isLinkLocalAddress()) {
+         *       // Deny!
+         *    }
+         * </pre>
+         *
+         * @param denyLocalRedirect <code>true</code> to deny redirect attempts to a local address; otherwise <code>false</code>
+         */
+        public ClientConfig setDenyLocalRedirect(boolean denyLocalRedirect) {
+            this.denyLocalRedirect = denyLocalRedirect;
             return this;
         }
 
@@ -700,6 +731,29 @@ public final class HttpClients {
                     }
                 }
             });
+        }
+    }
+
+    private static class DenyLocalRedirectStrategy extends DefaultRedirectStrategy {
+
+        static final DenyLocalRedirectStrategy DENY_LOCAL_INSTANCE = new DenyLocalRedirectStrategy();
+
+        private DenyLocalRedirectStrategy() {
+            super();
+        }
+
+        @Override
+        protected URI createLocationURI(String location) throws ProtocolException {
+            try {
+                URI locationURI = super.createLocationURI(location);
+                InetAddress inetAddress = InetAddress.getByName(locationURI.getHost());
+                if (InetAddresses.isInternalAddress(inetAddress)) {
+                    throw new ProtocolException("Invalid redirect URI: " + location + ". No redirect to local address allowed.");
+                }
+                return locationURI;
+            } catch (UnknownHostException e) {
+                throw new ProtocolException("Invalid redirect URI: " + location + ". Unknown host.", e);
+            }
         }
     }
 
