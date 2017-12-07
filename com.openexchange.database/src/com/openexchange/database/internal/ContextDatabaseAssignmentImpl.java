679dac065ae80ede999c271e8ea2648409f19eca
Thorben Betten <thorben.betten@open-xchange.com>
2017-12-07 21:18:35 +0100

    Change for bug 56419: Reduced lock scope when exclusively loading a database assignment from ConfigDB
    (cherry picked from commit 3a4a1f9733a5c8a29840658b4c56a1eeb8c0c750)

--- com.openexchange.database/src/com/openexchange/database/internal/ContextDatabaseAssignmentImpl.java
+++ com.openexchange.database/src/com/openexchange/database/internal/ContextDatabaseAssignmentImpl.java
@@ -64,7 +64,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -76,6 +75,8 @@ import com.openexchange.database.AssignmentInsertData;
 import com.openexchange.database.ConfigDatabaseService;
 import com.openexchange.database.DBPoolingExceptionCodes;
 import com.openexchange.exception.OXException;
+import com.openexchange.lock.AccessControl;
+import com.openexchange.lock.LockService;
 
 /**
  * Reads assignments from the database, maybe stores them in a cache for faster access.
@@ -94,18 +95,15 @@ public final class ContextDatabaseAssignmentImpl implements ContextDatabaseAssig
     private static final String CONTEXTS_IN_DATABASE = "SELECT cid FROM context_server2db_pool WHERE read_db_pool_id=? OR write_db_pool_id=?";
     private static final String NOTFILLED = "SELECT db_schema,COUNT(db_schema) AS count FROM context_server2db_pool WHERE write_db_pool_id=? GROUP BY db_schema HAVING count<? ORDER BY count ASC";
 
-    private final ConfigDatabaseService configDatabaseService;
-
     private static final String CACHE_NAME = "OXDBPoolCache";
 
+    private static class CacheLockHolder {
+        static final CacheLock fallbackGlobalCacheLock = CacheLock.cacheLockFor(new ReentrantLock(true));
+    }
+
+    private final ConfigDatabaseService configDatabaseService;
     private volatile CacheService cacheService;
-
     private volatile Cache cache;
-
-    /**
-     * Lock for the cache.
-     */
-    private final Lock cacheLock = new ReentrantLock(true);
 
     /**
      * Default constructor.
@@ -113,6 +111,16 @@ public final class ContextDatabaseAssignmentImpl implements ContextDatabaseAssig
     public ContextDatabaseAssignmentImpl(final ConfigDatabaseService configDatabaseService) {
         super();
         this.configDatabaseService = configDatabaseService;
+    }
+
+    private CacheLock cacheLockFor(int contextId) throws OXException {
+        LockService lockService = Initialization.getLockService();
+        if (null == lockService) {
+            return CacheLockHolder.fallbackGlobalCacheLock;
+        }
+
+        AccessControl accessControl = lockService.getAccessControlFor(new StringBuilder(16).append(contextId).append('-').append("dbassign").toString(), 1, 1, contextId);
+        return CacheLock.cacheLockFor(accessControl);
     }
 
     @Override
@@ -134,18 +142,22 @@ public final class ContextDatabaseAssignmentImpl implements ContextDatabaseAssig
         }
 
         // Need to load - synchronously!
+        CacheLock cacheLock = cacheLockFor(contextId);
         cacheLock.lock();
         try {
-            AssignmentImpl retval = (AssignmentImpl) myCache.get(key);
-            if (null == retval) {
-                retval = loadAssignment(contextId);
-                try {
-                    myCache.putSafe(key, retval);
-                } catch (OXException e) {
-                    LOG.error("Cannot put database assignment into cache.", e);
-                }
+            object = myCache.get(key);
+            if (object instanceof AssignmentImpl) {
+                // Loaded meanwhile
+                return (AssignmentImpl) object;
             }
-            return retval;
+
+            AssignmentImpl loaded = loadAssignment(contextId);
+            try {
+                myCache.putSafe(key, loaded);
+            } catch (OXException e) {
+                LOG.error("Cannot put database assignment into cache.", e);
+            }
+            return loaded;
         } finally {
             cacheLock.unlock();
         }
@@ -216,6 +228,7 @@ public final class ContextDatabaseAssignmentImpl implements ContextDatabaseAssig
         Cache myCache = this.cache;
         if (null != myCache) {
             final CacheKey key = myCache.newCacheKey(assign.getContextId(), assign.getServerId());
+            CacheLock cacheLock = cacheLockFor(assign.getContextId());
             cacheLock.lock();
             try {
                 if (update) {
