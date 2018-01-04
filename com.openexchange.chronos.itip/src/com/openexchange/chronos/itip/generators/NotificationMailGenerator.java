9a352e1b99b3f2935c2ec31c4fb71694a0a18f39
Daniel Becker <daniel.becker@open-xchange.com>
2018-01-04 08:19:12 +0100

    Reverted parts of refactoring
    See commit dd129a83ee859455f90868931b7ddb1ce43fc68a

--- com.openexchange.chronos.itip/src/com/openexchange/chronos/itip/generators/NotificationMailGenerator.java
+++ com.openexchange.chronos.itip/src/com/openexchange/chronos/itip/generators/NotificationMailGenerator.java
@@ -316,6 +316,7 @@ public class NotificationMailGenerator implements ITipMailGenerator {
 
         final ITipMessage message = new ITipMessage();
         message.setMethod(ITipMethod.REQUEST);
+        message.setEvent(appointmentToReport);
 
         if (CalendarUtils.isSeriesMaster(appointmentToReport)) {
             final List<Event> exceptions = util.getExceptions(appointmentToReport, session);
