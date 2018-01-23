5540ea725724ace6fc16dfeab81c7caa877b25c1
Thorben Betten <thorben.betten@open-xchange.com>
2018-01-23 10:42:18 +0100

    Follow-up for bug 56619: Also check black-list in case a redirect is performed when querying an RSS feed
    (cherry picked from commit 8603b23d4e5625a62b899cb2b626c84ced07202f)

--- com.openexchange.rss/src/com/openexchange/rss/util/TimoutHttpURLFeedFetcher.java
+++ com.openexchange.rss/src/com/openexchange/rss/util/TimoutHttpURLFeedFetcher.java
@@ -68,6 +68,7 @@ import com.openexchange.config.ConfigurationService;
 import com.openexchange.config.Reloadable;
 import com.openexchange.java.InetAddresses;
 import com.openexchange.java.Streams;
+import com.openexchange.java.Strings;
 import com.openexchange.rss.osgi.Services;
 import com.openexchange.tools.stream.CountingInputStream;
 import com.sun.syndication.feed.synd.SyndFeed;
@@ -142,10 +143,23 @@ public class TimoutHttpURLFeedFetcher extends AbstractFeedFetcher implements Rel
             return null;
         }
 
+        // Get redirect location & disconnect current URL connection instance
         String redirectUrl = httpConnection.getHeaderField("Location");
         httpConnection.disconnect();
+
+        // Validate redirect location
+        if (Strings.isEmpty(redirectUrl)) {
+            // Missing "Location" header
+            throw new FetcherException("Missing redirect URL");
+        }
+
+        // Examine redirect location
         try {
             URL feedUrl = new URL(redirectUrl);
+            if (RssProperties.isDenied(feedUrl.getProtocol(), feedUrl.getHost(), feedUrl.getPort())) {
+                // Deny redirecting to a local address
+                throw new FetcherException(feedUrl.toExternalForm() + " is not an allowed redirect URL");
+            }
             if (originalAddressIsRemote) {
                 try {
                     InetAddress inetAddress = InetAddress.getByName(feedUrl.getHost());
@@ -207,10 +221,8 @@ public class TimoutHttpURLFeedFetcher extends AbstractFeedFetcher implements Rel
                 httpConnection.setReadTimeout(readTimout);
             }
 
-            if (originalAddressIsRemote) {
-                // Deny to automatically follow redirects in case original address is remote
-                httpConnection.setInstanceFollowRedirects(false);
-            }
+            // Deny to automatically follow redirects
+            httpConnection.setInstanceFollowRedirects(false);
 
             FeedFetcherCache cache = getFeedInfoCache();
             if (cache == null) {
