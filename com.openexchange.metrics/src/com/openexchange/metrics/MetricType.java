8d2e259747c07e96bbfe25323efb86363c6454f6
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-02-08 13:12:42 +0100

    MW-765: replaced invalid char

--- com.openexchange.metrics/src/com/openexchange/metrics/MetricType.java
+++ com.openexchange.metrics/src/com/openexchange/metrics/MetricType.java
@@ -67,10 +67,10 @@ public enum MetricType {
      * <p>A {@link Meter} measures the rate at which a set of events occur.</p>
      * 
      * <p>Meters measure the rate of the events in a few different ways.
-     * The mean rate is the average rate of events. It’s generally useful
-     * for trivia, but as it represents the total rate for your application’s
+     * The mean rate is the average rate of events. It's generally useful
+     * for trivia, but as it represents the total rate for your application's
      * entire lifetime (e.g., the total number of requests handled, divided
-     * by the number of seconds the process has been running), it doesn’t
+     * by the number of seconds the process has been running), it doesn't
      * offer a sense of recency. Luckily, meters also record three different
      * exponentially-weighted moving average rates: the 1-, 5-, and 15-minute
      * moving averages.</p>
@@ -95,7 +95,7 @@ public enum MetricType {
      * a meter of the rate of its occurrence.</p>
      * <p><b>Note:</b>
      * Elapsed times for it events are measured internally in nanoseconds,
-     * using Java’s high-precision System.nanoTime() method. Its precision and
+     * using Java's high-precision System.nanoTime() method. Its precision and
      * accuracy vary depending on operating system and hardware.
      * </p>
      */
