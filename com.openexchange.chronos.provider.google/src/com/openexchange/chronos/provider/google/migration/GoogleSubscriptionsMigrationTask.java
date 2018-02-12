d6b392fb3239711c3e2315ad8b5c0ba6702e461c
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2018-02-12 10:31:46 +0100

    Fixed typo

--- com.openexchange.chronos.provider.google/src/com/openexchange/chronos/provider/google/migration/GoogleSubscriptionsMigrationTask.java
+++ com.openexchange.chronos.provider.google/src/com/openexchange/chronos/provider/google/migration/GoogleSubscriptionsMigrationTask.java
@@ -211,7 +211,7 @@ public class GoogleSubscriptionsMigrationTask extends UpdateTaskAdapter {
 
     @Override
     public String[] getDependencies() {
-        return new String[] { "com.openexchange.chronos.storage.rdb.groupware.ChronosCreateTableTask", "com.openexchange.groupware.update.tasks.AddSharedParentFolderToFolderPermissionTableUpdateTask", "com.openexchange.groupware.update.tasks.AddTypeToFolderPermissionTableUpdateTask, com.openexchange.groupware.update.tasks.AddOriginColumnToInfostoreDocumentTables" };
+        return new String[] { "com.openexchange.chronos.storage.rdb.groupware.ChronosCreateTableTask", "com.openexchange.groupware.update.tasks.AddSharedParentFolderToFolderPermissionTableUpdateTask", "com.openexchange.groupware.update.tasks.AddTypeToFolderPermissionTableUpdateTask","com.openexchange.groupware.update.tasks.AddOriginColumnToInfostoreDocumentTables" };
     }
 
     @Override
