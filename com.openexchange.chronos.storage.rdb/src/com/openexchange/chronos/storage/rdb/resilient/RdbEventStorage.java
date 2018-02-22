996a5d2fe82abaf88750e5a1f06bb7d0e6c9f31e
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2018-02-22 11:55:45 +0100

    launder ox exceptions from retried runnables

--- com.openexchange.chronos.storage.rdb/src/com/openexchange/chronos/storage/rdb/resilient/RdbEventStorage.java
+++ com.openexchange.chronos.storage.rdb/src/com/openexchange/chronos/storage/rdb/resilient/RdbEventStorage.java
@@ -57,30 +57,22 @@ import com.openexchange.chronos.RecurrenceId;
 import com.openexchange.chronos.service.CalendarUtilities;
 import com.openexchange.chronos.service.SearchFilter;
 import com.openexchange.chronos.service.SearchOptions;
-import com.openexchange.chronos.storage.CalendarStorage;
 import com.openexchange.chronos.storage.EventStorage;
-import com.openexchange.chronos.storage.rdb.CalendarStorageWarnings;
 import com.openexchange.exception.OXException;
 import com.openexchange.search.SearchTerm;
 import com.openexchange.server.ServiceLookup;
-import net.jodah.failsafe.Failsafe;
-import net.jodah.failsafe.RetryPolicy;
 
 /**
- * {@link CalendarStorage}
+ * {@link RdbEventStorage}
  *
  * @author <a href="mailto:tobias.friedrich@open-xchange.com">Tobias Friedrich</a>
  * @since v7.10.0
  */
-public class RdbEventStorage extends CalendarStorageWarnings implements EventStorage {
+public class RdbEventStorage extends RdbResilientStorage implements EventStorage {
 
     private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(RdbEventStorage.class);
-    private static final int MAX_RETRIES = 5;
 
     private final EventStorage delegate;
-    private final ServiceLookup services;
-    private final boolean handleTruncations;
-    private final boolean handleIncorrectStrings;
 
     /**
      * Initializes a new {@link RdbEventStorage}.
@@ -91,11 +83,8 @@ public class RdbEventStorage extends CalendarStorageWarnings implements EventSto
      * @param handleIncorrectStrings <code>true</code> to automatically handle incorrect string warnings, <code>false</code>, otherwise
      */
     public RdbEventStorage(ServiceLookup services, EventStorage delegate, boolean handleTruncations, boolean handleIncorrectStrings) {
-        super();
-        this.services = services;
+        super(services, handleTruncations, handleIncorrectStrings);
         this.delegate = delegate;
-        this.handleIncorrectStrings = handleIncorrectStrings;
-        this.handleTruncations = handleTruncations;
     }
 
     @Override
@@ -155,26 +144,17 @@ public class RdbEventStorage extends CalendarStorageWarnings implements EventSto
 
     @Override
     public void insertEvent(Event event) throws OXException {
-        Failsafe
-            .with(new RetryPolicy().withMaxRetries(MAX_RETRIES).retryOn(f -> handle(event, f)))
-            .run(() -> delegate.insertEvent(event)
-        );
+        runWithRetries(() -> delegate.insertEvent(event), f -> handle(event, f));
     }
 
     @Override
     public void insertEvents(List<Event> events) throws OXException {
-        Failsafe
-            .with(new RetryPolicy().withMaxRetries(MAX_RETRIES).retryOn(f -> handle(events, f)))
-            .run(() -> delegate.insertEvents(events)
-        );
+        runWithRetries(() -> delegate.insertEvents(events), f -> handle(events, f));
     }
 
     @Override
     public void updateEvent(Event event) throws OXException {
-        Failsafe
-            .with(new RetryPolicy().withMaxRetries(MAX_RETRIES).retryOn(f -> handle(event, f)))
-            .run(() -> delegate.updateEvent(event)
-        );
+        runWithRetries(() -> delegate.updateEvent(event), f -> handle(event, f));
     }
 
     @Override
@@ -194,18 +174,12 @@ public class RdbEventStorage extends CalendarStorageWarnings implements EventSto
 
     @Override
     public void insertEventTombstone(Event event) throws OXException {
-        Failsafe
-            .with(new RetryPolicy().withMaxRetries(MAX_RETRIES).retryOn(f -> handle(event, f)))
-            .run(() -> delegate.insertEventTombstone(event)
-        );
+        runWithRetries(() -> delegate.insertEventTombstone(event), f -> handle(event, f));
     }
 
     @Override
     public void insertEventTombstones(List<Event> events) throws OXException {
-        Failsafe
-            .with(new RetryPolicy().withMaxRetries(MAX_RETRIES).retryOn(f -> handle(events, f)))
-            .run(() -> delegate.insertEventTombstones(events)
-        );
+        runWithRetries(() -> delegate.insertEventTombstones(events), f -> handle(events, f));
     }
 
     /**
