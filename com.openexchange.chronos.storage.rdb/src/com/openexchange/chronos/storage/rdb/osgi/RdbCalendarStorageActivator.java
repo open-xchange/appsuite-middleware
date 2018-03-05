653cb04030f293e9c0db761ea04738f3177e72cf
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2018-03-05 10:40:48 +0100

    enabled ExceptionSeriesPatternConsistencyTask

--- com.openexchange.chronos.storage.rdb/src/com/openexchange/chronos/storage/rdb/osgi/RdbCalendarStorageActivator.java
+++ com.openexchange.chronos.storage.rdb/src/com/openexchange/chronos/storage/rdb/osgi/RdbCalendarStorageActivator.java
@@ -55,6 +55,7 @@ import com.openexchange.caching.CacheService;
 import com.openexchange.chronos.service.CalendarUtilities;
 import com.openexchange.chronos.service.RecurrenceService;
 import com.openexchange.chronos.storage.CalendarStorageFactory;
+import com.openexchange.chronos.storage.rdb.ExceptionSeriesPatternConsistencyTask;
 import com.openexchange.chronos.storage.rdb.groupware.ChronosCreateTableService;
 import com.openexchange.chronos.storage.rdb.groupware.ChronosCreateTableTask;
 import com.openexchange.chronos.storage.rdb.migration.ChronosStorageMigrationTask;
@@ -111,6 +112,7 @@ public class RdbCalendarStorageActivator extends HousekeepingActivator {
             /*
              * register services for infrastructure
              */
+            registerService(UpdateTaskProviderService.class, new DefaultUpdateTaskProviderService(new ExceptionSeriesPatternConsistencyTask()));
             registerService(UpdateTaskProviderService.class, new DefaultUpdateTaskProviderService(new ChronosCreateTableTask(), new ChronosStorageMigrationTask(this)));
             registerService(CreateTableService.class, new ChronosCreateTableService());
             /*
@@ -118,7 +120,7 @@ public class RdbCalendarStorageActivator extends HousekeepingActivator {
              */
             registerService(CalendarStorageFactory.class, storageFactory);
             // Availability disabled until further notice
-            //registerService(CalendarAvailabilityStorageFactory.class, new com.openexchange.chronos.storage.rdb.RdbCalendarAvailabilityStorageFactory()); 
+            //registerService(CalendarAvailabilityStorageFactory.class, new com.openexchange.chronos.storage.rdb.RdbCalendarAvailabilityStorageFactory());
         } catch (Exception e) {
             LOG.error("error starting {}", context.getBundle(), e);
             throw e;
