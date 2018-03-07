6cfc4c63ac28b5a6f0153389c89e02c4d2a2e7ed
Thorben Betten <thorben.betten@open-xchange.com>
2018-03-07 09:08:45 +0100

    COV-18235 Inefficient Map Iterator

--- com.openexchange.chronos.common/src/com/openexchange/chronos/common/SelfProtectionFactory.java
+++ com.openexchange.chronos.common/src/com/openexchange/chronos/common/SelfProtectionFactory.java
@@ -186,8 +186,7 @@ public class SelfProtectionFactory {
 
         public void checkResultMap(Map<?, ? extends EventsResult> map) throws OXException {
             int sum = 0;
-            for (Object key : map.keySet()) {
-                EventsResult collection = map.get(key);
+            for (EventsResult collection : map.values()) {
                 if (collection == null || null == collection.getEvents()) {
                     continue;
                 }
