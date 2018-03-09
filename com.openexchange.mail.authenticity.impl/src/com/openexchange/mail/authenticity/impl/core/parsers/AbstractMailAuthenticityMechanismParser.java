42c42ad926faeb807632606307c575f8093daf4b
Thorben Betten <thorben.betten@open-xchange.com>
2018-03-09 14:34:02 +0100

    Improved logging

--- com.openexchange.mail.authenticity.impl/src/com/openexchange/mail/authenticity/impl/core/parsers/AbstractMailAuthenticityMechanismParser.java
+++ com.openexchange.mail.authenticity.impl/src/com/openexchange/mail/authenticity/impl/core/parsers/AbstractMailAuthenticityMechanismParser.java
@@ -182,7 +182,7 @@ abstract class AbstractMailAuthenticityMechanismParser implements BiFunction<Map
      * Adds the specified attributes to the specified {@link MailAuthenticityMechanismResult}
      *
      * @param attributes The attributes to add
-     * @param mechResult The {@link MailAuthenticityMechanismResult} to add the attributs to
+     * @param mechResult The {@link MailAuthenticityMechanismResult} to add the attributes to
      */
     protected void addProperties(Map<String, String> attributes, AbstractAuthMechResult mechResult) {
         for (String key : attributes.keySet()) {
