a40b4bdc70b4d99cd4e2d20aba6b05dc48ac3d4e
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2018-03-12 10:18:00 +0100

    Fix for bug #57203: Always parse and apply categories from iCal

--- com.openexchange.data.conversion.ical.ical4j/src/com/openexchange/data/conversion/ical/ical4j/internal/calendar/Categories.java
+++ com.openexchange.data.conversion.ical.ical4j/src/com/openexchange/data/conversion/ical/ical4j/internal/calendar/Categories.java
@@ -56,6 +56,7 @@ import com.openexchange.data.conversion.ical.Mode;
 import com.openexchange.data.conversion.ical.ical4j.internal.AbstractVerifyingAttributeConverter;
 import com.openexchange.groupware.container.CalendarObject;
 import com.openexchange.groupware.contexts.Context;
+import com.openexchange.java.Strings;
 import net.fortuna.ical4j.model.PropertyList;
 import net.fortuna.ical4j.model.component.CalendarComponent;
 
@@ -79,8 +80,7 @@ public class Categories<T extends CalendarComponent, U extends CalendarObject> e
 
     @Override
     public boolean hasProperty(final T t) {
-        final PropertyList categoriesList = t.getProperties("CATEGORIES");
-        return categoriesList.size() > 0;
+        return true;
     }
 
     @Override
@@ -96,6 +96,7 @@ public class Categories<T extends CalendarComponent, U extends CalendarObject> e
         if(bob.length() > 0) {
             bob.setLength(bob.length()-1);
         }
-        cObj.setCategories(bob.toString());
+        String value = bob.toString();
+        cObj.setCategories(Strings.isEmpty(value) ? null : value);
     }
 }
