8022d1e00d38ffdce5b9d673f4e8b2918726b8ce
Thorben Betten <thorben.betten@open-xchange.com>
2018-03-28 14:48:42 +0200

    MW-954: Convert 'object_use_count' and 'object_permission' to utf8mb4 #2

--- com.openexchange.server/src/com/openexchange/groupware/update/tasks/objectusagecount/CreateObjectUseCountTableService.java
+++ com.openexchange.server/src/com/openexchange/groupware/update/tasks/objectusagecount/CreateObjectUseCountTableService.java
@@ -85,7 +85,7 @@ public class CreateObjectUseCountTableService extends AbstractCreateTableImpl {
             "object int(10) unsigned NOT NULL, " +
             "value int(10) unsigned NOT NULL, " +
             "PRIMARY KEY (cid, user, folder, object)" +
-            ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
         return new String[] { stmt };
     }
 
