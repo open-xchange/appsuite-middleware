61a62adebbe009b213f6966a8f29eedc97889f02
Thorben Betten <thorben.betten@open-xchange.com>
2018-03-28 10:15:50 +0200

    COV-18372 Dereference after null check

--- com.openexchange.carddav/src/com/openexchange/carddav/reports/AddressbookQueryReport.java
+++ com.openexchange.carddav/src/com/openexchange/carddav/reports/AddressbookQueryReport.java
@@ -197,10 +197,13 @@ public class AddressbookQueryReport extends PROPFINDAction {
             }
             term = compositeTerm;
         }
-        if (null != textMatchElement.getAttribute("negate-condition") && "yes".equals(textMatchElement.getAttribute("negate-condition").getValue())) {
-            CompositeSearchTerm notTerm = new CompositeSearchTerm(CompositeOperation.NOT);
-            notTerm.addSearchTerm(term);
-            term = notTerm;
+        if (null != textMatchElement) {
+            Attribute attribute = textMatchElement.getAttribute("negate-condition");
+            if (null != attribute && "yes".equals(attribute.getValue())) {
+                CompositeSearchTerm notTerm = new CompositeSearchTerm(CompositeOperation.NOT);
+                notTerm.addSearchTerm(term);
+                term = notTerm;
+            }
         }
         return term;
     }
