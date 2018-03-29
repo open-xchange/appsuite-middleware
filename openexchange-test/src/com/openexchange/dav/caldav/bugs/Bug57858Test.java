12501f07c9eab67363876e8a754603bbbb893de4
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2018-03-29 15:16:27 +0200

    Fix for bug #57858: Re-check newly indicated change exception event from the client's point of view when applying inoming event patches

---
+++ openexchange-test/src/com/openexchange/dav/caldav/bugs/Bug57858Test.java
@@ -0,0 +1,297 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.dav.caldav.bugs;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import org.junit.Test;
+import com.openexchange.dav.StatusCodes;
+import com.openexchange.dav.caldav.CalDAVTest;
+import com.openexchange.dav.caldav.ICalResource;
+import com.openexchange.dav.caldav.ical.SimpleICal.Component;
+import com.openexchange.groupware.calendar.TimeTools;
+import com.openexchange.groupware.container.Appointment;
+
+/**
+ * {@link Bug57858Test}
+ *
+ * notification mail is sent when using 'snooze' in macos calendar
+ *
+ * @author <a href="mailto:tobias.friedrich@open-xchange.com">Tobias Friedrich</a>
+ * @since v7.10.0
+ */
+public class Bug57858Test extends CalDAVTest {
+
+    @Test
+    public void testSnoozeReminderOfOccurrence() throws Exception {
+        /*
+         * create appointment
+         */
+        String uid = randomUID();
+        Date start = TimeTools.D("next saturday at 15:30");
+        Date end = TimeTools.D("next saturday at 17:15");
+        Date initialAcknowledged = TimeTools.D("next saturday at 15:14");
+        String iCal = // @formatter:off
+            "BEGIN:VCALENDAR\r\n" +
+            "VERSION:2.0\r\n" +
+            "PRODID:-//Apple Inc.//Mac OS X 10.8.5//EN\r\n" +
+            "CALSCALE:GREGORIAN\r\n" +
+            "BEGIN:VTIMEZONE\r\n" +
+            "TZID:Europe/Berlin\r\n" +
+            "BEGIN:DAYLIGHT\r\n" +
+            "TZOFFSETFROM:+0100\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n" +
+            "DTSTART:19810329T020000\r\n" +
+            "TZNAME:MESZ\r\n" +
+            "TZOFFSETTO:+0200\r\n" +
+            "END:DAYLIGHT\r\n" +
+            "BEGIN:STANDARD\r\n" +
+            "TZOFFSETFROM:+0200\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n" +
+            "DTSTART:19961027T030000\r\n" +
+            "TZNAME:MEZ\r\n" +
+            "TZOFFSETTO:+0100\r\n" +
+            "END:STANDARD\r\n" +
+            "END:VTIMEZONE\r\n" +
+            "BEGIN:VEVENT\r\n" +
+            "DTSTART;TZID=Europe/Berlin:" + format(start, "Europe/Berlin") + "\r\n" +
+            "DTEND;TZID=Europe/Berlin:" + format(end, "Europe/Berlin") + "\r\n" +
+            "TRANSP:OPAQUE\r\n" +
+            "UID:" + uid + "\r\n" +
+            "DTSTAMP:" + formatAsUTC(new Date()) + "\r\n" +
+            "CLASS:PUBLIC\r\n" +
+            "SUMMARY:RecurringReminder\r\n" +
+            "X-MICROSOFT-CDO-BUSYSTATUS:BUSY\r\n" +
+            "LAST-MODIFIED:" + formatAsUTC(new Date()) + "\r\n" +
+            "CREATED:" + formatAsUTC(new Date()) + "\r\n" +
+            "RRULE:FREQ=DAILY;INTERVAL=1\r\n" +
+            "ORGANIZER:mailto:" + getClient().getValues().getDefaultAddress() + "\r\n" +
+            "ATTENDEE;PARTSTAT=ACCEPTED:mailto:" + getClient().getValues().getDefaultAddress() + "\r\n" +
+            "ATTENDEE;PARTSTAT=NEEDS-ACTION;RSVP=TRUE:mailto:" + getClient2().getValues().getDefaultAddress() + "\r\n" +
+            "BEGIN:VALARM\r\n" +
+            "X-WR-ALARMUID:7B669A77-E205-4B03-A1AF-40FB146C4A3F\r\n" +
+            "UID:7B669A77-E205-4B03-A1AF-40FB146C4A3F\r\n" +
+            "TRIGGER:-PT15M\r\n" +
+            "DESCRIPTION:Ereignisbenachrichtigung\r\n" +
+            "ACTION:DISPLAY\r\n" +
+            "END:VALARM\r\n" +
+            "BEGIN:VALARM\r\n" +
+            "X-WR-ALARMUID:56C5C265-7442-44E6-8F9C-17C71DCF932A\r\n" +
+            "UID:56C5C265-7442-44E6-8F9C-17C71DCF932A\r\n" +
+            "TRIGGER;VALUE=DATE-TIME:19760401T005545Z\r\n" +
+            "X-APPLE-DEFAULT-ALARM:TRUE\r\n" +
+            "ACTION:NONE\r\n" +
+            "END:VALARM\r\n" +
+            "END:VEVENT\r\n" +
+            "END:VCALENDAR\r\n"
+        ; // @formatter:on
+        assertEquals("response code wrong", StatusCodes.SC_CREATED, putICal(uid, iCal));
+        /*
+         * verify appointment on server
+         */
+        Appointment appointment = getAppointment(uid);
+        assertNotNull("appointment not found on server", appointment);
+        rememberForCleanUp(appointment);
+        assertTrue("no reminder found", appointment.containsAlarm());
+        assertEquals("reminder minutes wrong", 15, appointment.getAlarm());
+        /*
+         * verify appointment on client
+         */
+        ICalResource iCalResource = get(uid);
+        assertNotNull("No VEVENT in iCal found", iCalResource.getVEvent());
+        assertEquals("UID wrong", uid, iCalResource.getVEvent().getUID());
+        assertNotNull("No ALARM in iCal found", iCalResource.getVEvent().getVAlarm());
+        assertEquals("ALARM wrong", "-PT15M", iCalResource.getVEvent().getVAlarm().getPropertyValue("TRIGGER"));
+        /*
+         * snooze reminder in client
+         */
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"));
+        calendar.setTime(start);
+        calendar.add(Calendar.DATE, 2);
+        Date exceptionStart = calendar.getTime();
+        calendar.setTime(end);
+        calendar.add(Calendar.DATE, 2);
+        Date exceptionEnd = calendar.getTime();
+        calendar.setTime(exceptionStart);
+        calendar.add(Calendar.MINUTE, -14);
+        calendar.add(Calendar.SECOND, 17);
+        Date acknowledgedDate = calendar.getTime();
+        calendar.add(Calendar.MINUTE, 5);
+        Date nextTrigger = calendar.getTime();
+        String relatedUID = randomUID();
+        iCal = // @formatter:off
+            "BEGIN:VCALENDAR\r\n" +
+            "VERSION:2.0\r\n" +
+            "PRODID:-//Apple Inc.//Mac OS X 10.8.5//EN\r\n" +
+            "CALSCALE:GREGORIAN\r\n" +
+            "BEGIN:VTIMEZONE\r\n" +
+            "TZID:Europe/Berlin\r\n" +
+            "BEGIN:DAYLIGHT\r\n" +
+            "TZOFFSETFROM:+0100\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n" +
+            "DTSTART:19810329T020000\r\n" +
+            "TZNAME:MESZ\r\n" +
+            "TZOFFSETTO:+0200\r\n" +
+            "END:DAYLIGHT\r\n" +
+            "BEGIN:STANDARD\r\n" +
+            "TZOFFSETFROM:+0200\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n" +
+            "DTSTART:19961027T030000\r\n" +
+            "TZNAME:MEZ\r\n" +
+            "TZOFFSETTO:+0100\r\n" +
+            "END:STANDARD\r\n" +
+            "END:VTIMEZONE\r\n" +
+            "BEGIN:VEVENT\r\n" +
+            "DTSTART;TZID=Europe/Berlin:" + format(start, "Europe/Berlin") + "\r\n" +
+            "DTEND;TZID=Europe/Berlin:" + format(end, "Europe/Berlin") + "\r\n" +
+            "TRANSP:OPAQUE\r\n" +
+            "UID:" + uid + "\r\n" +
+            "DTSTAMP:" + formatAsUTC(new Date()) + "\r\n" +
+            "CLASS:PUBLIC\r\n" +
+            "SUMMARY:RecurringReminder\r\n" +
+            "X-MICROSOFT-CDO-BUSYSTATUS:BUSY\r\n" +
+            "LAST-MODIFIED:" + formatAsUTC(new Date()) + "\r\n" +
+            "CREATED:" + formatAsUTC(new Date()) + "\r\n" +
+            "RRULE:FREQ=DAILY;INTERVAL=1\r\n" +
+            iCalResource.getVEvent().getProperty("ORGANIZER") + "\r\n" +
+            iCalResource.getVEvent().getProperties("ATTENDEE").get(0) + "\r\n" +
+            iCalResource.getVEvent().getProperties("ATTENDEE").get(1) + "\r\n" +
+            "BEGIN:VALARM\r\n" +
+            "X-WR-ALARMUID:56C5C265-7442-44E6-8F9C-17C71DCF932A\r\n" +
+            "UID:56C5C265-7442-44E6-8F9C-17C71DCF932A\r\n" +
+            "TRIGGER;RELATED=START:-PT15M\r\n" +
+            "ACTION:DISPLAY\r\n" +
+            "DESCRIPTION:Alarm\r\n" +
+            "X-MOZ-LASTACK:" + formatAsUTC(initialAcknowledged) + "\r\n" +
+            "ACKNOWLEDGED:" + formatAsUTC(acknowledgedDate) + "\r\n" +
+            "END:VALARM\r\n" +
+            "BEGIN:VALARM\r\n" +
+            "X-WR-ALARMUID:56C5C265-7442-44E6-8F9C-17C71DCF932A\r\n" +
+            "UID:56C5C265-7442-44E6-8F9C-17C71DCF932A\r\n" +
+            "TRIGGER;VALUE=DATE-TIME:19760401T005545Z\r\n" +
+            "X-APPLE-DEFAULT-ALARM:TRUE\r\n" +
+            "ACTION:NONE\r\n" +
+            "END:VALARM\r\n" +
+            "END:VEVENT\r\n" +
+            "BEGIN:VEVENT\r\n" +
+            "DTSTART;TZID=Europe/Berlin:" + format(exceptionStart, "Europe/Berlin") + "\r\n" +
+            "DTEND;TZID=Europe/Berlin:" + format(exceptionEnd, "Europe/Berlin") + "\r\n" +
+            "TRANSP:OPAQUE\r\n" +
+            "UID:" + uid + "\r\n" +
+            "DTSTAMP:" + formatAsUTC(new Date()) + "\r\n" +
+            "SUMMARY:RecurringReminder\r\n" +
+            "X-MICROSOFT-CDO-BUSYSTATUS:BUSY\r\n" +
+            "LAST-MODIFIED:" + formatAsUTC(new Date()) + "\r\n" +
+            "CREATED:" + formatAsUTC(new Date()) + "\r\n" +
+            "RECURRENCE-ID:" + formatAsUTC(exceptionStart) + "\r\n" +
+            iCalResource.getVEvent().getProperty("ORGANIZER") + "\r\n" +
+            iCalResource.getVEvent().getProperties("ATTENDEE").get(0) + "\r\n" +
+            iCalResource.getVEvent().getProperties("ATTENDEE").get(1) + "\r\n" +
+            "BEGIN:VALARM\r\n" +
+            "X-WR-ALARMUID:" + randomUID() + "\r\n" +
+            "UID:" + relatedUID + "\r\n" +
+            "TRIGGER:-PT15M\r\n" +
+            "DESCRIPTION:Alarm\r\n" +
+            "ACTION:DISPLAY\r\n" +
+            "ACKNOWLEDGED:" + formatAsUTC(acknowledgedDate) + "\r\n" +
+            "END:VALARM\r\n" +
+            "BEGIN:VALARM\r\n" +
+            "X-WR-ALARMUID:" + randomUID() + "\r\n" +
+            "UID:" + randomUID() + "\r\n" +
+            "TRIGGER;VALUE=DATE-TIME:" + formatAsUTC(nextTrigger) + "\r\n" +
+            "DESCRIPTION:Alarm\r\n" +
+            "RELATED-TO:" + relatedUID + "\r\n" +
+            "ACTION:DISPLAY\r\n" +
+            "END:VALARM\r\n" +
+            "BEGIN:VALARM\r\n" +
+            "X-WR-ALARMUID:" + randomUID() + "\r\n" +
+            "UID:" + randomUID() + "\r\n" +
+            "TRIGGER;VALUE=DATE-TIME:19760401T005545Z\r\n" +
+            "X-APPLE-DEFAULT-ALARM:TRUE\r\n" +
+            "ACTION:NONE\r\n" +
+            "END:VALARM\r\n" +
+            "END:VEVENT\r\n" +
+            "END:VCALENDAR\r\n"
+        ; // @formatter:on
+        assertEquals("response code wrong", StatusCodes.SC_CREATED, putICalUpdate(uid, iCal, iCalResource.getETag()));
+        /*
+         * verify appointment & exception on server
+         */
+        appointment = getAppointment(uid);
+        assertNotNull("appointment not found on server", appointment);
+        rememberForCleanUp(appointment);
+        assertTrue("no reminder found", appointment.containsAlarm());
+        assertEquals("reminder minutes wrong", 15, appointment.getAlarm());
+        assertTrue("change exceptions found on server", null == appointment.getChangeException() || 0 == appointment.getChangeException().length);
+        /*
+         * verify appointment on client
+         */
+        iCalResource = get(uid);
+        assertNotNull("No VEVENT in iCal found", iCalResource.getVEvent());
+        assertEquals("More than one VEVENT in iCal found", 1, iCalResource.getVEvents().size());
+        assertEquals("UID wrong", uid, iCalResource.getVEvent().getUID());
+        List<Component> vAlarms = iCalResource.getVEvent().getVAlarms();
+        assertEquals("Unexpected number of VALARMs found", 2, vAlarms.size());
+        for (Component vAlarm : vAlarms) {
+            if (null != vAlarm.getProperty("RELATED-TO")) {
+                assertEquals("ALARM wrong", "-PT8M43S", vAlarm.getPropertyValue("TRIGGER"));
+            } else {
+                assertEquals("ALARM wrong", "-PT15M", vAlarm.getPropertyValue("TRIGGER"));
+                assertEquals("ACKNOWLEDGED wrong", formatAsUTC(acknowledgedDate), vAlarm.getPropertyValue("ACKNOWLEDGED"));
+            }
+        }
+    }
+
+}
