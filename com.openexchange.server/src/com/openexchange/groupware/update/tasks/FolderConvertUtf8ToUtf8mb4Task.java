13be20b649b3cc4fee046663957f888eab13017f
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-04-04 08:41:42 +0200

    don't alter the same tables twice

--- com.openexchange.server/src/com/openexchange/groupware/update/tasks/FolderConvertUtf8ToUtf8mb4Task.java
+++ com.openexchange.server/src/com/openexchange/groupware/update/tasks/FolderConvertUtf8ToUtf8mb4Task.java
@@ -71,8 +71,7 @@ public class FolderConvertUtf8ToUtf8mb4Task extends SimpleConvertUtf8ToUtf8mb4Up
     public FolderConvertUtf8ToUtf8mb4Task() {
         //@formatter:off
         super(ImmutableList.of("oxfolder_tree", "oxfolder_permissions", "oxfolder_specialfolders", "oxfolder_userfolders", 
-            "oxfolder_userfolders_standardfolders", "del_oxfolder_tree", "del_oxfolder_permissions", "oxfolder_lock", "oxfolder_property",
-            "virtualTree", "virtualPermission", "virtualSubscription", "virtualBackupTree", "virtualBackupPermission", "virtualBackupSubscription"),
+            "oxfolder_userfolders_standardfolders", "del_oxfolder_tree", "del_oxfolder_permissions", "oxfolder_lock", "oxfolder_property"),
             AddTypeToFolderPermissionTableUpdateTask.class.getName());
         //@formatter:on
     }
