8f51146640a7e0e5cc926e35060c7a53517fabdf
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-04-04 13:10:38 +0200

    MW-954: use simple task #17

--- com.openexchange.oauth.provider.impl/src/com/openexchange/oauth/provider/impl/groupware/OAuthGrantConvertUtf8ToUtf8mb4Task.java
+++ com.openexchange.oauth.provider.impl/src/com/openexchange/oauth/provider/impl/groupware/OAuthGrantConvertUtf8ToUtf8mb4Task.java
@@ -51,12 +51,10 @@ package com.openexchange.oauth.provider.impl.groupware;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.openexchange.groupware.update.AbstractConvertUtf8ToUtf8mb4Task;
 import com.openexchange.groupware.update.PerformParameters;
-
+import com.openexchange.groupware.update.SimpleConvertUtf8ToUtf8mb4UpdateTask;
 
 /**
  * {@link OAuthGrantConvertUtf8ToUtf8mb4Task}
@@ -64,28 +62,13 @@ import com.openexchange.groupware.update.PerformParameters;
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  * @since v7.10.0
  */
-public class OAuthGrantConvertUtf8ToUtf8mb4Task extends AbstractConvertUtf8ToUtf8mb4Task {
+public class OAuthGrantConvertUtf8ToUtf8mb4Task extends SimpleConvertUtf8ToUtf8mb4UpdateTask {
 
     /**
      * Initializes a new {@link OAuthGrantConvertUtf8ToUtf8mb4Task}.
      */
     public OAuthGrantConvertUtf8ToUtf8mb4Task() {
-        super();
-    }
-
-    @Override
-    public String[] getDependencies() {
-        return new String[] { CreateOAuthGrantTableTask.class.getName() };
-    }
-
-    @Override
-    protected List<String> tablesToConvert() {
-        return ImmutableList.of();
-    }
-
-    @Override
-    protected void before(PerformParameters params, Connection connection) throws SQLException {
-        // Nothing
+        super(ImmutableList.of(), CreateOAuthGrantTableTask.class.getName());
     }
 
     @Override
@@ -95,5 +78,4 @@ public class OAuthGrantConvertUtf8ToUtf8mb4Task extends AbstractConvertUtf8ToUtf
         mapBuilder.put("access_token", 255);
         changeTable(connection, params.getSchema().getSchema(), "oauth_grant", mapBuilder.build());
     }
-
 }
