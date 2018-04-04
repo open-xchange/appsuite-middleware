4c631e480858d2aaf82c4eef3987f91dc49a93c1
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-04-04 12:27:25 +0200

    MW-954: convert 'oxfolder_user_property'

--- com.openexchange.server/src/com/openexchange/groupware/update/tasks/FolderConvertUtf8ToUtf8mb4Task.java
+++ com.openexchange.server/src/com/openexchange/groupware/update/tasks/FolderConvertUtf8ToUtf8mb4Task.java
@@ -71,7 +71,8 @@ public class FolderConvertUtf8ToUtf8mb4Task extends SimpleConvertUtf8ToUtf8mb4Up
     public FolderConvertUtf8ToUtf8mb4Task() {
         //@formatter:off
         super(ImmutableList.of("oxfolder_tree", "oxfolder_permissions", "oxfolder_specialfolders", "oxfolder_userfolders", 
-            "oxfolder_userfolders_standardfolders", "del_oxfolder_tree", "del_oxfolder_permissions", "oxfolder_lock", "oxfolder_property"),
+            "oxfolder_userfolders_standardfolders", "del_oxfolder_tree", "del_oxfolder_permissions", "oxfolder_lock", 
+            "oxfolder_property", "oxfolder_user_property"),
             AddTypeToFolderPermissionTableUpdateTask.class.getName());
         //@formatter:on
     }
