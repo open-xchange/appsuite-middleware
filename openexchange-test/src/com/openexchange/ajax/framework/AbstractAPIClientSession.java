c7de49a221a93fdf8e1d7fece4ca372575a66a14
Martin Schneider <martin.schneider@open-xchange.com>
2018-04-09 16:34:13 +0200

    Force user login with new client

--- openexchange-test/src/com/openexchange/ajax/framework/AbstractAPIClientSession.java
+++ openexchange-test/src/com/openexchange/ajax/framework/AbstractAPIClientSession.java
@@ -217,6 +217,7 @@ public abstract class AbstractAPIClientSession extends AbstractClientSession {
             newClient = new ApiClient();
             setBasePath(newClient);
             newClient.setUserAgent("ox-test-client");
+            newClient.login(user.getLogin(), user.getPassword());
         } catch (Exception e) {
             LOG.error("Could not generate new client for user {} in context {} ", user.getUser(), user.getContext());
             throw new OXException();
