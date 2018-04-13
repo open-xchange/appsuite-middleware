a548c2c403c4c98475a8ce7d6215aea0c80b5e2f
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-04-13 16:02:39 +0200

    MW-947: Consider the optional double quotes as part of the value

--- com.openexchange.mail.authenticity.impl/test/com/openexchange/mail/authenticity/test/StandardAuthenticationResultsValidatorTest.java
+++ com.openexchange.mail.authenticity.impl/test/com/openexchange/mail/authenticity/test/StandardAuthenticationResultsValidatorTest.java
@@ -51,8 +51,10 @@ package com.openexchange.mail.authenticity.test;
 
 import java.util.Arrays;
 import java.util.List;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import org.junit.Test;
+import com.openexchange.exception.OXException;
 import com.openexchange.mail.authenticity.AllowedAuthServId;
 import com.openexchange.mail.authenticity.MailAuthenticityStatus;
 import com.openexchange.mail.authenticity.impl.core.AuthenticationResultsValidator;
@@ -78,14 +80,16 @@ public class StandardAuthenticationResultsValidatorTest extends AbstractTestMail
     public void testCorrectOverallResult() throws Exception {
         AuthenticationResultsValidator validator = StandardAuthenticationResultsValidator.getInstance();
 
+        //@formatter:off
         List<String> authHeaders = Arrays.asList(
             "open-xchange.com; dkim=pass (1024-bit key; unprotected) header.d=foreigner.com header.i=rootserver@hosting.foreigner.com",
             "open-xchange.com; spf=none (mailfrom) smtp.mailfrom=bounce.superforeigner.com (client-ip=212.227.126.222; helo=mbulk.foreigner.com; envelope-from=99562902061843288.2.2030025036@bounce.superforeigner.com; receiver=<UNKNOWN>)",
             "open-xchange.com; dmarc=none (p=none dis=none) header.from=hosting.foreigner.com"
         );
+        //@formatter:on
+
         List<AllowedAuthServId> allowedAuthServIds = AllowedAuthServId.allowedAuthServIdsFor("open-xchange.com");
         InternetAddress from = new QuotedInternetAddress("Super Hosting <rootserver@hosting.foreigner.com>");
-
 
         MailAuthenticityResult result = validator.parseHeaders(authHeaders, from, allowedAuthServIds);
         assertStatus(MailAuthenticityStatus.PASS, result.getStatus());
@@ -94,14 +98,15 @@ public class StandardAuthenticationResultsValidatorTest extends AbstractTestMail
     @Test
     public void testCorrectOverallResult2() throws Exception {
         AuthenticationResultsValidator validator = StandardAuthenticationResultsValidator.getInstance();
-
+        //@formatter:off
         List<String> authHeaders = Arrays.asList(
             "open-xchange.com; spf=none (mailfrom) smtp.mailfrom=barfoo.org (client-ip=168.135.221.145; helo=hydra.barfoo.org; envelope-from=opensuse-buildservice+bounces-25693-marcus.klein=open-xchange.com@barfoo.org; receiver=<UNKNOWN>)",
             "open-xchange.com; dmarc=none (p=none dis=none) header.from=foo.de"
         );
+        //@formatter:on
+
         List<AllowedAuthServId> allowedAuthServIds = AllowedAuthServId.allowedAuthServIdsFor("open-xchange.com");
         InternetAddress from = new QuotedInternetAddress("Alice <alice@foo.de>");
-
 
         MailAuthenticityResult result = validator.parseHeaders(authHeaders, from, allowedAuthServIds);
         assertStatus(MailAuthenticityStatus.NONE, result.getStatus());
@@ -111,17 +116,37 @@ public class StandardAuthenticationResultsValidatorTest extends AbstractTestMail
     public void testCorrectOverallResult3() throws Exception {
         AuthenticationResultsValidator validator = StandardAuthenticationResultsValidator.getInstance();
 
+        //@formatter:off
         List<String> authHeaders = Arrays.asList(
             "open-xchange.com; dkim=pass (1024-bit key; unprotected) header.d=mailer.aha.io header.i=ce3453825a6c1f0be41c5dc0@mailer.aha.io header.b=\"m+mX94kg\"; dkim=pass (1024-bit key; unprotected) header.d=mandrillapp.com header.i=@mandrillapp.com header.b=\"g5De4kJs\"; dkim-atps=neutral",
             "open-xchange.com; spf=pass (mailfrom) smtp.mailfrom=mailer-return.aha.io (client-ip=198.2.186.180; helo=mail186-180.suw21.mandrillapp.com; envelope-from=bounce-md_30055513.5a8e8edb.v1-89334d1b8f164c21a4e139a517316437@mailer-return.aha.io; receiver=<UNKNOWN>)",
             "open-xchange.com; dmarc=none (p=none dis=none) header.from=mailer.aha.io"
         );
+        //@formatter:on
+
         List<AllowedAuthServId> allowedAuthServIds = AllowedAuthServId.allowedAuthServIdsFor("open-xchange.com");
         InternetAddress from = new QuotedInternetAddress("\"Aha! (Steffen Templin)\" <ce3453825a6c1f0be41c5dc0@mailer.aha.io>");
-
 
         MailAuthenticityResult result = validator.parseHeaders(authHeaders, from, allowedAuthServIds);
         assertStatus(MailAuthenticityStatus.PASS, result.getStatus());
     }
 
+    @Test
+    public void testCorrectOverallResult4() throws OXException, AddressException {
+        AuthenticationResultsValidator validator = StandardAuthenticationResultsValidator.getInstance();
+
+        //@formatter:off
+        List<String> authHeaders = Arrays.asList(
+            "open-xchange.com; dkim=fail reason=\"key not found in DNS\" (0-bit key; unprotected) header.d=cvcmail.onmicrosoft.com header.i=@cvcmail.onmicrosoft.com header.b=\"XCGeafBf\";dkim-atps=neutral",
+            "open-xchange.com; spf=pass (mailfrom) smtp.mailfrom=alticeusa.com (client-ip=104.47.42.101; helo=nam03-by2-obe.outbound.protection.outlook.com; envelope-from=frances.breckon@alticeusa.com; receiver=<UNKNOWN>)",
+            "open-xchange.com; dmarc=none (p=none dis=none) header.from=AlticeUSA.com"
+        );
+        //@formatter:on
+
+        List<AllowedAuthServId> allowedAuthServIds = AllowedAuthServId.allowedAuthServIdsFor("open-xchange.com");
+        InternetAddress from = new QuotedInternetAddress("Frances Breckon <Frances.Breckon@AlticeUSA.com>");
+
+        MailAuthenticityResult result = validator.parseHeaders(authHeaders, from, allowedAuthServIds);
+        assertStatus(MailAuthenticityStatus.NEUTRAL, result.getStatus());
+    }
 }
