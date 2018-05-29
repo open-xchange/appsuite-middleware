cf8275931ba71587850c39a851ee61c0c202eb16
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-05-29 10:07:15 +0200

    adjusted the update task run all update clt to use the rmi service

--- com.openexchange.oxscripts.clt/src/com/openexchange/groupware/update/tools/console/UpdateTaskRunAllUpdateCLT.java
+++ com.openexchange.oxscripts.clt/src/com/openexchange/groupware/update/tools/console/UpdateTaskRunAllUpdateCLT.java
@@ -49,199 +49,67 @@
 
 package com.openexchange.groupware.update.tools.console;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.LockSupport;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanException;
-import javax.management.MBeanServerConnection;
-import javax.management.ReflectionException;
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXServiceURL;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
-import com.openexchange.groupware.update.tools.Constants;
+import com.openexchange.groupware.update.UpdateTaskService;
 
 /**
  * {@link UpdateTaskRunAllUpdateCLT} - Command-Line access to run update process for a certain schema via update task toolkit.
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
  */
-public final class UpdateTaskRunAllUpdateCLT {
+public final class UpdateTaskRunAllUpdateCLT extends AbstractUpdateTasksCLT<Void> {
 
-    private static final Options toolkitOptions;
-
-    static {
-        toolkitOptions = new Options();
-        toolkitOptions.addOption("h", "help", false, "Prints a help text");
-        toolkitOptions.addOption("e", "error", false, "The flag indicating whether process is supposed to be stopped if an error occurs when trying to update a schema.");
-        toolkitOptions.addOption("H", "host", true, "The optional JMX host (default:localhost)");
-        toolkitOptions.addOption("p", "port", true, "The optional JMX port (default:9999)");
-        toolkitOptions.addOption("l", "login", true, "The optional JMX login (if JMX has authentication enabled)");
-        toolkitOptions.addOption("s", "password", true, "The optional JMX password (if JMX has authentication enabled)");
-        toolkitOptions.addOption(new Option(null, "responsetimeout", true, "The optional response timeout in seconds when reading data from server (default: 0s; infinite)"));
+    /**
+     * Entry point
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new UpdateTaskRunAllUpdateCLT().execute(args);
     }
 
-    private static void printHelp() {
-        final HelpFormatter helpFormatter = new HelpFormatter();
-        helpFormatter.printHelp("runallupdate", toolkitOptions);
-    }
+    boolean failOnError = false;
 
     /**
      * Initializes a new {@link UpdateTaskRunAllUpdateCLT}.
      */
     private UpdateTaskRunAllUpdateCLT() {
-        super();
+        super("runallupdate");
     }
 
-    public static void main(final String[] args) {
-        final CommandLineParser parser = new PosixParser();
-        boolean error = true;
-        try {
-            final CommandLine cmd = parser.parse(toolkitOptions, args);
-            if (cmd.hasOption('h')) {
-                printHelp();
-                System.exit(0);
-            }
-            String host = "localhost";
-            if (cmd.hasOption('H')) {
-                String tmp = cmd.getOptionValue('H');
-                if (null != tmp) {
-                    host = tmp.trim();
-                }
-            }
-            int port = 9999;
-            if (cmd.hasOption('p')) {
-                final String val = cmd.getOptionValue('p');
-                if (null != val) {
-                    try {
-                        port = Integer.parseInt(val.trim());
-                    } catch (final NumberFormatException e) {
-                        System.err.println("Port parameter is not a number: " + val);
-                        printHelp();
-                        System.exit(1);
-                    }
-                    if (port < 1 || port > 65535) {
-                        System.err.println("Port parameter is out of range: " + val + ". Valid range is from 1 to 65535.");
-                        printHelp();
-                        System.exit(1);
-                    }
-                }
-            }
-            int responseTimeout = 0;
-            if (cmd.hasOption("responsetimeout")) {
-                final String val = cmd.getOptionValue('p');
-                if (null != val) {
-                    try {
-                        responseTimeout = Integer.parseInt(val.trim());
-                    } catch (final NumberFormatException e) {
-                        System.err.println("responsetimeout parameter is not a number: " + val);
-                        printHelp();
-                        System.exit(1);
-                    }
-                }
-            }
-            boolean failOnError = false;
-            if (cmd.hasOption("error")) {
-                failOnError = true;
-            }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#addOptions(org.apache.commons.cli.Options)
+     */
+    @Override
+    protected void addOptions(Options options) {
+        options.addOption("e", "error", false, "The flag indicating whether process is supposed to be stopped if an error occurs when trying to update a schema.");
+    }
 
-            if (responseTimeout > 0) {
-                /*
-                 * The value of this property represents the length of time (in milliseconds) that the client-side Java RMI runtime will
-                 * use as a socket read timeout on an established JRMP connection when reading response data for a remote method invocation.
-                 * Therefore, this property can be used to impose a timeout on waiting for the results of remote invocations;
-                 * if this timeout expires, the associated invocation will fail with a java.rmi.RemoteException.
-                 *
-                 * Setting this property should be done with due consideration, however, because it effectively places an upper bound on the
-                 * allowed duration of any successful outgoing remote invocation. The maximum value is Integer.MAX_VALUE, and a value of
-                 * zero indicates an infinite timeout. The default value is zero (no timeout).
-                 */
-                System.setProperty("sun.rmi.transport.tcp.responseTimeout", Integer.toString(responseTimeout * 1000));
-            }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#invoke(org.apache.commons.cli.Options, org.apache.commons.cli.CommandLine, java.lang.String)
+     */
+    @Override
+    protected Void invoke(Options options, CommandLine cmd, String optRmiHostName) throws Exception {
+        UpdateTaskService updateTaskService = getRmiStub(UpdateTaskService.RMI_NAME);
+        updateTaskService.runAllUpdates(failOnError);
+        return null;
+    }
 
-            String jmxLogin = null;
-            if (cmd.hasOption('l')) {
-                jmxLogin = cmd.getOptionValue('l');
-            }
-            String jmxPassword = null;
-            if (cmd.hasOption('s')) {
-                jmxPassword = cmd.getOptionValue('s');
-            }
-
-            final Map<String, Object> environment;
-            if (jmxLogin == null || jmxPassword == null) {
-                environment = null;
-            } else {
-                environment = new HashMap<String, Object>(1);
-                String[] creds = new String[] { jmxLogin, jmxPassword };
-                environment.put(JMXConnector.CREDENTIALS, creds);
-            }
-
-            final JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + host + ":" + port + "/server");
-            final JMXConnector jmxConnector = JMXConnectorFactory.connect(url, environment);
-            try {
-                MBeanServerConnection mbsc = jmxConnector.getMBeanServerConnection();
-                Object jobId = mbsc.invoke(Constants.OBJECT_NAME, "runAllUpdate",  new Object[] { failOnError ? "true" : "false" }, null);
-                if (null != jobId) {
-                    Object status = mbsc.invoke(Constants.OBJECT_NAME, "getStatus",  new Object[] { jobId.toString() }, null);
-                    while (null != status) {
-                        String statusText = status.toString();
-                        if (statusText.startsWith("NOK:")) {
-                            System.out.println(statusText.substring(4).replaceAll("\\\\R", System.getProperty("line.separator")));
-                            status = null;
-                        } else {
-                            if (statusText.startsWith("OK:")) {
-                                statusText = statusText.substring(3);
-                            }
-                            System.out.println(statusText.replaceAll("\\\\R", System.getProperty("line.separator")));
-                            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(4L));
-                            status = mbsc.invoke(Constants.OBJECT_NAME, "getStatus",  new Object[] { jobId.toString() }, null);
-                        }
-                    }
-                }
-            } finally {
-                if (null != jmxConnector) {
-                    try {
-                        jmxConnector.close();
-                    } catch (final Exception e) {
-                        // Ignore
-                    }
-                }
-            }
-            error = false;
-        } catch (final ParseException e) {
-            System.err.println("Unable to parse command line: " + e.getMessage());
-            printHelp();
-        } catch (final MalformedURLException e) {
-            System.err.println("URL to connect to server is invalid: " + e.getMessage());
-        } catch (final IOException e) {
-            System.err.println("Unable to communicate with the server: " + e.getMessage());
-        } catch (final InstanceNotFoundException e) {
-            System.err.println("Instance is not available: " + e.getMessage());
-        } catch (final MBeanException e) {
-            Throwable t = e.getCause();
-            String message = null == t ? e.getMessage() : t.getMessage();
-            System.err.println(null == message ? "Unexpected error." : "Unexpected error: " + message);
-        } catch (final ReflectionException e) {
-            System.err.println("Problem with reflective type handling: " + e.getMessage());
-        } catch (final RuntimeException e) {
-            System.err.println("Problem in runtime: " + e.getMessage());
-            printHelp();
-        } finally {
-            if (error) {
-                System.exit(1);
-            }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractCLI#checkOptions(org.apache.commons.cli.CommandLine)
+     */
+    @Override
+    protected void checkOptions(CommandLine cmd) {
+        if (cmd.hasOption("error")) {
+            failOnError = true;
         }
     }
-
 }
