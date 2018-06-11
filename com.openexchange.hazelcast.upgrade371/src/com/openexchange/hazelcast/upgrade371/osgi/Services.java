f8ffbfdf1673ce850eab1a203e45f9ccba54d01b
Thorben Betten <thorben.betten@open-xchange.com>
2018-06-11 15:19:30 +0200

    Change for bug 58947: Explicitly invalidate contexts through calling dedicated Portable implementation

--- com.openexchange.hazelcast.upgrade371/src/com/openexchange/hazelcast/upgrade371/osgi/Services.java
+++ com.openexchange.hazelcast.upgrade371/src/com/openexchange/hazelcast/upgrade371/osgi/Services.java
@@ -47,27 +47,69 @@
  *
  */
 
-package com.openexchange.legacy;
+package com.openexchange.hazelcast.upgrade371.osgi;
 
-import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicReference;
+import com.openexchange.server.ServiceLookup;
 
 /**
- * {@link CacheKey} - A cache key that consists of a context ID and an unique (serializable) identifier of any object.
+ * {@link Services} - The static service lookup.
+ *
+ * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
  */
-public interface CacheKey extends Serializable {
+public final class Services {
 
     /**
-     * Gets the context ID
-     *
-     * @return The context ID
+     * Initializes a new {@link Services}.
      */
-    int getContextId();
+    private Services() {
+        super();
+    }
+
+    private static final AtomicReference<ServiceLookup> REF = new AtomicReference<ServiceLookup>();
 
     /**
-     * Gets the key objects
+     * Sets the service lookup.
      *
-     * @return The key objects
+     * @param serviceLookup The service lookup or <code>null</code>
      */
-    String[] getKeys();
+    public static void setServiceLookup(final ServiceLookup serviceLookup) {
+        REF.set(serviceLookup);
+    }
+
+    /**
+     * Gets the service lookup.
+     *
+     * @return The service lookup or <code>null</code>
+     */
+    public static ServiceLookup getServiceLookup() {
+        return REF.get();
+    }
+
+    /**
+     * Gets the service of specified type
+     *
+     * @param clazz The service's class
+     * @return The service
+     * @throws IllegalStateException If an error occurs while returning the demanded service
+     */
+    public static <S extends Object> S getService(final Class<? extends S> clazz) {
+        final com.openexchange.server.ServiceLookup serviceLookup = REF.get();
+        if (null == serviceLookup) {
+            throw new IllegalStateException("Missing ServiceLookup instance. Bundle \"com.openexchange.hazelcast.upgrade371\" not started?");
+        }
+        return serviceLookup.getService(clazz);
+    }
+
+    /**
+     * (Optionally) Gets the service of specified type
+     *
+     * @param clazz The service's class
+     * @return The service or <code>null</code> if absent
+     */
+    public static <S extends Object> S optService(final Class<? extends S> clazz) {
+        ServiceLookup serviceLookup = REF.get();
+        return null == serviceLookup ? null : serviceLookup.getOptionalService(clazz);
+    }
 
 }
