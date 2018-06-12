dde881f5fdf445dd586962c7db7326d337197e5a
Daniel Becker <daniel.becker@open-xchange.com>
2018-06-12 12:25:39 +0200

    Avoid endless loop
    Efficiently reverts commit 9152982b7e79c846855a713a5a6dcc1fea3a1256

--- com.openexchange.subscribe/src/com/openexchange/subscribe/TargetFolderSession.java
+++ com.openexchange.subscribe/src/com/openexchange/subscribe/TargetFolderSession.java
@@ -56,7 +56,6 @@ import java.util.Set;
 import com.openexchange.groupware.generic.TargetFolderDefinition;
 import com.openexchange.session.Session;
 import com.openexchange.sessiond.SessiondService;
-import com.openexchange.tools.session.ServerSession;
 import com.openexchange.tools.session.ServerSessionAdapter;
 
 
@@ -70,33 +69,22 @@ public class TargetFolderSession implements Session {
     private final int contextId;
     private final int userId;
     private final Map<String, Object> params;
-    private final ServerSession session;
+    private final Session             session;
 
     public TargetFolderSession(final TargetFolderDefinition target) {
         super();
         contextId = target.getContext().getContextId();
         userId = target.getUserId();
         
-        Subscription subscription = null;
-
-        if(Subscription.class.isAssignableFrom(target.getClass())) {
-            subscription = (Subscription) target;
-        }
-
         // Initialize
-        if (null != subscription && null != subscription.getSession()) {
-            session = subscription.getSession();
+        final SessiondService service = SessiondService.SERVICE_REFERENCE.get();
+        Session ses = null;
+        if (null != service && null != (ses = service.getAnyActiveSessionForUser(target.getUserId(), target.getContext().getContextId()))) {
+            session = ServerSessionAdapter.valueOf(ses, target.getContext());
             params = null;
         } else {
-            final SessiondService service = SessiondService.SERVICE_REFERENCE.get();
-            Session ses = null;
-            if (null != service && null != (ses = service.getAnyActiveSessionForUser(target.getUserId(), target.getContext().getContextId()))) {
-                session = ServerSessionAdapter.valueOf(ses, target.getContext());
-                params = null;
-            } else {
-                session = null;
-                params = new HashMap<String, Object>(8);
-            }
+            session = null;
+            params = new HashMap<String, Object>(8);
         }
     }
 
