33ae47b2e43713cb7cd58deb50b5b7f9aac5b45a
Thorben Betten <thorben.betten@open-xchange.com>
2018-06-14 10:23:32 +0200

    JavaDoc

--- com.openexchange.hazelcast.serialization/src/com/openexchange/hazelcast/serialization/CustomPortable.java
+++ com.openexchange.hazelcast.serialization/src/com/openexchange/hazelcast/serialization/CustomPortable.java
@@ -136,6 +136,7 @@ public interface CustomPortable extends Portable {
      *   &bull; 23 ---> com.openexchange.session.reservation.impl.portable.PortableReservation
      *   &bull; 24 ---> com.openexchange.sessiond.serialization.PortableUserSessionsCleaner
      *   &bull; 25 ---> com.openexchange.sessiond.serialization.PortableSessionFilterApplier
+     *   &bull; 26 ---> com.openexchange.ms.internal.portable.PortableContextInvalidationCallable
      *   &bull; 101 --> com.openexchange.push.impl.credstorage.inmemory.portable.PortableCredentials
      *   &bull; 102 --> com.openexchange.push.impl.portable.PortablePushUser
      *   &bull; 103 --> com.openexchange.push.impl.balancing.reschedulerpolicy.portable.PortableCheckForExtendedServiceCallable
