0801009daee3049954589ab63a0ec87f4583ae66
Vitali Sjablow <vitali.sjablow@open-xchange.com>
2018-06-20 13:00:46 +0200

    Added null guard to RcurrenceData gathering.

--- com.openexchange.chronos.storage.rdb/src/com/openexchange/chronos/storage/rdb/legacy/StoredEvent.java
+++ com.openexchange.chronos.storage.rdb/src/com/openexchange/chronos/storage/rdb/legacy/StoredEvent.java
@@ -175,7 +175,12 @@ public class StoredEvent extends DelegatingEvent {
                 recurrenceData = (RecurrenceData) recurrenceId;
             } else {
                 recurrenceData = eventStorage.selectRecurrenceData(connection, asInt(delegate.getSeriesId()), false);
+                // no recurrence data found for the events series, maybe already deleted?
+                if (null == recurrenceData) {
+                    return null;
+                }
             }
+            
             int recurrencePosition = Event2Appointment.getRecurrencePosition(services.getService(RecurrenceService.class), recurrenceData, recurrenceId);
             return new StoredRecurrenceId(recurrencePosition);
         } catch (OXException | SQLException e) {
