8e526f7c1df926711c6dd8fbf0f8616be4422233
Thorben Betten <thorben.betten@open-xchange.com>
2018-08-10 09:07:23 +0200

    Fix for bug 59753: Accept full-name for special "INBOX" folder as advertised by Yahoo IMAP server

--- com.openexchange.imap/src/com/openexchange/imap/cache/ListLsubEntry.java
+++ com.openexchange.imap/src/com/openexchange/imap/cache/ListLsubEntry.java
@@ -110,6 +110,15 @@ public interface ListLsubEntry {
     String getFullName();
 
     /**
+     * Gets the original full name (present in case the full name advertised by IMAP server is different from chosen one)
+     *
+     * @return The original full name or <code>null</code>
+     */
+    default String optOriginalFullName() {
+        return null;
+    }
+
+    /**
      * Gets the attributes
      *
      * @return The attributes
