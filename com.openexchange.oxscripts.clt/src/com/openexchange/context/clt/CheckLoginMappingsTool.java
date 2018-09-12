d2c731487442541c97c19c2b3b8ee02591b546c4
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-09-12 13:30:34 +0200

    MW-983: adjusted the CheckLoginMappingsTool

--- com.openexchange.oxscripts.clt/src/com/openexchange/context/clt/CheckLoginMappingsTool.java
+++ com.openexchange.oxscripts.clt/src/com/openexchange/context/clt/CheckLoginMappingsTool.java
@@ -49,54 +49,29 @@
 
 package com.openexchange.context.clt;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.HashMap;
-import java.util.Map;
-import javax.management.MBeanException;
-import javax.management.MBeanServerConnection;
-import javax.management.MBeanServerInvocationHandler;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXServiceURL;
+import java.rmi.RemoteException;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
-import com.openexchange.auth.mbean.AuthenticatorMBean;
-import com.openexchange.context.mbean.ContextMBean;
+import com.openexchange.auth.rmi.RemoteAuthenticator;
+import com.openexchange.cli.AbstractRmiCLI;
+import com.openexchange.context.rmi.ContextRMIService;
 
 /**
  * {@link CheckLoginMappingsTool} - Serves <code>checkloginmappings</code> command-line tool.
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
  */
-public final class CheckLoginMappingsTool {
+public final class CheckLoginMappingsTool extends AbstractRmiCLI<Void> {
 
-    private static final Options sOptions;
-    static {
-        sOptions = new Options();
-        sOptions.addOption("h", "help", false, "Prints a help text");
-        sOptions.addOption("c", "context", true, "Required. The context identifier");
-        sOptions.addOption("a", "all", false, "Required. The flag to signal that contexts shall be processed. Hence option -c/--context is then obsolete.");
+    private static final String SYNTAX = "checkloginmappings [[[-c <contextId>] | [-a]] [-A <masterAdmin | contextAdmin> -P <masterAdminPassword | contextAdminPassword> [-p <RMI-Port>] [-s <RMI-Server]]] | [-h]";
+    private static final String FOOTER = "\n\nThe options -c and -a are mutually exclusive.";
 
-        sOptions.addOption("H", "host", true, "The optional JMX host (default:localhost)");
-        sOptions.addOption("p", "port", true, "The optional JMX port (default:9999)");
-        sOptions.addOption("l", "login", true, "The optional JMX login (if JMX has authentication enabled)");
-        sOptions.addOption("s", "password", true, "The optional JMX password (if JMX has authentication enabled)");
-        sOptions.addOption(new Option(null, "responsetimeout", true, "The optional response timeout in seconds when reading data from server (default: 0s; infinite)"));
-
-        sOptions.addOption("A", "adminuser", true, "Admin username. In case -a/--all is provided master administrator's user name is required; else the one for context administrator");
-        sOptions.addOption("P", "adminpass", true, "Admin password. In case -a/--all is provided master administrator's password is required; else the one for context administrator");
-    }
+    private Integer contextId;
 
     /**
-     * Prevent instantiation.
+     * Prevent instantiation from outside.
      */
     private CheckLoginMappingsTool() {
         super();
@@ -108,205 +83,125 @@ public final class CheckLoginMappingsTool {
      * @param args program arguments
      */
     public static void main(String[] args) {
-        CommandLineParser parser = new PosixParser();
-        boolean error = true;
-        boolean authFailed = false;
-        try {
-            final CommandLine cmd = parser.parse(sOptions, args);
-            if (cmd.hasOption('h')) {
-                printHelp();
-                System.exit(0);
-                return;
-            }
+        new CheckLoginMappingsTool().execute(args);
+    }
 
-            final String contextOptionVal;
-            if (cmd.hasOption('a')) {
-                contextOptionVal = null;
-            } else {
-                if (!cmd.hasOption('c')) {
-                    System.out.println("Either parameter 'context' or parameter 'all' is required.");
-                    printHelp();
-                    System.exit(1);
-                    return;
-                }
-                contextOptionVal = cmd.getOptionValue('c');
-            }
-
-            String host = "localhost";
-            if (cmd.hasOption('H')) {
-                String tmp = cmd.getOptionValue('H');
-                if (null != tmp) {
-                    host = tmp.trim();
-                }
-            }
-
-            int port = 9999;
-            if (cmd.hasOption('p')) {
-                final String val = cmd.getOptionValue('p');
-                if (null != val) {
-                    try {
-                        port = Integer.parseInt(val.trim());
-                    } catch (final NumberFormatException e) {
-                        System.err.println(new StringBuilder("Port parameter is not a number: ").append(val).toString());
-                        printHelp();
-                        System.exit(1);
-                    }
-                    if (port < 1 || port > 65535) {
-                        System.err.println(new StringBuilder("Port parameter is out of range: ").append(val).append(
-                            ". Valid range is from 1 to 65535.").toString());
-                        printHelp();
-                        System.exit(1);
-                    }
-                }
-            }
-
-            int responseTimeout = 0;
-            if (cmd.hasOption("responsetimeout")) {
-                final String val = cmd.getOptionValue('p');
-                if (null != val) {
-                    try {
-                        responseTimeout = Integer.parseInt(val.trim());
-                    } catch (final NumberFormatException e) {
-                        System.err.println("responsetimeout parameter is not a number: " + val);
-                        printHelp();
-                        System.exit(1);
-                    }
-                }
-            }
-
-            if (!cmd.hasOption('A')) {
-                System.out.println("You must provide administrative credentials to proceed.");
-                printHelp();
-                System.exit(-1);
-                return;
-            }
-            if (!cmd.hasOption('P')) {
-                System.out.println("You must provide administrative credentials to proceed.");
-                printHelp();
-                System.exit(-1);
-                return;
-            }
-
-            if (responseTimeout > 0) {
-                /*
-                 * The value of this property represents the length of time (in milliseconds) that the client-side Java RMI runtime will
-                 * use as a socket read timeout on an established JRMP connection when reading response data for a remote method invocation.
-                 * Therefore, this property can be used to impose a timeout on waiting for the results of remote invocations;
-                 * if this timeout expires, the associated invocation will fail with a java.rmi.RemoteException.
-                 *
-                 * Setting this property should be done with due consideration, however, because it effectively places an upper bound on the
-                 * allowed duration of any successful outgoing remote invocation. The maximum value is Integer.MAX_VALUE, and a value of
-                 * zero indicates an infinite timeout. The default value is zero (no timeout).
-                 */
-                System.setProperty("sun.rmi.transport.tcp.responseTimeout", Integer.toString(responseTimeout * 1000));
-            }
-
-            final String login = cmd.getOptionValue('A');
-            final String password = cmd.getOptionValue('P');
-
-            String jmxLogin = null;
-            if (cmd.hasOption('l')) {
-                jmxLogin = cmd.getOptionValue('l');
-            }
-            String jmxPassword = null;
-            if (cmd.hasOption('s')) {
-                jmxPassword = cmd.getOptionValue('s');
-            }
-
-            // Environment
-            final Map<String, Object> environment;
-            if (jmxLogin == null || jmxPassword == null) {
-                environment = null;
-            } else {
-                environment = new HashMap<String, Object>(1);
-                String[] creds = new String[] { jmxLogin, jmxPassword };
-                environment.put(JMXConnector.CREDENTIALS, creds);
-            }
-
-            // Invoke MBean
-            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + host + ":" + port + "/server");
-            JMXConnector jmxConnector = JMXConnectorFactory.connect(url, environment);
-            try {
-                final MBeanServerConnection mbsc = jmxConnector.getMBeanServerConnection();
-                try {
-                    final AuthenticatorMBean authenticator = authenticatorMBean(mbsc);
-                    final ContextMBean contextMBean = contextMBean(mbsc);
-
-                    if (null == contextOptionVal) {
-                        try {
-                            authenticator.doAuthentication(login, password);
-                        } catch (final MBeanException e) {
-                            authFailed = true;
-                            throw e;
-                        }
-                        contextMBean.checkLogin2ContextMapping();
-                        System.out.println("All cache entries cleared.");
-                    } else {
-                        final int contextId = Integer.parseInt(contextOptionVal.trim());
-                        try {
-                            authenticator.doAuthentication(login, password, contextId);
-                        } catch (final MBeanException e) {
-                            authFailed = true;
-                            throw e;
-                        }
-                        contextMBean.checkLogin2ContextMapping(contextId);
-                        System.out.println("All cache entries cleared for context " + contextId);
-                    }
-                } catch (final MBeanException e) {
-                    final String errMsg = e.getMessage();
-                    System.out.println(errMsg == null ? "An error occurred." : errMsg);
-                } catch (final Exception e) {
-                    final String errMsg = e.getMessage();
-                    System.out.println(errMsg == null ? "An error occurred." : errMsg);
-                }
-            } finally {
-                try {
-                    jmxConnector.close();
-                } catch (Exception e) {
-                    // Ignore
-                }
-            }
-            error = false;
-        } catch (ParseException e) {
-            System.err.println("Unable to parse command line: " + e.getMessage());
-            printHelp();
-        } catch (MalformedURLException e) {
-            System.err.println("URL to connect to server is invalid: " + e.getMessage());
-        } catch (IOException e) {
-            System.err.println("Unable to communicate with the server: " + e.getMessage());
-        } finally {
-            if (error) {
-                System.exit(1);
-            } else if (authFailed) {
-                System.exit(-1);
-            }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#administrativeAuth(java.lang.String, java.lang.String, org.apache.commons.cli.CommandLine, com.openexchange.auth.rmi.RemoteAuthenticator)
+     */
+    @Override
+    protected void administrativeAuth(String login, String password, CommandLine cmd, RemoteAuthenticator authenticator) throws RemoteException {
+        if (contextId == null) {
+            authenticator.doAuthentication(login, password);
+        } else {
+            authenticator.doAuthentication(login, password, contextId.intValue());
         }
     }
 
-    private static void printHelp() {
-        HelpFormatter helpFormatter = new HelpFormatter();
-        helpFormatter.printHelp("checkloginmappings", sOptions);
-    }
-
-    private static ContextMBean contextMBean(MBeanServerConnection mbsc) throws MalformedObjectNameException {
-        return MBeanServerInvocationHandler.newProxyInstance(mbsc, getObjectName(ContextMBean.class.getName(), ContextMBean.DOMAIN), ContextMBean.class, false);
-    }
-
-    private static AuthenticatorMBean authenticatorMBean(MBeanServerConnection mbsc) throws MalformedObjectNameException {
-        return MBeanServerInvocationHandler.newProxyInstance(mbsc, getObjectName(AuthenticatorMBean.class.getName(), AuthenticatorMBean.DOMAIN), AuthenticatorMBean.class, false);
-    }
-
-    /**
-     * Creates an appropriate instance of {@link ObjectName} from specified class name and domain name.
-     *
-     * @param className The class name to use as object name
-     * @param domain The domain name
-     * @return An appropriate instance of {@link ObjectName}
-     * @throws MalformedObjectNameException If instantiation of {@link ObjectName} fails
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#addOptions(org.apache.commons.cli.Options)
      */
-    private static ObjectName getObjectName(final String className, final String domain) throws MalformedObjectNameException {
-        final int pos = className.lastIndexOf('.');
-        return new ObjectName(domain, "name", pos == -1 ? className : className.substring(pos + 1));
+    @Override
+    protected void addOptions(Options options) {
+        OptionGroup group = new OptionGroup();
+        group.addOption(createOption("c", "context", true, "Required. The context identifier", true));
+        group.addOption(createOption("a", "all", false, "Required. The flag to signal that contexts shall be processed. Hence option -c/--context is then obsolete.", true));
+        options.addOptionGroup(group);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#invoke(org.apache.commons.cli.Options, org.apache.commons.cli.CommandLine, java.lang.String)
+     */
+    @Override
+    protected Void invoke(Options options, CommandLine cmd, String optRmiHostName) throws Exception {
+        boolean error = true;
+        try {
+            ContextRMIService contextRMI = getRmiStub(optRmiHostName, ContextRMIService.RMI_NAME);
+            if (null == contextId) {
+                contextRMI.checkLogin2ContextMapping();
+                System.out.println("All cache entries cleared.");
+            } else {
+                contextRMI.checkLogin2ContextMapping(contextId.intValue());
+                System.out.println("All cache entries cleared for context " + contextId.intValue());
+            }
+            error = false;
+        } catch (final RemoteException e) {
+            final String errMsg = e.getMessage();
+            System.out.println(errMsg == null ? "An error occurred." : errMsg);
+        } catch (final Exception e) {
+            final String errMsg = e.getMessage();
+            System.out.println(errMsg == null ? "An error occurred." : errMsg);
+        } finally {
+            if (error) {
+                System.exit(1);
+            }
+        }
+        return null;
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractAdministrativeCLI#requiresAdministrativePermission()
+     */
+    @Override
+    protected boolean requiresAdministrativePermission() {
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractCLI#checkOptions(org.apache.commons.cli.CommandLine)
+     */
+    @Override
+    protected void checkOptions(CommandLine cmd) {
+        if (cmd.hasOption('a')) {
+            contextId = null;
+            return;
+        }
+
+        if (cmd.hasOption('c')) {
+            String contextVal = cmd.getOptionValue('c');
+            try {
+                contextId = Integer.parseInt(contextVal.trim());
+            } catch (NumberFormatException e) {
+                System.err.println("Cannot parse '" + contextVal + "' as a context id");
+                printHelp();
+                System.exit(1);
+            }
+            return;
+        }
+
+        System.out.println("Either parameter 'context' or parameter 'all' is required.");
+        printHelp();
+        System.exit(1);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractCLI#getFooter()
+     */
+    @Override
+    protected String getFooter() {
+        return FOOTER;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractCLI#getName()
+     */
+    @Override
+    protected String getName() {
+        return SYNTAX;
+    }
 }
