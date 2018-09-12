115882d9209090c06732ca66b3c2b5acb4083b10
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-09-12 15:33:50 +0200

    MW-983: adjusted the get missing services clt

--- com.openexchange.oxscripts.clt/src/com/openexchange/osgi/clt/DeferredActivatorCLT.java
+++ com.openexchange.oxscripts.clt/src/com/openexchange/osgi/clt/DeferredActivatorCLT.java
@@ -49,39 +49,32 @@
 
 package com.openexchange.osgi.clt;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanException;
-import javax.management.MBeanServerConnection;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-import javax.management.ReflectionException;
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXServiceURL;
+import java.util.Map.Entry;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
+import com.openexchange.auth.rmi.RemoteAuthenticator;
+import com.openexchange.cli.AbstractRmiCLI;
 import com.openexchange.exception.OXException;
-import com.openexchange.java.Streams;
-import com.openexchange.osgi.mbean.DeferredActivatorMBean;
-import com.openexchange.osgi.mbean.DeferredActivatorMBeanImpl;
+import com.openexchange.osgi.rmi.DeferredActivatorRMIService;
 
 /**
- * {@link DeferredActivatorCLT}
+ * {@link DeferredActivatorCLT} - Servces the <code>getmissingservices</code> command line tool
  *
  * @author <a href="mailto:jan.bauerdick@open-xchange.com">Jan Bauerdick</a>
+ * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
  */
-public class DeferredActivatorCLT {
+public class DeferredActivatorCLT extends AbstractRmiCLI<Void> {
+
+    private static String SYNTAX = "servicestate [[[-n <bundleName>] [-A <masterAdmin | contextAdmin> -P <masterAdminPassword | contextAdminPassword> [-p <RMI-Port>] [-s <RMI-Server]]]] | [-h]";
+    private static String FOOTER = "";
+
+    private boolean testAll;
+    private String bundleName;
 
     /**
      * Initializes a new {@link DeferredActivatorCLT}.
@@ -90,177 +83,59 @@ public class DeferredActivatorCLT {
         super();
     }
 
-    public static void main(final String[] args) {
-        final CommandLineParser parser = new PosixParser();
-        String bundleName = null;
-        boolean testAll = true;
+    /**
+     * entry point
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        new DeferredActivatorCLT().execute(args);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#administrativeAuth(java.lang.String, java.lang.String, org.apache.commons.cli.CommandLine, com.openexchange.auth.rmi.RemoteAuthenticator)
+     */
+    @Override
+    protected void administrativeAuth(String login, String password, CommandLine cmd, RemoteAuthenticator authenticator) throws RemoteException {
+        authenticator.doAuthentication(login, password);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#addOptions(org.apache.commons.cli.Options)
+     */
+    @Override
+    protected void addOptions(Options options) {
+        options.addOption("n", "name", true, "The optional bundle's symbolic name");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractRmiCLI#invoke(org.apache.commons.cli.Options, org.apache.commons.cli.CommandLine, java.lang.String)
+     */
+    @Override
+    protected Void invoke(Options options, CommandLine cmd, String optRmiHostName) throws Exception {
         try {
-            final CommandLine cmd = parser.parse(toolkitOptions, args);
-            if (cmd.hasOption('h')) {
-                printHelp();
-                System.exit(0);
-            }
-            String host = "localhost";
-            if (cmd.hasOption('H')) {
-                String tmp = cmd.getOptionValue('H');
-                if (null != tmp) {
-                    host = tmp.trim();
-                }
-            }
-            int port = 9999;
-            if (cmd.hasOption('p')) {
-                final String val = cmd.getOptionValue('p');
-                if (null != val) {
-                    try {
-                        port = Integer.parseInt(val.trim());
-                    } catch (final NumberFormatException e) {
-                        System.err.println(new StringBuilder("Port parameter is not a number: ").append(val).toString());
-                        printHelp();
-                        System.exit(3);
-                    }
-                    if (port < 1 || port > 65535) {
-                        System.err.println(new StringBuilder("Port parameter is out of range: ").append(val).append(
-                            ". Valid range is from 1 to 65535.").toString());
-                        printHelp();
-                        System.exit(3);
-                    }
-                }
-            }
-
-            int responseTimeout = 0;
-            if (cmd.hasOption("responsetimeout")) {
-                final String val = cmd.getOptionValue('p');
-                if (null != val) {
-                    try {
-                        responseTimeout = Integer.parseInt(val.trim());
-                    } catch (final NumberFormatException e) {
-                        System.err.println("responsetimeout parameter is not a number: " + val);
-                        printHelp();
-                        System.exit(1);
-                    }
-                }
-            }
-
-            if (cmd.hasOption('n')) {
-                bundleName = cmd.getOptionValue('n');
-                testAll = false;
-            }
-
-            if (responseTimeout > 0) {
-                /*
-                 * The value of this property represents the length of time (in milliseconds) that the client-side Java RMI runtime will
-                 * use as a socket read timeout on an established JRMP connection when reading response data for a remote method invocation.
-                 * Therefore, this property can be used to impose a timeout on waiting for the results of remote invocations;
-                 * if this timeout expires, the associated invocation will fail with a java.rmi.RemoteException.
-                 *
-                 * Setting this property should be done with due consideration, however, because it effectively places an upper bound on the
-                 * allowed duration of any successful outgoing remote invocation. The maximum value is Integer.MAX_VALUE, and a value of
-                 * zero indicates an infinite timeout. The default value is zero (no timeout).
-                 */
-                System.setProperty("sun.rmi.transport.tcp.responseTimeout", Integer.toString(responseTimeout * 1000));
-            }
-
-            String jmxLogin = null;
-            if (cmd.hasOption('l')) {
-                jmxLogin = cmd.getOptionValue('l');
-            }
-            String jmxPassword = null;
-            if (cmd.hasOption('s')) {
-                jmxPassword = cmd.getOptionValue('s');
-            }
-
-            final Map<String, Object> environment;
-            if (jmxLogin == null || jmxPassword == null) {
-                environment = null;
+            if (testAll) {
+                listAllMissingServices(optRmiHostName);
             } else {
-                environment = new HashMap<String, Object>(1);
-                String[] creds = new String[] { jmxLogin, jmxPassword };
-                environment.put(JMXConnector.CREDENTIALS, creds);
+                listMissingServicesForBundle(optRmiHostName);
             }
-
-            final JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + host + ":" + port + "/server");
-            final JMXConnector jmxConnector = JMXConnectorFactory.connect(url, environment);
-            try {
-                final MBeanServerConnection mbsc = jmxConnector.getMBeanServerConnection();
-
-                if (testAll) {
-                    final Object result = mbsc.invoke(
-                        getObjectName(DeferredActivatorMBeanImpl.class.getName(), DeferredActivatorMBean.OSGI_DOMAIN),
-                        "listAllMissingServices",
-                        null,
-                        null);
-                    if (result instanceof Map<?, ?>) {
-                        if (((Map) result).isEmpty()) {
-                            System.out.println("No services missing");
-                            System.exit(0);
-                        }
-                        for (Object bundle : ((Map) result).keySet()) {
-                            Object list = ((Map) result).get(bundle);
-                            if (list instanceof List<?>) {
-                                System.out.println("Services missing for bundle " + bundle.toString() + ":");
-                                final StringBuilder sb = new StringBuilder();
-                                for (final Object o : (List) list) {
-                                    sb.append(o.toString()).append(',');
-                                }
-                                sb.deleteCharAt(sb.length() - 1);
-                                System.out.println(sb.toString());
-                            }
-                            System.out.println();
-                        }
-                        System.exit(2);
-                    }
-                } else {
-                    final String[] signature = new String[] { String.class.getName() };
-                    final Object[] params = new Object[] { bundleName };
-                    final Object result = mbsc.invoke(
-                        getObjectName(DeferredActivatorMBeanImpl.class.getName(), DeferredActivatorMBean.OSGI_DOMAIN),
-                        "listMissingServices",
-                        params,
-                        signature);
-                    if (result instanceof ArrayList<?>) {
-                        final ArrayList<?> missing = (ArrayList<?>) result;
-                        if (missing.isEmpty()) {
-                            System.out.println("No services missing for bundle " + bundleName);
-                            System.exit(0);
-                        } else {
-                            System.out.println("Services missing for bundle " + bundleName + ":");
-                            final StringBuilder sb = new StringBuilder();
-                            for (final Object o : missing) {
-                                sb.append(o.toString()).append(',');
-                            }
-                            sb.deleteCharAt(sb.length() - 1);
-                            System.out.println(sb.toString());
-                            System.exit(2);
-                        }
-                    }
-                }
-            } finally {
-                Streams.close(jmxConnector);
-            }
-        } catch (final MalformedObjectNameException e) {
-            // Cannot occur
-            System.err.println("Invalid MBean name: " + e.getMessage());
-            System.exit(3);
-        } catch (final ParseException e) {
-            System.err.println("Unable to parse command line: " + e.getMessage());
-            printHelp();
-        } catch (final MalformedURLException e) {
+        } catch (MalformedURLException e) {
             System.err.println("URL to connect to server is invalid: " + e.getMessage());
             System.exit(3);
-        } catch (final IOException e) {
-            System.err.println("Unable to communicate with the server: " + e.getMessage());
-            System.exit(3);
-        } catch (final InstanceNotFoundException e) {
-            System.err.println("Instance is not available: " + e.getMessage());
-            System.exit(3);
-        } catch (final MBeanException e) {
-            final Throwable t = e.getCause();
-            final String message;
+        } catch (RemoteException e) {
+            Throwable t = e.getCause();
+            String message;
             if (null == t) {
                 message = e.getMessage();
             } else {
                 if ((t instanceof OXException)) {
-                    final OXException oxe = (OXException) t;
+                    OXException oxe = (OXException) t;
                     if ("CTX".equals(oxe.getPrefix())) {
                         message = "Cannot find bundle " + bundleName;
                     } else {
@@ -272,47 +147,105 @@ public class DeferredActivatorCLT {
             }
             System.err.println(null == message ? "Unexpected error." : "Unexpected error: " + message);
             System.exit(3);
-        } catch (final ReflectionException e) {
-            System.err.println("Problem with reflective type handling: " + e.getMessage());
-            System.exit(3);
-        } catch (final RuntimeException e) {
+        } catch (RuntimeException e) {
             System.err.println("Problem in runtime: " + e.getMessage());
             printHelp();
             System.exit(3);
         }
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractAdministrativeCLI#requiresAdministrativePermission()
+     */
+    @Override
+    protected boolean requiresAdministrativePermission() {
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractCLI#checkOptions(org.apache.commons.cli.CommandLine)
+     */
+    @Override
+    protected void checkOptions(CommandLine cmd) {
+        if (cmd.hasOption('n')) {
+            bundleName = cmd.getOptionValue('n');
+            testAll = false;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractCLI#getFooter()
+     */
+    @Override
+    protected String getFooter() {
+        return FOOTER;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.openexchange.cli.AbstractCLI#getName()
+     */
+    @Override
+    protected String getName() {
+        return SYNTAX;
     }
 
     /**
-     * Creates an appropriate instance of {@link ObjectName} from specified class name and domain name.
-     *
-     * @param className The class name to use as object name
-     * @param domain The domain name
-     * @return An appropriate instance of {@link ObjectName}
-     * @throws MalformedObjectNameException If instantiation of {@link ObjectName} fails
+     * Lists all missing services
+     * 
+     * @param rmiHostName The optional remote hostname
      */
-    private static ObjectName getObjectName(final String className, final String domain) throws MalformedObjectNameException {
-        final int pos = className.lastIndexOf('.');
-        return new ObjectName(domain, "name", pos == -1 ? className : className.substring(pos + 1));
+    private void listAllMissingServices(String rmiHostName) throws MalformedURLException, RemoteException, NotBoundException {
+        DeferredActivatorRMIService rmiService = getRmiStub(rmiHostName, DeferredActivatorRMIService.RMI_NAME);
+        Map<String, List<String>> allServices = rmiService.listAllMissingServices();
+        if (allServices.isEmpty()) {
+            System.out.println("No services missing");
+            System.exit(0);
+        }
+        for (Entry<String, List<String>> bundle : allServices.entrySet()) {
+            printMissingServicesForBundle(bundle.getKey(), bundle.getValue());
+            System.out.println();
+        }
+        System.exit(2);
     }
 
-    private static final Options toolkitOptions;
-
-    static {
-        toolkitOptions = new Options();
-        toolkitOptions.addOption("h", "help", false, "Prints a help text");
-
-        toolkitOptions.addOption("n", "name", true, "The optional bundle's symbolic name");
-
-        toolkitOptions.addOption("H", "host", true, "The optional JMX host (default:localhost)");
-        toolkitOptions.addOption("p", "port", true, "The optional JMX port (default:9999)");
-        toolkitOptions.addOption("l", "login", true, "The optional JMX login (if JMX has authentication enabled)");
-        toolkitOptions.addOption("s", "password", true, "The optional JMX password (if JMX has authentication enabled)");
-        toolkitOptions.addOption(new Option(null, "responsetimeout", true, "The optional response timeout in seconds when reading data from server (default: 0s; infinite)"));
+    /**
+     * Lists all missing services for the specified bundle
+     * 
+     * @param optRmiHostName The optional remote hostname
+     */
+    private void listMissingServicesForBundle(String rmiHostName) throws MalformedURLException, RemoteException, NotBoundException {
+        DeferredActivatorRMIService rmiService = getRmiStub(rmiHostName, DeferredActivatorRMIService.RMI_NAME);
+        List<String> missingServices = rmiService.listMissingServices(bundleName);
+        if (missingServices.isEmpty()) {
+            System.out.println("No services missing for bundle " + bundleName);
+            System.exit(0);
+        }
+        printMissingServicesForBundle(bundleName, missingServices);
+        System.exit(2);
     }
 
-    private static void printHelp() {
-        final HelpFormatter helpFormatter = new HelpFormatter();
-        helpFormatter.printHelp("servicestate", toolkitOptions);
+    /**
+     * Prints the missing services for the specified bundle
+     * 
+     * @param bundleName The bundle's name
+     * @param missingServices the missing services
+     */
+    private void printMissingServicesForBundle(String bundleName, List<String> missingServices) {
+        System.out.println("Services missing for bundle " + bundleName + ":");
+        StringBuilder sb = new StringBuilder();
+        for (String o : missingServices) {
+            sb.append(o.toString()).append(',');
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb.toString());
     }
-
 }
