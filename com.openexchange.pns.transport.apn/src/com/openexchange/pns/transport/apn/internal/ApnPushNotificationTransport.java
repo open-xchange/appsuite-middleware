da6495c3b7e758735d3760dbbe951be12ec3c36e
Thorben Betten <thorben.betten@open-xchange.com>
2018-09-28 15:25:53 +0200

    Fix for bug 60527: Prefer using 'javapns.notification.PushNotificationBigPayload' to have bigger size restrictions
    (cherry picked from commit a9ab50235f839312c85b4ac8e5bf721d06af5aa3)

--- com.openexchange.pns.transport.apn/src/com/openexchange/pns/transport/apn/internal/ApnPushNotificationTransport.java
+++ com.openexchange.pns.transport.apn/src/com/openexchange/pns/transport/apn/internal/ApnPushNotificationTransport.java
@@ -95,6 +95,7 @@ import javapns.devices.Device;
 import javapns.devices.exceptions.InvalidDeviceTokenFormatException;
 import javapns.notification.Payload;
 import javapns.notification.PayloadPerDevice;
+import javapns.notification.PushNotificationBigPayload;
 import javapns.notification.PushNotificationPayload;
 import javapns.notification.PushedNotification;
 import javapns.notification.PushedNotifications;
@@ -375,7 +376,7 @@ public class ApnPushNotificationTransport extends ServiceTracker<ApnOptionsProvi
     }
 
     private Payload toPayload(Map<String, Object> message) throws JSONException {
-        PushNotificationPayload payload = new PushNotificationPayload();
+        PushNotificationPayload payload = new PushNotificationBigPayload();
 
         Map<String, Object> source = new HashMap<>(message);
         {
