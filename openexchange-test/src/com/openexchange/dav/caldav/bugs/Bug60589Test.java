ce0c22b6d5006f87f11cb61c9fde4aa013753aa2
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2018-10-01 11:28:48 +0200

    also test update with new resource attendee

--- openexchange-test/src/com/openexchange/dav/caldav/bugs/Bug60589Test.java
+++ openexchange-test/src/com/openexchange/dav/caldav/bugs/Bug60589Test.java
@@ -56,6 +56,8 @@ import org.junit.Test;
 import com.openexchange.chronos.ResourceId;
 import com.openexchange.dav.StatusCodes;
 import com.openexchange.dav.caldav.CalDAVTest;
+import com.openexchange.dav.caldav.ICalResource;
+import com.openexchange.dav.caldav.ical.SimpleICal.Property;
 import com.openexchange.groupware.calendar.TimeTools;
 import com.openexchange.groupware.container.Appointment;
 import com.openexchange.groupware.container.Participant;
@@ -143,6 +145,88 @@ public class Bug60589Test extends CalDAVTest {
     }
 
     @Test
+    public void testUpdateWithInternalResource() throws Exception {
+        /*
+         * lookup internal resource for test
+         */
+        Resource resource = resTm.search(testContext.getResourceParticipants().get(0)).get(0);
+        String resourceUri = ResourceId.forResource(getClient().getValues().getContextId(), resource.getIdentifier());
+        /*
+         * create appointment
+         */
+        String uid = randomUID();
+        Date start = TimeTools.D("tomorrow at 15:30");
+        Date end = TimeTools.D("tomorrow at 16:00");
+        String iCal = // @formatter:off
+            "BEGIN:VCALENDAR\r\n" +
+            "VERSION:2.0\r\n" +
+            "PRODID:-//Apple Inc.//Mac OS X 10.8.5//EN\r\n" +
+            "CALSCALE:GREGORIAN\r\n" +
+            "BEGIN:VTIMEZONE\r\n" +
+            "TZID:Europe/Berlin\r\n" +
+            "BEGIN:DAYLIGHT\r\n" +
+            "TZOFFSETFROM:+0100\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n" +
+            "DTSTART:19810329T020000\r\n" +
+            "TZNAME:MESZ\r\n" +
+            "TZOFFSETTO:+0200\r\n" +
+            "END:DAYLIGHT\r\n" +
+            "BEGIN:STANDARD\r\n" +
+            "TZOFFSETFROM:+0200\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n" +
+            "DTSTART:19961027T030000\r\n" +
+            "TZNAME:MEZ\r\n" +
+            "TZOFFSETTO:+0100\r\n" +
+            "END:STANDARD\r\n" +
+            "END:VTIMEZONE\r\n" +
+            "BEGIN:VEVENT\r\n" +
+            "DTSTART;TZID=Europe/Berlin:" + format(start, "Europe/Berlin") + "\r\n" +
+            "DTEND;TZID=Europe/Berlin:" + format(end, "Europe/Berlin") + "\r\n" +
+            "TRANSP:OPAQUE\r\n" +
+            "UID:" + uid + "\r\n" +
+            "DTSTAMP:" + formatAsUTC(new Date()) + "\r\n" +
+            "CLASS:PUBLIC\r\n" +
+            "SUMMARY:Test\r\n" +
+            "LAST-MODIFIED:" + formatAsUTC(new Date()) + "\r\n" +
+            "CREATED:" + formatAsUTC(new Date()) + "\r\n" +
+            "ORGANIZER:mailto:" + getClient().getValues().getDefaultAddress() + "\r\n" +
+            "ATTENDEE;PARTSTAT=ACCEPTED:mailto:" + getClient().getValues().getDefaultAddress() + "\r\n" +
+            "END:VEVENT\r\n" +
+            "END:VCALENDAR\r\n"
+        ; // @formatter:on
+        assertEquals("response code wrong", StatusCodes.SC_CREATED, putICal(uid, iCal));
+        /*
+         * verify appointment on server
+         */
+        Appointment appointment = getAppointment(uid);
+        assertNotNull("appointment not found on server", appointment);
+        rememberForCleanUp(appointment);
+        assertEquals("wrong number of participants", 1, appointment.getParticipants().length);
+        /*
+         * update appointment on client & add resource
+         */
+        ICalResource iCalResource = get(uid);
+        iCalResource.getVEvent().getProperties().add(new Property("ATTENDEE;CUTYPE=RESOURCE;PARTSTAT=NEEDS-ACTION:" + resourceUri));
+        assertEquals("response code wrong", StatusCodes.SC_CREATED, putICalUpdate(iCalResource));
+        /*
+         * verify appointment on server
+         */
+        appointment = getAppointment(uid);
+        assertNotNull("appointment not found on server", appointment);
+        rememberForCleanUp(appointment);
+        assertEquals("wrong number of participants", 2, appointment.getParticipants().length);
+        Participant resourceParticipant = null;
+        for (Participant participant : appointment.getParticipants()) {
+            if (Participant.RESOURCE == participant.getType()) {
+                resourceParticipant = participant;
+                break;
+            }
+        }
+        assertNotNull("Resource participant not found in appointment", resourceParticipant);
+        assertEquals("Internal resource not resolved", resource.getIdentifier(), resourceParticipant.getIdentifier());
+    }
+
+    @Test
     public void testCreateWithForeignResource() throws Exception {
         /*
          * create appointment
@@ -197,6 +281,83 @@ public class Bug60589Test extends CalDAVTest {
         rememberForCleanUp(appointment);
         assertEquals("wrong number of participants", 2, appointment.getParticipants().length);
 
+        Participant resourceParticipant = null;
+        for (Participant participant : appointment.getParticipants()) {
+            if (Participant.RESOURCE == participant.getType()) {
+                resourceParticipant = participant;
+                break;
+            }
+        }
+        assertNotNull("Resource participant not found in appointment", resourceParticipant);
+        assertEquals("Internal identifier for foreign resource participant", 0, resourceParticipant.getIdentifier());
+    }
+
+    @Test
+    public void testUpdateWithForeignResource() throws Exception {
+        /*
+         * create appointment
+         */
+        String uid = randomUID();
+        Date start = TimeTools.D("tomorrow at 15:30");
+        Date end = TimeTools.D("tomorrow at 16:00");
+        String iCal = // @formatter:off
+            "BEGIN:VCALENDAR\r\n" +
+            "VERSION:2.0\r\n" +
+            "PRODID:-//Apple Inc.//Mac OS X 10.8.5//EN\r\n" +
+            "CALSCALE:GREGORIAN\r\n" +
+            "BEGIN:VTIMEZONE\r\n" +
+            "TZID:Europe/Berlin\r\n" +
+            "BEGIN:DAYLIGHT\r\n" +
+            "TZOFFSETFROM:+0100\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\n" +
+            "DTSTART:19810329T020000\r\n" +
+            "TZNAME:MESZ\r\n" +
+            "TZOFFSETTO:+0200\r\n" +
+            "END:DAYLIGHT\r\n" +
+            "BEGIN:STANDARD\r\n" +
+            "TZOFFSETFROM:+0200\r\n" +
+            "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\n" +
+            "DTSTART:19961027T030000\r\n" +
+            "TZNAME:MEZ\r\n" +
+            "TZOFFSETTO:+0100\r\n" +
+            "END:STANDARD\r\n" +
+            "END:VTIMEZONE\r\n" +
+            "BEGIN:VEVENT\r\n" +
+            "DTSTART;TZID=Europe/Berlin:" + format(start, "Europe/Berlin") + "\r\n" +
+            "DTEND;TZID=Europe/Berlin:" + format(end, "Europe/Berlin") + "\r\n" +
+            "TRANSP:OPAQUE\r\n" +
+            "UID:" + uid + "\r\n" +
+            "DTSTAMP:" + formatAsUTC(new Date()) + "\r\n" +
+            "CLASS:PUBLIC\r\n" +
+            "SUMMARY:Test\r\n" +
+            "LAST-MODIFIED:" + formatAsUTC(new Date()) + "\r\n" +
+            "CREATED:" + formatAsUTC(new Date()) + "\r\n" +
+            "ORGANIZER:mailto:" + getClient().getValues().getDefaultAddress() + "\r\n" +
+            "ATTENDEE;PARTSTAT=ACCEPTED:mailto:" + getClient().getValues().getDefaultAddress() + "\r\n" +
+            "END:VEVENT\r\n" +
+            "END:VCALENDAR\r\n"
+        ; // @formatter:on
+        assertEquals("response code wrong", StatusCodes.SC_CREATED, putICal(uid, iCal));
+        /*
+         * verify appointment on server
+         */
+        Appointment appointment = getAppointment(uid);
+        assertNotNull("appointment not found on server", appointment);
+        rememberForCleanUp(appointment);
+        assertEquals("wrong number of participants", 1, appointment.getParticipants().length);
+        /*
+         * update appointment on client & add resource
+         */
+        ICalResource iCalResource = get(uid);
+        iCalResource.getVEvent().getProperties().add(new Property("ATTENDEE;CUTYPE=RESOURCE;PARTSTAT=NEEDS-ACTION;CN=Telefonkonferenzraum-78:urn:uuid:00000000-000a-0003-0000-0000000001c1"));
+        assertEquals("response code wrong", StatusCodes.SC_CREATED, putICalUpdate(iCalResource));
+        /*
+         * verify appointment on server
+         */
+        appointment = getAppointment(uid);
+        assertNotNull("appointment not found on server", appointment);
+        rememberForCleanUp(appointment);
+        assertEquals("wrong number of participants", 1, appointment.getParticipants().length);
         Participant resourceParticipant = null;
         for (Participant participant : appointment.getParticipants()) {
             if (Participant.RESOURCE == participant.getType()) {
