ee3ed5b6cac51e6cba4b7135b5e09dfdfef054f6
Ioannis Chouklis <ioannis.chouklis@open-xchange.com>
2018-10-15 13:46:26 +0200

    Follow-up for bug 60826: Use a SegmentedUpdateService to fetch the migration url from
    (cherry picked from commit ebbf138edaf9021825ca817059f450bea8bc5d77)

--- com.openexchange.server/src/com/openexchange/groupware/upgrade/SegmentedUpdateService.java
+++ com.openexchange.server/src/com/openexchange/groupware/upgrade/SegmentedUpdateService.java
@@ -47,60 +47,25 @@
  *
  */
 
-package com.openexchange.share;
+package com.openexchange.groupware.upgrade;
 
-import com.openexchange.config.lean.Property;
+import com.openexchange.exception.OXException;
 
 /**
- * {@link ShareProperty}
+ * {@link SegmentedUpdateService}
  *
  * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ * @since v7.10.1
  */
-public enum ShareProperty implements Property {
+public interface SegmentedUpdateService {
 
     /**
-     * <p>Specifies the redirect URI/URL during cluster migration to which a client
-     * is redirected in case is landed on an unsuitable node (running incompatible
-     * application code).</p>
-     * <p>E.g. a user gets routed to a node running application code in version X,
-     * but that account has already been migrated to application code in version Y.</p>
-     * <p>No default value</p>
-     */
-    migrationRedirectURL(ShareProperty.EMPTY),
-    ;
-
-    private static final String EMPTY = "";
-
-    private final Object defaultValue;
-
-    private static final String PREFIX = "com.openexchange.share.";
-
-    /**
-     * Initialises a new {@link ShareProperty}.
+     * Returns the configured migrationRedirectURL by consulting the configuration for defined hosts (as-config.yml)
+     * and falling back to server configuration (server.properties) if no URL was defined for the host.
      * 
-     * @param defaultValue The default value of the property
+     * @param host The host to get the migrationRedirectURL for
+     * @return The redirect URL for mentioned host (if configured) or <code>null</code> if no configuration can be found.
+     * @throws OXException if an error is occurred
      */
-    private ShareProperty(Object defaultValue) {
-        this.defaultValue = defaultValue;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.openexchange.config.lean.Property#getFQPropertyName()
-     */
-    @Override
-    public String getFQPropertyName() {
-        return PREFIX + name();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.openexchange.config.lean.Property#getDefaultValue()
-     */
-    @Override
-    public Object getDefaultValue() {
-        return defaultValue;
-    }
+    String getMigrationRedirectURL(String host) throws OXException;
 }
