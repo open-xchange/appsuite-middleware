824c843c593161661c97cf2fe0cd43f64ee23b7c
Thorben Betten <thorben.betten@open-xchange.com>
2018-11-15 11:30:46 +0100

    Proper initial capacity

--- com.openexchange.imap/src/com/openexchange/imap/threader/references/Conversations.java
+++ com.openexchange.imap/src/com/openexchange/imap/threader/references/Conversations.java
@@ -274,7 +274,7 @@ public final class Conversations {
                 final Response response = r[len];
                 if (response.isOK()) {
                     try {
-                        final List<MailMessage> mails = new ArrayList<MailMessage>(messageCount);
+                        final List<MailMessage> mails = new ArrayList<MailMessage>(r.length);
                         final String fullName = imapFolder.getFullName();
                         final String sInReplyTo = "In-Reply-To";
                         final String sReferences = "References";
