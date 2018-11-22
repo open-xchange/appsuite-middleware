b8eea49753046206130f18cc06e6d80b18a2917c
Benjamin Grüdelbach <benjamin.gruedelbach@open-xchange.com>
2018-11-22 12:24:45 +0100

    Added support for parsing multiple PGP keyrings from binary data

--- com.openexchange.pgp.keys/src/com/openexchange/pgp/keys/parsing/PGPPublicKeyRingFactory.java
+++ com.openexchange.pgp.keys/src/com/openexchange/pgp/keys/parsing/PGPPublicKeyRingFactory.java
@@ -3,6 +3,7 @@ package com.openexchange.pgp.keys.parsing;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import org.bouncycastle.openpgp.PGPObjectFactory;
@@ -19,27 +20,60 @@ public class PGPPublicKeyRingFactory {
     private static Logger logger = org.slf4j.LoggerFactory.getLogger(PGPPublicKeyRingFactory.class);
 
     /**
-     * Creates a PGPPublicKeyRing object from some ASCII-armored data
+     * Creates a collection for PGPPublicKeyRing objects from the given data
      *
-     * @param asciiKeyData the ASCII-armored data to create the key from
-     * @return the key created from the ASCII-armored data
-     * @throws IOException due an error
-     * @throws IllegalArgumentException if asciiKeyData does not contain a valid ASCII-armored key
+     * @param keyData The data
+     * @return A collection of PGPPublicKeyRing objects or an empty collection if the data do not contain any valid PGPPublicKeyRing objects.
+     * @throws IOException if the keyData contain other/invalid PGP objects
      */
-    public static PGPPublicKeyRing create(String asciiKeyData) throws IOException {
+    public static List<PGPPublicKeyRing> createAll(InputStream keyData) throws IOException {
+        List<PGPPublicKeyRing> ret = new ArrayList<PGPPublicKeyRing>();
         // PGPUtil.getDecoderStream() will detect ASCII-armor automatically and decode it,
         // the PGPObject factory then knows how to read all the data in the encoded stream
-        PGPObjectFactory factory = new PGPObjectFactory(PGPUtil.getDecoderStream(new ByteArrayInputStream(asciiKeyData.getBytes("UTF-8"))), new BcKeyFingerprintCalculator());
-        // these files should really just have one object in them,
-        // and that object should be a PGPPublicKeyRing.
-        Object o = factory.nextObject();
-        if (o instanceof PGPPublicKeyRing) {
-            return (PGPPublicKeyRing) o;
+        PGPObjectFactory factory = new PGPObjectFactory(PGPUtil.getDecoderStream(keyData), new BcKeyFingerprintCalculator());
+        Object object = null;
+        while ((object = factory.nextObject()) != null) {
+            if (object instanceof PGPPublicKeyRing) {
+                ret.add((PGPPublicKeyRing) object);
+            } else {
+                logger.error("Input data does not contain a PGP Public Key");
+            }
         }
-        else {
-            logger.error("Input text does not contain a PGP Public Key");
-            return null;
+        return ret;
+    }
+
+    /**
+     * Creates a single PGPPublicKeyRing object from the given data.
+     * <br>
+     * <br>
+     * If the data contain more than one key ring, the first one is returned.
+     * Use {@link PGPPublicKeyRingFactory#createAll(InputStream)} to parse all keys.
+     *
+     * @param keyData The data to create the key from
+     * @return the first key created from the data, or null if the PGP data do not contain at least one valid PGPPublicKeyRing
+     * @throws IOException due an error
+     */
+    public static PGPPublicKeyRing create(InputStream keyData) throws IOException {
+        List<PGPPublicKeyRing> keyRings = createAll(keyData);
+        if (!keyRings.isEmpty()) {
+            return keyRings.iterator().next();
         }
+        return null;
+    }
+
+    /**
+     * Creates a single PGPPublicKeyRing object from the given, ASCII armored, data.
+     * <br>
+     * <br>
+     * If the data contain more than one key ring, the first one is returned.
+     * Use {@link PGPPublicKeyRingFactory#createAll(InputStream)} to parse all keys.
+     *
+     * @param keyData The data to create the key from
+     * @return the first key created from the data, or null if the PGP data do not contain at least one valid PGPPublicKeyRing
+     * @throws IOException
+     */
+    public static PGPPublicKeyRing create(String keyData) throws IOException {
+        return create(new ByteArrayInputStream(keyData.getBytes("UTF-8")));
     }
 
     /**
@@ -54,7 +88,7 @@ public class PGPPublicKeyRingFactory {
         List<PGPPublicKeyRing> ret = new ArrayList<PGPPublicKeyRing>();
         for (String publicKey : parserResult.getPublicKeysData()) {
             PGPPublicKeyRing parsedKey = create(publicKey);
-            if(parsedKey != null) {
+            if (parsedKey != null) {
                 ret.add(parsedKey);
             }
         }
