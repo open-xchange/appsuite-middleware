b93aeb215ea5df65c3072e8cb3cfea0d418a97c7
Daniel Becker <daniel.becker@open-xchange.com>
2018-11-29 10:00:00 +0100

    MW-1089 Drop data from genconf_attributes_* too
    Also enhanced logging

--- com.openexchange.server/src/com/openexchange/groupware/update/tasks/DropPublicationTablesTask.java
+++ com.openexchange.server/src/com/openexchange/groupware/update/tasks/DropPublicationTablesTask.java
@@ -54,14 +54,15 @@ import static com.openexchange.database.Databases.rollback;
 import static com.openexchange.database.Databases.startTransaction;
 import static com.openexchange.java.Autoboxing.I;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.openexchange.database.Databases;
@@ -98,8 +99,8 @@ public class DropPublicationTablesTask implements UpdateTaskV2 {
      * | user_id          | int(10) unsigned | NO   |     | NULL    |       |
      * | entity           | int(10) unsigned | NO   |     | NULL    |       |
      * | module           | varchar(255)     | NO   |     | NULL    |       |
-     * | configuration_id | int(10) unsigned | NO   |     | NULL    |       |
-     * | target_id        | varchar(255)     | NO   |     | NULL    |       |
+     * | configuration_id | int(10) unsigned | NO   |     | NULL    |       | --->  Key 'id' in 'genconf_attributes_strings'
+     * | target_id        | varchar(255)     | NO   |     | NULL    |       |       and 'genconf_attributes_bools'
      * | enabled          | tinyint(1)       | NO   |     | 1       |       |
      * | created          | bigint(20)       | NO   |     | 0       |       |
      * | lastModified     | bigint(20)       | NO   |     | 0       |       |
@@ -123,6 +124,16 @@ public class DropPublicationTablesTask implements UpdateTaskV2 {
      * @formatter:on 
      */
 
+    private static final String PUBLICATIONS = "publications";
+
+    private static final String PUBLICATION_USERS = "publication_users";
+
+    private static final String SEQUENCE_PUBLICATIONS = "sequence_publications";
+
+    private static final String GENCONF_ATTRIBUTES_BOOLS = "genconf_attributes_bools";
+
+    private static final String GENCONF_ATTRIBUTES_STRINGS = "genconf_attributes_strings";
+
     private final static Logger LOGGER = LoggerFactory.getLogger(DropPublicationTablesTask.class);
 
     public DropPublicationTablesTask() {
@@ -140,13 +151,13 @@ public class DropPublicationTablesTask implements UpdateTaskV2 {
             startTransaction(con);
             rollback = true;
 
-            boolean existsPublication = Tools.tableExists(con, "publications");
+            boolean existsPublication = Tools.tableExists(con, PUBLICATIONS);
 
             /*
              * Get all publication users
              */
             Map<Integer, Map<Integer, List<PublicationUser>>> contextToPub = new HashMap<>();
-            if (Tools.tableExists(con, "publication_users")) {
+            if (Tools.tableExists(con, PUBLICATION_USERS)) {
                 // Only fetch data if we can log something useful
                 if (existsPublication) {
                     executeQuery(con, "SELECT cid, id, name, created, lastModified FROM publication_users", (ResultSet resultSet) -> {
@@ -179,53 +190,69 @@ public class DropPublicationTablesTask implements UpdateTaskV2 {
                         } while (resultSet.next());
                     });
                 }
-                Tools.dropTable(con, "publication_users");
+                Tools.dropTable(con, PUBLICATION_USERS);
             }
 
+            Map<Integer, List<Integer>> confIds = new HashMap<>(contextToPub.size() + (int) (contextToPub.size() * 0.05), 0.99f);
             /*
              * Get all publications and print out the information that is going to be deleted
              */
             if (existsPublication) {
-                executeQuery(con, "SELECT cid, id, user_id, entity, module, configuration_id, target_id, enabled, created, lastModified FROM publications", (ResultSet rs) -> {
+                executeQuery(con, "SELECT cid, id, configuration_id, user_id, entity, module, target_id, enabled, created, lastModified FROM publications", (ResultSet rs) -> {
                     do {
                         int i = 1;
                         Integer contextId = I(rs.getInt(i++));
                         Integer publicationId = I(rs.getInt(i++));
+                        Integer configurationId = I(rs.getInt(i++));
 
                         //@formatter:off         
                         LOGGER.info(
-                            "Removing the publication with ID %s for user %s in context %s.\nThe publication has following attributes:\n" + 
-                            "##############################################################\n" +
-                            "entity:\t%s\nmodule:\t%s\nconfiguration_id:\t%s\ntarget_id:\t%s\nenabled:\t%s\ncreated:\t%s\nlastModified:\t%s\n" + 
-                            "users:\t[%s]\n\n",
+                            "##################################################################\n" +
+                            "Removing the publication with ID {} for user {} in context {}.\nThe publication has following attributes:\n" + 
+                            "##################################################################\n" +
+                            "entity:\t\t\t{}\nmodule:\t\t\t{}\nconfiguration_id:\t{}\ntarget_id:\t\t{}\nenabled:\t\t{}\ncreated:\t\t{}\nlastModified:\t\t{}\n" + 
+                            "users:\t\t\t[{}]\n",
                             publicationId, 
                             rs.getString(i++), // user ID
                             contextId,
                             I(rs.getInt(i++)), // entity
                             rs.getString(i++), // module
-                            I(rs.getInt(i++)), // configuration_id
+                            configurationId,
                             rs.getString(i++), // target_id
                             0 == rs.getInt(i++) ? Boolean.FALSE : Boolean.TRUE, // enabled
-                            new Date(rs.getInt(i++)), // created
-                            new Date(rs.getInt(i++)), // lastModified
+                            new Date(rs.getLong(i++)), // created
+                            new Date(rs.getLong(i++)), // lastModified
                             printPublicationUsers(contextToPub, contextId, publicationId)
                             );
-                        //@formatter:on         
-
+                        //@formatter:on   
+                        if (null != configurationId) {
+                            if (false == confIds.containsKey(contextId)) {
+                                confIds.put(contextId, new ArrayList<>());
+                            }
+                            confIds.get(contextId).add(configurationId);
+                        }
                     } while (rs.next());
                 });
-                Tools.dropTable(con, "publications");
+                Tools.dropTable(con, PUBLICATIONS);
+            }
+
+            // Delete related data
+            boolean existsAttributeStrings = Tools.tableExists(con, GENCONF_ATTRIBUTES_STRINGS);
+            boolean existsAttributeBools = Tools.tableExists(con, GENCONF_ATTRIBUTES_BOOLS);
+            for (Entry<Integer, List<Integer>> entry : confIds.entrySet()) {
+                deleteFromTable(con, entry, GENCONF_ATTRIBUTES_STRINGS, existsAttributeStrings);
+                deleteFromTable(con, entry, GENCONF_ATTRIBUTES_BOOLS, existsAttributeBools);
             }
 
             // Nothing to log, just remove the table
-            if (Tools.tableExists(con, "sequence_publications")) {
-                Tools.dropTable(con, "sequence_publications");
+            if (Tools.tableExists(con, SEQUENCE_PUBLICATIONS)) {
+                Tools.dropTable(con, SEQUENCE_PUBLICATIONS);
             }
 
             con.commit();
             rollback = false;
 
-            LOGGER.info("Publications have been removed successfully!");
+            LOGGER.info("Publications have been removed successfully! Tables \"sequence_publications\", \"publications\" and \"publication_users\" were dropped.");
         } catch (SQLException e) {
             throw UpdateExceptionCodes.SQL_PROBLEM.create(e, e.getMessage());
         } catch (RuntimeException e) {
@@ -268,6 +295,36 @@ public class DropPublicationTablesTask implements UpdateTaskV2 {
     }
 
     /**
+     * Deletes entries from given table name
+     * 
+     * @param con The {@link Connection} to use
+     * @param entry The entries to remove, sorted by context
+     * @param tableName The name of the table
+     * @param exists <code>true</code> if the table exists and data can be deleted
+     * @throws SQLException In case data can't be removed
+     */
+    private void deleteFromTable(Connection con, Entry<Integer, List<Integer>> entry, String tableName, boolean exists) throws SQLException {
+        if (exists) {
+            PreparedStatement stmt = null;
+            try {
+                stmt = con.prepareStatement(Databases.getIN("DELETE FROM " + tableName + " WHERE cid=? and id IN (", entry.getValue().size()));
+                if (null != entry.getKey()) {
+                    stmt.setInt(1, entry.getKey().intValue());
+                    int i = 2;
+                    for (Integer p : entry.getValue()) {
+                        stmt.setInt(i++, p.intValue());
+                    }
+                }
+
+                int result = stmt.executeUpdate();
+                LOGGER.debug("The statement has been executed successfully. Result was {}", I(result));
+            } finally {
+                Databases.closeSQLStuff(stmt);
+            }
+        }
+    }
+
+    /**
      * Executes the query and logs
      * 
      * @param con The {@link Connection} to use
@@ -276,11 +333,12 @@ public class DropPublicationTablesTask implements UpdateTaskV2 {
      * @throws SQLException If the query fails
      */
     private static void executeQuery(final Connection con, String query, SQLConsumer<ResultSet> logResults) throws SQLException {
-        Statement stmt = null;
+        PreparedStatement stmt = null;
         ResultSet rs = null;
         try {
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(query);
+            stmt = con.prepareStatement(query);
+
+            rs = stmt.executeQuery();
             if (rs.next()) {
                 logResults.accept(rs);
             } else {
