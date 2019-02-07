f65e25df4362382bb4aa101e2866a8aae67aff54
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2019-02-07 15:18:30 +0100

    MW-1105: Use ImageMetadataReader to extract image metadata

--- com.openexchange.imagetransformation.java/src/com/openexchange/imagetransformation/java/services/JavaImageMetadataService.java
+++ com.openexchange.imagetransformation.java/src/com/openexchange/imagetransformation/java/services/JavaImageMetadataService.java
@@ -52,10 +52,18 @@ package com.openexchange.imagetransformation.java.services;
 import static com.openexchange.imagetransformation.java.transformations.Utils.getImageInputStream;
 import static com.openexchange.imagetransformation.java.transformations.Utils.getImageReader;
 import java.awt.Dimension;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
+import com.drew.imaging.FileType;
+import com.drew.imaging.FileTypeDetector;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.heif.HeifDirectory;
 import com.openexchange.imagetransformation.ImageMetadata;
 import com.openexchange.imagetransformation.ImageMetadataOptions;
 import com.openexchange.imagetransformation.ImageMetadataService;
@@ -67,9 +75,12 @@ import com.openexchange.java.Strings;
  * {@link JavaImageMetadataService}
  *
  * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
+ * @author <a href="mailto:kevin.ruthmann@open-xchange.com">Kevin Ruthmann</a>
  * @since v7.8.4
  */
 public class JavaImageMetadataService implements ImageMetadataService {
+
+    private static final String HEIF_FILE_FORMAT = "heif";
 
     /**
      * Initializes a new {@link JavaImageMetadataService}.
@@ -80,61 +91,185 @@ public class JavaImageMetadataService implements ImageMetadataService {
 
     @Override
     public Dimension getDimensionFor(InputStream imageStream, String mimeType, String name) throws IOException {
-        ImageInputStream imageInputStream = null;
-        ImageReader reader = null;
+        BufferedInputStream bufferedInputStream = imageStream instanceof BufferedInputStream ? (BufferedInputStream) imageStream : new BufferedInputStream(imageStream, 64);
         try {
-            imageInputStream = getImageInputStream(imageStream);
-            reader = getImageReader(imageInputStream, mimeType, name);
-            reader.setInput(imageInputStream);
-            int width = reader.getWidth(0);
-            int height = reader.getHeight(0);
-            return new Dimension(width, height);
-        } finally {
-            if (null != reader) {
-                try {
-                    reader.dispose();
-                } catch (final Exception e) {
-                    // Ignore
-                }
+            // Check for heif file
+            Dimension result = getDimensionFromHeifFile(bufferedInputStream);
+            if(result != null) {
+                return result;
             }
-            Streams.close(imageInputStream, imageStream);
+            
+            // Using ImageIO to read metadata
+            ImageInputStream imageInputStream = null;
+            ImageReader reader = null;
+            try {
+                imageInputStream = getImageInputStream(bufferedInputStream);
+                reader = getImageReader(imageInputStream, mimeType, name);
+                reader.setInput(imageInputStream);
+                int width = reader.getWidth(0);
+                int height = reader.getHeight(0);
+                return new Dimension(width, height);
+            } finally {
+                if (null != reader) {
+                    try {
+                        reader.dispose();
+                    } catch (final Exception e) {
+                        // Ignore
+                    }
+                }
+                Streams.close(imageInputStream);
+            }
+        } finally {
+            Streams.close(bufferedInputStream);
         }
+    }
+    
+    /**
+     * Checks whether the given image is a heif file and uses {@link ImageMetadataReader} to get the dimension
+     * 
+     * @param bufferedInputStream The stream containing the image
+     * @return The {@link Dimension} or null
+     * @throws IOException
+     */
+    private Dimension getDimensionFromHeifFile(BufferedInputStream bufferedInputStream) throws IOException {
+        // Use ImageMetadataReader if the image is a heif file
+        FileType detectedFileType;
+        try {
+            detectedFileType = FileTypeDetector.detectFileType(bufferedInputStream);
+        } catch (AssertionError e) {
+            detectedFileType = FileType.Unknown;
+        }
+        bufferedInputStream.reset();
+        if (FileType.Heif == detectedFileType) {
+            Metadata metadata;
+            try {
+                metadata = ImageMetadataReader.readMetadata(bufferedInputStream, -1, detectedFileType);
+            } catch (ImageProcessingException e) {
+                throw new IOException(e.getMessage(), e);
+            }
+            Directory heifDirectory = metadata.getFirstDirectoryOfType(HeifDirectory.class);
+            if (heifDirectory != null) {
+
+                long width = -1;
+                Long longObject = heifDirectory.getLongObject(HeifDirectory.TAG_IMAGE_WIDTH);
+                if (null != longObject) {
+                    width = longObject.longValue();
+                }
+                long height = -1;
+                longObject = heifDirectory.getLongObject(HeifDirectory.TAG_IMAGE_HEIGHT);
+                if (null != longObject) {
+                    height = longObject.longValue();
+                }
+
+                return new Dimension(Math.toIntExact(width), Math.toIntExact(height));
+            }
+            // Don't contain a heif directory -> fall back to normal handling
+            bufferedInputStream.reset();
+        }
+        return null;
     }
 
     @Override
     public ImageMetadata getMetadataFor(InputStream imageStream, String mimeType, String name, ImageMetadataOptions imageMetadataOptions) throws IOException {
-        ImageInputStream imageInputStream = null;
-        ImageReader reader = null;
+        BufferedInputStream bufferedInputStream = imageStream instanceof BufferedInputStream ? (BufferedInputStream) imageStream : new BufferedInputStream(imageStream, 64);
         try {
-            imageInputStream = getImageInputStream(imageStream);
-            reader = getImageReader(imageInputStream, mimeType, name);
-            reader.setInput(imageInputStream);
-
-            ImageMetadata.Builder builder = ImageMetadata.builder();
-            if (imageMetadataOptions.isDimension()) {
-                int width = reader.getWidth(0);
-                int height = reader.getHeight(0);
-                builder.withDimension(new Dimension(width, height));
+            // Check for heif file
+            ImageMetadata result = getMetadataFromHeifFile(bufferedInputStream, imageMetadataOptions);
+            if(result != null) {
+                return result;
             }
 
-            if (imageMetadataOptions.isFormatName()) {
-                String formatName = reader.getFormatName();
-                if (null != formatName) {
-                    builder.withFormatName(Strings.asciiLowerCase(formatName));
+            // Using ImageIO to read metadata
+            ImageInputStream imageInputStream = null;
+            ImageReader reader = null;
+            try {
+                imageInputStream = getImageInputStream(bufferedInputStream);
+                reader = getImageReader(imageInputStream, mimeType, name);
+                reader.setInput(imageInputStream);
+                ImageMetadata.Builder builder = ImageMetadata.builder();
+                if (imageMetadataOptions.isDimension()) {
+                    int width = reader.getWidth(0);
+                    int height = reader.getHeight(0);
+                    builder.withDimension(new Dimension(width, height));
                 }
-            }
 
-            return builder.build();
+                if (imageMetadataOptions.isFormatName()) {
+                    String formatName = reader.getFormatName();
+                    if (null != formatName) {
+                        builder.withFormatName(Strings.asciiLowerCase(formatName));
+                    }
+                }
+
+                return builder.build();
+            } finally {
+                if (null != reader) {
+                    try {
+                        reader.dispose();
+                    } catch (final Exception e) {
+                        // Ignore
+                    }
+                }
+                Streams.close(imageInputStream);
+            }
         } finally {
-            if (null != reader) {
-                try {
-                    reader.dispose();
-                } catch (final Exception e) {
-                    // Ignore
-                }
-            }
-            Streams.close(imageInputStream, imageStream);
+            Streams.close(bufferedInputStream);
         }
+
+    }
+    
+    /**
+     * Checks whether the given image is a heif file and uses {@link ImageMetadataReader} to get the metadata
+     * 
+     * @param bufferedInputStream The stream containing the image
+     * @param imageMetadataOptions The {@link ImageMetadataOptions}
+     * @return The {@link ImageMetadata} or null
+     * @throws IOException
+     * @throws ImageProcessingException
+     */
+    private ImageMetadata getMetadataFromHeifFile(BufferedInputStream bufferedInputStream, ImageMetadataOptions imageMetadataOptions) throws IOException {
+        // Use ImageMetadataReader if the image is a heif file
+        FileType detectedFileType;
+        try {
+            detectedFileType = FileTypeDetector.detectFileType(bufferedInputStream);
+        } catch (AssertionError e) {
+            detectedFileType = FileType.Unknown;
+        }
+        bufferedInputStream.reset();
+        if (FileType.Heif == detectedFileType) {
+            Metadata metadata;
+            try {
+                metadata = ImageMetadataReader.readMetadata(bufferedInputStream, -1, detectedFileType);
+            } catch (ImageProcessingException e) {
+                throw new IOException(e.getMessage(), e);
+            }
+            Directory heifDirectory = metadata.getFirstDirectoryOfType(HeifDirectory.class);
+            if (heifDirectory != null) {
+                long width = -1;
+                Long longObject = heifDirectory.getLongObject(HeifDirectory.TAG_IMAGE_WIDTH);
+                if (null != longObject) {
+                    width = longObject.longValue();
+                }
+                long height = -1;
+                longObject = heifDirectory.getLongObject(HeifDirectory.TAG_IMAGE_HEIGHT);
+                if (null != longObject) {
+                    height = longObject.longValue();
+                }
+
+                ImageMetadata.Builder builder = ImageMetadata.builder();
+                if (imageMetadataOptions.isDimension()) {
+                    builder.withDimension(new Dimension(Math.toIntExact(width), Math.toIntExact(height)));
+                }
+
+                if (imageMetadataOptions.isFormatName()) {
+                    builder.withFormatName(HEIF_FILE_FORMAT);
+                }
+
+                return builder.build();
+            }
+            // Don't contain a heif directory -> reset stream and return null
+            bufferedInputStream.reset();
+        }
+        return null;
     }
 
 }
