782d29dbc758bd15ea1ae831cf552f62055561ff
Martin Schneider <martin.schneider@open-xchange.com>
2019-03-14 09:13:51 +0100

    Propagate alarm changes to event exceptions #1 follow-up

--- com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/performer/UpdatePerformer.java
+++ com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/performer/UpdatePerformer.java
@@ -67,6 +67,7 @@ import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 import java.util.SortedSet;
