634e2bf02f6dc55845b88391f8cc1c95a8adaeb9
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2019-03-16 08:38:43 +0100

    Change for bug #62008: Treat calendar-timezone value as CDATA

--- com.openexchange.caldav/src/com/openexchange/caldav/mixins/CalendarTimezone.java
+++ com.openexchange.caldav/src/com/openexchange/caldav/mixins/CalendarTimezone.java
@@ -49,7 +49,6 @@
 
 package com.openexchange.caldav.mixins;
 
-import java.io.UnsupportedEncodingException;
 import org.jdom2.Namespace;
 import com.openexchange.caldav.GroupwareCaldavFactory;
 import com.openexchange.chronos.ical.ICalService;
@@ -57,6 +56,7 @@ import com.openexchange.dav.DAVProtocol;
 import com.openexchange.dav.resources.FolderCollection;
 import com.openexchange.exception.OXException;
 import com.openexchange.groupware.ldap.User;
+import com.openexchange.java.Charsets;
 import com.openexchange.java.Strings;
 import com.openexchange.webdav.protocol.helpers.SingleXMLPropertyMixin;
 
@@ -96,16 +96,9 @@ public class CalendarTimezone extends SingleXMLPropertyMixin {
             if (Strings.isNotEmpty(user.getTimeZone())) {
                 ICalService iCalService = factory.getService(ICalService.class);
                 byte[] iCal = iCalService.exportICal(iCalService.initParameters()).add(user.getTimeZone()).toByteArray();
-                return new String(iCal, "UTF-8");
-                //                ICalEmitter icalEmitter = factory.getIcalEmitter();
-                //                ICalSession iCalSession = icalEmitter.createSession(new SimpleMode(ZoneInfo.OUTLOOK, null));
-                //                if (icalEmitter.writeTimeZone(iCalSession, user.getTimeZone(), new ArrayList<ConversionError>(), new ArrayList<ConversionWarning>())) {
-                //                    ByteArrayOutputStream outputStream = Streams.newByteArrayOutputStream(4096);
-                //                    icalEmitter.writeSession(iCalSession, outputStream);
-                //                    return new String(outputStream.toByteArray(), "UTF-8");
-                //                }
+                return "<![CDATA[" + new String(iCal, Charsets.UTF_8) + "]]>";
             }
-        } catch (OXException | UnsupportedEncodingException e) {
+        } catch (OXException e) {
             org.slf4j.LoggerFactory.getLogger(CalendarTimezone.class).warn("Error serializing calendar-timezone", e);
         }
         return null;
