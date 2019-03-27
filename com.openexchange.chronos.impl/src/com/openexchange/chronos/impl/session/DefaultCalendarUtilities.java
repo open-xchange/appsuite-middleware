c71dd7ce03847ac865c7ede37dfe383d9156c0f9
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2019-03-27 14:37:10 +0100

    removed obsolete utility methods

--- com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/session/DefaultCalendarUtilities.java
+++ com.openexchange.chronos.impl/src/com/openexchange/chronos/impl/session/DefaultCalendarUtilities.java
@@ -50,9 +50,7 @@
 package com.openexchange.chronos.impl.session;
 
 import java.util.Comparator;
-import java.util.List;
 import java.util.TimeZone;
-import com.openexchange.chronos.Attendee;
 import com.openexchange.chronos.Event;
 import com.openexchange.chronos.EventField;
 import com.openexchange.chronos.common.mapping.DefaultEventUpdate;
@@ -65,8 +63,6 @@ import com.openexchange.chronos.service.EntityResolver;
 import com.openexchange.chronos.service.EventUpdate;
 import com.openexchange.chronos.service.SortOrder;
 import com.openexchange.exception.OXException;
-import com.openexchange.groupware.tools.mappings.MappedIncorrectString;
-import com.openexchange.groupware.tools.mappings.MappedTruncation;
 import com.openexchange.groupware.tools.mappings.Mapping;
 import com.openexchange.server.ServiceLookup;
 
@@ -105,72 +101,6 @@ public class DefaultCalendarUtilities implements CalendarUtilities {
     @Override
     public EventUpdate compare(Event original, Event update, boolean considerUnset, EventField... ignoredFields) throws OXException {
         return new DefaultEventUpdate(original, update, considerUnset, ignoredFields);
-    }
-
-    @Override
-    public boolean handleIncorrectString(OXException e, Event event) {
-        boolean hasReplaced = false;
-        if (null != event) {
-            try {
-                hasReplaced |= MappedIncorrectString.replace(e.getProblematics(), event, "");
-            } catch (ClassCastException | OXException x1) {
-                // also try attendees
-                List<Attendee> attendees = event.getAttendees();
-                if (null != attendees) {
-                    for (Attendee attendee : attendees) {
-                        try {
-                            hasReplaced |= MappedIncorrectString.replace(e.getProblematics(), attendee, "");
-                        } catch (ClassCastException | OXException x2) {
-                            // ignore
-                        }
-                    }
-                }
-            }
-        }
-        return hasReplaced;
-    }
-
-    @Override
-    public boolean handleIncorrectString(OXException e, List<Attendee> attendees) {
-        boolean hasReplaced = false;
-        if (null != attendees) {
-            for (Attendee attendee : attendees) {
-                try {
-                    hasReplaced |= MappedIncorrectString.replace(e.getProblematics(), attendee, "");
-                } catch (ClassCastException | OXException x) {
-                    // ignore
-                }
-            }
-        }
-        return hasReplaced;
-    }
-
-    @Override
-    public boolean handleDataTruncation(OXException e, Event event) {
-        boolean hasTrimmed = false;
-        if (null != event) {
-            try {
-                hasTrimmed |= MappedTruncation.truncate(e.getProblematics(), event);
-            } catch (ClassCastException | OXException x) {
-                // ignore
-            }
-        }
-        return hasTrimmed;
-    }
-
-    @Override
-    public boolean handleDataTruncation(OXException e, List<Attendee> attendees) {
-        boolean hasTrimmed = false;
-        if (null != attendees) {
-            for (Attendee attendee : attendees) {
-                try {
-                    hasTrimmed |= MappedTruncation.truncate(e.getProblematics(), attendee);
-                } catch (ClassCastException | OXException x) {
-                    // ignore
-                }
-            }
-        }
-        return hasTrimmed;
     }
 
     @Override
