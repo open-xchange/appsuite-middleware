765d09176b63605170741cd64126aecf845a824d
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2019-03-27 09:38:05 +0100

    added missing classes

---
+++ openexchange-test/src/com/openexchange/dav/caldav/reports/CalendarQueryReport.java
@@ -0,0 +1,89 @@
+/*
+ *
+ *    OPEN-XCHANGE legal information
+ *
+ *    All intellectual property rights in the Software are protected by
+ *    international copyright laws.
+ *
+ *
+ *    In some countries OX, OX Open-Xchange, open xchange and OXtender
+ *    as well as the corresponding Logos OX Open-Xchange and OX are registered
+ *    trademarks of the OX Software GmbH group of companies.
+ *    The use of the Logos is not covered by the GNU General Public License.
+ *    Instead, you are allowed to use these Logos according to the terms and
+ *    conditions of the Creative Commons License, Version 2.5, Attribution,
+ *    Non-commercial, ShareAlike, and the interpretation of the term
+ *    Non-commercial applicable to the aforementioned license is published
+ *    on the web site http://www.open-xchange.com/EN/legal/index.html.
+ *
+ *    Please make sure that third-party modules and libraries are used
+ *    according to their respective licenses.
+ *
+ *    Any modifications to this package must retain all copyright notices
+ *    of the original copyright holder(s) for the original code used.
+ *
+ *    After any such modifications, the original and derivative code shall remain
+ *    under the copyright of the copyright holder(s) and/or original author(s)per
+ *    the Attribution and Assignment Agreement that can be located at
+ *    http://www.open-xchange.com/EN/developer/. The contributing author shall be
+ *    given Attribution for the derivative code and a license granting use.
+ *
+ *     Copyright (C) 2016-2020 OX Software GmbH
+ *     Mail: info@open-xchange.com
+ *
+ *
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU General Public License, Version 2 as published
+ *     by the Free Software Foundation.
+ *
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *     for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc., 59
+ *     Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package com.openexchange.dav.caldav.reports;
+
+import org.apache.jackrabbit.webdav.DavException;
+import org.apache.jackrabbit.webdav.DavResource;
+import org.apache.jackrabbit.webdav.version.DeltaVConstants;
+import org.apache.jackrabbit.webdav.version.report.Report;
+import org.apache.jackrabbit.webdav.version.report.ReportInfo;
+import org.apache.jackrabbit.webdav.version.report.ReportType;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import com.openexchange.dav.PropertyNames;
+
+/**
+ * {@link CalendarQueryReport}
+ *
+ * @author <a href="mailto:tobias.friedrich@open-xchange.com">Tobias Friedrich</a>
+ * @since v7.10.2
+ */
+public class CalendarQueryReport implements Report, DeltaVConstants {
+
+    public static final ReportType CALENDAR_QUERY = ReportType.register(PropertyNames.CALENDAR_QUERY.getName(), PropertyNames.CALENDAR_QUERY.getNamespace(), CalendarQueryReport.class);
+
+    @Override
+    public ReportType getType() {
+        return CALENDAR_QUERY;
+    }
+
+    @Override
+    public boolean isMultiStatusReport() {
+        return true;
+    }
+
+    @Override
+    public void init(DavResource dr, ReportInfo ri) throws DavException {}
+
+    @Override
+    public Element toXml(Document arg0) {
+        throw new UnsupportedOperationException();
+    }
+}
