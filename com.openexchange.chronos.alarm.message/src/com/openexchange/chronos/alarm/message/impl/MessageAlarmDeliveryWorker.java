042d5d7f0707b42aa571bfa641d997320b57b725
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2019-04-05 12:59:01 +0200

    Fixed mail alarm delivery + improved logging
    (cherry picked from commit fa60f49bd5a19762b9d0b9a3c6ae4a371e7685e3)

--- com.openexchange.chronos.alarm.message/src/com/openexchange/chronos/alarm/message/impl/MessageAlarmDeliveryWorker.java
+++ com.openexchange.chronos.alarm.message/src/com/openexchange/chronos/alarm/message/impl/MessageAlarmDeliveryWorker.java
@@ -290,6 +290,7 @@ public class MessageAlarmDeliveryWorker implements Runnable {
                 try {
                     UpdateStatus status = Updater.getInstance().getStatus(ctxId);
                     if (!status.isExecutedSuccessfully(MessageAlarmDeliveryWorkerUpdateTask.class.getName()) || status.backgroundUpdatesRunning() || status.blockingUpdatesRunning()) {
+                        LOG.info("Unable to handle context {} because it is currently updating or MessageAlarmDeliveryWorkerUpdateTask hasn't run successfully yet.", iCtxId);
                         continue NextCtxId;
                     }
                 } catch (OXException e) {
@@ -321,7 +322,7 @@ public class MessageAlarmDeliveryWorker implements Runnable {
 
                     writeCon = dbservice.getForUpdateTask(ctxId);
                     writeCon.setAutoCommit(false);
-                    lockedTriggers = storage.getAndLockTriggers(writeCon, until.getTime(), overdueTime.getTime(), true);
+                    lockedTriggers = storage.getAndLockTriggers(writeCon, until.getTime(), overdueTime.getTime(), true, registry.getActions());
                     if (lockedTriggers.isEmpty()) {
                         successful = true;
                         if (Thread.interrupted()) {
