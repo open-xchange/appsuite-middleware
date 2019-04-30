2c98389931148f66e64bd894fb14f59dd7208f35
Kevin Ruthmann <kevin.ruthmann@open-xchange.com>
2019-04-30 15:53:47 +0200

    Recreated Bug13501Test for new chronos api

--- openexchange-test/src/com/openexchange/ajax/chronos/bugs/Bug13501Test.java
+++ openexchange-test/src/com/openexchange/ajax/chronos/bugs/Bug13501Test.java
@@ -49,36 +49,57 @@
 
 package com.openexchange.ajax.chronos.bugs;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import com.openexchange.test.concurrent.ParallelSuite;
+import static org.junit.Assert.assertEquals;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import org.junit.Test;
+import com.openexchange.ajax.chronos.AbstractChronosTest;
+import com.openexchange.ajax.chronos.factory.EventFactory;
+import com.openexchange.ajax.chronos.factory.EventFactory.RecurringFrequency;
+import com.openexchange.ajax.chronos.factory.RRuleFactory;
+import com.openexchange.ajax.chronos.util.DateTimeUtil;
+import com.openexchange.testing.httpclient.models.EventData;
 
 /**
- * {@link ChronosBugsTestSuite}
+ * 
+ * {@link Bug13501Test}
  *
- * @author <a href="mailto:ioannis.chouklis@open-xchange.com">Ioannis Chouklis</a>
+ * @author <a href="mailto:kevin.ruthmann@open-xchange.com">Kevin Ruthmann</a>
+ * @since v7.10.3
  */
-@RunWith(ParallelSuite.class)
-@Suite.SuiteClasses({
-    // @formatter:off
-    Bug10154Test.class,
-    Bug10733Test.class,
-    Bug10836Test.class,
-    Bug11250Test.class,
-    Bug12099Test.class,
-    Bug12432Test.class,
-    Bug12444Test.class,
-    Bug12610Test.class,
-    Bug12842Test.class,
-    Bug13090Test.class,
-    Bug13214Test.class,
-    Bug13447Test.class,
-    Bug13501Test.class,
-    Bug58814Test.class,
-    Bug64836Test.class
-    // @formatter:on
+public class Bug13501Test extends AbstractChronosTest {
 
-})
-public class ChronosBugsTestSuite {
+    public Bug13501Test() {
+        super();
+    }
+
+    @Test
+    public void testBug13501() throws Exception {
+        Date startSearch = new Date(1246233600000L); // 29.06.2009 00:00:00
+        Date endSearch = new Date(1249257600000L); // 03.08.2009 00:00:00
+
+        Calendar start = Calendar.getInstance();
+        start.set(Calendar.DAY_OF_MONTH, 15);
+        start.set(Calendar.MONTH, Calendar.JUNE);
+        start.set(Calendar.YEAR, 2009);
+        start.set(Calendar.HOUR_OF_DAY, 8);
+        start.set(Calendar.MINUTE, 0);
+        start.set(Calendar.SECOND, 0);
+        start.set(Calendar.MILLISECOND, 0);
+        Calendar end = Calendar.getInstance();
+        end.setTimeInMillis(start.getTimeInMillis() + 3600000);
+
+        EventData series = EventFactory.createSeriesEvent(getCalendaruser(), "Bug13501Test", DateTimeUtil.getDateTime(start), DateTimeUtil.getDateTime(end), 5);
+        series.setFolder(folderId);
+        EventData createEvent = eventManager.createEvent(series, true);
+
+        createEvent.setRrule(RRuleFactory.getFrequencyWithOccurenceLimit(RecurringFrequency.WEEKLY, 5));
+
+        eventManager.updateEvent(createEvent, false, false);
+
+        List<EventData> allEvents = eventManager.getAllEvents(startSearch, endSearch, true, folderId);
+        assertEquals(3, allEvents.size());
+    }
 
 }
