d8eb77c5366fecde3ac6fa9f9c65bb730c497d87
thorben.betten <thorben.betten@open-xchange.com>
2019-04-30 12:03:34 +0200

    Fixed conflict

--- openexchange-test/src/com/openexchange/ajax/task/TasksTest.java
+++ openexchange-test/src/com/openexchange/ajax/task/TasksTest.java
@@ -51,6 +51,7 @@ package com.openexchange.ajax.task;
 
 import static com.openexchange.java.Autoboxing.I;
 import static com.openexchange.java.Autoboxing.L;
+import static com.openexchange.ajax.task.TaskTools.getTask;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
