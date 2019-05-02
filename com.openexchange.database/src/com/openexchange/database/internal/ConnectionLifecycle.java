d05d77ae9f32aa7fcc13be869c1460237a0c2d5e
Thorben Betten <thorben.betten@open-xchange.com>
2019-05-02 09:31:05 +0200

    Accept new property "com.openexchange.database.alwaysCheckOnActivate", which controls whether connection's validity is always explicitly checked on activate #3

--- com.openexchange.database/src/com/openexchange/database/internal/ConnectionLifecycle.java
+++ com.openexchange.database/src/com/openexchange/database/internal/ConnectionLifecycle.java
@@ -61,7 +61,6 @@ import java.util.Iterator;
 import java.util.Properties;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicReference;
-import org.slf4j.Logger;
 import com.openexchange.config.ConfigurationService;
 import com.openexchange.config.DefaultInterests;
 import com.openexchange.config.Interests;
@@ -78,11 +77,6 @@ import com.openexchange.pooling.PooledData;
  * Life cycle for database connections.
  */
 class ConnectionLifecycle implements PoolableLifecycle<Connection> {
-
-    /** Simple class to delay initialization until needed */
-    private static class LoggerHolder {
-        static final Logger LOG = org.slf4j.LoggerFactory.getLogger(ConnectionLifecycle.class);
-    }
 
     /**
      * SQL command for checking the connection.
@@ -223,7 +217,7 @@ class ConnectionLifecycle implements PoolableLifecycle<Connection> {
                 }
             }
         } catch (final SQLException e) {
-            LoggerHolder.LOG.debug("Test SELECT statement failed", e);
+            ConnectionPool.LOG.debug("Test SELECT statement failed", e);
             retval = false;
         } finally {
             Databases.closeSQLStuff(result, stmt);
