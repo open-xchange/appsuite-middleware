49123796320a89d883a39ee8fedb8151410cd636
Greg Hill <greg.hill@open-xchange.com>
2019-06-03 08:02:20 -0400

    Related to Bug 64992: Hardening signature verification
    (cherry picked from commit 2a13cdcadd65cb89f9920d83bb48129c75a6deb7)

--- com.openexchange.pgp.core/src/com/openexchange/pgp/core/PGPSignatureVerificationResult.java
+++ com.openexchange.pgp.core/src/com/openexchange/pgp/core/PGPSignatureVerificationResult.java
@@ -49,7 +49,11 @@
 
 package com.openexchange.pgp.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.bouncycastle.openpgp.PGPPublicKey;
 import org.bouncycastle.openpgp.PGPSignature;
+import org.bouncycastle.openpgp.PGPUserAttributeSubpacketVector;
 
 /**
  * {@link PGPSignatureVerificationResult}
@@ -59,9 +63,16 @@ import org.bouncycastle.openpgp.PGPSignature;
  */
 public class PGPSignatureVerificationResult {
 
-    private final PGPSignature signature;
-    private final boolean verified;
-    private final boolean missing;
+    private final PGPSignature              signature;
+    private boolean                         verified;
+    private final boolean                   missing;
+    private String                          userId;
+    private String                          error;
+    private PGPPublicKey                    publicKey;
+    private PGPUserAttributeSubpacketVector userAttributes;
+    private PGPPublicKey                    issuerKey;
+    private final List<String>              issuerUserIds = new ArrayList<String>();
+
 
     /**
      * Initializes a new {@link PGPSignatureVerificationResult}.
@@ -91,6 +102,28 @@ public class PGPSignatureVerificationResult {
     }
 
     /**
+
+     * Gets error if any
+     *
+     * @return  Error message if any
+     */
+    public String getError() {
+        return error;
+    }
+
+    /**
+     * Sets the error message
+     *
+     * @param error The message
+     * @return this
+     */
+    public PGPSignatureVerificationResult setError(String error) {
+        this.error = error;
+        return this;
+    }
+
+    /**
+
      * Gets the verification result
      *
      * @return Whether the signature has been verified or not
@@ -99,7 +132,119 @@ public class PGPSignatureVerificationResult {
         return verified;
     }
 
+    /**
+     * Sets the verification result
+     *
+     * @param verified The result to set
+     * @return this
+     */
+    public PGPSignatureVerificationResult setVerified(boolean verified) {
+        this.verified = verified;
+        return this;
+    }
+
     public boolean isMissing() {
         return missing;
     }
+
+
+    /**
+     * Sets the PGP user id related to the signature verification, or null if this signature is not related to a user-id
+     *
+     * @param userId The user ID related to the signature verification
+     * @return this, for a fluent like style.
+     */
+    public PGPSignatureVerificationResult setUserId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    /**
+     * Get the PGP user id related to the signature verification
+     *
+     * @return The PGP user id related to the signature verification, or null if this signature is not related to a user-id
+     */
+    public String getUserId() {
+        return this.userId;
+    }
+
+
+    /**
+     * Gets the public key for which the signature was made, or null if the signature is not related to a public key.
+     * @return The public key, or null if the signature is not related to a public key
+     */
+    public PGPPublicKey getPublicKey() {
+        return this.publicKey ;
+    }
+
+    /**
+     * Sets the public key related to the signature, or null if the signature is not related to a public key.
+     * @param publicKey The public key related to the signature, or null if the signature is not related to a public key.
+     * @return this, for a fluent like style
+     */
+    public PGPSignatureVerificationResult setPublicKey(PGPPublicKey publicKey) {
+       this.publicKey = publicKey;
+       return this;
+    }
+
+    /**
+     * Gets the user attributes related to the signature, or null if the signature is not related to any user attributes
+     *
+     * @return The user attributes related for the signature, or null if the signature is not related to any user attributes.
+     */
+    public PGPUserAttributeSubpacketVector getUserAttributes() {
+        return this.userAttributes;
+    }
+
+    /**
+     * Sets the user attributes related to the signature, or null if the signature is not related to any user attributes.
+     * @param userAttributes The user attributes related to the signature.
+     * @return this, for a fluent like style
+     */
+    public PGPSignatureVerificationResult setUserAttributes(PGPUserAttributeSubpacketVector userAttributes) {
+       this.userAttributes = userAttributes;
+       return this;
+    }
+
+    /**
+     * Returns the public key of the issuer if known, null otherwise
+     *
+     * @return The public key of the issuer, or null if unknown
+     */
+    public PGPPublicKey getIssuerKey() {
+        return this.issuerKey;
+    }
+
+    /**
+     * Sets the public key of the issuer, or null if the issuer's key is not known
+     *
+     * @param issuerKey The public key of the issuer, or null if the public key is unknown.
+     * @return this
+     */
+    public PGPSignatureVerificationResult setIssuerKey(PGPPublicKey issuerKey) {
+        this.issuerKey = issuerKey;
+        return this;
+    }
+
+    /**
+     * Returns the user IDs of the issuer if known, an empty list otherwise
+     *
+     * @return The user IDs of the signature's issuer, or an empty list if unknown
+     */
+    public List<String> getIssuerUserIds() {
+        return this.issuerUserIds;
+    }
+
+    /**
+     * Adds a user ID of the issuer
+     *
+     * @param  issuerUserId The ID of the issuer to add
+     * @return this
+     */
+    public PGPSignatureVerificationResult addIssuerUserId(String issuerUserId) {
+        this.issuerUserIds.add(issuerUserId);
+        return this;
+    }
+
+
 }
