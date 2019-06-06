dc949256d9af48572248323d152431e85e3dc15a
Thorben Betten <thorben.betten@open-xchange.com>
2019-06-06 13:22:01 +0200

    Orderly handle background update tasks

--- com.openexchange.server/src/com/openexchange/groupware/update/internal/UpdateExecutor.java
+++ com.openexchange.server/src/com/openexchange/groupware/update/internal/UpdateExecutor.java
@@ -280,33 +280,23 @@ public final class UpdateExecutor {
                 }
             }
 
-            // Is context cache supposed to be invalidated?
-            boolean doRemoveContexts = blocking;
-
-            // Either unlock schema or unlock schema and invalidate context cache
-            if (doRemoveContexts) {
-                try {
-                    unlockSchema(blocking, state);
-                } catch (OXException oxe) {
-                    if (!SchemaExceptionCodes.SQL_PROBLEM.equals(oxe)) {
-                        throw oxe;
-                    }
-
-                    Throwable cause = oxe.getCause();
-                    if (!(cause instanceof SQLException) || !Databases.isReadTimeout((SQLException) cause)) {
-                        throw oxe;
-                    }
-
-                    // Unlocking the schema might be successfully executed then...
-                }
-
-                // Remove contexts from cache if they are cached during update process.
-                if (doRemoveContexts) {
-                    removeContexts();
-                }
-            } else {
+            try {
                 unlockSchema(blocking, state);
+            } catch (OXException oxe) {
+                if (!SchemaExceptionCodes.SQL_PROBLEM.equals(oxe)) {
+                    throw oxe;
+                }
+
+                Throwable cause = oxe.getCause();
+                if (!(cause instanceof SQLException) || !Databases.isReadTimeout((SQLException) cause)) {
+                    throw oxe;
+                }
+
+                // Unlocking the schema might be successfully executed then...
             }
+
+            // Remove contexts from cache if they are cached during update process.
+            removeContexts();
         }
     }
 
