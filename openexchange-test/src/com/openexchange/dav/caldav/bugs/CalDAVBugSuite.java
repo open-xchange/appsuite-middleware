15c2684f855d52d961bcb65249de391e26c20ab5
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2019-07-05 10:45:13 +0200

    com.openexchange.dav.caldav.bugs.Bug65943Test

--- openexchange-test/src/com/openexchange/dav/caldav/bugs/CalDAVBugSuite.java
+++ openexchange-test/src/com/openexchange/dav/caldav/bugs/CalDAVBugSuite.java
@@ -139,6 +139,7 @@ import com.openexchange.test.concurrent.ParallelSuite;
     Bug64809Test.class,
     Bug64836Test.class,
     Bug64937Test.class,
+    Bug65943Test.class,
 }) // @formatter:on
 public final class CalDAVBugSuite {
 
