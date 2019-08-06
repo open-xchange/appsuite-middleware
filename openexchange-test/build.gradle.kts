198dabf117398074b7ae1aa7fb6d483dd349e1d9
Sebastian Quast <sebastian.quast@open-xchange.com>
2019-08-06 12:59:28 +0200

    Added eclipse builders. Removed test client jars. Removed transitive dependencies.

--- openexchange-test/build.gradle.kts
+++ openexchange-test/build.gradle.kts
@@ -12,14 +12,17 @@ val httpClient = configurations.create("httpClient")
 val driveClient = configurations.create("driveClient")
 
 dependencies {
-    //httpClient("com.openexchange.appsuite.mw", "httpClient, "1.+")
-    //driveClient("com.openexchange.appsuite.mw", "driveClient", "1.+")
+    httpClient("com.openexchange.appsuite.mw", "http_api_client", "1.+"){
+        isTransitive = false
+    }
+    driveClient("com.openexchange.appsuite.mw", "rest_api_client", "1.+"){
+        isTransitive = false
+    }
+    //httpClient(files("../http-api/client-gen/out/http_api_client/build/libs/http_api_client-1.0-SNAPSHOT.jar"))
 }
 
 tasks.register("loadClients", Copy::class){
     from(httpClient.resolve())
     from(driveClient.resolve())
     into("lib/")
-
-    println("Currently not enabled!")
 }
