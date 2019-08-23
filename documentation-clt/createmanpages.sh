2aa75e5a373e21ece79e4cb00a84dbcdf9663059
Marc Arens <marc.arens@open-xchange.com>
2019-08-23 15:44:30 +0200

    RM-530: Rewrite createmanpages.sh

--- documentation-clt/createmanpages.sh
+++ documentation-clt/createmanpages.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
+set -Eeuo pipefail
+
+output_dir=output/manpages
+md2mangz() {
+  target_dir="${1}"
+  md_file="${2}"
+  name=$(basename -s .md "${md_file}")
+  echo ${md_file}
+  pandoc "${md_file}" -s -t man -o - | gzip -c > ${target_dir}/${name}.1.gz
+}
+export -f md2mangz
+export output_dir
+
 echo "Make output directory..."
-mkdir -p ${PWD}/output/manpages
+mkdir -p ${output_dir}
+
 echo "Creating manpages with pandoc..."
-SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
-for DIR in $(ls); do
-    if [ -d $DIR ]; then
-        for SUBDIR in $(ls $DIR); do
-            if [ -d $DIR/$SUBDIR ]; then
-                for FILE in $(ls $DIR/$SUBDIR | egrep -i "\.md$"); do
-                    FILE=$(echo "${PWD}/${DIR}/${SUBDIR}/${FILE}" | sed 's/ /\\ /g')
-                    echo "${FILE}"
-                    pandoc ${FILE} -s -t man -o ${FILE%.*}.manp
-                    gzip -k ${FILE%.*}.manp
-                    mv ${FILE%.*}.manp.gz ${PWD}/output/manpages/$(basename ${FILE%.*}).1.gz
-                    rm ${FILE%.*}.manp
-                done
-            fi
-        done
-    fi
-done
-IFS=$SAVEIFS
+find -L . -maxdepth 3 -mindepth 3 -name "*.md" -exec bash -c "md2mangz ${output_dir} {}" \;
