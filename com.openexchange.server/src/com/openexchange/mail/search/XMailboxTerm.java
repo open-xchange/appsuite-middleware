3e79c1c9b6a7c6c99f18d06c668f7e577b63fe64
thorben.betten <thorben.betten@open-xchange.com>
2019-11-14 10:29:04 +0100

    Change for bug 68211: Proper ignore-case equality check
    (cherry picked from commit aa3da62cccee8a7e20d385798426e17679809f05)

--- com.openexchange.server/src/com/openexchange/mail/search/XMailboxTerm.java
+++ com.openexchange.server/src/com/openexchange/mail/search/XMailboxTerm.java
@@ -110,7 +110,7 @@ public final class XMailboxTerm extends SearchTerm<String> {
             }
 
             String xMailbox = (String) ((IMAPMessage) msg).getItem("X-MAILBOX");
-            return null != xMailbox && xMailbox.equals(this.xMailboxValue);
+            return null != xMailbox && xMailbox.equalsIgnoreCase(this.xMailboxValue);
         } catch (Exception e) {
             org.slf4j.LoggerFactory.getLogger(XMailboxTerm.class).warn("Error during search.", e);
             return false;
