07604a0b9a0fc56d2063974985472dfe884d464c
thorben.betten <thorben.betten@open-xchange.com>
2019-11-19 12:21:29 +0100

    Change for bug 67286: Avoid rescheduling on registration of a new permanent push listener

--- com.openexchange.push.impl/src/com/openexchange/push/impl/balancing/reschedulerpolicy/PermanentListenerRescheduler.java
+++ com.openexchange.push.impl/src/com/openexchange/push/impl/balancing/reschedulerpolicy/PermanentListenerRescheduler.java
@@ -217,7 +217,7 @@ public class PermanentListenerRescheduler implements ServiceTrackerCustomizer<Ha
     @Override
     public void memberAdded(MembershipEvent membershipEvent) {
         try {
-            planReschedule(true);
+            planReschedule(true, "Cluster member added: " + membershipEvent.getMember().getAddress());
         } catch (Exception e) {
             LOG.error("Failed to plan rescheduling", e);
         }
@@ -226,7 +226,7 @@ public class PermanentListenerRescheduler implements ServiceTrackerCustomizer<Ha
     @Override
     public void memberRemoved(MembershipEvent membershipEvent) {
         try {
-            planReschedule(true);
+            planReschedule(true, "Cluster member removed: " + membershipEvent.getMember().getAddress());
         } catch (Exception e) {
             LOG.error("Failed to plan rescheduling", e);
         }
@@ -244,7 +244,7 @@ public class PermanentListenerRescheduler implements ServiceTrackerCustomizer<Ha
         if (UpdaterEventConstants.TOPIC.equals(event.getTopic())) {
             if (ReschedulePolicy.MASTER.equals(policy)) {
                 try {
-                    planReschedule(true);
+                    planReschedule(true, "Update tasks executed.");
                 } catch (Exception e) {
                     LOG.error("Failed to plan rescheduling", e);
                 }
@@ -252,7 +252,7 @@ public class PermanentListenerRescheduler implements ServiceTrackerCustomizer<Ha
                 // Only handle if locally received
                 if (event.containsProperty(CommonEvent.PUBLISH_MARKER)) {
                     try {
-                        planReschedule(true);
+                        planReschedule(true, "Update tasks executed.");
                     } catch (Exception e) {
                         LOG.error("Failed to plan rescheduling", e);
                     }
@@ -269,7 +269,7 @@ public class PermanentListenerRescheduler implements ServiceTrackerCustomizer<Ha
      * @param remotePlan <code>true</code> for remote rescheduling; otherwise <code>false</code>
      * @throws OXException If timer service is absent
      */
-    public void planReschedule(boolean remotePlan) throws OXException {
+    public void planReschedule(boolean remotePlan, String reason) throws OXException {
         synchronized (this) {
             // Stopped
             if (stopped) {
@@ -296,11 +296,11 @@ public class PermanentListenerRescheduler implements ServiceTrackerCustomizer<Ha
             // Plan rescheduling
             if (remotePlan) {
                 rescheduleQueue.offerOrReplace(ReschedulePlan.getInstance(true));
-                LOG.info("Planned rescheduling including remote plan");
+                LOG.info("Planned rescheduling including remote plan. Reason: {}", reason);
             } else {
                 boolean added = rescheduleQueue.offerIfAbsentElseReset(ReschedulePlan.getInstance(false));
                 if (added) {
-                    LOG.info("Planned rescheduling with local-only plan");
+                    LOG.info("Planned rescheduling with local-only plan. Reason: {}", reason);
                 }
             }
         }
@@ -447,7 +447,7 @@ public class PermanentListenerRescheduler implements ServiceTrackerCustomizer<Ha
 
             hzInstancerRef.set(hzInstance);
 
-            planReschedule(true);
+            planReschedule(true, "Hazelcast instance appeared");
 
             return hzInstance;
         } catch (Exception e) {
