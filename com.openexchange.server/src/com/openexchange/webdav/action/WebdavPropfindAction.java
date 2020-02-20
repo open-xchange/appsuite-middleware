9a9117729a28994d5304886b54aaa7a036bfb106
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2020-02-20 08:53:46 +0100

    MW-1330: Fix for litmus/props/propfind_invalid|propfind_invalid2 (PROPFIND with non-well-formed XML request body got 207 response not 400, PROPFIND with invalid namespace declaration in body (see FAQ) got 207 response not 400)

--- com.openexchange.server/src/com/openexchange/webdav/action/WebdavPropfindAction.java
+++ com.openexchange.server/src/com/openexchange/webdav/action/WebdavPropfindAction.java
@@ -61,7 +61,6 @@ import org.jdom2.output.XMLOutputter;
 import com.openexchange.webdav.loader.LoadingHints;
 import com.openexchange.webdav.protocol.Protocol;
 import com.openexchange.webdav.protocol.WebdavCollection;
-import com.openexchange.webdav.protocol.WebdavPath;
 import com.openexchange.webdav.protocol.WebdavProtocolException;
 import com.openexchange.webdav.xml.resources.PropertiesMarshaller;
 import com.openexchange.webdav.xml.resources.PropfindAllPropsMarshaller;
@@ -100,12 +99,8 @@ public class WebdavPropfindAction extends AbstractAction {
 		Document requestBody = null;
 		try {
 			requestBody = req.getBodyAsDocument();
-		} catch (JDOMException e1) {
-
-			forceAllProp = true; //Assume All Prop, if all else fails
-
-		} catch (IOException e1) {
-			throw WebdavProtocolException.Code.GENERAL_ERROR.create(new WebdavPath(), HttpServletResponse.SC_BAD_REQUEST);
+        } catch (IOException | JDOMException e) {
+            throw WebdavProtocolException.Code.GENERAL_ERROR.create(req.getUrl(), HttpServletResponse.SC_BAD_REQUEST, e);
 		}
 
         final LoadingHints loadingHints = new LoadingHints();
