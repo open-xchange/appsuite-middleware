be094eab8ba8d917a904ddfa6008b19ee5865a61
thorben.betten <thorben.betten@open-xchange.com>
2020-02-22 12:25:24 +0100

    [JavaMail]: Improve SMTP error message with server returns bad greeting - fix #387

--- javax.mail/src/com/sun/mail/smtp/SMTPTransport.java
+++ javax.mail/src/com/sun/mail/smtp/SMTPTransport.java
@@ -1,41 +1,17 @@
 /*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
  *
- * Copyright (c) 1997-2018 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
  *
- * The contents of this file are subject to the terms of either the GNU
- * General Public License Version 2 only ("GPL") or the Common Development
- * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License.  You can
- * obtain a copy of the License at
- * https://oss.oracle.com/licenses/CDDL+GPL-1.1
- * or LICENSE.txt.  See the License for the specific
- * language governing permissions and limitations under the License.
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
  *
- * When distributing the software, include this License Header Notice in each
- * file and include the License file at LICENSE.txt.
- *
- * GPL Classpath Exception:
- * Oracle designates this particular file as subject to the "Classpath"
- * exception as provided by Oracle in the GPL Version 2 section of the License
- * file that accompanied this code.
- *
- * Modifications:
- * If applicable, add the following below the License Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
- * "Portions Copyright [year] [name of copyright owner]"
- *
- * Contributor(s):
- * If you wish your version of this file to be governed by only the CDDL or
- * only the GPL Version 2, indicate your decision by adding "[Contributor]
- * elects to include this software in this distribution under the [CDDL or GPL
- * Version 2] license."  If you don't indicate a single choice of license, a
- * recipient has the option to distribute your version of this file under
- * either the CDDL, the GPL Version 2 or to extend the choice of license to
- * its licensees as provided above.  However, if you add GPL Version 2 code
- * and therefore, elected the GPL Version 2 license, then the option applies
- * only if the new code is made subject to such option by the copyright
- * holder.
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
 package com.sun.mail.smtp;
@@ -2221,67 +2197,67 @@ public class SMTPTransport extends Transport {
 				throws MessagingException {
 
         if (logger.isLoggable(Level.FINE))
-	    logger.fine("trying to connect to host \"" + host +
-				"\", port " + port + ", isSSL " + isSSL);
+        logger.fine("trying to connect to host \"" + host +
+                "\", port " + port + ", isSSL " + isSSL);
 
-	try {
-	    Properties props = session.getProperties();
+    try {
+        Properties props = session.getProperties();
 
-	    serverSocket = SocketFetcher.getSocket(host, port,
-		props, "mail." + name, isSSL);
+        serverSocket = SocketFetcher.getSocket(host, port,
+        props, "mail." + name, isSSL);
 
-	    // socket factory may've chosen a different port,
-	    // update it for the debug messages that follow
-	    port = serverSocket.getPort();
-	    this.port = port;
-	    // save host name for startTLS
-	    this.host = host;
+        // socket factory may've chosen a different port,
+        // update it for the debug messages that follow
+        port = serverSocket.getPort();
+        // save host name for startTLS
+        this.host = host;
 
-	    initStreams();
+        initStreams();
 
-	    int r = -1;
-	    if ((r = readServerResponse()) != 220) {
-           try {
-               if (quitOnSessionReject) {
-                   sendCommand("QUIT");
-                   if (quitWait) {
-                       int resp = readServerResponse();
-                       if (resp != 221 && resp != -1 &&
-                           logger.isLoggable(Level.FINE))
-                           logger.fine("QUIT failed with " + resp);
-                   }
-               }
-           } catch (Exception e) {
-               if (logger.isLoggable(Level.FINE))
-                   logger.log(Level.FINE, "QUIT failed", e);
-           } finally {
-               serverSocket.close();
-               serverSocket = null;
-               serverOutput = null;
-               serverInput = null;
-               lineInputStream = null;
-           }
-           if (logger.isLoggable(Level.FINE))
-               logger.fine("could not connect to host \"" +
-                       host + "\", port: " + port +
-                       ", response: " + r);
-           throw new MessagingException(
-                   "Could not connect to SMTP host: " + host +
-                   ", port: " + port +
-                   ", response: " + r);
-	    } else {
-		if (logger.isLoggable(Level.FINE))
-		    logger.fine("connected to host \"" +
-				       host + "\", port: " + port);
-	    }
-	} catch (UnknownHostException uhex) {
-	    throw new MessagingException("Unknown SMTP host: " + host, uhex);
-	} catch (SocketConnectException scex) {
-	    throw new MailConnectException(scex);
-	} catch (IOException ioe) {
-	    throw new MessagingException("Could not connect to SMTP host: " +
-				    host + ", port: " + port, ioe);
-	}
+        int r = -1;
+        if ((r = readServerResponse()) != 220) {
+        String failResponse = lastServerResponse;
+        try {
+            if (quitOnSessionReject) {
+            sendCommand("QUIT");
+            if (quitWait) {
+                int resp = readServerResponse();
+                if (resp != 221 && resp != -1 &&
+                    logger.isLoggable(Level.FINE))
+                logger.fine("QUIT failed with " + resp);
+            }
+            }
+        } catch (Exception e) {
+            if (logger.isLoggable(Level.FINE))
+            logger.log(Level.FINE, "QUIT failed", e);
+        } finally {
+            serverSocket.close();
+            serverSocket = null;
+            serverOutput = null;
+            serverInput = null;
+            lineInputStream = null;
+        }
+        if (logger.isLoggable(Level.FINE))
+            logger.fine("got bad greeting from host \"" +
+                host + "\", port: " + port +
+                ", response: " + failResponse);
+        throw new MessagingException(
+                "Got bad greeting from SMTP host: " + host +
+                ", port: " + port +
+                ", response: " + failResponse);
+        } else {
+        if (logger.isLoggable(Level.FINE))
+            logger.fine("connected to host \"" +
+                       host + "\", port: " + port);
+        }
+    } catch (UnknownHostException uhex) {
+        throw new MessagingException("Unknown SMTP host: " + host, uhex);
+    } catch (SocketConnectException scex) {
+        throw new MailConnectException(scex);
+    } catch (IOException ioe) {
+        throw new MessagingException("Could not connect to SMTP host: " +
+                    host + ", port: " + port, ioe);
+    }
     }
 
     /**
