13508882bea09329c14d33a5c4f6c7d179890716
thorben.betten <thorben.betten@open-xchange.com>
2020-03-22 09:36:36 +0100

    Fixed check for service absence

--- com.openexchange.server/src/com/openexchange/passwordchange/PasswordChangeService.java
+++ com.openexchange.server/src/com/openexchange/passwordchange/PasswordChangeService.java
@@ -165,10 +165,6 @@ public abstract class PasswordChangeService {
              * Check whether to verify old password prior to applying new one
              */
             boolean checkOldPassword = checkOldPassword(event);
-            final AuthenticationService authenticationService = Authentication.getService();
-            if (checkOldPassword && authenticationService == null) {
-                throw ServiceExceptionCode.SERVICE_UNAVAILABLE.create(AuthenticationService.class.getName());
-            }
             UserService userService = ServerServiceRegistry.getInstance().getService(UserService.class);
             if (null == userService) {
                 throw ServiceExceptionCode.SERVICE_UNAVAILABLE.create(UserService.class.getName());
@@ -207,7 +203,11 @@ public abstract class PasswordChangeService {
                     }
                     basicService.handleLoginInfo(user.getId(), session.getContextId(), event.getOldPassword());
                 } else {
-                    Authentication.login(new _LoginInfo(session.getLogin(), event.getOldPassword(), properties), authenticationService);
+                    AuthenticationService authenticationService = Authentication.getService();
+                    if (checkOldPassword && authenticationService == null) {
+                        throw ServiceExceptionCode.SERVICE_UNAVAILABLE.create(AuthenticationService.class.getName());
+                    }
+                    Authentication.login(new LoginInfoImpl(session.getLogin(), event.getOldPassword(), properties), authenticationService);
                 }
             }
         } catch (OXException e) {
@@ -363,23 +363,23 @@ public abstract class PasswordChangeService {
      */
 
     /**
-     * {@link _LoginInfo} - Simple class that implements {@link LoginInfo}
+     * {@link LoginInfoImpl} - Simple class that implements {@link LoginInfo}
      *
      * @author <a href="mailto:thorben.betten@open-xchange.com">Thorben Betten</a>
      */
-    protected static final class _LoginInfo implements LoginInfo {
+    protected static final class LoginInfoImpl implements LoginInfo {
 
         private final String pw;
         private final String loginInfo;
         private final Map<String, Object> properties;
 
         /**
-         * Initializes a new {@link _LoginInfo}
+         * Initializes a new {@link LoginInfoImpl}
          *
          * @param loginInfo The login info
          * @param pw The password
          */
-        public _LoginInfo(String loginInfo, String pw, Map<String, Object> properties) {
+        public LoginInfoImpl(String loginInfo, String pw, Map<String, Object> properties) {
             super();
             this.loginInfo = loginInfo;
             this.pw = pw;
