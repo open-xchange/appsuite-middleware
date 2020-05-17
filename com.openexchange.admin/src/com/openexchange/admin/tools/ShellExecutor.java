7b873af5f1ba508f28a8fd7190e58096418299be
thorben.betten <thorben.betten@open-xchange.com>
2020-05-17 10:39:06 +0200

    COV-20899: Dubious method used

--- com.openexchange.admin/src/com/openexchange/admin/tools/ShellExecutor.java
+++ com.openexchange.admin/src/com/openexchange/admin/tools/ShellExecutor.java
@@ -52,6 +52,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -99,7 +100,7 @@ public class ShellExecutor {
             }
 
             Path foundProgram = null;
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream(), StandardCharsets.UTF_8))) {
                 foundProgram = Paths.get(reader.readLine());
             }
             LOGGER.debug("{} has been found at : {}", desiredProgram, foundProgram);
