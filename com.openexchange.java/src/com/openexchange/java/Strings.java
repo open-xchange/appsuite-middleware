3c4fba1053d8e76fa98b17b985641ab5307fdb99
thorben.betten <thorben.betten@open-xchange.com>
2020-07-07 20:48:33 +0200

    Added missing DatabaseAccessProvider for mail compose that is needed in case an Sproxyd file storage is used
    (cherry picked from commit 7e6b8bd607ead7e4af8b8bba5743f64d68aa60e4)

--- com.openexchange.java/src/com/openexchange/java/Strings.java
+++ com.openexchange.java/src/com/openexchange/java/Strings.java
@@ -312,6 +312,39 @@ public class Strings {
     }
 
     /**
+     * Gets the digit for given character (between '0' inclusive and '9' inclusive).
+     *
+     * @param c The character
+     * @return The characters's digit representation or <code>-1</code>
+     */
+    public static int digitForChar(char c) {
+        switch (c) {
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            default:
+                return -1;
+        }
+    }
+
+    /**
      * High speed test for ASCII numbers!
      *
      * @param c The character to check
