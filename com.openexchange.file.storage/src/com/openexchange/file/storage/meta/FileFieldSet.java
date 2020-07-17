60ef7973ff8f3df61aca634bf1b2dad3c9da3a47
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2020-07-17 16:07:36 +0200

    restored previous parsing of "locked_until"

--- com.openexchange.file.storage/src/com/openexchange/file/storage/meta/FileFieldSet.java
+++ com.openexchange.file.storage/src/com/openexchange/file/storage/meta/FileFieldSet.java
@@ -172,10 +172,8 @@ public class FileFieldSet extends AbstractFileFieldSwitcher {
 
     @Override
     public Object lockedUntil(final Object... args) {
-        Date date = date(1, args);
-        if (date != null) {
-            md(args).setLockedUntil(date);
-        }
+        long value = longValue(1, args);
+        md(args).setLockedUntil(0 < value ? new Date(value) : null);
         return ret(args);
     }
 
