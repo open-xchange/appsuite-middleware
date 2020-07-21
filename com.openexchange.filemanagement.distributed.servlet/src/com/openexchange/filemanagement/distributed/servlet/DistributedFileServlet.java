8303fb2a694ff84d3d7af82dec342ca16736029c
thorben.betten <thorben.betten@open-xchange.com>
2020-07-21 14:31:04 +0200

    MWB-494: Don't make end-point for distributed files publicly accessible
    (cherry picked from commit 14789160655cd73807fc14413d5826d0a69a479d)

--- com.openexchange.filemanagement.distributed.servlet/src/com/openexchange/filemanagement/distributed/servlet/DistributedFileServlet.java
+++ com.openexchange.filemanagement.distributed.servlet/src/com/openexchange/filemanagement/distributed/servlet/DistributedFileServlet.java
@@ -57,7 +57,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.openexchange.config.ConfigurationService;
 import com.openexchange.exception.OXException;
 import com.openexchange.filemanagement.ManagedFile;
 import com.openexchange.filemanagement.ManagedFileManagement;
@@ -71,28 +70,51 @@ import com.openexchange.server.ServiceLookup;
  */
 public class DistributedFileServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -3293278521646131568L;
 
     private final ServiceLookup services;
 
-    private final ManagedFileManagement fileManagement;
-
-    private final int port;
-
-    public static final String PATH = "distributedFiles";
-
+    /**
+     * Initializes a new {@link DistributedFileServlet}.
+     *
+     * @param services The service look-up
+     */
     public DistributedFileServlet(ServiceLookup services) {
+        super();
         this.services = services;
-        fileManagement = services.getService(ManagedFileManagement.class);
-        port = services.getService(ConfigurationService.class).getIntProperty("com.openexchange.filemanagement.distributed.port", 2003);
+    }
+
+    private ManagedFileManagement getFileManagement(HttpServletResponse resp) throws IOException {
+        try {
+            ManagedFileManagement service = services.getOptionalService(ManagedFileManagement.class);
+            if (service == null) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Missing service");
+                return null;
+            }
+            return service;
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Missing service");
+            return null;
+        }
+    }
+
+    private static String crop(String uri) {
+        int pos = uri.lastIndexOf('/');
+        return pos < 0 ? uri : uri.substring(pos + 1);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ManagedFileManagement fileManagement = getFileManagement(resp);
+        if (fileManagement == null) {
+            return;
+        }
+
         String uri = req.getRequestURI();
         String id = crop(uri);
 
         if (!fileManagement.containsLocal(id)) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "No such file");
             return;
         }
 
@@ -117,13 +139,13 @@ public class DistributedFileServlet extends HttpServlet {
         }
     }
 
-    private String crop(String uri) {
-        String[] split = uri.split("/");
-        return split[split.length-1];
-    }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ManagedFileManagement fileManagement = getFileManagement(resp);
+        if (fileManagement == null) {
+            return;
+        }
+
         String uri = req.getRequestURI();
         String id = crop(uri);
 
@@ -140,6 +162,11 @@ public class DistributedFileServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ManagedFileManagement fileManagement = getFileManagement(resp);
+        if (fileManagement == null) {
+            return;
+        }
+
         String uri = req.getRequestURI();
         String id = crop(uri);
 
--- com.openexchange.filemanagement.distributed/src/com/openexchange/filemanagement/distributed/DistributedFileManagementImpl.java
+++ com.openexchange.filemanagement.distributed/src/com/openexchange/filemanagement/distributed/DistributedFileManagementImpl.java
@@ -68,6 +68,7 @@ import com.hazelcast.core.IMap;
 import com.openexchange.exception.OXException;
 import com.openexchange.filemanagement.DistributedFileManagement;
 import com.openexchange.filemanagement.ManagedFileExceptionErrorMessage;
+import com.openexchange.java.Streams;
 import com.openexchange.server.ServiceExceptionCode;
 import com.openexchange.server.ServiceLookup;
 import com.openexchange.threadpool.ThreadPoolService;
@@ -84,8 +85,6 @@ public class DistributedFileManagementImpl implements DistributedFileManagement 
     private static final int CONNECT_TIMEOUT = 3000;
 
     private static AtomicReference<HazelcastInstance> REFERENCE = new AtomicReference<HazelcastInstance>();
-
-    private static final String PATH = "distributedFiles";
 
     public static void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
         DistributedFileManagementImpl.REFERENCE.set(hazelcastInstance);
@@ -297,28 +296,30 @@ public class DistributedFileManagementImpl implements DistributedFileManagement 
 
     private InputStream loadFile(String url) throws IOException {
         URL remoteUrl = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) remoteUrl.openConnection();
-        con.setRequestMethod("GET");
-        con.setConnectTimeout(CONNECT_TIMEOUT);
-        con.setReadTimeout(READ_TIMEOUT);
-        con.connect();
 
-        boolean close = true;
+        HttpURLConnection con = null;
+        InputStream in = null;
         try {
+            con = (HttpURLConnection) remoteUrl.openConnection();
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(CONNECT_TIMEOUT);
+            con.setReadTimeout(READ_TIMEOUT);
+            con.connect();
+
             int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                final InputStream in = con.getInputStream();
-                close = false;
-                return in;
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                return null;
             }
-            return null;
+
+            in = con.getInputStream();
+            InputStream retval = in;
+            in = null; // Avoid premature closing
+            return retval;
         } finally {
-            if (close) {
-                try {
-                    // The only way to close a HttpURLConnection
-                    con.getInputStream().close();
-                } catch (Exception e) {
-                    // Ignore
+            if (in != null) {
+                Streams.close(in);
+                if (con != null) {
+                    con.disconnect();
                 }
             }
         }
