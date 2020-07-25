81e0e6a6e3b41600582229d9361670d7215ef368
thorben.betten <thorben.betten@open-xchange.com>
2020-07-25 10:41:27 +0200

    MWB-500: Avoid unnecessary string duplication

--- com.openexchange.html/src/com/openexchange/html/internal/SaneScriptTags.java
+++ com.openexchange.html/src/com/openexchange/html/internal/SaneScriptTags.java
@@ -91,58 +91,73 @@ public final class SaneScriptTags {
         if (com.openexchange.java.Strings.isEmpty(html)) {
             return html;
         }
-        StringBuffer sb = new StringBuffer(html);
-        decode(sb);
-        dropConcatenations(sb);
-        dropScriptTags(sanitized, sb);
-        return sanitized[0] ? sb.toString() : html;
+
+        html = decode(html);
+        html = dropConcatenations(html);
+        html = dropScriptTags(sanitized, html);
+
+        return html;
     }
 
-    private static final Pattern PAT_URLDECODE_ENTITIES = Pattern.compile("%([0-9a-fA-F]{2})");
     private static final Pattern PAT_URLDECODE_PERCENT = Pattern.compile("%25");
+
+    private static final Pattern PAT_URLDECODE_ENTITIES = Pattern.compile("%([23][2ceCE])");
     private static final Set<String> REPLACEES = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList("3c", "3e", "22")));
 
-    private static void decode(StringBuffer sb) {
-        if (sb.indexOf("%") < 0) {
-            return;
+    private static String decode(String html) {
+        if (html.indexOf('%') < 0) {
+            return html;
         }
-        Matcher m = PAT_URLDECODE_PERCENT.matcher(sb.toString());
-        sb.setLength(0);
-        while (m.find()) {
-            m.appendReplacement(sb, "%");
+
+        Matcher m = PAT_URLDECODE_PERCENT.matcher(html);
+        if (m.find()) {
+            StringBuffer sb = new StringBuffer(html.length());
+            do {
+                m.appendReplacement(sb, "%");
+            } while (m.find());
+            m.appendTail(sb);
+            html = sb.toString();
+            sb = null;
         }
-        m.appendTail(sb);
-        m = PAT_URLDECODE_ENTITIES.matcher(sb.toString());
-        if (!m.find()) {
-            return;
+
+        m = PAT_URLDECODE_ENTITIES.matcher(html);
+        if (m.find()) {
+            StringBuffer sb = new StringBuffer(html.length());
+            do {
+                final String entity = com.openexchange.java.Strings.toLowerCase(m.group(1));
+                if (REPLACEES.contains(entity)) {
+                    m.appendReplacement(sb, com.openexchange.java.Strings.quoteReplacement(Character.toString((char) Integer.parseInt(m.group(1), 16))));
+                } else {
+                    m.appendReplacement(sb, "$0");
+                }
+            } while (m.find());
+            m.appendTail(sb);
+            html = sb.toString();
+            sb = null;
         }
-        sb.setLength(0);
-        do {
-            final String entity = com.openexchange.java.Strings.toLowerCase(m.group(1));
-            if (REPLACEES.contains(entity)) {
-                m.appendReplacement(sb, com.openexchange.java.Strings.quoteReplacement(Character.toString((char) Integer.parseInt(m.group(1), 16))));
-            } else {
-                m.appendReplacement(sb, "$0");
-            }
-        } while (m.find());
-        m.appendTail(sb);
+
+        return html;
     }
 
     private static final Pattern PAT_CONCAT = Pattern.compile("[\"\u201d\u201c](\\+|%2b)[\"\u201d\u201c]");
 
-    private static void dropConcatenations(StringBuffer sb) {
-        if ((sb.indexOf("+") < 0) && (sb.indexOf("%2b") < 0)) {
-            return;
+    private static String dropConcatenations(String html) {
+        if ((html.indexOf('+') < 0) && (html.indexOf("%2b") < 0)) {
+            return html;
         }
-        Matcher m = PAT_CONCAT.matcher(sb.toString());
-        if (!m.find()) {
-            return;
+
+        Matcher m = PAT_CONCAT.matcher(html);
+        if (m.find()) {
+            StringBuffer sb = new StringBuffer(html.length());
+            do {
+                m.appendReplacement(sb, "");
+            } while (m.find());
+            m.appendTail(sb);
+            html = sb.toString();
+            sb = null;
         }
-        sb.setLength(0);
-        do {
-            m.appendReplacement(sb, "");
-        } while (m.find());
-        m.appendTail(sb);
+
+        return html;
     }
 
     private static final Pattern PATTERN_SCRIPT_TAG;
@@ -156,36 +171,42 @@ public final class SaneScriptTags {
         PATTERN_SCRIPT_TAG_END = Pattern.compile(regexScriptEnd, Pattern.CASE_INSENSITIVE);
     }
 
-    private static void dropScriptTags(boolean[] sanitized, StringBuffer sb) {
-        Matcher m = PATTERN_SCRIPT_TAG.matcher(sb.toString());
+    private static String dropScriptTags(boolean[] sanitized, String html) {
+        Matcher m = PATTERN_SCRIPT_TAG.matcher(html);
         if (m.find()) {
-            sb.setLength(0);
+            StringBuffer sb = new StringBuffer(html.length());
             do {
                 m.appendReplacement(sb, "");
                 sanitized[0] = true;
             } while (m.find());
             m.appendTail(sb);
-            return;
+            return sb.toString();
         }
-        m = PATTERN_SCRIPT_TAG_START.matcher(sb.toString());
-        if (!m.find()) {
-            return;
+
+        m = PATTERN_SCRIPT_TAG_START.matcher(html);
+        if (m.find()) {
+            StringBuffer sb = new StringBuffer(html.length());
+            do {
+                m.appendReplacement(sb, "");
+                sanitized[0] = true;
+            } while (m.find());
+            m.appendTail(sb);
+            html = sb.toString();
+            sb = null;
         }
-        sb.setLength(0);
-        do {
-            m.appendReplacement(sb, "");
-            sanitized[0] = true;
-        } while (m.find());
-        m.appendTail(sb);
-        m = PATTERN_SCRIPT_TAG_END.matcher(sb.toString());
-        if (!m.find()) {
-            return;
+
+        m = PATTERN_SCRIPT_TAG_END.matcher(html);
+        if (m.find()) {
+            StringBuffer sb = new StringBuffer(html.length());
+            do {
+                m.appendReplacement(sb, "");
+                sanitized[0] = true;
+            } while (m.find());
+            m.appendTail(sb);
+            html = sb.toString();
+            sb = null;
         }
-        sb.setLength(0);
-        do {
-            m.appendReplacement(sb, "");
-            sanitized[0] = true;
-        } while (m.find());
-        m.appendTail(sb);
+
+        return html;
     }
 }
