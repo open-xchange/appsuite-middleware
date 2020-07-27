c519cb3fc177195a9697711a92d22e5bb74185f5
Martin Schneider <martin.schneider@open-xchange.com>
2020-07-27 16:06:17 +0200

    Again minor changes wrt tests

--- openexchange-test/src/com/openexchange/ajax/mail/UpdateMailTest.java
+++ openexchange-test/src/com/openexchange/ajax/mail/UpdateMailTest.java
@@ -53,20 +53,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.util.Date;
+
 import org.json.JSONException;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
+
 import com.openexchange.ajax.folder.actions.DeleteRequest;
 import com.openexchange.ajax.folder.actions.EnumAPI;
 import com.openexchange.ajax.folder.actions.InsertRequest;
 import com.openexchange.ajax.folder.actions.InsertResponse;
-import com.openexchange.ajax.framework.UserValues;
 import com.openexchange.ajax.mail.actions.AllRequest;
 import com.openexchange.ajax.mail.actions.AllResponse;
 import com.openexchange.ajax.mail.actions.NewMailRequest;
@@ -86,23 +85,9 @@ import com.openexchange.server.impl.OCLPermission;
  *
  * @author <a href="mailto:tobias.prinz@open-xchange.com">Tobias Prinz</a>
  */
-@RunWith(BlockJUnit4ClassRunner.class)
 public class UpdateMailTest extends AbstractMailTest {
 
-    private UserValues values;
-
     @Override
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        values = getClient().getValues();
-        clearFolder(getSentFolder());
-        clearFolder(getInboxFolder());
-        clearFolder(getTrashFolder());
-    }
-
-    @Override
-    @After
     public void tearDown() throws Exception {
         try {
             clearFolder(getSentFolder());
@@ -115,8 +100,8 @@ public class UpdateMailTest extends AbstractMailTest {
 
     @Test
     public void testShouldBeAbleToAddFlags() throws OXException, IOException, JSONException {
-        final String eml = "Message-Id: <4A002517.4650.0059.1@foobar.com>\n" + "Date: Tue, 05 May 2009 11:37:58 -0500\n" + "From: " + getSendAddress() + "\n" + "To: " + getSendAddress() + "\n" + "Subject: Invitation for launch\n" + "Mime-Version: 1.0\n" + "Content-Type: text/plain; charset=\"UTF-8\"\n" + "Content-Transfer-Encoding: 8bit\n" + "\n" + "This is a MIME message. If you are reading this text, you may want to \n" + "consider changing to a mail reader or gateway that understands how to \n" + "properly handle MIME multipart messages.";
-        NewMailRequest newMailRequest = new NewMailRequest(values.getInboxFolder(), eml, -1, true);
+        final String eml = "Message-Id: <4A102517.4650.0059.1@foobar.com>\n" + "Date: Tue, 05 May 2009 11:37:58 -0500\n" + "From: " + getSendAddress() + "\n" + "To: " + getSendAddress() + "\n" + "Subject: Invitation for launch\n" + "Mime-Version: 1.0\n" + "Content-Type: text/plain; charset=\"UTF-8\"\n" + "Content-Transfer-Encoding: 8bit\n" + "\n" + "This is a MIME message. If you are reading this text, you may want to \n" + "consider changing to a mail reader or gateway that understands how to \n" + "properly handle MIME multipart messages.";
+        NewMailRequest newMailRequest = new NewMailRequest(getClient().getValues().getInboxFolder(), eml, -1, true);
         NewMailResponse newMailResponse = getClient().execute(newMailRequest);
         String folder = newMailResponse.getFolder();
         String id = newMailResponse.getId();
@@ -150,7 +135,7 @@ public class UpdateMailTest extends AbstractMailTest {
             {
                 final FolderObject fo = new FolderObject();
                 {
-                    final String inboxFolder = values.getInboxFolder();
+                    final String inboxFolder = getClient().getValues().getInboxFolder();
                     final String name = "TestFolder" + System.currentTimeMillis();
                     final String fullName = inboxFolder + "/" + name;
                     fo.setFullName(fullName);
@@ -221,140 +206,5 @@ public class UpdateMailTest extends AbstractMailTest {
                 }
             }
         }
-    }
-
-    @Test
-    @Ignore("always failing - obsolete, wrong behavior, unfixable or whatever")
-    public void testShouldBeAbleToAddColorLabel2AllMessages() throws OXException, IOException, JSONException {
-        String newId = null;
-        try {
-            /*
-             * Create new mail folder
-             */
-            {
-                final FolderObject fo = new FolderObject();
-                {
-                    final String inboxFolder = values.getInboxFolder();
-                    final String name = "TestFolder" + System.currentTimeMillis();
-                    final String fullName = inboxFolder + "/" + name;
-                    fo.setFullName(fullName);
-                    fo.setFolderName(name);
-                }
-                fo.setModule(FolderObject.MAIL);
-
-                final OCLPermission oclP = new OCLPermission();
-                oclP.setEntity(getClient().getValues().getUserId());
-                oclP.setGroupPermission(false);
-                oclP.setFolderAdmin(true);
-                oclP.setAllPermission(OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION, OCLPermission.ADMIN_PERMISSION);
-                fo.setPermissionsAsArray(new OCLPermission[] { oclP });
-
-                final InsertRequest request = new InsertRequest(EnumAPI.OUTLOOK, fo);
-                final InsertResponse response = getClient().execute(request);
-
-                newId = (String) response.getResponse().getData();
-            }
-            /*
-             * Append mails to new folder
-             */
-            {
-                final String eml = "Message-Id: <4A002517.4650.0059.1@deployfast.com>\n" + "X-Mailer: Novell GroupWise Internet Agent 8.0.0 \n" + "Date: Tue, 05 May 2009 11:37:58 -0500\n" + "From: " + getSendAddress() + "\n" + "To: " + getSendAddress() + "\n" + "Subject: Re: Your order for East Texas Lighthouse\n" + "Mime-Version: 1.0\n" + "Content-Type: text/plain; charset=\"UTF-8\"\n" + "Content-Transfer-Encoding: 8bit\n" + "\n" + "This is a MIME message. If you are reading this text, you may want to \n" + "consider changing to a mail reader or gateway that understands how to \n" + "properly handle MIME multipart messages.";
-
-                for (int i = 0; i < 10; i++) {
-                    final NewMailRequest newMailRequest = new NewMailRequest(newId, eml, -1, true);
-                    final NewMailResponse newMailResponse = getClient().execute(newMailRequest);
-                    final String folder = newMailResponse.getFolder();
-                    assertNotNull("Missing folder in response.", folder);
-                    assertNotNull("Missing ID in response.", newMailResponse.getId());
-                    assertEquals("Folder ID mismatch in newly appended message.", newId, folder);
-                }
-            }
-            /*
-             * Perform batch update call
-             */
-            final int colorLable = 5;
-            {
-                final UpdateMailRequest updateRequest = new UpdateMailRequest(newId);
-                updateRequest.setColor(colorLable);
-                final UpdateMailResponse updateResponse = getClient().execute(updateRequest);
-                assertEquals("Folder ID mismatch.", newId, updateResponse.getFolder());
-            }
-            /*
-             * Check
-             */
-            {
-                final AllRequest allRequest = new AllRequest(newId, new int[] { MailListField.ID.getField(), MailListField.COLOR_LABEL.getField() }, MailSortField.RECEIVED_DATE.getField(), Order.ASCENDING, true);
-                final AllResponse allResponse = getClient().execute(allRequest);
-                final Object[][] array = allResponse.getArray();
-                for (final Object[] arr : array) {
-                    final Integer label = (Integer) arr[1];
-                    assertEquals("Color label not set for message " + arr[0] + " in folder " + newId, colorLable, label.intValue());
-                }
-            }
-
-        } finally {
-            if (null != newId) {
-                // Delete folder
-                try {
-                    final DeleteRequest deleteRequest = new DeleteRequest(EnumAPI.OUTLOOK, newId, new Date());
-                    getClient().execute(deleteRequest);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public void notestShouldBeAbleToAddFlagsByMessageId() throws OXException, IOException, JSONException {
-        final String mail = values.getSendAddress();
-        sendMail(createEMail(mail, "Update test for adding and removing a flag by message id", "ALTERNATE", "Just a little bit").toString());
-        final TestMail myMail = new TestMail(getFirstMailInFolder(values.getInboxFolder()));
-
-        final String messageId;
-        {
-            final Object obj = myMail.getHeader("Message-ID");
-            if (null == obj) {
-                messageId = null;
-            } else {
-                messageId = obj.toString();
-            }
-        }
-        assertNotNull("Message-ID header not found.", messageId);
-
-        final UpdateMailRequest updateRequest = new UpdateMailRequest(myMail.getFolder(), messageId).setMessageId(true);
-        final int additionalFlag = MailMessage.FLAG_ANSWERED; //note: doesn't work for 16 (recent) and 64 (user)
-        updateRequest.setFlags(additionalFlag);
-        updateRequest.updateFlags();
-        UpdateMailResponse updateResponse = getClient().execute(updateRequest);
-
-        TestMail updatedMail = getMail(updateResponse.getFolder(), updateResponse.getID());
-        assertTrue("Flag should have been changed", (updatedMail.getFlags() & additionalFlag) == additionalFlag);
-
-        updateRequest.removeFlags();
-        updateResponse = getClient().execute(updateRequest);
-
-        updatedMail = getMail(updateResponse.getFolder(), updateResponse.getID());
-        assertTrue("Flag should have been changed back again", (updatedMail.getFlags() & additionalFlag) == 0);
-    }
-
-    public void notestShouldBeAbleToSetColors() throws OXException, IOException, JSONException {
-        final String mail = values.getSendAddress();
-        sendMail(createEMail(mail, "Update test for changing colors", "ALTERNATE", "Just a little bit").toString());
-        final TestMail myMail = new TestMail(getFirstMailInFolder(values.getInboxFolder()));
-
-        final UpdateMailRequest updateRequest = new UpdateMailRequest(myMail.getFolder(), myMail.getId());
-        int myColor = 8;
-        updateRequest.setColor(myColor);
-        UpdateMailResponse updateResponse = getClient().execute(updateRequest);
-
-        TestMail updatedMail = getMail(updateResponse.getFolder(), updateResponse.getID());
-        assertEquals("Color should have been changed", myColor, updatedMail.getColor());
-
-        myColor = 4;
-        updateRequest.setColor(myColor);
-        updateResponse = getClient().execute(updateRequest);
-
-        updatedMail = getMail(updateResponse.getFolder(), updateResponse.getID());
-        assertEquals("Color should have been changed again", myColor, updatedMail.getColor());
     }
 }
