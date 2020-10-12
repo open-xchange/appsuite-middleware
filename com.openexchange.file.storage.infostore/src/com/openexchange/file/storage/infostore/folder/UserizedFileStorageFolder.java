8817f4aaea16a7264cd2bfa9a84c7353a61ac917
Tobias Friedrich <tobias.friedrich@open-xchange.com>
2020-10-12 14:40:28 +0200

    Use shared conversion routine for file storage folders

--- com.openexchange.file.storage.infostore/src/com/openexchange/file/storage/infostore/folder/UserizedFileStorageFolder.java
+++ com.openexchange.file.storage.infostore/src/com/openexchange/file/storage/infostore/folder/UserizedFileStorageFolder.java
@@ -49,34 +49,20 @@
 
 package com.openexchange.file.storage.infostore.folder;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import static com.openexchange.folderstorage.filestorage.FileStorageUtils.getFileStorageFolderType;
+import static com.openexchange.folderstorage.filestorage.FileStorageUtils.getFileStoragePermission;
+import static com.openexchange.folderstorage.filestorage.FileStorageUtils.getFileStoragePermissions;
 import java.util.Locale;
-import com.openexchange.exception.OXException;
 import com.openexchange.file.storage.CacheAware;
 import com.openexchange.file.storage.DefaultFileStorageFolder;
-import com.openexchange.file.storage.DefaultFileStoragePermission;
-import com.openexchange.file.storage.FileStorageExceptionCodes;
 import com.openexchange.file.storage.FileStorageFolder;
-import com.openexchange.file.storage.FileStorageFolderPermissionType;
 import com.openexchange.file.storage.FileStorageFolderType;
-import com.openexchange.file.storage.FileStoragePermission;
 import com.openexchange.file.storage.FolderPath;
 import com.openexchange.file.storage.OriginAwareFileStorageFolder;
 import com.openexchange.file.storage.TypeAware;
 import com.openexchange.file.storage.composition.FileID;
 import com.openexchange.file.storage.composition.FolderID;
-import com.openexchange.folderstorage.FolderPermissionType;
-import com.openexchange.folderstorage.Permission;
 import com.openexchange.folderstorage.UserizedFolder;
-import com.openexchange.folderstorage.type.DocumentsType;
-import com.openexchange.folderstorage.type.MusicType;
-import com.openexchange.folderstorage.type.PicturesType;
-import com.openexchange.folderstorage.type.PublicType;
-import com.openexchange.folderstorage.type.TemplatesType;
-import com.openexchange.folderstorage.type.TrashType;
-import com.openexchange.folderstorage.type.VideosType;
 import com.openexchange.i18n.LocaleTools;
 
 /**
@@ -96,7 +82,7 @@ public class UserizedFileStorageFolder extends DefaultFileStorageFolder implemen
      *
      * @param folder The userized folder to construct the file storage folder from
      */
-    public UserizedFileStorageFolder(UserizedFolder folder) throws OXException {
+    public UserizedFileStorageFolder(UserizedFolder folder) {
         super();
         this.folder = folder;
         setCreationDate(folder.getCreationDateUTC());
@@ -107,15 +93,15 @@ public class UserizedFileStorageFolder extends DefaultFileStorageFolder implemen
         String defaultName = folder.getLocalizedName(LocaleTools.DEFAULT_LOCALE, folder.isAltNames());
         setName(null != defaultName ? defaultName : folder.getName());
         setParentId(folder.getParentID());
-        setPermissions(parsePermission(folder.getPermissions()));
-        setOwnPermission(parsePermission(folder.getOwnPermission()));
+        setPermissions(getFileStoragePermissions(folder.getPermissions()));
+        setOwnPermission(getFileStoragePermission(folder.getOwnPermission()));
         setRootFolder(folder.getParentID() == null);
         setSubscribed(folder.isSubscribed());
         setSubscribedSubfolders(folder.hasSubscribedSubfolders());
         String[] subfolderIDs = folder.getSubfolderIDs();
         setSubfolders(subfolderIDs != null && subfolderIDs.length > 0);
         FolderID folderID = new FolderID(folder.getID());
-        setType(getType(folder.getType()));
+        setType(getFileStorageFolderType(folder.getType()));
         setCreatedBy(folder.getCreatedBy());
         setModifiedBy(folder.getModifiedBy());
         /*
@@ -162,98 +148,6 @@ public class UserizedFileStorageFolder extends DefaultFileStorageFolder implemen
     @Override
     public String toString() {
         return "UserizedFileStorageFolder [id=" + id + ", name=" + name + "]";
-    }
-
-    /**
-     * Parses given permission.
-     *
-     * @param permission The permission to parse
-     * @return The parsed permission
-     * @throws OXException If parsing fails
-     */
-    private static FileStoragePermission parsePermission(final Permission permission) throws OXException {
-        if (null == permission) {
-            return null;
-        }
-        try {
-            final int entity = permission.getEntity();
-            final DefaultFileStoragePermission oclPerm = DefaultFileStoragePermission.newInstance();
-            oclPerm.setType(parsePermissionType(permission.getType()));
-            oclPerm.setEntity(entity);
-            oclPerm.setGroup(permission.isGroup());
-            oclPerm.setAdmin(permission.isAdmin());
-            oclPerm.setAllPermissions(
-                permission.getFolderPermission(),
-                permission.getReadPermission(),
-                permission.getWritePermission(),
-                permission.getDeletePermission());
-                return (oclPerm);
-        } catch (RuntimeException e) {
-            throw FileStorageExceptionCodes.UNEXPECTED_ERROR.create(e, e.getMessage());
-        }
-    }
-
-    private static FileStorageFolderPermissionType parsePermissionType(FolderPermissionType type) {
-        if (null == type) {
-            return null;
-        }
-        switch (type) {
-            case INHERITED:
-                return FileStorageFolderPermissionType.INHERITED;
-            case LEGATOR:
-                return FileStorageFolderPermissionType.LEGATOR;
-            default:
-                return FileStorageFolderPermissionType.NORMAL;
-        }
-    }
-
-    /**
-     * Parses given permissions.
-     *
-     * @param permissions The permissions to parse
-     * @return The parsed permissions
-     * @throws OXException If parsing fails
-     */
-    private static List<FileStoragePermission> parsePermission(Permission[] permissions) throws OXException {
-        if (null == permissions || 0 == permissions.length) {
-            return Collections.emptyList();
-        }
-        List<FileStoragePermission> perms = new ArrayList<FileStoragePermission>(permissions.length);
-        for (Permission permission : permissions) {
-            perms.add(parsePermission(permission));
-        }
-        return perms;
-    }
-
-    /**
-     * Determines the file storage folder type matching the supplied folderstorage type.
-     *
-     * @param type The folder storage type to get the file storage folder type for
-     * @return The file storage folder type, or {@link FileStorageFolderType#NONE} if not matching folder type was detected
-     */
-    private static FileStorageFolderType getType(com.openexchange.folderstorage.Type type) {
-        if (TrashType.getInstance().equals(type)) {
-            return FileStorageFolderType.TRASH_FOLDER;
-        }
-        if (PublicType.getInstance().equals(type)) {
-            return FileStorageFolderType.PUBLIC_FOLDER;
-        }
-        if (PicturesType.getInstance().equals(type)) {
-            return FileStorageFolderType.PICTURES_FOLDER;
-        }
-        if (DocumentsType.getInstance().equals(type)) {
-            return FileStorageFolderType.DOCUMENTS_FOLDER;
-        }
-        if (MusicType.getInstance().equals(type)) {
-            return FileStorageFolderType.MUSIC_FOLDER;
-        }
-        if (VideosType.getInstance().equals(type)) {
-            return FileStorageFolderType.VIDEOS_FOLDER;
-        }
-        if (TemplatesType.getInstance().equals(type)) {
-            return FileStorageFolderType.TEMPLATES_FOLDER;
-        }
-        return FileStorageFolderType.NONE;
     }
 
     @Override
