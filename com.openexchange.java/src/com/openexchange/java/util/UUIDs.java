ddbf1aee3d2997a08f6440a81d73d53c8ee66d62
thorben.betten <thorben.betten@open-xchange.com>
2020-10-18 10:23:22 +0200

    JavaDoc

--- com.openexchange.java/src/com/openexchange/java/util/UUIDs.java
+++ com.openexchange.java/src/com/openexchange/java/util/UUIDs.java
@@ -97,14 +97,12 @@ public final class UUIDs {
     }
 
     /**
-     * Gets the unformatted string representation from a random {@link UUID} instance.<br>
-     * Example:<br>
-     * <code>
-     * &nbsp;&nbsp;067e6162-3b6f-4ae2-a171-2470b63dff00
-     * </code><br>
-     * is converted to<br>
-     * <code>
-     * &nbsp;&nbsp;067e61623b6f4ae2a1712470b63dff00
+     * Gets the unformatted string representation from a random {@link UUID} instance.
+     * <p>
+     * Example:
+     * <pre>067e6162-3b6f-4ae2-a171-2470b63dff00</pre>
+     * is converted to
+     * <pre>067e61623b6f4ae2a1712470b63dff00</pre>nbsp;067e61623b6f4ae2a1712470b63dff00
      * </code>
      *
      * @param uuid The {@link UUID} instance
