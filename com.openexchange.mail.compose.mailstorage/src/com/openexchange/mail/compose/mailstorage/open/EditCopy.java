a0e87e4ff7f3a401c6ae4b36263dbce4951c4ff6
Alexander Schulze-Ardey <alexander.schulze-ardey@open-xchange.com>
2020-11-17 08:51:41 +0100

    COV-21335 DLS: Dead local store

--- com.openexchange.mail.compose.mailstorage/src/com/openexchange/mail/compose/mailstorage/open/EditCopy.java
+++ com.openexchange.mail.compose.mailstorage/src/com/openexchange/mail/compose/mailstorage/open/EditCopy.java
@@ -266,7 +266,6 @@ public class EditCopy extends AbstractOpener {
 
         // Check if original mail may contain attachments
         if (multipart) {
-            Optional<Boolean> optionalEncrypt = Optional.of(state.encrypt);
             // Add mail's non-inline parts
             {
                 NonInlineForwardPartHandler handler = new NonInlineForwardPartHandler();
