886241e1372694c1c93a47da9b05262e6136b5fa
thorben.betten <thorben.betten@open-xchange.com>
2020-12-07 22:59:52 +0100

    Use faster replace methods

--- com.openexchange.config.cascade.context/src/com/openexchange/config/cascade/context/BasicPropertyImpl.java
+++ com.openexchange.config.cascade.context/src/com/openexchange/config/cascade/context/BasicPropertyImpl.java
@@ -62,6 +62,7 @@ import com.openexchange.context.ContextService;
 import com.openexchange.exception.OXException;
 import com.openexchange.groupware.contexts.Context;
 import com.openexchange.java.ConvertUtils;
+import com.openexchange.java.Strings;
 import com.openexchange.server.ServiceExceptionCode;
 import com.openexchange.server.ServiceLookup;
 
@@ -196,7 +197,8 @@ final class BasicPropertyImpl implements BasicProperty {
         String value = values.get(0);
         int pos = value.indexOf(';');
         if (pos < 0) {
-            value = value.replace("%3B", ";").replace("%25", "%");
+            value = Strings.replaceSequenceWith(value, "%3B", ";");
+            value = Strings.replaceSequenceWith(value, "%25", "%");
             this.value = ConvertUtils.loadConvert(value);
             // Assume "protected=true" by default
             metadata = new HashMap<String, String>(2);
@@ -207,7 +209,8 @@ final class BasicPropertyImpl implements BasicProperty {
 
         // Parameters available
         String params = value.substring(pos).trim();
-        value = value.substring(0, pos).trim().replace("%3B", ";").replace("%25", "%");
+        value = Strings.replaceSequenceWith(value.substring(0, pos).trim(), "%3B", ";");
+        value = Strings.replaceSequenceWith(value, "%25", "%");
         this.value = ConvertUtils.loadConvert(value);
         metadata = new LinkedHashMap<String, String>(2);
         pos = 0;
