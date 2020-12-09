37fdc2451b34a093c09293e6cf29abfbabea11ef
thorben.betten <thorben.betten@open-xchange.com>
2020-12-09 18:29:10 +0100

    MWB-666: Orderly advertise "java.lang.IllegalStateException: HTTP packet header is too large" error to client #2

--- com.openexchange.http.grizzly/src/org/glassfish/grizzly/http/server/CustomHttpCodecFilter.java
+++ com.openexchange.http.grizzly/src/org/glassfish/grizzly/http/server/CustomHttpCodecFilter.java
@@ -85,32 +85,34 @@ public class CustomHttpCodecFilter extends org.glassfish.grizzly.http.HttpServer
     @Override
     protected void onHttpHeaderError(HttpHeader httpHeader, FilterChainContext ctx, Throwable t) throws IOException {
         if (t instanceof IllegalStateException && "HTTP packet header is too large".equals(t.getMessage())) {
-            final HttpRequestPacket request = (HttpRequestPacket) httpHeader;
-            final HttpResponsePacket response = request.getResponse();
-
-            sendRequestEntityTooLargeResponse(ctx, response);
+            /*-
+             * Handle special exception when HTTP packet header is too large:
+             *
+             * java.lang.IllegalStateException: HTTP packet header is too large
+             *    at org.glassfish.grizzly.http.HttpCodecFilter$HeaderParsingState.checkOverflow(HttpCodecFilter.java:2060)
+             *    at org.glassfish.grizzly.http.HttpCodecFilter.decodeHttpPacketFromBytes(HttpCodecFilter.java:748)
+             *    at org.glassfish.grizzly.http.HttpCodecFilter.decodeHttpPacket(HttpCodecFilter.java:717)
+             *    at org.glassfish.grizzly.http.HttpCodecFilter.handleRead(HttpCodecFilter.java:565)
+             *    ...
+             */
+            HttpResponsePacket response = ((HttpRequestPacket) httpHeader).getResponse();
+            sendHttpPacketHeaderTooLargeResponse(ctx, response);
         } else {
             super.onHttpHeaderError(httpHeader, ctx, t);
         }
     }
 
-    private void sendRequestEntityTooLargeResponse(final FilterChainContext ctx, final HttpResponsePacket response) {
+    private void sendHttpPacketHeaderTooLargeResponse(final FilterChainContext ctx, final HttpResponsePacket response) {
         if (response.getHttpStatus().getStatusCode() < 400) {
             // 413 - Request entity too large
             HttpStatus.BAD_REQUEST_400.setValues(response);
         }
-        commitAndCloseAsError(ctx, response, HttpStatus.BAD_REQUEST_400, "Request headers or cookies too large. Please check your cookies.");
-    }
 
-    /*
-     * caller has the responsibility to set the status of th response.
-     */
-    private void commitAndCloseAsError(FilterChainContext ctx, HttpResponsePacket response, HttpStatus httpStatus, String desc) {
-        byte[] errorPage = getErrorPage(httpStatus.getStatusCode(), httpStatus.getReasonPhrase(),  desc).getBytes(StandardCharsets.UTF_8);
-
+        byte[] errorPage = getHttpPacketHeaderTooLargeErrorPage().getBytes(StandardCharsets.UTF_8);
         response.setContentLength(errorPage.length);
         response.setContentType("text/html; charset=UTF-8");
         HttpContent errorHttpResponse = HttpContent.builder(response).content(new ByteBufferWrapper(ByteBuffer.wrap(errorPage))).last(true).build();
+        errorPage = null;
 
         Buffer resBuf = encodeHttpPacket(ctx, errorHttpResponse);
         ctx.write(resBuf);
@@ -125,33 +127,55 @@ public class CustomHttpCodecFilter extends org.glassfish.grizzly.http.HttpServer
      * @param desc The optional status description; e.g. <code>"The requested URL was not found on this server."</code>
      * @return A simple error page
      */
-    private static String getErrorPage(int statusCode, String msg, String desc) {
+    private static String getHttpPacketHeaderTooLargeErrorPage() {
+        int statusCode = HttpStatus.BAD_REQUEST_400.getStatusCode();
+        String msg = "Request Header Or Cookie Too Large";
+
         StringBuilder sb = new StringBuilder(512);
         String lineSep = System.getProperty("line.separator");
         sb.append("<!DOCTYPE html>").append(lineSep);
         sb.append("<html><head>").append(lineSep);
-        {
-            sb.append("<title>").append(statusCode);
-            if (null != msg) {
-                sb.append(' ').append(msg);
-            }
-            sb.append("</title>").append(lineSep);
-        }
+
+        sb.append("<title>").append(statusCode);
+        sb.append(' ').append(msg);
+        sb.append("</title>").append(lineSep);
 
         sb.append("</head><body>").append(lineSep);
 
         sb.append("<h1>");
-        if (null == msg) {
-            sb.append(statusCode);
-        } else {
-            sb.append(msg);
-        }
+        sb.append(msg);
         sb.append("</h1>").append(lineSep);
 
-        String desc0 = null == desc ? msg : desc;
-        if (null != desc0) {
-            sb.append("<p>").append(desc0).append("</p>").append(lineSep);
-        }
+        sb.append("<p>").append(lineSep);
+        sb.append("The error might occur when the server detects that the size of cookies for the domain you are visiting is too large.").append(lineSep);
+        sb.append("It might also occur when the server finds that some of the cookies are corrupted.").append(lineSep);
+        sb.append("</p>").append(lineSep);
+
+        sb.append("<p>").append(lineSep);
+        sb.append("To clear the error message you can try clearing the cookies for that particular domain.").append(lineSep);
+        sb.append("Each browser has unique instructions for clearing the cache and cookies. Instructions for commonly used browser are listed below.").append(lineSep);
+        sb.append("</p>").append(lineSep);
+
+        sb.append("<ul>").append(lineSep);
+        sb.append("    <li>").append(lineSep);
+        sb.append("    <p><a href=\"https://support.google.com/accounts/answer/32050?co=GENIE.Platform=Desktop&amp;hl=en\">Chrome</a><a href=\"https://support.google.com/accounts/answer/32050?co=GENIE.Platform=Desktop&amp;hl=en\">: Clear Cache and Cookies (Google Support)</a></p>").append(lineSep);
+        sb.append("    </li>").append(lineSep);
+        sb.append("    <li>").append(lineSep);
+        sb.append("    <p><a href=\"http://www.wikihow.com/Clear-Cache-and-Cookies#Mozilla_Firefox_sub\">Firefox</a><a href=\"https://support.mozilla.org/en-US/kb/how-clear-firefox-cache\">: How to Clear the Firefox Cache (Mozilla Support)</a></p>").append(lineSep);
+        sb.append("    </li>").append(lineSep);
+        sb.append("    <li>").append(lineSep);
+        sb.append("    <p id=\"s-lg-content-20145857\"><a href=\"http://www.wikihow.com/Clear-Cache-and-Cookies#Internet_Explorer_.28IE.29_sub\">Internet Explorer</a><a href=\"https://support.microsoft.com/en-us/help/17438/windows-internet-explorer-view-delete-browsing-history\">: View and Delete your Browsing History in Internet Explorer (Microsoft Support)</a></p>").append(lineSep);
+        sb.append("    </li>").append(lineSep);
+        sb.append("    <li>").append(lineSep);
+        sb.append("    <p><a href=\"http://windows.microsoft.com/en-us/windows-10/view-delete-browsing-history-microsoft-edge\">Microsoft Edge</a><a href=\"https://support.microsoft.com/en-us/help/10607/microsoft-edge-view-delete-browser-history\">: View and Delete Browsing History in Microsoft Edge (Microsoft Edge Support)</a></p>").append(lineSep);
+        sb.append("    </li>").append(lineSep);
+        sb.append("    <li>").append(lineSep);
+        sb.append("    <p><a href=\"http://www.wikihow.com/Clear-Cache-and-Cookies#Apple_Safari_sub\">Safari</a><a href=\"https://support.apple.com/guide/safari/clear-your-browsing-history-sfri47acf5d6/mac\">: Clear your browsing history (Apple Support)</a></p>").append(lineSep);
+        sb.append("    </li>").append(lineSep);
+        sb.append("    <li>").append(lineSep);
+        sb.append("    <p><a href=\"http://www.wikihow.com/Clear-Cache-and-Cookies#iOS_sub\">A</a><a href=\"https://support.apple.com/en-us/HT201265\">pple iOS: Delete history. cache, and cookies (Apple Support)</a></p>").append(lineSep);
+        sb.append("    </li>").append(lineSep);
+        sb.append("</ul>").append(lineSep);
 
         sb.append("</body></html>").append(lineSep);
         return sb.toString();
