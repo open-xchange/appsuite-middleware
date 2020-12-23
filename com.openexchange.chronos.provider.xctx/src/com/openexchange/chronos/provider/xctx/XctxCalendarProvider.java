e9e06e8f1f5fbcbd7a605b141d87057315ba7135
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2020-12-23 09:11:26 +0100

    MWB-819: Follow-up, prevent NPE

--- com.openexchange.chronos.provider.xctx/src/com/openexchange/chronos/provider/xctx/XctxCalendarProvider.java
+++ com.openexchange.chronos.provider.xctx/src/com/openexchange/chronos/provider/xctx/XctxCalendarProvider.java
@@ -217,10 +217,12 @@ public class XctxCalendarProvider implements FolderCalendarProvider, FallbackAwa
         if (null != internalConfig && JSONObject.class.isInstance(internalConfig)) {
             return (JSONObject) internalConfig;
         }
-        internalConfig = calendarAccount.getInternalConfiguration();
-        if (null != internalConfig && JSONObject.class.isInstance(internalConfig)) {
-            ((JSONObject)internalConfig).remove("lastError");
-            return (JSONObject) internalConfig;
+        if (null != calendarAccount) {
+            internalConfig = calendarAccount.getInternalConfiguration();
+            if (null != internalConfig && JSONObject.class.isInstance(internalConfig)) {
+                ((JSONObject) internalConfig).remove("lastError");
+                return (JSONObject) internalConfig;
+            }
         }
         return null != calendarAccount ? calendarAccount.getInternalConfiguration() : new JSONObject();
     }
