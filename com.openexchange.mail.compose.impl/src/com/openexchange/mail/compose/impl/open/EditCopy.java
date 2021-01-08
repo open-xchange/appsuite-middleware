f8ce26870ce19536ddeb9632ed4a5bc885d8bc23
thorben.betten <thorben.betten@open-xchange.com>
2021-01-08 12:16:22 +0100

    MWB-851: Discard personal part when pre-filling the "From" address
    (cherry picked from commit 8e468c250fc1997630f023e4d86c52ff64d8558b)

--- com.openexchange.mail.compose.impl/src/com/openexchange/mail/compose/impl/open/EditCopy.java
+++ com.openexchange.mail.compose.impl/src/com/openexchange/mail/compose/impl/open/EditCopy.java
@@ -162,7 +162,7 @@ public class EditCopy extends AbstractOpener {
         {
             InternetAddress[] from = originalMail.getFrom();
             if (null != from && from.length > 0) {
-                state.message.setFrom(toAddress(from[0]));
+                state.message.setFrom(toAddress(from[0], false));
             }
         }
 
@@ -170,7 +170,7 @@ public class EditCopy extends AbstractOpener {
         {
             InternetAddress[] replyTo = originalMail.getReplyTo();
             if (null != replyTo && replyTo.length > 0) {
-                state.message.setReplyTo(toAddress(replyTo[0]));
+                state.message.setReplyTo(toAddress(replyTo[0], true));
             }
         }
 
