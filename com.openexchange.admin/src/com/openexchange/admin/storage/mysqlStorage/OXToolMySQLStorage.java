eae116fa9b738ed9b31da9a4617df2dc5863e8ae
thorben.betten <thorben.betten@open-xchange.com>
2021-02-22 10:24:18 +0100

    MWB-958: Check case-sensitive for equal context names when attempting to change a context's name

--- com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXToolMySQLStorage.java
+++ com.openexchange.admin/src/com/openexchange/admin/storage/mysqlStorage/OXToolMySQLStorage.java
@@ -2582,7 +2582,7 @@ public class OXToolMySQLStorage extends OXToolSQLStorage implements OXMySQLDefau
             if (null == foundName) {
                 throw new NoSuchContextException();
             }
-            return !name.equalsIgnoreCase(foundName);
+            return !name.equals(foundName);
         } catch (PoolException e) {
             log.error("Pool Error", e);
             throw new StorageException(e);
