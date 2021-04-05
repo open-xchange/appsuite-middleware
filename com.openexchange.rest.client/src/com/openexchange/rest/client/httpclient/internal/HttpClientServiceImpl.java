0a5f4ea1f9c5a29388d5a89b2581e5e9b3120f00
thorben.betten <thorben.betten@open-xchange.com>
2021-04-05 22:39:42 +0200

    MWB-1029: Fixed typo in property name prefix "com.openenexchange." --> "com.openexchange."

--- com.openexchange.rest.client/src/com/openexchange/rest/client/httpclient/internal/HttpClientServiceImpl.java
+++ com.openexchange.rest.client/src/com/openexchange/rest/client/httpclient/internal/HttpClientServiceImpl.java
@@ -457,8 +457,7 @@ public class HttpClientServiceImpl implements HttpClientService, ServiceTrackerC
     }
 
     /**
-     * Adjusts configuration based on properties that start with
-     * {@value HttpClientProperty#PREFIX} and the given identifier.
+     * Adjusts configuration based on properties that start with <code>"com.openexchange.httpclient."</code> prefix and the given identifier.
      * <p>
      * If a value for a specified property is set, this will overwrite
      * the set value in the configuration.
@@ -472,8 +471,7 @@ public class HttpClientServiceImpl implements HttpClientService, ServiceTrackerC
     }
 
     /**
-     * Adjusts configuration based on properties that start with
-     * {@value HttpClientProperty#PREFIX} and the given identifier.
+     * Adjusts configuration based on properties that start with <code>"com.openexchange.httpclient."</code> prefix and the given identifier.
      * <p>
      * If a value for a specified property is set, this will overwrite
      * the set value in the configuration.
