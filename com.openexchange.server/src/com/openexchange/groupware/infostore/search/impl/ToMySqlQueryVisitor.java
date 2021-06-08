f223964bc65c4af5592096cb83932f105f1a0a9e
Jan Bauerdick <jan.bauerdick@open-xchange.com>
2021-06-08 18:24:36 +0200

    MWB-1058: Don't use fulltext index for 'exact-match' search requests

--- com.openexchange.server/src/com/openexchange/groupware/infostore/search/impl/ToMySqlQueryVisitor.java
+++ com.openexchange.server/src/com/openexchange/groupware/infostore/search/impl/ToMySqlQueryVisitor.java
@@ -105,8 +105,8 @@ public class ToMySqlQueryVisitor implements SearchTermVisitor {
     private final List<Integer> readAllFolders;
     private final List<Integer> readOwnFolders;
     private final ServerSession session;
-    private final boolean fulltextSearch;
     private final int minimumPatternLength;
+    private boolean fulltextSearch;
     private String fulltextSearchPattern;
 
     /**
@@ -601,6 +601,11 @@ public class ToMySqlQueryVisitor implements SearchTermVisitor {
     }
 
     private void parseFulltextStringSearchTerm(AbstractStringSearchTerm searchTerm) throws OXException {
+        if (isExactMatchTerm(searchTerm)) {
+            // Fulltext index is not suitable for exact match searches, disable fulltext index matching for search request
+            this.fulltextSearch = false;
+            return;
+        }
         String pattern = searchTerm.getPattern();
         if (Strings.isNotEmpty(fulltextSearchPattern) && false == fulltextSearchPattern.equals(pattern)) {
             LOG.warn("FULLTEXT search pattern changed while processing search term.");
@@ -610,6 +615,10 @@ public class ToMySqlQueryVisitor implements SearchTermVisitor {
             throw InfostoreExceptionCodes.PATTERN_NEEDS_MORE_CHARACTERS.create(I(minimumPatternLength));
         }
         this.fulltextSearchPattern = pattern;
+    }
+
+    private boolean isExactMatchTerm(AbstractStringSearchTerm searchTerm) {
+        return false == searchTerm.isIgnoreCase() && false == searchTerm.isSubstringSearch();
     }
 
     private void parseStringSearchTerm(AbstractStringSearchTerm searchTerm, String field) {
